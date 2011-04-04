78d4d33f046695c150dcd4ea40e7f70646d56ecb
DeadNightWarrior <DeadNightWarrior@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2011-04-04 21:16:35 +0000

    Fixed some targeting bugs and added new dynamic collision volumes to some buildings.

--- luarules/configs/collisionvolumes.lua
+++ luarules/configs/collisionvolumes.lua
@@ -45,41 +45,53 @@ local unitCollisionVolume = {}
 		on={54,81,54,0,-2,0,2,1,0},
 		off={54,56,54,0,-15,0,2,1,0},
 	}
+	unitCollisionVolume["armlab"] = {
+		on={95,28,95,0,2,0,2,1,0},
+		off={95,22,95,0,-1,0,1,1,1},
+	}
 	unitCollisionVolume["armmmkr"] = {
 		on={60,72,60,0,7,0,1,1,1},
 		off={60,50,60,0,-4,0,1,1,1},
 	}
 	unitCollisionVolume["armpb"] = {
 		on={39,88,39,0,-14,0,0,1,0},
-		off={39,53,39,0,-20,0,0,1,0},
+		off={39,55,39,0,-27,0,0,1,0},
 	}
 	unitCollisionVolume["armplat"] = {
 		on={105,66,105,0,33,0,2,1,0},
 		off={105,44,105,0,0,0,2,1,0},
 	}
 	unitCollisionVolume["armsolar"] = {
-		on={83,100,83,0,-12,1,0,1,0},
-		off={50,95,50,0,-12,1,0,1,0},
+		on={83,76,83,0,-18,1,0,1,0},
+		off={50,76,50,0,-18,1,0,1,0},
 	}
 	unitCollisionVolume["armtarg"] = {
 		on={62,34,62,0,0,0,2,1,0},
-		off={55,78,55,0,-5.5,0,0,1,0},
+		off={55,78,55,0,-19.5,0,0,1,0},
+	}
+	unitCollisionVolume["armvp"] = {
+		on={120,34,92,0,0,0,2,1,0},
+		off={90,34,92,0,0,0,2,1,0},
 	}
 	unitCollisionVolume["cordoom"] = {
-		on={55,105,55,0,-3,0,2,1,0},
+		on={55,112,55,0,-3,0,2,1,0},
 		off={48,86,48,0,-15,0,2,1,0},
+	}
+	unitCollisionVolume["corfmkr"] = {
+		on={48,46,48,0,0,0,0,1,0},
+		off={48,43,48,0,-16,0,0,1,0},
 	}
 	unitCollisionVolume["cormmkr"] = {
 		on={60,60,60,0,0,0,1,1,1},
-		off={50,92,50,0,-17.5,0,0,1,0},
+		off={50,92,50,0,-22.5,0,0,1,0},
 	}
 	unitCollisionVolume["corplat"] = {
-		on={112,60,112,0,30,0,1,1,1},
-		off={112,37,112,0,0,0,1,1,1},
+		on={112,60,112,0,28,0,1,1,1},
+		off={112,35,112,0,0,0,1,1,1},
 	}
 	unitCollisionVolume["corsolar"] = {
-		on={86,43,86,0,-10,0,0,1,0},
-		off={57,32,57,0,-10,0,0,1,0},
+		on={86,78,86,0,-25,0,0,1,0},
+		off={77,78,77,0,-35,0,0,1,0},
 	}
 	unitCollisionVolume["cortarg"] = {
 		on={64,20,64,0,0,0,1,1,1},
@@ -91,11 +103,11 @@ local unitCollisionVolume = {}
 	}
 	unitCollisionVolume["corvipe"] = {
 		on={39,86,39,0,-13,0,0,1,0},
-		off={39,35,39,0,-17,0,0,1,0},
+		off={39,55,39,0,-27,0,0,1,0},
 	}
 	unitCollisionVolume["packo"] = {
 		on={49,45,49,-0.5,-14,0,0,1,0},
-		off={49,26,49,-0.5,0,0,0,1,0},
+		off={49,26,49,-0.5,-13,0,0,1,0},
 	}
 
 return unitCollisionVolume
