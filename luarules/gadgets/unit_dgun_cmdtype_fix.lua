f147ac5d8470671d303f21373ba22b5b9542d414
Niobium <Niobium@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2011-05-01 01:45:41 +0000

    DGun now able to target units (Doesn't aim ahead)

---
+++ luarules/gadgets/unit_dgun_cmdtype_fix.lua
@@ -0,0 +1,58 @@
+
+function gadget:GetInfo()
+	return {
+		name      = 'DGun CmdType Fix',
+		desc      = 'Fixed DGun CmdType so it can target units',
+		author    = 'Niobium',
+		date      = 'April 2011',
+		license   = 'GNU GPL, v2 or later',
+		layer     = 0,
+		enabled   = true
+	}
+end
+
+----------------------------------------------------------------
+-- Synced only
+----------------------------------------------------------------
+if not gadgetHandler:IsSyncedCode() then
+	return false
+end
+
+----------------------------------------------------------------
+-- Var
+----------------------------------------------------------------
+local canDGun = {}
+
+----------------------------------------------------------------
+-- Speedups
+----------------------------------------------------------------
+local spFindUnitCmdDesc = Spring.FindUnitCmdDesc
+local spGetUnitCmdDescs = Spring.GetUnitCmdDescs
+local spEditUnitCmdDesc = Spring.EditUnitCmdDesc
+
+local CMD_DGUN = CMD.DGUN
+local CMDTYPE_ICON_UNIT_OR_MAP = CMDTYPE.ICON_UNIT_OR_MAP
+
+----------------------------------------------------------------
+-- Callins
+----------------------------------------------------------------
+function gadget:Initialize()
+    for uDefID, uDef in pairs(UnitDefs) do
+        if uDef.canDGun then
+            canDGun[uDefID] = true
+        end
+    end
+end
+
+function gadget:UnitCreated(uID, uDefID, uTeam)
+    if canDGun[uDefID] then
+        local cmdIdx = spFindUnitCmdDesc(uID, CMD_DGUN)
+        if cmdIdx then
+            local cmdDesc = spGetUnitCmdDescs(uID, cmdIdx, cmdIdx)[1]
+            if cmdDesc then
+                cmdDesc.type = CMDTYPE_ICON_UNIT_OR_MAP
+                spEditUnitCmdDesc(uID, cmdIdx, cmdDesc)
+            end
+        end
+    end
+end
