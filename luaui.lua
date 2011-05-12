fc78169d0e701915fff520cc6a32e5c2949472f3
Niobium <Niobium@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2011-05-12 11:05:33 +0000

    Implemented widget blacklisting for likely metal maker widgets (Regex: :upper():match('METAL[ _]*MAKER'))

---
+++ luaui.lua
@@ -0,0 +1,57 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+--  file:    luaui.lua
+--  brief:   entry point for LuaUI
+--  author:  Dave Rodgers
+--
+--  Copyright (C) 2008.
+--  Licensed under the terms of the GNU GPL, v2 or later.
+--
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+LUAUI_VERSION = "LuaUI v0.3"
+LUAUI_DIRNAME = 'LuaUI/'
+VFS.DEF_MODE = VFS.RAW_FIRST
+local STARTUP_FILENAME = LUAUI_DIRNAME .. 'bamain.lua'
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+do
+  -- use a versionned directory name if it exists
+  local sansslash = string.sub(LUAUI_DIRNAME, 1, -2)
+  local versiondir = sansslash .. '-' .. Game.version .. '/'
+  if (VFS.FileExists(versiondir  .. 'bamain.lua', VFS.RAW_FIRST)) then
+    LUAUI_DIRNAME = versiondir
+  end
+end
+
+Spring.Echo('Using LUAUI_DIRNAME = ' .. LUAUI_DIRNAME)
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+-- load the user's UI
+--
+
+do
+  local text = VFS.LoadFile(STARTUP_FILENAME, VFS.RAW_FIRST)
+  if (text == nil) then
+    Script.Kill('Failed to load ' .. STARTUP_FILENAME)
+  end
+  local chunk, err = loadstring(text)
+  if (chunk == nil) then
+    Script.Kill('Failed to load ' .. STARTUP_FILENAME .. ' (' .. err .. ')')
+  else
+    chunk()
+    return
+  end
+end
+
+
+-------------------------------------------------------------------------------- 
+-------------------------------------------------------------------------------- 
---
+++ luaui/bamain.lua
@@ -0,0 +1,152 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+--  file:    main.lua
+--  brief:   the entry point from gui.lua, relays call-ins to the widget manager
+--  author:  Dave Rodgers
+--
+--  Copyright (C) 2007.
+--  Licensed under the terms of the GNU GPL, v2 or later.
+--
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+Spring.SendCommands({"ctrlpanel " .. LUAUI_DIRNAME .. "ctrlpanel.txt"})
+
+VFS.Include(LUAUI_DIRNAME .. 'utils.lua', utilFile)
+
+include("setupdefs.lua")
+include("savetable.lua")
+
+include("debug.lua")
+include("fonts.lua")
+include("layout.lua")   -- contains a simple LayoutButtons()
+-- include("bawidgets.lua")  -- the widget handler
+VFS.Include(LUAUI_DIRNAME .. 'bawidgets.lua', nil, VFS.RAW_FIRST)
+
+--------------------------------------------------------------------------------
+--
+-- print the header
+--
+
+if (RestartCount == nil) then
+  RestartCount = 0
+else 
+  RestartCount = RestartCount + 1
+end
+
+do
+  local restartStr = ""
+  if (RestartCount > 0) then
+    restartStr = "  (" .. RestartCount .. " Restarts)"
+  end
+  Spring.SendCommands({"echo " .. LUAUI_VERSION .. restartStr})
+end
+
+
+--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+--
+--  A few helper functions
+--
+
+function Say(msg)
+  Spring.SendCommands({'say ' .. msg})
+end
+
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+--
+--  Update()  --  called every frame
+--
+
+activePage = 0
+
+forceLayout = true
+
+
+function Update()
+  local currentPage = Spring.GetActivePage()
+  if (forceLayout or (currentPage ~= activePage)) then
+    Spring.ForceLayoutUpdate()  --  for the page number indicator
+    forceLayout = false
+  end
+  activePage = currentPage
+
+  fontHandler.Update()
+
+  widgetHandler:Update()
+
+  return
+end
+
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+--
+--  WidgetHandler fixed calls
+--
+
+function Shutdown()
+  return widgetHandler:Shutdown()
+end
+
+function ConfigureLayout(command)
+  return widgetHandler:ConfigureLayout(command)
+end
+
+function CommandNotify(id, params, options)
+  return widgetHandler:CommandNotify(id, params, options)
+end
+
+function DrawScreen(vsx, vsy)
+  widgetHandler:SetViewSize(vsx, vsy)
+  return widgetHandler:DrawScreen()
+end
+
+function KeyPress(key, mods, isRepeat, label, unicode)
+  return widgetHandler:KeyPress(key, mods, isRepeat, label, unicode)
+end
+
+function KeyRelease(key, mods, label, unicode)
+  return widgetHandler:KeyRelease(key, mods, label, unicode)
+end
+
+function MouseMove(x, y, dx, dy, button)
+  return widgetHandler:MouseMove(x, y, dx, dy, button)
+end
+
+function MousePress(x, y, button)
+  return widgetHandler:MousePress(x, y, button)
+end
+
+function MouseRelease(x, y, button)
+  return widgetHandler:MouseRelease(x, y, button)
+end
+
+function IsAbove(x, y)
+  return widgetHandler:IsAbove(x, y)
+end
+
+function GetTooltip(x, y)
+  return widgetHandler:GetTooltip(x, y)
+end
+
+function AddConsoleLine(msg, priority)
+  return widgetHandler:AddConsoleLine(msg, priority)
+end
+
+function GroupChanged(groupID)
+  return widgetHandler:GroupChanged(groupID)
+end
+
+
+--
+-- The unit (and some of the Draw) call-ins are handled
+-- differently (see LuaUI/widgets.lua / UpdateCallIns())
+--
+
+
+--------------------------------------------------------------------------------
+
