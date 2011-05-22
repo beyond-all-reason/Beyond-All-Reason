07f79d1725dac90e3fe7bfff0724ee0688062c4f
Niobium <Niobium@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2011-05-22 10:21:02 +0000

    Replaced take reminder widget with a far more efficient version. Probably works.

---
+++ luaui/widgets/gui_take_remind_II.lua
@@ -0,0 +1,97 @@
+
+function widget:GetInfo()
+	return {
+		name      = "Take Reminder II",
+		desc      = "Improved Take Reminder",
+		author    = "Niobium",
+		date      = "April 2011",
+		license   = "GNU GPL, v2 or later",
+		layer     = 0,
+		enabled   = true  --  loaded by default?
+	}
+end
+
+local checkEvery = 1.0
+local buttonWidth = 360
+local buttonHeight = 36
+
+local spGetSpectatingState = Spring.GetSpectatingState
+local spGetPlayerList = Spring.GetPlayerList
+local spGetPlayerInfo = Spring.GetPlayerInfo
+local spGetMyAllyTeamID = Spring.GetMyAllyTeamID
+local spGetTeamList = Spring.GetTeamList
+local spGetTeamInfo = Spring.GetTeamInfo
+local spGetTeamUnitCount = Spring.GetTeamUnitCount
+
+--local takeableTeams = {} -- takeableTeams[1..n] = tID
+local takeableCount = 0
+
+local function GetButtonPosition()
+    local vsx, vsy = gl.GetViewSizes()
+    return 0.65 * vsx, 0.7 * vsy
+end
+
+local tt = 0
+function widget:Update(dt)
+    
+	tt = tt + dt
+    if tt < checkEvery then return end
+    tt = 0
+    
+    -- Silent disable if spectator
+	if spGetSpectatingState() then
+		widgetHandler:RemoveWidget(self)
+		return
+	end
+    
+	-- Create map of teams that have players
+	local hasPlayer = {}
+	local playerList = spGetPlayerList(true) -- Get list of active players
+	for i = 1, #playerList do
+		local _, _, playerIsSpec, playerTeam = spGetPlayerInfo(playerList[i])
+		if not playerIsSpec then
+			hasPlayer[playerTeam] = true
+		end
+	end
+	
+	-- Search allied teams for units to take
+    --takeableTeams = {}
+    takeableCount = 0
+	local teamList = spGetTeamList(spGetMyAllyTeamID())
+	for i = 1, #teamList do
+        local tID = teamList[i]
+		if not hasPlayer[tID] then
+			local _, _, isDead, isAI = spGetTeamInfo(tID)
+			if not (isDead or isAI) then
+				--takeableTeams[#takeableTeams + 1] = tID
+                takeableCount = takeableCount + spGetTeamUnitCount(tID)
+			end
+		end
+	end
+end
+
+function widget:DrawScreen()
+    
+    if takeableCount == 0 then return end
+    
+    local posx, posy = GetButtonPosition()
+    gl.Color(1, math.abs(os.clock() % 2 - 1), 0, 0.5)
+    gl.Rect(posx, posy, posx + buttonWidth, posy + buttonHeight)
+    gl.Color(0, 0, 0, 1)
+    gl.Shape(GL.LINE_LOOP, {{ v = { posx              , posy                }}, 
+                            { v = { posx              , posy + buttonHeight }}, 
+                            { v = { posx + buttonWidth, posy + buttonHeight }}, 
+                            { v = { posx + buttonWidth, posy                }}})
+    gl.Text(string.format('\255\255\255\1Click to take %d abandoned units !', takeableCount), posx + 0.5 * buttonWidth, posy + 0.5 * buttonHeight, 20, 'ocv')
+end
+
+function widget:MousePress(mx, my, mButton)
+    if takeableCount > 0 and mButton == 1 then
+        local posx, posy = GetButtonPosition()
+        if mx >= posx and mx < posx + buttonWidth and
+           my >= posy and my < posy + buttonHeight then
+            Spring.SendCommands('take')
+            return true
+        end
+    end
+end
