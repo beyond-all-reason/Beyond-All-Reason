1d8aab73553623df4a0087206eba6e2e441a18c6
TheFatController <TheFatController@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2011-05-30 16:20:15 +0000

    Fixed submarines not being able to hit some t1 ships by expanding hitspheres a little downwards Fixed some boats chasing submarines which shouldn't

--- units/armsub.lua
+++ units/armsub.lua
@@ -105,7 +105,7 @@ return {
 				craterboost = 0,
 				cratermult = 0,
 				explosiongenerator = "custom:FLASH2",
-				flighttime = 2.2999999523163,
+				flighttime = 2.35,
 				impulseboost = 0.12300000339746,
 				impulsefactor = 0.12300000339746,
 				model = "torpedo",
