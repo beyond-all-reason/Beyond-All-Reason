5555de0ff4f257fb9ab482dcda49da944a003e75
DeadNightWarrior <DeadNightWarrior@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2011-10-09 18:42:03 +0000

    Updated Dynamic Collision Volume gadget works with new functions added to Spring 0.83 and has backwards compatibility

--- luarules/gadgets/unit_dynamic_collision_volume.lua
+++ luarules/gadgets/unit_dynamic_collision_volume.lua
@@ -3,23 +3,38 @@ function gadget:GetInfo()
     name      = "Dynamic collision volume & Hitsphere Scaledown",
     desc      = "Adjusts collision volume for pop-up style units & Reduces the diameter of default sphere collision volume for 3DO models",
     author    = "Deadnight Warrior",
-    date      = "Jul 12, 2011",
+    date      = "Oct 9, 2011",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
     enabled   = true  --  loaded by default?
   }
 end
 
-
+-- Pop-up style unit and per piece collision volume definitions
 local popupUnits = {}		--list of pop-up style units
-local unitCollisionVolume, pieceCollisionVolume = include("LuaRules/Configs/CollisionVolumes.lua")	--pop-up style unit and per piece collision volume definitions
+local unitCollisionVolume, pieceCollisionVolume = include("LuaRules/Configs/CollisionVolumes.lua")
 
+-- Localization and speedups
 local spGetPieceCollisionData = Spring.GetUnitPieceCollisionVolumeData
 local spSetPieceCollisionData = Spring.SetUnitPieceCollisionVolumeData
 local spGetUnitCollisionData = Spring.GetUnitCollisionVolumeData
 local spSetUnitCollisionData = Spring.SetUnitCollisionVolumeData
+local spGetFeatureCollisionData = Spring.GetFeatureCollisionVolumeData
+local spSetFeatureCollisionData = Spring.SetFeatureCollisionVolumeData
 local spArmor = Spring.GetUnitArmored
+local spActive = Spring.GetUnitIsActive
+local airScalX, airScalY, airScalZ
 local pairs = pairs	
+
+function gadget:Initialize()
+	-- Spring 0.83 doesn't scale collision volumes of aircraft by additional 0.5 like Spring 0.82 does
+	if spGetFeatureCollisionData then
+		airScalX, airScalY, airScalZ = 0.375, 0.225, 0.45
+	else
+		airScalX, airScalY, airScalZ = 0.75, 0.45, 0.9
+	end
+end
+
 	
 if (gadgetHandler:IsSyncedCode()) then
 
@@ -44,7 +59,7 @@ if (gadgetHandler:IsSyncedCode()) then
 					elseif (not UnitDefs[unitDefID].canFly) then
 						spSetUnitCollisionData(unitID, xs*0.75, ys*0.75, zs*0.75,  xo, yo, zo,  vtype, htype, axis)
 					else
-						spSetUnitCollisionData(unitID, xs*0.375, ys*0.36, zs*0.44,  xo, yo, zo,  vtype, htype, axis)
+						spSetUnitCollisionData(unitID, xs*airScalX, ys*airScalY, zs*airScalZ,  xo, yo, zo,  vtype, htype, axis)
 					end
 				end
 			end
@@ -52,15 +67,24 @@ if (gadgetHandler:IsSyncedCode()) then
 	end
 
 
-	--[[ unsupported by engine ATM, same as for 3DO units, but for features
-	function gadget:FeatureCreated(featureID, allyTeam)
-		local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = Spring.GetFeatureCollisionVolumeData(featureID)
-		if (vtype==4 and xs==ys and ys==zs) then
-			Spring.SetFeatureCollisionVolumeData(unitID, xs*0.75, ys*0.75, zs*0.75,  xo, yo, zo,  vtype, htype, axis)
-
+	-- Requires Spring 0.83 and up, same as for 3DO units, but for features,
+	-- disabled on Spring <0.83
+	if spGetFeatureCollisionData then
+		function gadget:FeatureCreated(featureID, allyTeam)
+			local featureModel = FeatureDefs[Spring.GetFeatureDefID(featureID)].modelname
+			featureModel = featureModel:lower()
+			if featureModel:find(".3do") then
+				local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetFeatureCollisionData(featureID)
+				if (vtype==4 and xs==ys and ys==zs) then
+					if (xs>47) then
+						spSetFeatureCollisionData(featureID, xs*0.68, ys*0.60, zs*0.68,  xo, yo, zo,  vtype, htype, axis)
+					else
+						spSetFeatureCollisionData(featureID, xs*0.75, ys*0.67, zs*0.75,  xo, yo, zo,  vtype, htype, axis)
+					end
+				end
+			end
 		end
 	end
-	]]--
 
 
 	--check if a unit is pop-up type (the list must be entered manually)
@@ -100,13 +124,6 @@ if (gadgetHandler:IsSyncedCode()) then
 					spSetUnitCollisionData(unitID, p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
 					popupUnits[unitID].state = 1
 				end
-				--[[ if units don't use ARMORED variable, uncomment this block to make it use ACTIVATION as well
-				if ( Spring.GetUnitIsActive(unitID) ) then
-					p = unitCollisionVolume[name].on
-				else
-					p = unitCollisionVolume[name].off
-				end
-				]]--
 			end			
 		end
 	end
