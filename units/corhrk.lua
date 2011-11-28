c753338761e8b7a7fb8de9a25176501eb4d9b4d3
Senna <Senna@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2011-11-28 18:09:47 +0000

    Fixed Bug with Dominator range

--- units/corhrk.lua
+++ units/corhrk.lua
@@ -100,7 +100,7 @@ return {
 				edgeeffectiveness = 0.5,
 				explosiongenerator = "custom:STARFIRE",
 				firestarter = 100,
-				flighttime = 10,
+				flighttime = 6,2,
 				impulseboost = 0.12300000339746,
 				impulsefactor = 0.12300000339746,
 				metalpershot = 0,
@@ -118,7 +118,7 @@ return {
 				weapontype = "StarburstLauncher",
 				weaponvelocity = 800,
 				damage = {
-					default = 750,
+					default = 820,
 					subs = 5,
 				},
 			},
