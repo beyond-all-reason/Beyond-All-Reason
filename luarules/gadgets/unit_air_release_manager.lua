b271914fb7da69e430492e7ebe606a5aaa69da53
knorke <knorke@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2011-12-16 16:27:28 +0000

    remove dead units from fallingUnits table instead of trying to do things with them -> no endless spam (hopefully)

--- luarules/gadgets/unit_air_release_manager.lua
+++ luarules/gadgets/unit_air_release_manager.lua
@@ -22,33 +22,37 @@ local fallingUnits = {}
 function gadget:GameFrame(n)
   if fC > 0 then
     for k,t in ipairs(fallingUnits) do
-	local x,y,z = Spring.GetUnitBasePosition(t[1])
-	local h = Spring.GetGroundHeight(x,z)
-	if h > y-10 then
-	  Spring.AddUnitDamage(t[1], t[2])
-	  table.remove(fallingUnits,k)
-	  fC = fC - 1
-	end
+		local unitAlive = (Spring.ValidUnitID (t[1]) and (not Spring.GetUnitIsDead (t[1]))) --+++
+		if (unitAlive) then						--++
+			x,y,z = Spring.GetUnitBasePosition(t[1])		--maybe check if units were off map		
+			h = Spring.GetGroundHeight(x,z)
+		--else
+			--Spring.Echo ("unit was not alive anymore. There would have been error spam")
+		end
+		if (not unitAlive or h > y-10)  then			--+++
+		  Spring.AddUnitDamage(t[1], t[2])
+		  table.remove(fallingUnits,k)
+		  fC = fC - 1
+		end
     end
   end
 end
 
 function gadget:UnitUnloaded(unitID, unitDefID, _, transID)
-			    if unitDefID ~= COMMANDO then
-					local x,y,z = Spring.GetUnitBasePosition(unitID)
-					local h = Spring.GetGroundHeight(x,z)
-					local damage = 0
-					if (y-h) > 25 then
-					    local Udef = UnitDefs[unitDefID]
-						damage = Udef.mass * (y-h)/50
-					elseif (y-h) > 5 then
-					  damage = ((y-h)*25)
-					end
-				if damage > 10 then 
-				  fC = fC + 1
-				  --Spring.Echo(y-h, damage)
-				  fallingUnits[fC] = {unitID, damage}
-				end
-			      end
-	
+	if unitDefID ~= COMMANDO then
+		local x,y,z = Spring.GetUnitBasePosition(unitID)
+		local h = Spring.GetGroundHeight(x,z)
+		local damage = 0
+		if (y-h) > 25 then
+			local Udef = UnitDefs[unitDefID]
+			damage = Udef.mass * (y-h)/50
+		elseif (y-h) > 5 then
+			damage = ((y-h)*25)
+		end
+		if damage > 10 then 
+			fC = fC + 1
+			--Spring.Echo(y-h, damage)
+			fallingUnits[fC] = {unitID, damage}
+		end
+	end	
 end
