53d6ee82b67e667496d9d76551e83b03ebcad8f4
nobody <nobody@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2011-12-24 13:16:31 +0000

    workaround attempt for units not changing velocity in 85

--- gamedata/unitdefs_post.lua
+++ gamedata/unitdefs_post.lua
@@ -23,14 +23,14 @@ if (Spring.GetModOptions) then
 	
 end
 
-for name, ud in pairs(UnitDefs) do
-	if (ud.maxvelocity) then 
-		ud.turninplacespeedlimit = ud.maxvelocity or 0
-	end
-	if ud.category and (ud.category:find("TANK",1,true) or ud.category:find("HOVER",1,true)) then
-		if (ud.maxvelocity) then 
-			ud.turninplace = 0
-			ud.turninplacespeedlimit = ud.maxvelocity or 0
-		end
-	end
-end 
+--for name, ud in pairs(UnitDefs) do
+--	if (ud.maxvelocity) then 
+--		ud.turninplacespeedlimit = ud.maxvelocity or 0
+--	end
+--	if ud.category and (ud.category:find("TANK",1,true) or ud.category:find("HOVER",1,true)) then
+--		if (ud.maxvelocity) then 
+--			ud.turninplace = 0
+--			ud.turninplacespeedlimit = ud.maxvelocity or 0
+--		end
+--	end
+--end 
