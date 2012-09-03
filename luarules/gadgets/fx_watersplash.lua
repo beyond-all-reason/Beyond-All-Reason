e33a1936ecb733b6fc231b04a8a4139bb941a1a6
nixtux <nixtux@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2012-09-03 18:25:22 +0000

    Minor tweak to splash gagdet

--- luarules/gadgets/fx_watersplash.lua
+++ luarules/gadgets/fx_watersplash.lua
@@ -15,6 +15,7 @@ end
 LUAUI_DIRNAME= 'LuaUI/'
 local random  = math.random
 local abs = math.abs
+local GetGroundBlocked = Spring.GetGroundBlocked
 local nonexplosiveWeapons = {
 	LaserCannon = true,
 	BeamLaser = true,
@@ -37,21 +38,20 @@ else
 	local splashCEG2					= "Watersplash_Small"
 	local splashCEG3					= "Watersplash_Large"
 	local splashCEG4					= "Watersplash_Extralarge"
-	local GetGroundBlocked					= Spring.GetGroundBlocked
 
 	
 	function gadget:Explosion(weaponID, px, py, pz, ownerID)
 		local isWater = Spring.GetGroundHeight(px,pz) < 0
 		local aoe = WeaponDefs[weaponID]["damageAreaOfEffect"] / 2
 		local wType = WeaponDefs[weaponID].type
-		if not nonexplosiveWeapons[wType] and isWater and abs(py) <= aoe then
-		        if aoe >= 8 and aoe < 16 and (not GetGroundBlocked(px, pz)) then
+				if not nonexplosiveWeapons[wType] and isWater and abs(py) <= aoe and (not GetGroundBlocked(px, pz)) then
+			if aoe >= 8 and aoe < 16 then
 				Spring.SpawnCEG(splashCEG1, px, 0, pz)
-			elseif aoe >= 16 and aoe < 48 and (not GetGroundBlocked(px, pz)) then
+			elseif aoe >= 16 and aoe < 48 then
 				Spring.SpawnCEG(splashCEG2, px, 0, pz)
-			elseif aoe >= 48 and aoe < 64 and (not GetGroundBlocked(px, pz)) then
+			elseif aoe >= 48 and aoe < 64 then
 				Spring.SpawnCEG(splashCEG3, px, 0, pz)
-			elseif aoe >= 64 and (not GetGroundBlocked(px, pz)) then
+			elseif aoe >= 64 and aoe < 300 then
 				Spring.SpawnCEG(splashCEG4, px, 0, pz)
 			end
 			return true
@@ -64,7 +64,7 @@ else
 	
 		for id,Def in pairs(WeaponDefs) do
 			local weaponID
-			if Def.damageAreaOfEffect ~= nil and Def.damageAreaOfEffect > 16 and not nonexplosiveWeapons[Def.type] then
+			if Def.damageAreaOfEffect ~= nil and Def.damageAreaOfEffect >8 and not nonexplosiveWeapons[Def.type] then
 				Script.SetWatchWeapon(Def.id, true)
 			end
 		end
