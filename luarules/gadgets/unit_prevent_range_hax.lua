dc180b20bf751ab79dbd261965130b19ede57783
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2012-09-04 08:28:24 +0000

    Fixed bombers + torp bombers ignoring attack commands.

--- luarules/gadgets/unit_prevent_range_hax.lua
+++ luarules/gadgets/unit_prevent_range_hax.lua
@@ -25,12 +25,28 @@ local GetGroundHeight = Spring.GetGroundHeight
 
 local CMD_ATTACK = CMD.ATTACK
 local CMD_INSERT = CMD.INSERT
-
+local Bombers = { -- ALL BOMBER TYPE AIRCRAFT FAIL THIS CHECK
+					--So they are ignored, cause of a bug in .91 
+					--hacky hack solution by beherith
+  [UnitDefNames["corseap"].id] = true,
+  [UnitDefNames["corsb"].id] = true,
+  [UnitDefNames["corhurc"].id] = true,
+  [UnitDefNames["cortitan"].id] = true,
+  [UnitDefNames["corshad"].id] = true,
+  [UnitDefNames["corgripn"].id] = true,
+  [UnitDefNames["armcybr"].id] = true,
+  [UnitDefNames["armlance"].id] = true,
+  [UnitDefNames["armpnix"].id] = true,
+  [UnitDefNames["armthund"].id] = true,
+  [UnitDefNames["armseap"].id] = true,
+  [UnitDefNames["armsb"].id] = true,
+}
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions, cmdTag, synced)
   if synced then return true end
+  if (Bombers[unitDefID]) then return true end
   if (cmdID == CMD_INSERT) and (CMD_ATTACK == cmdParams[2]) and cmdParams[6] then
     local y = GetGroundHeight(cmdParams[4],cmdParams[6])
     if (cmdParams[5] > y) then
