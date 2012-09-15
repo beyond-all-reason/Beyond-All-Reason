a20dbf20d60012e75cc5d19155b4a18dc239c9da
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2012-09-15 22:46:48 +0000

    Fix air repair pads not repairing gunships. (their radius was too big)

--- luarules/gadgets/unit_collision_radius_adjuster.lua
+++ luarules/gadgets/unit_collision_radius_adjuster.lua
@@ -1,7 +1,7 @@
    function gadget:GetInfo()
       return {
         name      = "Transport radius adjuster",
-        desc      = "a hack in .90, to adjust the radius of transports so they can pick up units, also because they cant be adjusted in 3do, nor per unitdef.",
+        desc      = "a hack in .90-.91, to adjust the radius of transports so they can pick up units, also because they cant be adjusted in 3do, nor per unitdef.",
         author    = "Beherith",
         date      = "aug 2012",
         license   = "PD",
@@ -13,18 +13,21 @@
 if (not gadgetHandler:IsSyncedCode()) then
   return
 end
-local AirTrans = {
+local special = {
   [UnitDefNames["corvalk"].id] = 16,
   [UnitDefNames["armsl"].id] = 16,
   [UnitDefNames["armatlas"].id] = 16,
   [UnitDefNames["armdfly"].id] = 16,
+  [UnitDefNames["armasp"].id] = 32,
+  [UnitDefNames["corasp"].id] = 32,
+  [UnitDefNames["corcrw"].id] = 24,
 }
 function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
-	if AirTrans[unitDefID] then
+	if special[unitDefID] then
 		
 		--Spring.Echo('reset radius for airtrans')
 	--Spring.SetUnitRadiusAndHeight New in version 89.0( number unitID, number radius, number height )
-		Spring.SetUnitRadiusAndHeight(unitID, AirTrans[unitDefID], 32 )
+		Spring.SetUnitRadiusAndHeight(unitID, special[unitDefID], 32 )
 	else
 		Spring.SetUnitRadiusAndHeight(unitID,Spring.GetUnitRadius(unitID)*0.66,Spring.GetUnitHeight(unitID))
 	end
