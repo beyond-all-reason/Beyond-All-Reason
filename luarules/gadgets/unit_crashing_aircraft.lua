19afabf68d1f1aae45592d300690b56bb538259d
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2012-09-27 20:26:03 +0000

    Fixed paralyze in unit_crashing_aircraft.lua

--- luarules/gadgets/unit_crashing_aircraft.lua
+++ luarules/gadgets/unit_crashing_aircraft.lua
@@ -18,9 +18,34 @@ local random			= math.random
 local SetUnitCOBValue 	= Spring.SetUnitCOBValue
 local SetUnitNoSelect	= Spring.SetUnitNoSelect
 local SetUnitCosts		= Spring.SetUnitCosts
-local crashing={}
-
+local crashable  ={ --fucking hacky bugfix, strafemovetype cant crash, and sometimes doesnt take the damage it is dealt. 
+	[UnitDefNames["armthund"].id] = true,
+	[UnitDefNames["armpeep"].id] = true,
+	[UnitDefNames["armfig"].id] = true,
+	[UnitDefNames["armcybr"].id] = true,
+	[UnitDefNames["armawac"].id] = true,
+	[UnitDefNames["armlance"].id] = true,
+	[UnitDefNames["armhawk"].id] = true,
+	[UnitDefNames["armpnix"].id] = true,
+	[UnitDefNames["armsehak"].id] = true,
+	[UnitDefNames["armsfig"].id] = true,
+	[UnitDefNames["armseap"].id] = true,
+	[UnitDefNames["armsb"].id] = true,
+	[UnitDefNames["corgripn"].id] = true,
+	[UnitDefNames["corawac"].id] = true,
+	[UnitDefNames["cortitan"].id] = true,
+	[UnitDefNames["corvamp"].id] = true,
+	[UnitDefNames["corhurc"].id] = true,
+	[UnitDefNames["corshad"].id] = true,
+	[UnitDefNames["corfink"].id] = true,
+	[UnitDefNames["corveng"].id] = true,
+	[UnitDefNames["corhunt"].id] = true,
+	[UnitDefNames["corseap"].id] = true,
+	[UnitDefNames["corsfig"].id] = true,
+	[UnitDefNames["corsb"].id] = true,
+}
 function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, attackerID, attackerDefID, attackerTeam)
+	if paralyzer then return damage,1 end --OOPS FORGOT THIS
 	if crashing[unitID] then 
 		return 0,0
 	end --hacky
