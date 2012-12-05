0afffc10c58c7678a856657985292a0988efb640
nixtux <nixtux@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2012-12-05 23:07:52 +0000

    Update dynamic collision volume gadget and update hitsphere for next engine release

--- luarules/configs/collisionvolumes.lua
+++ luarules/configs/collisionvolumes.lua
@@ -10,6 +10,7 @@ Spring.SetUnitCollisionVolumeData ( number unitID, number scaleX, number scaleY,
 Spring.SetUnitPieceCollisionVolumeData ( number unitID, number pieceIndex, boolean enabled, number scaleX, number scaleY, number scaleZ,
 					number offsetX, number offsetY, number offsetZ, number vType, number Axis) -> nil
 	per piece collision volumes always use COLVOL_TEST_CONT as tType
+	above syntax is for 0.83, for 0.82 compatibility repeat enabled 3 more times
 	
    possible vType constants
      DISABLED = -1  disables collision volume and collision detection for that unit, do not use
@@ -33,23 +34,26 @@ Spring.SetUnitPieceCollisionVolumeData ( number unitID, number pieceIndex, boole
 		on=            -- Unit is active/open/poped-up 
 		   {60,80,60,  -- Volume X scale, Volume Y scale, Volume Z scale,
 		    0,15,0,    -- Volume X offset, Volume Y offset, Volume Z offset,
-		    0,1,0},    -- vType, tType, axis}
+		    0,1,0[,    -- vType, tType, axis [,  -- Optional
+			0,0,0]}    -- Aimpoint X offset, Aimpoint Y offset, Aimpoint Z offset]},
 		off={32,48,32,0,-10,0,0,1,0},
-	}
+	}                  -- Aimpoint offsets are relative to unit's base position (aka unit coordiante space)
 	pieceCollisionVolume["arm_big_bertha"] = {
-		["0"]={            -- [pieceIndexNumber]={
-			   48,74,48,   --              Volume X scale, Volume Y scale, Volume Z scale,
-		       0,0,0,      --              Volume X offset, Volume Y offset, Volume Z offset,
-			   1,1}        --              vType, axis},
+		["0"]={true,       -- [pieceIndexNumber]={enabled,
+			   48,74,48,   --            Volume X scale, Volume Y scale, Volume Z scale,
+		       0,0,0,      --            Volume X offset, Volume Y offset, Volume Z offset,
+			   1,1},       --            vType, axis},   
 		....               -- All undefined pieces will be treated as disabled for collision detection
 	}
 	dynamicPieceCollisionVolume["core_viper"] = {	--same as with pieceCollisionVolume only uses "on" and "off" tables
 		on = {
-			["0"]={51,12,53,0,4,0,2,0},
-			["5"]={25,66,25,0,-14,0,1,1},
-		},
+			["0"]={true,51,12,53,0,4,0,2,0},
+			["5"]={true,25,66,25,0,-14,0,1,1},
+			offsets={0,35,0}   -- Aimpoint X offset, Aimpoint Y offset, Aimpoint Z offset
+		},                     -- offsets entry is optional 
 		off = {
-			["0"]={51,12,53,0,4,0,2,0},
+			["0"]={true,51,12,53,0,4,0,2,0},
+			offsets={0,8,0}
 		}
 	}
 	
@@ -62,24 +66,24 @@ Spring.SetUnitPieceCollisionVolumeData ( number unitID, number pieceIndex, boole
 	A: Edit unit's definition file and add:
 		usePieceCollisionVolumes=1;    (FBI)
 		usePieceCollisionVolumes=true, (LUA)
-	Q: Why does my unit always have the same volume and doesn't change it when opening/closing?
-	A: You need to edit the unit's animation script and change ARMORED state of the unit in
-	   functions that open/close that unit. 0/false for open, 1/true for closed
+	Q: The unit always has on/off volume and it never changes
+	A: You need to edit the unit script and set ARMORED status to on or off depending on the
+	   unit's on/off status, unarmored for on and armored for off
 ]]--
-
 
 --Collision volume definitions, ones entered here are for TA, for other mods modify apropriatly
 local unitCollisionVolume = {}			--dynamic collision volume definitions
 local pieceCollisionVolume = {}			--per piece collision volume definitions
 local dynamicPieceCollisionVolume = {}	--dynamic per piece collision volume definitions
 
+
 	unitCollisionVolume["armason"] = {
-		on={57,60,57,0,-7,0,0,1,0},    --{Xscale,Yscale,Zscale, Xoffset,Yoffset,Zoffset, vType,tType,axis}
-		off={24,34,24,0,10,0,0,1,0},
+		on={57,60,57,0,-7,0,1,1,1},    --{Xscale,Yscale,Zscale, Xoffset,Yoffset,Zoffset, vType,tType,axis}
+		off={24,34,24,0,10,0,1,1,1},
 	}
 	unitCollisionVolume["armamb"] = {
-		on={49,45,49,-0.5,-14,0,0,1,0},
-		off={49,26,49,-0.5,-14,0,0,1,0},
+		on={49,45,49,-0.5,-14,0,1,1,1},
+		off={49,26,49,-0.5,-14,0,1,1,1},
 	}
 	unitCollisionVolume["armanni"] = {
 		on={54,81,54,0,-2,0,2,1,0},
@@ -90,24 +94,24 @@ local dynamicPieceCollisionVolume = {}	--dynamic per piece collision volume defi
 		off={95,22,95,0,-1,0,1,1,1},
 	}
 	unitCollisionVolume["armmmkr"] = {
-		on={60,72,60,0,7,0,1,1,1},
+		on={60,70,60,0,7,0,1,1,1},
 		off={60,50,60,0,-4,0,1,1,1},
 	}
 	unitCollisionVolume["armpb"] = {
-		on={39,88,39,0,-11,0,0,1,0},
-		off={39,55,39,0,-27,0,0,1,0},
+		on={39,78,39,0,-11,0,1,1,1},
+		off={39,40,39,0,-27,0,1,1,1},
 	}
 	unitCollisionVolume["armplat"] = {
 		on={105,66,105,0,33,0,2,1,0},
 		off={105,44,105,0,0,0,2,1,0},
 	}
 	unitCollisionVolume["armsolar"] = {
-		on={83,76,83,0,-18,1,0,1,0},
-		off={50,76,50,0,-18,1,0,1,0},
+		on={83,76,83,0,-18,1,1,1,1},
+		off={50,76,50,0,-18,1,1,1,1},
 	}
 	unitCollisionVolume["armtarg"] = {
 		on={62,34,62,0,0,0,2,1,0},
-		off={55,78,55,0,-19.5,0,0,1,0},
+		off={55,78,55,0,-19.5,0,1,1,1},
 	}
 	unitCollisionVolume["armvp"] = {
 		on={120,34,92,0,0,0,2,1,0},
@@ -118,78 +122,73 @@ local dynamicPieceCollisionVolume = {}	--dynamic per piece collision volume defi
 		off={45,87,45,0,0,0,2,1,0},
 	}
 	unitCollisionVolume["corfmkr"] = {
-		on={48,46,48,0,0,0,0,1,0},
-		off={48,43,48,0,-16,0,0,1,0},
+		on={48,46,48,0,0,0,1,1,1},
+		off={48,43,48,0,-16,0,1,1,1},
 	}
 	unitCollisionVolume["cormexp"] = {
-		on={83,77,78,0,-27,0,0,1,0},
-		off={90,135,84,0,-27,0,0,1,0},
+		on={83,77,83,0,-27,0,1,1,1},
+		off={90,135,90,0,-27,0,1,1,1},
 	}
 	unitCollisionVolume["cormmkr"] = {
 		on={60,60,60,0,0,0,1,1,1},
-		off={50,92,50,0,-22.5,0,0,1,0},
+		off={55,92,55,0,-22.5,0,1,1,1},
 	}
 	unitCollisionVolume["corplat"] = {
 		on={112,60,112,0,28,0,1,1,1},
 		off={112,35,112,0,0,0,1,1,1},
 	}
 	unitCollisionVolume["corsolar"] = {
-		on={86,78,86,0,-25,0,0,1,0},
-		off={77,78,77,0,-35,0,0,1,0},
+		on={86,78,86,0,-25,0,1,1,1},
+		off={60,78,60,0,-35,0,1,1,1},
 	}
 	unitCollisionVolume["cortarg"] = {
 		on={64,20,64,0,0,0,1,1,1},
 		off={38,20,38,0,0,0,1,1,1},
 	}
 	unitCollisionVolume["packo"] = {
-		on={49,51,49,-0.5,-10,0,0,1,0},
-		off={49,23,49,-0.5,-10,0,0,1,0},
+		on={49,51,49,-0.5,-10,0,1,1,1},
+		off={49,23,49,-0.5,-10,0,1,1,1},
 	}
 	unitCollisionVolume["shiva"] = {
-		on={54,50,50,0,2,-1,0,1,0},
-		off={54,45,50,0,-4,-1,0,1,0},
+		on={50,50,50,0,2,-1,1,1,1},
+		off={54,45,54,0,-4,-1,1,1,1},
+	}
+	pieceCollisionVolume["armbrtha"] = {
+			["0"]={32,80,32,0,20,0,1,1},
+			["2"]={30,30,70,0,0,14,1,2},
+			["3"]={8,8,42,0,1,94,1,2},
+	}
+	pieceCollisionVolume["corint"] = {
+			["0"]={73,103,73,0,50,0,1,1},
+			["2"]={13,13,48,0,1,55,1,2},
+	}		
+	pieceCollisionVolume["armveil"] = {
+			["0"]={25,58,25,0,30,0,1,1},
+			["2"]={76,16,16,6,0,0,1,0},
 	}
 	pieceCollisionVolume["armrad"] = {
 			["0"]={22,58,22,0,30,0,1,1},
 			["2"]={60,13,13,11,0,0,1,0},
 	}
-	pieceCollisionVolume["armveil"] = {
-			["0"]={25,58,25,0,30,0,1,1},
-			["2"]={76,16,16,6,0,0,1,0},
-	}
-	pieceCollisionVolume["armbrtha"] = {
-			["0"]={32,80,32,0,20,0,1,1},
-			["2"]={24,30,75,0,23,0,1,2},
-			["3"]={8,8,42,0,1,70,1,2},
-	}
-	pieceCollisionVolume["corint"] = {
-			["0"]={73,103,73,0,50,0,1,1},
-			["2"]={13,13,48,0,1,55,1,2},
-	}
-	pieceCollisionVolume["armvulc"] = {
-			["1"]={98,165,98,0,82,0,1,1},
-			["4"]={75,75,155,0,0,90,1,2},
-	}		
-			
 	dynamicPieceCollisionVolume["corgant"] = {
 		on = {
-			["0"]={118,96,130,0,80,0,1,2},
-			["17"]={105,55,105,0,7,30,1,2},
+			["0"]={130,96,130,0,0,0,1,2},
+			["17"]={105,35,105,0,13,30,2,2},
 		},
 		off = {
-			["0"]={110,66,130,0,0,0,1,0},
+			["0"]={130,66,130,0,0,0,1,2},
 		}
-	}	
+	}
 	dynamicPieceCollisionVolume["armarad"] = {
 		on = {
 			["0"]={25,45,25,0,22,0,1,1},
 			["2"]={76,29,29,0,3,0,1,0},
 		},
 		off = {
-			["0"]={32,51,32,0,8,1,0,0},
+			["0"]={32,51,32,0,8,1,1,1},
 		}
 	}
-	dynamicPieceCollisionVolume["cortoast"] = {
+		dynamicPieceCollisionVolume["cortoast"] = {
 		on = {
 			["1"]={44,23,44,0,0,0,2,0},
 			["7"]={10,10,60,0,3,13,1,2},
@@ -207,4 +206,5 @@ local dynamicPieceCollisionVolume = {}	--dynamic per piece collision volume defi
 			["0"]={39,55,39,0,0,0,0,1},
 		}
 	}
+
 return unitCollisionVolume, pieceCollisionVolume, dynamicPieceCollisionVolume
--- luarules/gadgets/unit_dynamic_collision_volume.lua
+++ luarules/gadgets/unit_dynamic_collision_volume.lua
@@ -14,17 +14,30 @@ end
 local popupUnits = {}		--list of pop-up style units
 local unitCollisionVolume, pieceCollisionVolume, dynamicPieceCollisionVolume = include("LuaRules/Configs/CollisionVolumes.lua")
 
+-- Building's radius must be scaled after UnitFinished()
+local scaleBuilding = {}
+
 -- Localization and speedups
 local spGetPieceCollisionData = Spring.GetUnitPieceCollisionVolumeData
 local spSetPieceCollisionData = Spring.SetUnitPieceCollisionVolumeData
 local spGetPieceList = Spring.GetUnitPieceList
 local spGetUnitCollisionData = Spring.GetUnitCollisionVolumeData
 local spSetUnitCollisionData = Spring.SetUnitCollisionVolumeData
+local spSetUnitRadiusAndHeight = Spring.SetUnitRadiusAndHeight
+local spGetUnitRadius = Spring.GetUnitRadius
+local spGetUnitHeight = Spring.GetUnitHeight
+local spSetUnitMidAndAimPos = Spring.SetUnitMidAndAimPos
 local spGetFeatureCollisionData = Spring.GetFeatureCollisionVolumeData
 local spSetFeatureCollisionData = Spring.SetFeatureCollisionVolumeData
+local spSetFeatureRadiusAndHeight = Spring.SetFeatureRadiusAndHeight
+local spGetFeatureRadius = Spring.GetFeatureRadius
+local spGetFeatureHeight = Spring.GetFeatureHeight
+local spGetUnitHealth = Spring.GetUnitHealth
+local spValidUnitID = Spring.ValidUnitID
+local spGetGameFrame = Spring.GetGameFrame
+
 local spArmor = Spring.GetUnitArmored
 local spActive = Spring.GetUnitIsActive
-local airScalX, airScalY, airScalZ
 local pairs = pairs	
 
 
@@ -32,8 +45,57 @@ if (gadgetHandler:IsSyncedCode()) then
 
 	--Process all initial map features
 	function gadget:Initialize()
-		for _, featID in pairs(Spring.GetAllFeatures()) do
-			gadget:FeatureCreated(featID)
+		local mapConfig = "LuaRules/Configs/DynCVmapCFG/" .. Game.mapName .. ".lua"
+		if VFS.FileExists(mapConfig) then
+			local mapFeatures = VFS.Include(mapConfig)
+			for _, featID in pairs(Spring.GetAllFeatures()) do
+				local featureModel = FeatureDefs[Spring.GetFeatureDefID(featID)].modelname:lower()
+				if featureModel:len() > 4 then
+					local featureModelTrim
+					if Game.version > "91.0" then
+						featureModelTrim = featureModel:sub(1,-5) -- featureModel:match("/.*%."):sub(2,-2)
+					else
+						featureModelTrim = featureModel:match("/.*%."):sub(2,-2)
+					end
+					if mapFeatures[featureModelTrim] then
+						local p = mapFeatures[featureModelTrim]
+						spSetFeatureCollisionData(featID, p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+						spSetFeatureRadiusAndHeight(featID, math.min(p[1], p[3])/2, p[2])
+					elseif featureModel:find(".s3o") then
+						local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetFeatureCollisionData(featID)
+						Spring.Echo(featureModel, xs, ys, zs, xo, yo, zo, vtype, htype, axis)
+						if (vtype>=3 and xs==ys and ys==zs) then
+							if Game.version > "91.0" then
+								spSetFeatureCollisionData(featID, xs, ys*0.75, zs,  xo, yo-ys*0.09, zo,  1, htype, 1)
+							else
+								spSetFeatureCollisionData(featID, xs, ys*0.75, zs,  xo, yo-ys*0.09, zo,  vtype, htype, axis)
+							end
+						end
+					end
+				end
+			end			
+		else
+			for _, featID in pairs(Spring.GetAllFeatures()) do
+				local featureModel = FeatureDefs[Spring.GetFeatureDefID(featID)].modelname:lower()
+				if featureModel:find(".3do") then
+					local rs, hs
+					if (spGetFeatureRadius(featID)>47) then
+						rs, hs = 0.68, 0.60
+					else
+						rs, hs = 0.75, 0.67  
+					end
+					local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetFeatureCollisionData(featID)
+					if (vtype>=3 and xs==ys and ys==zs) then
+						spSetFeatureCollisionData(featID, xs*rs, ys*hs, zs*rs,  xo, yo-ys*0.1323529*rs, zo,  vtype, htype, axis)
+					end
+					spSetFeatureRadiusAndHeight(featID, spGetFeatureRadius(featID)*rs, spGetFeatureHeight(featID)*hs)			
+				elseif featureModel:find(".s3o") then
+					local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetFeatureCollisionData(featID)
+					if (vtype>=3 and xs==ys and ys==zs) then
+						spSetFeatureCollisionData(featID, xs, ys*0.75, zs,  xo, yo-ys*0.09, zo,  vtype, htype, axis)
+					end
+				end
+			end
 		end
 	end
 
@@ -62,44 +124,49 @@ if (gadgetHandler:IsSyncedCode()) then
 					spSetPieceCollisionData(unitID, pieceIndex, false, 1, 1, 1, 0, 0, 0, 1, 1)
 				end
 			end
-		else
-			if UnitDefs[unitDefID].model.type=="3do" then
-				local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetUnitCollisionData(unitID)
-				if (vtype==4 and xs==ys and ys==zs) then
-					if (xs>47 and not UnitDefs[unitDefID].canFly) then
-						spSetUnitCollisionData(unitID, xs*0.68, ys*0.68, zs*0.68,  xo, yo, zo,  vtype, htype, axis)
-					elseif (not UnitDefs[unitDefID].canFly) then
-						spSetUnitCollisionData(unitID, xs*0.75, ys*0.75, zs*0.75,  xo, yo, zo,  vtype, htype, axis)
-					else
-						spSetUnitCollisionData(unitID, xs*0.375, ys*0.225, zs*0.45,  xo, yo, zo,  vtype, htype, axis)
-					end
-				end
+		elseif UnitDefs[unitDefID].model.type=="3do" then
+			local rs, hs, ws
+			if (spGetUnitRadius(unitID)>47 and not UnitDefs[unitDefID].canFly) then
+				rs, hs, ws = 0.68, 0.68, 0.68
+			elseif (not UnitDefs[unitDefID].canFly) then
+				rs, hs, ws = 0.75, 0.75, 0.75
+			else
+				rs, hs, ws = 0.48, 0.225, 0.35
 			end
+			local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetUnitCollisionData(unitID)
+			if (vtype>=3 and xs==ys and ys==zs) then
+				spSetUnitCollisionData(unitID, xs*ws, ys*hs, zs*rs,  xo, yo, zo,  vtype, htype, axis)
+			end
+			--Reduce radius and height for 3DO units, for buildings this results in cons not being able
+			--to start or finish them depending if the scaling was made on UnitCreated() or UnitFinished()
+			--if not UnitDefs[unitDefID].isBuilding then
+				spSetUnitRadiusAndHeight(unitID, spGetUnitRadius(unitID)*rs, spGetUnitHeight(unitID)*hs)
+			--end
 		end
 	end
 
 
-	-- Same as for 3DO units, but for features,
+	-- Same as for 3DO units, but for features
 	function gadget:FeatureCreated(featureID, allyTeam)
-		local featureModel = FeatureDefs[Spring.GetFeatureDefID(featureID)].modelname
-		featureModel = featureModel:lower()
+		local featureModel = FeatureDefs[Spring.GetFeatureDefID(featureID)].modelname:lower()
 		if featureModel:find(".3do") then
-			local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetFeatureCollisionData(featureID)
-			if (vtype==4 and xs==ys and ys==zs) then
-				local yrs
-				if (xs>47) then
-					yrs = ys*0.60
-					spSetFeatureCollisionData(featureID, xs*0.68, yrs, zs*0.68,  xo, yo-yrs*0.15, zo,  vtype, htype, axis)
-				else
-					yrs = ys*0.67
-					spSetFeatureCollisionData(featureID, xs*0.75, yrs, zs*0.75,  xo, yo-yrs*0.15, zo,  vtype, htype, axis)
-				end
+			local rs, hs
+			if (spGetFeatureRadius(featureID)>47) then
+				rs, hs = 0.68, 0.60
+			else
+				rs, hs = 0.75, 0.67
 			end
+			local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetFeatureCollisionData(featureID)
+			if (vtype>=3 and xs==ys and ys==zs) then
+				spSetFeatureCollisionData(featureID, xs*rs, ys*hs, zs*rs,  xo, yo-ys*0.09, zo,  vtype, htype, axis)
+			end
+			spSetFeatureRadiusAndHeight(featureID, spGetFeatureRadius(featureID)*rs, spGetFeatureHeight(featureID)*hs)			
 		end
 	end
 
 
-	--check if a unit is pop-up type (the list must be entered manually)
+	-- Check if a unit is pop-up type (the list must be entered manually)
+	-- If a building was constructed add it to the list for later radius and height scaling
 	function gadget:UnitFinished(unitID, unitDefID, unitTeam)
 		local un = UnitDefs[unitDefID].name
 		if unitCollisionVolume[un] then
@@ -107,6 +174,17 @@ if (gadgetHandler:IsSyncedCode()) then
 		elseif dynamicPieceCollisionVolume[un] then
 			popupUnits[unitID]={name=un, state=-1, perPiece=true, numPieces = #spGetPieceList(unitID)-1}
 		end
+		--[[
+		if UnitDefs[unitDefID].isBuilding and UnitDefs[unitDefID].model.type=="3do" then
+			local rs, hs
+			if (spGetUnitRadius(unitID)>47) then
+				rs, hs, ws = 0.68, 0.68
+			else
+				rs, hs, ws = 0.75, 0.75
+			end
+			scaleBuilding[unitID] = {rs, hs, 3+select(1,spGetGameFrame())}
+		end
+		--]]
 	end
 
 
@@ -118,8 +196,8 @@ if (gadgetHandler:IsSyncedCode()) then
 	end
 
 	
-	--Dynamic adjustment of pop-up style of units' collision volumes based on
-	--unit's ARMORED status, runs twice per second
+	--Dynamic adjustment of pop-up style of units' collision volumes based on unit's ARMORED status, runs twice per second
+	--rescaling of radius and height of 3DO buildings
 	function gadget:GameFrame(n)
 		if (n%15 ~= 0) then
 			return
@@ -137,10 +215,17 @@ if (gadgetHandler:IsSyncedCode()) then
 							else
 								spSetPieceCollisionData(unitID, pieceIndex, false, 1, 1, 1, 0, 0, 0, 1, 1)
 							end
-						end	
+						end
+						if t.offsets then
+							p = t.offsets
+							Spring.SetUnitMidAndAimPos(unitID, 0, spGetUnitHeight(unitID)/2, 0, p[1], p[2], p[3],true)
+						end
 					else
 						p = unitCollisionVolume[defs.name].off
 						spSetUnitCollisionData(unitID, p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+						if p[10] then
+							Spring.SetUnitMidAndAimPos(unitID, 0, spGetUnitHeight(unitID)/2, 0, p[10], p[11], p[12],true)
+						end
 					end
 					popupUnits[unitID].state = 0
 				end
@@ -156,14 +241,34 @@ if (gadgetHandler:IsSyncedCode()) then
 								spSetPieceCollisionData(unitID, pieceIndex, false, 1, 1, 1, 0, 0, 0, 1, 1)
 							end
 						end
+						if t.offsets then
+							p = t.offsets
+							Spring.SetUnitMidAndAimPos(unitID, 0, spGetUnitHeight(unitID)/2, 0, p[1], p[2], p[3],true)
+						end
 					else
 						p = unitCollisionVolume[defs.name].on
 						spSetUnitCollisionData(unitID, p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+						if p[10] then
+							Spring.SetUnitMidAndAimPos(unitID, 0, spGetUnitHeight(unitID)/2, 0, p[10], p[11], p[12],true)
+						end
 					end
 					popupUnits[unitID].state = 1
 				end
 			end			
 		end
+		-- Rescale radius and height of buildings in list
+		--[[
+		for unitID, p in pairs(scaleBuilding) do
+			if spValidUnitID(unitID) then
+				if p[3] < select(1,spGetGameFrame()) then
+					spSetUnitRadiusAndHeight(unitID, spGetUnitRadius(unitID)*p[1], spGetUnitHeight(unitID)*p[2])
+					scaleBuilding[unitID] = nil
+				end
+			else
+				scaleBuilding[unitID] = nil
+			end
+		end
+		--]]
 	end
 	
 end
