1c24b623e19dadda2fc8c0bdbfe8c67a1a135e15
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2012-12-08 00:41:41 +0000

    makes unit_transportfix compatible with spring 0.92, also a bit less hacky

--- luarules/gadgets/unit_transportfix.lua
+++ luarules/gadgets/unit_transportfix.lua
@@ -2,14 +2,16 @@
 --when UnitDestroyed() is called, Spring.GetUnitIsTransporting is already empty -> meh
 --checking newDamage>health in UnitDamaged() does not work because UnitDamaged() does not trigger on selfdestruct -> meh
 --with releaseHeld, on death of a transport UnitUnload is called before UnitDestroyed
---when UnitUnload is called due to transport death, Spring.GetUnitIsDead (transportID) is still false
+--when UnitUnloaded is called due to transport death, Spring.GetUnitIsDead (transportID) is still false
+--when trans is self d'ed, on the frame it dies it has both Spring.GetUnitHealth(ID)>0 and Spring.UnitSelfDTime(ID)>0
+--when trans is crashing it isn't dead
 
 function gadget:GetInfo()
   return {
-    name      = "workaround for airtrans-com-death crash",
-    desc      = "less crashing, hopefully",
-    author    = "knorke",
-    date      = "Dec 2011",
+    name      = "transportfix",
+    desc      = "kills units in transports when transports dies (except commandos)",
+    author    = "knorke, bluestone",
+    date      = "Dec 2012",
     license   = "horse has fallen over",
     layer     = 0,
     enabled   = true
@@ -18,39 +20,70 @@ end
 
 if (not gadgetHandler:IsSyncedCode()) then return end
 
---in BA "commando" unit survives being shot down during transport
+--in BA "commando" unit survives being shot down during transport, but nothing else
+--a commando falling out of a trans will recieve a move order to the point at which the trans died, that order should be cancelled
 local COMMANDO = UnitDefNames["commando"].id
 
-toKill = {} -- [frame] [1]=unitID
+toKill = {} -- [frame][unitID]
+fromtrans = {}
+clearorders = {}
+
 currentFrame = 0
 function gadget:GameFrame (f)
 	currentFrame = f
 	if (toKill[f]) then
-		for i,u in pairs (toKill[f]) do
-			--Spring.Echo ("delay killing " ..i)
-			if (Spring.ValidUnitID (i))	then	--just in case the unit got killed during that 1-frame-wait by other means
-			--***FIXME: copy more closely how units normaly die in destroyed transports: normal explosion + no wreck
-			--Spring.AddUnitDamage (i, math.huge) --this makes it use normal death epxlo but leave wreck. -> would change balance
+		for i,u in pairs (toKill[currentFrame]) do
+			t = fromtrans[currentFrame][i]
+			--Spring.Echo ("delayed killing check called for unit " .. i .. " and trans " .. t .. ". ")
+			
+			--check that trans is dead (or crashing) and unit is still alive 
+			if ((not Spring.GetUnitIsDead(i)) and (Spring.GetUnitIsDead(t) or (Spring.GetUnitMoveTypeData(t).aircraftState=="crashing")))	then	
+				--Spring.AddUnitDamage (i, math.huge) -- would make a normal death explo but would leave wreck
+				--Spring.Echo("killing unit " .. i)
 				Spring.DestroyUnit (i, true, false)
 			end
 		end
-	toKill[f] = nil
+	toKill[currentFrame] = nil
+	fromtrans[currentFrame] = nil
+	end
+
+	if (clearorders[f])	then
+		for i,u in pairs (clearorders[currentFrame]) do
+			--check that unit is still alive 
+			if (not Spring.GetUnitIsDead(i))	then	
+				--Spring.Echo("giving stop order to  unit ".. i)
+				Spring.GiveOrderToUnit(i, CMD.STOP, {}, {})
+			end
+		end
+	clearorders[currentFrame] = nil
 	end
 end
 
+
 function gadget:UnitUnloaded(unitID, unitDefID, teamID, transportID)
-	--Spring.Echo ("unloaded " .. unitID .. " from a DEAD transport")
-	if (Spring.GetUnitSelfDTime (transportID) > 0 or Spring.GetUnitHealth (transportID) < 0) then	--***not sure what happens with transports with selfDestructTime=0
-		--local transDefID =Spring.GetUnitDefID(transportID)
-		--local transDef = UnitDefs [transDefID]
-		--if (not transDef) then Spring.Echo ("transDef = nil!!!!!!!!!!!!!!!!!!!!!!!!") end		
-		--if (not (transDef.customParams and transDef.customParams.releaseheld)) then --BA has no transport that "normally" releaseHeld=true, so commenting thisout
-		
-		if (unitDefID ~= COMMANDO) then
-			--Spring.Echo ("BOOM PASSENGER IS DEAD!")
-			--Spring.AddUnitDamage (unitID, math.huge)	--simply doing this here will still result in crash
-			if (not toKill[currentFrame+1]) then toKill[currentFrame+1] = {} end
-			toKill[currentFrame+1][unitID] = true			
-		end
+
+	--Spring.Echo ("unloaded " .. unitID .. " from transport " .. transportID)
+	
+	--BA has no transport that "normally" releaseHeld=true, so this is commented out
+	--local transDefID =Spring.GetUnitDefID(transportID)
+	--local transDef = UnitDefs [transDefID]
+	--if (not transDef) then Spring.Echo ("transDef = nil!!!!!!!!!!!!!!!!!!!!!!!!") end		
+	--if (not (transDef.customParams and transDef.customParams.releaseheld)) then *** 
+	
+	if (unitDefID ~= COMMANDO) then
+		--Spring.AddUnitDamage (unitID, math.huge)	--simply doing this here will result in a crash
+
+		if (not toKill[currentFrame+1]) then toKill[currentFrame+1] = {} end
+		toKill[currentFrame+1][unitID] = true
+		if (not fromtrans[currentFrame+1]) then fromtrans[currentFrame+1] = {} end
+		fromtrans[currentFrame+1][unitID] = transportID
+
+	else
+		if (not clearorders[currentFrame+2]) then clearorders[currentFrame+2] = {} end 
+		clearorders[currentFrame+2][unitID] = true
 	end
+
+
 end
+
+
