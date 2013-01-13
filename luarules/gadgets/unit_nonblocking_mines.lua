a1d252295283f1f44cf6ead7ec9376fa65e27c86
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-01-13 13:53:36 +0000

    Fixing nonblocking mines and stacked mines (wip)

---
+++ luarules/gadgets/unit_nonblocking_mines.lua
@@ -0,0 +1,77 @@
+
+function gadget:GetInfo()
+	return {
+		name      = 'Nonblocking mines',
+		desc      = 'For 92.+ mines need to be manually unblocked. But other units cannot be built on them.',
+		author    = 'Beherith',
+		date      = 'Jan 2013',
+		license   = 'GNU GPL, v2 or later',
+		layer     = 0,
+		enabled   = true
+	}
+end
+
+----------------------------------------------------------------
+-- Synced only
+----------------------------------------------------------------
+if not gadgetHandler:IsSyncedCode() then
+	return false
+end
+----------------------------------------------------------------
+-- Var
+----------------------------------------------------------------
+local minedefs ={ 
+	[UnitDefNames["armfmine3"].id] = true,
+	[UnitDefNames["corfmine3"].id] = true,
+	[UnitDefNames["armmine1"].id] = true,
+	[UnitDefNames["armmine2"].id] = true,
+	[UnitDefNames["armmine3"].id] = true,
+	[UnitDefNames["cormine1"].id] = true,
+	[UnitDefNames["cormine2"].id] = true,
+	[UnitDefNames["cormine3"].id] = true,
+}
+local mines={}
+local nummines=0
+----------------------------------------------------------------
+-- Speedups
+----------------------------------------------------------------
+local spSetUnitBlocking = Spring.SetUnitBlocking
+local spGetUnitPosition = Spring.GetUnitPosition
+local spGetMyTeamID = Spring.GetMyTeamID
+----------------------------------------------------------------
+-- Callins
+----------------------------------------------------------------
+function gadget:UnitCreated(uID, uDefID, uTeam)
+    if minedefs[uDefID] then
+		--nummines=nummines+1
+		--Spring.Echo('its a mine!',#mines,nummines)
+		local x,_,z= Spring.GetUnitPosition(uID)
+		mines[uID]={x,y,z}
+        spSetUnitBlocking(uID,false,false,false) -- ( number unitID, boolean blocking [, boolean collide [, boolean crushable]] )
+    end
+end
+function gadget:UnitDestroyed(uID, uDefID, uTeam)
+    if minedefs[uDefID] and mines[uID] then
+		--nummines=nummines-1
+		mines[uID] = nil
+        spSetUnitBlocking(uID,false,false,false) -- ( number unitID, boolean blocking [, boolean collide [, boolean crushable]] )
+    end
+end
+function gadget:AllowUnitCreation(unitDefID, builderID,builderTeam, x, y, z) 
+	if x and y and z then
+		local footprintx= UnitDefs[unitDefID]['xsize'] * 4+8 --add 8 for the mines size too
+		local footprintz= UnitDefs[unitDefID]['zsize'] * 4+8  --size is 2x footprint in engine
+		for mine, pos in pairs(mines) do
+			if math.abs(x-pos[1])<footprintx and math.abs(z-pos[3])<footprintz then
+				if builderTeam ~=spGetMyTeamID() then
+					local udef = UnitDefs[builderID]
+					Spring.Echo( udef.humanName  .. ": Can't build on top of mines!" )
+				end
+				
+				return false
+			end
+		end
+	end
+		--Spring.Echo('AllowUnitCreation',x,y,z)
+	return true
+end
