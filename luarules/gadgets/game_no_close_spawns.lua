8356f574bc95e5908896b02dd1a6757dd31041e2
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-01-26 14:15:03 +0000

    Fix game_no_close_spawns.lua from previous commit.

--- luarules/gadgets/game_no_close_spawns.lua
+++ luarules/gadgets/game_no_close_spawns.lua
@@ -23,13 +23,56 @@ if gadgetHandler:IsSyncedCode() then
     ----------------------------------------------------------------
     -- Synced Callins
     ----------------------------------------------------------------
-    
+    local function to_string(data, indent)
+		local str = ""
+
+		if(indent == nil) then
+			indent = 0
+		end
+
+		-- Check the type
+		if(type(data) == "string") then
+			str = str .. (" "):rep(indent) .. data .. "\n"
+		elseif(type(data) == "number") then
+			str = str .. (" "):rep(indent) .. data .. "\n"
+		elseif(type(data) == "boolean") then
+			if(data == true) then
+				str = str .. "true\n"
+			else
+				str = str .. "false\n"
+			end
+		elseif(type(data) == "table") then
+			local i, v
+			for i, v in pairs(data) do
+				-- Check for a table in a table
+				if(type(v) == "table") then
+					str = str .. (" "):rep(indent) .. i .. ":\n"
+					str = str .. to_string(v, indent + 2)
+				else
+			str = str .. (" "):rep(indent) .. i .. ": " ..to_string(v, 0)
+			end
+			end
+		elseif (data ==nil) then
+			str=str..'nil'
+		else
+		   -- print_debug(1, "Error: unknown data type: %s", type(data))
+			--str=str.. "Error: unknown data type:" .. type(data)
+			Spring.Echo(type(data) .. 'X data type')
+		end
+
+		return str
+	end
+	
+	
     function gadget:AllowStartPosition(x, y, z, playerID)
-		--Spring.Echo('allowstart',x,z,playerID)
-		for otherplayerID, startPos in pairs(coopStartPoints) do
+		--Spring.Echo('allowstart',x,z,playerID,to_string(startpoints))
+		for otherplayerID, startPos in pairs(startpoints) do
 			if otherplayerID ~= playerID then
+				
+				--Spring.Echo('vs',x,z,playerID, startPos[1],startPos[3])
 				if ((startPos[1]-x)*(startPos[1]-x)+(startPos[2]-y)*(startPos[2]-y)+(startPos[3]-z)*(startPos[3]-z))<250*250 then --at least dgun range away from everyone else
 					Spring.SendMessageToPlayer(playerID,"You cannot place a start position inside of the D-Gun range of an Ally")
+				--	Spring.Echo('canceled start')
 					return false
 				end
 			end
