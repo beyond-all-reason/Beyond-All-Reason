487090c5a41d2f6c34786fdb530fcefd16499d83
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-03-05 23:22:19 +0000

    Given merc/scr sensible explosions

--- units/mercury.lua
+++ units/mercury.lua
@@ -99,7 +99,7 @@ return {
 				craterboost = 0,
 				cratermult = 0,
 				edgeeffectiveness = 0,
-				explosiongenerator = "custom:FLASHSMALLBUILDINGEX",
+				explosiongenerator = "custom:FLASHNUKE280",
 				firestarter = 90,
 				flighttime = 1.5,
 				impulseboost = 0,
