cb4723a9631b108cb5f2a9bf055f1322eb2991d5
Decay <Decay@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-03-16 18:35:47 +0000

    disable by default snd_volume_osd. When this commit will be included in release, we eill enable widget by default and change hotkeys to '+/-': https://github.com/spring/spring/commit/f3282c4dd3cf0560cc69b33a9808be8cf4bd4f51

--- luaui/widgets/snd_volume_osd.lua
+++ luaui/widgets/snd_volume_osd.lua
@@ -15,7 +15,7 @@ function widget:GetInfo()
     date      = "Jan 10, 2012",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = false  --  loaded by default?
   }
 end
 
