9c62f87680d2c256ac627403d0b47476ae639804
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-04-01 01:56:21 +0000

    Added functionality for initial_spawm to intelligently place startpoints of players (and AIs) who did not make one themselves.
    NEEDS testing and modoption.

--- luarules/gadgets/game_initial_spawn.lua
+++ luarules/gadgets/game_initial_spawn.lua
@@ -12,6 +12,8 @@ function gadget:GetInfo()
     }
 end
 
+-- 31/03/13, mo_coop_II deals with the extra startpoints etc needed for teamsIDs with more than one playerID.
+
 ----------------------------------------------------------------
 -- Synced only
 ----------------------------------------------------------------
@@ -44,6 +46,10 @@ if ((modOptions.mo_storageowner) and (modOptions.mo_storageowner == "com")) then
 end
 local startMetal  = tonumber(modOptions.startmetal)  or 1000
 local startEnergy = tonumber(modOptions.startenergy) or 1000
+
+local StartPointTable = {}
+local claimradius = 250*2.5 -- the radius about your own startpoint which the startpoint guesser regards as containing mexes that you've claimed for yourself (dgun range=250)
+local claimheight = 300 -- the height difference relative your own startpoint in which, within the claimradius, the startpoint guesser regards you as claiming mexes (coms can build up a claiff ~200 high but not 250).
 
 ----------------------------------------------------------------
 -- Speedups
@@ -90,23 +96,188 @@ if tonumber((Spring.GetModOptions() or {}).mo_allowfactionchange) == 1 then
     end
 end
 
+-- Construct a table of startpoints that have actually been placed, somewhat hackily because engine does not have the appropriate callins; see http://springrts.com/mantis/view.php?id=3665
+local function MakeStartPointTable()
+	local GaiateamID = Spring.GetGaiaTeamID()
+	local allyTeamIDs = Spring.GetAllyTeamList()
+	for j=1,#allyTeamIDs do
+		local teamIDs = Spring.GetTeamList(allyTeamIDs[j])
+		--Spring.Echo("teams",j)--DEBUG
+		--Spring.Echo(teamIDs)--DEBUG
+		local xmin, zmin, xmax, zmax = spGetAllyTeamStartBox(allyTeamIDs[j]) 
+		for i=1,#teamIDs do
+			local isactive,isspec = true,false
+			local _,_,_,isAIteam,_,_,_,_ = Spring.GetTeamInfo(teamIDs[i]) 
+			local x,_,z = Spring.GetTeamStartPosition(teamIDs[i])
+			local isGaiateam = (teamIDs[i] == GaiateamID)
+			playerIDs = Spring.GetPlayerList(teamIDs[i])
+			--Spring.Echo("players", i)
+			--Spring.Echo(playerIDs)--DEBUG
+			if #playerIDs ~= 0 then
+				if playerIDs[1] ~= nil then
+					local _,isactive,isspec,_,_,_,_,_,_,_ = Spring.GetPlayerInfo(playerIDs[1])
+				end
+			end
+			if (not isAIteam) and (isactive and (not isspec) and (not isGaiateam) and ((xmin ~= x) or (zmin ~= z))) then 
+				StartPointTable[teamIDs[i]]={x,z} --we believe this startpoint is genuine!
+			else
+				if (not isGaiateam) then
+					StartPointTable[teamIDs[i]]={-1,-1}
+				end
+			end
+		end
+	end
+	return StartPointTable
+end
+
+
 local function SpawnTeamStartUnit(teamID, allyID, x, z)
     local startUnit = spGetTeamRulesParam(teamID, startUnitParamName)
-    if x <= 0 or z <= 0 then
-        local xmin, zmin, xmax, zmax = spGetAllyTeamStartBox(allyID)
-        x = 0.5 * (xmin + xmax)
-        z = 0.5 * (zmin + zmax)
+    local xmin, zmin, xmax, zmax = spGetAllyTeamStartBox(allyID) 
+
+	--Spring.Echo("Checking team, ",teamID)--DEBUG
+	if (StartPointTable[teamID][1] == -1) then 
+		--Spring.Echo("Guessing for team, ",teamID)--DEBUG
+		x,z=GuessStartSpot(teamID, allyID, xmin, zmin, xmax, zmax)
     end
-    local unitID = spCreateUnit(startUnit, x, spGetGroundHeight(x, z), z, 0, teamID)
+
+    local unitID = spCreateUnit(startUnit, x, spGetGroundHeight(x, z), z, 0, teamID) -- feed x=z=0 into this and engine will place in TL corner of startbox, or failing that in center of map
     if (comStorage) then
       Spring.AddUnitResource(unitID, 'm', startMetal)
       Spring.AddUnitResource(unitID, 'e', startEnergy)
     end
 end
 
-function gadget:GameStart()
+function gadget:GameStart() -- At this point it seems all playerIDs "without" a startpoint have had a startpoint placed by the engine at the north west point of their startbox.
+	local StartPointTable = MakeStartPointTable() --TODO modoption
     for teamID, allyID in pairs(spawnTeams) do
         local startX, _, startZ = Spring.GetTeamStartPosition(teamID)
-        SpawnTeamStartUnit(teamID, allyID, startX, startZ)
+        SpawnTeamStartUnit(teamID, allyID, startX, startZ) 
     end
 end
+
+
+
+function GuessStartSpot(teamID, allyID, xmin, zmin, xmax, zmax)
+	--Sanity check, covers case when there are no startboxes 
+	if (xmin >= xmax) or (zmin>=zmax) then return 0,0 end 
+	
+	-- Try our guesses
+	local x,z = GuessOne(teamID, allyID, xmin, zmin, xmax, zmax)
+	Spring.Echo(x,z)
+	if x>=0 and z>=0 then
+		StartPointTable[teamID]={x,z} 
+		return x,z 
+	else
+		x,z = GuessTwo(teamID, allyID, xmin, zmin, xmax, zmax)
+		if x>=0 and z>=0 then 
+				StartPointTable[teamID]={x,z} 
+			return x,z 
+		end
+	end
+	
+
+	-- GIVE UP, fuuuuuuuuuuuuu --
+	x = (xmin + xmax) / 2
+	z = (zmin + zmax) / 2
+	StartPointTable[teamID]={x,z} 
+	return x,z
+end
+
+-- guess based on metal spots --
+function GuessOne(teamID, allyID, xmin, zmin, xmax, zmax) 	
+
+	-- TODO check if mex list is sensible
+	local metalspots = GG.metalSpots
+
+	-- find free metal spots
+	local freemetalspots = {} -- will contain all metalspots that are within teamIDs startbox and are not within one of the cylinders given by (claimradius,claimheight) about an already existing startpoint
+	local k = 1
+	for i=1,#metalspots do 
+		local spot = metalspots[i]
+		local mx,mz = spot.x,spot.z
+		local my = Spring.GetGroundHeight(mx,mz)
+		local iswithinstartbox = ((xmin < mx) and (mx < xmax) and (zmin < mz) and (mz < zmax))
+
+		local isfree = true
+		for _,startpoint in pairs(StartPointTable) do -- we avoid enemy startpoints too, to prevent unnecessary explosions
+			local sx,sz = startpoint[1],startpoint[2]
+			local sy = Spring.GetGroundHeight(sx,sz)
+			local iswithinclaimradius = ((sx-mx)*(sx-mx)+(sz-mz)*(sz-mz) <= (claimradius)*(claimradius))
+			local iswithinclaimheight = (math.abs(my-sy) <= claimheight)
+			if iswithinclaimradius and iswithinclaimheight then 
+				isfree = false 
+				break
+			end
+		end		
+		
+		if isfree and iswithinstartbox then
+			freemetalspots[k] = {mx,mz}
+			k = k + 1		
+		end
+	end
+
+	if (not freemetalspots) then 
+		return -1,-1
+	end
+	
+	-- score each free metal spot
+	local freemetalspotscores = {}
+	for i=1,#freemetalspots do freemetalspotscores[i]=0 end 	
+	
+	for i=1,#freemetalspots do
+		local ix,iz = freemetalspots[i][1], freemetalspots[i][2]
+		for j=1,i do
+			local jx,jz = freemetalspots[j][1],freemetalspots[j][2]
+			local score = 1/((ix-jx)*(ix-jx)+(iz-jz)*(iz-jz)) -- Magic formula. (Assumes all metal spots are of equal production value, TODO...)
+			freemetalspotscores[i] = freemetalspotscores[i] + score
+			freemetalspotscores[j] = freemetalspotscores[j] + score
+		end
+	end
+	
+	-- find free metal spot with highest score
+	local bestindex = 1
+	for i=2,#freemetalspotscores do
+		if freemetalspotscores[i] >= freemetalspotscores[bestindex] then
+			bestindex = i
+		end	
+	end
+	
+	-- find nearest free spot closest to best 
+	local bx,bz = freemetalspots[bestindex][1],freemetalspots[bestindex][2]
+	local nx,nz 
+	local bestdistance = (xmax)*(xmax)+(zmax)*(zmax) -- meh, just need to be big
+
+	for i=1,#freemetalspots do
+		if i ~= bestindex then
+			local mx,mz = freemetalspots[i][1],freemetalspots[i][2]
+			local thisdistance = (bx-mx)*(bx-mx)+(bz-mz)*(bz-mz)
+			if thisdistance < bestdistance then
+				bestdistance = thisdistance
+				nx = mx
+				nz = mz
+			end
+		end
+	end
+	
+	-- if it wasn't possible to find a nearest free spot, or some error caused us to find ourselves, make a reasonable choice
+	if nx==nil or nx==bx or nz==nil or nz==bz then 
+		nx=bx+1
+		nz=bx+1
+	end
+			
+	-- move slightly towards nearest from best 
+	local norm = math.sqrt((bx-nx)*(bx-nx)+(bz-nz)*(bz-nz))
+	local dispx = (nx-bx)/norm
+	local dispz = (nz-bz)/norm
+	local disp = 120 -- magic number
+	x = bx + disp * (dispx)
+	z = bz + disp * (dispz)
+	
+	return x,z
+end
+
+function GuessTwo(teamID, allyID, xmin, zmin, xmax, zmax)
+	return -1,-1 --TODO, cycle through map startpoints looking for one that isn't close to already placed startpoint
+end
+
---
+++ luarules/gadgets/mex_spot_finder.lua
@@ -0,0 +1,505 @@
+
+function gadget:GetInfo()
+	return {
+		name      = "Metalspot Finder Gadget",
+		desc      = "Finds metal spots",
+		author    = "Niobium, modified by Google Frog",
+		version   = "v1.1",
+		date      = "November 2010",
+		license   = "GNU GPL, v2 or later",
+		layer     = -math.huge,
+		enabled   = true
+	}
+end
+
+--------------------------------------------------------------------------------
+-- SYNCED
+--------------------------------------------------------------------------------
+if (gadgetHandler:IsSyncedCode()) then
+
+
+------------------------------------------------------------
+-- Config
+------------------------------------------------------------
+local MAPSIDE_METALMAP = "mapconfig/map_metal_layout.lua"
+local GAMESIDE_METALMAP = "LuaRules/Configs/MetalSpots/" .. (Game.mapName or "") .. ".lua"
+
+local DEFAULT_MEX_INCOME = 2
+local MINIMUN_MEX_INCOME = 0.2
+
+local gridSize = 16 -- Resolution of metal map
+local buildGridSize = 8 -- Resolution of build positions
+
+local METAL_MAP_SQUARE_SIZE = 16
+local MEX_RADIUS = Game.extractorRadius
+local MAP_SIZE_X = Game.mapSizeX
+local MAP_SIZE_X_SCALED = MAP_SIZE_X / METAL_MAP_SQUARE_SIZE
+local MAP_SIZE_Z = Game.mapSizeZ
+local MAP_SIZE_Z_SCALED = MAP_SIZE_Z / METAL_MAP_SQUARE_SIZE
+
+local gameConfig = VFS.FileExists(GAMESIDE_METALMAP) and VFS.Include(GAMESIDE_METALMAP) or false
+local mapConfig = VFS.FileExists(MAPSIDE_METALMAP) and VFS.Include(MAPSIDE_METALMAP) or false
+
+------------------------------------------------------------
+-- Speedups
+------------------------------------------------------------
+local min, max = math.min, math.max
+local floor, ceil = math.floor, math.ceil
+local sqrt = math.sqrt
+local huge = math.huge
+
+local spGetGroundInfo = Spring.GetGroundInfo
+local spGetGroundHeight = Spring.GetGroundHeight
+local spTestBuildOrder = Spring.TestBuildOrder
+
+local extractorRadius = Game.extractorRadius
+local extractorRadiusSqr = extractorRadius * extractorRadius
+ 
+local buildmapSizeX = Game.mapSizeX - buildGridSize
+local buildmapSizeZ = Game.mapSizeZ - buildGridSize
+local buildmapStartX = buildGridSize
+local buildmapStartZ = buildGridSize
+
+local metalmapSizeX = Game.mapSizeX - 1.5 * gridSize
+local metalmapSizeZ = Game.mapSizeZ - 1.5 * gridSize
+local metalmapStartX = 1.5 * gridSize
+local metalmapStartZ = 1.5 * gridSize
+
+------------------------------------------------------------
+-- Variables
+------------------------------------------------------------
+
+local mexUnitDef = UnitDefNames["cormex"]
+
+local mexDefInfo = {
+	extraction = 0.001,
+	square = mexUnitDef.extractSquare,
+	oddX = mexUnitDef.xsize % 4 == 2,
+	oddZ = mexUnitDef.zsize % 4 == 2,
+}
+
+local modOptions
+if (Spring.GetModOptions) then
+  modOptions = Spring.GetModOptions()
+end
+
+------------------------------------------------------------
+-- Callins
+------------------------------------------------------------
+function gadget:Initialize()
+	Spring.Log(gadget:GetInfo().name, LOG.INFO, "Mex Spot Finder Initialising")
+	local metalSpots = GetSpots()
+	local metalSpotsByPos = false
+	
+	if #metalSpots < 6 then
+		Spring.Log(gadget:GetInfo().name, LOG.INFO, "Indiscrete metal map detected")
+		metalSpots = false
+	end
+	
+	local metalValueOverride = gameConfig and gameConfig.metalValueOverride
+	
+	if metalSpots then
+		local mult = (modOptions and modOptions.metalmult) or 1
+		local i = 1
+		while i <= #metalSpots do
+			local spot = metalSpots[i]
+			if spot.metal > MINIMUN_MEX_INCOME then
+				if metalValueOverride then
+					spot.metal = metalValueOverride
+				end
+				spot.metal = spot.metal*mult
+				i = i + 1
+			else
+				metalSpots[i] = metalSpots[#metalSpots]
+				metalSpots[#metalSpots] = nil
+			end
+		end
+		
+		metalSpotsByPos = GetSpotsByPos(metalSpots)
+	end
+
+	GG.metalSpots = metalSpots
+	GG.metalSpotsByPos = metalSpotsByPos
+	_G.metalSpots = metalSpots
+	
+	GG.IntegrateMetal = IntegrateMetal
+	
+	Spring.Log(gadget:GetInfo().name, LOG.INFO, "Metal Spots found and GGed")
+end
+
+------------------------------------------------------------
+-- Extractor Income Processing
+------------------------------------------------------------
+
+function IntegrateMetal(x, z, radius)
+	local centerX, centerZ
+	
+	radius = radius or MEX_RADIUS
+	
+	if (mexDefInfo.oddX) then
+		centerX = (floor( x / METAL_MAP_SQUARE_SIZE) + 0.5) * METAL_MAP_SQUARE_SIZE
+	else
+		centerX = floor( x / METAL_MAP_SQUARE_SIZE + 0.5) * METAL_MAP_SQUARE_SIZE
+	end
+	
+	if (mexDefInfo.oddZ) then
+		centerZ = (floor( z / METAL_MAP_SQUARE_SIZE) + 0.5) * METAL_MAP_SQUARE_SIZE
+	else
+		centerZ = floor( z / METAL_MAP_SQUARE_SIZE + 0.5) * METAL_MAP_SQUARE_SIZE
+	end
+	
+	local startX = floor((centerX - radius) / METAL_MAP_SQUARE_SIZE)
+	local startZ = floor((centerZ - radius) / METAL_MAP_SQUARE_SIZE)
+	local endX = floor((centerX + radius) / METAL_MAP_SQUARE_SIZE)
+	local endZ = floor((centerZ + radius) / METAL_MAP_SQUARE_SIZE)
+	startX, startZ = max(startX, 0), max(startZ, 0)
+	endX, endZ = min(endX, MAP_SIZE_X_SCALED - 1), min(endZ, MAP_SIZE_Z_SCALED - 1)
+	
+	local mult = mexDefInfo.extraction
+	local square = mexDefInfo.square
+	local result = 0
+	
+	if (square) then
+		for i = startX, endX do
+			for j = startZ, endZ do
+				local cx, cz = (i + 0.5) * METAL_MAP_SQUARE_SIZE, (j + 0.5) * METAL_MAP_SQUARE_SIZE
+				local _, metal = spGetGroundInfo(cx, cz)
+				result = result + metal
+			end
+		end
+	else
+		for i = startX, endX do
+			for j = startZ, endZ do
+				local cx, cz = (i + 0.5) * METAL_MAP_SQUARE_SIZE, (j + 0.5) * METAL_MAP_SQUARE_SIZE
+				local dx, dz = cx - centerX, cz - centerZ
+				local dist = sqrt(dx * dx + dz * dz)
+				
+				if (dist < radius) then
+					local _, metal = spGetGroundInfo(cx, cz)
+					result = result + metal
+				end
+			end
+		end
+	end
+	
+	return result * mult, centerX, centerZ
+end
+
+------------------------------------------------------------
+-- Mex finding
+------------------------------------------------------------
+local function SanitiseSpots(spots)
+	local i = 1
+	while i <= #spots do
+		local spot = spots[i]
+		if spot and spot.x and spot.z then
+			local metal
+			metal, spot.x, spot.z = IntegrateMetal(spot.x, spot.z)
+			spot.y = spGetGroundHeight(spot.x, spot.z)
+			spot.metal = metalValueOverride or spot.metal or (metal > 0 and metal) or DEFAULT_MEX_INCOME
+			i = i + 1
+		else
+			spot[i] = spot[#spots]
+			spot[#spots] = nil
+		end
+	end
+	
+	return spots
+end
+
+local function makeString(group)
+	if group then
+		local ret = ""
+		for i, v in pairs(group.left) do
+			ret = ret .. i .. v
+		end
+		ret = ret .. " "
+		for i, v in pairs(group.right) do
+			ret = ret .. i .. v
+		end
+		ret = ret .. " " .. group.minZ .. " " .. group.maxZ .. " " .. group.worth
+		return ret
+	else
+		return ""
+	end
+end
+
+function GetSpots()
+	
+	local spots = {}
+
+	-- Check configs
+	if gameConfig then
+		Spring.Log(gadget:GetInfo().name, LOG.INFO, "Loading gameside mex config")
+		if gameConfig.spots then
+			spots = SanitiseSpots(gameConfig.spots)
+			return spots
+		end
+	end
+	
+	if mapConfig then
+		Spring.Log(gadget:GetInfo().name, LOG.INFO, "Loading mapside mex config")
+		loadConfig = true
+		spots = SanitiseSpots(mapConfig.spots)
+		return spots
+	end
+	
+	Spring.Log(gadget:GetInfo().name, LOG.INFO, "Detecting mex config from metalmap")
+
+	-- Main group collection
+	local uniqueGroups = {}
+	
+	-- Strip info
+	local nStrips = 0
+	local stripLeft = {}
+	local stripRight = {}
+	local stripGroup = {}
+	
+	-- Indexes
+	local aboveIdx
+	local workingIdx
+	
+	-- Strip processing function (To avoid some code duplication)
+	local function DoStrip(x1, x2, z, worth)
+		
+		local assignedTo
+		
+		for i = aboveIdx, workingIdx - 1 do
+			if stripLeft[i] > x2 + gridSize then
+				break
+			elseif stripRight[i] + gridSize >= x1 then
+				local matchGroup = stripGroup[i]
+				if assignedTo then
+					if matchGroup ~= assignedTo then
+						for iz = matchGroup.minZ, assignedTo.minZ - gridSize, gridSize do
+							assignedTo.left[iz] = matchGroup.left[iz]
+						end
+						for iz = matchGroup.minZ, matchGroup.maxZ, gridSize do
+							assignedTo.right[iz] = matchGroup.right[iz]
+						end
+						if matchGroup.minZ < assignedTo.minZ then
+							assignedTo.minZ = matchGroup.minZ
+						end
+						assignedTo.maxZ = z
+						assignedTo.worth = assignedTo.worth + matchGroup.worth
+						uniqueGroups[makeString(matchGroup)] = nil
+					end
+				else
+					assignedTo = matchGroup
+					assignedTo.left[z] = assignedTo.left[z] or x1 -- Only accept the first
+					assignedTo.right[z] = x2 -- Repeated overwrite gives us result we want
+					assignedTo.maxZ = z -- Repeated overwrite gives us result we want
+					assignedTo.worth = assignedTo.worth + worth
+				end
+			else
+				aboveIdx = aboveIdx + 1
+			end
+		end
+		
+		nStrips = nStrips + 1
+		stripLeft[nStrips] = x1
+		stripRight[nStrips] = x2
+		
+		if assignedTo then
+			stripGroup[nStrips] = assignedTo
+		else
+			local newGroup = {
+					left = {[z] = x1},
+					right = {[z] = x2},
+					minZ = z,
+					maxZ = z,
+					worth = worth
+				}
+			stripGroup[nStrips] = newGroup
+			uniqueGroups[makeString(newGroup)] = newGroup
+		end
+	end
+	
+	-- Strip finding
+	workingIdx = huge
+	for mz = metalmapStartX, metalmapSizeZ, gridSize do
+		
+		aboveIdx = workingIdx
+		workingIdx = nStrips + 1
+		
+		local stripStart = nil
+		local stripWorth = 0
+		
+		for mx = metalmapStartZ, metalmapSizeX, gridSize do
+			local _, groundMetal = spGetGroundInfo(mx, mz)
+			if groundMetal > 0 then
+				stripStart = stripStart or mx
+				stripWorth = stripWorth + groundMetal
+			elseif stripStart then
+				DoStrip(stripStart, mx - gridSize, mz, stripWorth)
+				stripStart = nil
+				stripWorth = 0
+			end
+		end
+		
+		if stripStart then
+			DoStrip(stripStart, metalmapSizeX, mz, stripWorth)
+		end
+	end
+	
+	-- Final processing
+	for _, g in pairs(uniqueGroups) do
+		local d = {}
+		
+		local gMinX, gMaxX = huge, -1
+		local gLeft, gRight = g.left, g.right
+		for iz = g.minZ, g.maxZ, gridSize do
+			if gLeft[iz] < gMinX then gMinX = gLeft[iz] end
+			if gRight[iz] > gMaxX then gMaxX = gRight[iz] end
+		end
+		local x = (gMinX + gMaxX) * 0.5
+		local z = (g.minZ + g.maxZ) * 0.5
+		
+		d.metal, d.x, d.z = IntegrateMetal(x,z)
+		
+		d.y = spGetGroundHeight(d.x, d.z)
+		
+		local merged = false
+		
+		for i = 1, #spots do
+			local spot = spots[i]
+			local dis = (d.x - spot.x)^2 + (d.z - spot.z)^2
+			if dis < extractorRadiusSqr*4 then
+				local metal, mx, mz = IntegrateMetal((d.x + spot.x) * 0.5, (d.z + spot.z) * 0.5)
+				
+				if dis < extractorRadiusSqr*1.7 or metal > (d.metal + spot.metal)*0.95 then
+					spot.x = mx
+					spot.y = spGetGroundHeight(mx, mx)
+					spot.z = mz
+					spot.metal = metal
+					merged = true
+					break
+				end
+			end
+		end
+		
+		if not merged then
+			spots[#spots + 1] = d
+		end
+	end
+	
+	--for i = 1, #spots do
+	--	Spring.MarkerAddPoint(spots[i].x,spots[i].y,spots[i].z,"")
+	--end
+	
+	return spots
+end
+
+function GetValidStrips(spot)
+	
+	local sMinZ, sMaxZ = spot.minZ, spot.maxZ
+	local sLeft, sRight = spot.left, spot.right
+	
+	local validLeft = {}
+	local validRight = {}
+	
+	local maxZOffset = buildGridSize * ceil(extractorRadius / buildGridSize - 1)
+	for mz = max(sMaxZ - maxZOffset, buildmapStartZ), min(sMinZ + maxZOffset, buildmapSizeZ), buildGridSize do
+		local vLeft, vRight = buildmapStartX, buildmapSizeX
+		for sz = sMinZ, sMaxZ, gridSize do
+			local dz = sz - mz
+			local maxXOffset = buildGridSize * ceil(sqrt(extractorRadiusSqr - dz * dz) / buildGridSize - 1) -- Test for metal being included is dist < extractorRadius
+			local left, right = sRight[sz] - maxXOffset, sLeft[sz] + maxXOffset
+			if left  > vLeft  then vLeft  = left  end
+			if right < vRight then vRight = right end
+		end
+		validLeft[mz] = vLeft
+		validRight[mz] = vRight
+	end
+	
+	spot.validLeft = validLeft
+	spot.validRight = validRight
+end
+
+
+------------------------------------------------------------
+-- Speedup
+------------------------------------------------------------
+function GetSpotsByPos(spots)
+	local spotPos = {}
+	for i = 1, #spots do
+		local spot = spots[i]
+		local x = spot.x
+		local z = spot.z
+		--Spring.MarkerAddPoint(x,0,z,x .. ", " .. z)
+		spotPos[x] = spotPos[x] or {}
+		spotPos[x][z] = i
+	end
+	return spotPos
+end
+
+------------------------------------------------------------
+-- Widget asks Gadget to send data
+------------------------------------------------------------
+function gadget:RecvLuaMsg(msg, playerID)
+	if msg == "RequestMetalSpots" then
+		SendToUnsynced("SendMetalSpots", playerID) 
+	end
+end
+
+------------------------------------------------------------
+-- UNSYNCED
+------------------------------------------------------------
+else
+
+local metalSpots = {}
+local metalSpotsByPos = {}
+
+function WrapToLuaUI(_,playerID)
+	if playerID == Spring.GetLocalPlayerID() and Script.LuaUI('SendMetalSpots') then
+		Script.LuaUI.SendMetalSpots(playerID, metalSpots, metalSpotsByPos)
+	end
+end
+
+local prevFrame = 1
+local FRAME_GAP = 300
+
+function gadget:Update() 
+	local frame = Spring.GetGameFrame()
+	if Spring.IsReplay() then
+		if Script.LuaUI('SendMetalSpots') then
+			Script.LuaUI.SendMetalSpots(playerID, metalSpots, metalSpotsByPos)
+			gadgetHandler:RemoveCallIn("Update")
+		end
+		if (frame > prevFrame) then 
+			if Script.LuaUI('SendMetalSpots') then
+				Script.LuaUI.SendMetalSpots(playerID, metalSpots, metalSpotsByPos)
+				gadgetHandler:RemoveCallIn("Update")
+			else
+				prevFrame = frame + FRAME_GAP
+			end
+		end 
+	else
+		gadgetHandler:RemoveCallIn("Update")
+	end
+end 
+
+function gadget:Initialize()
+	
+	if type(SYNCED.metalSpots) == "table" then
+		for i, v in spairs(SYNCED.metalSpots) do
+			local x = v.x
+			local z = v.z
+			metalSpots[i] = {
+				x = x,
+				y = v.y,
+				z = z,
+				metal = v.metal
+			}
+			--Spring.MarkerAddPoint(x,0,z,x .. ", " .. z)
+			metalSpotsByPos[x] = metalSpotsByPos[x] or {}
+			metalSpotsByPos[x][z] = i
+		end
+	else
+		metalSpots = false
+		metalSpotsByPos = false
+	end
+	gadgetHandler:AddSyncAction('SendMetalSpots',WrapToLuaUI)
+end
+
+end
