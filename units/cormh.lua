f85203dbf7fd56a65fb3c397c09188f521c86af8
nixtux <nixtux@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-05-26 08:29:18 +0000

    Add firesubmerge tag true , fixes core missile hover not firing correctly closes #322

--- units/cormh.lua
+++ units/cormh.lua
@@ -98,6 +98,7 @@ return {
 				craterboost = 0,
 				cratermult = 0,
 				explosiongenerator = "custom:FLASH4",
+				firesubmersed = true,
 				firestarter = 100,
 				flighttime = 10,
 				impulseboost = 0.12300000339746,
