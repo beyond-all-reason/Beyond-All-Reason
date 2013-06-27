e0e838e14055c1c07dd5e93c607a8bba84710519
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-06-27 23:02:59 +0000

    make specific unit reclaimer work with nanos too

--- luaui/widgets/unit_specific_unit_reclaimer.lua
+++ luaui/widgets/unit_specific_unit_reclaimer.lua
@@ -1,14 +1,14 @@
 
 function widget:GetInfo()
-  return {
-    name      = "Specific Unit Reclaimer",
-    desc      = "Hold down Alt and give an area reclaim order, centered on a unit of the type to reclaim.",
-    author    = "Google Frog",
-    date      = "May 12, 2008",
-    license   = "GNU GPL, v2 or later",
-    layer     = 0,
-    enabled   = true  --  loaded by default?
-  }
+	return {
+		name			= "Specific Unit Reclaimer",
+		desc			= "Hold down Alt and give an area reclaim order, centered on a unit of the type to reclaim.",
+		author		= "Google Frog",
+		date			= "May 12, 2008",
+		license	 = "GNU GPL, v2 or later",
+		layer		 = 0,
+		enabled	 = true	--	loaded by default?
+	}
 end
 
 local team = Spring.GetMyTeamID()
@@ -30,69 +30,56 @@ local spGetModKeyState = Spring.GetModKeyState
 
 local reclaimEnemy = Game.reclaimAllowEnemies
 
+local CMD_RECLAIM = CMD.RECLAIM
+local CMD_STOP = CMD.STOP
 --
 
-function spEcho(text)
-  Spring.Echo(text)
-end
 
 function widget:CommandNotify(id, params, options)
 
-
-  if (id == 90) and (#params == 4) then
-	local alt,_,_,_ = spGetModKeyState()
-	if not alt then 
-		return false
+	if id ~= CMD_RECLAIM or #params ~= 4 then
+		return
 	end
-    
+	if not options.alt then 
+		return
+	end
+	
 	local cx, cy, cz = params[1], params[2], params[3]
-	
-	local mx,my,mz = spWorldToScreenCoords(cx, cy, cz)
-    local cType,id = spTraceScreenRay(mx,my) 
-	
-	if (cType == "unit") then 
-  
-	  local cr = params[4]
 
-	  local selUnits = spGetSelectedUnits()
-	  
-	  local shift = options.shift
-	  
-	  if not shift then
-	    for i, sid in ipairs(selUnits) do 
-		  spGiveOrderToUnit(sid, CMD.STOP, {}, CMD.OPT_RIGHT)
-	    end
-	  end
-	  
-	  if reclaimEnemy and spGetUnitAllyTeam(id) ~= allyTeam then
-	    
+	local mx,my,mz = spWorldToScreenCoords(cx, cy, cz)
+	local cType,id = spTraceScreenRay(mx,my) 
+
+	if cType == "unit" then 
+
+		local cr = params[4]
+
+		local selUnits = spGetSelectedUnits()
+	
+		
+		local targetEnemy = reclaimEnemy and spGetUnitAllyTeam(id) ~= allyTeam
+		local unitDef = spGetUnitDefID(id)
 		local areaUnits = spGetUnitsInCylinder(cx ,cz , cr)
 		
-	    for i, aid in ipairs(areaUnits) do 
-		  if spGetUnitAllyTeam(aid) ~= allyTeam then
-		    spGiveOrderToUnitArray( selUnits, CMD.RECLAIM, {aid}, CMD.OPT_SHIFT)
-		  end
-	    end
-	  
-	  else
-	  
-	    local areaUnits = spGetUnitsInCylinder(cx ,cz , cr, team)
-	    local unitDef = spGetUnitDefID(id)
-	  
-        for i, aid in ipairs(areaUnits) do 
-		  if spGetUnitDefID(aid) == unitDef then
-		    spGiveOrderToUnitArray( selUnits, CMD.RECLAIM, {aid}, CMD.OPT_SHIFT)
-		  end
-	    end
-		
-	  end
-	  
-	return true 
-	  
+		if not targetEnemy and not options.ctrl then
+			areaUnits = spGetUnitsInCylinder(cx ,cz , cr, team)
+		end
+	
+		local count = 0
+		for i, aid in ipairs(areaUnits) do 
+			if (targetEnemy and spGetUnitAllyTeam(aid) ~= allyTeam) or ( not targetEnemy and spGetUnitDefID(aid) == unitDef ) then
+				local cmdOpts = {}
+				if count ~= 0 or options.shift then
+					cmdOpts = {"shift"}
+				end
+				spGiveOrderToUnitArray( selUnits, CMD_RECLAIM, {aid}, cmdOpts)
+				count = count + 1
+			end
+		end
+		return true 
+	
 	end
 	
-  end
-  
+	
 end
 
 
