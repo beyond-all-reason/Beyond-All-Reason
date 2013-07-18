88e63882142babe591e1129a5db9b5e94ddac273
TheFatController <TheFatController@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-07-18 19:18:22 +0000

    Increased the death explosion damage of Construction Ships and Tidal Generators a little, added a heap corpse for tidal generators

---
+++ weapons/tidal_deathex.lua
@@ -0,0 +1,20 @@
+return {
+	tidal_deathex = {
+		areaofeffect = 180,
+		craterboost = 0,
+		cratermult = 0,
+		explosiongenerator = "custom:FLASHSMALLBUILDINGEX",
+		impulseboost = 0.12300000339746,
+		impulsefactor = 0.12300000339746,
+		name = "TidalDeath",
+		range = 480,
+		reloadtime = 3.5999999046326,
+		soundhit = "xplosml3",
+		soundstart = "largegun",
+		turret = 1,
+		weaponvelocity = 70,
+		damage = {
+			default = 150,
+		},
+	},
+}
