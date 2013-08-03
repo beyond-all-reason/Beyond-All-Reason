4ace488ccc7b99e8a3ea32cb48fb6909c44a8107
BrainDamage <BrainDamage@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-08-03 03:22:46 +0000

    fixed unit mover not to fuck with other units except mobile built from engineers with no queue

--- luaui/widgets/cmd_unit_mover.lua
+++ luaui/widgets/cmd_unit_mover.lua
@@ -13,105 +13,107 @@
 
 function widget:GetInfo()
 	return {
-		name			= "Unit Mover",
-		desc			= "Allows combat engineers to use repeat when building mobile units (use 2 or more build spots)",
+		name		= "Unit Mover",
+		desc		= "Allows combat engineers to use repeat when building mobile units (use 2 or more build spots)",
 		author		= "TheFatController",
-		date			= "Mar 20, 2007",
-		license	 = "GNU GPL, v2 or later",
-		layer		 = 0,
-		enabled	 = false	--	loaded by default?
+		date		= "Mar 20, 2007",
+		license		= "GNU GPL, v2 or later",
+		layer		= 0,
+		enabled		= false	--	loaded by default?
 	}
 end
-
---------------------------------------------------------------------------------
-
+local GetSpectatingState = Spring.GetSpectatingState
 local GetCommandQueue = Spring.GetCommandQueue
-local GetPlayerInfo = Spring.GetPlayerInfo
-local GetUnitPosition = Spring.GetUnitPosition
 local GiveOrderToUnit = Spring.GiveOrderToUnit
+local GetUnitPosition = Spring.GetUnitPosition
 local GetMyTeamID = Spring.GetMyTeamID
+local GetSelectedUnits = Spring.GetSelectedUnits
+local GetUnitDefID = Spring.GetUnitDefID
+
+local random = math.random
+
+local CMD_MOVE = CMD.MOVE
+
 
 --------------------------------------------------------------------------------
 
-local countDown = -1
-local DELAY = 0.2
+local myTeamID = GetMyTeamID()
+local engineers = {}
 local moveUnits = {}
-local myID = 0
-local enable = false
+local engineerDefs = {}
+local moveUnitsDefs = {}
 
-local function checkSpec()
-	local _, _, spec = GetPlayerInfo(myID)
-	if spec then
+function widget:PlayerChanged()
+	if GetSpectatingState() then
 		widgetHandler:RemoveWidget()
 	end
 end
 
 function widget:Initialize()
- myID = Spring.GetMyPlayerID()
- checkSpec()
-end
-
-function widget:Update(deltaTime)
- if (countDown == -1) then
-	 return
- else
-	 countDown = countDown + deltaTime
- end
-
- if (countDown > DELAY) then
-	 for unitID,_ in pairs(moveUnits) do
-		 local cQueue = GetCommandQueue(unitID)
-		 if (table.getn(cQueue) == 0) then
-			 local x, y, z = GetUnitPosition(unitID)
-			 if (math.random(1,2) == 1) then
-				 x = x + math.random(50,100)
-			 else
-				 x = x - math.random(50,100)
-			 end
-			 if (math.random(1,2) == 1) then
-				 z = z + math.random(50,100)
-			 else
-				 z = z - math.random(50,100)
-			 end
-			 GiveOrderToUnit(unitID, CMD.FIGHT,	{ x, y, z}, { "" })
-		 end
-	 end
-	 moveUnits = {}
-	 countDown = -1
- end
-end
-
-function widget:UnitFromFactory(unitID, unitDefID, unitTeam, factID, factDefID, userOrders)
-	for uID,_ in pairs(moveUnits) do
-		if (uID == unitID) then
-			table.remove(moveUnits,uID)
-			break
+	widget:PlayerChanged()
+	for unitDefID,unitDef in pairs(UnitDefs) do
+		if unitDef.canMove and unitDef.speed > 0 then --mobile builder
+			for _,buildeeDefID in pairs(unitDef.buildOptions) do
+				local buildeeDef = UnitDefs[buildeeDefID]
+				if buildeeDef.canMove and buildeeDef.speed > 0 then -- can build a mobile unit
+					engineerDefs[unitDefID] = true -- mark the engineer
+					moveUnitsDefs[buildeeDefID] = true --mark the mobile unit
+				end
+			end
 		end
 	end
-end
-
-function widget:PlayerChanged()
-	checkSpec()
-end
-
-function widget:GameFrame(n)
-	if (n > 10) then
-		enable = true -- really ugly workaround until i find the proper fix
+	for _,unitID in pairs(Spring.GetTeamUnits(myTeamID)) do
+		widget:UnitCreated(unitID,GetUnitDefID(unitID),myTeamID)
 	end
+end
+
+
+
+function widget:UnitCreated(unitID, unitDefID, unitTeam,builderID)
+	if unitTeam ~= myTeamID then
+		return
+	end
+	if engineerDefs[unitDefID] then
+		engineers[unitID] = {}
+	end
+	if builderID and moveUnitsDefs[unitDefID] and engineers[builderID] then
+		moveUnits[unitID] = builderID
+	end
+end
+
+function widget:UnitDestroyed(unitID)
+	moveUnits[unitID] = nil
+	engineers[unitID] = nil
+end
+
+function widget:UnitGiven(unitID, unitDefID, newTeam, oldTeamID)
+	widget:UnitDestroyed(unitID)
+	widget:UnitCreated(unitID, unitDefID, newTeam)
+end
+
+function widget:UnitTaken(unitID, unitDefID, oldTeamID, newTeam)
+	widget:UnitDestroyed(unitID)
+	widget:UnitCreated(unitID, unitDefID, newTeam)
 end
 
 function widget:UnitFinished(unitID, unitDefID, unitTeam)
-	if (not enable) then
+	if unitTeam ~= myTeamID then
 		return
 	end
-	if (unitTeam ~= GetMyTeamID()) then
+	local builderID = moveUnits[unitID]
+	if not builderID then
 		return
 	end
-	local ud = UnitDefs[unitDefID]
-	if (ud and (ud.canManualFire) and (ud.speed > 0)) then
-		moveUnits[unitID] = true
-		countDown = 0
-	end
+	moveUnits[unitID] = nil
+	if #GetCommandQueue(unitID) ~= 0 then
+		return
+	end 
+	local x, y, z = GetUnitPosition(unitID)
+	x = x + (random(1,2) == 1 and 1 or -1)*random(50,100)
+	z = z + (random(1,2) == 1 and 1 or -1)*random(50,100)
+	GiveOrderToUnit(unitID, CMD_MOVE, {x, y, z}, { "" })
 end
+
+
 
 --------------------------------------------------------------------------------
