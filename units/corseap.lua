eb8bc61726bb350ae261177dc31f32e2c8f54ea7
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-08-06 13:58:37 +0000

    fixed torpedo bombers unit targeting (but not force fire)

--- units/corseap.lua
+++ units/corseap.lua
@@ -70,7 +70,7 @@ return {
 				model = "torpedo",
 				name = "TorpedoLauncher",
 				noselfdamage = true,
-				range = 500,
+				range = 1100,
 				reloadtime = 8,
 				soundhit = "xplodep2",
 				soundstart = "bombrel",
