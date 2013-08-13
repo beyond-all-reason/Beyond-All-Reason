be613b800de1cd791cdb9942ab64ffb7ff957fa5
BrainDamage <BrainDamage@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-08-13 14:58:25 +0000

    fixed grace on chose ingame

--- luarules/gadgets/cmd_idle_players.lua
+++ luarules/gadgets/cmd_idle_players.lua
@@ -16,6 +16,8 @@ local finishedResumingPing = 2 --in seconds
 local maxInitialQueueSlack = 120 -- in seconds
 local takeCommand = "take2"
 local minTimeToTake = 10 -- in seconds
+--in chose ingame startpostype, players must place beforehand, so take an action, grace period can be shorter
+minTimeToTake = Spring.GetModOptions().startpostype == 2 and 1 or minTimeToTake
 
 local AFKMessage = 'idleplayers '
 local AFKMessageSize = #AFKMessage
