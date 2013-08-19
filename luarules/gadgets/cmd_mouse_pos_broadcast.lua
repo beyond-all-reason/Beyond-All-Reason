c0551a3ff4d5f3bf6f1433ea976429b77fd2b583
BrainDamage <BrainDamage@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-08-19 02:22:07 +0000

    prevent players to see spec camera / mouse

--- luarules/gadgets/cmd_mouse_pos_broadcast.lua
+++ luarules/gadgets/cmd_mouse_pos_broadcast.lua
@@ -100,8 +100,8 @@ function handleMousePosEvent(_,playerID,x,z,click)
 	local spec, fullView = GetSpectatingState()
 	if not spec or not fullView then
 		local _,_,_,_,myAllyTeamID = GetPlayerInfo(myPlayerID)
-		local _,_,_,_,allyTeamID = GetPlayerInfo(playerID)
-		if allyTeamID ~= myAllyTeamID then
+		local _,_,targetSpec,_,allyTeamID = GetPlayerInfo(playerID)
+		if targetSpec or allyTeamID ~= myAllyTeamID then
 			return
 		end
 	end
