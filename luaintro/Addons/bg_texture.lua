4b91e690d911d5dd41b6799dab6f9be84982f90a
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-08-28 12:31:38 +0000

    added luaintro

---
+++ luaintro/Addons/bg_texture.lua
@@ -0,0 +1,58 @@
+
+if addon.InGetInfo then
+	return {
+		name    = "LoadTexture",
+		desc    = "",
+		author  = "jK",
+		date    = "2012",
+		license = "GPL2",
+		layer   = 2,
+		depend  = {"LoadProgress"},
+		enabled = true,
+	}
+end
+
+------------------------------------------
+
+local loadscreens = VFS.DirList("bitmaps/loadpictures/")
+for i=1,(math.floor(10*os.clock()))  do
+		math.random(#loadscreens) --because smth is wrong with math.randomseed(), see http://springrts.com/mantis/view.php?id=3962 (fixme!)
+end
+local screenNum = math.random(#loadscreens) 
+local backgroundTexture = loadscreens[screenNum]
+local aspectRatio
+
+function addon.DrawLoadScreen()
+
+	local loadProgress = SG.GetLoadProgress()
+
+	if not aspectRatio then
+		local texInfo = gl.TextureInfo(backgroundTexture)
+		if not texInfo then return end
+		aspectRatio = texInfo.xsize / texInfo.ysize
+	end
+
+	local vsx, vsy = gl.GetViewSizes()
+	local screenAspectRatio = vsx / vsy
+
+	local xDiv = 0
+	local yDiv = 0
+	local ratioComp = screenAspectRatio / aspectRatio
+
+	if (ratioComp > 1) then
+		xDiv = (1 - (1 / ratioComp)) * 0.5;
+	elseif (math.abs(ratioComp - 1) < 0) then
+	else
+		yDiv = (1 - ratioComp) * 0.5;
+	end
+
+	-- background
+	gl.Color(1,1,1,1)
+	gl.Texture(backgroundTexture)
+	gl.TexRect(0+xDiv,0+yDiv,1-xDiv,1-yDiv)
+	gl.Texture(false)
+end
+
+function addon.Shutdown()
+	gl.DeleteTexture(backgroundTexture)
+end
