789d6c6f80f69546319a806173f51481775beba0
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-11-27 19:47:19 +0000

    add widget to show comblast + dgun range

---
+++ luaui/widgets/gui_comblast_dgun_range.lua
@@ -0,0 +1,182 @@
+function widget:GetInfo()
+    return {
+        name      = "Comblast & Dgun Range",
+        desc      = "Shows the range of commander death explosion and dgun ranges",
+        author    = "Bluestone, based on similar widgets by vbs, tfc, decay",
+        date      = "11/2013",
+        license   = "GPL v3 or later",
+        layer     = 0,
+        enabled   = false  -- loaded by default
+    }
+end
+
+-- locals --
+
+local pairs					= pairs
+
+local spGetUnitPosition     = Spring.GetUnitPosition
+local spGetUnitDefID 		= Spring.GetUnitDefID
+local spGetAllUnits			= Spring.GetAllUnits
+local spGetSpectatingState 	= Spring.GetSpectatingState
+local spGetMyPlayerID		= Spring.GetMyPlayerID
+local spGetPlayerInfo		= Spring.GetPlayerInfo
+local spGetGroundHeight		= Spring.GetGroundHeight
+local spIsSphereInView		= Spring.IsSphereInView
+
+
+local glDepthTest 			= gl.DepthTest
+local glDrawGroundCircle 	= gl.DrawGroundCircle
+local glColor				= gl.Color
+local GL_ALWAYS				= GL.ALWAYS
+
+local circleDivs = 32
+local blastRadius = 360 -- com explosion
+local dgunRange = WeaponDefNames["armcom_arm_disintegrator"].range + 2*WeaponDefNames["armcom_arm_disintegrator"].damageAreaOfEffect
+
+local comCenters = {}
+local drawList
+local amSpec = false
+local inSpecFullView = false
+
+
+-- track coms --
+
+function widget:Initialize()
+    checkComs()
+	checkSpecView()
+    return true
+end
+
+function addCom(unitID)
+	local x,y,z = Spring.GetUnitPosition(unitID)
+	comCenters[unitID] = {x,y,z}
+end
+
+function removeCom(unitID)
+	comCenters[unitID] = nil
+end
+
+function widget:UnitFinished(unitID, unitDefID, unitTeam)
+    if UnitDefs[unitDefID].customParams.iscommander == "1" then
+        addCom(unitID)
+    end
+end
+
+function widget:UnitCreated(unitID, unitDefID, teamID, builderID)
+    if UnitDefs[unitDefID].customParams.iscommander == "1" then
+        addCom(unitID)
+    end
+end
+
+function widget:UnitTaken(unitID, unitDefID, unitTeam, newTeam)
+    if UnitDefs[unitDefID].customParams.iscommander == "1" then
+        addCom(unitID)
+    end
+end
+
+
+function widget:UnitGiven(unitID, unitDefID, unitTeam, oldTeam)
+    if UnitDefs[unitDefID].customParams.iscommander == "1" then
+        addCom(unitID)
+    end
+end
+
+function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
+    if comCenters[unitID] then
+        removeCom(unitID)
+    end
+end
+
+function widget:UnitEnteredLos(unitID, unitTeam)
+    if not amSpec then
+        local unitDefID = spGetUnitDefID(unitID)
+        if UnitDefs[unitDefID].customParams.iscommander == "1" then
+            addCom(unitID)
+        end
+    end
+end
+
+function widget:UnitLeftLos(unitID, unitDefID, unitTeam)
+    if not amSpec then
+        if comCenters[unitID] then
+            removeCom(unitID)
+        end
+    end
+end
+
+function widget:PlayerChanged(playerID)
+    checkSpecView()
+    return true
+end
+
+function widget:GameOver()
+	widgetHandler:RemoveWidget()
+end
+
+function checkSpecView()
+	--check if we became a spec
+    local _,_,spec,_ = spGetPlayerInfo(spGetMyPlayerID())
+    if spec ~= amSpec then
+        amSpec = spec 
+		checkComs()
+    end
+end
+
+function checkComs()
+	--remake list of coms
+	for k,_ in pairs(comCenters) do
+		comCenters[k] = nil
+	end
+	
+    local visibleUnits = spGetAllUnits()
+    if visibleUnits ~= nil then
+        for _, unitID in ipairs(visibleUnits) do
+            local unitDefID = spGetUnitDefID(unitID)
+            if unitDefID and UnitDefs[unitDefID].customParams.iscommander == "1" then
+                addCom(unitID)
+            end
+        end
+    end
+end
+
+
+-- draw -- 
+ 
+-- map out what to draw
+function widget:GameFrame(n)
+	-- check if we are in spec full view
+	local _, specFullView, _ = spGetSpectatingState()
+    if specFullView ~= inSpecFullView then
+		checkComs()
+		inSpecFullView = specFullView
+    end
+
+	-- check com movement
+	for unitID in pairs(comCenters) do
+		local x,y,z = spGetUnitPosition(unitID)
+		local yg = spGetGroundHeight(x,z) 
+		local draw = true
+		-- check if com is off the ground
+		if y-yg>10 then 
+			draw = false
+		-- check if is in view
+		elseif not spIsSphereInView(x,y,z,blastRadius) then
+			draw = false
+		end
+		comCenters[unitID] = {x,y,z,draw}
+	end	
+end
+
+-- draw circles
+function widget:DrawWorldPreUnit()
+	glDepthTest(GL_ALWAYS)
+	for _,center in pairs(comCenters) do
+		if center[4] then
+			glColor(1, 0.8, 0, .6)
+			glDrawGroundCircle(center[1], center[2], center[3], dgunRange, circleDivs)
+			glColor(1, 0, 0, .35)
+			glDrawGroundCircle(center[1], center[2], center[3], blastRadius, circleDivs)
+		end
+	end
+	glDepthTest(false)
+end
