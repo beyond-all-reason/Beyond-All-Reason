619cf949c987c8182631ffbba79143b42d538018
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-12-05 23:57:57 +0000

    start point guesser can also spot when there is an available mex within walking distance of the startbox & start com on edge of startbox

--- luarules/gadgets/game_initial_spawn.lua
+++ luarules/gadgets/game_initial_spawn.lua
@@ -19,6 +19,17 @@ end
 ----------------------------------------------------------------
 if gadgetHandler:IsSyncedCode() then
 
+----------------------------------------------------------------
+-- Speedups
+----------------------------------------------------------------
+local spGetPlayerInfo = Spring.GetPlayerInfo
+local spGetTeamInfo = Spring.GetTeamInfo
+local spGetTeamRulesParam = Spring.GetTeamRulesParam
+local spSetTeamRulesParam = Spring.SetTeamRulesParam
+local spGetTeamStartPosition = Spring.GetTeamStartPosition
+local spGetAllyTeamStartBox = Spring.GetAllyTeamStartBox
+local spCreateUnit = Spring.CreateUnit
+local spGetGroundHeight = Spring.GetGroundHeight
 
 ----------------------------------------------------------------
 -- Config
@@ -38,7 +49,6 @@ local validStartUnits = {
 }
 local spawnTeams = {} -- spawnTeams[teamID] = allyID
 
-
 --each player gets to choose a faction
 local playerStartingUnits = {} -- playerStartingUnits[unitID] = unitDefID
 GG.playerStartingUnits = playerStartingUnits 
@@ -48,8 +58,9 @@ local teamStartPoints = {} -- teamStartPoints[teamID] = {x,y,z}
 GG.teamStartPoints = teamStartPoints 
 
 -- guessing vars for StartPointAssist
-local claimradius = 250*2.3 -- the radius about your own startpoint which the startpoint guesser regards as containing mexes that you've claimed for yourself (dgun range=250)
-local claimheight = 300 -- the height difference relative your own startpoint in which, within the claimradius, the startpoint guesser regards you as claiming mexes (coms can build up a cliff ~200 high but not much more).
+local claimRadius = 250*2.3 -- the radius about your own startpoint which the startpoint guesser regards as containing mexes that you've claimed for yourself (dgun range=250)
+local claimHeight = 300 -- the height difference relative your own startpoint in which, within the claimRadius, the startpoint guesser regards you as claiming mexes (coms can build up a cliff ~200 high but not much more).
+local walkRadius = 250*3 -- the radius outside of the startbox that we regard as being able to walk to a mex on
 local startPointTable = {} --temporary, only for use within this gadget
 
 ----------------------------------------------------------------
@@ -96,17 +107,6 @@ function isNewbie(teamID)
 end
 
 
-----------------------------------------------------------------
--- Speedups
-----------------------------------------------------------------
-local spGetPlayerInfo = Spring.GetPlayerInfo
-local spGetTeamInfo = Spring.GetTeamInfo
-local spGetTeamRulesParam = Spring.GetTeamRulesParam
-local spSetTeamRulesParam = Spring.SetTeamRulesParam
-local spGetTeamStartPosition = Spring.GetTeamStartPosition
-local spGetAllyTeamStartBox = Spring.GetAllyTeamStartBox
-local spCreateUnit = Spring.CreateUnit
-local spGetGroundHeight = Spring.GetGroundHeight
 
 ----------------------------------------------------------------
 -- Initialize
@@ -188,7 +188,7 @@ function gadget:AllowStartPosition(x,y,z,playerID,readyState)
 		
 	-- record table of starting points for startpoint assist to use
 	if readyState == 2 then 
-		startPointTable[teamID]={-3*claimradius,-3*claimradius} --player readied or game was forced started, but player did not place a startpoint.  make a point far away enough not to bother the placer
+		startPointTable[teamID]={-3*claimRadius,-3*claimRadius} --player readied or game was forced started, but player did not place a startpoint.  make a point far away enough not to bother the placing guesser
 	else		
 		startPointTable[teamID]={x,z} --player placed startpoint (may or may not have clicked ready)
 	end	
@@ -250,6 +250,7 @@ end
 --- StartPoint Placing Routines ---
 ----------------------------------------------------------------
 
+
 function GuessStartSpot(teamID, allyID, xmin, zmin, xmax, zmax)
 	--Sanity check
 	if (xmin >= xmax) or (zmin>=zmax) then return 0,0 end 
@@ -259,15 +260,15 @@ function GuessStartSpot(teamID, allyID, xmin, zmin, xmax, zmax)
 	if x>=0 and z>=0 then
 		startPointTable[teamID]={x,z} 
 		return x,z 
-	else
-		x,z = GuessTwo(teamID, allyID, xmin, zmin, xmax, zmax)
-		if x>=0 and z>=0 then 
-				startPointTable[teamID]={x,z} 
-			return x,z 
-		end
 	end
 	
-
+	x,z = GuessTwo(teamID, allyID, xmin, zmin, xmax, zmax)
+	if x>=0 and z>=0 then 
+		startPointTable[teamID]={x,z} 
+		return x,z 
+	end
+	
+	
 	-- GIVE UP, fuuuuuuuuuuuuu --
 	x = (xmin + xmax) / 2
 	z = (zmin + zmax) / 2
@@ -275,96 +276,146 @@ function GuessStartSpot(teamID, allyID, xmin, zmin, xmax, zmax)
 	return x,z
 end
 
--- guess based on metal spots --
+
+
+function IsSteep(x,z)
+	--check if the position (x,z) is too step to start a commander on or not
+	local mtta = math.acos(1.0 - 0.41221) - 0.02 --http://springrts.com/wiki/Movedefs.lua#How_slope_is_determined & the -0.02 is for safety 
+	local a1,a2,a3,a4 = 0,0,0,0
+	local d = 5
+	local y = Spring.GetGroundHeight(x,z)
+	local y1 = Spring.GetGroundHeight(x+d,z)
+	if math.abs(y1 - y) > 0.1 then a1 = math.atan((y1-y)/d) end
+	local y2 = Spring.GetGroundHeight(x,z+d)
+	if math.abs(y2 - y) > 0.1 then a2 = math.atan((y2-y)/d) end
+	local y3 = Spring.GetGroundHeight(x-d,z)
+	if math.abs(y3 - y) > 0.1 then a3 = math.atan((y3-y)/d) end
+	local y4 = Spring.GetGroundHeight(x,z+d)
+	if math.abs(y4 - y) > 0.1 then a4 = math.atan((y4-y)/d) end
+	if math.abs(a1) > mtta or math.abs(a2) > mtta or math.abs(a3) > mtta or math.abs(a4) > mtta then 
+		return true --too steep
+	else
+		return false --ok
+	end	
+	
+end
+
+
+
+-- guess based on metal spots within startbox --
 function GuessOne(teamID, allyID, xmin, zmin, xmax, zmax) 	
 
-	-- Note: This code is deliberately easy to read and not optimized in its logic since there is no pressure on its runtime.
-	-- It's also got magic number style guesswork in it.
 
 	-- check if mex list generation worked and retrieve if so
 	if not GG.metalSpots then
 		return -1,-1
 	end
-	local metalspots = GG.metalSpots
-	if metalspots == false then 
+	local metalSpots = GG.metalSpots
+	if metalSpots == false then 
 		return -1,-1 
 	end
 
 	-- find free metal spots
-	local freemetalspots = {} -- will contain all metalspots that are within teamIDs startbox and are not within one of the cylinders given by (claimradius,claimheight) about an already existing startpoint
+	local freeMetalSpots = {} 		-- will contain all metalspots that are within teamIDs startbox and are not within one of the cylinders given by (claimradius,claimheight) about an already existing startpoint
+	local walkableMetalSpots = {} -- will contain all metalspots that are NOT within teamIDs startbox and are not within one of the cylinders given by (claimradius,claimheight) about an already existing startpoint, but are within walkradius of the startbox
 	local k,j = 1,1
-	for i=1,#metalspots do 
-		local spot = metalspots[i]
+	for i=1,#metalSpots do 
+		local spot = metalSpots[i]
 		local mx,mz = spot.x,spot.z
 		local my = Spring.GetGroundHeight(mx,mz)
-		local iswithinstartbox = (xmin < mx) and (mx < xmax) and (zmin < mz) and (mz < zmax)
+		local isWithinStartBox = (xmin < mx) and (mx < xmax) and (zmin < mz) and (mz < zmax)
+		local isWithinWalkRadius = (mx >= xmin - walkRadius) and (mx <= xmax + walkRadius) and (mz >= zmin - walkRadius) and (mz <= zmax + walkRadius)
 		
-		local isfree = true
+		local isFree = true
 		for _,startpoint in pairs(startPointTable) do -- we avoid enemy startpoints too, to prevent unnecessary explosions and to deal with the case of having no startboxes
 			local sx,sz = startpoint[1],startpoint[2]
 			local sy = Spring.GetGroundHeight(sx,sz)
-			local iswithinclaimradius = ((sx-mx)*(sx-mx)+(sz-mz)*(sz-mz) <= (claimradius)*(claimradius))
-			local iswithinclaimheight = (math.abs(my-sy) <= claimheight)
-			if iswithinclaimradius and iswithinclaimheight then 
-				isfree = false 
+			local isWithinClaimRadius = ((sx-mx)*(sx-mx)+(sz-mz)*(sz-mz) <= (claimRadius)*(claimRadius))
+			local isWithinClaimHeight = (math.abs(my-sy) <= claimHeight)
+			if isWithinClaimRadius and isWithinClaimHeight then 
+				isFree = false 
 				break
 			end
 		end		
 		
-		if isfree and iswithinstartbox then
-			freemetalspots[k] = {mx,mz}
-			k = k + 1		
+		if isFree then
+			if isWithinStartBox then
+				freeMetalSpots[k] = {mx,mz}
+				k = k + 1		
+			elseif isWithinWalkRadius then
+				walkableMetalSpots[j] = {mx,mz}
+				j = j + 1
+			end
 		end
 	end
 
-	if k==1 then --found no free metal spots
-		return -1,-1
+	if k==1 then --found no free metal spots within startbox
+		if j==1 then --found no walkable spots either -> give up
+			return -1,-1
+		end
+		
+		-- find nearest walkable metal spot to startbox
+		local bx,bz -- will contain nearest point in startbox to nearest walkable mex
+		local bestDist = 2*walkRadius
+		for i=1,#walkableMetalSpots do
+			local mx,mz = walkableMetalSpots[i][1], walkableMetalSpots[i][2]
+			local nx = math.max(math.min(mx,xmax),xmin)
+			local nz = math.max(math.min(mz,zmax),zmin)
+			local dist = math.sqrt((mx-nx)^2 + (mz-nz)^2)
+			
+			if not IsSteep(nx,nz) and dist < bestDist then
+				bx = nx
+				bz = nz
+			end		
+		end
+		
+		return bx,bz
 	end
 		
 	-- score each free metal spot
-	local freemetalspotscores = {}
-	for i=1,#freemetalspots do freemetalspotscores[i]=0 end 	
+	local freeMetalSpotScores = {}
+	for i=1,#freeMetalSpots do freeMetalSpotScores[i]=0 end 	
 	
-	for i=1,#freemetalspots do
-		local ix,iz = freemetalspots[i][1], freemetalspots[i][2]
-		for j=1,#freemetalspots do
-			local jx,jz = freemetalspots[j][1],freemetalspots[j][2]
+	for i=1,#freeMetalSpots do
+		local ix,iz = freeMetalSpots[i][1], freeMetalSpots[i][2]
+		for j=1,#freeMetalSpots do
+			local jx,jz = freeMetalSpots[j][1],freeMetalSpots[j][2]
 			if ix ~= jx or iz ~= jz then
-				local r = ((math.abs(ix-jx))^2+(math.abs(iz-jz))^2)^(1/2)
+				local r = math.sqrt((ix-jx)^2+(iz-jz)^2)
 				local iy = Spring.GetGroundHeight(ix,iz)
 				local jy = Spring.GetGroundHeight(jx,jz)
-				local iswithinclaimradius = (r <= claimradius)
-				local iswithinclaimheight = (math.abs(iy-jy) <= claimheight)
+				local isWithinClaimRadius = (r <= claimRadius)
+				local isWithinClaimHeight = (math.abs(iy-jy) <= claimHeight)
 				local score -- Magic formula. Assumes all metal spots are of equal production value, TODO...
-				if iswithinclaimradius and iswithinclaimheight then
+				if isWithinClaimRadius and isWithinClaimHeight then
 					score = 10
 				else
 					score = r^(-1/2)
 				end
-				freemetalspotscores[i] = freemetalspotscores[i] + score
+				freeMetalSpotScores[i] = freeMetalSpotScores[i] + score
 			end
 		end
 	end
 	
 	-- find free metal spot with highest score
-	local bestindex = 1
-	for i=2,#freemetalspotscores do
-		if freemetalspotscores[i] >= freemetalspotscores[bestindex] then
-			bestindex = i
+	local bestIndex = 1
+	for i=2,#freeMetalSpotScores do
+		if freeMetalSpotScores[i] >= freeMetalSpotScores[bestIndex] then
+			bestIndex = i
 		end	
 	end
 	
 	-- find nearest free spot closest to best 
-	local bx,bz = freemetalspots[bestindex][1],freemetalspots[bestindex][2]
+	local bx,bz = freeMetalSpots[bestIndex][1],freeMetalSpots[bestIndex][2]
 	local nx,nz 
-	local bestdistance = (xmax)*(xmax)+(zmax)*(zmax) -- meh, just need to be big
+	local bestDistance = (xmax)*(xmax)+(zmax)*(zmax) -- meh, just need to be big
 
-	for i=1,#freemetalspots do
-		if i ~= bestindex then
-			local mx,mz = freemetalspots[i][1],freemetalspots[i][2]
-			local thisdistance = (bx-mx)*(bx-mx)+(bz-mz)*(bz-mz) --no need to squareroot, we care only about the order 
-			if thisdistance < bestdistance then
-				bestdistance = thisdistance
+	for i=1,#freeMetalSpots do
+		if i ~= bestIndex then
+			local mx,mz = freeMetalSpots[i][1],freeMetalSpots[i][2]
+			local thisDistance = (bx-mx)*(bx-mx)+(bz-mz)*(bz-mz) --no need to squareroot, we care only about the order 
+			if thisDistance < bestDistance then
+				bestDistance = thisDistance
 				nx = mx
 				nz = mz
 			end
@@ -385,30 +436,8 @@ function GuessOne(teamID, allyID, xmin, zmin, xmax, zmax)
 	x = bx + disp * (dispx)
 	z = bz + disp * (dispz)
 	
-	--see how steep the terrain around (x,z) is
-	local steep
-	local mtta = math.acos(1.0 - 0.41221) - 0.02 --http://springrts.com/wiki/Movedefs.lua#How_slope_is_determined & the -0.02 is for safety 
-	local a1,a2,a3,a4 = 0,0,0,0
-	local d = 5
-	local y = Spring.GetGroundHeight(x,z)
-	local y1 = Spring.GetGroundHeight(x+d,z)
-	if math.abs(y1 - y) > 0.1 then a1 = math.atan((y1-y)/d) end
-	local y2 = Spring.GetGroundHeight(x,z+d)
-	if math.abs(y2 - y) > 0.1 then a2 = math.atan((y2-y)/d) end
-	local y3 = Spring.GetGroundHeight(x-d,z)
-	if math.abs(y3 - y) > 0.1 then a3 = math.atan((y3-y)/d) end
-	local y4 = Spring.GetGroundHeight(x,z+d)
-	if math.abs(y4 - y) > 0.1 then a4 = math.atan((y4-y)/d) end
-	if math.abs(a1) > mtta or math.abs(a2) > mtta or math.abs(a3) > mtta or math.abs(a4) > mtta then 
-		steep = true
-	else
-		steep = false
-	end
-	--Spring.Echo(teamID,y-y1,y-y2,y-y3,y-y4,a1,a2,a3,a4,mtta,steep) --debug
-	--Spring.MarkerAddPoint(x,y,z,tostring(teamID))
-	
 	-- if the terrain nearby was too steep, just start on the mex (-> assume mex is passable)
-	if steep then
+	if IsSteep(x,z) then
 		x = bx
 		z = bz
 	end
@@ -416,13 +445,12 @@ function GuessOne(teamID, allyID, xmin, zmin, xmax, zmax)
 	return x,z
 end
 
--- guess based on map startpoints --
+
+
 function GuessTwo(teamID, allyID, xmin, zmin, xmax, zmax)
 	return -1,-1 --TODO: 
 				 -- cycle through map startpoints looking for one that isn't close to an already placed startpoint
-				 -- also try looking for mexes that are just outside of the startbox
 end
-
 
 
 ----------------------------------------------------------------
