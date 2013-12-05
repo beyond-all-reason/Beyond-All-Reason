135569fe64fccc36e866bf57bce7e49da4529538
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-12-05 21:02:09 +0000

    make sure commando doesn't get insane velocity on unload

--- luarules/gadgets/unit_prevent_unload_hax.lua
+++ luarules/gadgets/unit_prevent_unload_hax.lua
@@ -2,7 +2,7 @@ function gadget:GetInfo()
   return {
     name      = "Prevent Unload Hax",
     desc      = "removes unit velocity on unload (and prevents firing units across the map with 'stored' impulse)",
-    author    = "bluestone",
+    author    = "Bluestone",
     date      = "12/08/2013",
     license   = "horse has fallen over, again",
     layer     = 0,
@@ -14,17 +14,17 @@ if (not gadgetHandler:IsSyncedCode()) then return end
 
 local COMMANDO = UnitDefNames["commando"].id
 
-local max = math.max
 local SpSetUnitVelocity = Spring.SetUnitVelocity
 local SpGetUnitVelocity = Spring.GetUnitVelocity
 
 function gadget:UnitUnloaded(unitID, unitDefID, teamID, transportID)
-	if unitID == nil then return end
-	if unitDefID == nil then return end
-	if transportID == nil then return end
+	if unitID == nil or unitDefID == nil or transportID == nil then return end
 
 	if (unitDefID == COMMANDO) then		
 		local x,y,z = SpGetUnitVelocity(transportID)
+		Spring.Echo(x,y,z)
+		if x > 10 then x = 10 elseif x <- 10 then x = -10 end -- 10 is well above 'normal' airtrans velocity
+		if z > 10 then z = 10 elseif z <- 10 then z = -10 end		
 		SpSetUnitVelocity(unitID, x, y,z)
 	else
 		SpSetUnitVelocity(unitID, 0,0,0)	
