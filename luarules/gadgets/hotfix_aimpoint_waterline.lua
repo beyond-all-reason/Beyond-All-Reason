49e2d7cef8c42ec13e88320ba77b96210fd48797
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-12-07 21:57:24 +0000

    opps forgot this...

--- luarules/gadgets/hotfix_aimpoint_waterline.lua
+++ luarules/gadgets/hotfix_aimpoint_waterline.lua
@@ -6,7 +6,7 @@ function gadget:GetInfo()
     date      = "13/11/2013",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = false  --  loaded by default?
+    enabled   = true  --  loaded by default?
   }
 end
 
