cdf345e31fc540bf2feca70de4f74bbf9a993719
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-01-11 20:05:11 +0000

    fix heaps not inheriting reclaim %

--- luarules/gadgets/unit_reclaim_fix.lua
+++ luarules/gadgets/unit_reclaim_fix.lua
@@ -21,10 +21,14 @@ if (not gadgetHandler:IsSyncedCode()) then
 end
 
 local SetFeatureReclaim = Spring.SetFeatureReclaim
+local GetFeaturePosition = Spring.GetFeaturePosition
+local GetFeatureDefID = Spring.GetFeatureDefID
 local GetUnitDefID = Spring.GetUnitDefID
 local GetFeatureResources = Spring.GetFeatureResources
 local GetFeatureResources = Spring.GetFeatureResources
 local CMD_RESURRECT = CMD.RESURRECT
+
+-- implement reclaim 
 
 function gadget:AllowFeatureBuildStep(builderID, builderTeam, featureID, featureDefID, step)
   if step > 0 then return true end
@@ -39,6 +43,54 @@ function gadget:AllowFeatureBuildStep(builderID, builderTeam, featureID, feature
   local newpercent = select(5,GetFeatureResources(featureID)) - ((((resource * oldformula) * 6) - (resource * newformula)) / resource)
   SetFeatureReclaim(featureID, newpercent) 
   return true
+end
+
+
+-- when a wreck dies and becomes a heap, we need to set the reclaim % of the heap to be equal to its 'parent' wreck
+-- order of callins below: featurecreated (for heap), feature destroyed (for wreck), gameframe
+-- two features should not be able to occupy the same pos on the same frame 
+-- so; keep track of features created on that frame, then when a feature dies in coord matching the feature created, transfer reclaim % onto it
+-- no need to transfer rez % since heaps are not rezzable
+local featuresCreatedThisFrame = {}
+
+function gadget:FeatureDestroyed(featureID, allyTeamID)
+	local bpx,bpy,bpz = GetFeaturePosition(featureID)
+	local _,_,_,_, reclaimLeft = GetFeatureResources(featureID)
+	--Spring.Echo("died:", featureID, bpx,bpy,bpz,reclaimLeft, heap)
+
+	--seek out heap, if one exists
+	local replaceFID
+	local nbpx,nbpy,nbpz 
+	for i=1,#featuresCreatedThisFrame do 
+		local nbpx, nbpy, nbpz = GetFeaturePosition(featuresCreatedThisFrame[i])
+		--Spring.Echo("possible", featuresCreatedThisFrame[i], bpx,bpy,bpz,nbpx,nbpy,nbpz)
+		if bpx==nbpx and bpy==nbpy and bpz==nbpz then --floating point errors
+			replaceFID = featuresCreatedThisFrame[i]
+		end
+	end
+	
+	--set heap reclaim %
+	if replaceFID and reclaimLeft then
+		--Spring.Echo("set:", replaceFID, reclaimLeft)
+		SetFeatureReclaim(replaceFID, reclaimLeft)
+	end
+	
+end
+
+function gadget:FeatureCreated(featureID, allyTeamID)
+	--record feature creation
+	--Spring.Echo("created:",featureID)
+	featuresCreatedThisFrame[#featuresCreatedThisFrame+1] = featureID
+end
+
+function gadget:GameFrame()
+	--flush featuresCreatedThisFrame
+	if featuresCreatedThisFrame then
+		for i=1,#featuresCreatedThisFrame do
+			--Spring.Echo("removed",i,featuresCreatedThisFrame[i])
+			featuresCreatedThisFrame[i] = nil
+		end
+	end
 end
 
 --------------------------------------------------------------------------------
