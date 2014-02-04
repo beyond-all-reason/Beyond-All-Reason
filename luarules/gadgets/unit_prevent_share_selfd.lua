61ba2c31a5a0b3b09d7ceede0f88b218b91b0265
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-02-04 23:02:33 +0000

    remove self-d orders when a player quits

--- luarules/gadgets/unit_prevent_share_selfd.lua
+++ luarules/gadgets/unit_prevent_share_selfd.lua
@@ -1,37 +1,39 @@
--- $Id: unit_noselfd.lua 3171 2008-11-06 09:06:29Z det $
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---
---  Copyright (C) 2008.
---  Licensed under the terms of the GNU GPL, v2 or later.
---
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
 function gadget:GetInfo()
   return {
     name      = "No Self-D",
-    desc      = "Prevents self-destruction when a unit changes hands.",
-    author    = "quantum",
+    desc      = "Prevents self-destruction when a unit changes hands or a player leaves.",
+    author    = "quantum, Bluestone",
     date      = "July 13, 2008",
     license   = "GNU GPL, v2 or later",
-    layer     = -10,
+    layer     = 0,
     enabled   = true  --  loaded by default?
   }
 end
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
 
 if (not gadgetHandler:IsSyncedCode()) then
-  return false
-end
-
+ 
 function gadget:UnitGiven(unitID, unitDefID, unitTeam, oldTeam)
   if (Spring.GetUnitSelfDTime(unitID) > 0) then
     Spring.GiveOrderToUnit(unitID, CMD.SELFD, {}, {})
   end
 end
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+function gadget:PlayerChanged(playerID) --necessary wtfness, probably can remove in 97.0+
+end
+
+else
+
+function gadget:PlayerChanged(playerID)
+	local _,active,spec,teamID = Spring.GetPlayerInfo(playerID)
+	if active and not spec then return end
+	local units = Spring.GetTeamUnits(teamID)
+	for _,unitID in pairs(units) do
+		  if (Spring.GetUnitSelfDTime(unitID) > 0) then
+			Spring.GiveOrderToUnit(unitID, CMD.SELFD, {}, {})
+		end
+	end
+end
+
+
+end
