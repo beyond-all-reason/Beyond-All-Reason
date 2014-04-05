c9609f7ac0ae82f5c93fa87b42029d560cb2ebcd
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-04-05 16:44:36 +0000

    add a /luarules loadmissiles command which puts 5 missiles into each stockpilable unit (needs /cheat)

---
+++ luarules/gadgets/cmd_loadmissiles.lua
@@ -0,0 +1,42 @@
+function gadget:GetInfo()
+  return {
+    name      = "Load Missiles",
+    desc      = "provides a \luarules loadmissiles command, can only be used after /cheat",
+    author    = "Bluestone",
+    date      = "05/04/2013",
+    license   = "Horses",
+    layer     = 0,
+    enabled   = true  --  loaded by default?
+  }
+end
+
+if (not gadgetHandler:IsSyncedCode()) then
+	gadgetHandler:RemoveGadget()
+	return
+end
+
+function LoadMissiles()
+	if not Spring.IsCheatingEnabled() then return end
+
+	local units = Spring.GetAllUnits()
+	for _,unitID in ipairs(units) do
+		Spring.SetUnitStockpile(unitID, 5) --no effect if the unit can't stockpile
+	end
+
+end
+
+
+function gadget:Initialize()
+	gadgetHandler:AddChatAction('loadmissiles', LoadMissiles, "")
+end
+
+
+function gadget:Shutdown()
+	gadgetHandler:RemoveChatAction('loadmissiles')
+end
+
+
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
