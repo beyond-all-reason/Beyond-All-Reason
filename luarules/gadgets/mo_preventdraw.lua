fd8ae56f901f10d0ef768fde26ae3313e16c9df3
BrainDamage <BrainDamage@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-04-20 20:52:04 +0000

    updated prevent draw gadget so it can work in FFA and team games reliably too, it checks for allyteam's last coms added an option ( not added to modoptions.lua yet ) to apply to any combomb attack

--- luarules/gadgets/mo_preventdraw.lua
+++ luarules/gadgets/mo_preventdraw.lua
@@ -17,20 +17,25 @@ end
 --------------------------------------------------------------------------------
 
 if (not gadgetHandler:IsSyncedCode()) then
-  return false
+	return false
 end
 
-if (Spring.GetModOptions().deathmode~="com") and (not Game.commEnds) then
-  return false
+local preventAllCombomb = (tonumber(Spring.GetModOptions().mo_preventcombomb) or 0) ~= 0
+local preventDraw = (tonumber(Spring.GetModOptions().mo_preventdraw) or 0) ~= 0
+
+if not preventDraw and not preventAllCombomb then
+	return false
 end
 
-local enabled = tonumber(Spring.GetModOptions().mo_preventdraw) or 0
+local GetTeamInfos = Spring.GetTeamInfos
+local GetUnitPosition = Spring.GetUnitPosition
+local GetGroundHeight = Spring.GetGroundHeight
+local MoveCtrl = Spring.MoveCtrl
+local GetGameFrame = Spring.GetGameFrame
+local DestroyUnit = Spring.DestroyUnit
 
-if (enabled == 0) then 
-  return false
-end
 
-local GetAllUnits = Spring.GetAllUnits
+
 
 local COM_BLAST = WeaponDefNames['commander_blast'].id
 
@@ -44,80 +49,100 @@ local COMMANDER = {
   [UnitDefNames["armcom"].id] = true,
 }
 
-local watchList = {}
 
-local immuneCom = nil
-local ctrlCom = nil
-local cantFall = nil
+local allyTeamComCount = {}
 
-function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, 
-                            weaponID, projectileID, attackerID, attackerDefID, attackerTeam)  
-  if cantFall and (cantFall == unitID) and (weaponID < 0) then      
-    return 0, 0
-  end     
-  if DGUN[weaponID] then
-    if (immuneCom == unitID) then
-      return 0, 0
-    elseif COMMANDER[attackerDefID] and COMMANDER[unitDefID] then
-      for _, uID in ipairs(GetAllUnits()) do
-        if (uID ~= unitID) and (uID ~= attackerID) then
-          if COMMANDER[Spring.GetUnitDefID(uID)] then
-            return damage
-          end
-        end
-      end
-      immuneCom = unitID
-      watchList[unitID] = Spring.GetGameFrame() + 30
-      Spring.DestroyUnit(attackerID,false,false,unitID)
-      Spring.Echo("Commander Ends No Draw: Reversal of Fortune!")
-      return 0, 0
-    end             
-  elseif (weaponID == COM_BLAST) and COMMANDER[unitDefID] then
-    for _, uID in ipairs(GetAllUnits()) do
-      if uID ~= unitID then
-        if COMMANDER[Spring.GetUnitDefID(uID)] and (not Spring.GetUnitIsDead(uID)) then
-          return damage
-        end
-      end
-    end
-    Spring.MoveCtrl.Enable(unitID)
-    ctrlCom = unitID
-    cantFall = unitID
-    watchList[unitID] = Spring.GetGameFrame() + 30
-    return 0, 0
-  end
-  return damage,1
+local immuneDgunList = {}
+local ctrlCom = {}
+local cantFall = {}
+
+function gadget:UnitCreated(unitID, unitDefID, unitTeam)
+	if COMMANDER[unitDefID] then
+		local allyTeamID = select(6,GetTeamInfos(unitTeam))
+		allyTeamComCount[allyTeamID] = (allyTeamComCount[allyTeamID] or 0) + 1
+	end
 end
 
-function gadget:GameFrame(n)
-  for unitID,t in pairs(watchList) do
-    if (n > t) then
-      if (immuneCom == unitID) then
-        immuneCom = nil
-        break
-      elseif (ctrlCom == unitID) then
-        --if the game was actually a draw then this unitID is not valid anymore
-        --if that is the case then just remove it from the watchList and clear the ctrlCom flag		
-        local x,_,z = Spring.GetUnitPosition(unitID)
-        if (x) then
-            local y = Spring.GetGroundHeight(x,z) 
-            Spring.MoveCtrl.SetPosition(unitID, x,y,z)
-            Spring.MoveCtrl.Disable(unitID)
-            watchList[unitID] = Spring.GetGameFrame() + 220
-        else
-            watchList[unitID] = nil
-        end
-        
-        ctrlCom = nil
-        break
-      elseif (cantFall == unitID) then
-        cantFall = nil
-        break
-      else
-        watchList[unitID] = nil
-      end     
-    end
-  end 
+function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
+	if COMMANDER[unitDefID] then
+		local allyTeamID = select(6,GetTeamInfos(unitTeam))
+		allyTeamComCount[allyTeamID] = (allyTeamComCount[allyTeamID] or 0) - 1
+	end
+end
+
+function gadget:UnitGiven(unitID, unitDefID, newTeam, oldTeam)
+	gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
+	gadget:UnitCreated(unitID, unitDefID, unitTeam)
+end
+
+function gadget:UnitTaken(unitID, unitDefID, oldTeam,newTeam)
+	gadget:UnitGiven(unitID, unitDefID, newTeam, oldTeam)
+end
+
+function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer,
+                            weaponID, projectileID, attackerID, attackerDefID, attackerTeam)
+	--falling damage
+	if weaponID < 0 and cantFall[unitID] then
+		return 0, 0
+	end
+	if DGUN[weaponID] then
+		if immuneDgunList[unitID] then
+			return 0, 0
+		elseif COMMANDER[attackerDefID] and COMMANDER[unitDefID] then
+			local targetAllyTeamID = select(6,Spring.GetTeamInfos(unitTeam))
+			if preventAllCombomb or allyTeamComCount[targetAllyTeamID] == 1 then
+				--enemy's last com
+				immuneDgunList[unitID] = GetGameFrame() + 30
+				DestroyUnit(attackerID,false,false,unitID)
+				return 0, 0
+			else
+				return damage
+			end
+		end
+	elseif weaponID == COM_BLAST and COMMANDER[unitDefID] then
+		local targetAllyTeamID = select(6,Spring.GetTeamInfos(unitTeam))
+		if unitID ~= attackerID and (allyTeamComCount[targetAllyTeamID] == 1 or preventAllCombomb ) then
+			--prevent falling damage to the unit, and lock position
+			MoveCtrl.Enable(unitID)
+			ctrlCom[unitID] = GetGameFrame() + 30
+			cantFall[unitID] = GetGameFrame() + 30
+			return 0, 0
+		else
+			--com blast hurts the attacker
+			return damage
+		end
+	end
+	return damage,1
+end
+
+function gadget:GameFrame(currentFrame)
+	for _,expirationTime in pairs(immuneDgunList) do
+		if currentFrame > expirationTime then
+			expirationTime = nil
+		end
+	end
+	for unitID,expirationTime in pairs(ctrlCom) do
+		if currentFrame > expirationTime then
+			--if the game was actually a draw then this unitID is not valid anymore
+			--if that is the case then just remove it from the cantFall list and clear the ctrlCom flag
+			local x,_,z = GetUnitPosition(unitID)
+			if x then
+				local y = GetGroundHeight(x,z)
+				MoveCtrl.SetPosition(unitID, x,y,z)
+				MoveCtrl.Disable(unitID)
+				cantFall[unitID] = currentFrame + 220
+			else
+				cantFall[unitID] = nil
+			end
+
+			expirationTime = nil
+		end
+	end
+	for _,expirationTime in pairs(cantFall) do
+		if currentFrame > expirationTime then
+			expirationTime = nil
+		end
+	end
 end
 
 --------------------------------------------------------------------------------
