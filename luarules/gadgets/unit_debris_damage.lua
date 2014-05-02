4eec8800414ad00702f483629a9c1cb52b206689
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-05-02 21:05:57 +0000

    debris damage = math.random(15,35)

---
+++ luarules/gadgets/unit_debris_damage.lua
@@ -0,0 +1,22 @@
+function gadget:GetInfo()
+  return {
+    name      = "Debris Damage",
+    desc      = "Controls damage done by flying unit debris",
+    author    = "Bluestone",
+    date      = "May 2014",
+    license   = "Horses",
+    layer     = 0,
+    enabled   = true
+  }
+end
+
+if (not gadgetHandler:IsSyncedCode()) then return end
+
+function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
+	-- debris damage occurs when weaponDefID == -1
+	-- in this case attackerID and attackerDefID are nil
+	if weaponDefID ~= -1 then 
+		return math.random(15,35), nil
+	end
+	return damage, nil
+end
