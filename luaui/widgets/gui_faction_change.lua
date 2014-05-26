2afdc36196994c916672f6d679c55ecd1b16dfa5
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-05-26 08:33:55 +0000

    gllist for faction change

--- luaui/widgets/gui_faction_change.lua
+++ luaui/widgets/gui_faction_change.lua
@@ -38,6 +38,9 @@ local glTranslate = gl.Translate
 local glBeginText = gl.BeginText
 local glEndText = gl.EndText
 local glText = gl.Text
+local glCallList = gl.CallList
+local glCreateList = gl.CreateList
+local glDeleteList = gl.DeleteList
 
 local spGetTeamStartPosition = Spring.GetTeamStartPosition
 local spGetTeamRulesParam = Spring.GetTeamRulesParam
@@ -50,6 +53,8 @@ local corcomDefID = UnitDefNames.corcom.id
 
 local commanderDefID = spGetTeamRulesParam(myTeamID, 'startUnit')
 local amNewbie = (spGetTeamRulesParam(myTeamID, 'isNewbie') == 1)
+
+local factionChangeList
 
 --------------------------------------------------------------------------------
 -- Funcs
@@ -101,36 +106,45 @@ function widget:DrawScreen()
 
 	-- Positioning
 	glPushMatrix()
-		glTranslate(px, py, 0)
-
-		-- Panel
-		glColor(0, 0, 0, 0.5)
-		glRect(0, 0, 128, 80)
-
-		-- Highlight
-		glColor(1, 1, 0, 0.5)
-		if commanderDefID == armcomDefID then
-			glRect(1, 1, 63, 63)
-		else
-			glTexRect(65, 1, 127, 63)
-		end
-
-		-- Icons
-		glColor(1, 1, 1, 1)
-		glTexture('LuaUI/Images/ARM.png')
-		glTexRect(8, 8, 56, 56)
-		glTexture('LuaUI/Images/CORE.png')
-		glTexRect(72, 8, 120, 56)
-		glTexture(false)
-
-		-- Text
-		glBeginText()
-			glText('Choose Your Faction', 64, 64, 12, 'cd')
-			glText('ARM', 32, 0, 12, 'cd')
-			glText('CORE', 96, 0, 12, 'cd')
-		glEndText()
+	glTranslate(px, py, 0)
+	--call list
+	if factionChangeList then
+		glCallList(factionChangeList)
+	else 
+		factionChangeList = glCreateList(FactionChangeList)
+	end
 	glPopMatrix()
+
+	
 end
+
+function FactionChangeList()
+	-- Panel
+	glColor(0, 0, 0, 0.5)
+	glRect(0, 0, 128, 80)
+		-- Highlight
+	glColor(1, 1, 0, 0.5)
+	if commanderDefID == armcomDefID then
+		glRect(1, 1, 63, 63)
+	else
+		glTexRect(65, 1, 127, 63)
+	end
+		-- Icons
+	glColor(1, 1, 1, 1)
+	glTexture('LuaUI/Images/ARM.png')
+	glTexRect(8, 8, 56, 56)
+	glTexture('LuaUI/Images/CORE.png')
+	glTexRect(72, 8, 120, 56)
+	glTexture(false)
+		-- Text
+	glBeginText()
+		glText('Choose Your Faction', 64, 64, 12, 'cd')
+		glText('ARM', 32, 0, 12, 'cd')
+		glText('CORE', 96, 0, 12, 'cd')
+	glEndText()
+end
+
+
 
 function widget:MousePress(mx, my, mButton)
 
@@ -155,12 +169,22 @@ function widget:MousePress(mx, my, mButton)
 			end
 			if newCom then
 				commanderDefID = newCom
-				spSendLuaRulesMsg('\138' .. tostring(commanderDefID)) -- tell initial_spawn
-				if WG["faction_change"] then -- tell initial_queue
+				-- tell initial_spawn
+				spSendLuaRulesMsg('\138' .. tostring(commanderDefID)) 
+				-- tell initial_queue
+				if WG["faction_change"] then 
 					WG["faction_change"](commanderDefID)
 				end
+				
+				--Remake gui
+				if factionChangeList then
+					glDeleteList(factionChangeList)
+				end
+				factionChangeList = glCreateList(FactionChangeList)
+			
 				return true
 			end
+			
 		elseif (mButton == 2 or mButton == 3) and mx < px + 128 then
 			-- Dragging
 			return true
