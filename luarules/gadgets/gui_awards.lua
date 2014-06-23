9dd48bf32742591a265ae9660d80e7958ab3fefa
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-06-23 23:23:51 +0000

    [2624] again, but this time make sure to include data for the replay site in the demo file

--- luarules/gadgets/gui_awards.lua
+++ luarules/gadgets/gui_awards.lua
@@ -132,8 +132,6 @@ end
 
 
 function gadget:GameOver(winningAllyTeams)
-    if Spring.GetGameFrame()<=0 then return end
-
 	--calculate average damage dealt
 	local avgTeamDmg = 0 
 	local numTeams = 0
@@ -373,6 +371,18 @@ function ProcessAwards(_,ecoKillAward, ecoKillAwardSec, ecoKillAwardThi, ecoKill
 	bx = cx - w/2
 	by = cy - h/2 - 50
 	
+    --record who won which awards in chat message (for demo parsing by replays.springrts.com)
+	--make all values positive, as unsigned ints are easier to parse
+	local ecoKillLine    = '\161' .. tostring(1+ecoKillAward) .. ':' .. tostring(ecoKillScore) .. '\161' .. tostring(1+ecoKillAwardSec) .. ':' .. tostring(ecoKillScoreSec) .. '\161' .. tostring(1+ecoKillAwardThi) .. ':' .. tostring(ecoKillScoreThi)  
+	local fightKillLine  = '\162' .. tostring(1+fightKillAward) .. ':' .. tostring(fightKillScore) .. '\162' .. tostring(1+fightKillAwardSec) .. ':' .. tostring(fightKillScoreSec) .. '\162' .. tostring(1+fightKillAwardThi) .. ':' .. tostring(fightKillScoreThi)
+	local effKillLine    = '\163' .. tostring(1+effKillAward) ..  ':' .. tostring(effKillScore) .. '\163' .. tostring(1+effKillAwardSec) .. ':' .. tostring(effKillScoreSec) .. '\163' .. tostring(1+effKillAwardThi) .. ':' .. tostring(effKillScoreThi)
+	local otherLine      = '\164' .. tostring(1+cowAward) .. '\165' ..  tostring(1+ecoAward) .. ':' .. tostring(ecoScore).. '\166' .. tostring(1+dmgRecAward) .. ':' .. tostring(dmgRecScore) ..'\167' .. tostring(1+sleepAward) .. ':' .. tostring(sleepScore)
+	local awardsMsg = ecoKillLine .. fightKillLine .. effKillLine .. otherLine
+	Spring.SendLuaRulesMsg(awardsMsg)
+    
+    --don't show anything if game didn't start
+    if Spring.GetGameFrame()<=0 then return end
+
 	--create awards
 	CreateBackground()				
 	FirstAward = CreateAward('fuscup',0,'Destroying enemy resource production', white, ecoKillAward, ecoKillAwardSec, ecoKillAwardThi, ecoKillScore, ecoKillScoreSec, ecoKillScoreThi, 100) 
@@ -387,15 +397,6 @@ function ProcessAwards(_,ecoKillAward, ecoKillAwardSec, ecoKillAwardThi, ecoKill
 	
 	--don't show graph
 	Spring.SendCommands('endgraph 0')	
-
-	--record who won which awards in chat message (for demo parsing by replays.springrts.com)
-	--make all values positive, as unsigned ints are easier to parse
-	local ecoKillLine    = '\161' .. tostring(1+ecoKillAward) .. ':' .. tostring(ecoKillScore) .. '\161' .. tostring(1+ecoKillAwardSec) .. ':' .. tostring(ecoKillScoreSec) .. '\161' .. tostring(1+ecoKillAwardThi) .. ':' .. tostring(ecoKillScoreThi)  
-	local fightKillLine  = '\162' .. tostring(1+fightKillAward) .. ':' .. tostring(fightKillScore) .. '\162' .. tostring(1+fightKillAwardSec) .. ':' .. tostring(fightKillScoreSec) .. '\162' .. tostring(1+fightKillAwardThi) .. ':' .. tostring(fightKillScoreThi)
-	local effKillLine    = '\163' .. tostring(1+effKillAward) ..  ':' .. tostring(effKillScore) .. '\163' .. tostring(1+effKillAwardSec) .. ':' .. tostring(effKillScoreSec) .. '\163' .. tostring(1+effKillAwardThi) .. ':' .. tostring(effKillScoreThi)
-	local otherLine      = '\164' .. tostring(1+cowAward) .. '\165' ..  tostring(1+ecoAward) .. ':' .. tostring(ecoScore).. '\166' .. tostring(1+dmgRecAward) .. ':' .. tostring(dmgRecScore) ..'\167' .. tostring(1+sleepAward) .. ':' .. tostring(sleepScore)
-	local awardsMsg = ecoKillLine .. fightKillLine .. effKillLine .. otherLine
-	Spring.SendLuaRulesMsg(awardsMsg)
 end
 
 
