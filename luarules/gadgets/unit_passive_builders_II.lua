634b332eebb67ee3cbf0adc244384a9a23cb5365
BrainDamage <BrainDamage@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-09-02 23:07:54 +0000

    rewrote passive builders gadget, now it's *slightly* more expensive, but can micromanage builders to the least resource drop, also it won't glitch and stop constructors with edge cases like when pull exceeds storage

--- luarules/gadgets/unit_passive_builders_II.lua
+++ luarules/gadgets/unit_passive_builders_II.lua
@@ -3,8 +3,8 @@ function gadget:GetInfo()
 	return {
 		name      = 'Passive Builders II',
 		desc      = '',
-		author    = 'Niobium',
-		date      = 'April 2011',
+		author    = 'BD',
+		date      = 'Why is date even relevant',
 		license   = 'GNU GPL, v2 or later',
 		layer     = 0,
 		enabled   = true
@@ -22,21 +22,25 @@ end
 -- Var
 ----------------------------------------------------------------
 local CMD_PASSIVE = 34571
-local passiveBuilders = {} -- passiveBuilders[uID] = nil / bool
+local stallMargin = 0.01
+
 local canPassive = {} -- canPassive[uDefID] = nil / true
-local requiresMetal = {} -- requiresMetal[uDefID] = bool
-local requiresEnergy = {} -- requiresEnergy[uDefID] = bool
-local teamList = {} -- teamList[1..n] = teamID
-local teamMetalStalling = {} -- teamStalling[teamID] = nil / bool
-local teamEnergyStalling = {} -- teamStalling[teamID] = nil / bool
+local cost = {} -- cost[uDefID] = {metal=value,energy=value}
+local costID = {}
+local teamStalling = {} -- teamStalling[teamID] = {resName=leftover}
+local passiveCons = {}
+
+local ruleName = "passiveBuilders"
+
+local resTable = {"metal","energy"}
 
 local cmdPassiveDesc = {
-      id      = CMD_PASSIVE,
-      name    = 'passive',
-      action  = 'passive',
-      type    = CMDTYPE.ICON_MODE,
-      tooltip = 'Building Mode: Passive wont build when stalling',
-      params  = {0, 'Active', 'Passive'}
+	  id      = CMD_PASSIVE,
+	  name    = 'passive',
+	  action  = 'passive',
+	  type    = CMDTYPE.ICON_MODE,
+	  tooltip = 'Building Mode: Passive wont build when stalling',
+	  params  = {0, 'Active', 'Passive'}
 }
 
 ----------------------------------------------------------------
@@ -47,72 +51,99 @@ local spFindUnitCmdDesc = Spring.FindUnitCmdDesc
 local spGetUnitCmdDescs = Spring.GetUnitCmdDescs
 local spEditUnitCmdDesc = Spring.EditUnitCmdDesc
 local spGetTeamResources = Spring.GetTeamResources
+local spGetTeamList = Spring.GetTeamList
+local spSetUnitRulesParam = Spring.SetUnitRulesParam
+local spGetUnitRulesParam = Spring.GetUnitRulesParam
+local spGetTeamUnits = Spring.GetTeamUnits
+local spGetUnitIsBuilding = Spring.GetUnitIsBuilding
+local spGetUnitCurrentBuildPower = Spring.GetUnitCurrentBuildPower
+local simSpeed = Game.gameSpeed
 
 ----------------------------------------------------------------
 -- Callins
 ----------------------------------------------------------------
 function gadget:Initialize()
-    for uDefID, uDef in pairs(UnitDefs) do
-        canPassive[uDefID] = ((uDef.canAssist and uDef.buildSpeed > 0) or #uDef.buildOptions > 0)
-        requiresMetal[uDefID] = (uDef.metalCost > 1) -- T1 metal makers cost 1 metal.
-        requiresEnergy[uDefID] = (uDef.energyCost > 0) -- T1 solars cost 0 energy.
-    end
-    teamList = Spring.GetTeamList()
+	for uDefID, uDef in pairs(UnitDefs) do
+		canPassive[uDefID] = ((uDef.canAssist and uDef.buildSpeed > 0) or #uDef.buildOptions > 0)
+		cost[uDefID] = {}
+		cost[uDefID].buildTime = uDef.buildTime
+		for _,resName in pairs(resTable) do
+			cost[uDefID][resName] = uDef[resName .. "Cost"]
+		end
+	end
+	for _,unitID in pairs(Spring.GetAllUnits()) do
+		gadget:UnitCreated(unitID, Spring.GetUnitDefID(unitID), Spring.GetUnitTeam(unitID))
+	end
 end
 
 function gadget:UnitCreated(uID, uDefID, uTeam)
-    if canPassive[uDefID] then
-        spInsertUnitCmdDesc(uID, cmdPassiveDesc)
-    end
-end
-
-function gadget:AllowCommand(uID, uDefID, uTeam, cmdID, cmdParams, cmdOptions, cmdTag, synced)
-    if cmdID == CMD_PASSIVE and canPassive[uDefID] then
-        local cmdIdx = spFindUnitCmdDesc(uID, CMD_PASSIVE)
-        local cmdDesc = spGetUnitCmdDescs(uID, cmdIdx, cmdIdx)[1]
-        cmdDesc.params[1] = cmdParams[1]
-        spEditUnitCmdDesc(uID, cmdIdx, cmdDesc)
-        passiveBuilders[uID] = (cmdParams[1] == 1)
-		--Spring.Echo(uID,'is passive')
-        return false -- Allowing command causes command queue to be lost if command is unshifted
-    end
-    return true
+	if canPassive[uDefID] then
+		spInsertUnitCmdDesc(uID, cmdPassiveDesc)
+	end
+	passiveCons[uID] = spGetUnitRulesParam(uID,ruleName) == 1 or nil
+	costID[uID] = cost[uDefID]
 end
 
 function gadget:UnitDestroyed(uID, uDefID, uTeam)
-    passiveBuilders[uID] = nil
+	costID[uID] = nil
+	passiveCons[uID] = nil
 end
 
+
+function gadget:AllowCommand(uID, uDefID, uTeam, cmdID, cmdParams, cmdOptions, cmdTag, synced)
+	if cmdID == CMD_PASSIVE and canPassive[uDefID] then
+		local cmdIdx = spFindUnitCmdDesc(uID, CMD_PASSIVE)
+		local cmdDesc = spGetUnitCmdDescs(uID, cmdIdx, cmdIdx)[1]
+		cmdDesc.params[1] = cmdParams[1]
+		spEditUnitCmdDesc(uID, cmdIdx, cmdDesc)
+		spSetUnitRulesParam(uID,ruleName,cmdParams[1])
+		passiveCons[uID] = cmdParams[1] == 1 or nil
+		return false -- Allowing command causes command queue to be lost if command is unshifted
+	end
+	return true
+end
+
+
 function gadget:GameFrame(n)
-    if n % 32 == 0 then
-        for i = 1, #teamList do
-            local teamID = teamList[i]
-            local mCur, mStor, mPull, mInc, mExp, mShare, mSent, mRec, mExc = spGetTeamResources(teamID, 'metal')
-            local eCur, eStor, ePull, eInc, eExp, eShare, eSent, eRec, eExc = spGetTeamResources(teamID, 'energy')
-			--Spring.Echo("teamID",teamID,"mCur",mCur,"mPull",mPull, "mExp",mExp, "mExc",mExc,'mInc',mInc,'mRec',mRec,'mSent',mSent)
-            -- stabilize the situation if storage is small
-            if ePull > eExp then
-                eCur = eCur - (ePull - eExp)
-            end
-            if eExc > 0 then
-                eCur = eCur + eExc
-            end            
-            if mPull > mExp then
-                mCur = mCur - (mPull - mExp)
-            end
-            if mExc > 0 then
-                mCur = mCur + mExc
-            end
-            -- never consider it a stall if the actual combined income is higher than the total expense
-            teamMetalStalling[teamID] = (mCur < 0.5 * mPull) and ((mInc + mRec) <= (mExp*1.1 + mSent)) --switch mExp to mPull! otherwise it makes no sense, it shouldnt have even worked!
-            teamEnergyStalling[teamID] = (eCur < 0.5 * ePull) and ((eInc + eRec) <= (eExp*1.1 + eSent))
-			--Spring.Echo("teamID",teamID,"mCur",mCur,"mPull",mPull, "mExp",mExp, "mExc",mExc,'mInc',mInc,'mRec',mRec,'mSent',mSent)
-			--Spring.Echo("teamID",teamID,'estall',teamEnergyStalling[teamID],'mstall',teamMetalStalling[teamID])
+	--with 500 nanos, turning on/off the gadget has no visible impact on sim load -> it's ok to calculate every frame
+	for _,teamID in pairs(spGetTeamList()) do
+		--calculate how much pull passive cons would require
+		local passiveConsPull = {}
+		for unitID in pairs(passiveCons) do
+			local builtUnit = spGetUnitIsBuilding(unitID)
+			if builtUnit then
+				local targetCosts = costID[builtUnit]
+				local step = spGetUnitCurrentBuildPower(unitID)/targetCosts.buildTime
+				for _,resName in pairs(resTable) do
+					local costTick = targetCosts[resName]*step
+					passiveConsPull[resName] = (passiveConsPull[resName] or 0 ) + costTick
+				end
+			end
 		end
-    end
+		teamStalling[teamID] = {}
+		for _,resName in pairs(resTable) do
+			local cur, stor, pull, inc, exp, share, sent, rec, exc = spGetTeamResources(teamID, resName)
+			stor = stor * share -- consider capacity only up to the share slider
+			local reservedExpense = pull - (passiveConsPull[resName] or 0) -- we don't want to touch this part of expense
+			teamStalling[teamID][resName] = cur - stor*stallMargin +(inc+rec-sent-reservedExpense)/simSpeed --amount of res available to assign to passive builders ( in current sim frame )
+		end
+	end
 end
 
 function gadget:AllowUnitBuildStep(builderID, builderTeamID, uID, uDefID, step)
-	--Spring.Echo('gooby pls', not (passiveBuilders[builderID] and ((teamMetalStalling[builderTeamID] and requiresMetal[uDefID]) or (teamEnergyStalling[builderTeamID] and requiresEnergy[uDefID]))))
-    return (step <= 0) or not (passiveBuilders[builderID] and ((teamMetalStalling[builderTeamID] and requiresMetal[uDefID]) or (teamEnergyStalling[builderTeamID] and requiresEnergy[uDefID])))
+	if step <= 0 or not passiveCons[builderID] then
+		return true
+	end
+	local newPulls = {}
+	local wouldStall = false
+	for resName,allocatedPull in pairs(teamStalling[builderTeamID]) do
+		newPulls[resName] = allocatedPull - step*cost[uDefID][resName]
+		if newPulls[resName] <= 0 then
+			wouldStall = true
+		end
+	end
+	if not wouldStall then
+		teamStalling[builderTeamID] = newPulls
+	end
+	return not wouldStall
 end
