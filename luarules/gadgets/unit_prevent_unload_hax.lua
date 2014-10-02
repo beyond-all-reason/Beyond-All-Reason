2c81c7cd2b866cf9dd90706e2acac3378fef0b94
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-10-02 00:14:48 +0000

    fix commandos sometimes getting catapulted across the map if they were unloaded to slightly below groundheight

--- luarules/gadgets/unit_prevent_unload_hax.lua
+++ luarules/gadgets/unit_prevent_unload_hax.lua
@@ -16,22 +16,22 @@ local COMMANDO = UnitDefNames["commando"].id
 
 local SpSetUnitVelocity = Spring.SetUnitVelocity
 local SpGetUnitVelocity = Spring.GetUnitVelocity
+local SpGetGroundHeight = Spring.GetGroundHeight
 
 function gadget:UnitUnloaded(unitID, unitDefID, teamID, transportID)
 	if unitID == nil or unitDefID == nil or transportID == nil then return end
 
 	if (unitDefID == COMMANDO) then		
 		local x,y,z = SpGetUnitVelocity(transportID)
-		if x > 10 then x = 10 elseif x <- 10 then x = -10 end -- 10 is well above 'normal' airtrans velocity
+		if x > 10 then x = 10 elseif x <- 10 then x = -10 end -- 10 is well above 'normal' air-trans velocity
 		if z > 10 then z = 10 elseif z <- 10 then z = -10 end		
-		SpSetUnitVelocity(unitID, x, y,z)
+        local bx,by,bz = Spring.GetUnitPosition(unitID)
+        if by-Spring.GetGroundHeight(bx,bz) < 5 then
+            x = 0; y = 0; z = 0 --in particular, don't give any velocity if the transport has placed the unit slightly underground (or wierdness...)
+        end
+		SpSetUnitVelocity(unitID, x, y, z)
 	else
 		SpSetUnitVelocity(unitID, 0,0,0)	
 	end
 end
-
-
-
 	
-
-
