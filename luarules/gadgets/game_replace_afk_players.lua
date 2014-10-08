b8a23384826ed04b8a6e06b9838c546863f094bf
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-10-08 18:02:27 +0000

    add temporary modoption for replace_afk_players (in case it misbehaves)

--- luarules/gadgets/game_replace_afk_players.lua
+++ luarules/gadgets/game_replace_afk_players.lua
@@ -13,6 +13,11 @@ end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+-- kill switch (just in case... this gadget is  hard to test fully)
+if (tonumber((Spring.GetModOptions() or {}).mo_subs) or 0) == 0 then
+	return false
+end
+
 -----------------------------
 if gadgetHandler:IsSyncedCode() then 
 -----------------------------
