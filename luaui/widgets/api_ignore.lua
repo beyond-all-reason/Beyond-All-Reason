d4244168ac9275aefc9b83526ec9deda9268d980
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-10-09 20:37:40 +0000

    better name

--- luaui/widgets/api_ignore.lua
+++ luaui/widgets/api_ignore.lua
@@ -1,6 +1,6 @@
 function widget:GetInfo()
 	return {
-	name      = "ignorelist", --version 4.1
+	name      = "API Ignore List", --version 4.1
 	desc      = "Adds /ignoreplayer <name>, /unignoreplayer <name>, /ignorelist\n(puts ignoredPlayers table into WG)",
 	author    = "Bluestone",
 	date      = "June 2014", --last change September 10,2009
