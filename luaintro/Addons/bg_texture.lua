7310bb435d77827cadd2ba3a825ae438fd2dee5e
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-10-28 09:51:22 +0000

    hotfix luaintro random number generator

--- luaintro/Addons/bg_texture.lua
+++ luaintro/Addons/bg_texture.lua
@@ -16,7 +16,8 @@ end
 
 local loadscreens = VFS.DirList("bitmaps/loadpictures/")
 local screenNum = math.random(#loadscreens) 
-local backgroundTexture = loadscreens[screenNum]
+--local backgroundTexture = loadscreens[screenNum]
+local backgroundTexture = loadscreens[1+(math.floor((1000*os.clock())%#loadscreens))] -- hacky hotfix for http://springrts.com/mantis/view.php?id=4572
 local aspectRatio
 
 function addon.DrawLoadScreen()
