1db55ce75b026674f057ea6132d417faafa9ea72
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-02-22 20:31:15 +0000

    restore debris damage for ground scouts to pre [2409] level

--- luarules/gadgets/unit_debris_damage.lua
+++ luarules/gadgets/unit_debris_damage.lua
@@ -12,11 +12,26 @@ end
 
 if (not gadgetHandler:IsSyncedCode()) then return end
 
+local groundScouts = {
+    [UnitDefNames["corfav"].id] = true,
+    [UnitDefNames["armfav"].id] = true,
+    [UnitDefNames["corak"].id] = true,
+    [UnitDefNames["armpw"].id] = true,
+    [UnitDefNames["armflea"].id] = true,
+}
+
+local min = math.min
+local random = math.random
+
 function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
 	-- debris damage occurs when weaponDefID == -1
 	-- in this case attackerID and attackerDefID are nil
 	if weaponDefID == -1 then 
-		return math.min(damage, math.random(15,35)), nil
-	end
+        if groundScouts[unitDefID] then
+            return random(45,65), nil
+        else
+            return min(damage, random(15,35)), nil
+        end
+    end
 	return damage, nil
 end
