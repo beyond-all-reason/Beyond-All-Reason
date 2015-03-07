1e9c1a9c2bfc4099a334499bad670704f06b6392
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-03-07 09:25:44 +0000

    fix maverick_exp

--- luarules/gadgets/unit_maverick_exp.lua
+++ luarules/gadgets/unit_maverick_exp.lua
@@ -18,6 +18,7 @@ end
 local SetUnitExperience = Spring.SetUnitExperience
 local AreTeamsAllied = Spring.AreTeamsAllied
 local GetUnitHealth = Spring.GetUnitHealth
+local SetUnitWeaponState = Spring.SetUnitWeaponState
 
 local min = math.min
 
@@ -27,10 +28,11 @@ end
 
 local maverickUnitDefID = UnitDefNames["armmav"].id
 local maverickCost = getCost(maverickUnitDefID)
+local maverickOriginalRange = WeaponDefNames["armmav_armmav_weapon"].range
 
 local mavericks = {}
 
-function gadget:UnitDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, attackerID, attackerDefID, attackerTeam)
+function gadget:UnitDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
 	if not attackerTeam then
 		return
 	end
@@ -41,17 +43,21 @@ function gadget:UnitDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weap
 	if not curExp then
 		return
 	end
+    
 	local _,targetMaxHealth = GetUnitHealth(unitID)
 	local damageFraction = min(damage/targetMaxHealth,1)
 	local costFraction = getCost(unitDefID) / maverickCost
 	local expIncrease = damageFraction*costFraction*0.3
 	if curExp > 3 then expIncrease = expIncrease / (curExp/3) end --linear up to 3, then level off. exp 3 is about the point at which the health increase becomes negligable.
+    if curExp > 4 then expIncrease = 0 end --safety   
 	curExp = curExp + expIncrease
+    
 	SetUnitExperience(attackerID,curExp)
-	mavericks[attackerID] = curExp
+    mavericks[attackerID] = curExp
 end
 
 function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
+    Spring.Echo(unitID, unitDefID, teamID, builderID, maverickUnitDefID)
 	if unitDefID == maverickUnitDefID then
 		mavericks[unitID] = 0
 	end
