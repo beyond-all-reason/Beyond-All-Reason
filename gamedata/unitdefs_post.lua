f829320aa2d2150ab7007809b84ff8eddbfa15c0
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-05-02 08:14:07 +0000

    add commented out functionality to save raw unitdef tables to a customparam (see [3639])

--- gamedata/unitdefs_post.lua
+++ gamedata/unitdefs_post.lua
@@ -1,24 +1,5 @@
 --NOTE: unitdefs_post does not deal with the normal lua UnitDefs table, it deals with the UnitDefs table built from unit definition files.
 
-if (Spring.GetModOptions) then
-	local modOptions = Spring.GetModOptions()
-
-  if (modOptions.mo_transportenemy == "notcoms") then
-  	for name,ud in pairs(UnitDefs) do  
-      if (name == "armcom" or name == "corcom" or name == "armdecom" or name == "cordecom") then
-        ud.transportbyenemy = false
-      end
-    end
-  elseif (modOptions.mo_transportenemy == "none") then
-  	for name, ud in pairs(UnitDefs) do  
-			ud.transportbyenemy = false
-		end
-  end
-  
-end
-
-
-
 local cons = {['armcv'] = true,
 	['armacv']  = true,
 	['consul'] = true,
@@ -82,9 +63,34 @@ for name, ud in pairs(UnitDefs) do
 			end	
 		end
 	end	
+
+    -- deprecated tags
+    ud.collisionvolumetest = nil    
 end
 
--- Setting collisionvolumetest true for all units
-for name, ud in pairs(UnitDefs) do
-		ud.collisionvolumetest = 1
+-------------------------
+
+-- save raw unitdef tables to a string in custom params, can then be written to file as widget
+-- this allows stuff in unitdefs_post to be painlessly baked into unitdef files
+-- VFS.Include("gamedata/unitdefs_post_save_to_customparams.lua")
+
+--------------------------
+
+-- implement various modoptions
+
+if (Spring.GetModOptions) then
+	local modOptions = Spring.GetModOptions()
+
+  if (modOptions.mo_transportenemy == "notcoms") then
+  	for name,ud in pairs(UnitDefs) do  
+      if (name == "armcom" or name == "corcom" or name == "armdecom" or name == "cordecom") then
+        ud.transportbyenemy = false
+      end
+    end
+  elseif (modOptions.mo_transportenemy == "none") then
+  	for name, ud in pairs(UnitDefs) do  
+			ud.transportbyenemy = false
+		end
+  end
+  
 end
---
+++ gamedata/unitdefs_post_save_to_customparams.lua
@@ -0,0 +1,43 @@
+function table.val_to_str ( v )
+  if "string" == type( v ) then
+    v = string.gsub( v, "\n", "\\n" )
+    if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+      return "'" .. v .. "'"
+    end
+    return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+  else
+    return "table" == type( v ) and table.tostring( v ) or
+      tostring( v )
+  end
+end
+
+function table.key_to_str ( k )
+  if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+    if k == "else" then k = "other" end --temp
+    return k
+  else
+    return "[" .. table.val_to_str( k ) .. "]"
+  end
+end
+
+function table.tostring( tbl )
+  local result, done = {}, {}
+  for k, v in ipairs( tbl ) do
+    table.insert( result, table.val_to_str( v ) )
+    done[ k ] = true
+  end
+  for k, v in pairs( tbl ) do
+    if not done[ k ] then
+      table.insert( result,
+        table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+    end
+  end
+  return "{" .. table.concat( result, "," ) .. "}"
+end
+
+for name,ud in pairs(UnitDefs) do
+    local ud_string = table.tostring(ud)
+    ud.customparams = ud.customparams or {}
+    ud.customparams.__ud = table.tostring(ud)
+    Spring.Echo("saved UnitDefs." .. name .. " to customparams.__ud as string")
+end
