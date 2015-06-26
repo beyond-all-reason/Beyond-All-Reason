a1ca3527a280a5139ca1a4161b82b26860c5fe55
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-06-26 17:04:41 +0000

    add widget to auto-enable los view, default diabled

---
+++ luaui/widgets/gfx_los_view.lua
@@ -0,0 +1,38 @@
+
+function widget:GetInfo()
+	return {
+		name      = "LOS View",
+		desc      = "Turns LOS view on while the game is in progress",
+		author    = "Bluestone",
+		date      = "",
+		license   = "Round Objects",
+		layer     = 0,
+		enabled   = false
+	}
+end
+
+function TurnOnLOS()
+    if Spring.GetMapDrawMode()~="los" then
+        Spring.SendCommands("togglelos")
+    end
+end
+
+function TurnOffLOS()
+    if Spring.GetMapDrawMode()=="los" then
+        Spring.SendCommands("togglelos")
+    end
+end
+
+function widget:Initialize()
+    if Spring.GetGameFrame()>0 then
+        TurnOnLOS()
+    end
+end
+
+function widget:GameStart()
+    TurnOnLOS()
+end
+
+function widget:Shutdown()
+    TurnOffLOS()
+end
