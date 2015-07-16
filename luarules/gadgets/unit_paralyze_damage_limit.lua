5c776399415770417c49f242c205f7a2158b651d
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-07-16 17:24:18 +0000

    fix #894

--- luarules/gadgets/unit_paralyze_damage_limit.lua
+++ luarules/gadgets/unit_paralyze_damage_limit.lua
@@ -25,6 +25,14 @@ end
 
 local maxTime = 15 -- in seconds
 
+local excluded = {
+    -- mobile units that are excluded from the maxTime limit
+    [UnitDefNames.armscab.id] = true,
+    [UnitDefNames.cormabm.id] = true,
+    [UnitDefNames.corcarry.id] = true,
+    [UnitDefNames.armcarry.id] = true,
+}
+
 ----------------------------------------------------------------
 -- Callins
 ----------------------------------------------------------------
@@ -32,7 +40,7 @@ local maxTime = 15 -- in seconds
 function gadget:UnitPreDamaged(uID, uDefID, uTeam, damage, paralyzer, weaponID, projID, aID, aDefID, aTeam)
     if paralyzer then
         -- restrict the max paralysis time of mobile units to 15 sec
-        if aDefID and uDefID and weaponID and not UnitDefs[uDefID].isBuilding then
+        if aDefID and uDefID and weaponID and not UnitDefs[uDefID].isBuilding and not excluded[uDefID] then
             local max_para_time = WeaponDefs[weaponID].damages and WeaponDefs[weaponID].damages.paralyzeDamageTime or maxParalysisTime
             local h,mh,ph = Spring.GetUnitHealth(uID)
             local max_para_damage = mh + ((max_para_time<maxTime) and mh or mh*maxTime/max_para_time)
