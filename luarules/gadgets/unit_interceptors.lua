9ca8c4eaa4698e71f8a93d60315b1bd1fdff5454
Decay <Decay@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-07-17 15:16:30 +0000

    Fix #896

--- luarules/gadgets/unit_interceptors.lua
+++ luarules/gadgets/unit_interceptors.lua
@@ -2,7 +2,7 @@ function gadget:GetInfo()
 	return {
 		name     = "Don't target flyover nukes",
 		desc     = "bla",
-		author	 = "ashdnazg",
+		author	 = "ashdnazg + [teh]decay",
 		date     = "Too late",
 		license	 = "GNU GPL, v2 or later",
 		layer    = 0,
@@ -10,6 +10,9 @@ function gadget:GetInfo()
 	}
 end
 
+
+-- changelog:
+-- 17 jul 2015 [teh]decay - fixed error: unit_interceptors.lua"]:27: bad argument #1 to 'unpack' (table expected, got number)
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
@@ -23,9 +26,17 @@ function gadget:AllowWeaponInterceptTarget(interceptorUnitID, interceptorWeaponI
 	local ud = UnitDefs[Spring.GetUnitDefID(interceptorUnitID)]
 	local wd = WeaponDefs[ud.weapons[interceptorWeaponID + 1].weaponDef]
 	local ox, _, oz = Spring.GetUnitPosition(interceptorUnitID)
-	local _, nukeTarget = Spring.GetProjectileTarget(targetProjectileID)
-	local tx, _, tz = unpack(nukeTarget)
-	return (ox - tx) ^ 2 + (oz - tz) ^ 2 < wd.coverageRange ^ 2
+
+    --Spring.GetProjectileTarget( number projectileID ) -> nil | [number targetTypeInt, number targetID | table targetPos = {x, y, z}]
+	local target_type_or_targetPos, targetID = Spring.GetProjectileTarget(targetProjectileID)
+
+    if targetID then -- target = unit
+        local tx,_, tz = Spring.GetUnitPosition(targetID)
+        return (ox - tx) ^ 2 + (oz - tz) ^ 2 < wd.coverageRange ^ 2
+    elseif target_type_or_targetPos then -- table with coordinates
+        local tx, _, tz = unpack(target_type_or_targetPos)
+        return (ox - tx) ^ 2 + (oz - tz) ^ 2 < wd.coverageRange ^ 2
+    end
 end
 
 
