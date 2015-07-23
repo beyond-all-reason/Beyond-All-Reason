42be5b11079e03349024d7d7f182acf2e2a72068
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-07-23 15:39:06 +0000

    Ally cursors: replaced with correct version

--- luaui/widgets/gui_ally_cursors.lua
+++ luaui/widgets/gui_ally_cursors.lua
@@ -9,10 +9,10 @@
 
 function widget:GetInfo()
 	return {
-		name	= "Ally Cursors - dev",
-		desc	= "Shows the mouse pos of players and specs (try: /allycursors_names  _specs  _scaling  _namesfade)",
+		name	= "AllyCursors",
+		desc	= "Shows the mouse pos of allied players",
 		author	= "Floris,jK,TheFatController",
-		date	= "Apr,2009",
+		date	= "31 may 2015",
 		license	= "GNU GPL, v2 or later",
 		layer	= 0,
 		enabled	= true,
@@ -22,38 +22,76 @@ end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
--- Console commands:
-
--- allycursors_names
--- allycursors_specs
--- allycursors_scaling
--- allycursors_namesfade
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
 
 -- configs
 
-local cursorSize				= 11
+local cursorSize					= 11
+local drawNamesCursorSize			= 8.5
 
-local hideSpecs					= false
+local dlistAmount					= 3		-- number of dlists genereted for each player (# availible opacity levels)
 
-local drawNames					= true
-local drawNamesScaling			= true		-- scale up when camera is more distant
-local drawNamesFade				= true
-local NameFadeStartDistance		= 4800
-local NameFadeEndDistance		= 6600
+local sendPacketEvery				= 0.8
+local numMousePos					= 2 --//num mouse pos in 1 packet
 
-local drawNamesCursorSize		= 7
-local fontSizePlayer			= 15
-local fontSizeSpec				= 12.5
-local sendPacketEvery			= 0.8
-local numMousePos				= 2 --//num mouse pos in 1 packet
+local showSpectatorName    			= true
+local showPlayerName       			= true
+local drawNamesScaling				= true
+local drawNamesFade					= true
+
+local fontSizePlayer        		= 18
+local fontOpacityPlayer     		= 0.65
+local fontSizeSpec          		= 14
+local fontOpacitySpec       		= 0.40
+
+local NameFadeStartDistance			= 4800
+local NameFadeEndDistance			= 7200
+
+-- tweak ui
+local buttonsize					= 18
+local rowgap						= 6
+local leftmargin					= 20
+local buttontab						= 200
+local vsx, vsy 						= gl.GetViewSizes()
+local tweakUiWidth, tweakUiHeight	= 240, 215
+local tweakUiPosX, tweakUiPosY		= 500, 550
+
+-- images
+local allyCursor      			    = ":n:"..LUAUI_DIRNAME.."Images/allycursor.dds"
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local pairs					= pairs
+
+function widget:TextCommand(command)
+    local mycommand = false
+    if (string.find(command, "allycursorspecname") == 1  and  string.len(command) == 18) then showSpectatorName = not showSpectatorName end
+
+    if (string.find(command, "allycursorplayername") == 1  and  string.len(command) == 20) then showPlayerName = not showPlayerName end
+
+    if (string.find(command, "+allycursorspecfontsize") == 1) then fontSizeSpec = fontSizeSpec + 1.5 end
+    if (string.find(command, "-allycursorspecfontsize") == 1) then fontSizeSpec = fontSizeSpec - 1.5 end
+
+    if (string.find(command, "+allycursorspecfontopacity") == 1) then fontOpacitySpec = fontOpacitySpec + 0.05 end
+    if (string.find(command, "-allycursorspecfontopacity") == 1) then fontOpacitySpec = fontOpacitySpec - 0.05 end
+
+    if (string.find(command, "+allycursorplayerfontsize") == 1) then fontSizePlayer = fontSizePlayer + 1.5 end
+    if (string.find(command, "-allycursorplayerfontsize") == 1) then fontSizePlayer = fontSizePlayer - 1.5 end
+
+    if (string.find(command, "+allycursorplayerfontopacity") == 1) then fontOpacityPlayer = fontOpacityPlayer + 0.05 end
+    if (string.find(command, "-allycursorplayerfontopacity") == 1) then fontOpacityPlayer = fontOpacityPlayer - 0.05 end
+
+    if fontOpacitySpec > 1 then fontOpacitySpec = 1 end if fontOpacitySpec < 0.15 then fontOpacitySpec = 0.15 end
+    if fontOpacityPlayer > 1 then fontOpacityPlayer = 1 end if fontOpacityPlayer < 0.15 then fontOpacityPlayer = 0.15 end
+    if fontSizeSpec > 60 then fontSizeSpec = 60 end if fontSizeSpec < 10 then fontSizeSpec = 10 end
+    if fontSizePlayer > 60 then fontSizePlayer = 60 end if fontSizePlayer < 10 then fontSizePlayer = 10 end
+    
+	if showPlayerName then
+		usedCursorSize = drawNamesCursorSize
+	end
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 local spGetGroundHeight		= Spring.GetGroundHeight
 local spGetPlayerInfo		= Spring.GetPlayerInfo
@@ -61,21 +99,39 @@ local spGetTeamColor		= Spring.GetTeamColor
 local spIsSphereInView		= Spring.IsSphereInView
 local spGetCameraPosition	= Spring.GetCameraPosition
 
-local alliedCursorsPos		= {}
-local teamColors			= {}
-local notIdle				= {}
+local glCreateList			= gl.CreateList
+local glDeleteList			= gl.DeleteList
+local glCallList			= gl.CallList
+
+local floor					= math.floor
+local min					= math.min
+local GL_QUADS				= GL.QUADS
+local clock					= os.clock
+local Button				= {}
+local Panel					= {}
+local alliedCursorsPos      = {}
+local prevCursorPos			= {}
+local alliedCursorsTime		= {}		-- for API purpose
 local usedCursorSize		= cursorSize
-local color,time,wx,wz,lastUpdateDiff,scale,iscale,fscale,gy --keep memory always allocated for these since they are referenced so frequently
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
+local prevMouseX,prevMouseY = 0
+local allycursorDrawList	= {}
 
 function widget:Initialize()
 	widgetHandler:RegisterGlobal('MouseCursorEvent', MouseCursorEvent)
 	
-	if drawNames then
+	if showPlayerName then
 		usedCursorSize = drawNamesCursorSize
+	end
+	
+	WG['allycursor_api'] = {}
+	WG['allycursor_api'].GetCursorTimes = function()
+		return alliedCursorsTime
+	end
+	
+	local now = clock()
+	local pList = Spring.GetPlayerList()
+	for _,playerID in ipairs(pList) do
+		alliedCursorsTime[playerID] = now
 	end
 end
 
@@ -84,6 +140,32 @@ function widget:Shutdown()
 	widgetHandler:DeregisterGlobal('MouseCursorEvent')
 end
 
+
+function widget:GetConfigData(data)
+    savedTable = {}
+    savedTable.showSpectatorName  = showSpectatorName
+    savedTable.showPlayerName     = showPlayerName
+    savedTable.useTeamColor       = useTeamColor
+    savedTable.fontSizePlayer     = fontSizePlayer
+    savedTable.fontOpacityPlayer  = fontOpacityPlayer
+    savedTable.fontSizeSpec       = fontSizeSpec
+    savedTable.fontOpacitySpec    = fontOpacitySpec
+    return savedTable
+end
+
+function widget:SetConfigData(data)
+    if data.showSpectatorName ~= nil   then  showSpectatorName   = data.showSpectatorName end
+    if data.showPlayerName ~= nil      then  showPlayerName      = data.showPlayerName end
+    if data.useTeamColor ~= nil        then  useTeamColor        = data.useTeamColor end
+    fontSizePlayer        = data.fontSizePlayer     or fontSizePlayer
+    fontOpacityPlayer     = data.fontOpacityPlayer  or fontOpacityPlayer
+    fontSizeSpec          = data.fontSizeSpec       or fontSizeSpec
+    fontOpacitySpec       = data.fontOpacitySpec    or fontOpacitySpec
+    
+	if showPlayerName then
+		usedCursorSize = drawNamesCursorSize
+	end
+end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
@@ -117,8 +199,8 @@ function MouseCursorEvent(playerID,x,z,click)
 			acp[i*2+1] = playerPosList[i+1].x
 			acp[i*2+2] = playerPosList[i+1].z
 		end
-		
-		acp[(numMousePos+1)*2+1] = os.clock()
+
+		acp[(numMousePos+1)*2+1] = clock()
 		acp[(numMousePos+1)*2+2] = playerPosList[#playerPosList].click
 	else
 		local acp = {}
@@ -132,15 +214,27 @@ function MouseCursorEvent(playerID,x,z,click)
 		acp[(numMousePos)*2+1]   = playerPosList[(numMousePos-2)*2+1].x
 		acp[(numMousePos)*2+2]   = playerPosList[(numMousePos-2)*2+1].z
 
-		acp[(numMousePos+1)*2+1] = os.clock()
+		acp[(numMousePos+1)*2+1] = clock()
 		acp[(numMousePos+1)*2+2] = playerPosList[#playerPosList].click
 		_,_,_,acp[(numMousePos+1)*2+3] = spGetPlayerInfo(playerID)
 	end
+	
+	
+	-- check if there has been changes
+	wx,wz = alliedCursorsPos[playerID][1*2+1],alliedCursorsPos[playerID][1*2+1]
+	
+	if prevCursorPos[playerID] == nil or wx ~= prevCursorPos[playerID].wx or wz ~= prevCursorPos[playerID].wz then
+		alliedCursorsTime[playerID] = clock()
+	end
+	
+	if prevCursorPos[playerID] == nil then
+		prevCursorPos[playerID] = {}
+	end
+	prevCursorPos[playerID].wx = wx
+	prevCursorPos[playerID].wz = wz
 end
 
 --------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
 
 local function DrawGroundquad(wx,gy,wz,size)
 	gl.TexCoord(0,0)
@@ -152,6 +246,11 @@ local function DrawGroundquad(wx,gy,wz,size)
 	gl.TexCoord(1,0)
 	gl.Vertex(wx+size,gy+size,wz-size)
 end
+
+local teamColors = {}
+local color
+local time,wx,wz,lastUpdateDiff,scale,iscale,fscale,gy --keep memory always allocated for these since they are referenced so frequently
+local notIdle = {}
 
 local function SetTeamColor(teamID,playerID,a)
 	color = teamColors[playerID]
@@ -186,13 +285,67 @@ function widget:PlayerChanged(playerID)
 	teamColors[playerID] = color
 end
 
+	
+function createCursorDrawList(playerID, opacityMultiplier)
 
+	allycursorDrawList[playerID][opacityMultiplier] = glCreateList( function()
+			
+		local name,_,spec,teamID = spGetPlayerInfo(playerID)
+		local r, g, b = spGetTeamColor(teamID)
+		local wx,gy,wz = 0,0,0
+		local quadSize = usedCursorSize
+		if spec then
+			quadSize = usedCursorSize * 0.77
+		end
+		
+		SetTeamColor(teamID,playerID,1)
+		
+		if not spec  and not showPlayerName    or    spec  and  not showSpectatorName  then
+			--draw a cursor
+			gl.Texture(allyCursor)
+			gl.BeginEnd(GL.QUADS,DrawGroundquad,wx,gy,wz,quadSize)
+			gl.Texture(false)
+		else
+			if not spec then
+				--draw a cursor
+				gl.Texture(allyCursor)
+				gl.BeginEnd(GL.QUADS,DrawGroundquad,wx,gy,wz,quadSize)
+				gl.Texture(false)
+			end
+			
+			--draw the nickname
+			gl.PushMatrix()
+			gl.Translate(wx, gy, wz)
+			gl.Billboard()
+			
+			if spec then
+				gl.Color(1,1,1,fontOpacitySpec*opacityMultiplier)
+				gl.Text(name, 0, 0, fontSizeSpec, "cn")
+			else
+				local verticalOffset = usedCursorSize + 8
+				local horizontalOffset = usedCursorSize + 1
+				-- text shadow
+				gl.Color(0,0,0,fontOpacityPlayer*0.62*opacityMultiplier)
+				gl.Text(name, horizontalOffset-(fontSizePlayer/50), verticalOffset-(fontSizePlayer/42), fontSizePlayer, "n")
+				gl.Text(name, horizontalOffset+(fontSizePlayer/50), verticalOffset-(fontSizePlayer/42), fontSizePlayer, "n")
+				-- text
+				gl.Color(r,g,b,fontOpacityPlayer*opacityMultiplier)
+				gl.Text(name, horizontalOffset, verticalOffset, fontSizePlayer, "n")
+			end
+			gl.PopMatrix()
+		end
+					
+	end)
+end
+
+local xDifference, yDifference, zDifference, camDistance, glScale
 
 function widget:DrawWorldPreUnit()
+    if Spring.IsGUIHidden() then return end
 	gl.DepthTest(GL.ALWAYS)
-	gl.Texture(LUAUI_DIRNAME..'Images/allycursor.png')
+    gl.Blending(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA)
 	gl.PolygonOffset(-7,-10)
-	time = os.clock()
+	time = clock()
 	
 	local camX,camY,camZ = spGetCameraPosition()
 	
@@ -204,72 +357,51 @@ function widget:DrawWorldPreUnit()
 		
 		if (lastUpdatedDiff<sendPacketEvery) then
 			scale  = (1-(lastUpdatedDiff/sendPacketEvery))*numMousePos
-			iscale = math.min(math.floor(scale),numMousePos-1)
+			iscale = min(floor(scale),numMousePos-1)
 			fscale = scale-iscale
 			wx = CubicInterpolate2(data[iscale*2+1],data[(iscale+1)*2+1],fscale)
 			wz = CubicInterpolate2(data[iscale*2+2],data[(iscale+1)*2+2],fscale)
 		end
 		
 		if notIdle[playerID] then
-			--draw a cursor
 			local gy = spGetGroundHeight(wx,wz)
 			if (spIsSphereInView(wx,gy,wz,usedCursorSize)) then
-				SetTeamColor(teamID,playerID,1)
-				if not spec     or    not drawNames and spec and not hideSpecs then
-					local quadSize = usedCursorSize
-					if spec then
-						quadSize = usedCursorSize * 0.77
-					end
-					if drawNames then
-						gl.Texture(LUAUI_DIRNAME..'Images/allycursor.png')
-						gl.BeginEnd(GL.QUADS,DrawGroundquad,wx,gy,wz,quadSize)
-						gl.Texture(false)
-					else
-						gl.BeginEnd(GL.QUADS,DrawGroundquad,wx,gy,wz,quadSize)
+			
+				--calc scale
+				xDifference = camX - wx
+				yDifference = camY - gy
+				zDifference = camZ - wz
+				camDistance = math.sqrt(xDifference*xDifference + yDifference*yDifference + zDifference*zDifference)
+				glScale = 0.83 + camDistance / 5000
+					
+				-- calc opacity
+				local opacityMultiplier = 1
+				if drawNamesFade and camDistance > NameFadeStartDistance then 
+					opacityMultiplier = (1 - (camDistance-NameFadeStartDistance) / (NameFadeEndDistance-NameFadeStartDistance))
+					if opacityMultiplier > 1 then
+						opacityMultiplier = 1
 					end
 				end
 				
-				--draw nickname
-				if drawNames then
-					gl.PushMatrix()
-					gl.Translate(wx, gy, wz)
-					gl.Billboard()
+				opacityMultiplier = math.floor(opacityMultiplier * dlistAmount)/dlistAmount
+				
+				if opacityMultiplier > 0.11 then
+					if allycursorDrawList[playerID] == nil then 
+						allycursorDrawList[playerID] = {}
+					end
+					if allycursorDrawList[playerID][opacityMultiplier] == nil then
+						createCursorDrawList(playerID,opacityMultiplier)
+					end
 					
-					local opacityMultiplier = 1
-					if drawNamesFade and camZ   or   drawNamesScaling and camZ then
-						local xDifference = camX - wx
-						local yDifference = camY - gy
-						local zDifference = camZ - wz
-						local camDistance = math.sqrt(xDifference*xDifference + yDifference*yDifference + zDifference*zDifference)
-						
-						if drawNamesScaling then
-							local glScale = 0.83 + camDistance / 3000
-							gl.Scale(glScale,glScale,glScale)
-						end
-						if drawNamesFade and camDistance > NameFadeStartDistance then 
-							local fadeDistance = NameFadeEndDistance - NameFadeStartDistance
-							opacityMultiplier = opacityMultiplier - ((camDistance - NameFadeStartDistance) / fadeDistance)
-						end
+					gl.Translate(wx, gy, wz)
+					if drawNamesScaling then
+						--gl.Scale(glScale,0,glScale)
 					end
-					if opacityMultiplier then
-						if spec then
-							if not hideSpecs then
-								gl.Color(1,1,1,0.55*opacityMultiplier)
-								gl.Text(name, 0, 0, fontSizeSpec, "cn")
-							end
-						else
-							local verticalOffset = usedCursorSize + 8
-							local horizontalOffset = usedCursorSize + 1
-							-- text shadow
-							gl.Color(0,0,0,0.62*opacityMultiplier)
-							gl.Text(name, horizontalOffset-(fontSizePlayer/45), verticalOffset-(fontSizePlayer/38), fontSizePlayer, "n")
-							gl.Text(name, horizontalOffset+(fontSizePlayer/45), verticalOffset-(fontSizePlayer/38), fontSizePlayer, "n")
-							-- text
-							gl.Color(teamColors[playerID][1],teamColors[playerID][2],teamColors[playerID][3],0.75*opacityMultiplier)
-							gl.Text(name, horizontalOffset, verticalOffset, fontSizePlayer, "n")
-						end
+					glCallList(allycursorDrawList[playerID][opacityMultiplier])
+					if drawNamesScaling then
+						--gl.Scale(-glScale,0,-glScale)
 					end
-					gl.PopMatrix()
+					gl.Translate(-wx, -gy, -wz)
 				end
 			end
 		else
@@ -282,47 +414,12 @@ function widget:DrawWorldPreUnit()
 				wx_old = wx
 				wz_old = wz
 			end
+			
 		end
 	end
-
+    --gl.EndText
 	gl.PolygonOffset(false)
 	gl.Texture(false)
 	gl.DepthTest(false)
 end
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
--- (console) commands
-function widget:TextCommand(command)
-    if (string.find(command, "allycursors_names") == 1	and  string.len(command) == 17) then drawNames = not drawNames end
-    if (string.find(command, "allycursors_specs") == 1  and  string.len(command) == 17) then hideSpecs = not hideSpecs end
-    if (string.find(command, "allycursors_scaling") == 1  and  string.len(command) == 19) then drawNamesScaling = not drawNamesScaling end
-    if (string.find(command, "allycursors_namesfade") == 1  and  string.len(command) == 21) then drawNamesFade = not drawNamesFade end
-    
-	if drawNames then
-		usedCursorSize = drawNamesCursorSize
-	end
-end
-
--- save data
-function widget:GetConfigData(data)
-    savedTable = {}
-    savedTable.drawNames		= drawNames
-    savedTable.hideSpecs		= hideSpecs
-    savedTable.drawNamesScaling	= drawNamesScaling
-    savedTable.drawNamesFade	= drawNamesFade
-    return savedTable
-end
-
--- restore data
-function widget:SetConfigData(data)
-    if data.drawNames ~= nil			then  drawNames			= data.drawNames end
-    if data.hideSpecs ~= nil			then  hideSpecs			= data.hideSpecs end
-    if data.drawNamesScaling ~= nil		then  drawNamesScaling	= data.drawNamesScaling end
-    if data.drawNamesFade ~= nil		then  drawNamesFade		= data.drawNamesFade end
-	
-	if drawNames then
-		usedCursorSize = drawNamesCursorSize
-	end
-end
