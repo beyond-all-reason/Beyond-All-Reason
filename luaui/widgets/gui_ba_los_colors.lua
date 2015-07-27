fba162dbf8cd936991f6d1934db669473a76c978
Decay <Decay@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-07-27 00:01:20 +0000

    Changed colors for "radar" mode + remember ; mode + fix keybinding for non english layouts

--- luaui/widgets/gui_ba_los_colors.lua
+++ luaui/widgets/gui_ba_los_colors.lua
@@ -2,11 +2,11 @@ function widget:GetInfo()
     return {
         name      = "BA LOS colors",
         desc      = "custom colors for LOS",
-        author    = "[teh]decay (thx to Floris, gluon, hokomoko)",
+        author    = "[teh]decay (thx to Floris, gluon, hokomoko, [teh]Teddy)",
         date      = "23 jul 2015",
         license   = "public domain",
         layer     = 0,
-        version   = 1,
+        version   = 2,
         enabled   = true  -- loaded by default
     }
 end
@@ -14,49 +14,43 @@ end
 -- project page: this widget is included in BA repo
 
 --Changelog
--- v2
+-- v2 Changed colors + remember ; mode + fix keybindings for non english layouts
 
---TODO: add ability to remember ";" mode
-
+local losWithRadarEnabled = false;
 
 local losColorsWithRadars = {
-    fog = {0.1,0.1,0.1},
-    los = {0.3,0.3,0.3},
-    radar = {0.17,0.17,0.17},
-    jam = {0.12,0,0},
-    radar2 = {0.17,0.17,0.17},
+    fog =    {0.15, 0.15, 0.15},
+    los =    {0.22, 0.14, 0.30},
+    radar2 = {0.08, 0.16, 0.00},
+    jam =    {0.20, 0.00, 0.00},
+    radar =  {0.00, 0.00, 0.00},
 }
 
 local losColorsWithoutRadars = {
-    fog = {0.3,0.3,0.3},
-    los = {0.25,0.25,0.25},
-    radar = {0,0,0},
-    jam = {0.12,0,0},
-    radar2 = {0,0,0},
+    fog =    {0.30, 0.30, 0.30},
+    los =    {0.25, 0.25, 0.25},
+    radar =  {0.00, 0.00, 0.00},
+    jam =    {0.12, 0.00, 0.00},
+    radar2 = {0.00, 0.00, 0.00},
 }
 
-function widget:Initialize()
-    setLosWithoutRadars()
-
-    Spring.SendCommands('unbindkeyset ;')
-    Spring.SendCommands('bind ; loswithradars')
-
-    widgetHandler:AddAction("loswithradars", setLosWithRadars)
-    widgetHandler:AddAction("loswithoutradars", setLosWithoutRadars)
-end
+local spSendCommands = Spring.SendCommands
+local spSetLosViewColors = Spring.SetLosViewColors
 
 function setLosWithRadars()
-    Spring.SetLosViewColors(losColorsWithRadars.fog, losColorsWithRadars.los, losColorsWithRadars.radar,
+    losWithRadarEnabled = true
+    spSetLosViewColors(losColorsWithRadars.fog, losColorsWithRadars.los, losColorsWithRadars.radar,
         losColorsWithRadars.jam, losColorsWithRadars.radar2)
-    Spring.SendCommands('unbindkeyset ;')
-    Spring.SendCommands('bind ; loswithoutradars')
+    spSendCommands('unbindkeyset Any+;')
+    spSendCommands('bind Any+; loswithoutradars')
 end
 
 function setLosWithoutRadars()
-    Spring.SetLosViewColors(losColorsWithoutRadars.fog, losColorsWithoutRadars.los, losColorsWithoutRadars.radar,
+    losWithRadarEnabled = false
+    spSetLosViewColors(losColorsWithoutRadars.fog, losColorsWithoutRadars.los, losColorsWithoutRadars.radar,
         losColorsWithoutRadars.jam, losColorsWithoutRadars.radar2)
-    Spring.SendCommands('unbindkeyset ;')
-    Spring.SendCommands('bind ; loswithradars')
+    spSendCommands('unbindkeyset Any+;')
+    spSendCommands('bind Any+; loswithradars')
 end
 
 function widget:PlayerChanged(playerID)
@@ -64,12 +58,37 @@ function widget:PlayerChanged(playerID)
     local _, _, spec, _, _, _, _, _ = Spring.GetPlayerInfo(playerID)
 
     if spec then
-        setLosWithoutRadars()
+        spSetLosViewColors(losColorsWithoutRadars.fog, losColorsWithoutRadars.los, losColorsWithoutRadars.radar,
+            losColorsWithoutRadars.jam, losColorsWithoutRadars.radar2)
+        spSendCommands('unbindkeyset Any+;')
+        spSendCommands('bind Any+; loswithradars')
     end
     return true
 end
 
 function widget:ShutDown()
-    Spring.SendCommands('unbindkeyset ;')
+    spSendCommands('unbindkeyset Any+;')
+end
+
+
+function widget:GetConfigData()
+    return { losWithRadarEnabled = losWithRadarEnabled }
+end
+
+function widget:SetConfigData(data)
+    widgetHandler:AddAction("loswithradars", setLosWithRadars)
+    widgetHandler:AddAction("loswithoutradars", setLosWithoutRadars)
+
+    if data.losWithRadarEnabled ~= nil then
+        losWithRadarEnabled = data.losWithRadarEnabled
+    else
+        losWithRadarEnabled = false
+    end
+
+    if losWithRadarEnabled == true then
+        setLosWithRadars()
+    else
+        setLosWithoutRadars()
+    end
 end
 
