dab2104dba91d6b81bc6c7f695adc4466281dc63
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-09-14 18:39:50 +0000

    anti ranges: upped the circle resolution a bit again

--- luaui/widgets/gui_anti_ranges.lua
+++ luaui/widgets/gui_anti_ranges.lua
@@ -161,11 +161,11 @@ function drawCircle(uID, coverageRange, x, y, z, camX, camY, camZ)
 		if showLineGlow then
 			glLineWidth(10)
 			glColor(circleColor[1],circleColor[2],circleColor[3], .016*lineOpacityMultiplier)
-			glDrawGroundCircle(x, y, z, coverageRange, 96)
+			glDrawGroundCircle(x, y, z, coverageRange, 128)
 		end]]--
 		glColor(circleColor[1],circleColor[2],circleColor[3], .5*lineOpacityMultiplier)
 		glLineWidth(3-lineWidthMinus)
-		glDrawGroundCircle(x, y, z, coverageRange, 96)
+		glDrawGroundCircle(x, y, z, coverageRange, 128)
 	end
 end
 
