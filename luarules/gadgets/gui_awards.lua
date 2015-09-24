368814ac4400137d2ec749606b89819767b726c1
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-09-24 23:29:02 +0000

    fix #953

--- luarules/gadgets/gui_awards.lua
+++ luarules/gadgets/gui_awards.lua
@@ -141,6 +141,7 @@ function gadget:UnitTaken(unitID, unitDefID, teamID, newTeam)
 	if not newTeam then return end 
     if newTeam==gaiaTeamID then return end
 	if not present[newTeam] then return end
+    if not present[teamID] then return end
 	if not unitDefID then return end --should never happen
 
 	local ud = UnitDefs[unitDefID]
