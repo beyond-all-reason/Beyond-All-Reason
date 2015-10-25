19533964ffa7a2ea6dd0f66fe084c5594d9882fb
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-10-25 14:48:49 +0000

    added gadget: unit_resurrected.lua:  marks resurrected units as resurrected.

---
+++ luarules/gadgets/unit_resurrected.lua
@@ -0,0 +1,24 @@
+function gadget:GetInfo()
+   return {
+      name      = "resurrected param",
+      desc      = "marks resurrected units as resurrected.",
+      author    = "Floris",
+      date      = "25 oct 2015",
+      license   = "GNU GPL, v2 or later",
+      layer     = 5,
+      enabled   = true
+   }
+end
+
+if (gadgetHandler:IsSyncedCode()) then
+	
+	-- detect resurrected units here
+	function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+		if builderID  and  UnitDefs[Spring.GetUnitDefID(builderID)].canResurrect then
+			
+			Spring.SetUnitRulesParam(unitID, "resurrected", 1)
+			
+		end
+	end
+	
+end
