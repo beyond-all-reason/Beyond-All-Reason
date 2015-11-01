1e57f330531fa8331ff5d8c5356f1ec056eaa408
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-11-01 01:09:04 +0000

    air_transports gadget optimized

--- luarules/gadgets/air_transports.lua
+++ luarules/gadgets/air_transports.lua
@@ -14,6 +14,7 @@ local TRANSPORTED_MASS_SPEED_PENALTY = 1 -- higher makes unit slower
 local FRAMES_PER_SECOND = 30
 
 local airTransports = {}
+local airTransportMaxSpeeds = {}
 local unloadedUnits = {}
 	
 --------------------------------------------------------------------------------
@@ -23,14 +24,39 @@ if (gadgetHandler:IsSyncedCode()) then
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
--- add transports to table when created
+local massUsageFraction = 0
+local allowedSpeed = 0
+local currentMassUsage = 0
+-- update allowed speed for transport
+function updateAllowedSpeed(transportId, transportUnitDef)
+	
+	-- get sum of mass and size for all transported units                                
+	currentMassUsage = 0
+	for _,tUnitId in pairs(Spring.GetUnitIsTransporting(transportId)) do
+		local tUd = UnitDefs[Spring.GetUnitDefID(tUnitId)]
+		-- currentCapacityUsage = currentCapacityUsage + tUd.xsize 
+		currentMassUsage = currentMassUsage + tUd.mass
+	end
+	massUsageFraction = (currentMassUsage / transportUnitDef.transportMass)
+	allowedSpeed = transportUnitDef.speed * (1 - massUsageFraction * TRANSPORTED_MASS_SPEED_PENALTY) / FRAMES_PER_SECOND 
+	--Spring.Echo("unit "..transportUnitDef.name.." is air transport at  "..(massUsageFraction*100).."%".." load, curSpeed="..vw.." allowedSpeed="..allowedSpeed)
+
+	airTransportMaxSpeeds[transportId] = allowedSpeed
+end
+
+
+-- add transports to table when they load a unit
 function gadget:UnitLoaded(unitId, unitDefId, unitTeam, transportId, transportTeam)
 	local ud = UnitDefs[Spring.GetUnitDefID(transportId)]
 	if ud.canFly and not airTransports[transportId] then
 		airTransports[transportId] = ud
+
+		-- update allowed speed
+		updateAllowedSpeed(transportId, ud)
 	end
 end
--- remove transports from table when destroyed
+
+-- cleanup transports and unloaded unit tables when destroyed
 function gadget:UnitDestroyed(unitId, unitDefId, teamId, attackerId, attackerDefId, attackerTeamId)
 	if airTransports[unitId] then
 		airTransports[unitId] = nil
@@ -38,14 +64,17 @@ function gadget:UnitDestroyed(unitId, unitDefId, teamId, attackerId, attackerDef
 	if unloadedUnits[unitId] then
 		unloadedUnits[unitId] = nil
 	end
+	if airTransportMaxSpeeds[unitId] then
+		airTransportMaxSpeeds[unitId] = nil
+	end
 end
 
 -- every frame, adjust speed of air transports according to transported mass, if any
 function gadget:GameFrame(n)
     
-    -- prevent unloaded units from sliding across the map
+	-- prevent unloaded units from sliding across the map
 	-- TODO remove when fixed in the engine
-    for unitId,data in pairs(unloadedUnits) do
+	for unitId,data in pairs(unloadedUnits) do
     	if (n > data.frame + 10 ) then
 			-- reset position
 			Spring.SetUnitPhysics(unitId,data.px,data.py,data.pz,0,0,0,0,0,0,0,0,0)
@@ -55,48 +84,41 @@ function gadget:GameFrame(n)
 			-- remove from table
 			unloadedUnits[unitId] = nil
 		end
-    end
+	end
     
-    -- for each air transport
+	-- for each air transport with units loaded, reduce speed if currently greater than allowed
+	local factor = 1
+	local vx,vy,vz,vw = 0
+	local alSpeed = 0
 	for unitId,ud in pairs(airTransports) do
-		
-		-- local currentCapacityUsage = 0 
-		local currentMassUsage = 0
-
-		-- get sum of mass and size for all transported units                                
-        for _,tUnitId in pairs(Spring.GetUnitIsTransporting(unitId)) do
-			local tUd = UnitDefs[Spring.GetUnitDefID(tUnitId)]
-			-- currentCapacityUsage = currentCapacityUsage + tUd.xsize 
-			currentMassUsage = currentMassUsage + tUd.mass
-        end
-		
-		-- compute max allowed speed
-		local vx,vy,vz,vw = Spring.GetUnitVelocity(unitId)
-		local massUsageFraction = (currentMassUsage / ud.transportMass)
-		local allowedSpeed = ud.speed * (1 - massUsageFraction * TRANSPORTED_MASS_SPEED_PENALTY) / FRAMES_PER_SECOND 
-
-		--Spring.Echo("unit "..ud.name.." is air transport at  "..(massUsageFraction*100).."%".." load, curSpeed="..vw.." allowedSpeed="..allowedSpeed)
-
-		-- reduce speed if currently greater than allowed
-		if (vw > allowedSpeed) then
-			local factor = allowedSpeed / vw
+		vx,vy,vz,vw = Spring.GetUnitVelocity(unitId)
+		alSpeed = airTransportMaxSpeeds[unitId]
+		if (alSpeed and vw > alSpeed) then
+			factor = alSpeed / vw
 			Spring.SetUnitVelocity(unitId,vx * factor,vy * factor,vz * factor)
 		end
 	end
 end
 
 
--- prevent unloaded units from sliding across the map
--- TODO remove when fixed in the engine
 function gadget:UnitUnloaded(unitId, unitDefId, teamId, transportId)
+
+	-- prevent unloaded units from sliding across the map
+	-- TODO remove when fixed in the engine
 	if not unloadedUnits[unitId] then
 		local px,py,pz = Spring.GetUnitPosition(unitId,false,false)
 		local dx,dy,dz = Spring.GetUnitDirection(unitId)
 		local frame = Spring.GetGameFrame()
 		unloadedUnits[unitId] = {["px"]=px,["py"]=py,["pz"]=pz,["dx"]=dx,["dy"]=dy,["dz"]=dz,["frame"]=frame}
 	end
-	if airTransports[transportId] and Spring.GetUnitIsTransporting(transportId)[1]) ~= nil then
+
+	if airTransports[transportId] and not Spring.GetUnitIsTransporting(transportId)[1] then
+		-- transport is empty, cleanup tables
 		airTransports[transportId] = nil
+		airTransportMaxSpeeds[transportId] = nil
+	else
+		-- update allowed speed
+		updateAllowedSpeed(transportId, airTransports[transportId])
 	end
 end
 
