763c1ca873a16712e9c33e9a70c19a12f78119dc
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-11-14 14:36:17 +0000

    minimap size enlarged a tiny tiny bit

--- luaui/widgets/gui_red_minimap.lua
+++ luaui/widgets/gui_red_minimap.lua
@@ -11,7 +11,7 @@ function widget:GetInfo()
 	handler   = true, --can use widgetHandler:x()
 	}
 end
-local rescalevalue = 1.25
+local rescalevalue = 1.26
 local buttonScale = 0.5
 local NeededFrameworkVersion = 8
 local CanvasX,CanvasY = 1272/rescalevalue,734/rescalevalue --resolution in which the widget was made (for 1:1 size)
