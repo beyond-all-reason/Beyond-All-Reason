032eaf043ea9aff275afb0df0bfa677517e14802
BrainDamage <BrainDamage@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-11-14 20:36:49 +0000

    split ally sel units into gadget to broadcast and widget to view as requested

---
+++ luarules/gadgets/cmd_selected_units.lua
@@ -0,0 +1,214 @@
+
+function gadget:GetInfo()
+	return {
+		name = "Ally Selected Units",
+		desc = "sends your selected units to others",
+		author    = "very_bad_soldier",
+		date      = "August 1, 2008",
+		license   = "GNU GPL v2",
+		layer     = 0,
+		enabled = true
+	}
+end
+
+local unitLimitPerFrame = 300 -- controls how many units will be send per frame
+local fullSelectionUpdateInt = 0 -- refresh full selection info once in n seconds, 0 = disabled
+local minZlibSize = 130  --minimum size threshold of msg to use zlib (msg smaller than this will not be compressed before sending)
+
+local HEADER_SEL_UNCOMPRESSED = "cosu"
+local HEADER_SEL_COMPRESSED = "cosc"
+local HEADER_LENGHT = string.len(HEADER_SEL_UNCOMPRESSED)
+
+
+
+if gadgetHandler:IsSyncedCode() then
+	function gadget:RecvLuaMsg(inMsg, playerID)	
+		if inMsg:sub(1,HEADER_LENGHT)==HEADER_SEL_UNCOMPRESSED or inMsg:sub(1,HEADER_LENGHT)==HEADER_SEL_COMPRESSED then
+			SendToUnsynced("selectionUpdate",playerID,inMsg:sub(5),inMsg:sub(4,4) == "c")
+			return true
+		end
+	end
+else
+	local floor = math.floor
+	local ZlibCompress = Spring.ZlibCompress
+	local ZlibDeCompress = Spring.ZlibDeCompress
+	local SendLuaRulesMsg = Spring.SendLuaRulesMsg
+	local GetSelectedUnits = Spring.GetSelectedUnits
+	local IsUnitSelected = Spring.IsUnitSelected
+	local GetSpectatingState = Spring.GetSpectatingState
+	local GetLastUpdateSeconds = Spring.GetLastUpdateSeconds
+	local GetPlayerInfo = Spring.GetPlayerInfo
+	local PackU16 = VFS.PackU16
+	local UnpackU16 = VFS.UnpackU16
+
+	local myPlayerID = Spring.GetMyPlayerID()
+
+	local time = 0
+	local timeSeconds = 0
+	local myLastSelectedUnits = {}
+
+
+	function gadget:Initialize()
+		gadgetHandler:AddSyncAction("selectionUpdate", handleSelectionUpdateEvent)
+	end
+	
+	function gadget:Shutdown()
+		gadgetHandler:RemoveSyncAction("selectionUpdate")
+	end
+
+	function handleSelectionUpdateEvent(_,playerID,msg,compressed)
+		local spec, fullView = GetSpectatingState()
+		if not spec or not fullView then
+			local _,_,_,_,myAllyTeamID = GetPlayerInfo(myPlayerID)
+			local _,_,targetSpec,_,allyTeamID = GetPlayerInfo(playerID)
+			if targetSpec or allyTeamID ~= myAllyTeamID then
+				return
+			end
+		end
+			
+		if compressed then		--we have a compressed msg here
+			msg = ZlibDecompress( msg )
+		end
+		
+		local counts = UnpackU16( msg, 1, 2 )
+		if counts[1] == counts[2] and counts[1] == 0xffff then
+			--clear all
+			if Script.LuaUI.selectedUnitsClear then
+				Script.LuaUI.selectedUnitsClear(playerID)
+			end
+		else
+			local addCount = counts[1]
+			local removeCount = counts[2]
+
+			if removeCount > 0 and Script.LuaUI.selectedUnitsRemove then
+				local remUnits = UnpackU16( msg, 5 + addCount * 2, removeCount )
+				for i=1,removeCount do
+					Script.LuaUI.selectedUnitsRemove(playerID,remUnits[i])
+				end
+			end
+
+			if addCount > 0 and Script.LuaUI.selectedUnitsAdd then
+				local addUnits = UnpackU16( msg, 5, addCount )
+				for i=1,addCount do
+					Script.LuaUI.selectedUnitsAdd(playerID,addUnits[i])
+				end
+			end
+		end
+	end
+
+	function gadget:CommandsChanged( id, params, options )
+		sendSelectedUnits()
+	end
+
+	function gadget:UnitDestroyed(unitID, attacker )
+		myLastSelectedUnits[ unitID ] = nil
+	end
+
+	function gadget:Update()
+		local deltaTime = GetLastUpdateSeconds()
+		if time+deltaTime == time then
+			time = 0 --prevent floating point errors
+		end
+		time = time + deltaTime
+
+		if timeSeconds == floor(time) then
+			return --only run on whole seconds
+		end
+
+		timeSeconds = floor(time)
+		
+		if fullSelectionUpdateInt ~= 0 and timeSeconds%fullSelectionUpdateInt == 0 then
+			--its time for a full update
+			sendUnitsMsg(PackU16(0xffff) .. PackU16(0xffff))
+			myLastSelectedUnits = {}
+		end
+
+		sendSelectedUnits()
+	end
+
+	--all values are 16bit
+	--FORMAT: uncompressed msg "cosu[addCount][removeCount]([unitIdToAdd]*)([unitIdToRemove]*)"
+	--FORMAT: compressed msg "cosc{[addCount][removeCount]([unitIdToAdd]*)([unitIdToRemove]*)}"  the part in curly braces has to be zlib compressed 
+	--FORMAT clear all: "cosu[0xffffff][0xffffff]"  --magic value. impossible to have as normal message
+
+	function sendFullRefresh()
+
+	end
+
+	function sendUnitsMsg( msg )
+		local finalMsg = msg
+		local header = HEADER_SEL_UNCOMPRESSED
+		if ZlibCompress and msg:len() >= minZlibSize then
+			finalMsg = ZlibCompress( finalMsg ) 
+			header = HEADER_SEL_COMPRESSED
+		end
+		
+		SendLuaRulesMsg( header .. finalMsg)
+	end
+
+	function sendSelectedUnits()
+		local units = GetSelectedUnits()
+		
+		local partAdd = ""
+		local addCount = 0
+		for i, unitId in pairs(units) do
+			--check if unit is new this time
+			if not myLastSelectedUnits[unitId] then
+				partAdd = partAdd .. PackU16(unitId)
+				myLastSelectedUnits[unitId] = true
+				addCount = addCount + 1
+				
+				if addCount > unitLimitPerFrame then
+					break
+				end
+			end
+		end
+
+		local remTab = {} --these units will be removed in the next step
+		local partRemove = ""
+		local remCount = 0
+		for unitId, unit in pairs(myLastSelectedUnits) do
+			--check if unit is still selected
+			if not IsUnitSelected(unitId) then
+				partRemove = partRemove .. PackU16(unitId)
+				remTab[unitId] = true
+				remCount = remCount + 1
+				
+				if (addCount + remCount) > unitLimitPerFrame then
+					break
+				end
+			end
+		end
+		
+		--remove not anymore selected units
+		for unitId, b in pairs(remTab) do
+			myLastSelectedUnits[unitId] = nil
+		end
+		
+		local msg = partAdd .. partRemove
+		if msg:len() > 1 then
+			local msgToSend = ""
+			if #units > 0 and ( addCount + remCount) > #units then
+				--its more efficient to clear all and then start from zero
+				--so: 1. Clear All
+				msgToSend = PackU16(0xffff) .. PackU16(0xffff)
+				sendUnitsMsg( msgToSend)
+				myLastSelectedUnits = {}
+				--2. do normal send
+				sendSelectedUnits()
+			else
+				if #units == 0 then
+					--send clear all message
+					msgToSend = PackU16(0xffff) .. PackU16(0xffff)
+					myLastSelectedUnits = {}
+				else
+					--send standard message
+					msgToSend = PackU16(addCount) .. PackU16(remCount) .. msg
+					
+				end
+				
+				sendUnitsMsg( msgToSend)
+			end
+		end
+	end
+end
--- luaui/widgets/gui_allySelectedUnits.lua
+++ luaui/widgets/gui_allySelectedUnits.lua
@@ -15,7 +15,6 @@ function widget:GetInfo()
 end
 
 --callin driven
---send 300 units per frame, if more, send next frame
 --"hot" units
 
 local floor                 = math.floor
@@ -26,25 +25,16 @@ local spGetUnitDefID        = Spring.GetUnitDefID
 local spEcho                = Spring.Echo
 local spGetUnitPosition     = Spring.GetUnitPosition
 local spGetUnitBasePosition = Spring.GetUnitBasePosition
-local spGetGameSeconds      = Spring.GetGameSeconds
 local spGetMyPlayerID       = Spring.GetMyPlayerID
 local spGetPlayerInfo       = Spring.GetPlayerInfo
 local spGetLocalTeamID		= Spring.GetLocalTeamID
-local spSendLuaUIMsg    	= Spring.SendLuaUIMsg
-local spIsUnitSelected 		= Spring.IsUnitSelected
-local spGetSelectedUnits	= Spring.GetSelectedUnits
 local spGetUnitDefDimensions = Spring.GetUnitDefDimensions
 local spSelectUnitMap		= Spring.SelectUnitMap
-local spArePlayersAllied 	= Spring.ArePlayersAllied
 local spGetTeamColor 		= Spring.GetTeamColor
 local spGetGroundHeight 	= Spring.GetGroundHeight
 local spIsSphereInView  	= Spring.IsSphereInView
-
-local spZlibCompress        = Spring.ZlibCompress
-local spZlibDecompress      = Spring.ZlibDecompress
-
-local vfsPackU16			= VFS.PackU16
-local vfsUnpackU16			= VFS.UnpackU16
+local spGetSpectatingState	= Spring.GetSpectatingState
+local spGetGameSeconds		= Spring.GetGameSeconds
 
 local glDrawGroundCircle 	= gl.DrawGroundCircle
 local glColor               = gl.Color
@@ -65,15 +55,12 @@ local GL_LINE_LOOP			= GL.LINE_LOOP
 
 ----------------------------------------------------------------
 
-local debug = false
-local unitLimitPerFrame = 300 -- controls how many units will be send per frame
-local fullSelectionUpdateInt = 0 -- refresh full selection info once in ten seconds, 0 = disabled
+
+
 local hotFadeTime = 10.0
 local lockTeamUnits = false --disallow selection of units selected by teammates
 local showAlly = true 		--also show allies (besides coop)
 local useHotColor = false --use RED for all hot units, if false use playerColor starting with transparency
-local minZlibSize = 130  --minimum size threshold of msg to use zlib (msg smaller than this will not be compressed before sending)
-local updateInt = 0.1  --seconds for the ::update loop -- just controls isSpec check interval
 local circleDivsCoop = 32  --nice circle
 local circleDivsAlly = 5  --aka pentagon
 local selectionDrawScaleFactor = 1.1
@@ -93,28 +80,77 @@ playerColorPool[8] = { 1.0, 0.0, 0.0 }
 --Internals------------------------------------------------------
 local playerColors = {}
 local nextPlayerPoolId = 1
-local myTeamId
-local myPlayerId
-local coopSelectedUnits = {}
-local myLastSelectedUnits = {}
-local lastTime = 0
-local lastFullUpdateTime = 0
-local sendInNextFrame = false
+local myTeamID = Spring.GetMyTeamID()
+local myPlayerID = Spring.GetMyPlayerID()
+local playerSelectedUnits = {}
 local hotUnits = {}
-local useZlib = false
 local circleLinesCoop
 local circleLinesAlly
 ------------------------------------------------------------------
 function widget:Initialize()
-	myPlayerId = spGetMyPlayerID() --spGetLocalTeamID()
-	myTeamId = spGetLocalTeamID()
-	
-	if ( spZlibCompress ~= nil ) then
-		useZlib = true
-	end
-	
 	circleLinesCoop = calcCircleLines(circleDivsCoop)
 	circleLinesAlly = calcCircleLines(circleDivsAlly) 
+
+	for _, playerID in pairs(Spring.GetPlayerList()) do
+		widget:PlayerAdded(playerID)
+	end
+
+	widget:PlayerChanged(myPlayerID)
+
+	widgetHandler:RegisterGlobal('selectedUnitsRemove', selectedUnitsRemove)
+	widgetHandler:RegisterGlobal('selectedUnitsClear', selectedUnitsClear)
+	widgetHandler:RegisterGlobal('selectedUnitsAdd', selectedUnitsAdd)
+end
+
+function widget:Shutdown()
+	widgetHandler:DeregisterGlobal('selectedUnitsRemove')
+	widgetHandler:DeregisterGlobal('selectedUnitsClear')
+	widgetHandler:DeregisterGlobal('selectedUnitsAdd')
+	gl.DeleteList(circleLines)
+end
+
+function widget:PlayerAdded(playerID)
+	local playerTeam = select(4,spGetPlayerInfo(playerID))
+	if not playerSelectedUnits[ playerID ] then
+		playerSelectedUnits[ playerID ] = {
+			["units"]={},
+			["coop"]=(playerTeam == myTeamID),
+			["todraw"] = DoDrawPlayer(playerID),
+		}
+	end
+	--grab color from color pool for new teammate
+	--no color yet
+	getPlayerColour(playerID,playerTeam)
+end
+
+function getPlayerColour(playerID,teamID)
+	if playerSelectedUnits[ playerID ]["coop"] then 
+		if not playerColorPool[ nextPlayerPoolId ] then
+			playerColors[ playerID ] = playerColorPool[ 1 ]  --we have only 8 colors, take color 1 as default
+		else
+			playerColors[ playerID ] = playerColorPool[ nextPlayerPoolId ]
+		end
+		nextPlayerPoolId = nextPlayerPoolId + 1
+	else
+		playerColors[ playerID ] = { spGetTeamColor( teamID ) }--he is only ally, use his color
+	end
+end
+
+function widget:PlayerChanged(playerID)
+	if spGetSpectatingState() then
+		widgetHandler:RemoveWidget()
+		return
+	end
+	myTeamID = spGetLocalTeamID()
+	local playerTeam = select(4,spGetPlayerInfo(playerID))
+	local oldCoopStatus = playerSelectedUnits[ playerID ]["coop"]
+	playerSelectedUnits[ playerID ]["coop"] = (teamID == myTeamID)
+	playerSelectedUnits[ playerID ]["todraw"] = DoDrawPlayer(playerID)
+	
+	--grab color from color pool for new teammate
+	if oldCoopStatus ~= playerSelectedUnits[ playerID ]["coop"] then
+		getPlayerColour(playerID,playerTeam)
+	end
 end
 
 function calcCircleLines(divs)
@@ -139,271 +175,78 @@ function calcCircleLines(divs)
 	return lines
 end
 
-function widget:Shutdown()
-	gl.DeleteList(circleLines)
-end
 
 function widget:CommandsChanged( id, params, options )
 	if ( lockTeamUnits ) then
 		deselectAllTeamSelected()
 	end
-	
-	sendSelectedUnits()
 end
 
 function widget:UnitDestroyed(unitID, attacker )
 	hotUnits[ unitID ] = nil
-	myLastSelectedUnits[ unitID ] = nil
 	
-	for playerId, selUnits in pairs( coopSelectedUnits ) do
+	for playerID, selUnits in pairs( playerSelectedUnits ) do
 		selUnits["units"][ unitID ] = nil
 	end
 end
 
-function newHotUnit( unitId, coop, playerId )
+function newHotUnit( unitId, coop, playerID )
+	if not playerSelectedUnits[playerID]["todraw"] then
+		return
+	end
 	local timestamp = spGetGameSeconds()
 	local udef = spGetUnitDefID( unitId )
 	if ( udef ~= nil ) then
 		local realDefRadius = GetUnitDefRealRadius( udef )
 		if ( realDefRadius ~= nil ) then
 			local defRadius = selectionDrawScaleFactor * realDefRadius
-			hotUnits[ unitId ] = { ts = timestamp, coop = coop, defRadius = defRadius, playerId = playerId }
+			hotUnits[ unitId ] = { ts = timestamp, coop = coop, defRadius = defRadius, playerID = playerID }
 		end
 	end
 end 
 
-function widget:Update(deltaTime)
-	local time = spGetGameSeconds()
 
-	-- update timers once every <updateInt> seconds
-	if ( ( lastTime + updateInt ) < time ) then	
-		lastTime = time
-		--do update stuff:
-		if ( checkSpecstate() == false ) then
-			return false
-		end
+function selectedUnitsClear(playerID)
+	--make all hot
+	for unitId, defRadius in pairs(playerSelectedUnits[ playerID ]["units"]) do
+		newHotUnit( unitId, playerSelectedUnits[ playerID ]["coop"], playerID )
 	end
-	
-	if ( ( fullSelectionUpdateInt ~= 0 ) and ( lastFullUpdateTime + fullSelectionUpdateInt ) < time ) then
-		--its time for a full update
-		sendFullRefresh()
-		lastFullUpdateTime = time
-	elseif ( sendInNextFrame ) then
-		sendSelectedUnits()
+	--clear all
+	playerSelectedUnits[ playerID ]["units"] = {}
+end
+
+function selectedUnitsAdd(playerID,unitID)
+	--add unit
+	local realDefRadius = GetUnitDefRealRadius( spGetUnitDefID( unitID ) )
+	if realDefRadius then
+		playerSelectedUnits[ playerID ]["units"][ unitID] = realDefRadius * selectionDrawScaleFactor
+		--un-hot it
+		hotUnits[  unitID ] = nil
 	end
 end
 
---all values are 16bit
---FORMAT: uncompressed msg "cosu[addCount][removeCount]([unitIdToAdd]*)([unitIdToRemove]*)"
---FORMAT: compressed msg "cosc{[addCount][removeCount]([unitIdToAdd]*)([unitIdToRemove]*)}"  the part in curly braces has to be zlib compressed 
---FORMAT clear all: "cosu[0xffffff][0xffffff]"  --magic value. impossible to have as normal message
-
-function sendFullRefresh()
-	myLastSelectedUnits = {}
-	sendSelectedUnits()
+function selectedUnitsRemove(playerID,unitID)
+	--remove unit
+	playerSelectedUnits[ playerID ]["units"][unitID] = nil
+	--make it hot
+	newHotUnit( unitID, playerSelectedUnits[ playerID ]["coop"], playerID )
 end
 
-function sendUnitsMsg( msg )
-	local finalMsg = msg
-	local header = "cosu"
-	if ( useZlib == true and msg:len() >= minZlibSize ) then
-		finalMsg = spZlibCompress( finalMsg ) 
-		header = "cosc"
-		
-		printDebug("ZLIBSEND")
-	end
-	
-	spSendLuaUIMsg( header .. finalMsg, "allies")
-end
-
-
-function sendSelectedUnits()
-	local units = spGetSelectedUnits()
-	
-	local partAdd = ""
-	local addCount = 0
-	for i, unitId in ipairs(units) do
-		--check if unit is new this time
-		if ( myLastSelectedUnits[unitId] == nil ) then
-			--printDebug( "adding: " .. unitId )
-			partAdd = partAdd .. vfsPackU16(unitId)
-			myLastSelectedUnits[unitId] = true
-			addCount = addCount + 1
-			
-			if ( addCount > unitLimitPerFrame ) then
-				break;
-			end
-		end
-	end
-
-	local remTab = {} --these units will be removed in the next step
-	local partRemove = ""
-	local remCount = 0
-	for unitId, unit in pairs(myLastSelectedUnits) do
-		--check if unit is still selected
-		if ( spIsUnitSelected(unitId) == false ) then
-			partRemove = partRemove .. vfsPackU16(unitId)
-			remTab[unitId] = true
-			remCount = remCount + 1
-			
-			if ( (addCount + remCount) > unitLimitPerFrame ) then
-				break;
-			end
-		end
-	end
-	
-	--remove not anymore selected units
-	for unitId, b in pairs(remTab) do
-		myLastSelectedUnits[unitId] = nil
-	end
-	
-	sendInNextFrame = false --reset
-	local msg = partAdd .. partRemove
-	if ( msg:len() > 1 ) then
-		local msgToSend = ""
-		if ( #units > 0 ) and ( addCount + remCount > #units ) then
-			printDebug("OptimizedMove" )
-			--its more efficient to clear all and then start from zero
-			--so: 1. Clear All
-			msgToSend = vfsPackU16(0xffff) .. vfsPackU16(0xffff)
-			sendUnitsMsg( msgToSend, "allies")
-			myLastSelectedUnits = {}
-			--2. do normal send
-			sendSelectedUnits()
-		else
-			if ( #units == 0 ) then
-				--send clear all message
-				printDebug("Clear" )
-				msgToSend = vfsPackU16(0xffff) .. vfsPackU16(0xffff)
-				myLastSelectedUnits = {}
-			else
-				--send standard message
-				printDebug("Sending: " .. addCount .. " " .. remCount )
-				msgToSend = vfsPackU16(addCount) .. vfsPackU16(remCount) .. msg
-				
-				if ( addCount + remCount >= unitLimitPerFrame ) then
-					--there is more (very probably)
-					sendInNextFrame = true
-				end
-			end
-			
-			sendUnitsMsg( msgToSend, "allies")
-		end
-	end
-end
-
-function checkSpecstate()
-	local _, _, spec, _, _, _, _, _ = spGetPlayerInfo(myPlayerId)
-	
-	if ( spec == true ) then
-		spEcho("<CoopSelectedUnits> Spectator mode. Widget removed.")
-		widgetHandler:RemoveWidget()
+function DoDrawPlayer(playerID,teamID)	
+	if playerID == myPlayerID then
 		return false
 	end
-end
-
-function tokenizeSelectedUnitsMsg( addCount, removeCount, msg )
-	local addUnits = {}
-	if ( addCount > 0 ) then
-		addUnits = vfsUnpackU16( msg, 5, addCount )
+	if teamID ~= myTeamID and not showAlly then
+		return false
 	end
-	
-	local remUnits = {}
-	if ( removeCount > 0 ) then
-		remUnits = vfsUnpackU16( msg, 5 + addCount * 2, removeCount )
-	end
-	
-	local units = addUnits
-	for i, unitId in ipairs(remUnits) do
-		table.insert( units, -unitId)
-	end
-	
-	return units
-end
-
-
-function widget:RecvLuaMsg(inMsg, playerID)	
-	if ( ( (inMsg:sub(1,4)=="cosu") or (inMsg:sub(1,4)=="cosc") ) ) then
-		if ( ( playerID == myPlayerId ) and ( debug == false ) ) then
-			return false
-		end
-		
-		local _,_,_,teamId = spGetPlayerInfo(playerID)
-		--teamId = 1
-		if ( teamId == myTeamId or ( showAlly and spArePlayersAllied( playerID, myPlayerId ) ) ) then
-			if ( coopSelectedUnits[ playerID ] == nil ) then
-				coopSelectedUnits[ playerID ] = {}
-				coopSelectedUnits[ playerID ]["units"] = {}
-				coopSelectedUnits[ playerID ]["coop"] = true
-				if ( teamId ~= myTeamId ) then
-					coopSelectedUnits[ playerID ]["coop"] = false					
-				end
-			end
-			
-			--grab color from color pool for new teammate
-			if ( playerColors[ playerID ] == nil ) then
-				--no color yet
-				if ( coopSelectedUnits[ playerID ]["coop"] == true ) then 
-					if ( playerColorPool[ nextPlayerPoolId ] == nil ) then
-						playerColors[ playerID ] = playerColorPool[ 1 ]  --we have only 8 colors, take color 1 as default
-					else
-						playerColors[ playerID ] = playerColorPool[ nextPlayerPoolId ]
-					end
-					nextPlayerPoolId = nextPlayerPoolId + 1
-				else
-					local r,g,b,a  = spGetTeamColor( teamId )--he is only ally, use his color
-					playerColors[ playerID ] = { r, g, b, a }
-				end
-			end
-			
-			local msg = inMsg:sub(5)
-			if ( inMsg:sub(4,4) == "c" ) then		--we have a compressed msg here
-				printDebug( "Received compressed msg!")
-				msg = spZlibDecompress( msg )
-			end
-			
-			local counts = vfsUnpackU16( msg, 1, 2 )
-			
-			if ( ( counts[1] == counts[2] ) and ( counts[1] == 0xffff ) ) then
-				printDebug("Recvd Clearall")
-				--make all hot
-				for unitId, defRadius in pairs(coopSelectedUnits[ playerID ]["units"]) do
-					newHotUnit( unitId, coopSelectedUnits[ playerID ]["coop"], playerID )
-				end
-				--clear all
-				coopSelectedUnits[ playerID ]["units"] = {}
-			else
-				selUnits = tokenizeSelectedUnitsMsg( counts[1], counts[2], msg )
-				for i,unitId in ipairs(selUnits) do
-					if ( unitId < 0 ) then
-						--remove unit
-						coopSelectedUnits[ playerID ]["units"][-unitId] = nil
-						--make it hot
-						newHotUnit( -unitId, coopSelectedUnits[ playerID ]["coop"], playerID )
-					else
-						--add unit
-						local realDefRadius = GetUnitDefRealRadius( spGetUnitDefID( unitId ) )
-						if ( realDefRadius ~= nil ) then
-							coopSelectedUnits[ playerID ]["units"][ unitId] = realDefRadius * selectionDrawScaleFactor
-							--un-hot it
-							hotUnits[  unitId ] = nil
-						end
-					end
-				end
-			end
-		end
-		
-		return true
-	end
-	
-	return false; 
+	return true
+		 
 end
 
 function deselectAllTeamSelected()
 	local selectedUnits = array2Table( spGetSelectedUnits() )
-	for playerId, selUnits in pairs( coopSelectedUnits ) do
+	for playerID, selUnits in pairs( playerSelectedUnits ) do
 		for unitId, defRadius in pairs( selUnits["units"] ) do
-			--printDebug("Desel: " .. unitId )
 			selectedUnits[unitId] = nil
 		end
 	end
@@ -451,26 +294,27 @@ function DrawSelectedUnits()
 	glLineWidth( 2 )
 	
 	local now = spGetGameSeconds()
-	for playerId, selUnits in pairs( coopSelectedUnits ) do
-		glColor( playerColors[ playerId ][1],  playerColors[ playerId ][2],  playerColors[ playerId ][3] )  
-		for unitId, defRadius in pairs( selUnits["units"] ) do
-			local x, y, z = spGetUnitBasePosition(unitId)
-			local inView = false
-			if ( z ~= nil ) then --checking z should be enough insteady of x,y,z
-				inView = spIsSphereInView( x, y, z, defRadius )
-			end
-			--printDebug( "Draw unitID:" .. unitId  .. " radius: " .. defRadius)
-			if ( inView ) then
-				local lines = circleLinesAlly
-				if ( selUnits["coop"] == true ) then
-					lines = circleLinesCoop
+	for playerID, selUnits in pairs( playerSelectedUnits ) do
+		if selUnits["todraw"] then
+			glColor( playerColors[ playerID ][1],  playerColors[ playerID ][2],  playerColors[ playerID ][3] )  
+			for unitId, defRadius in pairs( selUnits["units"] ) do
+				local x, y, z = spGetUnitBasePosition(unitId)
+				local inView = false
+				if ( z ~= nil ) then --checking z should be enough insteady of x,y,z
+					inView = spIsSphereInView( x, y, z, defRadius )
 				end
+				if ( inView ) then
+					local lines = circleLinesAlly
+					if ( selUnits["coop"] == true ) then
+						lines = circleLinesCoop
+					end
 
-				glPushMatrix()
-				glTranslate( x, y, z)
-				glScale( defRadius, 1, defRadius)
-				glCallList(lines)
-				glPopMatrix()
+					glPushMatrix()
+					glTranslate( x, y, z)
+					glScale( defRadius, 1, defRadius)
+					glCallList(lines)
+					glPopMatrix()
+				end
 			end
 		end
 	end
@@ -502,7 +346,7 @@ function DrawHotUnits()
 					hotColor[4] = 1.0 - ( timeDiff / hotFadeTime )
 					glColor( hotColor )
 				else
-					local cl = playerColors[ val["playerId"] ]
+					local cl = playerColors[ val["playerID"] ]
 					cl[4] = 0.5 - 0.5 * ( timeDiff / hotFadeTime )
 					glColor( cl )  
 				end
@@ -532,20 +376,4 @@ function DrawHotUnits()
 	glDepthTest(false)
  	glColor(1, 1, 1, 1)
 	glLineWidth( 1 )
-end
-
-function printDebug( value )
-	if ( debug ) then
-		if ( type( value ) == "boolean" ) then
-			if ( value == true ) then spEcho( "true" )
-				else spEcho("false") end
-		elseif ( type(value ) == "table" ) then
-			spEcho("Dumping table:")
-			for key,val in pairs(value) do 
-				spEcho(key,val) 
-			end
-		else
-			spEcho( value )
-		end
-	end
 end
