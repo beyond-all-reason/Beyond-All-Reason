20b3553a6e5a4b0ec4562c544b251266bcd60458
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-11-23 15:40:11 +0000

    darken map:  default on + ctrl+alt+[ or ]   remembers per map,  reset with /resetmapdarknes

--- luaui/widgets/gfx_darken_map.lua
+++ luaui/widgets/gfx_darken_map.lua
@@ -5,62 +5,95 @@
 function widget:GetInfo()
   return {
     name      = "Darken map",
-    desc      = "Darkens map",
+    desc      = "use: Ctrl+Alt+] or [   remembers per map   reset with /resetmapdarkness",
     author    = "Floris",
     date      = "2015",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = false  --  loaded by default?
+    enabled   = true  --  loaded by default?
   }
 end
 
+
 local mapMargin = 20000
-local darkenMapOpacity = 0.08
-local darkenWorldOpacity = 0
+local darknessvalue = 0
+local darknessIncrease = 'Ctrl+Alt+]'
+local darknessDecrease = 'Ctrl+Alt+['
+local darknessStep = 0.01
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 local msx = Game.mapSizeX
 local msz = Game.mapSizeZ
-
-local glCreateList	= gl.CreateList
-local glDeleteList	= gl.DeleteList
-local glCallList	= gl.CallList
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
+local currentMapname = Game.mapName:lower()
+local maps = {}
 
 function widget:Initialize()
-    darken = glCreateList(function()
+    darken = gl.CreateList(function()
 		gl.PushMatrix()
 		gl.Translate(0,0,0)
 		gl.Rotate(90,1,0,0)
 		gl.Rect(-mapMargin, -mapMargin, msx+mapMargin, msz+mapMargin)
 		gl.PopMatrix()
     end)
+    
+	widgetHandler:AddAction("mapDarknessIncrease", mapDarknessIncrease, nil, "t")
+	Spring.SendCommands({"bind "..darknessIncrease.." mapDarknessIncrease"})
+
+	widgetHandler:AddAction("mapDarknessDecrease", mapDarknessDecrease, nil, "t")
+	Spring.SendCommands({"bind "..darknessDecrease.." mapDarknessDecrease"})
 end
 
 
 function widget:Shutdown()
-	glDeleteList(darken)
+	gl.DeleteList(darken)
 end
 
+
+function mapDarknessIncrease()
+	darknessvalue = darknessvalue + darknessStep
+	if darknessvalue > 0.66 then
+		darknessvalue = 0.66
+	end
+	maps[currentMapname] = darknessvalue
+end
+function mapDarknessDecrease()
+	darknessvalue = darknessvalue - darknessStep
+	if darknessvalue < 0 then
+		darknessvalue = 0
+	end
+	maps[currentMapname] = darknessvalue
+end
 
 function widget:DrawWorldPreUnit()
-	if darken ~= nil and darkenMapOpacity > 0 then
-		gl.Color(0,0,0,darkenMapOpacity)
-		glCallList(darken)
+	if darken ~= nil and darknessvalue > 0 then
+		gl.Color(0,0,0,darknessvalue)
+		gl.CallList(darken)
 		gl.Color(1,1,1,1)
 	end
 end
 
-function widget:DrawWorld()
-	if darken ~= nil and darkenWorldOpacity > 0 then
-		gl.Color(0,0,0,darkenWorldOpacity)
-		glCallList(darken)
-		gl.Color(1,1,1,1)
+
+function widget:GetConfigData(data)
+    savedTable = {}
+    savedTable.maps	= maps
+    return savedTable
+end
+
+function widget:SetConfigData(data)
+	if data.maps ~= nil then
+		maps = data.maps
+		if data.maps[currentMapname] ~= nil then
+			darknessvalue = data.maps[currentMapname]
+		end
 	end
 end
 
+
+function widget:TextCommand(command)
+    if (string.find(command, "resetmapdarkness") == 1  and  string.len(command) == 16) then 
+		maps = {}
+		darknessvalue = 0
+	end
+end
