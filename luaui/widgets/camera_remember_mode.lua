3d8f6f5ba44c25c32dbdd355d8213163fa3dbcf7
Decay <Decay@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-01-10 19:09:02 +0000

    BA: Avoid bug with overview->spring camera combination (spring is not meant to start with overview cam) https://springrts.com/mantis/view.php?id=5028

--- luaui/widgets/camera_remember_mode.lua
+++ luaui/widgets/camera_remember_mode.lua
@@ -24,12 +24,26 @@ function widget:SetConfigData(data)
 end
 
 function widget:Initialize()
+    avoidOverviewCam(); -- bug is when you switch from overview to spring
+
     --Spring.Echo("wanted", camName)
     if camName then
         local camState = Spring.GetCameraState()
         camState.name = camName
         camState.mode = GetModeFromName(camName)
         --Spring.Echo("set", camName, camState.mode)
+        Spring.SetCameraState(camState, 0)
+    end
+
+    avoidOverviewCam() -- and we don't want to switch to overview at game start anyway
+end
+
+function avoidOverviewCam()
+    local camState = Spring.GetCameraState()
+    if camState.name == 'ov' then -- dirty workaround for https://springrts.com/mantis/view.php?id=5028
+        Spring.Echo("Warning: you have Overview camera as default. This camera is not intended to start game with. Switching to TA camera.");
+        camState.name = defaultCamName
+        camState.mode = GetModeFromName(defaultCamName)
         Spring.SetCameraState(camState, 0)
     end
 end
