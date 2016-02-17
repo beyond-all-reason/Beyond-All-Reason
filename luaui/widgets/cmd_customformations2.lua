0107067edb579445dac54841334c655314d436b6
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-02-17 20:54:34 +0000

    customformation2 101 fix

--- luaui/widgets/cmd_customformations2.lua
+++ luaui/widgets/cmd_customformations2.lua
@@ -601,7 +601,7 @@ function widget:MouseRelease(mx, my, mButton)
 				-- Give order (i.e. pass the command to the engine to use as normal)
 				GiveNotifyingOrder(usingCmd, {targetID}, cmdOpts)			
 			elseif usingCmd == CMD_MOVE then 
-				GiveNotifyingOrder(usingCmd, fNodes[1], cmdOpts)			
+				GiveNotifyingOrder(usingCmd, {fNodes[1][1],fNodes[1][2],fNodes[1][3]}, cmdOpts)			
 			else
 				-- Deselect command, select default command instead
 				spSetActiveCommand(0)
