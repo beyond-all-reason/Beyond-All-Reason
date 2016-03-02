c53bd09a41ffd7661a91ce9f9ce4f2e85ca8d13c
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-03-02 15:25:26 +0000

    fix map_sun_handler.lua error

--- luaui/widgets/map_sun_handler.lua
+++ luaui/widgets/map_sun_handler.lua
@@ -19,7 +19,7 @@ end
 
 function widget:Initialize()
 	if (Spring.SetSunLighting ~= nil) and (gl.GetAtmosphere ~= nil) then
-		local mapSpecR, mapSpecG, mapSpecB = gl.GetAtmosphere("groundSpecularColor")
+		local mapSpecR, mapSpecG, mapSpecB = gl.GetSun("specular")
 		if (math.abs(mapSpecR - 0.1) + math.abs(mapSpecG - 0.1) + math.abs(mapSpecB - 0.1)) < 0.0001 then --default groundSpecularColor is (0.1, 0.1, 0.1)
 			Spring.Echo("Map Sun Handler: Clearing groundSpecularColor via SetSunLighting because map does not change it from default.")
 			Spring.SetSunLighting({groundSpecularColor = {0,0,0,0}})
