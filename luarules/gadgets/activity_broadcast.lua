fae9ed0b624321a484a7854cd12a9906db27b112
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-07-27 20:12:19 +0000

    added gadget: activity_broadcast, checks who is active: mouse/keyboard

---
+++ luarules/gadgets/activity_broadcast.lua
@@ -0,0 +1,93 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+--	Copyright (C) 2007.
+--	Licensed under the terms of the GNU GPL, v2 or later.
+--
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function gadget:GetInfo()
+	return {
+		name	= "Activity Broadcast",
+		desc	= "Checks if there is keyboard or mouse activity",
+		author	= "Floris",
+		date	= "July,2016",
+		license	= "GNU GPL, v2 or later",
+		layer	= 0,
+		enabled = true,
+	}
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+-- configs
+
+local sendPacketEvery	= 2
+
+
+--------------------------------------------------------------------------------
+-- synced
+--------------------------------------------------------------------------------
+if gadgetHandler:IsSyncedCode() then
+	function gadget:RecvLuaMsg(msg, playerID)
+		if msg:sub(1,2)=="^" then
+			SendToUnsynced("activityBroadcast",playerID)
+			return true
+		end
+	end
+	
+	
+else
+	--------------------------------------------------------------------------------
+	-- unsynced
+	--------------------------------------------------------------------------------
+
+	local GetMyPlayerID		= Spring.GetMyPlayerID
+	local GetMouseState		= Spring.GetMouseState
+	local GetLastUpdateSeconds= Spring.GetLastUpdateSeconds
+	local SendLuaRulesMsg	= Spring.SendLuaRulesMsg
+	
+	local myPlayerID = GetMyPlayerID()
+	local activity = false
+	local old_mx,old_my = 0,0
+	local updateTimer = 0
+
+	function gadget:Initialize()
+		gadgetHandler:AddSyncAction("activityBroadcast", handleActivityEvent)
+	end
+
+	function gadget:Shutdown()
+		gadgetHandler:RemoveSyncAction("activityBroadcast")
+	end
+
+	function handleActivityEvent(_,playerID)
+    if Script.LuaUI("ActivityEvent") then
+    	Script.LuaUI.ActivityEvent(playerID)
+    end
+	end
+
+	function gadget:Update()
+		local mx,my = GetMouseState()
+		if mx ~= old_mx or my ~= old_my then
+			old_mx,old_my = mx,my
+			activity = true
+		end
+		
+		updateTimer = updateTimer + GetLastUpdateSeconds()
+		if updateTimer > sendPacketEvery then
+			if activity then
+				SendLuaRulesMsg("^")
+			end
+			activity = false
+			updateTimer = 0
+		end
+	end
+
+	function gadget:KeyPress(key, mods, isRepeat)
+		activity = true
+	end
+
+end
+
