250142a662537a3d8883c1f57d47581dd6c7eabb
BrainDamage <BrainDamage@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-08-13 13:13:40 +0000

    make red build menu honour hidden commands

--- luaui/widgets/gui_red_buildordermenu.lua
+++ luaui/widgets/gui_red_buildordermenu.lua
@@ -656,6 +656,7 @@ local function GetCommands()
 			(not hiddencmds[cmd.id]) and
 			(cmd.action ~= nil) and
 			--(not cmd.disabled) and
+			(not widgetHandler.commands[index].hidden) and --apparently GetActiveCmdDescs is bugged and returns hidden for every command
 			(cmd.type ~= 21) and
 			(cmd.type ~= 18) and
 			(cmd.type ~= 17)
