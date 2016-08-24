569e1dbc2c03d5337619e8a809d0f3738d6ffb42
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-08-24 00:06:14 +0000

    commands_fx: config tweak

--- luaui/widgets/gui_commands_fx.lua
+++ luaui/widgets/gui_commands_fx.lua
@@ -52,15 +52,15 @@ local drawLineTexture			= true
 local drawUnitHighlight 		= true
 local drawUnitHighlightSkipFPS	= 5		-- (0 to disable) skip drawing when framerate gets below this value
 
-local opacity      				= 0.95
-local duration     				= 2.2
+local opacity      				= 0.9
+local duration     				= 2.5
 
-local lineWidth	   				= 7.6
+local lineWidth	   				= 7.5
 local lineOpacity				= 1
 local lineDuration 				= 1		-- set a value <= 1
 local lineWidthEnd				= 0.5		-- multiplier (this wont affect textured lines)
 local lineTextureLength 		= 3.5
-local lineTextureSpeed  		= 3
+local lineTextureSpeed  		= 2.2
 
 local glowRadius    			= 26
 local glowDuration  			= 0.5
