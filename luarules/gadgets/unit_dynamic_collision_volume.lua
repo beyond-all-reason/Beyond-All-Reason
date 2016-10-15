eb46613aff1d847a9df84bb381d8c2098a4b0d38
ForbodingAngel <forbodingangel@gmail.com>
2016-10-15 03:54:38 -0700

    Tell gadget to go away if fixed hitspheres are enabled

--- luarules/gadgets/unit_dynamic_collision_volume.lua
+++ luarules/gadgets/unit_dynamic_collision_volume.lua
@@ -10,6 +10,8 @@ function gadget:GetInfo()
   }
 end
 
+if Spring.GetModOptions().fixedhitspheres == "enabled" then return false end
+
 -- Pop-up style unit and per piece collision volume definitions
 local popupUnits = {}		--list of pop-up style units
 local unitCollisionVolume, pieceCollisionVolume, dynamicPieceCollisionVolume
