7812b83ff6840e40015234b5f3e35fa4fad8e0f5
Ruwetuin <floris@ruwhof.net>
2016-11-02 00:31:36 +0100

    forgot to add new reclaimshards file

---
+++ gamedata/explosions/AdvancedCEGEffects/reclaimshards.lua
@@ -0,0 +1,180 @@
+-- deathceg1
+-- deathceg0
+
+local definitions = {
+  ["reclaimshards1"] = {
+    groundflash = {
+      flashalpha         = 0.03,
+      flashsize          = 40,
+      ttl                = 9,
+      color = {
+        [1]  = 1,
+        [2]  = 1,
+        [3]  = 1,
+      },
+    },
+		shards = {
+			class = [[CSimpleParticleSystem]],
+      count = 1,
+			air = true,
+			water = true,
+			ground = true,
+			count = true,
+			properties = {
+				airdrag = 0.9,
+				directional = true,
+				emitRot = 30,
+				emitRotSpread = 30,
+				emitVector = [[0, 0.8, 0]],
+				gravity = [[0, 0.03, 0]],
+				colorMap = [[0.6 0.6 0.6 1   0.2 0.2 0.2 1]],
+				numParticles = 2,
+				particleLife = 16,
+				particleLifeSpread = 16,
+				particleSpeed = 2.5,
+				particleSpeedSpread = 1.7,
+				particleSize = 2.6,
+				particleSizeSpread = 1.8,
+				pos = [[0, -0.5, 0]],
+				sizeGrowth = -0.16,
+				sizeMod = 1,
+				texture = [[shard1]],
+			},
+		},
+    dirt = {
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      unit               = false,
+      properties = {
+        airdrag            = 1,
+        alwaysvisible      = true,
+        colormap           = [[0.3 0.3 0.3 0.7   0.3 0.3 0.3 0.6   0 0 0 0]],
+        directional        = true,
+        emitrot            = 12,
+        emitrotspread      = 33,
+        emitvector         = [[0, 1, 0]],
+        gravity            = [[0, -0.3, 0]],
+        numparticles       = 9,
+        particlelife       = 15,
+        particlelifespread = 8,
+        particlesize       = 1.2,
+        particlesizespread = -1,
+        particlespeed      = 1.6,
+        particlespeedspread = 2.2,
+        pos                = [[0, 4, 0]],
+        sizegrowth         = -0.015,
+        sizemod            = 1,
+        texture            = [[bigexplosmoke]],
+        useairlos          = false,
+      },
+    },
+    dirt2 = {
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      unit               = false,
+      properties = {
+        airdrag            = 1,
+        alwaysvisible      = true,
+        colormap           = [[0.3 0.3 0.3 0.7   0.3 0.3 0.3 0.6   0 0 0 0]],
+        directional        = true,
+        emitrot            = 0,
+        emitrotspread      = 12,
+        emitvector         = [[0, 1, 0]],
+        gravity            = [[0, -0.3, 0]],
+        numparticles       = 5,
+        particlelife       = 25,
+        particlelifespread = 10,
+        particlesize       = 1.2,
+        particlesizespread = -1,
+        particlespeed      = 1.5,
+        particlespeedspread = 2.6,
+        pos                = [[0, 4, 0]],
+        sizegrowth         = -0.015,
+        sizemod            = 1,
+        texture            = [[bigexplosmoke]],
+        useairlos          = false,
+      },
+    },
+    clouddust = {
+      air                = true,
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      properties = {
+        airdrag            = 0.92,
+        alwaysvisible      = true,
+        colormap           = [[0.02 0.02 0.02 0.03  0.055 0.055 0.055 0.066  0.05 0.05 0.05 0.052  0.03 0.03 0.03 0.028  0 0 0 0]],
+        directional        = true,
+        emitrot            = 45,
+        emitrotspread      = 3,
+        emitvector         = [[0.25, 0.8, 0.25]],
+        gravity            = [[0, 0.02, 0]],
+        numparticles       = [[r1.3]],
+        particlelife       = 40,
+        particlelifespread = 90,
+        particlesize       = 13,
+        particlesizespread = 16,
+        particlespeed      = 4,
+        particlespeedspread = 4,
+        pos                = [[0, 6, 0]],
+        sizegrowth         = 0.5,
+        sizemod            = 1.0,
+        texture            = [[bigexplosmoke]],
+      },
+    },
+    grounddust = {
+      air                = false,
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      unit               = false,
+      properties = {
+        airdrag            = 0.92,
+        alwaysvisible      = true,
+        colormap           = [[0.09 0.09 0.09 0.14 	0 0 0 0.0]],
+        directional        = true,
+        emitrot            = 90,
+        emitrotspread      = -2,
+        emitvector         = [[0, 1, 0]],
+        gravity            = [[0, 0.1, 0]],
+        numparticles       = 22,
+        particlelife       = 5,
+        particlelifespread = 25,
+        particlesize       = 3.2,
+        particlesizespread = 1.4,
+        particlespeed      = 1,
+        particlespeedspread = 2.2,
+        pos                = [[0, 0, 0]],
+        sizegrowth         = 0.15,
+        sizemod            = 1.0,
+        texture            = [[bigexplosmoke]],
+      },
+    },
+  },
+}
+
+
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
+definitions["reclaimshards2"] = deepcopy(definitions["reclaimshards1"])
+definitions["reclaimshards2"].shards.properties.texture = [[shard2]]
+definitions["reclaimshards3"] = deepcopy(definitions["reclaimshards1"])
+definitions["reclaimshards2"].shards.properties.texture = [[shard3]]
+
+
+return definitions
