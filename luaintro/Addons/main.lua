bfdff275111ea7fade3236b15922a5ce95ae77f0
Ruwetuin <floris@ruwhof.net>
2016-12-13 02:49:27 +0100

    another loadscreen loadbar gradient

--- luaintro/Addons/main.lua
+++ luaintro/Addons/main.lua
@@ -94,6 +94,15 @@ function RectRound(px,py,sx,sy,cs)
 	gl.Texture(false)
 end
 
+function gradienth(px,py,sx,sy, c1,c2)
+	gl.Color(c1)
+	gl.Vertex(sx, sy, 0)
+	gl.Vertex(sx, py, 0)
+	gl.Color(c2)
+	gl.Vertex(px, py, 0)
+	gl.Vertex(px, sy, 0)
+end
+
 function addon.DrawLoadScreen()
 	local loadProgress = SG.GetLoadProgress()
 
@@ -111,25 +120,30 @@ function addon.DrawLoadScreen()
 	local paddingW = 0.004 * (vsy/vsx)
 	local paddingH = 0.004
 	gl.Color(0.06,0.06,0.06,0.8)
-	RectRound(0.2-paddingW,0.1-paddingH,0.8+paddingW,0.15+paddingH,0.0065)
+	RectRound(0.2-paddingW,0.1-paddingH,0.8+paddingW,0.15+paddingH,0.007)
 	
+	-- loadvalue
 	gl.Color(0.4-(loadProgress/7),loadProgress*0.4,0,0.4)
-	RectRound(0.2,0.1,loadvalue,0.15,0.005)
+	RectRound(0.2,0.1,loadvalue,0.15,0.0055)
 	
-	gl.Color(1-(loadProgress/3.5)+0.15,loadProgress+0.15,0+0.05,0.08)
+	-- loadvalue gradient
+	gl.Texture(false)
+	gl.BeginEnd(GL.QUADS, gradienth, 0.2,0.1,loadvalue,0.15, {1-(loadProgress/3)+0.2,loadProgress+0.2,0+0.08,0.14}, {0,0,0,0.14})
+	
+	-- loadvalue inner glow
+	gl.Color(1-(loadProgress/3.5)+0.15,loadProgress+0.15,0+0.05,0.085)
 	gl.Texture(":n:luaui/Images/barglow-center.dds")
 	gl.TexRect(0.2,0.1,loadvalue,0.15)
 	
-	
+	-- loadvalue glow
 	local glowSize = 0.045
 	gl.Color(1-(loadProgress/3)+0.15,loadProgress+0.15,0+0.05,0.07)
 	gl.Texture(":n:luaui/Images/barglow-center.dds")
 	gl.TexRect(0.2,	0.1-glowSize,	loadvalue,	0.15+glowSize)
 	
 	gl.Texture(":n:luaui/Images/barglow-edge.dds")
-	gl.TexRect(0.2-(glowSize*1.33), 0.1-glowSize, 0.2, 0.15+glowSize)
-	
-	gl.TexRect(loadvalue+(glowSize*1.33), 0.1-glowSize, loadvalue, 0.15+glowSize)
+	gl.TexRect(0.2-(glowSize*1.3), 0.1-glowSize, 0.2, 0.15+glowSize)
+	gl.TexRect(loadvalue+(glowSize*1.3), 0.1-glowSize, loadvalue, 0.15+glowSize)
 	
 	-- progressbar text
 	gl.PushMatrix()
@@ -138,7 +152,7 @@ function addon.DrawLoadScreen()
 
 		--font:Print(lastLoadMessage, vsx * 0.5, vsy * 0.3, 50, "sc")
 		--font:Print(Game.gameName, vsx * 0.5, vsy * 0.95, vsy * 0.07, "sca")
-		font:Print(lastLoadMessage, vsx * 0.209, vsy * 0.133, barTextSize * 0.64, "oa")
+		font:Print(lastLoadMessage, vsx * 0.21, vsy * 0.133, barTextSize * 0.67, "oa")
 		if loadProgress>0 then
 			font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * 0.1175, barTextSize, "oc")
 		else
