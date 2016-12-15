b17ab82aa7372a27823e4e01146916acd1196adf
Ruwetuin <floris@ruwhof.net>
2016-12-15 18:22:11 +0100

    options widget making snow description fit again

--- luaui/Widgets_BA/gui_options.lua
+++ luaui/Widgets_BA/gui_options.lua
@@ -946,7 +946,7 @@ function widget:Initialize()
 		{id="sndvolmaster", name="Sound volume", type="slider", min=0, max=200, value=tonumber(Spring.GetConfigInt("snd_volmaster",1) or 100)},
 		{id="fpstimespeed", name="Display FPS, GameTime and Speed", type="bool", value=tonumber(Spring.GetConfigInt("ShowFPS",1) or 1) == 1, description='Located at the top right of the screen\n\nIndividually toggle them with /fps /clock /speed'},
 		
-		{id="snow", widget="Snow", name="Snow", type="bool", value=widgetHandler.orderList["Snow"] ~= nil and (widgetHandler.orderList["Snow"] > 0), description='Enable snow on winter maps, auto reduces show when fps gets lower and unitcount higher\n\nUse /snow to toggle snow for current map (it remembers)'},
+		{id="snow", widget="Snow", name="Snow", type="bool", value=widgetHandler.orderList["Snow"] ~= nil and (widgetHandler.orderList["Snow"] > 0), description='Snows at winter maps, auto reduces amount when fps gets lower and unitcount higher\n\nUse /snow to toggle snow for current map (it remembers)'},
 		{id="teamcolors", widget="Common Team Colors", name="Team colors based on a palette", type="bool", value=widgetHandler.orderList["Common Team Colors"] ~= nil and (widgetHandler.orderList["Common Team Colors"] > 0), description='Replaces lobby team colors for a color palette based one\n\nNOTE: reloads all widgets because these need to update their teamcolors'},
 		
 		{id="camera", name="Camera", type="select", options={'fps','overhead','spring','rot overhead','free'}, value=(tonumber(Spring.GetConfigInt("CamMode",1) or 2))},
