9a244993262043a593a90d8f98140f0f0d5e2cec
ForbodingAngel <forbodingangel@gmail.com>
2017-01-24 18:19:28 -0800

    Update beamer and script Add beamer sound Make damaged ceg show bubbles when underwater

--- gamedata/explosions/AdvancedCEGEffects/blacksmoke.lua
+++ gamedata/explosions/AdvancedCEGEffects/blacksmoke.lua
@@ -1,5 +1,3 @@
-
-
 return {
   ["blacksmoke"] = {
     dirtg = {
@@ -8,6 +6,7 @@ return {
       count              = 2,
       ground             = true,
 	  water				 = true,
+	  underwater		 = false,
       properties = {
         airdrag            = 0.7,
         --alwaysvisible      = true,
@@ -27,6 +26,37 @@ return {
         sizegrowth         = 1,
         sizemod            = 0.9,
         texture            = [[new_dirta]],
+        useairlos          = false,
+		colorchange			= "stuffs",
+      },
+    },
+	
+    bubblesuw = {
+      air                = false,
+      class              = [[CSimpleParticleSystem]],
+      count              = 2,
+      ground             = false,
+	  water				 = false,
+	  underwater		 = true,
+      properties = {
+        airdrag            = 0.7,
+        --alwaysvisible      = true,
+        colormap           = [[1 1 1 0.5	0.5 0.5 1 0.8	0 0 0 0.0]],
+        directional        = true,
+        emitrot            = 45,
+        emitrotspread      = 32,
+        emitvector         = [[0, 1, 0]],
+        gravity            = [[0, 0.3, 0]],
+        numparticles       = 4,
+        particlelife       = 60,
+        particlelifespread = 20,
+        particlesize       = 1,
+        particlesizespread = 2,
+        particlespeed      = 1,
+        particlespeedspread = 4,
+        sizegrowth         = 1,
+        sizemod            = 0.9,
+        texture            = [[randdots]],
         useairlos          = false,
 		colorchange			= "stuffs",
       },
---
+++ scripts/armbeamer_lus.lua
@@ -0,0 +1,49 @@
+base, turret, sleeve, barrel, flare = piece('base', 'turret', 'sleeve', 'barrel', 'flare')
+
+local SIG_AIM = {}
+
+function script.Create()
+	StartThread(common.SmokeUnit, {base, turret, barrel})
+end
+
+common = include("headers/common_includes_lus.lua")
+
+local function RestoreAfterDelay()
+	Sleep(2000)
+	Turn(turret, y_axis, 0, 5)
+	Turn(sleeve, x_axis, 0, 5)
+end		
+
+function script.AimFromWeapon(weaponID)
+	--Spring.Echo("AimFromWeapon: FireWeapon")
+	return turret
+end
+
+function script.QueryWeapon(weaponID)
+	--Spring.Echo("QueryWeapon: FireWeapon")
+	return flare
+end
+
+function script.AimWeapon(weaponID, heading, pitch)
+	Signal(SIG_AIM)
+	SetSignalMask(SIG_AIM)
+	Turn(turret, y_axis, heading, 100)
+	Turn(sleeve, x_axis, -pitch, 100)
+	WaitForTurn(turret, y_axis)
+	WaitForTurn(sleeve, x_axis)
+	StartThread(RestoreAfterDelay)
+	--Spring.Echo("AimWeapon: FireWeapon")
+	return true
+end
+
+function script.FireWeapon(weaponID)
+	--Spring.Echo("FireWeapon: FireWeapon")
+	--EmitSfx (flare, 1024)
+end
+
+function script.Killed()
+		Explode(barrel, SFX.EXPLODE_ON_HIT)
+		Explode(turret, SFX.EXPLODE_ON_HIT)
+		Explode(sleeve, SFX.EXPLODE_ON_HIT)
+		return 3   -- spawn ARMSTUMP_DEAD corpse / This is the equivalent of corpsetype = 1; in bos
+end
