aa7c4aeb9ead44e905085ff694ed46b5760a100a
Fx-Doo <hedibertille93@gmail.com>
2017-05-03 12:32:48 +0200

    Armcrus update
    - Armcrus rescripted: All weapons properly separated: 2 Plasma canons
    (double barrel) with parralel shots (unit_armcrus_wpn.lua + script + wpn
    defs) can only shoot same target-- 2 light lasers (front/rear) with
    halved rate of fire but can shoot both at once and both at different
    targets -- 1 depthcharge launcher. Max speed decreased, accel and
    brakerate dereased, turnrate decreased (195 according to formula). Costs
    increased and same build time (20500e-2500m-19700bt)-- model scaled *1.5
    ----Note: The two barrels can somehow still go out of sync even though
    they should always be able to shoot at the same time-- If the delay
    between two shots excess 4 frames then the parralel shots effect wont
    apply. (avoid projectile flying in wrong direction when out of sync)

---
+++ luarules/gadgets/unit_armcrus_wpn.lua
@@ -0,0 +1,68 @@
+function gadget:GetInfo()
+   return {
+      name         = "ARMCRUS wpn fix",
+      desc         = "Copies velocity value from proj #1 to proj #2 for parralel shots",
+      author       = "Doo",
+      date         = "05/03/2017",
+      license      = "GPL 2.0 or later", -- should be compatible with Spring
+      layer        = 0,
+      enabled      = true
+   }
+end
+
+if (gadgetHandler:IsSyncedCode()) then
+testedweaponname = "armcrus_heavyplasmacannon"
+ProOwner = {}
+function gadget:Initialize()
+for wname, wdef in pairs(WeaponDefNames) do
+	if wname == testedweaponname then testedweaponID = wdef.id end
+end
+end
+
+function gadget:ProjectileCreated(proID, proOwner, weaponDefID)
+
+if (Spring.GetProjectileDefID(proID)) and (testedweaponID) then
+if Spring.GetProjectileDefID(proID) == testedweaponID then
+if ProOwner[proOwner] then
+if ProOwner[proOwner]["f"] then
+	if ProOwner[proOwner]["f"] >= Spring.GetGameFrame() + 4 then
+	ProOwner[proOwner] = nil
+	end
+end
+end
+
+if not (ProOwner[proOwner]) then
+ProOwner[proOwner] = {}
+ProOwner[proOwner]["n"] = 1
+-- Spring.Echo("table created")
+end
+
+ProOwner[proOwner]["f"] = Spring.GetGameFrame()
+
+if ProOwner[proOwner]["n"] == 1 then
+ProOwner[proOwner]["vx"], ProOwner[proOwner]["vy"], ProOwner[proOwner]["vz"] = Spring.GetProjectileVelocity(proID)
+-- Spring.Echo("1")
+end
+
+if ProOwner[proOwner]["n"] == 2 then
+Spring.SetProjectileVelocity(proID, ProOwner[proOwner]["vx"], ProOwner[proOwner]["vy"], ProOwner[proOwner]["vz"])
+-- Spring.Echo("2")
+end
+
+ProOwner[proOwner]["n"] = ProOwner[proOwner]["n"] + 1
+
+if ProOwner[proOwner]["n"] == 3 then
+ProOwner[proOwner]["n"] = 1
+-- Spring.Echo("3 = 1")
+end
+
+
+
+f1 = Spring.GetGameFrame()
+-- Spring.Echo("Projectile ("..proID..") created at frame "..f1)
+end
+end
+end
+
+
+end
