f5397e4bd6faa67626a0d335762b648ba616806b
Floris <floris@ruwhof.net>
2017-05-26 23:00:26 +0200

    revert "add Shard Lua AI"

--- luarules/gadgets/ai/modules.lua
+++
@@ -1,7 +0,0 @@
-shard_include( "spothandler" )
-shard_include( "controlpointhandler" )
-shard_include( "unithandler" )
-shard_include( "attackhandler" )
-shard_include( "sleep" )
-
-modules = { UnitHandler, AttackHandler, ControlPointHandler, MetalSpotHandler, Sleep }
