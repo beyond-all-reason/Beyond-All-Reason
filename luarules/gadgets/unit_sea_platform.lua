c71e733b061073cc5be189986c6412c4000740f0
Fx-Doo <hedibertille93@gmail.com>
2017-06-24 23:38:47 +0200

    Added checks for ground height in 4 directions to eventually level the ground near coasts

--- luarules/gadgets/unit_sea_platform.lua
+++ luarules/gadgets/unit_sea_platform.lua
@@ -29,6 +29,50 @@ Spring.SetUnitMidAndAimPos(unitID, x, GroundHeight[unitID]/2, z, x, 4, z, false)
 Spring.SetUnitRadiusAndHeight(unitID, 24, -GroundHeight[unitID])
 Spring.SetUnitCollisionVolumeData(unitID, 48, -GroundHeight[unitID], 48, 0,0,0,2,1,0)
 Spring.LevelHeightMap(x-15,z-15,x+16,z+16, 1)
+--CHECK NORTHERN GroundHeight
+local i = -200
+	for l = -15,16 do
+	for k = 15,47 do
+		i = Spring.GetGroundHeight(x+l, z - k)
+			if i >= 2 and i <= 5 then
+				Spring.LevelHeightMap(x+l, z, x+l, z-k,1)
+				end
+		end
+	end
+	
+-- Check southern
+local i = -200
+	for l = -15,16 do
+	for k = -15,-47 do
+		i = Spring.GetGroundHeight(x+l, z - k)
+			if i >= 2 and i <= 5 then
+				Spring.LevelHeightMap(x+l, z, x+l, z-k,1)
+				end
+		end
+	end
+-- Check eastern
+local i = -200
+	for l = -15,16 do
+	for k = -15,-47 do
+		i = Spring.GetGroundHeight(x-k, z + l)
+			if i >= 2 and i <= 5 then
+				Spring.LevelHeightMap(x-k, z+l, x-k, z+l,1)
+				end
+		end
+	end
+-- Check western
+local i = -200
+	for l = -15,16 do
+	for k = 15,47 do
+		i = Spring.GetGroundHeight(x-k, z + l)
+			if i >= 2 and i <= 5 then
+				Spring.LevelHeightMap(x-k, z+l, x-k, z+l,1)
+				end
+		end
+	end
+
+
+
 end
 end
 
