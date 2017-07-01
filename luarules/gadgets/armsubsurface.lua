dd05db8662ba4bd3e390695608790413ef562abc
Fx-Doo <hedibertille93@gmail.com>
2017-07-01 13:56:10 +0200

    Testing Armsub with two modes: Submarine and Surface
    - Surface upon disabling submarine mode
    - Sonar deactivates upon surfacing
    - Rocket Launcher activates upon surfacing (Basically, extra dps
    activates when surfacing, the rocketlauncher is just a mean to that and
    wont be kept)
    - Torpedos can only shoot downwards, this means a submarine cannot hit
    ships in its submarine state
    - No animation, no delay yet
    - Exp, health, capture status, buildprogress are kept between both
    states
    -Reloadstate is not (yet) kept between states so it is pretty much
    abusable for now.

---
+++ luarules/gadgets/armsubsurface.lua
@@ -0,0 +1,78 @@
+function gadget:GetInfo()
+	return {
+		name = "armsubsurface",
+		desc = "Handles Sea Platforms behaviours",
+		author = "[Fx]Doo",
+		date = "25 of June 2017",
+		license = "Free",
+		layer = 0,
+		enabled = true
+	}
+end
+
+
+if (gadgetHandler:IsSyncedCode()) then
+UW = {}
+SURF = {}
+function gadget:UnitFinished(unitID)
+unitDefID = Spring.GetUnitDefID(unitID)
+unitName = UnitDefs[unitDefID].name
+
+if unitName == "armsub" then
+UW[unitID] = true
+end
+if unitName == "armsubsurface" then
+SURF[unitID] = true
+end
+end
+
+function gadget:GameFrame(f)
+for unitID, under in pairs(UW) do
+if not Spring.GetUnitIsActive(unitID) == true then
+local x,y,z = Spring.GetUnitPosition(unitID)
+local rx, ry, rz = Spring.GetUnitRotation(unitID)
+local team = Spring.GetUnitTeam(unitID)
+local health, maxHealth, paralyzeDamage, captureProgress, buildProgress = Spring.GetUnitHealth(unitID)
+local expert = Spring.GetUnitExperience(unitID)
+local vx, vy, vz = Spring.GetUnitVelocity(unitID)
+Spring.DestroyUnit(unitID, false, true)
+UW[unitID] = nil
+newID = Spring.CreateUnit("armsubsurface", x,y,z, "n", team)
+Spring.SetUnitRotation(newID, rx,ry,rz)
+Spring.SetUnitHealth(newID, health, captureProgress, paralyzeDamage, buildProgress)
+Spring.SetUnitExperience(newID, expert)
+Spring.SetUnitVelocity(newID, vx, vy, vz)
+SURF[newID] = true
+end
+end
+for unitID, under in pairs(SURF) do
+if not Spring.GetUnitIsActive(unitID) == false then
+local x,y,z = Spring.GetUnitPosition(unitID)
+local rx, ry, rz = Spring.GetUnitRotation(unitID)
+local team = Spring.GetUnitTeam(unitID)
+local health, maxHealth, paralyzeDamage, captureProgress, buildProgress = Spring.GetUnitHealth(unitID)
+local expert = Spring.GetUnitExperience(unitID)
+local vx, vy, vz = Spring.GetUnitVelocity(unitID)
+Spring.DestroyUnit(unitID, false, true)
+SURF[unitID] = nil
+newID = Spring.CreateUnit("armsub", x,y,z, "n", team)
+Spring.SetUnitRotation(newID, rx,ry,rz)
+Spring.SetUnitHealth(newID, health, captureProgress, paralyzeDamage, buildProgress)
+Spring.SetUnitExperience(newID, expert)
+Spring.SetUnitVelocity(newID, vx, vy, vz)
+UW[newID] = true
+end
+end
+end
+
+
+
+function gadget:UnitDestroyed(unitID)
+if UW[unitID] then
+UW[unitID] = nil
+end
+if SURF[unitID] then
+UW[unitID] = nil
+end
+end
+end
