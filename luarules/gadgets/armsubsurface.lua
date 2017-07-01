efe1643b602975928ac7d79ba015b627d2d53044
Fx-Doo <hedibertille93@gmail.com>
2017-07-01 17:08:33 +0200

    Added comments and desc to the testing gadget

--- luarules/gadgets/armsubsurface.lua
+++ luarules/gadgets/armsubsurface.lua
@@ -1,9 +1,9 @@
 function gadget:GetInfo()
 	return {
-		name = "armsubsurface",
-		desc = "Handles Sea Platforms behaviours",
+		name = "ARMSUB SURFACING Testing gadget",
+		desc = "Makes On/Off button act as a selector for Surface/Sub modes",
 		author = "[Fx]Doo",
-		date = "25 of June 2017",
+		date = "1st of July 2017",
 		license = "Free",
 		layer = 0,
 		enabled = true
@@ -14,7 +14,7 @@ end
 if (gadgetHandler:IsSyncedCode()) then
 UW = {}
 SURF = {}
-function gadget:UnitFinished(unitID)
+function gadget:UnitFinished(unitID) -- add UW[unitID] and SURF[unitID] upon creation
 unitDefID = Spring.GetUnitDefID(unitID)
 unitName = UnitDefs[unitDefID].name
 if unitName == "armsub" then
@@ -26,55 +26,54 @@ end
 end
 
 function gadget:GameFrame(f)
-for unitID, under in pairs(UW) do
-if not Spring.GetUnitIsActive(unitID) == true then
-
+for unitID, under in pairs(UW) do -- check all UW submarines
+if not Spring.GetUnitIsActive(unitID) == true then -- Check if they changed state
+-- Gather stats of an UW sub that has been turned off
 local x,y,z = Spring.GetUnitPosition(unitID)
 local rx, ry, rz = Spring.GetUnitRotation(unitID)
 local team = Spring.GetUnitTeam(unitID)
 local health, maxHealth, paralyzeDamage, captureProgress, buildProgress = Spring.GetUnitHealth(unitID)
 local expert = Spring.GetUnitExperience(unitID)
 local vx, vy, vz = Spring.GetUnitVelocity(unitID)
-Spring.DestroyUnit(unitID, false, true)
-UW[unitID] = nil
-newID = Spring.CreateUnit("armsubsurface", x,y,z, "n", team)
+Spring.DestroyUnit(unitID, false, true) -- destroy it
+UW[unitID] = nil -- clear UW[unitID] entry
+newID = Spring.CreateUnit("armsubsurface", x,y,z, "n", team) --Create newID (surface)
+-- Reapply stats
 Spring.SetUnitRotation(newID, rx,ry,rz)
 Spring.SetUnitHealth(newID, health, captureProgress, paralyzeDamage, buildProgress)
 Spring.SetUnitExperience(newID, expert)
 Spring.SetUnitVelocity(newID, vx, vy, vz)
-Spring.AddUnitDamage(newID, 835*1.05, 835*1.05, -1, -2)
-Spring.Echo("added damages")
-SURF[newID] = true
+
+Spring.AddUnitDamage(newID, 835*1.05, 835*1.05, -1, -2) -- Apply an EMP (simulates animation delay)
+-- Spring.Echo("added damages")
+SURF[newID] = true -- add SURF[newID] entry
 end
 end
-for unitID, under in pairs(SURF) do
-if not Spring.GetUnitIsActive(unitID) == false then
+for unitID, under in pairs(SURF) do -- check all SURF submarines
+if not Spring.GetUnitIsActive(unitID) == false then-- Check if they changed state
+-- Gather stats of an SURF sub that has been turned on
 local x,y,z = Spring.GetUnitPosition(unitID)
 local rx, ry, rz = Spring.GetUnitRotation(unitID)
 local team = Spring.GetUnitTeam(unitID)
 local health, maxHealth, paralyzeDamage, captureProgress, buildProgress = Spring.GetUnitHealth(unitID)
 local expert = Spring.GetUnitExperience(unitID)
 local vx, vy, vz = Spring.GetUnitVelocity(unitID)
-Spring.DestroyUnit(unitID, false, true)
-SURF[unitID] = nil
-newID = Spring.CreateUnit("armsub", x,y,z, "n", team)
+Spring.DestroyUnit(unitID, false, true)-- destroy it
+SURF[unitID] = nil-- clear UW[unitID] entry
+newID = Spring.CreateUnit("armsub", x,y,z, "n", team)--Create newID (surface)
+-- Reapply stats
 Spring.SetUnitRotation(newID, rx,ry,rz)
 Spring.SetUnitHealth(newID, health, captureProgress, paralyzeDamage, buildProgress)
 Spring.SetUnitExperience(newID, expert)
 Spring.SetUnitVelocity(newID, vx, vy, vz)
-Spring.AddUnitDamage(newID, 835*1.05, 835*1.05, -1, -2)
-Spring.Echo("added damages")
-UW[newID] = true
+Spring.AddUnitDamage(newID, 835*1.05, 835*1.05, -1, -2) -- Apply an EMP (simulates animation delay)
+-- Spring.Echo("added damages")
+UW[newID] = true -- add UW[newID] entry
 end
 end
 end
 
-function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer)
-Spring.Echo(damage)
-Spring.Echo(paralyzer)
-end
-
-function gadget:UnitDestroyed(unitID)
+function gadget:UnitDestroyed(unitID) -- Clear UW[unitID] and SURF[unitID] upon death
 if UW[unitID] then
 UW[unitID] = nil
 end
