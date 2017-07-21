707da6c459f40488f621e382568f6feca664987e
tzaeru <tzaeru@gmail.com>
2017-07-21 17:11:45 +0300

    Fixed alt and ctrl formations

--- luaui/Widgets_BA/cmd_customformations2.lua
+++ luaui/Widgets_BA/cmd_customformations2.lua
@@ -613,16 +613,15 @@ function widget:MouseRelease(mx, my, mButton)
 			if targetID then
 				-- Give order (i.e. pass the command to the engine to use as normal)
 				GiveNotifyingOrder(usingCmd, {targetID}, cmdOpts)			
-			elseif usingCmd == CMD_MOVE then 
+			elseif usingCmd == CMD_MOVE then
 				VFS.Include("LuaRules/Configs/customcmds.h.lua")
-				-- Spring.Echo("Issuing move order: " .. usingCmd)
 				local selUnits = spGetSelectedUnits()
 				local uSpeed = UnitDefs[spGetUnitDefID(selUnits[1])].speed
 				--spGiveOrderToUnit(selUnits[1],
 			--		CMD_INSERT,
 			--		{-1,CMD_RAW_MOVE,0,fNodes[1][1],fNodes[1][2],fNodes[1][3]},
 			--		{"alt"})
-			    GiveNotifyingOrder(usingCmd, {fNodes[1][1],fNodes[1][2],fNodes[1][3]}, cmdOpts)			
+			    GiveNotifyingOrder(CMD.MOVE, {fNodes[1][1],fNodes[1][2],fNodes[1][3]}, cmdOpts)			
 			else
 				-- Deselect command, select default command instead
 				spSetActiveCommand(0)
@@ -712,6 +711,44 @@ function widget:KeyRelease(key)
 		spSetActiveCommand(0)
 		endShift = false
 	end
+end
+
+function table_print (tt, indent, done)
+  done = done or {}
+  indent = indent or 0
+  if type(tt) == "table" then
+    local sb = {}
+    for key, value in pairs (tt) do
+      table.insert(sb, string.rep (" ", indent)) -- indent it
+      if type (value) == "table" and not done [value] then
+        done [value] = true
+        table.insert(sb, "{\n");
+        table.insert(sb, table_print (value, indent + 2, done))
+        table.insert(sb, string.rep (" ", indent)) -- indent it
+        table.insert(sb, "}\n");
+      elseif "number" == type(key) then
+        table.insert(sb, string.format("\"%s\"\n", tostring(value)))
+      else
+        table.insert(sb, string.format(
+            "%s = \"%s\"\n", tostring (key), tostring(value)))
+       end
+    end
+    return table.concat(sb)
+  else
+    return tt .. "\n"
+  end
+end
+
+function to_string( tbl )
+    if  "nil"       == type( tbl ) then
+        return tostring(nil)
+    elseif  "table" == type( tbl ) then
+        return table_print(tbl)
+    elseif  "string" == type( tbl ) then
+        return tbl
+    else
+        return tostring(tbl)
+    end
 end
 
 --------------------------------------------------------------------------------
