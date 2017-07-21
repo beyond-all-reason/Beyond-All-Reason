51e47bcb02266a6019ead1aef7209d17a6dbffbd
Floris <floris@ruwhof.net>
2017-07-22 00:47:21 +0200

    fix darken map widget glitch

--- luaui/Widgets_BA/gfx_darken_map.lua
+++ luaui/Widgets_BA/gfx_darken_map.lua
@@ -15,32 +15,18 @@ function widget:GetInfo()
 end
 
 
-local mapMargin = 20000
 local darknessvalue = 0
-local darknessIncrease = 'Ctrl+Alt+]'
-local darknessDecrease = 'Ctrl+Alt+['
-local darknessStep = 0.02
 local maxDarkness = 0.6
 local darkenFeatures = false
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local msx = Game.mapSizeX
-local msz = Game.mapSizeZ
 local currentMapname = Game.mapName:lower()
 local maps = {}
 local gaia = Spring.GetGaiaTeamID()
 
 function widget:Initialize()
-  darken = gl.CreateList(function()
-		gl.PushMatrix()
-		gl.Translate(0,0,0)
-		gl.Rotate(90,1,0,0)
-		gl.Rect(-mapMargin, -mapMargin, msx+mapMargin, msz+mapMargin)
-		gl.PopMatrix()
-  end)
-  
   WG['darkenmap'] = {}
   WG['darkenmap'].getMapDarkness = function()
   	return darknessvalue
@@ -60,11 +46,6 @@ function widget:Initialize()
 end
 
 
-function widget:Shutdown()
-	gl.DeleteList(darken)
-end
-
-
 function mapDarkness(_,_,params)
 	if #params == 1 then
 		if type(tonumber(params[1])) == "number" then
@@ -77,31 +58,35 @@ function mapDarkness(_,_,params)
 	end
 end
 
-function widget:DrawWorldPreUnit()
-	
-  local drawMode = Spring.GetMapDrawMode()
-  if (drawMode=="height") or (drawMode=="path") then return end
-
-	if darken ~= nil and darknessvalue > 0 then
-		gl.Color(0,0,0,darknessvalue)
-		gl.CallList(darken)
-		gl.Color(1,1,1,1)
-	end
-end
-
 
 local prevCam = {}
 prevCam[1],prevCam[2],prevCam[3] = Spring.GetCameraPosition()
 function widget:Update(dt)
-	local camX, camY, camZ = Spring.GetCameraPosition()
+	camX, camY, camZ = Spring.GetCameraPosition()
+    camDirX,camDirY,camDirZ = Spring.GetCameraDirection()
 	if camX ~= prevCam[1] or  camY ~= prevCam[2] or  camZ ~= prevCam[3] then
 		features = Spring.GetVisibleFeatures(gaia, 250, false)
 	end
 end
 
+function widget:DrawWorldPreUnit()
+
+    local drawMode = Spring.GetMapDrawMode()
+    if (drawMode=="height") or (drawMode=="path") then return end
+
+    if darknessvalue >= 0.01 then
+        gl.PushMatrix()
+        gl.Color(0,0,0,darknessvalue)
+        gl.Translate(camX+(camDirX*360),camY+(camDirY*360),camZ+(camDirZ*360))
+        gl.Billboard()
+        gl.Rect(-500, -500, 500, 500)
+        gl.PopMatrix()
+    end
+end
+
 local spGetFeatureDefID = Spring.GetFeatureDefID
 function widget:DrawWorld()
-	if darkenFeatures and darken ~= nil and darknessvalue > 0.03 then
+	if darkenFeatures and darknessvalue >= 0.01 then
 		
 		if features == nil then
 			features = Spring.GetVisibleFeatures(gaia, 250, false)
