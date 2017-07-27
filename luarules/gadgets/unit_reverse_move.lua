cdb7d8d38be571080d1ce4ce0588c9b03c7cf0a6
Fx-Doo <hedibertille93@gmail.com>
2017-07-27 16:03:07 +0200

    Now forces reverse move when weapon1 is targetting, reverts to default settings when not targetting

--- luarules/gadgets/unit_reverse_move.lua
+++ luarules/gadgets/unit_reverse_move.lua
@@ -12,12 +12,36 @@ end
 
 
 if (gadgetHandler:IsSyncedCode()) then
+reverseUnit = {}
 	function gadget:UnitCreated(unitID)
 		unitDefID = Spring.GetUnitDefID(unitID)
 		if not (UnitDefs[unitDefID].rSpeed == nil or UnitDefs[unitDefID].rSpeed == 0) then
-			-- Spring.Echo("Setting for "..unitID)
+			Spring.Echo("Setting for "..unitID)
+			reverseUnit[unitID] = true
 			Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 512)
 			Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 140)
+		end
+	end
+	
+	function gadget:UnitDestroyed(unitID)
+		if reverseUnit[unitID] then
+			reverseUnit[unitID] = nil
+		end
+	end
+	
+	function gadget:GameFrame(f)
+	-- Spring.Echo("XXX")
+		for unitID, canReverse in pairs(reverseUnit) do
+		a, b, c, d, e = Spring.GetUnitWeaponTarget(unitID, 1)
+				if a ~= 0 then
+					-- Spring.Echo(unitID.." is targetting")
+					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 150000)
+					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 90)
+				else
+					-- Spring.Echo(unitID.." stopped targetting")
+					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 512)
+					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 140)
+				end
 		end
 	end
 end
