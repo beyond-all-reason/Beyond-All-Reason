a21420fe0a5646035da91cf0c94e8151273d7673
Fx-Doo <hedibertille93@gmail.com>
2017-07-27 15:09:14 +0200

    unit_reverse_move optimization
    Doesn't need an exhaustive list of units that can reversemove anymore.
    It checks rspeed in the unitdefs instead.

--- luarules/gadgets/unit_reverse_move.lua
+++ luarules/gadgets/unit_reverse_move.lua
@@ -12,19 +12,9 @@ end
 
 
 if (gadgetHandler:IsSyncedCode()) then
-	Tanks = {
-	[UnitDefNames["armmanni"].id] = true,
-	[UnitDefNames["armart"].id] = true,
-	[UnitDefNames["armmart"].id] = true,
-	[UnitDefNames["cormart"].id] = true,
-	[UnitDefNames["corban"].id] = true,
-	[UnitDefNames["cortrem"].id] = true,
-	[UnitDefNames["corwolv"].id] = true,
-	}
-
 	function gadget:UnitCreated(unitID)
 		unitDefID = Spring.GetUnitDefID(unitID)
-		if Tanks[unitDefID] == true then
+		if not (UnitDefs[unitDefID].rSpeed == nil or UnitDefs[unitDefID].rSpeed == 0) then
 			-- Spring.Echo("Setting for "..unitID)
 			Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 512)
 			Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 140)
