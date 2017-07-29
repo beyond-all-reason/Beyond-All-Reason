f9f6cd5d80d23894c84a0d4c6e708b68193ac9f7
Fx-Doo <hedibertille93@gmail.com>
2017-07-29 20:15:03 +0200

    Max reverse distance when aiming decreased to 700 (pene/arti) and 480 (all others)// Min angle decreased for 120 for all, 90 when aiming.
    For further changes, minreverseangle is the angle between the unit
    current direction and the wanted move order direction.
    maxreversedist is the maximum distance of a move waypoint that can
    trigger reverse mode if within minreverseangle.

--- luarules/gadgets/unit_reverse_move.lua
+++ luarules/gadgets/unit_reverse_move.lua
@@ -70,19 +70,19 @@ reverseUnit = {}
 		local unitDefID = Spring.GetUnitDefID(unitID)
 			if a ~= 0 and isFront == true and not (UnitDefs[unitDefID].isBuilder == true) then -- When Targetting + facing unit + not a builder
 				if (UnitDefs[unitDefID].rSpeed/UnitDefs[unitDefID].speed) >= 50 then -- If penetrator/arti/banisher/tremor
-					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 5000000)
+					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 700)
 					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 90)
 				else -- If not penetrator/arti/banisher/tremor
-					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 5000000)
-					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 120)
+					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 480)
+					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 90)
 				end
 			else -- If not Targetting + facing unit + not a builder
 				if (UnitDefs[unitDefID].rSpeed/UnitDefs[unitDefID].speed) >= 50 then -- If penetrator/arti/banisher/tremor
 					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 300)
-					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 140)
+					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 120)
 				else -- If not penetrator/arti/banisher/tremor
 					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "maxReverseDist", 180)
-					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 160)
+					Spring.MoveCtrl.SetGroundMoveTypeData(unitID, "minReverseAngle", 120)
 				end
 			end
 		end
