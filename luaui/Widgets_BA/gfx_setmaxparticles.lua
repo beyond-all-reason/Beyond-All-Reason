6acf642fe5a8be31d977f584b4a8184805ea2e2f
Floris <floris@ruwhof.net>
2017-08-14 00:12:14 +0200

    set maxparticles widget: makes it only set at first launch

--- luaui/Widgets_BA/gfx_setmaxparticles.lua
+++ luaui/Widgets_BA/gfx_setmaxparticles.lua
@@ -1,10 +1,10 @@
 
-local minMaxparticles = 10000
+local minMaxparticles = 12000
 
 function widget:GetInfo()
   return {
-    name      = "MaxParticles",
-    desc      = "sets a minimum amount of maxparticles of: "..minMaxparticles,
+    name      = "Set MaxParticles",
+    desc      = "Sets a minimum amount of maxparticles of: "..minMaxparticles.." (only once)",
     author    = "Floris",
     date      = "GPL v3 or later",
     license   = "August 2017",
@@ -13,10 +13,28 @@ function widget:GetInfo()
   }
 end
 
+local hasSetMaxparticles = false
+
+
+function widget:GetConfigData()
+    savedTable = {}
+    savedTable.hasSetMaxparticles = hasSetMaxparticles
+    return savedTable
+end
+
+function widget:SetConfigData(data)
+    if data.hasSetMaxparticles ~= nil then
+        hasSetMaxparticles = data.hasSetMaxparticles
+    end
+end
+
 function widget:Initialize()
-    if tonumber(Spring.GetConfigInt("MaxParticles",1) or 0) < minMaxparticles then
-        Spring.SetConfigInt("MaxParticles", minMaxparticles)
-        Spring.Echo('MaxParticles widget:  setting MaxParticles config value to '..minMaxparticles)
+    if hasSetMaxparticles == false then
+        if tonumber(Spring.GetConfigInt("MaxParticles",1) or 0) < minMaxparticles then
+            hasSetMaxparticles = true
+            Spring.SetConfigInt("MaxParticles", minMaxparticles)
+            Spring.Echo('MaxParticles widget:  setting MaxParticles config value to '..minMaxparticles)
+        end
     end
     widgetHandler:RemoveWidget()
 end
