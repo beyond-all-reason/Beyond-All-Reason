780a18e76793a9ae3e189853ee0226a93e9e9d38
Floris <floris@ruwhof.net>
2017-08-16 14:10:54 +0200

    removed old explosion particle effect for some unit scripts

--- scripts/coramph.lua
+++ scripts/coramph.lua
@@ -347,23 +347,23 @@ function script.Killed(recentDamage, maxHealth)
 	
 	elseif  (severity <= .50)  then
 		Explode(body, SFX.NONE)
-		Explode(gun, SFX.FALL)
-		Explode(head, SFX.SHATTER)
-		Explode(hips, SFX.FALL)
-		Explode(lfoot, SFX.FALL)
-		Explode(lleg, SFX.FALL)
-		Explode(rfoot, SFX.FALL)
-		Explode(rleg, SFX.FALL)
+		Explode(gun, SFX.FALL + SFX.NO_HEATCLOUD)
+		Explode(head, SFX.SHATTER + SFX.NO_HEATCLOUD)
+		Explode(hips, SFX.FALL + SFX.NO_HEATCLOUD)
+		Explode(lfoot, SFX.FALL + SFX.NO_HEATCLOUD)
+		Explode(lleg, SFX.FALL + SFX.NO_HEATCLOUD)
+		Explode(rfoot, SFX.FALL + SFX.NO_HEATCLOUD)
+		Explode(rleg, SFX.FALL + SFX.NO_HEATCLOUD)
 		return 2
 	else
 		Explode(body, SFX.NONE)
-		Explode(gun, SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT )
-		Explode(head, SFX.SHATTER)
-		Explode(hips,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT )
-		Explode(lfoot,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT )
-		Explode(lleg,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT )
-		Explode(rfoot,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT )
-		Explode(rleg,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT )
+		Explode(gun, SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT + SFX.NO_HEATCLOUD)
+		Explode(head, SFX.SHATTER + SFX.NO_HEATCLOUD)
+		Explode(hips,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT + SFX.NO_HEATCLOUD )
+		Explode(lfoot,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT + SFX.NO_HEATCLOUD )
+		Explode(lleg,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT + SFX.NO_HEATCLOUD )
+		Explode(rfoot,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT + SFX.NO_HEATCLOUD )
+		Explode(rleg,  SFX.FALL + SFX.SMOKE  + SFX.FIRE  + SFX.EXPLODE_ON_HIT + SFX.NO_HEATCLOUD )
 
 	return 3
 	end
