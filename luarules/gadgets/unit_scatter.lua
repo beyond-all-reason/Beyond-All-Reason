d6bbb5e6e42f4c8e63a2ebf83efad5f5cf7227b1
Fx-Doo <hedibertille93@gmail.com>
2017-09-19 17:00:06 +0200

    Fixed "vibrating nanos" issue

--- luarules/gadgets/unit_scatter.lua
+++ luarules/gadgets/unit_scatter.lua
@@ -30,12 +30,12 @@ dx1, dy1, dz1 = Spring.GetUnitDirection(unitID1)
 dx2, dy2, dz2 = Spring.GetUnitDirection(unitID2)
 scalar2d = math.abs(dx1*dx2 + dz1*dz2)
 mass1, mass2 = UnitDefs[Spring.GetUnitDefID(unitID1)].mass*scalar2d*5, UnitDefs[Spring.GetUnitDefID(unitID2)].mass
-if not UnitDefs[Spring.GetUnitDefID(unitID1)].isBuilding == true then
+if not (UnitDefs[Spring.GetUnitDefID(unitID1)].isBuilding == true or UnitDefs[Spring.GetUnitDefID(unitID1)].name == "armnanotc" or UnitDefs[Spring.GetUnitDefID(unitID1)].name == "cornanotc") then
 if (UnitDefs[Spring.GetUnitDefID(unitID2)].isFactory == true and Spring.AreTeamsAllied(Spring.GetUnitTeam(unitID1),Spring.GetUnitTeam(unitID2)) == false) or not UnitDefs[Spring.GetUnitDefID(unitID2)].isFactory == true then
 Spring.SetUnitVelocity(unitID1, vx1*(mass1/(mass1+mass2))+vx2*(mass2/(mass1+mass2)),vy1*(mass1/(mass1+mass2))+vy2*(mass2/(mass1+mass2)),vz1*(mass1/(mass1+mass2))+vz2*(mass2/(mass1+mass2)))
 end
 end
-if not UnitDefs[Spring.GetUnitDefID(unitID2)].isBuilding == true then
+if not (UnitDefs[Spring.GetUnitDefID(unitID2)].isBuilding == true or UnitDefs[Spring.GetUnitDefID(unitID2)].name == "armnanotc" or UnitDefs[Spring.GetUnitDefID(unitID2)].name == "cornanotc") then
 if (UnitDefs[Spring.GetUnitDefID(unitID1)].isFactory == true and Spring.AreTeamsAllied(Spring.GetUnitTeam(unitID1),Spring.GetUnitTeam(unitID2)) == false) or not UnitDefs[Spring.GetUnitDefID(unitID1)].isFactory == true then
 Spring.SetUnitVelocity(unitID2, vx1*(mass1/(mass1+mass2))+vx2*(mass2/(mass1+mass2)),vy1*(mass1/(mass1+mass2))+vy2*(mass2/(mass1+mass2)),vz1*(mass1/(mass1+mass2))+vz2*(mass2/(mass1+mass2)))
 end
