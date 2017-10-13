7eea7a502afef801fe12ec17434791a9c18ef5da
Floris <floris@ruwhof.net>
2017-10-14 01:14:35 +0200

    teamstats: forgot to remove debug line

--- luaui/Widgets_BA/gui_teamstats.lua
+++ luaui/Widgets_BA/gui_teamstats.lua
@@ -522,7 +522,7 @@ function widget:GameFrame(n,forceupdate)
 					else
 						history.killEfficiency = huge
 					end
-					playerName = nil
+					
 					playerName = playerName or ''
 
 					history.frame = colorToChar(teamColor) .. playerName..'    '
