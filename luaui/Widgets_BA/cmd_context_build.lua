f846dcf3b88a7a537e9f644f1244b3222c1052e8
Fx-Doo <hedibertille93@gmail.com>
2017-10-15 22:36:20 +0200

    Revert "gplat is now llt counterpart in sea, nanotcplat = nanotc counterpart"
    This reverts commit d2b70536a2529c72f9e32210445b734220b4cd34.

--- luaui/Widgets_BA/cmd_context_build.lua
+++ luaui/Widgets_BA/cmd_context_build.lua
@@ -33,10 +33,8 @@ local unitlist={--- Human friendly list. Automatically converted to unitdef IDs 
 {'corsolar','cortide'},
 {'armlab','armsy'},
 {'corlab','corsy'},
-{'armllt','armgplat'},
-{'corllt','corgplat'},
-{"armnanotc", "armnanotcplat"},
-{"cornanotc", "cornanotcplat"},
+{'armllt','armtl'},
+{'corllt','cortl'},
 
 -- XTA - arm
 
