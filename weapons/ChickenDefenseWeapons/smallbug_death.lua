3119e817c1e81304c9127e9c0dc3b9ec801876b3
Floris <floris@ruwhof.net>
2017-11-22 22:14:47 +0100

    some chicken explosion edits

---
+++ weapons/ChickenDefenseWeapons/smallbug_death.lua
@@ -0,0 +1,19 @@
+return {
+	smallbug_death = {
+		areaofeffect = "16",
+		craterboost = "0",
+		cratermult = "0",
+		impulseboost = "0",
+		impulsefactor = "0.4",
+		explosiongenerator = "custom:chickenunitexplosion-small-red",
+		name = "bug_death",
+		range = "400",
+		reloadtime = "3.6",
+		soundhit = "ChickenDefenseSounds/bugdie",
+		turret = "1",
+		weaponvelocity = "250",
+		damage = {
+			default = "0.001",
+		},
+	},
+}
