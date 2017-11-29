057b0328ad8e45a520d455cdbda0a853b2223179
Floris <floris@ruwhof.net>
2017-11-29 14:26:44 +0100

    state remover widget: leaves roam availible for nanos

--- luaui/Widgets_BA/cmd_state_remove.lua
+++ luaui/Widgets_BA/cmd_state_remove.lua
@@ -12,9 +12,16 @@ end
 
 local spGetSelectedUnits = Spring.GetSelectedUnits
 local spGiveOrderToUnit = Spring.GiveOrderToUnit
-
+local spGetUnitDefID = Spring.GetUnitDefID
 local CMD_FIRE_STATE = CMD.FIRE_STATE
 local CMD_MOVE_STATE = CMD.MOVE_STATE
+
+local excludedUnitsMovestate = {}
+for uDefID, uDef in pairs(UnitDefs) do
+	if uDef.deathExplosion == "nanoboom" then	-- still nice to have on nanos so they will assist ally
+		excludedUnitsMovestate[uDefID] = true
+	end
+end
 
 function widget:CommandNotify(id, params, options)
 
@@ -32,10 +39,14 @@ function widget:CommandNotify(id, params, options)
 		if params[1] == 2 then
 			local units = spGetSelectedUnits()
 			for _,sid in ipairs(units) do
-				spGiveOrderToUnit(sid, CMD_MOVE_STATE, { 0 }, {})	
+				if (excludedUnitsMovestate[spGetUnitDefID(sid)] == nil) then
+					spGiveOrderToUnit(sid, CMD_MOVE_STATE, { 0 }, {})
+				else
+					spGiveOrderToUnit(sid, CMD_MOVE_STATE, { 2 }, {})
+				end
 			end
 			return true
 		end
 	end
-	
+
 end
