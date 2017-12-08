cdace21a08cae011eda6f16e93c3a15caa27ec27
Fx-Doo <hedibertille93@gmail.com>
2017-12-08 13:16:01 +0100

    Shipyard collision: Rotates the units instead of pushing it away. Will allow pathfinding to take over control

--- luarules/gadgets/unit_shipyardcollisions.lua
+++ luarules/gadgets/unit_shipyardcollisions.lua
@@ -62,15 +62,36 @@ if shipyard[collideeID] or shipyard[colliderID] then
 	end
 		-- Shipyard position
 		local sx, sy, sz = shipyard[shipyardID].x, shipyard[shipyardID].y, shipyard[shipyardID].z
-		-- Unit position
+		-- Unit position and direction
 		local ux, uy, uz = Spring.GetUnitPosition(unitID)	
+		local udx, udy, udz = Spring.GetUnitDirection(unitID)	
+		-- Unit Velocity
+		-- local vx, vy, vz = Spring.GetUnitVelocity(unitID)
+		-- local velratio = 1
 		-- Collision direction (unit to shipyard)
 		local dx, dy, dz = sx - ux, sy - uy, sz - uz
 		local ndx, ndy, ndz = Norm3D(dx, dy, dz)
-		-- Move unit
-		Spring.MoveCtrl.Enable(unitID)
-		Spring.MoveCtrl.SetPosition(unitID, ux - 5*ndx, uy - 5*ndy, uz - 5*ndz)
-		Spring.MoveCtrl.Disable(unitID)
+		-- Stop and Move unit
+		-- Spring.MoveCtrl.Enable(unitID)
+		-- Spring.MoveCtrl.SetPosition(unitID, ux - 5*ndx, uy - 5*ndy, uz - 5*ndz)
+		-- Spring.MoveCtrl.Disable(unitID)
+		-- Change its direction (progressively)
+		Spring.SetUnitDirection(unitID, udx*0.8 -ndx * 0.2, udy*0.8 -ndy * 0.2, udz*0.8 -ndz * 0.2)
+		
+		--Check UnitCMDQueue to stop when can't reach target
+		-- local cmdQueue = Spring.GetUnitCommands(unitID, 1)
+		-- if cmdQueue[1] and cmdQueue[1].id == CMD.MOVE then
+			-- local cmdParams = {cmdQueue[1].params}
+			-- if #cmdParams == 3 then
+				-- if Spring.TestMoveOrder(Spring.GetUnitDefID[unitID], cmdParams[1], cmdParams[2], cmdParams[3]) == false then
+				-- Erase move order
+				-- end
+			-- elseif #cmdParams == 6 then
+				-- if Spring.TestMoveOrder(Spring.GetUnitDefID[unitID], cmdParams[4], cmdParams[5], cmdParams[6]) == false then
+				-- Erase move order
+				-- end
+			-- end
+		-- end
 end
 end
 end	
