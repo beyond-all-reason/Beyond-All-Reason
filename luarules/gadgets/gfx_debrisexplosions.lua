e7ac8a7838a98e2d79eae35a4709d6e490ec7c4a
Fx-Doo <hedibertille93@gmail.com>
2017-12-09 14:58:52 +0100

    Debris explosions gadget-- spawns ceg when piece projectile destroyed

---
+++ luarules/gadgets/gfx_debrisexplosions.lua
@@ -0,0 +1,50 @@
+function gadget:GetInfo()
+  return {
+    name      = "Debris Explosions",
+    desc      = "Spawns CEG for debris explosions",
+    author    = "Doo",
+    date      = "Dec 9th 2017",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = true  --  loaded by default?
+  }
+end
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+if gadgetHandler:IsSyncedCode() then
+
+--CEG setup: For more spawn chance of a certain ceg, copy it multiple times in the table...
+cegtospawn = {
+"genericshellexplosion-small",
+"genericshellexplosion-small",
+"genericshellexplosion-tiny",
+"genericshellexplosion-tiny",
+"genericshellexplosion-tiny",
+"genericshellexplosion-tiny",
+"genericshellexplosion-tiny",
+"genericshellexplosion-tiny",
+"genericshellexplosion-tiny",
+"genericshellexplosion-tiny",
+"genericshellexplosion-medium",
+}
+
+function gadget:Initialize()
+	Script.SetWatchWeapon(-1, true)
+end
+
+function gadget:ProjectileDestroyed(proID) -- Catch debris explosions, get position, pick random ceg, spawn it at position.
+	local weapon, piece = Spring.GetProjectileType(proID)
+	if piece == true then
+	-- Spring.Echo("explosion")
+	local px, py, pz = Spring.GetProjectilePosition(proID)
+	local i = math.random(1,#cegtospawn)
+	if cegtospawn[i] and px and py and pz then
+	Spring.SpawnCEG(cegtospawn[i], px, py, pz, 0, 1, 0, 50, 0)
+	end
+	end
+end
+
+end	
