6d7f2714f2204a7a29cda524087b116eecd527c6
Fx-Doo <hedibertille93@gmail.com>
2017-12-28 12:41:20 +0100

    Implementation of common mines_lus.lua, handling detonation of mines.

---
+++ scripts/mines_lus.lua
@@ -0,0 +1,50 @@
+
+local base = piece("base")
+local GetUnitPosition 	= Spring.GetUnitPosition
+local unitDefID = Spring.GetUnitDefID(unitID)
+local triggerRange = tonumber(UnitDefs[unitDefID].customParams and UnitDefs[unitDefID].customParams.detonaterange) or 64
+
+-- Author: Doo
+-- Requires customParams.detonaterange in unitDefs or 64 elmos range will be used
+-- Possible enhancements: Use GetUnitsInCylinder(or sphere) of detonaterange and check for any restriction on target units
+
+function GetClosestEnemyDistance()
+	targetID = Spring.GetUnitNearestEnemy(unitID, triggerRange)
+	if targetID then
+	local tx,ty,tz = Spring.GetUnitPosition(targetID)
+	local dis = distance(ux,uy,uz,tx,ty,tz)
+	return dis
+	else
+	return math.huge
+	end
+end
+
+function distance(x1,y1,z1,x2,y2,z2)
+	local dist = math.sqrt((x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2)
+	return dist
+end
+
+function script.Create()
+	ux,uy,uz = Spring.GetUnitPosition(unitID)
+	StartThread(EnemyDetect)
+end
+
+function EnemyDetect()
+	while true do
+		if Spring.GetUnitStates(unitID) and Spring.GetUnitStates(unitID).firestate > 0 and GetClosestEnemyDistance() <= triggerRange then
+			StartThread(Detonate)
+			break
+		else
+		Sleep(1)
+		end
+	end
+end
+
+function Detonate()
+	Sleep(500)
+	Spring.DestroyUnit(unitID, false, false)
+end
+
+function script.Killed()
+return 3
+end
