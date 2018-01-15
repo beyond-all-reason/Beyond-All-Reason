cc40b73f71c1f9b0a4690c0f4757acb827d7c512
Fx-Doo <hedibertille93@gmail.com>
2018-01-15 14:34:15 +0100

    Unbalanced Commanders: Use one stats.lua file only for all comm related upgrades
    Just a maintainability update: all stats upgrade packed in one file
    only.

--- scripts/armcom_lus.lua
+++ scripts/armcom_lus.lua
@@ -23,33 +23,8 @@ local rthigh5, lthigh5, rleg5, lleg5 = piece("rthigh5", "lthigh5", "rleg5", "lle
 lvl1hides = {}
 lvl1hides = {rthigh5, lthigh5, rleg5, lleg5, biggun4, rbigflash4, nanolath3, nanospray3, lfirept3, torso2}
 
---Stats Tables:
---Weapon: Laser
-Range = {300,350,375,400,430,475,530,575,630,700,750}
-AOE = {12,12,12,16,16,16,24,24,24,32,32}
-ReloadTime = {0.4,0.4,0.4,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2}
-Damages = {75,75,125,125,125,150,150,200,200,250,250}
---Weapon:SeaLaser
-Range2 = {300,350,375,400,430,475,530,575,630,700,750}
-AOE2 = {12,12,12,16,16,16,24,24,24,32,32}
-ReloadTime2 = {1,1,1,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5}
-Damages21 = {75*2,75*2,125*2,125*2,125*2,150*2,150*2,200*2,200*2,250*2,250*2}
-Damages22 = {0.5,0.5,0.5,0.75,0.75,0.75,1,1,1,1,1}
---Weapon:Dgun
-ReloadTime3 = {0.9,0.9,0.8,0.8,0.7,0.7,0.6,0.6,0.6,0.5,0.4}
---Speeds
-BuildSpeed = {300,325,350,400,450,500,550,600,700,800,900}
-MoveSpeed = {1.25,1.275,1.3,1.35,1.40,1.45,1.5,1.5,1.5,1.5,1.5}
---Armor/hp
-ShieldPower = {1000,1250,1500,2000,2500,3000,4000,4000,4000,4000,4000}
-HealOnLevelUp = {0,250,250,500,500,750,750,1000,1000,1000,1000}
-DamageMultiplierNoDgun = {1,1,1,1,1,1,0.95,0.9,0.85,0.8,0.75}
---Vision
-LOS = {450,500,550,600,625,650,675,700,725,750,800}
-Sonar = {450,450,500,500,550,550,600,600,650,650,700}
-Radar = {700,700,800,1000,1200,1400,1600,2000,2400,2800,3200}
-
-repairRange = 300
+--Stats Table:
+VFS.Include("units/unba/stats.lua")
 
 local SIG_WALK = 2
 local PlaySoundFile 	= Spring.PlaySoundFile
