b6e57e3e6cec4d72856e0a33516a547c5ed8f0c0
Fx-Doo <hedibertille93@gmail.com>
2018-01-24 19:40:54 +0100

    gui_units_stats: show accurate weapon stats for unba commanders

--- luaui/Widgets_BA/gui_unit_stats.lua
+++ luaui/Widgets_BA/gui_unit_stats.lua
@@ -446,6 +446,11 @@ function widget:DrawScreen()
 			weaponNums[#wepsCompact] = i
 		end
 	end
+	
+	if unbacom then
+		weaponNums = { level, level + 11, 30}
+	end
+	
 	local selfDWeaponID = WeaponDefNames[uDef.selfDExplosion].id
 	local deathWeaponID = WeaponDefNames[uDef.deathExplosion].id
 	local selfDWeaponIndex
@@ -495,6 +500,14 @@ function widget:DrawScreen()
 			local accuracyBonus = accuracy ~= 0 and (uWep.accuracy/accuracy-1) or 0
 			local moveErrorBonus = moveError ~= 0 and (uWep.targetMoveError/moveError-1) or 0
 			local range = spGetUnitWeaponState(uID,weaponNums[i] or -1,"range") or uWep.range
+			if unbacom then
+				if i == 1 then
+				range = Range[level]
+				elseif i == 2 then
+				range = Range2[level]
+				end
+			end
+			local range = range
 			local rangeBonus = range ~= 0 and (range/uWep.range-1) or 0
 			if uExp ~= 0 then
 				DrawText("Exp:", format("+%d%% accuracy, +%d%% aim, +%d%% firerate, +%d%% range", accuracyBonus*100, moveErrorBonus*100, reloadBonus*100, rangeBonus*100 ))
