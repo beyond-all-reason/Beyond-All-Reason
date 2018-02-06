641cc83dead77b9586450130eae00401d94e9561
Floris <floris@ruwhof.net>
2018-02-06 01:11:02 +0100

    ecostats: bugfix

--- luaui/Widgets_BA/gui_ecostats.lua
+++ luaui/Widgets_BA/gui_ecostats.lua
@@ -956,7 +956,7 @@ end
 local reclaimerUnitDefs = {}
 for udefID,def in ipairs(UnitDefs) do
 	if def.isBuilder and not def.isFactory then
-		reclaimerUnitDefs[udefID] = def.metalMake
+		reclaimerUnitDefs[udefID] = {def.metalMake,def.energyMake}
 	end
 end
 
@@ -974,10 +974,10 @@ function getTeamProduction(teamID)
 				for i, unitID in ipairs(units) do
 					local metalMake,metalUse,energyMake,energyUse = Spring.GetUnitResources(unitID)
 					if metalMake > 0 then
-						totalMetalReclaim = totalMetalReclaim + (metalMake-reclaimerUnitDefs[unitDefID])
+						totalMetalReclaim = totalMetalReclaim + (metalMake-reclaimerUnitDefs[unitDefID][1])
 					end
 					if energyMake > 0 then
-						totalEnergyReclaim = totalEnergyReclaim + (energyMake-reclaimerUnitDefs[unitDefID])
+						totalEnergyReclaim = totalEnergyReclaim + (energyMake-reclaimerUnitDefs[unitDefID][2])
 					end
 				end
 			end
