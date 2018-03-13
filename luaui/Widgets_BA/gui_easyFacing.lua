74bc362cb997ee001d16a7e9ed88945db1d05174
Fx-Doo <hedibertille93@gmail.com>
2018-03-13 16:08:25 +0100

    EasyFacing: if not shift modifier then set facing with lmb, else set it with lmb+mmb
    --> Allows use of lmb to set build facing when possible
    --> Needs lmb + mmb if shift is pressed

--- luaui/Widgets_BA/gui_easyFacing.lua
+++ luaui/Widgets_BA/gui_easyFacing.lua
@@ -206,7 +206,7 @@ function manipulateFacing()
 	local udef = udefTab[unitDefID]
 
 	--if (lmb and (mmb or (not shift or (udef ~= nil and udef["isFactory"])))) then
-	if (lmb and mmb) then
+	if lmb and ((not shift) or (mmb)) then
 		--in
         if not inDrag then
             mouseDeltaX = 0
