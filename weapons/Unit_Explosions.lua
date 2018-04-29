809b9336605267b9573da6a4a151009b5576c3f7
Floris <floris@ruwhof.net>
2018-04-29 23:30:29 +0200

    changed explosion fx for armamex

--- weapons/Unit_Explosions.lua
+++ weapons/Unit_Explosions.lua
@@ -16,6 +16,31 @@ unitDeaths = {
         },
     },
 
+    empblast = {    -- for armamex
+        areaofeffect = 400,
+        commandfire = 1,
+        craterboost = 0,
+        cratermult = 0,
+        edgeeffectiveness = 0.75,
+        explosiongenerator = "custom:genericshellexplosion-huge-lightning",
+        firestarter = 90,
+        impulseboost = 0,
+        impulsefactor = 0,
+        model = "bomb",
+        name = "EMPboom",
+        paralyzer = 1,
+        paralyzetime = 15,
+        range = 600,
+        reloadtime = 0.30000001192093,
+        soundhit = "EMGPULS1",
+        soundstart = "bombrel",
+        tolerance = 7000,
+        turret = 1,
+        damage = {
+            default = 4000,
+        },
+    },
+
     pyro = {
         weaponType = "Cannon",
         areaofeffect = 64,
