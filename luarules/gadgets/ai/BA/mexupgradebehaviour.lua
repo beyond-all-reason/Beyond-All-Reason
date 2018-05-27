ed619c00c6733a3cd9e775fdb633bd3b541bb97d
Fx-Doo <hedibertille93@gmail.com>
2018-05-27 22:45:56 +0200

    Make mexupgraderbehaviour properly queue mex upgrades and resume to other tasks when no mexes to upgrade

--- luarules/gadgets/ai/BA/mexupgradebehaviour.lua
+++ luarules/gadgets/ai/BA/mexupgradebehaviour.lua
@@ -12,6 +12,7 @@ function MexUpgradeBehavior:Init()
 	UDN = UnitDefNames
 	mc, ms, mp, mi, me = Spring.GetTeamResources(self.teamid, "metal")
 	self.active = false
+	self.unit:Internal():ExecuteCustomCommand(CMD_AUTOMEX, {1}, {})
 end
 
 function MexUpgradeBehavior:OwnerBuilt()
@@ -21,11 +22,8 @@ end
 function MexUpgradeBehavior:Update()
 	local unit = self.unit:Internal()
 	local countT1mex = UDC(self.teamid, UDN.cormex.id) + UDC(self.teamid, UDN.corexp.id) + UDC(self.teamid, UDN.armmex.id) + UDC(self.teamid, UDN.armamex.id)
-	if countT1mex == 0 then
-		unit:ExecuteCustomCommand(CMD_AUTOMEX, {1}, {})
+	if countT1mex == 0 an not (Spring.GetUnitCommands(unit.id, 1)) then
 		self.unit:ElectBehaviour()
-	else
-		unit:ExecuteCustomCommand(CMD_AUTOMEX, {2}, {})
 	end
 end
 
