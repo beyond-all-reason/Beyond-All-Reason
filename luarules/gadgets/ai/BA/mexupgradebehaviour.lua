87b572cccde584ddf1199d7a65c7d6436809192a
Damgam <c_2_damiano@op.pl>
2018-05-28 08:03:56 +0200

    fixed DAI MexUpgradeBehaviour (again)

--- luarules/gadgets/ai/BA/mexupgradebehaviour.lua
+++ luarules/gadgets/ai/BA/mexupgradebehaviour.lua
@@ -6,24 +6,24 @@ function MexUpgradeBehavior:Name()
 end
 
 function MexUpgradeBehavior:Init()
-	self.teamid = self.unit:Internal():Team()
+	--self.teamid = self.unit:Internal():Team()
 	CMD_AUTOMEX = 31143 
 	UDC = Spring.GetTeamUnitDefCount
 	UDN = UnitDefNames
-	mc, ms, mp, mi, me = Spring.GetTeamResources(self.teamid, "metal")
+	mc, ms, mp, mi, me = Spring.GetTeamResources(self.ai.id, "metal")
 	self.active = false
-	self.unit:Internal():ExecuteCustomCommand(CMD_AUTOMEX, {1}, {})
 end
 
 function MexUpgradeBehavior:OwnerBuilt()
-
+	self.unit:Internal():ExecuteCustomCommand(CMD_AUTOMEX, {1}, {})
 end
 
 function MexUpgradeBehavior:Update()
 	local unit = self.unit:Internal()
-	local countT1mex = UDC(self.teamid, UDN.cormex.id) + UDC(self.teamid, UDN.corexp.id) + UDC(self.teamid, UDN.armmex.id)
+	local countT1mex = UDC(self.ai.id, UDN.cormex.id) + UDC(self.ai.id, UDN.corexp.id) + UDC(self.ai.id, UDN.armmex.id)
 	local curQueue = Spring.GetUnitCommands(unit.id, 1)
-	if countT1mex == 0 and not (#curQueue > 0) then
+	if countT1mex < 2 and not (#curQueue > 0) then
+		self.unit:Internal():ExecuteCustomCommand(CMD_AUTOMEX, {0}, {})
 		self.unit:ElectBehaviour()
 	end
 end
@@ -41,9 +41,8 @@ function MexUpgradeBehavior:Deactivate()
 end
 
 function MexUpgradeBehavior:Priority()
-	local countT1mex = UDC(self.teamid, UDN.cormex.id) + UDC(self.teamid, UDN.corexp.id) + UDC(self.teamid, UDN.armmex.id)
-	if countT1mex == 0 then
-		self.unit:Internal():ExecuteCustomCommand(CMD_AUTOMEX, {0}, {})
+	local countT1mex = UDC(self.ai.id, UDN.cormex.id) + UDC(self.ai.id, UDN.corexp.id) + UDC(self.ai.id, UDN.armmex.id)
+	if countT1mex < 2 then
 		return 0
 	else
 		return 100
