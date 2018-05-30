bcb746c8169ccfcf5992b2277f0fb0350c47ac42
Fx-Doo <hedibertille93@gmail.com>
2018-05-30 22:29:33 +0200

    Map_waterlevel use flat waterlevel change value and better featuredelete script

--- luarules/gadgets/map_waterlevel.lua
+++ luarules/gadgets/map_waterlevel.lua
@@ -9,7 +9,7 @@ function gadget:GetInfo()
 		author    = "Doo",
 		date      = "Nov 2017", 
 		license   = "GPL",
-		layer     = -1,	--higher layer is loaded last
+		layer     = math.huge,	--higher layer is loaded last
 		enabled   = true,  
 	}
 end
@@ -21,43 +21,24 @@ if (gadgetHandler:IsSyncedCode()) then
 function gadget:Initialize()
 	if Spring.GetModOptions() and Spring.GetModOptions().map_waterlevel and Spring.GetModOptions().map_waterlevel ~= "0" then
 		Spring.SetMapRenderingParams({ voidWater = false})
-		local miny, maxy = Spring.GetGroundExtremes()
-				if Spring.GetModOptions() and Spring.GetModOptions().map_waterlevel == "-1" then
-					Spring.AdjustHeightMap(0, 0, Game.mapSizeX, Game.mapSizeZ, -miny+100)
-					Spring.AdjustSmoothMesh(0, 0, Game.mapSizeX, Game.mapSizeZ, -miny+100)
-				else
-					local waterlevel1 = ((Spring.GetModOptions() and tonumber(Spring.GetModOptions().map_waterlevel))/100)
-					if miny >= 0 then						
-						local delta = maxy-miny
-						waterlevel = miny + (math.abs(delta*waterlevel1))
-						if waterlevel1 == 1 then
-						Spring.AdjustHeightMap(0, 0, Game.mapSizeX, Game.mapSizeZ, -waterlevel-50)
-						Spring.AdjustSmoothMesh(0, 0, Game.mapSizeX, Game.mapSizeZ, -waterlevel-50)
-						else
-						Spring.AdjustHeightMap(0, 0, Game.mapSizeX, Game.mapSizeZ, -waterlevel)
-						Spring.AdjustSmoothMesh(0, 0, Game.mapSizeX, Game.mapSizeZ, -waterlevel)
-						end
+		local waterlevel = ((Spring.GetModOptions() and tonumber(Spring.GetModOptions().map_waterlevel)))
+		Spring.AdjustHeightMap(0, 0, Game.mapSizeX, Game.mapSizeZ, -waterlevel)
+		Spring.AdjustSmoothMesh(0, 0, Game.mapSizeX, Game.mapSizeZ, -waterlevel)
+	end
+end
 
-					elseif miny < 0 then
-						Spring.AdjustHeightMap(0, 0, Game.mapSizeX, Game.mapSizeZ, -miny)
-						Spring.AdjustSmoothMesh(0, 0, Game.mapSizeX, Game.mapSizeZ, -miny)
-						local delta = maxy - miny
-						waterlevel = 0 + (math.abs(delta*waterlevel1))
-						Spring.AdjustHeightMap(0, 0, Game.mapSizeX, Game.mapSizeZ, -waterlevel)
-						Spring.AdjustSmoothMesh(0, 0, Game.mapSizeX, Game.mapSizeZ, -waterlevel)
-				end
-			end
-		featuretable = Spring.GetAllFeatures()
+function gadget:GamePreload()
+			featuretable = Spring.GetAllFeatures()
 		for i = 1,#featuretable do
 		featureDefID = Spring.GetFeatureDefID(featuretable[i])
 		x,_,z = Spring.GetFeaturePosition(featuretable[i])
 				Spring.DestroyFeature(featuretable[i])
-				if Spring.GetGroundHeight(x,z) >= 0 or FeatureDefs[featureDefID].geoThermal == true or FeatureDefs[featureDefID].metal > 0 then -- Keep features (> 0 height) or (geovents) or (contains metal)
+				if (Spring.GetGroundHeight(x,z) >= 0) or (FeatureDefs[featureDefID].geoThermal == true) or (FeatureDefs[featureDefID].metal > 0) then -- Keep features (> 0 height) or (geovents) or (contains metal)
 				Spring.CreateFeature(featureDefID, x, Spring.GetGroundHeight(x,z), z)
 				end
 		end
-	end
 end
+
 end
 
 
