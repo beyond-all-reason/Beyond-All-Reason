2ddb625223779d960c5e45745e338e3f8c0eb34d
Damgam <c_2_damiano@op.pl>
2018-06-02 12:09:05 +0200

    simplified and fixed last change to picking factory logic

--- luarules/gadgets/ai/BA/taskqueues.lua
+++ luarules/gadgets/ai/BA/taskqueues.lua
@@ -101,19 +101,21 @@ function CorRandomLab()
 	local countBasicFacs = UDC(ai.id, UDN.corvp.id) + UDC(ai.id, UDN.corlab.id) + UDC(ai.id, UDN.corap.id) + UDC(ai.id, UDN.corhp.id)
 	
 	if countBasicFacs + countAdvFacs < Spring.GetGameSeconds() / 300 + 1 and ms + Spring.GetGameSeconds() > 800 then
-		if UDC(ai.id, UDN.corvp.id) < UDC(ai.id, UDN.corlab.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		if UDC(ai.id, UDN.corlab.id) < 1 then
+			return "corlab"
+		elseif UDC(ai.id, UDN.corvp.id) < UDC(ai.id, UDN.corlab.id) then
 			return "corvp"
-		elseif UDC(ai.id, UDN.corap.id) < UDC(ai.id, UDN.corvp.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.corap.id) < UDC(ai.id, UDN.corvp.id) then
 			return "corap"
-		elseif UDC(ai.id, UDN.corhp.id) < UDC(ai.id, UDN.corap.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.corhp.id) < UDC(ai.id, UDN.corap.id) then
 			return "corhp"
-		elseif UDC(ai.id, UDN.coralab.id) < UDC(ai.id, UDN.corhp.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.coralab.id) < UDC(ai.id, UDN.corhp.id) then
 			return "coralab"
-		elseif UDC(ai.id, UDN.coravp.id) < UDC(ai.id, UDN.coralab.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.coravp.id) < UDC(ai.id, UDN.coralab.id) then
 			return "coravp"
-		elseif UDC(ai.id, UDN.coraap.id) < UDC(ai.id, UDN.coravp.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.coraap.id) < UDC(ai.id, UDN.coravp.id) then
 			return "coraap"
-		elseif UDC(ai.id, UDN.corgant.id) < UDC(ai.id, UDN.coraap.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.corgant.id) < UDC(ai.id, UDN.coraap.id) then
 			return "corgant"
 		else
 			return "corlab"
@@ -356,19 +358,21 @@ function ArmRandomLab()
 	local countBasicFacs = UDC(ai.id, UDN.armvp.id) + UDC(ai.id, UDN.armlab.id) + UDC(ai.id, UDN.armap.id) + UDC(ai.id, UDN.armhp.id)
 	
 	if countBasicFacs + countAdvFacs < Spring.GetGameSeconds() / 300 + 1 and ms + Spring.GetGameSeconds() > 800 then
-		if UDC(ai.id, UDN.armvp.id) < UDC(ai.id, UDN.armlab.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		if UDC(ai.id, UDN.corlab.id) < 1 then
+			return "armlab"
+		elseif UDC(ai.id, UDN.armvp.id) < UDC(ai.id, UDN.armlab.id) then
 			return "armvp"
-		elseif UDC(ai.id, UDN.armap.id) < UDC(ai.id, UDN.armvp.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.armap.id) < UDC(ai.id, UDN.armvp.id) then
 			return "armap"
-		elseif UDC(ai.id, UDN.armhp.id) < UDC(ai.id, UDN.armap.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.armhp.id) < UDC(ai.id, UDN.armap.id) then
 			return "armhp"
-		elseif UDC(ai.id, UDN.armalab.id) < UDC(ai.id, UDN.armhp.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.armalab.id) < UDC(ai.id, UDN.armhp.id) then
 			return "armalab"
-		elseif UDC(ai.id, UDN.armavp.id) < UDC(ai.id, UDN.armalab.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.armavp.id) < UDC(ai.id, UDN.armalab.id) then
 			return "armavp"
-		elseif UDC(ai.id, UDN.armaap.id) < UDC(ai.id, UDN.armavp.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.armaap.id) < UDC(ai.id, UDN.armavp.id) then
 			return "armaap"
-		elseif UDC(ai.id, UDN.armshltx.id) < UDC(ai.id, UDN.armaap.id) and UDC(ai.id, UDN.corlab.id) > 0 then
+		elseif UDC(ai.id, UDN.armshltx.id) < UDC(ai.id, UDN.armaap.id) then
 			return "armshltx"
 		else
 			return "armlab"
