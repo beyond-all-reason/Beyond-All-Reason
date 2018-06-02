587aa941323ec2c728dfa064586265f1453d068e
Damgam <c_2_damiano@op.pl>
2018-06-02 10:14:52 +0200

    AI shouldn't use all T2 constructors to upgrade mexes anymore

--- luarules/gadgets/ai/BA/mexupgradebehaviour.lua
+++ luarules/gadgets/ai/BA/mexupgradebehaviour.lua
@@ -10,20 +10,22 @@ function MexUpgradeBehavior:Init()
 	CMD_AUTOMEX = 31143 
 	UDC = Spring.GetTeamUnitDefCount
 	UDN = UnitDefNames
-	mc, ms, mp, mi, me = Spring.GetTeamResources(self.ai.id, "metal")
-	ec, es, ep, ei, ee = Spring.GetTeamResources(self.ai.id, "energy")
 	self.active = false
 end
 
 function MexUpgradeBehavior:OwnerBuilt()
-	self.unit:Internal():ExecuteCustomCommand(CMD_AUTOMEX, {1}, {})
+	local mc, ms, mp, mi, me = Spring.GetTeamResources(self.ai.id, "metal")
+	local ec, es, ep, ei, ee = Spring.GetTeamResources(self.ai.id, "energy")
+	if not (ec < es*0.20 or mc > ms*0.20) then
+		self.unit:Internal():ExecuteCustomCommand(CMD_AUTOMEX, {1}, {})
+	end
 end
 
 function MexUpgradeBehavior:Update()
 	local unit = self.unit:Internal()
 	local countT1mex = UDC(self.ai.id, UDN.cormex.id) + UDC(self.ai.id, UDN.corexp.id) + UDC(self.ai.id, UDN.armmex.id)
 	local curQueue = Spring.GetUnitCommands(unit.id, 1)
-	if (countT1mex < 2 and not (#curQueue > 0)) or (ec < es*0.20 and mc > ms*0.20) then
+	if countT1mex < 2 and not (#curQueue > 0) then
 		self.unit:Internal():ExecuteCustomCommand(CMD_AUTOMEX, {0}, {})
 		self.unit:ElectBehaviour()
 	end
