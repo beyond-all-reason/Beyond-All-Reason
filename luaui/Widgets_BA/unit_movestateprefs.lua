dbc49ae1c69bc7aacc704d3b6b58c3ad6e76b59d
Fx-Doo <hedibertille93@gmail.com>
2018-06-03 03:59:35 +0200

    holdfire prefs => Only register for owned units that received the command

--- luaui/Widgets_BA/unit_movestateprefs.lua
+++ luaui/Widgets_BA/unit_movestateprefs.lua
@@ -50,7 +50,7 @@ end
 
 function widget:UnitCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOpts, cmdTag)
 	local alt, ctrl, meta, shift = Spring.GetModKeyState()
-	if ctrl and cmdID == CMD.MOVE_STATE then
+	if ctrl and cmdID == CMD.MOVE_STATE and unitTeam == Spring.GetMyTeamID() then
 		if cmdParams[1] == 0 then --holdpos
 			unitSet[UnitDefs[unitDefID].name] = 0
 			Spring.Echo("Preference set for: "..UnitDefs[unitDefID].name.." = 0.")
