0576698f4ced8cc472836bc3a2100649457ea979
Damgam <c_2_damiano@op.pl>
2018-06-04 16:38:39 +0200

    fixed 'corcom has empty taskqueue'

---
+++ luarules/gadgets/ai/BA/modules.lua
@@ -0,0 +1,8 @@
+shard_include( "spothandler" )
+shard_include( "controlpointhandler" )
+shard_include( "unithandler" )
+shard_include( "attackhandler" )
+shard_include( "placementhandler" )
+shard_include( "sleep" )
+
+modules = { UnitHandler, AttackHandler, ControlPointHandler, MetalSpotHandler, PlacementHandler, Sleep }
