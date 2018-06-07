cb54a419215ca23c39e6dccbc865d390b717a602
Fx-Doo <hedibertille93@gmail.com>
2018-06-07 14:13:17 +0200

    line 107 playerID => pID

--- luarules/gadgets/game_initial_spawn.lua
+++ luarules/gadgets/game_initial_spawn.lua
@@ -104,7 +104,7 @@ function isPlayerNewbie(pID)
 	local customtable
 	local name,_,isSpec,tID,_,_,_,_,pRank = Spring.GetPlayerInfo(pID) 
 	playerRank = tonumber(pRank) or 0
-	if select(11,Spring.GetPlayerInfo(playerID)) then   -- changed to 11th in engine 104.0.1.547
+	if select(11,Spring.GetPlayerInfo(pID)) then   -- changed to 11th in engine 104.0.1.547
 		customtable = select(11,Spring.GetPlayerInfo(pID)) or {} -- player custom table
 	else
 		customtable = select(10,Spring.GetPlayerInfo(pID)) or {}
