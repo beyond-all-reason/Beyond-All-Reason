714d59e5d4c60fef0191e07f68d1c5f3f6820596
Floris <floris@ruwhof.net>
2018-06-08 02:19:08 +0200

    ai namer wont give name to chickens

--- luarules/gadgets/ai_namer.lua
+++ luarules/gadgets/ai_namer.lua
@@ -34,7 +34,23 @@ if gadgetHandler:IsSyncedCode() then
 		local t = Spring.GetTeamList()
 		for _,teamID in ipairs(t) do
 			if select(4,Spring.GetTeamInfo(teamID)) then	-- is AI?
-				Spring.SetGameRulesParam('ainame_'..teamID, getName(teamID))
+				local chickensEnabled = false
+				local luaAI = Spring.GetTeamLuaAI(teamID)
+				if string.find(luaAI, "Chicken:") then
+					if luaAI == "Chicken: Very Easy" or
+							luaAI == "Chicken: Easy" or
+							luaAI == "Chicken: Normal" or
+							luaAI == "Chicken: Hard" or
+							luaAI == "Chicken: Very Hard" or
+							luaAI == "Chicken: Epic!" or
+							luaAI == "Chicken: Custom" or
+							luaAI == "Chicken: Survival" then
+						chickensEnabled = true
+					end
+				end
+				if not chickensEnabled then
+					Spring.SetGameRulesParam('ainame_'..teamID, getName(teamID))
+				end
 			end
 		end
 		gadgetHandler:RemoveGadget(self)
