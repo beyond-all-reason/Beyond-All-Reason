fe2c62adb7b86928f85916421a579e4b9efc180c
Fx-Doo <hedibertille93@gmail.com>
2018-06-15 02:31:32 +0200

    applied the 3% speed nerf (for real this time)

--- units/CoreVehicles/corgator.lua
+++ units/CoreVehicles/corgator.lua
@@ -23,7 +23,7 @@ return {
 		leavetracks = true,
 		maxdamage = 727,
 		maxslope = 10,
-		maxvelocity = 3.08,
+		maxvelocity = 2.99,
 		maxwaterdepth = 12,
 		movementclass = "TANK2",
 		name = "Instigator",
