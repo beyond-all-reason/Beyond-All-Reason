e785bd18f64db340050458a7be74397c699cb13b
Floris <floris@ruwhof.net>
2018-08-04 16:50:01 +0200

    updated modinfo file

--- modinfo.lua
+++ modinfo.lua
@@ -1,11 +1,12 @@
 return {
-  name='Balanced Annihilation',
-  description='Balanced Annihilation',
-  shortname='BA',
-  version='$VERSION',
-  mutator='Official',
-  game='Balanced Annihilation',
-  shortGame='BA',
-  modtype=1,
-  engine = "103.0.1-1222-g37dc534 develop",
+  name = 'Balanced Annihilation',
+  description = 'Balanced Annihilation',
+  shortname = 'BA',
+  version = '$VERSION',
+  mutator = 'Official',
+  game = 'Balanced Annihilation',
+  shortGame = 'BA',
+  modtype = 1,
+  url =	"https://www.balancedannihilation.com",
+  engine = "104.0.1-567-gc484c10",    -- for chobby
 }
