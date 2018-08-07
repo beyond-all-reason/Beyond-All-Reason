df791c2d0cf7d937a7bbd1b59e197cf79e329ab2
Fx-Doo <hedibertille93@gmail.com>
2018-08-07 23:21:40 +0200

    Attempt to optimize attackerBehaviour

--- luarules/gadgets/ai/BA/attackerbehaviour.lua
+++ luarules/gadgets/ai/BA/attackerbehaviour.lua
@@ -16,6 +16,7 @@ function AttackerBehaviour:Init()
 	--self.game:AddMarker({ x = startPosx, y = startPosy, z = startPosz }, "my start position")
 	CMD.MOVE_STATE = 50
 	CMD.FIRE_STATE = 45
+	self.ai.commpos = {x = 0, y = 0, z = 0}
 end
 
 local function Distance(x1,z1, x2,z2)
@@ -26,7 +27,12 @@ local function Distance(x1,z1, x2,z2)
 end
 
 function AttackerBehaviour:Update()
-
+	local x,y,z
+	local comms = Spring.GetTeamUnitsByDefs(self.ai.id, {UnitDefNames.armcom.id, UnitDefNames.corcom.id})
+	if comms[1] then
+		x,y,z = Spring.GetUnitPosition(comms[1])
+		self.ai.commpos = {x = x, y = y, z = z}
+	end
 end
 
 function AttackerBehaviour:OwnerBuilt()
@@ -51,149 +57,99 @@ function AttackerBehaviour:AttackCell(type)
 	local attacker = (type == "attacker")
 	local unit = self.unit:Internal()
 	local unitID = unit.id
-	-- Spring.Echo(unitID)
-	local myRange = Spring.GetUnitMaxRange(unitID)
-	local closestUnit = Spring.GetUnitNearestEnemy(unitID, myRange)
-	local allyTeamID = self.ai.allyId
-	local currenthealth = unit:GetHealth()
-	local maxhealth = unit:GetMaxHealth()
-	local startPosx, startPosy, startPosz = Spring.GetTeamStartPosition(ai.id)
-	local startBoxMinX, startBoxMinZ, startBoxMaxX, startBoxMaxZ = Spring.GetAllyTeamStartBox(allyTeamID)
 	local utype = self.game:GetTypeByName(unit:Name())
-	local nearestUnitRangeCheck = ((not attacker) and (400 + myRange)) or (5*myRange)
-	-- Skirmishing
-	if (not utype:CanFly() == true) and unitID % 30 == Spring.GetGameFrame() % 30 then
-		if closestUnit and (Spring.IsUnitInLos(closestUnit, allyTeamID)) and (currenthealth >= maxhealth*0.75 or currenthealth > 3000) then
-			local enemyRange = Spring.GetUnitMaxRange(closestUnit)
-			if myRange*0.9 > enemyRange and enemyRange > 50 and enemyRange ~= nil then
-				local moverandom = math.random(0,2)
-				if moverandom == 0 then
-					self.unit:Internal():ExecuteCustomCommand(CMD.MOVE_STATE, { 0 }, {})
-				elseif moverandom == 1 then
-					self.unit:Internal():ExecuteCustomCommand(CMD.MOVE_STATE, { 1 }, {})
-				else
-					self.unit:Internal():ExecuteCustomCommand(CMD.MOVE_STATE, { 2 }, {})
+	local nearestUnit = Spring.GetUnitNearestEnemy(unitID, _, false)
+	local distance = nearestUnit and Spring.GetUnitSeparation(unitID, nearestUnit)
+	local refreshRate = math.floor((distance or 500)/10)
+	local frame = Spring.GetGameFrame()
+	-- Spring.Echo(unitID)
+	if unitID%refreshRate == frame%refreshRate then
+		local myRange = Spring.GetUnitMaxRange(unitID)
+		local closestUnit = Spring.GetUnitNearestEnemy(unitID, myRange)
+		local allyTeamID = self.ai.allyId
+		local closestVisible = closestUnit and (Spring.IsUnitInLos(closestUnit, allyTeamID) or Spring.IsUnitInRadar(closestUnit, allyTeamID))
+		local currenthealth = unit:GetHealth()
+		local maxhealth = unit:GetMaxHealth()
+		-- Skirmishing
+		if (not utype:CanFly() == true) then
+			if closestUnit and (closestVisible) and (currenthealth >= maxhealth*0.75 or currenthealth > 3000) then
+				local enemyRange = Spring.GetUnitMaxRange(closestUnit)
+				if myRange >= enemyRange and enemyRange > 50 and enemyRange ~= nil then
+					local ex,ey,ez = Spring.GetUnitPosition(closestUnit)
+					local ux,uy,uz = Spring.GetUnitPosition(unitID)
+					local pointDis = Spring.GetUnitSeparation(unitID,closestUnit)
+					local dis = 120
+					local f = dis/pointDis
+					if (pointDis+dis > Spring.GetUnitMaxRange(unitID)) then
+					  f = (Spring.GetUnitMaxRange(unitID)-pointDis)/pointDis
+					end
+					local cx = ux+(ux-ex)*f
+					local cy = uy
+					local cz = uz+(uz-ez)*f
+					self.unit:Internal():ExecuteCustomCommand(CMD.MOVE, {cx, cy, cz}, {"ctrl"})
+					return
 				end
-				local ex,ey,ez = Spring.GetUnitPosition(closestUnit)
-				local ux,uy,uz = Spring.GetUnitPosition(unitID)
-				local pointDis = Spring.GetUnitSeparation(unitID,closestUnit)
-				local dis = 120
-				local f = dis/pointDis
-				if (pointDis+dis > Spring.GetUnitMaxRange(unitID)) then
-				  f = (Spring.GetUnitMaxRange(unitID)-pointDis)/pointDis
-				end
-				local cx = ux+(ux-ex)*f
-				local cy = uy
-				local cz = uz+(uz-ez)*f
-				self.unit:Internal():ExecuteCustomCommand(CMD.MOVE, {cx, cy, cz}, {"ctrl"})
 			end
 		end
-	end
-	
+		
+		local nearestUnitRangeCheck = ((not attacker) and (400 + myRange)) or (5*myRange)
 	-- Attacking
-	if unitID % 150 == Spring.GetGameFrame() % 150 then
-		local TeamID = ai.id
-		local allyTeamID = ai.allyId
-		local nearestUnit = Spring.GetUnitNearestEnemy(unitID, nearestUnitRangeCheck, false)
+		local TeamID = self.ai.id
+		local allyTeamID = self.ai.allyId
 		if nearestUnit and not (Spring.IsUnitInRadar(nearestUnit, allyTeamID) or Spring.IsUnitInLos(nearestUnit, allyTeamID)) then
 			nearestUnit = nil
 		end
-		--local nearestVisibleUnit = Spring.GetUnitNearestEnemy(unitID, _, true)
-		local ec, es = Spring.GetTeamResources(ai.id, "energy")
-		--attack
 		if (currenthealth >= maxhealth*0.75 or currenthealth > 3000)  then
-				local moverandom = math.random(0,2)
-				if moverandom == 0 then
-					self.unit:Internal():ExecuteCustomCommand(CMD.MOVE_STATE, { 0 }, {})
-				elseif moverandom == 1 then
-					self.unit:Internal():ExecuteCustomCommand(CMD.MOVE_STATE, { 1 }, {})
+			if nearestUnit == nil and type == "defender" then
+				local cms = self.ai.metalspothandler:ClosestFreeSpot(utype, self.unit:Internal():GetPosition())
+				if cms then
+					enemyposx, enemyposy, enemyposz = cms.x, cms.y, cms.z
 				else
-					self.unit:Internal():ExecuteCustomCommand(CMD.MOVE_STATE, { 2 }, {})
+					return
 				end
-				if nearestUnit == nil and type == "defender" then
-					local cms = self.ai.metalspothandler:ClosestFreeSpot(utype, self.unit:Internal():GetPosition())
-					if cms then
-						enemyposx, enemyposy, enemyposz = cms.x, cms.y, cms.z
-					else
-						return
-					end
-				elseif nearestUnit == nil and type == "attacker" then
-					local cms = self.ai.metalspothandler:ClosestEnemySpot(utype, self.unit:Internal():GetPosition())
-					if cms then
-						enemyposx, enemyposy, enemyposz = cms.x, cms.y, cms.z
-					else
-						return
-					end
+			elseif nearestUnit == nil and type == "attacker" then
+				local cms = self.ai.metalspothandler:ClosestEnemySpot(utype, self.unit:Internal():GetPosition())
+				if cms then
+					enemyposx, enemyposy, enemyposz = cms.x, cms.y, cms.z
 				else
-					enemyposx, enemyposy, enemyposz = Spring.GetUnitPosition(nearestUnit)
+					return
 				end
-				--else
-					--enemyposx, enemyposy, enemyposz = Spring.GetUnitPosition(nearestVisibleUnit)
-				--end
-				p = api.Position()
-				p.x = enemyposx + math.random(0,200) - math.random(0,200)
-				p.z = enemyposz + math.random(0,200) - math.random(0,200)
-				p.y = enemyposy
-				self.target = p
-				self.attacking = true
-				self.ai.attackhandler:AddRecruit(self)
-				if self.active then
-					if unit:Name() == "armrectr" or unit:Name() == "cornecro" then
-						if Spring.GetUnitCurrentBuildPower(unit.id) == 0 then -- if currently IDLE
-							unit:ExecuteCustomCommand(CMD.FIGHT, {p.x, p.y, p.z}, {"alt"})
-						end
-					else
-						--if nearestVisibleUnit and Spring.IsUnitInLos(nearestVisibleUnit, allyTeamID) then
-							local moverandom = math.random(0,4)
-							if (utype:CanFly() == true) then
-								moverandom = 0
-							end
-							if moverandom == 0 then
-								unit:Patrol(self.target)
-							elseif moverandom == 1 then
-								unit:MoveAndFire(self.target)
-							else
-								unit:Move(self.target)
-							end
-						--else
-							--local myUnits = Spring.GetTeamUnits(TeamID)
-							--for i = 1,#myUnits do
-								--pickMyUnit = myUnits[i]
-								--local r = math.random(0,#myUnits)
-								--if r == pickMyUnit then
-									--local r2 = math.random(0,#myUnits)
-									--if r2 == pickMyUnit then
-										--unit:Move(self.target)
-									--end
-								--end
-							--end
-						--end
+			else
+				enemyposx, enemyposy, enemyposz = Spring.GetUnitPosition(nearestUnit)
+			end
+			p = api.Position()
+			p.x = enemyposx + math.random(-math.sqrt(2)/2*myRange*0.90, math.sqrt(2)/2*myRange*0.90)
+			p.z = enemyposz + math.random(-math.sqrt(2)/2*myRange*0.90, math.sqrt(2)/2*myRange*0.90)
+			p.y = enemyposy
+			self.target = p
+			self.attacking = true
+			self.ai.attackhandler:AddRecruit(self)
+			if self.active then
+				if unit:Name() == "armrectr" or unit:Name() == "cornecro" then
+					if Spring.GetUnitCurrentBuildPower(unit.id) == 0 then -- if currently IDLE
+						unit:ExecuteCustomCommand(CMD.FIGHT, {p.x, p.y, p.z}, {"alt"})
 					end
 				else
-					self.unit:ElectBehaviour()
+					if (utype:CanFly() == true) then
+						unit:MoveAndFire(self.target)
+					else
+						unit:Move(self.target)
+					end
+				return
 				end
+			else
+				self.unit:ElectBehaviour()
+				return
+			end
 		end
-	end
-	
 	-- Retreating
-	if unitID % 30 == Spring.GetGameFrame() % 30 then
 		if not (currenthealth >= maxhealth*0.75 or currenthealth > 3000) then
-		self.unit:Internal():ExecuteCustomCommand(CMD.MOVE_STATE, { 0 }, {})
 		local nanotcx, nanotcy, nanotcz = GG.GetClosestNanoTC(unitID)
 			if nanotcx and nanotcy and nanotcz then
 				p = api.Position()
 				p.x, p.y, p.z = nanotcx, nanotcy, nanotcz
 			else
-				if startBoxMinX == 0 and startBoxMinZ == 0 and startBoxMaxZ == Game.mapSizeZ and startBoxMaxX == Game.mapSizeX then
-					p = api.Position()
-					p.x = startPosx
-					p.z = startPosz
-				else
-					p = api.Position()
-					p.x = math.random(startBoxMinX, startBoxMaxX)
-					p.z = math.random(startBoxMinZ, startBoxMaxZ)
-				end
-				p.y = startPosy
+				p = self.ai.commpos
 			end
 			self.target = p
 			self.attacking = false
@@ -203,6 +159,7 @@ function AttackerBehaviour:AttackCell(type)
 			else
 				self.unit:ElectBehaviour()
 			end
+			return
 		end
 	end
 end
