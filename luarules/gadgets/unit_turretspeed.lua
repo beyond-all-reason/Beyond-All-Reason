476be940074d4fe2433d3e6a236c79eca2475801
Fx-Doo <hedibertille93@gmail.com>
2018-09-07 10:14:17 +0200

    Made setting up turretturnspeed values via unitdefs a little easier
    All there is left is to replace speed values in restore/aim scripts with
    "WeaponXTurretX" and "WeaponXTurretY" and set up the wpnXturretx/y
    customParams in the unitDefs

--- luarules/gadgets/unit_turretspeed.lua
+++ luarules/gadgets/unit_turretspeed.lua
@@ -12,22 +12,24 @@
     end
 
 --[[ HOW TO:
- -Edit the unit COB script by adding the static_vars "WeaponXTurretX" and "WeaponXTurretY" for each weapons that have a turret speed
- -Set them to 1 in Create() (this will prevent cob script from erroring in the unlikely case it aims before LUA could call the setting function)
- -Create a "SetWeaponXTurretSpeed(var1, var2)" function for each weapon used in the script:
-{
-	Weapon1TurretX = var1;
-	weapon1TurretY = var2;
-}
- -Replace the turret turnspeed values in the AimWeaponX and RestoreAfterDelay functions with the static_vars "WeaponXTurretX" and "WeaponXTurretY"
+ -Include unitDefsTurretSpeeds.h in unitScript
+ -Replace the turret turnspeed values in the AimWeaponX and RestoreAfterDelay functions with the corresponding static_vars "WeaponXTurretX" and "WeaponXTurretY"
  -Set customParams.wpnXturretx and customParams.wpnXturrety values (= old COB values)
  -The gadget will call the SetWeaponXTurretSpeed(var1,var2) cob function with wpnXturretx, wpnXturrety values, therefore setting the static_vars to their wanted values
+ -If using continuous aiming, the correct values for the waitforturn checks are: > 65536, > WeaponXTurretY/30, < 65536 - WeaponXTurretY/30 (== not within one frame of the last valid heading)
  
- This gadget will only call the setting function if it finds both the wpnXturretx and wpnXturrety customParams, if the weapon doesn't use a rotation around x-axis in its aiming then just set it to 1 (not nil)
-]]
+ This gadget will only call the setting function if it finds both the wpnXturretx and wpnXturrety customParams, if the weapon doesn't use a rotation around x-axis in its aiming then just set it to 1 (not nil) 
+ ]]
 	
     if (not gadgetHandler:IsSyncedCode()) then return end
 
+	function gadget:Initialize()
+		for ct, unitID in pairs(Spring.GetAllUnits()) do
+			local udefID = Spring.GetUnitDefID(unitID)
+			gadget:UnitCreated(unitID, udefID)
+		end
+	end
+	
     function gadget:UnitCreated(unitID, unitDefID)
 		if UnitDefs[unitDefID].weapons then
 		for i = 1, 32 do
