23ad96c7a714d862124f99d52a059446c29efdbb
Fx-Doo <hedibertille93@gmail.com>
2018-09-10 20:46:08 +0200

    added trees_fire wpn

---
+++ weapons/trees_fire.lua
@@ -0,0 +1,23 @@
+return {
+	trees_fire = {
+		areaofeffect = 32,
+		craterboost = 0,
+		cratermult = 0,
+		edgeeffectiveness = 0.40000000596046,
+		explosiongenerator = "custom:burn",
+		impulseboost = 0.12300000339746,
+		impulsefactor = 0.12300000339746,
+		name = "TreesFire",
+		range = 1,
+		reloadtime = 3.5999999046326,
+		soundhit = "",
+		soundstart = "",
+		turret = 1,
+		weaponvelocity = 250,
+		damage = {
+			commanders = 10,
+			crawlingbombs = 10,
+			default = 10,
+		},
+	},
+}
