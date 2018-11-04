fcecf50ac613dd8d9d63d5fe55e031c3045eb25f
Fx-Doo <hedibertille93@gmail.com>
2018-11-04 13:58:32 +0100

    more smallfixes and optimizations for porting

--- scripts/weapon1control.h
+++ scripts/weapon1control.h
@@ -1,3 +1,42 @@
+/*
+HOW TO:
+- Model MUST contain aimx1 and aimy1 pieces
+- Script must have defined Weapon1TurretX and Weapon1TurretY static-vars (turret turn speeds)
+- Script must have defined aimx1 and aimy1 pieces
+- Change script as such:
+Create()
+{
+	[...]
+	start-script InitialSetup1();
+	[...]
+}
+
+AimWeapon1(heading, pitch)
+{
+	[...]
+	start-script Weapon1Drawn(); -- can call a function that draws weapons and then calls Weapon1Drawn() when done if there is an actual animation (ie pw)
+	[...] -- Remove animations from aimWeapon scripts (use a DrawWeapon1() if an animation is needed, weapon1control will rotate the different aimpieces)
+	start-script Weapon1SetWtdAim(heading, pitch);
+	[...]
+	return (aim1);
+}
+
+RestoreAfterDelay()
+{
+	[...]
+	sleep sleeptime;
+	start-script RestoreWeapon1();
+	[...] -- other animations;
+	call-script Weapon1Restored();
+	[...] -- tell script wpn has been restored if needed for walkscripts
+}
+
+- Weapon1Control moves the aim pieces depending on turretSpeeds, sets pitch = 1 when pitch reached and head = 1 when head reached
+- Weapon1Drawn allows unit to fire by setting wpnReady1 = 1
+- if pitch = 1, head = 1 and wpnReady = 1 then the weapon can shoot: aim1 = 1 and aimweapon returns aim1
+- Restore animations go in RestoreAfterDelay, RestoreWeapon1() restores aimy and aimx pieces orientation, Weapon1Restored() waits for these pieces to be restored
+*/
+
 static-var curHead1, wtdHead1, head1, curPitch1, wtdPitch1, pitch1, aim1, wpnReady1;
 
 Weapon1Control()
@@ -96,6 +135,16 @@ RestoreWeapon1()
 {
 	wtdHead1 = 0;
 	wtdPitch1 = 0;
+}
+
+Weapon1Restored()
+{
+	wpnReady1 = 0;
+	while (curHead1 != 0)
+	{
+		sleep 25;
+	}
+	return (TRUE);
 }
 
 Weapon1SetWtdAim(heading, pitch)
