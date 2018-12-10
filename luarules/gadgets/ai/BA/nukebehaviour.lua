65895f068ee4ed704f4124304ff84a025ef7c16d
Fx-Doo <hedibertille93@gmail.com>
2018-12-10 22:34:52 +0100

    Only target when stockpiled

--- luarules/gadgets/ai/BA/nukebehaviour.lua
+++ luarules/gadgets/ai/BA/nukebehaviour.lua
@@ -12,18 +12,19 @@ function NukeBehaviour:Update()
 			local _,curStockQ = Spring.GetUnitStockpile(self.unit:Internal().id)
 			if curStockQ <1 then
 				self.unit:Internal():ExecuteCustomCommand(CMD_STOCKPILE)
-			end
-		end
-		if not self.target then
-			self.target = GG.AiHelpers.TargetsOfInterest.Nuke(self.ai.id)
-		end
-		if self.target then
-			if not Spring.ValidUnitID(self.target) then
-				self.target = nil
-				self.unit:Internal():ExecuteCustomCommand(CMD.STOP, {}, {})
 			else
-				local x,y,z = Spring.GetUnitPosition(self.target)
-				self.unit:Internal():ExecuteCustomCommand(CMD.ATTACK, {x,y,z}, {})
+				if not self.target then
+					self.target = GG.AiHelpers.TargetsOfInterest.Nuke(self.ai.id)
+				end
+				if self.target then
+					if not Spring.ValidUnitID(self.target) then
+						self.target = nil
+						self.unit:Internal():ExecuteCustomCommand(CMD.STOP, {}, {})
+					else
+						local x,y,z = Spring.GetUnitPosition(self.target)
+						self.unit:Internal():ExecuteCustomCommand(CMD.ATTACK, {x,y,z}, {})
+					end
+				end
 			end
 		end
 	end
