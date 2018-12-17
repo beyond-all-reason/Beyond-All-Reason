6250c12786199f5b50e4fd85bb80e07573833c92
Damgam <c_2_damiano@op.pl>
2018-12-17 15:47:40 +0100

    DAI: fixed energy bonus

--- luarules/gadgets/ai_resource_multiplier.lua
+++ luarules/gadgets/ai_resource_multiplier.lua
@@ -30,7 +30,8 @@ for _,teamID in ipairs(Spring.GetTeamList()) do
 	if select(4,Spring.GetTeamInfo(teamID)) then	-- is AI?
 		aiCount = aiCount + 1
 		aiTeams[teamID] = { energy = 0, metal = 0, winds = 0, mexes = {} }
-		aiCountResMultiplier = aiCount*0.2 + 0.8
+		aiCountEResMultiplier = aiCount*0.2 + 0.8
+		aiCountMResMultiplier = aiCount*0.1 + 0.9
 	end
 end
 if aiCount == 0 then
@@ -165,8 +166,8 @@ function gadget:GameFrame(n)
 
 			--Spring.Echo(totalEnergy..'   +   '..((totalEnergy * (aiResourceMultiplier + timedResBonus)) - totalEnergy))
 			--Spring.Echo(totalMetal..'   +   '..((totalMetal * (aiResourceMultiplier + timedResBonus)) - totalMetal))
-			Spring.AddTeamResource(TeamID,"e", (totalEnergy * (aiResourceMultiplier + timedResBonus)*(aiCountResMultiplier*0.2)) - totalEnergy)
-			Spring.AddTeamResource(TeamID,"m", (totalMetal * (aiResourceMultiplier + timedResBonus)*aiCountResMultiplier) - totalMetal)
+			Spring.AddTeamResource(TeamID,"e", (totalEnergy * (aiResourceMultiplier + timedResBonus)*aiCountEResMultiplier) - totalEnergy)
+			Spring.AddTeamResource(TeamID,"m", (totalMetal * (aiResourceMultiplier + timedResBonus)*aiCountMResMultiplier) - totalMetal)
 		end
 	end
 end
