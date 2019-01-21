4e6b1d12e04ce0521ce2b83b25e5066e46caead0
Damgam <c_2_damiano@op.pl>
2019-01-21 23:57:20 +0100

    DAI: much higher chance to start with Kbots... AI is actually doing better with Kbots so...

--- luarules/gadgets/ai/BA/taskqueues.lua
+++ luarules/gadgets/ai/BA/taskqueues.lua
@@ -655,7 +655,7 @@ end
 ---- TECHTREE RELATED ----
 function KbotOrVeh()
 	local veh = 0
-	local kbot = 0
+	local kbot = 10
 	-- mapsize
 	mapsize = Game.mapX * Game.mapY
 	local randomnumber = math.random(1,mapsize+1)
