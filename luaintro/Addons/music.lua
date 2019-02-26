31f68d2ef0b714a30bc38fa9a936c272e2386956
Floris <floris@ruwhof.net>
2019-02-26 19:27:23 +0100

    fixed decals

--- luaintro/Addons/music.lua
+++ luaintro/Addons/music.lua
@@ -8,33 +8,30 @@ if addon.InGetInfo then
 		license = "GPL2",
 		layer   = 0,
 		depend  = {"LoadProgress"},
-		enabled = true,
+		enabled = false, -- loading makes it choppy towards the end; also, volume cannot be adjusted
 	}
 end
 
-------------------------------------------
-
-Spring.SetSoundStreamVolume(0.5)
-local musicfiles = VFS.DirList(LUA_DIRNAME .. "Assets/music", "*.ogg")
-if (#musicfiles > 0) then
-	Spring.PlaySoundStream(musicfiles[ math.random(#musicfiles) ], 0.5)
-	Spring.SetSoundStreamVolume(0.5)
-end
-
-
 function addon.DrawLoadScreen()
-	--local loadProgress = SG.GetLoadProgress()
+	local loadProgress = SG.GetLoadProgress()
 
 	-- fade in & out music with progress
-	--if (loadProgress < 0.9) then
-	--	Spring.SetSoundStreamVolume(loadProgress)
-	--else
-	--	Spring.SetSoundStreamVolume(0.9 + ((0.9 - loadProgress) * 9))
-	--end
+	if (loadProgress > 0.9) then
+		Spring.SetSoundStreamVolume(1 - ((loadProgress - 0.9) * 10))
+	end
 end
-
 
 function addon.Shutdown()
 	Spring.StopSoundStream()
-	Spring.SetSoundStreamVolume(0.5)
+	Spring.SetSoundStreamVolume(1)
+end
+
+function addon.Initialize()
+	Spring.SetSoundStreamVolume(1)
+	local musicfiles = VFS.DirList("sounds/music/loading", "*.ogg")
+	Spring.Echo("musicfiles", #musicfiles)
+	if (#musicfiles > 0) then
+		Spring.PlaySoundStream(musicfiles[ math.random(#musicfiles) ], 1)
+		Spring.SetSoundStreamVolume(1)
+	end
 end
