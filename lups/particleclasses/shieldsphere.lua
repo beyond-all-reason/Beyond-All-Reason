682f56ddde1bbf6c1bbfa4cb9dbd29d4586aaf22
LHoG <ccna@mail.ru>
2019-03-07 14:20:24 +0300

    1) Un-jK'ed formatting style 2) New, more random electricity effect

--- lups/particleclasses/shieldsphere.lua
+++ lups/particleclasses/shieldsphere.lua
@@ -14,17 +14,17 @@ local checkStunned = true
 
 function ShieldSphereParticle.GetInfo()
 	return {
-		name			= "ShieldSphere",
+		name		= "ShieldSphere",
 		backup		= "", --// backup class, if this class doesn't work (old cards,ati's,etc.)
-		desc			= "",
+		desc		= "",
 
-		layer		 = -23, --// extreme simply z-ordering :x
+		layer		= -23, --// extreme simply z-ordering :x
 
 		--// gfx requirement
-		fbo			 = false,
+		fbo			= false,
 		shader		= true,
-		rtt			 = false,
-		ctt			 = false,
+		rtt			= false,
+		ctt			= false,
 	}
 end
 
@@ -32,17 +32,17 @@ ShieldSphereParticle.Default = {
 	pos				= {0,0,0}, -- start pos
 	layer			= -23,
 
-	life			 = 0,
+	life			= 0,
 
-	size			 = 0,
-	sizeGrowth = 0,
+	size			= 0,
+	sizeGrowth		= 0,
 
-	margin		 = 1,
+	margin			= 1,
 
-	colormap1	= { {0, 0, 0, 0} },
-	colormap2	= { {0, 0, 0, 0} },
+	colormap1		= { {0, 0, 0, 0} },
+	colormap2		= { {0, 0, 0, 0} },
 
-	repeatEffect = false,
+	repeatEffect	= false,
 }
 
 -----------------------------------------------------------------------------------------------------------------
@@ -67,10 +67,10 @@ function ShieldSphereParticle:EndDraw()
 	gl.Culling(GL.BACK)
 	gl.Culling(false)
 
-	glMultiTexCoord(1, 1,1,1,1)
-	glMultiTexCoord(2, 1,1,1,1)
-	glMultiTexCoord(3, 1,1,1,1)
-	glMultiTexCoord(4, 1,1,1,1)
+	glMultiTexCoord(1, 1, 1, 1, 1)
+	glMultiTexCoord(2, 1, 1, 1, 1)
+	glMultiTexCoord(3, 1, 1, 1, 1)
+	glMultiTexCoord(4, 1, 1, 1, 1)
 end
 
 function ShieldSphereParticle:Draw()
@@ -85,11 +85,11 @@ function ShieldSphereParticle:Draw()
 		return
 	end
 	local color = self.color1
-	glMultiTexCoord(1, color[1],color[2],color[3],color[4] or 1)
+	glMultiTexCoord(1, color[1], color[2], color[3], color[4] or 1)
 	color = self.color2
-	glMultiTexCoord(2, color[1],color[2],color[3],color[4] or 1)
+	glMultiTexCoord(2, color[1], color[2], color[3], color[4] or 1)
 	local pos = self.pos
-	glMultiTexCoord(3, pos[1],pos[2],pos[3], 0)
+	glMultiTexCoord(3, pos[1], pos[2], pos[3], 0)
 	glMultiTexCoord(4, self.margin, self.size, gameFrame, 1)
 
 	glCallList(sphereList)
@@ -132,41 +132,82 @@ function ShieldSphereParticle:Initialize()
 			varying vec4 color2;
 			varying vec4 modelPos;
 
-			const float pi = acos(0.0) * 2.0;
-			const float phi = (1.0 + sqrt(5.0))/2.0;
+			const float PI = acos(0.0) * 2.0;
 
-			float SphFib1(vec3 v, float n) {	 // based on iq's version of Keinert et al's Spherical Fibonnacci Mapping code
-				vec4 b;
-				vec3 q;
-				vec2 ff, c;
-				float fk,	a, z, ni;
+			float Perlin3D( vec3 P ) {
+				//  https://github.com/BrianSharpe/Wombat/blob/master/Perlin3D.glsl
 
-				ni = 1.0 / n;
-				fk = pow (phi, max (2., floor (log (n * pi * sqrt (5.) * (1. - v.z * v.z)) / log (phi + 1.)))) / sqrt (5.);
-				ff = vec2 (floor (fk + 0.5), floor (fk * phi + 0.5));
-				b = 2. * vec4 (ff * ni, pi * (fract ((ff + 1.) * phi) - (phi - 1.)));
-				c = floor ((mat2 (b.y, - b.x, b.w, - b.z) / (b.y * b.z - b.x * b.w)) * vec2 (atan (v.y, v.x), v.z - (1. - ni)));
-				float ddMin = 4.1;
-				for (int s = 0; s < 4; s ++) {
-					a = dot (ff, vec2 (s - 2 * (s / 2), s / 2) + c);
-					z = 1. - (2. * a + 1.) * ni;
-					q = vec3 (sin (2. * pi * fract (phi * a) + vec2 (0.5 * pi, 0.)) * sqrt (1. - z * z), z) - v;
-					ddMin = min (ddMin, dot (q, q));
-				}
-				return sqrt (ddMin);
+				// establish our grid cell and unit position
+				vec3 Pi = floor(P);
+				vec3 Pf = P - Pi;
+				vec3 Pf_min1 = Pf - 1.0;
+
+				// clamp the domain
+				Pi.xyz = Pi.xyz - floor(Pi.xyz * ( 1.0 / 69.0 )) * 69.0;
+				vec3 Pi_inc1 = step( Pi, vec3( 69.0 - 1.5 ) ) * ( Pi + 1.0 );
+
+				// calculate the hash
+				vec4 Pt = vec4( Pi.xy, Pi_inc1.xy ) + vec2( 50.0, 161.0 ).xyxy;
+				Pt *= Pt;
+				Pt = Pt.xzxz * Pt.yyww;
+				const vec3 SOMELARGEFLOATS = vec3( 635.298681, 682.357502, 668.926525 );
+				const vec3 ZINC = vec3( 48.500388, 65.294118, 63.934599 );
+				vec3 lowz_mod = vec3( 1.0 / ( SOMELARGEFLOATS + Pi.zzz * ZINC ) );
+				vec3 highz_mod = vec3( 1.0 / ( SOMELARGEFLOATS + Pi_inc1.zzz * ZINC ) );
+				vec4 hashx0 = fract( Pt * lowz_mod.xxxx );
+				vec4 hashx1 = fract( Pt * highz_mod.xxxx );
+				vec4 hashy0 = fract( Pt * lowz_mod.yyyy );
+				vec4 hashy1 = fract( Pt * highz_mod.yyyy );
+				vec4 hashz0 = fract( Pt * lowz_mod.zzzz );
+				vec4 hashz1 = fract( Pt * highz_mod.zzzz );
+
+				// calculate the gradients
+				vec4 grad_x0 = hashx0 - 0.49999;
+				vec4 grad_y0 = hashy0 - 0.49999;
+				vec4 grad_z0 = hashz0 - 0.49999;
+				vec4 grad_x1 = hashx1 - 0.49999;
+				vec4 grad_y1 = hashy1 - 0.49999;
+				vec4 grad_z1 = hashz1 - 0.49999;
+				vec4 grad_results_0 = inversesqrt( grad_x0 * grad_x0 + grad_y0 * grad_y0 + grad_z0 * grad_z0 ) * ( vec2( Pf.x, Pf_min1.x ).xyxy * grad_x0 + vec2( Pf.y, Pf_min1.y ).xxyy * grad_y0 + Pf.zzzz * grad_z0 );
+				vec4 grad_results_1 = inversesqrt( grad_x1 * grad_x1 + grad_y1 * grad_y1 + grad_z1 * grad_z1 ) * ( vec2( Pf.x, Pf_min1.x ).xyxy * grad_x1 + vec2( Pf.y, Pf_min1.y ).xxyy * grad_y1 + Pf_min1.zzzz * grad_z1 );
+
+				// Classic Perlin Interpolation
+				vec3 blend = Pf * Pf * Pf * (Pf * (Pf * 6.0 - 15.0) + 10.0);
+				vec4 res0 = mix( grad_results_0, grad_results_1, blend.z );
+				vec4 blend2 = vec4( blend.xy, vec2( 1.0 - blend.xy ) );
+				float final = dot( res0, blend2.zxzx * blend2.wwyy );
+				return ( final * 1.1547005383792515290182975610039 );  // scale things to a strict -1.0->1.0 range  *= 1.0/sqrt(0.75)
 			}
 
 			#define SNORM2NORM(value) (value * 0.5 + 0.5)
 
 			void main(void)
 			{
-				float waveFront = mod(-gameFrame * 0.005, 1.0);
-				float band = SNORM2NORM(cos((modelPos.y - waveFront) * pi * 4.0));
-				float fibRaw = SphFib1(-modelPos.xzy, 384.0 + 128.0 * sin(gameFrame * 0.0025) );
-				float fib = pow(smoothstep(0.1, 0.0, fibRaw * band), 3.0);
+				//float waveFront = mod(-gameFrame * 0.005, 1.0);
+				//float band = SNORM2NORM(cos((modelPos.y - waveFront) * PI * 8.0));
+
+				const float resolution = 3.5;
+				const float sharpness = 10.0;
+				const float outerMult = 4.0;
+				const float timePace = 0.015;
+
+				vec3 pos = -modelPos.xzy;
+				float noiseVal = 0.0;
+
+				for (int i = 0; i < 4; ++i) {
+					noiseVal += abs(Perlin3D( float(i+1) * pos * resolution + gameFrame * timePace )) / float(i+2);
+					pos = pos.yzx;
+				}
+
+				noiseVal = 1.0 - noiseVal;
+				noiseVal = pow(noiseVal, sharpness);
+
+				noiseVal *= outerMult;
+				noiseVal = clamp(noiseVal, 0.0, 1.0);
+				//noiseVal *= band;
 
 				gl_FragColor = mix(color1, color2, opac);
-				gl_FragColor = pow(gl_FragColor, vec4(1.4 - fib));
+				gl_FragColor = pow(gl_FragColor, vec4(1.3 - noiseVal));
 			}
 
 		]],
@@ -180,7 +221,7 @@ function ShieldSphereParticle:Initialize()
 		return false
 	end
 
-	sphereList = gl.CreateList(DrawSphere,0,0,0,1,30,false)
+	sphereList = gl.CreateList(DrawSphere, 0, 0, 0, 1, 30, false)
 end
 
 function ShieldSphereParticle:Finalize()
