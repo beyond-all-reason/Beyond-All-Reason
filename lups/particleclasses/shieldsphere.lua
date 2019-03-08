dfcee41be899fa3ab94b6ce6d139a06d3b3c17f7
LHoG <ccna@mail.ru>
2019-03-08 23:34:59 +0300

    PowerOrb

--- lups/particleclasses/shieldsphere.lua
+++ lups/particleclasses/shieldsphere.lua
@@ -134,80 +134,92 @@ function ShieldSphereParticle:Initialize()
 
 			const float PI = acos(0.0) * 2.0;
 
-			float Perlin3D( vec3 P ) {
-				//  https://github.com/BrianSharpe/Wombat/blob/master/Perlin3D.glsl
+			float rand(vec2 c){
+				return fract(sin(dot(c.xy, vec2(12.9898, 78.233))) * 43758.5453);
+			}
 
-				// establish our grid cell and unit position
-				vec3 Pi = floor(P);
-				vec3 Pf = P - Pi;
-				vec3 Pf_min1 = Pf - 1.0;
+			float noise(vec2 p){
+				vec2 ij = floor(p);
+				vec2 xy = fract(p);
+				xy = 3.0 * xy * xy - 2.0 * xy * xy * xy;
+				//xy = 0.5 * (1.0 - cos(PI * xy));
+				float a = rand((ij + vec2(0.0, 0.0)));
+				float b = rand((ij + vec2(1.0, 0.0)));
+				float c = rand((ij + vec2(0.0, 1.0)));
+				float d = rand((ij + vec2(1.0, 1.0)));
+				float x1 = mix(a, b, xy.x);
+				float x2 = mix(c, d, xy.x);
+				return mix(x1, x2, xy.y);
+			}
 
-				// clamp the domain
-				Pi.xyz = Pi.xyz - floor(Pi.xyz * ( 1.0 / 69.0 )) * 69.0;
-				vec3 Pi_inc1 = step( Pi, vec3( 69.0 - 1.5 ) ) * ( Pi + 1.0 );
+			float fbm(vec2 P)
+			{
+				const int octaves = 3;
+				const float lacunarity = 2.1;
+				const float gain = 0.49;
 
-				// calculate the hash
-				vec4 Pt = vec4( Pi.xy, Pi_inc1.xy ) + vec2( 50.0, 161.0 ).xyxy;
-				Pt *= Pt;
-				Pt = Pt.xzxz * Pt.yyww;
-				const vec3 SOMELARGEFLOATS = vec3( 635.298681, 682.357502, 668.926525 );
-				const vec3 ZINC = vec3( 48.500388, 65.294118, 63.934599 );
-				vec3 lowz_mod = vec3( 1.0 / ( SOMELARGEFLOATS + Pi.zzz * ZINC ) );
-				vec3 highz_mod = vec3( 1.0 / ( SOMELARGEFLOATS + Pi_inc1.zzz * ZINC ) );
-				vec4 hashx0 = fract( Pt * lowz_mod.xxxx );
-				vec4 hashx1 = fract( Pt * highz_mod.xxxx );
-				vec4 hashy0 = fract( Pt * lowz_mod.yyyy );
-				vec4 hashy1 = fract( Pt * highz_mod.yyyy );
-				vec4 hashz0 = fract( Pt * lowz_mod.zzzz );
-				vec4 hashz1 = fract( Pt * highz_mod.zzzz );
+				float sum = 0.0;
+				float amp = 1.0;
+				vec2 pp = P;
 
-				// calculate the gradients
-				vec4 grad_x0 = hashx0 - 0.49999;
-				vec4 grad_y0 = hashy0 - 0.49999;
-				vec4 grad_z0 = hashz0 - 0.49999;
-				vec4 grad_x1 = hashx1 - 0.49999;
-				vec4 grad_y1 = hashy1 - 0.49999;
-				vec4 grad_z1 = hashz1 - 0.49999;
-				vec4 grad_results_0 = inversesqrt( grad_x0 * grad_x0 + grad_y0 * grad_y0 + grad_z0 * grad_z0 ) * ( vec2( Pf.x, Pf_min1.x ).xyxy * grad_x0 + vec2( Pf.y, Pf_min1.y ).xxyy * grad_y0 + Pf.zzzz * grad_z0 );
-				vec4 grad_results_1 = inversesqrt( grad_x1 * grad_x1 + grad_y1 * grad_y1 + grad_z1 * grad_z1 ) * ( vec2( Pf.x, Pf_min1.x ).xyxy * grad_x1 + vec2( Pf.y, Pf_min1.y ).xxyy * grad_y1 + Pf_min1.zzzz * grad_z1 );
+				int i;
 
-				// Classic Perlin Interpolation
-				vec3 blend = Pf * Pf * Pf * (Pf * (Pf * 6.0 - 15.0) + 10.0);
-				vec4 res0 = mix( grad_results_0, grad_results_1, blend.z );
-				vec4 blend2 = vec4( blend.xy, vec2( 1.0 - blend.xy ) );
-				float final = dot( res0, blend2.zxzx * blend2.wwyy );
-				return ( final * 1.1547005383792515290182975610039 );  // scale things to a strict -1.0->1.0 range  *= 1.0/sqrt(0.75)
+				for(i = 0; i < octaves; i+=1)
+				{
+					amp *= gain;
+					sum += amp * noise(pp);
+					pp *= lacunarity;
+				}
+				return sum;
 			}
 
 			#define SNORM2NORM(value) (value * 0.5 + 0.5)
+			#define NORM2SNORM(value) (value * 2.0 - 1.0)
+
+			#define time (gameFrame * 0.03333333)
+
+			vec3 LightningOrb(vec2 vUv) {
+				vec2 uv = NORM2SNORM(vUv);
+
+				vec3 finalColor = vec3( 0.0 );
+				const float inverseStrength = 65.0;
+				const float colorIntensity = 0.1;
+
+				for( int i=1; i < 4; ++i )
+				{
+					float hh = float(i) * colorIntensity;
+
+					float t = 0.0;
+
+					t += abs(1.0 / ((uv.x + 0.3 + fbm( uv + time/float(i))) * inverseStrength));
+					t += abs(1.0 / ((uv.x - 0.1 + fbm( uv + time/float(i))) * inverseStrength));
+					t += abs(1.0 / ((uv.x - 0.4 + fbm( uv + time/float(i))) * inverseStrength));
+					t += abs(1.0 / ((uv.x - 0.8 + fbm( uv + time/float(i))) * inverseStrength));
+					t += abs(1.0 / ((uv.x - 1.2 + fbm( uv + time/float(i))) * inverseStrength));
+
+					finalColor +=  t * vec3( hh + 0.1, 0.3, 1.0 );
+				}
+				return finalColor;
+			}
+
+			vec2 RadialCoords(vec3 a_coords)
+			{
+				vec3 a_coords_n = normalize(a_coords);
+				float lon = atan(a_coords_n.z, a_coords_n.x);
+				float lat = acos(a_coords_n.y);
+				vec2 sphereCoords = vec2(lon, lat) / PI;
+				return vec2(sphereCoords.x * 0.5 + 0.5, 1.0 - sphereCoords.y);
+			}
 
 			void main(void)
 			{
-				//float waveFront = mod(-gameFrame * 0.005, 1.0);
-				//float band = SNORM2NORM(cos((modelPos.y - waveFront) * PI * 8.0));
-
-				const float resolution = 3.5;
-				const float sharpness = 10.0;
-				const float outerMult = 4.0;
-				const float timePace = 0.015;
-
-				vec3 pos = -modelPos.xzy;
-				float noiseVal = 0.0;
-
-				for (int i = 0; i < 4; ++i) {
-					noiseVal += abs(Perlin3D( float(i+1) * pos * resolution + gameFrame * timePace )) / float(i+2);
-					pos = pos.yzx;
-				}
-
-				noiseVal = 1.0 - noiseVal;
-				noiseVal = pow(noiseVal, sharpness);
-
-				noiseVal *= outerMult;
-				noiseVal = clamp(noiseVal, 0.0, 1.0);
-				//noiseVal *= band;
-
 				gl_FragColor = mix(color1, color2, opac);
-				gl_FragColor = pow(gl_FragColor, vec4(1.3 - noiseVal));
+
+				vec2 vUv = (RadialCoords(modelPos.xyz));
+				vec3 orbColor = LightningOrb(vUv);
+
+				gl_FragColor.rgb = max(gl_FragColor.rgb, orbColor * orbColor);
+				gl_FragColor.a = length(gl_FragColor.rgb);
 			}
 
 		]],
