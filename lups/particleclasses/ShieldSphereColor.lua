e031262b4ca08d2d625e9a0c8f3322ee0524a65b
Floris <floris@ruwhof.net>
2019-03-11 03:12:15 +0100

    adjusted shield wobble

--- lups/particleclasses/ShieldSphereColor.lua
+++ lups/particleclasses/ShieldSphereColor.lua
@@ -204,7 +204,7 @@ function ShieldSphereColorParticle:Initialize()
 	shieldShader:Initialize()
 
 	geometryLists = {
-		large = gl.CreateList(DrawSphere, 0, 0, 0, 1, 48),
+		large = gl.CreateList(DrawSphere, 0, 0, 0, 1, 64),
 		small = gl.CreateList(DrawSphere, 0, 0, 0, 1, 28),
 	}
 end
