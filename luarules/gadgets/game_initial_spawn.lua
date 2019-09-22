d2282eff76c6c94243d61d41fedb16745530b2ee
Floris <floris@ruwhof.net>
2019-09-22 18:03:55 +0200

    set min engine version to 104.0.1.1398

--- luarules/gadgets/game_initial_spawn.lua
+++ luarules/gadgets/game_initial_spawn.lua
@@ -34,8 +34,8 @@ end
 -- set minimun engine version
 local unsupportedEngine = true
 local enabled = false
-local minEngineVersionTitle = '104.0.1.1344'
-if (engineVersion < 1000 and engineVersion >= 105) or engineVersion >= 104011344 then
+local minEngineVersionTitle = '104.0.1.1398'
+if (engineVersion < 1000 and engineVersion >= 105) or engineVersion >= 104011398 then
 	unsupportedEngine = false
 	enabled = true
 end
