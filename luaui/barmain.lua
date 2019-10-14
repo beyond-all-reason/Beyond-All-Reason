a2465b1c61c9059903d0eae38868e73a2a1aacd4
Floris <floris@ruwhof.net>
2019-10-14 02:11:28 +0200

    disabled inclusion of fonts.lua

--- luaui/barmain.lua
+++ luaui/barmain.lua
@@ -19,9 +19,8 @@ include("setupdefs.lua")
 include("savetable.lua")
 
 include("debug.lua")
-include("fonts.lua")
+--include("fonts.lua")
 include("layout.lua")   -- contains a simple LayoutButtons()
--- include("barwidgets.lua")  -- the widget handler
 VFS.Include(LUAUI_DIRNAME .. 'barwidgets.lua', nil, VFS.ZIP)
 
 --------------------------------------------------------------------------------
