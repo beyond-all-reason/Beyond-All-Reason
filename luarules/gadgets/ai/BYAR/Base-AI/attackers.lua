cac0d5a7fc78bc7679c7c599ef7adb92190fd40d
Fx-Doo <hedibertille93@gmail.com>
2019-11-02 17:16:42 +0100

    Erase unwanted changes

--- luarules/gadgets/ai/BYAR/Base-AI/attackers.lua
+++ luarules/gadgets/ai/BYAR/Base-AI/attackers.lua
@@ -3,193 +3,49 @@ Attackers!
 ]]--
 
 skirmisherlist = {
-	-- "armham", 	--	Hammer 			/	Light Plasma
-	-- "armrock",	--	Rocko			/	Rocket Bot
-	-- "armfboy",  --  Fatboy          /   Heavy Plasma Bot
-	-- "armfido",  --  Fido            /   Mortar/Skirmish Kbot
-	-- "armmav",   --  Maverick        /   Skirmish Kbot Fast Learning, Auto Repair
-	-- "armsnipe", --  Sharpshooter    /   Sniper Kbot
-	-- "armjanus",	--	Janus			/	Rocket Tank
-	-- "armsam",	-- 	Samson			/	Missile Truck
-	-- "armcroc",	--	Triton			/	Heavy Amphibious Tank
-	-- "armbanth",	--	Bantha			/	Assault Mech
-	-- "corthud",	--	Thud			/	Light Plasma
-	-- "corstorm",	--	Storm			/	Rocket Bot
-	-- "cormando",	--	Commando		/	Sealthy Paratrooper Kbot
-	-- "cormort",	--	Morty			/	Mobile Mortar Kbot
-	-- "cormist",	--	Slasher			/	Misile Truck
-	-- "corban",	--	Banisher		/	Heavy Missile Tank
-	-- "corgol",	--	Goliath			/	Very heavy Assault Tank
-	-- "corparrow",--	Posion Arrow	/	Very Heavy Amphibious Tank
-	-- "corshiva",	--	Shiva			/	Amphibious Siege Mech
-	-- "corkrog",	--	Krogoth			/	Experimental Assault Kbot
-	-- "cordecom",	--	Decoy Commander /   Decoy Commander -- I know it's weird but it's not that bad
-	-- "armdecom", --  Decoy Commander /   Decoy Commander -- I know it's weird but it's not that bad
+	"armham", 	--	Hammer 			/	Light Plasma
+	"armrock",	--	Rocko			/	Rocket Bot
+	"armfboy",  --  Fatboy          /   Heavy Plasma Bot
+	"armfido",  --  Fido            /   Mortar/Skirmish Kbot
+	"armmav",   --  Maverick        /   Skirmish Kbot Fast Learning, Auto Repair
+	"armsnipe", --  Sharpshooter    /   Sniper Kbot
+	"armjanus",	--	Janus			/	Rocket Tank
+	"armsam",	-- 	Samson			/	Missile Truck
+	"armcroc",	--	Triton			/	Heavy Amphibious Tank
+	"armbanth",	--	Bantha			/	Assault Mech
+	"corthud",	--	Thud			/	Light Plasma
+	"corstorm",	--	Storm			/	Rocket Bot
+	"cormando",	--	Commando		/	Sealthy Paratrooper Kbot
+	"cormort",	--	Morty			/	Mobile Mortar Kbot
+	"cormist",	--	Slasher			/	Misile Truck
+	"corban",	--	Banisher		/	Heavy Missile Tank
+	"corgol",	--	Goliath			/	Very heavy Assault Tank
+	"corparrow",--	Posion Arrow	/	Very Heavy Amphibious Tank
+	"corshiva",	--	Shiva			/	Amphibious Siege Mech
+	"corkrog",	--	Krogoth			/	Experimental Assault Kbot
+	--"cordecom",	--	Decoy Commander /   Decoy Commander -- I know it's weird but it's not that bad
+	--"armdecom", --  Decoy Commander /   Decoy Commander -- I know it's weird but it's not that bad
 }
 
 artillerylist = {
-	-- "armart",	--	Shellshocker	/ 	Light Artilery
-	-- "armmanni",	--	Penetrator		/	Mobile Tachyon Weapon
-	-- "armmerl",	--	Merl			/	Stealthy Rocket Launcher
-	-- "armvang",	--	Vanguard		/	All Terrain Heavy Plasma
-	-- "armmart",	--	Lugger			/	Mobile Artillery
-	-- "armmh",	--	Wombat			/	Hovercraft Rocket Launcher
-	-- "corhrk",	--	Dominator		/	Heavy Rocket Kbot
-	-- "corwolv",	--	Wolverine		/	Light Artilery
-	-- "cormart",	--	Pillager		/	Mobile Artillery
-	-- "cortrem",	--	Tremor			/	Heavy Artillery Vehicle
-	-- "corvroc",	--	Diplomat		/	Stealthy Rocket Launcher
-	-- "cormh",	--	Nixer			/	Hovercraft Rocket Launcher
-	-- "corsok",	--	Sokolov			/	Heavy Laser Hovertank
-	-- "armseer",	--	Seer			/	Radar Vehicle	
-	-- "corvrad",	--	Informer		/	Radar Vehicle	
-	-- "armmark",  --  Marky           /   Radar Kbot	
-	-- "corvoyr",	--	Voyeur			/	Radar Kbot
+	"armart",	--	Shellshocker	/ 	Light Artilery
+	"armmanni",	--	Penetrator		/	Mobile Tachyon Weapon
+	"armmerl",	--	Merl			/	Stealthy Rocket Launcher
+	"armvang",	--	Vanguard		/	All Terrain Heavy Plasma
+	"armmart",	--	Lugger			/	Mobile Artillery
+	"armmh",	--	Wombat			/	Hovercraft Rocket Launcher
+	"corhrk",	--	Dominator		/	Heavy Rocket Kbot
+	"corwolv",	--	Wolverine		/	Light Artilery
+	"cormart",	--	Pillager		/	Mobile Artillery
+	"cortrem",	--	Tremor			/	Heavy Artillery Vehicle
+	"corvroc",	--	Diplomat		/	Stealthy Rocket Launcher
+	"cormh",	--	Nixer			/	Hovercraft Rocket Launcher
+	"corsok",	--	Sokolov			/	Heavy Laser Hovertank
+	"armseer",	--	Seer			/	Radar Vehicle	
+	"corvrad",	--	Informer		/	Radar Vehicle	
+	"armmark",  --  Marky           /   Radar Kbot	
+	"corvoyr",	--	Voyeur			/	Radar Kbot
 }
-mobileaalist = {
-	-- "corah",	--	Slinger			/	Anti-Air Hovercraft
-	-- "corsent",	--	Copperhead		/	Anti Air Flak Vehicle
-	-- "coraak",   --	Manticore		/	Heavy Amphibious Anti-Air Kbot
-	-- "corcrash",	--	Crasher			/	Anti-Air
-	-- "corarch",	--	Shredder		/	Anti-Air Ship
-	-- "armyork",	--	Phalanx			/	Anti-Air Flak Vehicle
-	-- "armjeth",	--	Jethro			/	Anti-Air
-	-- "armaak",   --  Archangel       /   Advanced Amphibious Anti-Air Kbot
-	-- "armah",	--	Swatter			/	Anti-Air Hovercraft
-	-- "armrectr",	--	Rector			/	Rez-Reclaim
-	-- "cornecro",	--	Necro			/ 	Rez-Reclaim
-}
-
-raiderlist = {
-	-- "armspkt",  --  Recluse         /   All-Terrain Rocket Spider
-	-- "armpw", 	--	Peewee 			/ 	Fast Infantry
-	-- "armwar",	--	Warrior			/	Medium Infantry
-	-- "armamph",  --  Pelican         /   Amphibious Kbot
-	-- "armspid",  --  Spider          /   All-Terrain EMP Spider
-	-- "armzeus",  --  Zeus            /   Assault Kbot
-	-- "armpincer",--	Pincer			/	Light Amphibious Tank
-	-- "armflash",	-- 	Flash			/	Fast Assault Tank
-	-- "armstump",	--	Stumpy			/	Medium Assault Tank
-	-- "armbull",	--	Bulldog			/	Heavy Assault Tank
-	-- "armlatnk",	--	Panther			/	Lightning Tank
-	-- "armmar",	--	Marauder		/	Amphibious Assault Mech
-	-- "armraz",	--	Razxorbach		/	Battle Mech
-	-- "armanac",	--	Anaconda		/	Hovertank
-	-- "armlun",	--	Lun				/	Heavy Hovertank
-	-- "armdecade",--	Decade			/	Fast Assault Corvette
-	-- "armpship",	--	Ellysaw			/	Assault Frigate
-	-- "armcrus",	--	Conqueror		/	Cruiser
-	-- "corak",	--	A.K.			/	Fast Infantry
-	-- "coramph",	--	Gimp			/	Amphibious Kbot
-	-- "corcan",	--	Can				/	Armored Assault Kbot
-	-- "corpyro",	--	Pyro			/	Fast Assault Kbot
-	-- "cortermite", -- Termite		/	Heavy All-Terrain Assault Spider
-	-- "corsumo",	--	Sumo			/	Heavily-Armored Assault Kbot
-	-- "corgator",	--	Instigator		/	Fast Assault Tank
-	-- "corraid",	--	Raider			/	Medium Assault Tank
-	-- "corgarp",	--	Garpike			/	Light Amphibious Tank
-	-- "correap",	--	Reaper			/	Heavy Assault Tank
-	-- "corseal",	--	Croc			/	Medium Amphibious Tank
-	-- "corjugg",	--	Juggernaut		/	(barely) Mobile Heavy Turret
-	-- "corkarg",	--	Karganeth		/	All-Terrain Assault Mech
-	-- "corhal",	--	Halberd			/	Assault Hovertank
-	-- "corsnap",	--	Snapper			/	Hovertank
-	-- "coresupp",	--	Supporter		/	Fast Assault Corvette
-	-- "corpship",	--	Era				/	Assault Frigate
-	-- "corcrus",	--	Executioner		/	Cruiser
-}
-local function UnitType(name, uDef)
-	if uDef.humanName == "Commander" then
-		return "commander"
-	end
-	if uDef.canFly == true then
-		return "air"
-	elseif string.find(uDef.moveDef.name, "tank") then
-		return "tank"
-	elseif string.find(uDef.moveDef.name, "bot") then
-		return "bot"
-	elseif string.find(uDef.moveDef.name, "hover") then
-		return "hover"
-	elseif string.find(uDef.moveDef.name, "boat") then
-		return "boat"
-	end
-end
-
-local function IsMobile(name, uDef)
-	if uDef.canMove == true and uDef.speed > 0 then
-		return true
-	end
-	return false
-end
-
-local function CanAttackGround(name, uDef)
-	for i, weap in pairs (uDef.weapons) do
-		local wDef = WeaponDefs[weap.weaponDef]
-		if wDef.canAttackGround == true then
-			return true
-		end
-	end
-	return false
-end
-
-local function CanAttackAir(name, uDef)
-	for i, weap in pairs (uDef.weapons) do
-		local wDef = WeaponDefs[weap.weaponDef]
-		if (not weap.onlyTargets["notair"]) and not (weap.badTargets["vtol"]) then
-			return true
-		end
-	end
-	return false
-end
-
-local function GroundAttackTypes(name, uDef)
-	local attackTypes = { skirmish = false, artillery = false, raider = false }
-	for i, weap in pairs (uDef.weapons) do
-		local wDef = WeaponDefs[weap.weaponDef]
-		if (wDef.range > 400) and (wDef.reload > 2) then
-				attackTypes["skirmish"] = true
-		elseif (wDef.highTrajectory == 1) or (wDef.type == "StarburstMissile") then
-				attackTypes["artillery"] = true
-		else 
-				attackTypes["raider"] = true
-		end
-	end
-	return attackTypes
-end
-
-
-for id, uDef in pairs (UnitDefs) do
-	local name = uDef.name
-	if IsMobile(name, uDef) then
-		Spring.Echo (name, "mobile")
-		if UnitType(name, uDef) == "tank" or UnitType(name, uDef) == "bot" or UnitType(name, uDef) == "hover" then
-			Spring.Echo (name, "tank bot or hover")
-			if CanAttackGround(name, uDef) then
-				Spring.Echo (name, "attacks ground")
-				if GroundAttackTypes(name, uDef).skirmish == true then
-					Spring.Echo (name, "skirmish")
-					skirmisherlist[#skirmisherlist + 1] = name
-				end
-				if GroundAttackTypes(name, uDef).raider == true then
-					Spring.Echo (name, "raider")
-					raiderlist[#raiderlist + 1] = name
-				end
-				if GroundAttackTypes(name, uDef).artillery == true then
-					Spring.Echo (name, "artillery")
-					artillerylist[#artillerylist + 1] = name
-				end
-			end
-			if CanAttackAir(name, uDef) then
-				Spring.Echo (name, "attacks air")
-				mobileaalist[#mobileaalist + 1] = name
-			end
-		end
-	end
-
-end
-
-
-
 
 attackerlist = {
 	"armrectr",	--	Rector			/	Rez-Reclaim
@@ -282,4 +138,55 @@ fighterlist = {
 	"armfig",	-- 	Freedom Fighter /	Fighter
 }
 
-	
+mobileaalist = {
+	"corah",	--	Slinger			/	Anti-Air Hovercraft
+	"corsent",	--	Copperhead		/	Anti Air Flak Vehicle
+	"coraak",   --	Manticore		/	Heavy Amphibious Anti-Air Kbot
+	"corcrash",	--	Crasher			/	Anti-Air
+	"corarch",	--	Shredder		/	Anti-Air Ship
+	"armyork",	--	Phalanx			/	Anti-Air Flak Vehicle
+	"armjeth",	--	Jethro			/	Anti-Air
+	"armaak",   --  Archangel       /   Advanced Amphibious Anti-Air Kbot
+	"armah",	--	Swatter			/	Anti-Air Hovercraft
+	"armrectr",	--	Rector			/	Rez-Reclaim
+	"cornecro",	--	Necro			/ 	Rez-Reclaim
+}
+
+raiderlist = {
+	"armspkt",  --  Recluse         /   All-Terrain Rocket Spider
+	"armpw", 	--	Peewee 			/ 	Fast Infantry
+	"armwar",	--	Warrior			/	Medium Infantry
+	"armamph",  --  Pelican         /   Amphibious Kbot
+	"armspid",  --  Spider          /   All-Terrain EMP Spider
+	"armzeus",  --  Zeus            /   Assault Kbot
+	"armpincer",--	Pincer			/	Light Amphibious Tank
+	"armflash",	-- 	Flash			/	Fast Assault Tank
+	"armstump",	--	Stumpy			/	Medium Assault Tank
+	"armbull",	--	Bulldog			/	Heavy Assault Tank
+	"armlatnk",	--	Panther			/	Lightning Tank
+	"armmar",	--	Marauder		/	Amphibious Assault Mech
+	"armraz",	--	Razxorbach		/	Battle Mech
+	"armanac",	--	Anaconda		/	Hovertank
+	"armlun",	--	Lun				/	Heavy Hovertank
+	"armdecade",--	Decade			/	Fast Assault Corvette
+	"armpship",	--	Ellysaw			/	Assault Frigate
+	"armcrus",	--	Conqueror		/	Cruiser
+	"corak",	--	A.K.			/	Fast Infantry
+	"coramph",	--	Gimp			/	Amphibious Kbot
+	"corcan",	--	Can				/	Armored Assault Kbot
+	"corpyro",	--	Pyro			/	Fast Assault Kbot
+	"cortermite", -- Termite		/	Heavy All-Terrain Assault Spider
+	"corsumo",	--	Sumo			/	Heavily-Armored Assault Kbot
+	"corgator",	--	Instigator		/	Fast Assault Tank
+	"corraid",	--	Raider			/	Medium Assault Tank
+	"corgarp",	--	Garpike			/	Light Amphibious Tank
+	"correap",	--	Reaper			/	Heavy Assault Tank
+	"corseal",	--	Croc			/	Medium Amphibious Tank
+	"corjugg",	--	Juggernaut		/	(barely) Mobile Heavy Turret
+	"corkarg",	--	Karganeth		/	All-Terrain Assault Mech
+	"corhal",	--	Halberd			/	Assault Hovertank
+	"corsnap",	--	Snapper			/	Hovertank
+	"coresupp",	--	Supporter		/	Fast Assault Corvette
+	"corpship",	--	Era				/	Assault Frigate
+	"corcrus",	--	Executioner		/	Cruiser
+}	
