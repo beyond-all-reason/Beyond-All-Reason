d81bdae59a97c8d2fc4ae6fd05f54b40658e0af2
PtaQQ <ptaszekpawel@gmail.com>
2019-11-09 20:10:29 +0100

    whops

--- units/ArmGantry/armraz.lua
+++ units/ArmGantry/armraz.lua
@@ -26,7 +26,7 @@ return {
 		movementclass = "HKBOT4",
 		name = "Razorback",
 		objectname = "Units/ARMRAZ.s3o",
-		script = "Units/ARMRAZ.cob",
+		script = "Units/armraz_lus.lua",
 		seismicsignature = 0,
 		selfdestructas = "hugeExplosionGenericSelfd",
 		sightdistance = 450,
