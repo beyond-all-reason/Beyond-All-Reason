2dc2195449ff122d6b8835ed8eac6f55b2fed5d7
pandaro <padarogames@gmail.com>
2019-11-10 01:13:52 +0100

    delete some debugs (#11)

--- luarules/gadgets/ai/BYAR/stai/common.lua
+++ luarules/gadgets/ai/BYAR/stai/common.lua
@@ -1,12 +1,9 @@
 	if not unitTable or not featureTable then
 		unitTable, featureTable = shard_include("getunitfeaturetable",subf)
-        print('sadv')
 	end
 	if not CommonFunctionsLoaded then
 		shard_include ("commonfunctions",subf)
-        print('fgb')
 	end
 	if not UnitListsLoaded then
 		shard_include ("unitlists",subf)
-        print('zxcv')
 	end
