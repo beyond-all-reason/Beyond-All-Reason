c6f62760624876eb54f1abb4a4cc2e715ebe0a3c
Floris <floris@ruwhof.net>
2019-11-18 01:29:11 +0100

    added gadget: Nano Radar Pos

---
+++ luarules/gadgets/unit_nanoradarpos.lua
@@ -0,0 +1,39 @@
+
+function gadget:GetInfo()
+  return {
+    name      = "Nano Radar Pos",
+    desc      = "Removes radar icon wobble for nanos since these units are technically not buildings (no yardmap)",
+    author    = "Floris",
+    date      = "November 2019",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = true
+  }
+end
+
+
+if (gadgetHandler:IsSyncedCode()) then
+
+    local isNano = {}
+
+    function gadget:Initialize(uid, udid)
+        for unitDefID, defs in pairs(UnitDefs) do
+            if string.find(defs.name, "nanotc") then
+                isNano[unitDefID] = true
+            end
+        end
+        local units = Spring.GetAllUnits()
+        for i=1, #units do
+            if isNano[Spring.GetUnitDefID(units[i])] then
+                Spring.SetUnitPosErrorParams(units[i], 0,0,0, 0,0,0, math.huge)
+            end
+        end
+    end
+
+    function gadget:UnitCreated(uid, udid)
+        if isNano[udid] then
+            Spring.SetUnitPosErrorParams(units[i], 0,0,0, 0,0,0, math.huge)
+        end
+    end
+
+end
