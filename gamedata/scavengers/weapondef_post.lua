9206b00f88f0cc2a1b8f4967c03a2dce2c53e3d8
Floris <floris@ruwhof.net>
2020-01-11 01:18:17 +0100

    made scavenger weapon lights purple

--- gamedata/scavengers/weapondef_post.lua
+++ gamedata/scavengers/weapondef_post.lua
@@ -7,6 +7,14 @@ function convertToPurple(value)
     return value
 end
 
+local function split(s, separator)
+    local results = {}
+    for part in s:gmatch("[^"..separator.."]+") do
+        results[#results + 1] = part
+    end
+    return results
+end
+
 function scav_Wdef_Post(name, wDef)
     if wDef.weapontype == "Cannon" then
 		wDef.rgbcolor = {0.95, 0.36, 1}
@@ -33,6 +41,17 @@ function scav_Wdef_Post(name, wDef)
         wDef.rgbcolor = {0.95, 0.32, 1}
         wDef.rgbcolor2 = {1, 0.8, 1}
         wDef.explosiongenerator = convertToPurple(wDef.explosiongenerator)
+    end
+    -- make lighting purple (sort of: by switching around the rgb values)
+    if wDef.customparams then
+        for k, v in pairs(wDef.customparams) do
+            if type(v) == 'string' and string.find(k, 'light_color') then
+                local colors = split(v, ' ')
+                if colors[1] and colors[3] and colors[1] == '1' then
+                    wDef.customparams[k] = colors[2]..' '..colors[3]..' '..colors[1]
+                end
+            end
+        end
     end
     return wDef
 end
