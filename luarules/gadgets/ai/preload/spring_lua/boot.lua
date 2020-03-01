8d7867f72db595a503f4c1072790253acbf7c982
pandaro <padarogames@gmail.com>
2020-03-01 16:31:48 +0100

    fix wrong path in ai/boot.lua

--- luarules/gadgets/ai/preload/spring_lua/boot.lua
+++ luarules/gadgets/ai/preload/spring_lua/boot.lua
@@ -46,7 +46,7 @@ function byar_hacky_include_shim( path )
 end
 end
 --shard_include = shard_generate_include_func( "luarules/gadgets/ai/preload", subdir, "luarules/gadgets/ai" )
-shard_include = byar_hacky_include_shim("luarules/gadgets/ai/preload")
+shard_include = byar_hacky_include_shim("luarules/gadgets/ai")
 
 shard_include "preload/spring_lua/shard"
 os = shard_include "preload/spring_lua/fakeos"
