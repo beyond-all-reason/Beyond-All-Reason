32311e92976a915ace57829d0af31d361aadef16
icexuick <info@ixstudios.nl>
2020-03-23 22:19:43 +0100

    Scavengers | Droppod Lighting tweaks
     Less bright/large lights - to fix overbright on (large) spawns.

--- units/other/Scavengers/scavengerdroppodbeacon_scav.lua
+++ units/other/Scavengers/scavengerdroppodbeacon_scav.lua
@@ -94,8 +94,8 @@ return {
 				customparams = {
 					expl_light_color = "1 0 0",
 					expl_light_life_mult = 1.2,
-					expl_light_mult = 1.2,
-					expl_light_radius_mult = 1.2,
+					expl_light_mult = 1.0,
+					expl_light_radius_mult = 1.0,
 				},
 				damage = {
 					chicken = 0,
