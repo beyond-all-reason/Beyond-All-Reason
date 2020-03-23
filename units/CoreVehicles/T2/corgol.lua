b8705a2565c96aed3c2371cdc7b1307b19ab1a7c
triton13 <tristan.aznar@gmail.com>
2020-03-23 03:25:42 +0100

    Update corgol.lua
    small mistake fix

--- units/CoreVehicles/T2/corgol.lua
+++ units/CoreVehicles/T2/corgol.lua
@@ -24,7 +24,7 @@ return {
 		mass = 5001,
 		maxdamage = 7000,
 		maxslope = 12,
-		maxvelocity = 1.4,
+		maxvelocity = 1.35,
 		maxwaterdepth = 12,
 		movementclass = "HTANK4",
 		name = "Goliath",
