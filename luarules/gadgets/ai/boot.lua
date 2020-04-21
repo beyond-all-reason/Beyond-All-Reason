16d5e08ce65a588ebf8f8f82b4be8b830de74115
tomjn <contact@tomjn.com>
2020-04-21 18:09:31 +0100

    don't load the preload files in boot.lua the AILoader will do that

--- luarules/gadgets/ai/boot.lua
+++ luarules/gadgets/ai/boot.lua
@@ -1,7 +1,7 @@
 -- @TODO: Move top level logic out of ai.lua into here, and then make it load environment specific boot.lua files
 
 if ShardSpringLua then
-	VFS.Include( "luarules/gadgets/ai/preload/spring_lua/boot.lua" )
+	-- VFS.Include( "luarules/gadgets/ai/preload/spring_lua/boot.lua" )
 elseif game_engine then
 	require "spring_cpp/boot"
 else
