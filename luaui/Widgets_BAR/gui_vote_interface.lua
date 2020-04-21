52ab2c41c123e6b98f194526a86cd62dce376a1d
Floris <floris@ruwhof.net>
2020-04-22 01:46:05 +0200

    vote interface update

--- luaui/Widgets_BAR/gui_vote_interface.lua
+++ luaui/Widgets_BAR/gui_vote_interface.lua
@@ -31,41 +31,53 @@ local myPlayerID = Spring.GetMyPlayerID()
 local myName,_,mySpec,myTeamID,myAllyTeamID = Spring.GetPlayerInfo(myPlayerID,false)
 local startedAsPlayer = not mycSpec
 
-local function DrawRectRound(px,py,sx,sy,cs)
+local glBlending = gl.Blending
+local GL_SRC_ALPHA = GL.SRC_ALPHA
+local GL_ONE_MINUS_SRC_ALPHA = GL.ONE_MINUS_SRC_ALPHA
+local GL_ONE = GL.ONE
 
-	local csx = cs
-	local csy = cs
-	if sx-px < (cs*2) then
-		csx = (sx-px)/2
-		if csx < 0 then csx = 0 end
-	end
-	if sy-py < (cs*2) then
-		csy = (sy-py)/2
-		if csy < 0 then csy = 0 end
-	end
-	cs = (csx < csy and csx or csy)
-
+local function DrawRectRound(px,py,sx,sy,cs, tl,tr,br,bl, c1,c2)
 	gl.TexCoord(0.8,0.8)
+	if c2 then
+		gl.Color(c1[1],c1[2],c1[3],c1[4])
+	end
 	gl.Vertex(px+cs, py, 0)
 	gl.Vertex(sx-cs, py, 0)
+	if c2 then
+		gl.Color(c2[1],c2[2],c2[3],c2[4])
+	end
 	gl.Vertex(sx-cs, sy, 0)
 	gl.Vertex(px+cs, sy, 0)
 
+	if c2 then
+		gl.Color(c1[1],c1[2],c1[3],c1[4])
+	end
 	gl.Vertex(px, py+cs, 0)
 	gl.Vertex(px+cs, py+cs, 0)
+	if c2 then
+		gl.Color(c2[1],c2[2],c2[3],c2[4])
+	end
 	gl.Vertex(px+cs, sy-cs, 0)
 	gl.Vertex(px, sy-cs, 0)
 
+	if c2 then
+		gl.Color(c1[1],c1[2],c1[3],c1[4])
+	end
 	gl.Vertex(sx, py+cs, 0)
 	gl.Vertex(sx-cs, py+cs, 0)
+	if c2 then
+		gl.Color(c2[1],c2[2],c2[3],c2[4])
+	end
 	gl.Vertex(sx-cs, sy-cs, 0)
 	gl.Vertex(sx, sy-cs, 0)
 
-	local offset = 0.05		-- texture offset, because else gaps could show
-	local o = offset
+	local offset = 0.25		-- texture offset, because else gaps could show
 
-	-- top left
-	if py <= 0 or px <= 0 then o = 0.5 else o = offset end
+	-- bottom left
+	if c2 then
+		gl.Color(c1[1],c1[2],c1[3],c1[4])
+	end
+	if ((py <= 0 or px <= 0)  or (bl ~= nil and bl == 0)) and bl ~= 2   then o = 0.5 else o = offset end
 	gl.TexCoord(o,o)
 	gl.Vertex(px, py, 0)
 	gl.TexCoord(o,1-offset)
@@ -74,8 +86,8 @@ local function DrawRectRound(px,py,sx,sy,cs)
 	gl.Vertex(px+cs, py+cs, 0)
 	gl.TexCoord(1-offset,o)
 	gl.Vertex(px, py+cs, 0)
-	-- top right
-	if py <= 0 or sx >= vsx then o = 0.5 else o = offset end
+	-- bottom right
+	if ((py <= 0 or sx >= vsx) or (br ~= nil and br == 0)) and br ~= 2   then o = 0.5 else o = offset end
 	gl.TexCoord(o,o)
 	gl.Vertex(sx, py, 0)
 	gl.TexCoord(o,1-offset)
@@ -84,8 +96,11 @@ local function DrawRectRound(px,py,sx,sy,cs)
 	gl.Vertex(sx-cs, py+cs, 0)
 	gl.TexCoord(1-offset,o)
 	gl.Vertex(sx, py+cs, 0)
-	-- bottom left
-	if sy >= vsy or px <= 0 then o = 0.5 else o = offset end
+	-- top left
+	if c2 then
+		gl.Color(c2[1],c2[2],c2[3],c2[4])
+	end
+	if ((sy >= vsy or px <= 0) or (tl ~= nil and tl == 0)) and tl ~= 2   then o = 0.5 else o = offset end
 	gl.TexCoord(o,o)
 	gl.Vertex(px, sy, 0)
 	gl.TexCoord(o,1-offset)
@@ -94,8 +109,8 @@ local function DrawRectRound(px,py,sx,sy,cs)
 	gl.Vertex(px+cs, sy-cs, 0)
 	gl.TexCoord(1-offset,o)
 	gl.Vertex(px, sy-cs, 0)
-	-- bottom right
-	if sy >= vsy or sx >= vsx then o = 0.5 else o = offset end
+	-- top right
+	if ((sy >= vsy or sx >= vsx)  or (tr ~= nil and tr == 0)) and tr ~= 2   then o = 0.5 else o = offset end
 	gl.TexCoord(o,o)
 	gl.Vertex(sx, sy, 0)
 	gl.TexCoord(o,1-offset)
@@ -105,12 +120,9 @@ local function DrawRectRound(px,py,sx,sy,cs)
 	gl.TexCoord(1-offset,o)
 	gl.Vertex(sx, sy-cs, 0)
 end
-
-function RectRound(px,py,sx,sy,cs)
-	local px,py,sx,sy,cs = math.floor(px),math.floor(py),math.ceil(sx),math.ceil(sy),math.floor(cs)
-
+function RectRound(px,py,sx,sy,cs, tl,tr,br,bl, c1,c2)		-- (coordinates work differently than the RectRound func in other widgets)
 	gl.Texture(bgcorner)
-	gl.BeginEnd(GL.QUADS, DrawRectRound, px,py,sx,sy,cs)
+	gl.BeginEnd(GL.QUADS, DrawRectRound, px,py,sx,sy,cs, tl,tr,br,bl, c1,c2)
 	gl.Texture(false)
 end
 
@@ -249,21 +261,24 @@ function StartVote(name, owner)
 		noButtonArea = {xpos+(buttonMargin/2), ypos-(height/2)+buttonMargin, xpos+(width/2)-buttonMargin, ypos-(height/2)+buttonHeight-buttonMargin}
 
 		-- window
-		gl.Color(0,0,0,0.82)
-		RectRound(windowArea[1], windowArea[2], windowArea[3], windowArea[4], 5.5*widgetScale)
-		gl.Color(1,1,1,0.05)
-		RectRound(windowArea[1]+padding, windowArea[2]+padding, windowArea[3]-padding, windowArea[4]-padding, 5*widgetScale)
+		RectRound(windowArea[1], windowArea[2], windowArea[3], windowArea[4], 5.5*widgetScale, 1,1,1,1, {0.05,0.05,0.05,WG['guishader'] and 0.8 or 0.88}, {0,0,0,WG['guishader'] and 0.8 or 0.88})
+		RectRound(windowArea[1]+padding, windowArea[2]+padding, windowArea[3]-padding, windowArea[4]-padding, 3.5*widgetScale, 1,1,1,1, {0.25,0.25,0.25,0.2}, {0.5,0.5,0.5,0.2})
 
 		-- close
 		--gl.Color(0.1,0.1,0.1,0.55+(0.36))
 		--RectRound(closeButtonArea[1], closeButtonArea[2], closeButtonArea[3], closeButtonArea[4], 3.5*widgetScale)
+		local color1,color2
 		if IsOnRect(x, y, closeButtonArea[1], closeButtonArea[2], closeButtonArea[3], closeButtonArea[4]) then
 			hovered = 'esc'
-			gl.Color(1,1,1,0.55)
+			--gl.Color(1,1,1,0.55)
+			color1 = {0.6,0.6,0.6,0.6}
+			color2 = {1,1,1,0.6}
 		else
-			gl.Color(1,1,1,0.027)
+			--gl.Color(1,1,1,0.027)
+			color1 = {0.6,0.6,0.6,0.08}
+			color2 = {1,1,1,0.08}
 		end
-		RectRound(closeButtonArea[1]+padding, closeButtonArea[2]+padding, closeButtonArea[3]-padding, closeButtonArea[4]-padding, 3*widgetScale)
+		RectRound(closeButtonArea[1]+padding, closeButtonArea[2]+padding, closeButtonArea[3]-padding, closeButtonArea[4]-padding, 3.5*widgetScale, 0,1,0,1, color1, color2)
 
 		fontSize = fontSize*0.85
 		gl.Color(0,0,0,1)
@@ -278,15 +293,27 @@ function StartVote(name, owner)
 		font2:Print("\255\0\0\0ESC", closeButtonArea[1]+((closeButtonArea[3]-closeButtonArea[1])/2), closeButtonArea[2]+((closeButtonArea[4]-closeButtonArea[2])/2)-(fontSize/3), fontSize, "cn")
 
 		-- NO
+		local color1,color2,mult
 		if IsOnRect(x, y, noButtonArea[1], noButtonArea[2], noButtonArea[3], noButtonArea[4]) then
 			hovered = 'n'
-			gl.Color(0.7,0.1,0.1,0.8)
+			--gl.Color(0.7,0.1,0.1,0.8)
+			color1 = {0.5,0.07,0.07,0.8}
+			color2 = {0.7,0.1,0.1,0.8}
+			mult = 1.15
 		else
-			gl.Color(0.5,0,0,0.7)
+			--gl.Color(0.5,0,0,0.7)
+			color1 = {0.4,0,0,0.75}
+			color2 = {0.5,0,0,0.75}
+			mult = 1
 		end
-		RectRound(noButtonArea[1], noButtonArea[2], noButtonArea[3], noButtonArea[4], 3.3*widgetScale)
-		gl.Color(0,0,0,0.075)
-		RectRound(noButtonArea[1]+buttonPadding, noButtonArea[2]+buttonPadding, noButtonArea[3]-buttonPadding, noButtonArea[4]-buttonPadding, 2.2*widgetScale)
+		RectRound(noButtonArea[1], noButtonArea[2], noButtonArea[3], noButtonArea[4], 3*widgetScale, 1,1,1,1, color1, color2)
+
+		glBlending(GL_SRC_ALPHA, GL_ONE)
+		RectRound(noButtonArea[1], noButtonArea[4]-((noButtonArea[4]-noButtonArea[2])*0.5), noButtonArea[3], noButtonArea[4], 3*widgetScale, 2,2,0,0, {1,1,1,0.06*mult}, {1,1,1,0.2*mult})
+		RectRound(noButtonArea[1], noButtonArea[2], noButtonArea[3], noButtonArea[2]+((noButtonArea[4]-noButtonArea[2])*0.35), 3*widgetScale, 0,0,2,2, {1,1,1,0.11*mult}, {1,1,1,0})
+		glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+		--gl.Color(0,0,0,0.075)
+		--RectRound(noButtonArea[1]+buttonPadding, noButtonArea[2]+buttonPadding, noButtonArea[3]-buttonPadding, noButtonArea[4]-buttonPadding, 2.2*widgetScale)
 
 		fontSize = fontSize*0.85
 		local noText = 'NO'
@@ -300,13 +327,23 @@ function StartVote(name, owner)
 		if not voteOwner then
 			if IsOnRect(x, y, yesButtonArea[1], yesButtonArea[2], yesButtonArea[3], yesButtonArea[4]) then
 				hovered = 'y'
-				gl.Color(0.05,0.6,0.05,0.8)
+				--gl.Color(0.05,0.6,0.05,0.8)
+				color1 = {0.035,0.4,0.035,0.8}
+				color2 = {0.05,0.6,0.5,0.8}
+				mult = 1.15
 			else
-				gl.Color(0,0.5,0,0.35)
+				--gl.Color(0,0.5,0,0.35)
+				color1 = {0,0.4,0,0.38}
+				color2 = {0,0.5,0,0.38}
+				mult = 1
 			end
-			RectRound(yesButtonArea[1], yesButtonArea[2], yesButtonArea[3], yesButtonArea[4], 3.3*widgetScale)
-			gl.Color(0,0,0,0.075)
-			RectRound(yesButtonArea[1]+buttonPadding, yesButtonArea[2]+buttonPadding, yesButtonArea[3]-buttonPadding, yesButtonArea[4]-buttonPadding, 2.2*widgetScale)
+			RectRound(yesButtonArea[1], yesButtonArea[2], yesButtonArea[3], yesButtonArea[4], 3*widgetScale, 1,1,1,1, color1, color2)
+			glBlending(GL_SRC_ALPHA, GL_ONE)
+			RectRound(yesButtonArea[1], yesButtonArea[4]-((yesButtonArea[4]-yesButtonArea[2])*0.5), yesButtonArea[3], yesButtonArea[4], 3*widgetScale, 2,2,0,0, {1,1,1,0.06*mult}, {1,1,1,0.2*mult})
+			RectRound(yesButtonArea[1], yesButtonArea[2], yesButtonArea[3], yesButtonArea[2]+((yesButtonArea[4]-yesButtonArea[2])*0.35), 3*widgetScale, 0,0,2,2, {1,1,1,0.11*mult}, {1,1,1,0})
+			glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+			--gl.Color(0,0,0,0.075)
+			--RectRound(yesButtonArea[1]+buttonPadding, yesButtonArea[2]+buttonPadding, yesButtonArea[3]-buttonPadding, yesButtonArea[4]-buttonPadding, 2.2*widgetScale)
 
 			font2:Print("YES", yesButtonArea[1]+((yesButtonArea[3]-yesButtonArea[1])/2), yesButtonArea[2]+((yesButtonArea[4]-yesButtonArea[2])/2)-(fontSize/3), fontSize, "con")
 		end
