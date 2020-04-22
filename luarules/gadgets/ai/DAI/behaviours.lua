d340e84a6cbdf89edf64cb623719dbe7ff41da89
tomjn <contact@tomjn.com>
2020-04-22 19:39:36 +0100

    swap named tables for returned tables

--- luarules/gadgets/ai/DAI/behaviours.lua
+++ luarules/gadgets/ai/DAI/behaviours.lua
@@ -14,7 +14,7 @@ shard_include(  "staticweaponbehaviour")
 shard_include(  "nukebehaviour")
 shard_include(  "fighterbehaviour")
 
-behaviours = {
+return {
 	--CoreNanoTurret
 	cornanotc = {
 		TaskQueueBehaviour,
@@ -95,45 +95,3 @@ behaviours = {
 		StaticWeaponBehaviour,
 		},
 }
-
-function defaultBehaviours(unit)
-	b = {}
-	u = unit:Internal()
-	table.insert(b, BootBehaviour )
-	if unit:Internal():Name() == "corak" then
-		if math.random(1,5) == 1 then
-			table.insert(b,ScoutsBehaviour)
-		else
-			table.insert(b,RaiderBehaviour)
-		end
-		return b
-	end
-	if u:CanBuild() then
-		table.insert(b,TaskQueueBehaviour)
-	end
-	if IsSkirmisher(unit) then
-		table.insert(b,SkirmisherBehaviour)
-	end
-	if IsRaider(unit) then
-		table.insert(b,RaiderBehaviour)
-	end
-	if IsFighter(unit) then
-		table.insert(b,FighterBehaviour)
-	end
-	if IsBomber(unit) then
-		table.insert(b,BomberBehaviour)
-	end
-	if IsArtillery(unit) then
-		table.insert(b,ArtilleryBehaviour)
-	end
-	if IsScouts(unit) then
-		table.insert(b,ScoutsBehaviour)
-	end
-	if IsStaticWeapon(unit) then
-		table.insert(b,StaticWeaponBehaviour)
-	end
-	--if IsPointCapturer(unit) then
-		--table.insert(b,PointCapturerBehaviour)
-	--end
-	return b
-end
