1042999570f1999a8114b96c9d8354f8039a3b52
tomjn <contact@tomjn.com>
2020-04-22 17:18:04 +0100

    move the shard include generator function into boot.lua

--- luarules/gadgets/ai/shard_runtime/spring_lua/boot.lua
+++ luarules/gadgets/ai/shard_runtime/spring_lua/boot.lua
@@ -1,14 +1,33 @@
 -- initial setup of things
 
+-- Generates a per AI shard_include function
+function shard_generate_include_func( runtime_path, ai_path )
+	return function( file )
+		if type(file) ~= 'string' then
+			return nil
+		end
+		local ai_file = ai_path .. "/" .. file .. ".lua"
+		local runtime_file = runtime_path .. "/" .. file .. ".lua"
+		if VFS.FileExists(ai_file) then
+			return VFS.Include(ai_file)
+		elseif VFS.FileExists(runtime_file) then
+			return VFS.Include(runtime_file)
+		end
+		Spring.Echo( "Failed to load "..ai_file.." or "..runtime_file )
+		return nil
+	end
+end
+
+-- the default shard include function, only deals with the runtime files for boot up
 function shard_include(file)
 	if type(file) ~= 'string' then
 		return nil
 	end
-	local preloadFile = "luarules/gadgets/ai/shard_runtime/" .. file .. ".lua"
-	if VFS.FileExists(preloadFile) then
-		return VFS.Include(preloadFile)
+	local runtime_file = "luarules/gadgets/ai/shard_runtime/" .. file .. ".lua"
+	if VFS.FileExists(runtime_file) then
+		return VFS.Include(runtime_file)
 	end
-	Spring.Echo( "Failed to load "..preloadFile )
+	Spring.Echo( "Failed to load "..runtime_file )
 	return
 end
 
