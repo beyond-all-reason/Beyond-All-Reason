15ac89e4dba273d5be82ce824041ad901d788936
PtaQQ <ptaszekpawel@gmail.com>
2020-04-24 17:42:07 +0200

    Update gui_options.lua

--- luaui/Widgets_BAR/gui_options.lua
+++ luaui/Widgets_BAR/gui_options.lua
@@ -1986,7 +1986,7 @@ function init()
 		 end,
 		},
 
-		{id="disticon", group="gfx", basic=true, name="Strategic icon distance", type="slider", min=100, max=700, step=10, value=tonumber(Spring.GetConfigInt("UnitIconDist",1) or 400), description='Set a lower value to get better performance',
+		{id="disticon", group="gfx", basic=true, name="Strategic icon distance", type="slider", min=100, max=700, step=10, value=tonumber(Spring.GetConfigInt("UnitIconDist",1) or 200), description='Set a lower value to get better performance',
 		 onload = function(i) end,
 		 onchange = function(i, value)
 			 if Spring.GetConfigInt("distdraw",1) < 10000 then
