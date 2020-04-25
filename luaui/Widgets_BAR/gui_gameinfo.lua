4e0b3de5d694ffdcc3e09c25c06ce9a7c343f045
Floris <floris@ruwhof.net>
2020-04-26 00:28:13 +0200

    more rectround work

--- luaui/Widgets_BAR/gui_gameinfo.lua
+++ luaui/Widgets_BAR/gui_gameinfo.lua
@@ -22,8 +22,6 @@ local fontfile2 = "fonts/" .. Spring.GetConfigString("bar_font2", "Exo2-SemiBold
 local font2 = gl.LoadFont(fontfile2, fontfileSize*fontfileScale, fontfileOutlineSize*fontfileScale, fontfileOutlineStrength)
 local loadedFontSize = fontfileSize*fontfileScale
 
-local bgcorner = ":l:LuaUI/Images/bgcorner.png"
-
 
 local titlecolor = "\255\255\205\100"
 local keycolor = ""
@@ -213,7 +211,8 @@ local amNewbie = (Spring.GetTeamRulesParam(myTeamID, 'isNewbie') == 1)
 local showOnceMore = false		-- used because of GUI shader delay
 
 local function DrawRectRound(px,py,sx,sy,cs, tl,tr,br,bl, c1,c2)
-	gl.TexCoord(0.8,0.8)
+	local csyMult = 1 / ((sy-py)/cs)
+
 	if c2 then
 		gl.Color(c1[1],c1[2],c1[3],c1[4])
 	end
@@ -225,81 +224,96 @@ local function DrawRectRound(px,py,sx,sy,cs, tl,tr,br,bl, c1,c2)
 	gl.Vertex(sx-cs, sy, 0)
 	gl.Vertex(px+cs, sy, 0)
 
+	-- left side
 	if c2 then
-		gl.Color(c1[1],c1[2],c1[3],c1[4])
+		gl.Color(c1[1]*(1-csyMult)+(c2[1]*csyMult),c1[2]*(1-csyMult)+(c2[2]*csyMult),c1[3]*(1-csyMult)+(c2[3]*csyMult),c1[4]*(1-csyMult)+(c2[4]*csyMult))
 	end
 	gl.Vertex(px, py+cs, 0)
 	gl.Vertex(px+cs, py+cs, 0)
 	if c2 then
-		gl.Color(c2[1],c2[2],c2[3],c2[4])
+		gl.Color(c2[1]*(1-csyMult)+(c1[1]*csyMult),c2[2]*(1-csyMult)+(c1[2]*csyMult),c2[3]*(1-csyMult)+(c1[3]*csyMult),c2[4]*(1-csyMult)+(c1[4]*csyMult))
 	end
 	gl.Vertex(px+cs, sy-cs, 0)
 	gl.Vertex(px, sy-cs, 0)
 
+	-- right side
 	if c2 then
-		gl.Color(c1[1],c1[2],c1[3],c1[4])
+		gl.Color(c1[1]*(1-csyMult)+(c2[1]*csyMult),c1[2]*(1-csyMult)+(c2[2]*csyMult),c1[3]*(1-csyMult)+(c2[3]*csyMult),c1[4]*(1-csyMult)+(c2[4]*csyMult))
 	end
 	gl.Vertex(sx, py+cs, 0)
 	gl.Vertex(sx-cs, py+cs, 0)
 	if c2 then
-		gl.Color(c2[1],c2[2],c2[3],c2[4])
+		gl.Color(c2[1]*(1-csyMult)+(c1[1]*csyMult),c2[2]*(1-csyMult)+(c1[2]*csyMult),c2[3]*(1-csyMult)+(c1[3]*csyMult),c2[4]*(1-csyMult)+(c1[4]*csyMult))
 	end
 	gl.Vertex(sx-cs, sy-cs, 0)
 	gl.Vertex(sx, sy-cs, 0)
 
-	local offset = 0.25		-- texture offset, because else gaps could show
+	local offset = 0.15		-- texture offset, because else gaps could show
 
 	-- bottom left
 	if c2 then
 		gl.Color(c1[1],c1[2],c1[3],c1[4])
 	end
-	if ((py <= 0 or px <= 0)  or (bl ~= nil and bl == 0)) and bl ~= 2   then o = 0.5 else o = offset end
-	gl.TexCoord(o,o)
-	gl.Vertex(px, py, 0)
-	gl.TexCoord(o,1-offset)
+	if ((py <= 0 or px <= 0)  or (bl ~= nil and bl == 0)) and bl ~= 2   then
+		gl.Vertex(px, py, 0)
+	else
+		gl.Vertex(px+cs, py, 0)
+	end
 	gl.Vertex(px+cs, py, 0)
-	gl.TexCoord(1-offset,1-offset)
+	if c2 then
+		gl.Color(c1[1]*(1-csyMult)+(c2[1]*csyMult),c1[2]*(1-csyMult)+(c2[2]*csyMult),c1[3]*(1-csyMult)+(c2[3]*csyMult),c1[4]*(1-csyMult)+(c2[4]*csyMult))
+	end
 	gl.Vertex(px+cs, py+cs, 0)
-	gl.TexCoord(1-offset,o)
 	gl.Vertex(px, py+cs, 0)
 	-- bottom right
-	if ((py <= 0 or sx >= vsx) or (br ~= nil and br == 0)) and br ~= 2   then o = 0.5 else o = offset end
-	gl.TexCoord(o,o)
-	gl.Vertex(sx, py, 0)
-	gl.TexCoord(o,1-offset)
+	if c2 then
+		gl.Color(c1[1],c1[2],c1[3],c1[4])
+	end
+	if ((py <= 0 or sx >= vsx) or (br ~= nil and br == 0)) and br ~= 2 then
+		gl.Vertex(sx, py, 0)
+	else
+		gl.Vertex(sx-cs, py, 0)
+	end
 	gl.Vertex(sx-cs, py, 0)
-	gl.TexCoord(1-offset,1-offset)
+	if c2 then
+		gl.Color(c1[1]*(1-csyMult)+(c2[1]*csyMult),c1[2]*(1-csyMult)+(c2[2]*csyMult),c1[3]*(1-csyMult)+(c2[3]*csyMult),c1[4]*(1-csyMult)+(c2[4]*csyMult))
+	end
 	gl.Vertex(sx-cs, py+cs, 0)
-	gl.TexCoord(1-offset,o)
 	gl.Vertex(sx, py+cs, 0)
 	-- top left
 	if c2 then
 		gl.Color(c2[1],c2[2],c2[3],c2[4])
 	end
-	if ((sy >= vsy or px <= 0) or (tl ~= nil and tl == 0)) and tl ~= 2   then o = 0.5 else o = offset end
-	gl.TexCoord(o,o)
-	gl.Vertex(px, sy, 0)
-	gl.TexCoord(o,1-offset)
+	if ((sy >= vsy or px <= 0) or (tl ~= nil and tl == 0)) and tl ~= 2 then
+		gl.Vertex(px, sy, 0)
+	else
+		gl.Vertex(px+cs, sy, 0)
+	end
 	gl.Vertex(px+cs, sy, 0)
-	gl.TexCoord(1-offset,1-offset)
+	if c2 then
+		gl.Color(c2[1]*(1-csyMult)+(c1[1]*csyMult),c2[2]*(1-csyMult)+(c1[2]*csyMult),c2[3]*(1-csyMult)+(c1[3]*csyMult),c2[4]*(1-csyMult)+(c1[4]*csyMult))
+	end
 	gl.Vertex(px+cs, sy-cs, 0)
-	gl.TexCoord(1-offset,o)
 	gl.Vertex(px, sy-cs, 0)
 	-- top right
-	if ((sy >= vsy or sx >= vsx)  or (tr ~= nil and tr == 0)) and tr ~= 2   then o = 0.5 else o = offset end
-	gl.TexCoord(o,o)
-	gl.Vertex(sx, sy, 0)
-	gl.TexCoord(o,1-offset)
+	if c2 then
+		gl.Color(c2[1],c2[2],c2[3],c2[4])
+	end
+	if ((sy >= vsy or sx >= vsx)  or (tr ~= nil and tr == 0)) and tr ~= 2 then
+		gl.Vertex(sx, sy, 0)
+	else
+		gl.Vertex(sx-cs, sy, 0)
+	end
 	gl.Vertex(sx-cs, sy, 0)
-	gl.TexCoord(1-offset,1-offset)
+	if c2 then
+		gl.Color(c2[1]*(1-csyMult)+(c1[1]*csyMult),c2[2]*(1-csyMult)+(c1[2]*csyMult),c2[3]*(1-csyMult)+(c1[3]*csyMult),c2[4]*(1-csyMult)+(c1[4]*csyMult))
+	end
 	gl.Vertex(sx-cs, sy-cs, 0)
-	gl.TexCoord(1-offset,o)
 	gl.Vertex(sx, sy-cs, 0)
 end
 function RectRound(px,py,sx,sy,cs, tl,tr,br,bl, c1,c2)		-- (coordinates work differently than the RectRound func in other widgets)
-	gl.Texture(bgcorner)
-	gl.BeginEnd(GL.QUADS, DrawRectRound, px,py,sx,sy,cs, tl,tr,br,bl, c1,c2)
 	gl.Texture(false)
+	gl.BeginEnd(GL.QUADS, DrawRectRound, px,py,sx,sy,cs, tl,tr,br,bl, c1,c2)
 end
 
 function DrawTextarea(x,y,width,height,scrollbar)
@@ -310,7 +324,7 @@ function DrawTextarea(x,y,width,height,scrollbar)
 	local scrollbarPosWidth  		= 4
 	local scrollbarPosMinHeight 	= 8
 	local scrollbarBackgroundColor	= {0,0,0,0.24	}
-	local scrollbarBarColor			= {1,1,1,0.08}
+	local scrollbarBarColor			= {1,1,1,0.15}
 
 	local fontSizeTitle				= 18		-- is version number
 	local fontSizeDate				= 14
@@ -350,11 +364,11 @@ function DrawTextarea(x,y,width,height,scrollbar)
 			-- bar
 			gl.Color(scrollbarBarColor)
 			RectRound(
-				x+width-scrollbarMargin-scrollbarWidth + (scrollbarWidth - scrollbarPosWidth),
-				scrollbarPos,
-				x+width-scrollbarMargin-(scrollbarWidth - scrollbarPosWidth),
-				scrollbarPos - (scrollbarPosHeight),
-				scrollbarPosWidth/2.2
+				x+width-scrollbarMargin-(scrollbarWidth*0.75),
+					scrollbarPos - (scrollbarPosHeight),
+				x+width-scrollbarMargin-(scrollbarWidth*0.25),
+					scrollbarPos,
+				scrollbarPosWidth/2.5
 			)
 		end
 	end
