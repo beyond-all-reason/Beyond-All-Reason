8b88d241da04d60def4436e9fd93da8513bd514f
Damgam <c_2_damiano@op.pl>
2020-05-24 17:46:52 +0200

    oops x2

--- luarules/gadgets/scavengers/Configs/BYAR/config.lua
+++ luarules/gadgets/scavengers/Configs/BYAR/config.lua
@@ -83,7 +83,7 @@ if scavengersAIEnabled then
 	constructorControllerModuleConfig = {
 		constructortimerstart				= 120, -- ammount of seconds it skips from constructortimer for the first spawn (make first spawn earlier - this timer starts on timer-Timer1)
 		constructortimer 					= 240, -- time in seconds between commander/constructor spawns
-		constructortimerreductionframes		= 18000,
+		constructortimerreductionframes		= 36000,
 		minimumconstructors					= 3,
 		useresurrectors						= true,
 			searesurrectors					= false,
