665230e96fe4ca03337f66775b2fe146ff5fd91e
Damgam <c_2_damiano@op.pl>
2020-05-25 22:44:59 +0200

    scav final boss fix

--- luarules/gadgets/scavengers/Modules/unit_spawner.lua
+++ luarules/gadgets/scavengers/Modules/unit_spawner.lua
@@ -27,27 +27,48 @@ function BossWaveTimer(n)
 				end
 			end
 			
-			for b = 1,1000 do
-				local pickedBeaconTest = SpawnBeacons[math_random(1,#SpawnBeacons)]
-				local _,_,_,pickedBeaconCaptureProgress = Spring.GetUnitHealth(pickedBeaconTest)
-				if pickedBeaconCaptureProgress == 0 then
-					pickedBeacon = pickedBeaconTest
-					break
-				else
-					pickedBeacon = 1234567890
+			if #SpawnBeacons > 1 then
+				for b = 1,1000 do
+					local pickedBeaconTest = SpawnBeacons[math_random(1,#SpawnBeacons)]
+					local _,_,_,pickedBeaconCaptureProgress = Spring.GetUnitHealth(pickedBeaconTest)
+					if pickedBeaconCaptureProgress == 0 then
+						pickedBeacon = pickedBeaconTest
+						break
+					else
+						pickedBeacon = 1234567890
+					end
 				end
+			elseif #SpawnBeacons == 1 then
+				pickedBeacon = SpawnBeacons[1]
 			end
+			SpawnBeacons = nil
+			
 			if pickedBeacon == 1234567890 then
 				return
+			else
+				if pickedBeacon then
+					local posx,posy,posz = Spring.GetUnitPosition(pickedBeacon)
+					local posy = Spring.GetGroundHeight(posx, posz)
+					Spring.CreateUnit(bossunit, posx, posy, posz, math_random(0,3),GaiaTeamID)
+					FinalBossUnitSpawned = true
+					Spring.Echo("BOSS COMMANDER SPAWNED")
+				elseif ScavengerStartboxExists == true then
+					local posx = math.floor((ScavengerStartboxXMin + ScavengerStartboxXMax)/2)
+					local posz = math.floor((ScavengerStartboxZMin + ScavengerStartboxZMax)/2)
+					local posy = Spring.GetGroundHeight(posx, posz)
+					Spring.CreateUnit(bossunit, posx, posy, posz, math_random(0,3),GaiaTeamID)
+					FinalBossUnitSpawned = true
+					Spring.Echo("BOSS COMMANDER SPAWNED")
+				else
+					local posx = math.floor(mapsizeX/2)
+					local posz = math.floor(mapsizeZ/2)
+					local posy = Spring.GetGroundHeight(posx, posz)
+					Spring.CreateUnit(bossunit, posx, posy, posz, math_random(0,3),GaiaTeamID)
+					FinalBossUnitSpawned = true
+					Spring.Echo("BOSS COMMANDER SPAWNED")
+				end
 			end
-			
-			local posx,posy,posz = Spring.GetUnitPosition(pickedBeacon)
-			local posy = Spring.GetGroundHeight(posx, posz)
-			local posradius = 256
-			
-			Spring.CreateUnit(bossunit, posx, posy, posz, math_random(0,3),GaiaTeamID)
-			FinalBossUnitSpawned = true
-			Spring.Echo("BOSS COMMANDER SPAWNED")
+			pickedBeacon = nil
 
 		elseif (not unitSpawnerModuleConfig.FinalBossUnit) or FinalBossKilled == true then
 			local units = Spring.GetTeamUnits(GaiaTeamID)
