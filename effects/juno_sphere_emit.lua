05f91eed72943606a322183401806b0275992bed
icexuick <info@ixstudios.nl>
2020-06-03 23:13:49 +0200

    Juno Particle Fix

--- effects/juno_sphere_emit.lua
+++ effects/juno_sphere_emit.lua
@@ -1,33 +1,33 @@
 return {
 	["juno_sphere_emit"] = {
-		["nonodecay"] = {
+		"nonodecay" = {
 			class = [[CSimpleParticleSystem]],
 			air = true,
 			water = true,
 			ground = true,
 			unit = true,
-			count = 3,
+			count = 4,
 			properties =  {
-				airdrag = 0.95,
-				sizeGrowth = -0.02,
+				airdrag = 0.92,
+				sizeGrowth = -0.03,
 				sizeMod = 1.0,
 				pos = [[0, 1, 0]],
 				emitVector = [[0, 1, 0]],
-				gravity = [[0,0.2,0]],
-				colorMap = [[0 0 0 0.0001   0.0 0.0 0.0 0.0001   0.0 0.0 0.0 0.7	  0.0 0.0 0.0 0.5 	0.0 0.0 0.0 0.001]],
-				texture = [[nanoball]],
-				particleLife = 5,
-				particleLifeSpread = 15,
+				gravity = [[0,0.1,0]],
+				colorMap = [[0.1 0.1 0.1 0.0001   0.4 0.4 0.4 0.4   0.4 0.2 0.2 0.5	  0.4 0.0 0.0 0.4 	0.0 0.0 0.0 0.001]],
+				texture = [[nanobeam-resurrect]],
+				particleLife = 30,
+				particleLifeSpread = 20,
 				numParticles = 1,
 				particleSpeed = 1,
-				particleSpeedSpread = 2,
-				particleSize = 2,
-				particleSizeSpread = 3,
+				particleSpeedSpread = 1.5,
+				particleSize = 1,
+				particleSizeSpread = 2,
 				directional = true,
 				emitRot = 40,
 				emitRotSpread = 32,
 				useAirLos = false;
-			}
+			},
 		},
 	},
 }
