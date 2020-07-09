d70ca99a8d61a9cc9094fa489f2fc2b062c96319
Beherith <mysterme@gmail.com>
2020-07-09 10:58:43 +0200

    add corkarg and corthud death animations

---
+++ luarules/gadgets/unit_death_animations.lua
@@ -0,0 +1,32 @@
+function gadget:GetInfo()
+	return {
+		name      = "Death Animations",
+		desc      = "Prevent moving of Dying units",
+		author    = "Beherith",
+		date      = "2020",
+		license   = "CC BY NC ND",
+		layer     = 1000,
+		enabled   = true,
+	}
+end
+
+if (not gadgetHandler:IsSyncedCode()) then
+	return
+end
+
+local hasDeathAnim = {
+  [UnitDefNames.corkarg.id] = true,
+  [UnitDefNames.corthud.id] = true,
+}
+
+local SetUnitNoSelect	= Spring.SetUnitNoSelect
+local GiveOrderToUnit	= Spring.GiveOrderToUnit
+local CMD_STOP = CMD.STOP
+
+function gadget:UnitDestroyed(unitID, unitDefID, teamID, attackerID, attackerDefID, attackerTeamID)
+	if hasDeathAnim[unitDefID] then
+		--Spring.Echo("gadget:UnitDestroyed",unitID, unitDefID, teamID, attackerID, attackerDefID, attackerTeamID)
+    SetUnitNoSelect(unitID,true)
+    GiveOrderToUnit(unitID, CMD_STOP, {}, 0)
+	end
+end
