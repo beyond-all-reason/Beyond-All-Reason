18d4867862d577e7e9170794d14bf846aed6f710
icexuick <info@ixstudios.nl>
2020-08-06 00:31:13 +0200

    Atmospherics for Centerrock Dry Fix

--- luarules/gadgets/atmosphereconfigs/Centerrock Remake Dry 1.2.lua
+++ luarules/gadgets/atmosphereconfigs/Centerrock Remake Dry 1.2.lua
@@ -5,39 +5,39 @@ function gadget:GameFrame(n)
 
 -- common foggy cliffs	
 	if n%360 == 0 then
-		SpawnCEGInPosition("fogdirty", 5437, 212, 3089)
-		SpawnCEGInPosition("fogdirty", 3658, 179, 4861)
-		SpawnCEGInPosition("fogdirty", 6594, 93, 1463)
-		SpawnCEGInPosition("fogdirty", 7414, 317, 1930)
-		SpawnCEGInPosition("fogdirty", 7224, 384, 5786)
-		SpawnCEGInPosition("fogdirty", 3034, 32, 3736)
+		SpawnCEGInPositionGround("fogdirty", 5437, 0, 3089)
+		SpawnCEGInPositionGround("fogdirty", 3658, 0, 4861)
+		SpawnCEGInPositionGround("fogdirty", 6594, 0, 1463)
+		SpawnCEGInPositionGround("fogdirty", 7414, 0, 1930)
+		SpawnCEGInPositionGround("fogdirty", 7224, 0, 5786)
+		SpawnCEGInPositionGround("fogdirty", 3034, 0, 3736)
 	end
 
 -- rare foggy cliffs	
 	if n%700 == 0 then
-		SpawnCEGInPosition("fogdirty", 2861, 407, 659)
-		SpawnCEGInPosition("fogdirty", 3687, 383, 4067)
-		SpawnCEGInPosition("fogdirty", 248, 150, 7927)
-		SpawnCEGInPosition("fogdirty", 1891, 255, 4373)
-		SpawnCEGInPosition("fogdirty", 1050, 450, 2410)
+		SpawnCEGInPositionGround("fogdirty", 2861, 0, 659)
+		SpawnCEGInPositionGround("fogdirty", 3687, 0, 4067)
+		SpawnCEGInPositionGround("fogdirty", 248, 0, 7927)
+		SpawnCEGInPositionGround("fogdirty", 1891, 0, 4373)
+		SpawnCEGInPositionGround("fogdirty", 1050, 0, 2410)
 	end
 
 -- super rare foggy cliffs	
 	if n%1100 == 0 then
-		SpawnCEGInPosition("fogdirty", 4026, 672, 4667)
-		SpawnCEGInPosition("fogdirty", 7104, 569, 2290)
+		SpawnCEGInPositionGround("fogdirty", 4026, 0, 4667)
+		SpawnCEGInPositionGround("fogdirty", 7104, 0, 2290)
 	end
 
 -- powerup heavy metal
 	if n%900 == 0 then
-		SpawnCEGInPosition("powerupwhite", 3447, 525, 4172)
-		SpawnCEGInPosition("powerupwhite", 3582, 496, 4383)
-		SpawnCEGInPosition("powerupwhite", 5076, 523, 3385)
-		SpawnCEGInPosition("powerupwhite", 4937, 513, 3099)
-		SpawnCEGInPosition("powerupwhite", 7530, 210, 272)
-		SpawnCEGInPosition("powerupwhite", 7772, 206, 276)
-		SpawnCEGInPosition("powerupwhite", 588, 156, 7937)
-		SpawnCEGInPosition("powerupwhite", 814, 144, 8011)
+		SpawnCEGInPositionGround("powerupwhite", 3447, 0, 4172)
+		SpawnCEGInPositionGround("powerupwhite", 3582, 0, 4383)
+		SpawnCEGInPositionGround("powerupwhite", 5076, 0, 3385)
+		SpawnCEGInPositionGround("powerupwhite", 4937, 0, 3099)
+		SpawnCEGInPositionGround("powerupwhite", 7530, 0, 272)
+		SpawnCEGInPositionGround("powerupwhite", 7772, 0, 276)
+		SpawnCEGInPositionGround("powerupwhite", 588, 0, 7937)
+		SpawnCEGInPositionGround("powerupwhite", 814, 0, 8011)
 	end
 
 -- rare sandclouds
