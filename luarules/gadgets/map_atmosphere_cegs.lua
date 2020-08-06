eb6f467cc93eb01ec5ebdb7642f1327a636f2d12
Damgam <c_2_damiano@op.pl>
2020-08-06 18:41:28 +0200

    atmosphere gadget update

--- luarules/gadgets/map_atmosphere_cegs.lua
+++ luarules/gadgets/map_atmosphere_cegs.lua
@@ -21,52 +21,73 @@ if (not gadgetHandler:IsSyncedCode()) then
 	return false
 end
 
-function SpawnCEGInPosition(cegname, posx, posy, posz, sound)
+function SpawnCEGInPosition(cegname, posx, posy, posz, sound, soundvolume)
 	spSCEG(cegname, posx, posy, posz)
+	if sound then
+		Spring.PlaySoundFile(sound, soundvolume, posx, posy, posz, 'sfx')
+	end
 end
 
-function SpawnCEGInPositionGround(cegname, posx, addposy, posz, sound)
-	local posy = spGroundHeight(posx, posz) + (addposy or 0)
+function SpawnCEGInPositionGround(cegname, posx, groundOffset, posz, sound, soundvolume)
+	local posy = spGroundHeight(posx, posz) + (groundOffset or 0)
 	spSCEG(cegname, posx, posy, posz)
+	if sound then
+		Spring.PlaySoundFile(sound, soundvolume, posx, posy, posz, 'sfx')
+	end
 end
 
-function SpawnCEGInArea(cegname, midposx, posy, midposz, radius, sound)
+function SpawnCEGInArea(cegname, midposx, posy, midposz, radius, sound, soundvolume)
 	local posx = midposx+mathrandom(-radius,radius)
 	local posz = midposz+mathrandom(-radius,radius)
 	spSCEG(cegname, posx, posy, posz)
+	if sound then
+		Spring.PlaySoundFile(sound, soundvolume, posx, posy, posz, 'sfx')
+	end
 end
 
-function SpawnCEGInAreaGround(cegname, midposx, addposy, midposz, radius, sound)
+function SpawnCEGInAreaGround(cegname, midposx, groundOffset, midposz, radius, sound, soundvolume)
 	local posx = midposx+mathrandom(-radius,radius)
 	local posz = midposz+mathrandom(-radius,radius)
-	local posy = spGroundHeight(posx, posz) + (addposy or 0)
+	local posy = spGroundHeight(posx, posz) + (groundOffset or 0)
 	spSCEG(cegname, posx, posy, posz)
+	if sound then
+		Spring.PlaySoundFile(sound, soundvolume, posx, posy, posz, 'sfx')
+	end
 end
 
-function SpawnCEGInRandomMapPos(cegname, addposy, sound)
+function SpawnCEGInRandomMapPos(cegname, groundOffset, sound, soundvolume)
 	local posx = mathrandom(0,mapsizeX)
 	local posz = mathrandom(0,mapsizeZ)
-	local posy = spGroundHeight(posx, posz) + (addposy or 0)
+	local posy = spGroundHeight(posx, posz) + (groundOffset or 0)
 	spSCEG(cegname, posx, posy, posz)
+	if sound then
+		Spring.PlaySoundFile(sound, soundvolume, posx, posy, posz, 'sfx')
+	end
 end
 
-function SpawnCEGInRandomMapPosBelowY(cegname, addposy, belowposy, sound)
+function SpawnCEGInRandomMapPosBelowY(cegname, groundOffset, spawnOnlyBelowY, sound, soundvolume)
 	for i = 1,100 do
 		local posx = mathrandom(0,mapsizeX)
 		local posz = mathrandom(0,mapsizeZ)
 		local groundposy = spGroundHeight(posx, posz)
-		local posy = spGroundHeight(posx, posz) + (addposy or 0)
-		if groundposy <= belowposy then
+		local posy = spGroundHeight(posx, posz) + (groundOffset or 0)
+		if groundposy <= spawnOnlyBelowY then
 			spSCEG(cegname, posx, posy, posz)
+			if sound then
+				Spring.PlaySoundFile(sound, soundvolume, posx, posy, posz, 'sfx')
+			end
 			break
 		end
 	end
 end
 
-function SpawnCEGInRandomMapPosPresetY(cegname, posy, sound)
+function SpawnCEGInRandomMapPosPresetY(cegname, posy, sound, soundvolume)
 	local posx = mathrandom(0,mapsizeX)
 	local posz = mathrandom(0,mapsizeZ)
 	spSCEG(cegname, posx, posy, posz)
+	if sound then
+		Spring.PlaySoundFile(sound, soundvolume, posx, posy, posz, 'sfx')
+	end
 end
 
 VFS.Include("luarules/gadgets/atmosphereconfigs/" .. mapname .. ".lua")
