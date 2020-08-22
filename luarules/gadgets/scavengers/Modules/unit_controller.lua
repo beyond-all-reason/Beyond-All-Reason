751e53f307b893d2dcd667fcdd5c3cd3a6d330ab
Damgam <c_2_damiano@op.pl>
2020-08-22 23:15:46 +0200

    fix attempt

--- luarules/gadgets/scavengers/Modules/unit_controller.lua
+++ luarules/gadgets/scavengers/Modules/unit_controller.lua
@@ -31,7 +31,7 @@ function SelfDestructionControls(n, scav, scavDef)
 				Constructing[scav] = false
 			end
 		end
-		local nearestselfd = Spring.GetUnitNearestEnemy(scav, UnitDefs[scavDef].maxWeaponRange + 200, false)
+		local nearestselfd = Spring.GetUnitNearestEnemy(scav, (UnitDefs[scavDef].maxWeaponRange + 200) or 1500, false)
 		if not nearestselfd and (oldselfdx[scav] and oldselfdy[scav] and oldselfdz[scav]) and (oldselfdx[scav] > selfdx[scav]-10 and oldselfdx[scav] < selfdx[scav]+10) and (oldselfdy[scav] > selfdy[scav]-10 and oldselfdy[scav] < selfdy[scav]+10) and (oldselfdz[scav] > selfdz[scav]-10 and oldselfdz[scav] < selfdz[scav]+10) then
 			if selfdx[scav] < mapsizeX and selfdx[scav] > 0 and selfdz[scav] < mapsizeZ and selfdz[scav] > 0 then
 				if not scavConstructor[scav] or Constructing[scav] == false then
