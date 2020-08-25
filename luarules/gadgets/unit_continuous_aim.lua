3dfc8a7102179de9173784c403dcc46f5ea29065
Floris <floris@ruwhof.net>
2020-08-25 20:19:37 +0200

    stripped preaim code from continuous aim gadget into its own gadget

--- luarules/gadgets/unit_continuous_aim.lua
+++ luarules/gadgets/unit_continuous_aim.lua
@@ -1,18 +1,21 @@
 function gadget:GetInfo()
 	return {
-		name      = "Continuous Aim",
-		desc      = "Applies lower 'reaimTime for continuous aim'",
-		author    = "Doo",
-		date      = "April 2018",
-		license   = "Whatever works",
-		layer     = 0,
-		enabled   = true, -- When we will move on 105 :)
+		name = "Continuous Aim",
+		desc = "Applies lower 'reaimTime for continuous aim'",
+		author = "Doo",
+		date = "April 2018",
+		license = "Whatever works",
+		layer = 0,
+		enabled = true, -- When we will move on 105 :)
 	}
 end
-if (not gadgetHandler:IsSyncedCode()) then return end
+
+if not gadgetHandler:IsSyncedCode() then
+	return
+end
 
 local convertedUnits = {
-  -- value is reaimtime in frames, engine default is 15
+	-- value is reaimtime in frames, engine default is 15
 	--[UnitDefNames.armart.id] = true,
 	--[UnitDefNames.armfav.id] = true,
 	--[UnitDefNames.armflash.id] = true,
@@ -60,21 +63,19 @@ local convertedUnits = {
 	--[UnitDefNames.corkarg.id] = true,
 	--[UnitDefNames.corjugg.id] = true,
 	[UnitDefNames.armvang.id] = 1,
-  
-  -- the following units get a faster reaimtime to counteract their turret acceleration
-  [UnitDefNames.corhlt.id] = 5,
-  [UnitDefNames.corfhlt.id] = 5,
-  [UnitDefNames.cordoom.id] = 5,
-  [UnitDefNames.corshiva.id] = 5,
-  [UnitDefNames.corcat.id] = 5,
-  [UnitDefNames.corkarg.id] = 5,
-  [UnitDefNames.corbhmth.id] = 5,
-  [UnitDefNames.cortoast.id] = 5,
-  [UnitDefNames.corbats.id] = 5,
-  [UnitDefNames.corblackhy.id] = 5,
-  [UnitDefNames.corscreamer.id] = 5,
-  
-  
+
+	-- the following units get a faster reaimtime to counteract their turret acceleration
+	[UnitDefNames.corhlt.id] = 5,
+	[UnitDefNames.corfhlt.id] = 5,
+	[UnitDefNames.cordoom.id] = 5,
+	[UnitDefNames.corshiva.id] = 5,
+	[UnitDefNames.corcat.id] = 5,
+	[UnitDefNames.corkarg.id] = 5,
+	[UnitDefNames.corbhmth.id] = 5,
+	[UnitDefNames.cortoast.id] = 5,
+	[UnitDefNames.corbats.id] = 5,
+	[UnitDefNames.corblackhy.id] = 5,
+	[UnitDefNames.corscreamer.id] = 5,
 }
 
 for udid, ud in pairs(UnitDefs) do
@@ -100,46 +101,13 @@ for unitDefID, unitDef in pairs(UnitDefs) do
 	end
 end
 
-local weaponRange = {}
-for weaponDefID, def in pairs(WeaponDefs) do
-	weaponRange[weaponDefID] = def.range
-end
-
-local popups = {	-- exclude auto target range boost for popup units
-	[UnitDefNames.armclaw.id] = true,
-	[UnitDefNames.armpb.id] = true,
-	[UnitDefNames.armamb.id] = true,
-	[UnitDefNames.cormaw.id] = true,
-	[UnitDefNames.corvipe.id] = true,
-	[UnitDefNames.corpun.id] = true,
-	[UnitDefNames.corexp.id] = true,
-
-	[UnitDefNames.corllt.id] = true,
-	[UnitDefNames.armllt.id] = true,
-}
-if UnitDefNames['armcom_scav'] then
-	local scavengerPopups = {}
-	for k,v in pairs(popups) do
-		scavengerPopups[k..'_scav'] = v
-	end
-	for k,v in pairs(scavengerPopups) do
-		popups[k] = v
-	end
-	scavengerPopups = nil
-end
-
-function gadget:UnitCreated(unitID,unitDefID)
+function gadget:UnitCreated(unitID, unitDefID)
 	if convertedUnits[unitDefID] and unitWeapons[unitDefID] then
 		for id, _ in pairs(unitWeapons[unitDefID]) do
-      -- NOTE: this will prevent unit from firing if it does not IMMEDIATELY return from AimWeapon (no sleeps, not wait for turns!)
-      -- So you have to manually check in script if it is at the desired heading
-      -- https://springrts.com/phpbb/viewtopic.php?t=36654
+			-- NOTE: this will prevent unit from firing if it does not IMMEDIATELY return from AimWeapon (no sleeps, not wait for turns!)
+			-- So you have to manually check in script if it is at the desired heading
+			-- https://springrts.com/phpbb/viewtopic.php?t=36654
 			Spring.SetUnitWeaponState(unitID, id, "reaimTime", convertedUnits[unitDefID])
-		end
-	end
-	if not popups[unitDefID] and unitWeapons[unitDefID] then
-		for id, wdefID in pairs(unitWeapons[unitDefID]) do
-			Spring.SetUnitWeaponState(unitID, id, "autoTargetRangeBoost", (0.1*weaponRange[wdefID]) or 20)
 		end
 	end
 end
---
+++ luarules/gadgets/unit_preaim.lua
@@ -0,0 +1,66 @@
+function gadget:GetInfo()
+	return {
+		name = "Pre-aim",
+		desc = "Makes units preaim their weapons before its actually in range'",
+		author = "Doo, Floris",
+		date = "April 2018",
+		license = "Whatever works",
+		layer = 0,
+		enabled = true, -- When we will move on 105 :)
+	}
+end
+
+if not gadgetHandler:IsSyncedCode() then
+	return
+end
+
+local isPreaimUnit = {}
+for unitDefID, unitDef in pairs(UnitDefs) do
+	if #unitDef.weapons > 0 then
+		for id, table in pairs(unitDef.weapons) do
+			if not isPreaimUnit[unitDefID] then
+				isPreaimUnit[unitDefID] = {}
+			end
+			isPreaimUnit[unitDefID][id] = table.weaponDef
+		end
+	end
+end
+
+local weaponRange = {}
+for weaponDefID, def in pairs(WeaponDefs) do
+	weaponRange[weaponDefID] = def.range
+end
+
+local popups = {    -- exclude auto target range boost for popup units
+	[UnitDefNames.armclaw.id] = true,
+	[UnitDefNames.armpb.id] = true,
+	[UnitDefNames.armamb.id] = true,
+	[UnitDefNames.cormaw.id] = true,
+	[UnitDefNames.corvipe.id] = true,
+	[UnitDefNames.corpun.id] = true,
+	[UnitDefNames.corexp.id] = true,
+
+	[UnitDefNames.corllt.id] = true,
+	[UnitDefNames.armllt.id] = true,
+}
+local scavengerPopups = {}
+for k, v in pairs(popups) do
+	scavengerPopups[k .. '_scav'] = v
+end
+for k, v in pairs(scavengerPopups) do
+	popups[k] = v
+end
+scavengerPopups = nil
+
+-- exclude popups here
+for k, v in pairs(popups) do
+	isPreaimUnit[k] = nil
+end
+
+function gadget:UnitCreated(unitID, unitDefID)
+	if isPreaimUnit[unitDefID] then
+		for id, wdefID in pairs(isPreaimUnit[unitDefID]) do
+			Spring.SetisPreaimUnittate(unitID, id, "autoTargetRangeBoost", (0.1 * weaponRange[wdefID]) or 20)
+		end
+	end
+end
