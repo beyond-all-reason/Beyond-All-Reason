084ccae3c9381bf7e1492e9228d20e654d2ca0d9
Floris <floris@ruwhof.net>
2020-08-28 20:54:28 +0200

    airjets: added thrusters for scavenger planes

--- luaui/Widgets_BAR/gfx_airjets.lua
+++ luaui/Widgets_BAR/gfx_airjets.lua
@@ -251,6 +251,30 @@ local effectDefs = {
 	},
 }
 
+local function deepcopy(orig)
+	local orig_type = type(orig)
+	local copy
+	if orig_type == 'table' then
+		copy = {}
+		for orig_key, orig_value in next, orig, nil do
+			copy[deepcopy(orig_key)] = deepcopy(orig_value)
+		end
+		setmetatable(copy, deepcopy(getmetatable(orig)))
+	else -- number, string, boolean, etc
+		copy = orig
+	end
+	return copy
+end
+
+for name, effects in pairs(effectDefs) do
+	if UnitDefNames[name..'_scav'] then
+		effectDefs[name..'_scav'] = deepcopy(effects)
+		for i,effect in pairs(effects) do
+			effectDefs[name..'_scav'][i].color = {0.6, 0.12, 0.7}
+		end
+	end
+end
+
 local distortion = 0.008
 local animSpeed = 3
 local jitterWidthScale = 3
