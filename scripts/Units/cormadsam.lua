92da28c806537cbfc5bc8b33df0cfb6574017830
Floris <floris@ruwhof.net>
2020-09-11 21:18:18 +0200

    madsam/frock spin turret after firing again

--- scripts/Units/cormadsam.lua
+++ scripts/Units/cormadsam.lua
@@ -39,7 +39,7 @@
 		Hide(flare8)
 
 		StartThread(dmgsmoke, dmgPieces)
-		
+
                 Turn(launcher, x_axis, math.rad(-55.000000), math.rad(60.000000))
                 WaitForTurn(launcher, x_axis)
 
@@ -48,32 +48,32 @@
 
         local function RestoreAfterDelay(unitID)
                 Sleep(2500)
-                Turn(turret, x_axis, 0, math.rad(50))
-		Turn(launcher, x_axis, 0, math.rad(50))
+                --Turn(turret, x_axis, 0, math.rad(50))
+				--Turn(launcher, x_axis, 0, math.rad(50))
         end
 
 
         function script.QueryWeapon1()
-                if (currBarrel == 1) then 
+                if (currBarrel == 1) then
                         return flare1 end
-                if (currBarrel == 2) then 
+                if (currBarrel == 2) then
                         return flare2 end
-                if (currBarrel == 3) then 
+                if (currBarrel == 3) then
                         return flare3 end
-                if (currBarrel == 4) then 
+                if (currBarrel == 4) then
                         return flare4 end
-                if (currBarrel == 5) then 
+                if (currBarrel == 5) then
                         return flare5 end
-                if (currBarrel == 6) then 
+                if (currBarrel == 6) then
                         return flare6 end
-                if (currBarrel == 7) then 
+                if (currBarrel == 7) then
                         return flare7 end
-                if (currBarrel == 8) then 
+                if (currBarrel == 8) then
                         return flare8 end
         end
-        
+
         function script.AimFromWeapon1() return turret end
-        
+
         function script.AimWeapon1( heading, pitch )
                 Signal(SIG_AIM)
                 SetSignalMask(SIG_AIM)
@@ -83,7 +83,7 @@
                 WaitForTurn(launcher, x_axis)
                 return true
         end
-        
+
         function script.FireWeapon1()
                 if currBarrel == 1 then
 			--EmitSfx(flare1, 1024+0)
@@ -92,46 +92,49 @@
 
                 if currBarrel == 2 then
 			--EmitSfx(flare2, 1024+0)
-			Sleep (150)	
+			Sleep (150)
                 end
 
 
                 if currBarrel == 3 then
 			--EmitSfx(flare3, 1024+0)
-			Sleep (150)	
+			Sleep (150)
                 end
 
 
                 if currBarrel == 4 then
 			--EmitSfx(flare4, 1024+0)
-			Sleep (150)	
+			Sleep (150)
                 end
 
                 if currBarrel == 5 then
 			--EmitSfx(flare5, 1024+0)
-			Sleep (150)	
+			Sleep (150)
                 end
 
                 if currBarrel == 6 then
 			--EmitSfx(flare6, 1024+0)
-			Sleep (150)	
+			Sleep (150)
                 end
 
                 if currBarrel == 7 then
 			--EmitSfx(flare7, 1024+0)
-			Sleep (150)	
+			Sleep (150)
                 end
 
                 if currBarrel == 8 then
 			--EmitSfx(flare8, 1024+0)
-			Sleep (150)	
+			Sleep (150)
                 end
 
                 currBarrel = currBarrel + 1
 
                 if currBarrel == 9 then currBarrel = 1 end
+
+			Sleep (3000)
+			StartThread (animSpin, unitID, turret, y_axis, math.rad(25.000000))
         end
-        
+
 	function script.Killed(recentDamage, maxHealth)
 		local severity = recentDamage / maxHealth
 
