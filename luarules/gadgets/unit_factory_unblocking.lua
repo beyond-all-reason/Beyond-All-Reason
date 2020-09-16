bfd03f63fb8d888d4ecdaeed4e503ed0cb14ea67
Floris <floris@ruwhof.net>
2020-09-16 16:20:49 +0200

    factory unblocking: stripped unitisinbox check

--- luarules/gadgets/unit_factory_unblocking.lua
+++ luarules/gadgets/unit_factory_unblocking.lua
@@ -6,7 +6,7 @@ end
 function gadget:GetInfo()
   return {
     name      = "Factory Unblocking",
-    desc      = "Set new factory unit unblocking when factory is footprint is occupied ",
+    desc      = "This prevents exiting units get stuck on the newly initiated (big) unit",
     author    = "Floris",
     date      = "September 2020",
     license   = "GNU GPL, v2 or later",
@@ -15,25 +15,18 @@ function gadget:GetInfo()
   }
 end
 
-local spGetUnitPosition = Spring.GetUnitPosition
-local spGetUnitsInBox  = Spring.GetUnitsInBox
-local boxHeight = 30
 local setBlockingOnFinished = {}
 local factoryUnits = {}
 local isFactory = {}
 for unitDefID, unitDef in pairs(UnitDefs) do
 	if unitDef.isFactory and #unitDef.buildOptions > 0 then
-		isFactory[unitDefID] = {unitDef.xsize, unitDef.zsize}
+		isFactory[unitDefID] = true
 	end
 end
 
 function gadget:UnitFinished(unitID, unitDefID, unitTeam)
 	if isFactory[unitDefID] then
 		factoryUnits[unitID] = isFactory[unitDefID]
-		local x,y,z = spGetUnitPosition(unitID)
-		factoryUnits[unitID][3] = x
-		factoryUnits[unitID][4] = y
-		factoryUnits[unitID][5] = z
 	end
 	if setBlockingOnFinished[unitID] then
 		Spring.SetUnitBlocking(unitID, true)
@@ -41,14 +34,10 @@ function gadget:UnitFinished(unitID, unitDefID, unitTeam)
 	end
 end
 
-
 function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
 	if factoryUnits[builderID] then
-		local units = spGetUnitsInBox(factoryUnits[builderID][3]-(factoryUnits[builderID][1]/2), factoryUnits[builderID][4]-boxHeight, factoryUnits[builderID][5]-(factoryUnits[builderID][2]/2), factoryUnits[builderID][3]+(factoryUnits[builderID][1]/2), factoryUnits[builderID][4]+boxHeight, factoryUnits[builderID][5]+(factoryUnits[builderID][2]/2))
-		if #units > 1 then
-			Spring.SetUnitBlocking(unitID, false)
-			setBlockingOnFinished[unitID] = true
-		end
+		Spring.SetUnitBlocking(unitID, false)
+		setBlockingOnFinished[unitID] = true
 	end
 end
 
