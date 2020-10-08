00ca55390d1493daac2a4c84589e592f943c3d36
icexuick <info@ixstudios.nl>
2020-10-08 13:30:49 +0200

    Accidentally made AdvSol Volatile :)
    not anymore.

--- units/ArmBuildings/LandEconomy/armadvsol.lua
+++ units/ArmBuildings/LandEconomy/armadvsol.lua
@@ -22,7 +22,7 @@ return {
 		description = "Produces 75 Energy",
 		energymake = 75,
 		energystorage = 100,
-		explodeas = "mediumBuildingExplosionGeneric",
+		explodeas = "smallBuildingExplosionGeneric",
 		footprintx = 4,
 		footprintz = 4,
 		icontype = "building",
