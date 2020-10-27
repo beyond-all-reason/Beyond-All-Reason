706cb5d468ee590696ee38207e10fe977e82573c
Damgam <c_2_damiano@op.pl>
2020-10-27 10:59:23 +0100

    scav update - more lootboxes

--- luarules/gadgets/scavengers/Blueprints/BYAR/Constructor/IceXuick_lootboxes.lua
+++ luarules/gadgets/scavengers/Blueprints/BYAR/Constructor/IceXuick_lootboxes.lua
@@ -20,7 +20,7 @@ local nameSuffix = '_scav'
 
 local function IceXuickLootboxT1(scav, posx, posy, posz, GaiaTeamID, radiusCheck)
     local posradius = 48
-	local r = math_random(0,4)
+	local r = 0 --math_random(0,4)
         if radiusCheck then
             return posradius
         else
@@ -35,7 +35,7 @@ table.insert(ScavengerConstructorBlueprintsT0Sea,IceXuickLootboxT1)
 
 local function IceXuickLootboxT2(scav, posx, posy, posz, GaiaTeamID, radiusCheck)
     local posradius = 48
-    local r = math_random(0,5)
+    local r = 0 --math_random(0,5)
         if radiusCheck then
             return posradius
         else
@@ -49,7 +49,7 @@ table.insert(ScavengerConstructorBlueprintsT1Sea,IceXuickLootboxT2)
 
 local function IceXuickLootboxT3(scav, posx, posy, posz, GaiaTeamID, radiusCheck)
     local posradius = 48
-    local r = math_random(0,6)
+    local r = 0 --math_random(0,6)
         if radiusCheck then
             return posradius
         else
@@ -63,7 +63,7 @@ table.insert(ScavengerConstructorBlueprintsT2Sea,IceXuickLootboxT3)
 
 local function IceXuickLootboxT4(scav, posx, posy, posz, GaiaTeamID, radiusCheck)
     local posradius = 48
-	local r = math_random(0,10)
+	local r = 0 --math_random(0,10)
         if radiusCheck then
             return posradius
         else
@@ -80,7 +80,7 @@ table.insert(ScavengerConstructorBlueprintsT3Sea,IceXuickLootboxT4)
 
 local function IceXuickSpecNanoT1(scav, posx, posy, posz, GaiaTeamID, radiusCheck)
     local posradius = 64
-	local r = math_random(0,1)
+	local r = 0 --math_random(0,1)
 	local options = {UDN.lootboxnano_t1_var1_scav.id,UDN.lootboxnano_t1_var2_scav.id,UDN.lootboxnano_t1_var3_scav.id,UDN.lootboxnano_t1_var4_scav.id}
 	if radiusCheck then
 		return posradius
@@ -95,7 +95,7 @@ table.insert(ScavengerConstructorBlueprintsT1Sea,IceXuickSpecNanoT1)
 
 local function IceXuickSpecNanoT2(scav, posx, posy, posz, GaiaTeamID, radiusCheck)
     local posradius = 80
-	local r = math_random(0,2)
+	local r = 0 --math_random(0,2)
 	local options = {UDN.lootboxnano_t2_var1_scav.id,UDN.lootboxnano_t2_var2_scav.id,UDN.lootboxnano_t2_var3_scav.id,UDN.lootboxnano_t2_var4_scav.id}
 	if radiusCheck then
 		return posradius
@@ -110,7 +110,7 @@ table.insert(ScavengerConstructorBlueprintsT2Sea,IceXuickSpecNanoT2)
 
 local function IceXuickSpecNanoT3(scav, posx, posy, posz, GaiaTeamID, radiusCheck)
     local posradius = 112
-	local r = math_random(0,2)
+	local r = 0 --math_random(0,2)
 	local options = {UDN.lootboxnano_t3_var1_scav.id,UDN.lootboxnano_t3_var2_scav.id,UDN.lootboxnano_t3_var3_scav.id,UDN.lootboxnano_t3_var4_scav.id}
 	if radiusCheck then
 		return posradius
@@ -125,7 +125,7 @@ table.insert(ScavengerConstructorBlueprintsT3Sea,IceXuickSpecNanoT3)
 
 local function IceXuickSpecNanoT4(scav, posx, posy, posz, GaiaTeamID, radiusCheck)
     local posradius = 128
-	local r = math_random(0,2)
+	local r = 0 --math_random(0,2)
 	local options = {UDN.lootboxnano_t4_var1_scav.id,UDN.lootboxnano_t4_var2_scav.id,UDN.lootboxnano_t4_var3_scav.id,UDN.lootboxnano_t4_var4_scav.id}
 	if radiusCheck then
 		return posradius
