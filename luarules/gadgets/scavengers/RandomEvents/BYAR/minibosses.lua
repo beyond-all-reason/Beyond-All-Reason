ac25883495aa53b01e7d8238402058b674b46ff3
Damgam <c_2_damiano@op.pl>
2020-11-05 12:23:42 +0100

    fixed miniboss event

--- luarules/gadgets/scavengers/RandomEvents/BYAR/minibosses.lua
+++ luarules/gadgets/scavengers/RandomEvents/BYAR/minibosses.lua
@@ -1,6 +1,5 @@
 
 function RandomEventMiniboss1(CurrentFrame)
-	ScavSendNotification("scav_eventminiboss")
 	local T2bosses = {"corsumo_scav","corgol_scav","corparrow_scav","armfboy_scav","armmanni_scav",}
 	local T3bosses = {"armmar_scav","armvang_scav","armraz_scav","corshiva_scav","corkarg_scav","corcat_scav","armlun_scav","corsok_scav","armsptkt4_scav",}
 	local T4bosses = {"corkorg_scav", "corjugg_scav", "armbanth_scav","armpwt4_scav","armrattet4_scav","armvadert4_scav","cordemont4_scav","corkarganetht4_scav",}
@@ -8,18 +7,9 @@ function RandomEventMiniboss1(CurrentFrame)
 		local posx = math_random(300,mapsizeX-300)
 		local posz = math_random(300,mapsizeZ-300)
 		local posy = Spring.GetGroundHeight(posx, posz)
-		CanSpawnEvent = posOccupied(posx, posy, posz, 300)
-		if CanSpawnEvent then
-			CanSpawnEvent = posCheck(posx, posy, posz, 300)
-		end
-		if CanSpawnEvent then
-			CanSpawnEvent = posLosCheckNoRadar(posx, posy, posz, 300)
-		end
+		CanSpawnEvent = posLosCheckNoRadar(posx, posy, posz, 300)
 		if CanSpawnEvent then
 			CanSpawnEvent = posLandCheck(posx, posy, posz, 300)
-		end
-		if CanSpawnEvent then
-			CanSpawnEvent = posMapsizeCheck(posx, posy, posz, 300)
 		end
 		if CanSpawnEvent then
 			if globalScore < scavconfig.timers.T2start then
@@ -36,6 +26,7 @@ function RandomEventMiniboss1(CurrentFrame)
 				local unit = T4bosses[math_random(1,#T4bosses)]
 				Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
 			end
+			ScavSendNotification("scav_eventminiboss")
 			break
 		end
 	end
