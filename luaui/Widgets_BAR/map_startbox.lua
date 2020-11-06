d8368987385c590ee41442fa295f05902830033e
Beherith <mysterme@gmail.com>
2020-11-06 10:51:41 +0100

    Remove F4 info on metal view cause we only have maps that have proper spots

--- luaui/Widgets_BAR/map_startbox.lua
+++ luaui/Widgets_BAR/map_startbox.lua
@@ -66,8 +66,8 @@ local fontSize = 18
 local fontShadow = true        -- only shows if font has a white outline
 local shadowOpacity = 0.35
 
-local infotext = "Pick a startspot and click the Ready button. (F4 shows metal spots)"
-local infotextBoxes = "Pick a startspot within the green area, and click the Ready button. (F4 shows metal spots)"
+local infotext = "Pick a starting position and click the Ready button"
+local infotextBoxes = "Pick a starting position within the green area, and click the Ready button"
 local infotextFontsize = 13
 
 local comnameList = {}
