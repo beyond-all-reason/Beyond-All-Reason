12734a9fd408a54ed1299fa41485b76c263a1713
Damgam <c_2_damiano@op.pl>
2020-11-07 15:06:17 +0100

    scav update

--- luarules/gadgets/scavengers/RandomEvents/BYAR/minibosses.lua
+++ luarules/gadgets/scavengers/RandomEvents/BYAR/minibosses.lua
@@ -3,6 +3,7 @@ function RandomEventMiniboss1(CurrentFrame)
 	local T2bosses = {"corsumo_scav","corgol_scav","corparrow_scav","armfboy_scav","armmanni_scav",}
 	local T3bosses = {"armmar_scav","armvang_scav","armraz_scav","corshiva_scav","corkarg_scav","corcat_scav","armlun_scav","corsok_scav","armsptkt4_scav",}
 	local T4bosses = {"corkorg_scav", "corjugg_scav", "armbanth_scav","armpwt4_scav","armrattet4_scav","armvadert4_scav","cordemont4_scav","corkarganetht4_scav",}
+	local baseNumber = math.ceil((spawnmultiplier*0.5)+(teamcount*0.5))
 	for i = 1,1000 do
 		local posx = math_random(300,mapsizeX-300)
 		local posz = math_random(300,mapsizeZ-300)
@@ -12,19 +13,66 @@ function RandomEventMiniboss1(CurrentFrame)
 			CanSpawnEvent = posLandCheck(posx, posy, posz, 300)
 		end
 		if CanSpawnEvent then
-			if globalScore < scavconfig.timers.T2start then
+			if globalScore < scavconfig.timers.T1low then
 				local unit = T2bosses[math_random(1,#T2bosses)]
-				for a = 1,3 do
+				for a = 1,baseNumber do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
+			elseif globalScore < scavconfig.timers.T1high then
+				local unit = T2bosses[math_random(1,#T2bosses)]
+				for a = 1,baseNumber*2 do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
+			elseif globalScore < scavconfig.timers.T2start then
+				local unit = T2bosses[math_random(1,#T2bosses)]
+				for a = 1,baseNumber*3 do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
+			elseif globalScore < scavconfig.timers.T2low then
+				local unit = T3bosses[math_random(1,#T3bosses)]
+				for a = 1,baseNumber do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
+			elseif globalScore < scavconfig.timers.T2high then
+				local unit = T3bosses[math_random(1,#T3bosses)]
+				for a = 1,baseNumber*2 do
 					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
 				end
 			elseif globalScore < scavconfig.timers.T3start then
 				local unit = T3bosses[math_random(1,#T3bosses)]
-				for a = 1,2 do
+				for a = 1,baseNumber*3 do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
+			elseif globalScore < scavconfig.timers.T3low then
+				local unit = T4bosses[math_random(1,#T4bosses)]
+				for a = 1,baseNumber do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
+			elseif globalScore < scavconfig.timers.T3high then
+				local unit = T4bosses[math_random(1,#T4bosses)]
+				for a = 1,baseNumber*2 do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
+			elseif globalScore < scavconfig.timers.T4start then
+				local unit = T4bosses[math_random(1,#T4bosses)]
+				for a = 1,baseNumber*3 do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
+			elseif globalScore < scavconfig.timers.T4low then
+				local unit = T4bosses[math_random(1,#T4bosses)]
+				for a = 1,baseNumber*5 do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
+			elseif globalScore < scavconfig.timers.T4high then
+				local unit = T4bosses[math_random(1,#T4bosses)]
+				for a = 1,baseNumber*7 do
 					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
 				end
 			else
 				local unit = T4bosses[math_random(1,#T4bosses)]
-				Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				for a = 1,baseNumber*9 do
+					Spring.CreateUnit(unit, posx+math_random(-300,300), posy, posz+math_random(-300,300), math_random(0,3),GaiaTeamID)
+				end
 			end
 			ScavSendNotification("scav_eventminiboss")
 			break
