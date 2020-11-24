9ce83ab37822276afa05503cc9b3cc75567f59c7
Beherith <mysterme@gmail.com>
2020-11-24 15:40:05 +0100

    Prevent some gadgets from causing load spikes on frame 0 with many features

--- luarules/gadgets/unit_rez_exp.lua
+++ luarules/gadgets/unit_rez_exp.lua
@@ -83,6 +83,7 @@ end
 
 function gadget:FeatureCreated(featureID)
 	local currentFrame = GetGameFrame()
+  if currentFrame < 1 then return end -- dont proces all map features created on frame 0 by game
 	local featureList = deadUnits[currentFrame]
 	if not featureList then
 		return
