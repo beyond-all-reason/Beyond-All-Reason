a149c6aaf4e325c8e8261266ed0d617bd1201d82
Floris <floris@ruwhof.net>
2020-11-24 23:16:17 +0100

    removed echo

--- luaui/Widgets_BAR/gui_buildmenu.lua
+++ luaui/Widgets_BAR/gui_buildmenu.lua
@@ -1324,7 +1324,6 @@ local function drawCell(cellRectID, usedZoom, cellColor, progress, highlightColo
 	-- group icon
 	if showGroupIcon and unitGroup[uDefID] then
 		glColor(1, 1, 1, 0.9)
-		Spring.Echo(groups[unitGroup[uDefID]])
 		glTexture(groups[unitGroup[uDefID]])
 		glTexRect(cellRects[cellRectID][1]+iconPadding, cellRects[cellRectID][4]-groupiconSize-iconPadding, cellRects[cellRectID][1] + groupiconSize + iconPadding, cellRects[cellRectID][4]-iconPadding)
 		glTexture(false)
