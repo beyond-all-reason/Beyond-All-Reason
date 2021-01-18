31a564e23600834224878434a5b09691c1d2c1c5
Beherith <mysterme@gmail.com>
2021-01-18 22:02:38 +0100

    fix corcom hatty boy

--- units/other/hats/hat_viking_teamcolored.lua
+++ units/other/hats/hat_viking_teamcolored.lua
@@ -22,7 +22,7 @@ return {
 		hidedamage = true,
     icontype = "blank",
 		idleautoheal = 100000, --so it doesnt die
-		mass = 0,
+		mass = 1,
 		maxdamage = 5000000,
 		maxslope = 64,
 		maxwaterdepth = 1000,
