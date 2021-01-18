0a891f6e3be440f9879fec9f5f8752a850316c34
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2021-01-18 14:51:07 -0500

    i18n: Add topbar to new interface file

--- language/interface-en.lua
+++ language/interface-en.lua
@@ -1,7 +1,57 @@
 return {
 	en = {
 		ui = {
-			
+			topbar = {
+				button = {
+					quit = 'Quit',
+					resign = 'Resign',
+					lobby = 'Lobby',
+					settings = 'Settings',
+					changes = 'Changes',
+					commands = 'Commands',
+					keys = 'Keys',
+					scavengers = 'Scavengers',
+					stats = 'Statistics',
+				},
+				quit = {
+					quit = 'Quit',
+					resign = 'Resign',
+					really_quit = 'Are you sure you want to quit?',
+					really_quitresign = 'Are you sure you want to resign or quit to desktop?',
+					really_resign = 'Are you sure you want to resign?',
+					really_resign2 = 'Are you sure you want to resign and spectate?',
+				},
+				catchingUp = 'Catching up',
+				catchingup_tooltip = 'Displays the catch-up progress',
+				comcount_tooltip = '\255\215\255\215Commander Counter\n\255\240\240\240Displays the number of ally\nand enemy commanders',
+				wind = {
+					nowind1 = 'No',
+					nowind2 = 'wind',
+					tooltip = '\255\215\255\215Wind Display\n\255\240\240\240Displays current wind strength (small numbers are minimum and maximum)\n\255\255\215\215It is better to build Solar Collectors when average\n\255\255\215\215wind speed is below 5 (Armada) or 6 (Cortex)',
+					worth1 = 'Wind is not worth it',
+					worth2 = 'Wind is viable',
+					worth3 = 'Wind is okay',
+					worth4 = 'Wind is good',
+					worth5 = 'Wind is really good',
+					worth6 = 'Wind is insanely good',
+				},
+				resbar = {
+					metal = 'Metal',
+					energy = 'Energy',
+					overflowing = 'Overflowing',
+					wastingmetal = 'Wasting metal',
+					wastingenergy = 'Wasting energy',
+					overflowing_energy_tooltip = 'Energy Share Slider\n\255\240\240\240Share excess energy energy above this point with your team',
+					overflowing_metal_tooltip = 'Metal Share Slider\n\255\240\240\240Share excess metal above this point with your team',
+					pull_tooltip = 'spending (per second)',
+					income_tooltip = 'income (per second)',
+					expense_tooltip = 'potential spending  (per second)',
+					storage_tooltip = 'storage',
+					conversion_tooltip = '\255\215\255\215Energy Conversion Slider\n\255\240\240\240Use excess energy above this point\nto power your Energy Converters',
+					current_energy_tooltip = 'Share energy to a specific player by\n1) Using the player list, click-and-drag the\n    energy icon to the right of their name\n2) Using the interface by pressing the H key',
+					current_metal_tooltip = 'Share metal to a specific player by\n1) Using the player list, click-and-drag the\n    metal icon to the right of their name\n2) Using the interface by pressing the H key',
+				},
+			},
 		}
 	}
 }
