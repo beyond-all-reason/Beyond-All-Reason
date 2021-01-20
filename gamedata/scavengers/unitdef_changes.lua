b39d3464452e00c4c26b8b68daeb9300cdd1c0bf
Beherith <mysterme@gmail.com>
2021-01-20 17:00:30 +0100

    misson editor compatibility

--- gamedata/scavengers/unitdef_changes.lua
+++ gamedata/scavengers/unitdef_changes.lua
@@ -7,7 +7,7 @@
 
 customDefs = {}
 
-scavDifficulty = Spring.GetModOptions().scavdifficulty or "easy"
+scavDifficulty = (Spring.GetModOptions and Spring.GetModOptions().scavdifficulty) or "easy" -- mission editor compat
 if scavDifficulty == "noob" then
 	ScavDifficultyMultiplier = 0.1
 elseif scavDifficulty == "easy" then
