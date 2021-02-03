5470dd4cf63868de239253bb1433bf45dbeba22d
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2021-02-02 19:49:27 -0500

    i18n: Add and include Spring command IDs

--- luaui/barwidgets.lua
+++ luaui/barwidgets.lua
@@ -26,6 +26,7 @@ include("savetable.lua")
 
 Spring.Utilities = {}
 VFS.Include("LuaRules/Utilities/tablefunctions.lua")
+VFS.Include("modules/constants/springConstants.lua")
 VFS.Include("modules/flowui/flowui.lua")
 VFS.Include("modules/i18n/i18n.lua")
 
