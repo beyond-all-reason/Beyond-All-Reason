65d009d68c625f2bdb962dc249a68236848be743
Damgam <c_2_damiano@op.pl>
2021-03-13 21:44:12 +0100

    gui soundeffects update

--- luarules/gadgets/gui_soundeffects.lua
+++ luarules/gadgets/gui_soundeffects.lua
@@ -12,8 +12,7 @@ function gadget:GetInfo()
 	}
 end
 
-DefaultSoundEffects = {
-    BaseSound = "cmd-defaultbaselayer",
+CommandSoundEffects = {
     Move = "cmd-move",
     LineMove = "cmd-move",   
     Fight = "cmd-fight",
@@ -28,10 +27,15 @@ DefaultSoundEffects = {
     Patrol = "cmd-fightxs", -- no patrol sound yet so i'm using fight
 }
 
+DefaultSoundEffects = {
+    BaseSoundMovementType = "cmd-defaultbaselayer",
+    BaseSoundWeaponType = "cmd-defaultbaselayer", 
+}
+
 UnitSoundEffects = {
-    armpw = {BaseSound = "cmd-defaultbaselayer",},
-    armstump = {BaseSound = "tnkt1canok",},
-    corak = {BaseSound = "cmd-defaultbaselayer",},
+    armpw = {BaseSoundMovementType = "cmd-defaultbaselayer",},
+    armstump = {BaseSoundMovementType = "tnkt1canok",},
+    corak = {BaseSoundMovementType = "cmd-defaultbaselayer",},
 }
 
 
@@ -45,6 +49,10 @@ Repair = CMD.REPAIR
 Reclaim = CMD.RECLAIM
 Dgun = CMD.DGUN
 Resurrect = CMD.RESURRECT
+
+--- create table with all unit sounds
+
+
 
 if gadgetHandler:IsSyncedCode() then -- Synced part
     
@@ -65,13 +73,21 @@ else -- Unsynced part
                 --Spring.Echo(unitName)
                 UsedFrame = CurrentFrame
 
-                -- posx, posy, posz = Spring.GetUnitPosition(unitID)
-                -- if UnitSoundEffects[unitName] and UnitSoundEffects[unitName].BaseSound then
+                -- local posx, posy, posz = Spring.GetUnitPosition(unitID)
+                -- if UnitSoundEffects[unitName] and UnitSoundEffects[unitName].BaseSoundMovementType then
                 --     --Spring.Echo(unitName.." base sound")
-                --     Spring.PlaySoundFile(UnitSoundEffects[unitName].BaseSound, 0.8, posx, posy, posz, 'unitreply')
+                --     Spring.PlaySoundFile(UnitSoundEffects[unitName].BaseSoundMovementType, 0.8, posx, posy, posz, 'unitreply')
                 -- else
                 --     --Spring.Echo("Generic base sound") 
-                --     Spring.PlaySoundFile(DefaultSoundEffects.BaseSound, 0.8, posx, posy, posz, 'unitreply')
+                --     Spring.PlaySoundFile(DefaultSoundEffects.BaseSoundMovementType, 0.8, posx, posy, posz, 'unitreply')
+                -- end
+
+                -- if UnitSoundEffects[unitName] and UnitSoundEffects[unitName].BaseSoundWeaponType then
+                --     --Spring.Echo(unitName.." base sound")
+                --     Spring.PlaySoundFile(UnitSoundEffects[unitName].BaseSoundWeaponType, 0.8, posx, posy, posz, 'unitreply')
+                -- else
+                --     --Spring.Echo("Generic base sound") 
+                --     Spring.PlaySoundFile(DefaultSoundEffects.BaseSoundWeaponType, 0.8, posx, posy, posz, 'unitreply')
                 -- end
 
 
@@ -84,26 +100,26 @@ else -- Unsynced part
                         Spring.PlaySoundFile(UnitSoundEffects[unitName].Move, 0.5, 2)
                         -- for cmd sounds in 3D use Spring.PlaySoundFile(UnitSoundEffects[unitName].Move, 0.75, posx1, posy1, posz1, 'sfx')
                     else
-                        Spring.PlaySoundFile(DefaultSoundEffects.Move, 0.5, 2)
+                        Spring.PlaySoundFile(CommandSoundEffects.Move, 0.5, 2)
                     end
                 elseif cmdID == Fight then
-                    Spring.PlaySoundFile(DefaultSoundEffects.Fight, 0.8, 2)
+                    Spring.PlaySoundFile(CommandSoundEffects.Fight, 0.8, 2)
                 elseif cmdID == Patrol then
-                    Spring.PlaySoundFile(DefaultSoundEffects.Patrol, 0.8, 2)
+                    Spring.PlaySoundFile(CommandSoundEffects.Patrol, 0.8, 2)
                 elseif cmdID == Guard then
-                    Spring.PlaySoundFile(DefaultSoundEffects.Guard, 0.8, 2)
+                    Spring.PlaySoundFile(CommandSoundEffects.Guard, 0.8, 2)
                 elseif cmdID == Groupselect then
-                    Spring.PlaySoundFile(DefaultSoundEffects.Groupselect, 0.8, 2)
+                    Spring.PlaySoundFile(CommandSoundEffects.Groupselect, 0.8, 2)
                 elseif cmdID == Repair then
-                    Spring.PlaySoundFile(DefaultSoundEffects.Repair, 0.6, 2)
+                    Spring.PlaySoundFile(CommandSoundEffects.Repair, 0.6, 2)
                 elseif cmdID == Reclaim then
-                    Spring.PlaySoundFile(DefaultSoundEffects.Reclaim, 0.8, 2)
+                    Spring.PlaySoundFile(CommandSoundEffects.Reclaim, 0.8, 2)
                 elseif cmdID == Dgun then
-                    Spring.PlaySoundFile(DefaultSoundEffects.Dgun, 0.8, 2)
+                    Spring.PlaySoundFile(CommandSoundEffects.Dgun, 0.8, 2)
                 elseif cmdID == Resurrect then
-                    Spring.PlaySoundFile(DefaultSoundEffects.Resurrect, 1, 2)
+                    Spring.PlaySoundFile(CommandSoundEffects.Resurrect, 1, 2)
                 elseif cmdID < 0 then
-                    Spring.PlaySoundFile(DefaultSoundEffects.Build, 0.5, 2) 
+                    Spring.PlaySoundFile(CommandSoundEffects.Build, 0.5, 2) 
                 end
             end
         end
