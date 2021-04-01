3d7735c01f186d9917eb32b33382a935de0f0f8a
Floris <floris@ruwhof.net>
2021-04-01 22:23:08 +0200

    created dgun projectile volume gadget

---
+++ weapons/dgun_projectile.lua
@@ -0,0 +1,17 @@
+return {
+	dgun_projectile = {
+		areaofeffect = 7,		-- probably needs AoE of around 7
+		cratermult = 0,
+		edgeeffectiveness = 0.98,
+		impulseboost = 0,
+		range = 16,
+		noselfdamage = 1,
+		customparams = {
+			expl_light_skip = 1,
+		},
+		damage = {
+			default = 99999,
+			scavboss = 1000,
+		},
+	},
+}
