a3b6ac2595797c7abad166fb3539d985874cd6bb
Floris <floris@ruwhof.net>
2021-04-06 00:21:53 +0200

    tweak

--- luarules/configs/Atmosphereconfigs/generic_config_setup.lua
+++ luarules/configs/Atmosphereconfigs/generic_config_setup.lua
@@ -11,16 +11,16 @@ local nightlenght = math.ceil(daylenght*0.35)
 if windmax < 5 then
     atmosphere = 1
 elseif windmax < 10 then
-    atmosphere = 1.20
+    atmosphere = 1.2
 elseif windmax < 20 then
-    atmosphere = 1.40
+    atmosphere = 1.35
 else
-    atmosphere = 1.60
+    atmosphere = 1.5
 end
 
 function gadget:GameFrame(n)
     if n%daylenght > nightlenght then
-		SendToUnsynced("MapAtmosphereConfigSetSun", 0.7, transitionSpeed, 0.7*atmosphere)
+		SendToUnsynced("MapAtmosphereConfigSetSun", 0.7, transitionSpeed, math.min(1, 0.7*atmosphere))
 	else
 		SendToUnsynced("MapAtmosphereConfigSetSun", 1, transitionSpeed, 1)
 	end
