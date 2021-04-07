740e0b87f77c7bc7aea7db13b4cff9c7aa284f19
Floris <floris@ruwhof.net>
2021-04-07 23:32:40 +0200

    showbuilderqueue: cleanup

--- luaui/Widgets/unit_showbuilderqueue.lua
+++ luaui/Widgets/unit_showbuilderqueue.lua
@@ -1,33 +1,28 @@
 function widget:GetInfo()
 	return {
-		name      = "Show Builder Queue",	-- old name: Show Build
+		name      = "Show Builder Queue",
 		desc      = "Shows buildings about to be built",
-		author    = "WarXperiment",
+		author    = "WarXperiment, Decay, Floris",
 		date      = "February 15, 2010",
 		license   = "GNU GPL, v2 or later",
-        version   = 6,
+        version   = 7,
         layer     = 55,
-		enabled   = true,  --  loaded by default?
+		enabled   = true,
     }
 end
 
-local showForCreatedUnits = false		-- keep drawing unitshape for building when it has a nanoframe but isnt finished
 local maxDisplayedUnits = 70
 local dontShowWhenDistIcon = true
 
 --Changelog
--- before v2 developed outside of BA by WarXperiment
+-- before v2 developed by WarXperiment
 -- v2 [teh]decay - fixed crash: Error in DrawWorld(): [string "LuaUI/Widgets/unit_showbuild.lua"]:82: bad argument #1 to 'GetTeamColor' (number expected, got no value)
 -- v3 [teh]decay - updated for spring 98 engine -- project page on github: https://github.com/jamerlan/unit_showbuild
 -- v4 Floris - lots of performance increases
 -- v5 Floris - cleanup, polishing and fixes
 -- v6 Floris - limited to not show when (would be) icon
+-- v7 Floris - simplified/cleanup
 
-local command = {}
-local commandOrdered = {}
-local commandCreatedUnits = {}
-local commandCreatedUnitsIDs = {}
-local builders = {}
 local myPlayerID = Spring.GetMyPlayerID()
 local myAllyTeamID = Spring.GetMyAllyTeamID()
 local spec,fullview,_ = Spring.GetSpectatingState()
@@ -38,7 +33,9 @@ local spGetUnitTeam = Spring.GetUnitTeam
 local spGetGroundHeight = Spring.GetGroundHeight
 local spIsAABBInView = Spring.IsAABBInView
 local spGetCameraPosition = Spring.GetCameraPosition
+local spGetUnitPosition = Spring.GetUnitPosition
 local diag = math.diag
+local floor = math.floor
 
 local glPushMatrix = gl.PushMatrix
 local glLoadIdentity = gl.LoadIdentity
@@ -47,14 +44,29 @@ local glRotate = gl.Rotate
 local glUnitShape = gl.UnitShape
 local glPopMatrix = gl.PopMatrix
 
-local builderUnitDefs = {}
 local disticon = Spring.GetConfigInt("UnitIconDist", 200)
 
 local chobbyInterface
 
-local function clearBuilderCommands(unitID)
-	if commandOrdered[unitID] then
-		for id, _ in pairs(commandOrdered[unitID]) do
+local sec = 0
+local lastUpdate = 0
+
+local command = {}
+local builderCommands = {}
+local createdUnit = {}
+local createdUnitID = {}
+local newBuilderCmd = {}
+
+local isBuilder = {}
+for udefID,def in ipairs(UnitDefs) do
+	if def.isBuilder and not def.isFactory and def.buildOptions[1] then
+		isBuilder[udefID] = true
+	end
+end
+
+local function clearbuilderCommands(unitID)
+	if builderCommands[unitID] then
+		for id, _ in pairs(builderCommands[unitID]) do
 			if command[id] and command[id][unitID] then
 				command[id][unitID] = nil
 				command[id].builders = command[id].builders - 1
@@ -63,74 +75,54 @@ local function clearBuilderCommands(unitID)
 				end
 			end
 		end
-		commandOrdered[unitID] = nil
+		builderCommands[unitID] = nil
 	end
 end
 
 local function checkBuilder(unitID)
-	clearBuilderCommands(unitID)
+	clearbuilderCommands(unitID)
 	local queueDepth = spGetCommandQueue(unitID, 0)
 	if queueDepth and queueDepth > 0 then
 		local queue = spGetCommandQueue(unitID, math.min(queueDepth, 200))
 		for i=1,#queue do
 			local cmd = queue[i]
-			if ( cmd.id < 0 ) then
+			if cmd.id < 0 then
 				local myCmd = {
 					id = -cmd.id,
 					teamid = spGetUnitTeam(unitID),
 					params = cmd.params
 				}
-				local id = myCmd.teamid..'_'..math.abs(cmd.id)..'_'..cmd.params[1]..'_'..cmd.params[3]
-				if showForCreatedUnits or commandCreatedUnits[id] == nil then
+				local id = myCmd.teamid..'_'..math.abs(cmd.id)..'_'..floor(cmd.params[1])..'_'..floor(cmd.params[3])
+				if createdUnit[id] == nil then
 					if command[id] == nil then
 						command[id] = {id = myCmd, builders = 0}
 					end
 					command[id][unitID] = true
 					command[id].builders = command[id].builders + 1
-					if commandOrdered[unitID] == nil then
-						commandOrdered[unitID] = {}
+					if builderCommands[unitID] == nil then
+						builderCommands[unitID] = {}
 					end
-					commandOrdered[unitID][id] = true
+					builderCommands[unitID][id] = true
 				end
 			end
 		end
 	end
 end
 
-
-function addBuilders()
+local function init()
 	command = {}
 	local allUnits = Spring.GetAllUnits()
 	for i=1,#allUnits do
 		local unitID = allUnits[i]
-		local uDefID = spGetUnitDefID(unitID)
-		if builderUnitDefs[uDefID] then
-			builders[unitID] = true
+		if isBuilder[ spGetUnitDefID(unitID) ] then
 			checkBuilder(unitID)
 		end
 	end
 end
 
 function widget:Initialize()
-	for udefID,def in ipairs(UnitDefs) do
-		if def.isBuilder and not def.isFactory and def.buildOptions[1] then
-			local buildOptions = {}
-			for _,unit in ipairs(def.buildOptions) do
-				buildOptions[unit] = true
-			end
-			builderUnitDefs[udefID] = buildOptions
-		end
-	end
 	if Spring.GetGameFrame() > 0 then
-		addBuilders()
-	end
-
-	WG['showbuilderqueue'] = {}
-	WG['showbuilderqueue'].setMaxUnits = function(value)
-		maxDisplayedUnits = value
-	end
-	WG['showbuilderqueue'].getMaxUnits = function()
-		return maxDisplayedUnits
+		init()
 	end
 end
 
@@ -141,68 +133,62 @@ function widget:PlayerChanged(playerID)
 	spec, fullview,_ = Spring.GetSpectatingState()
 	myAllyTeamID = Spring.GetMyAllyTeamID()
 	if playerID == myPlayerID or (spec and prevMyAllyTeamID ~= myAllyTeamID or prevFullview ~= fullview) then
-		addBuilders()
-		for unitID, _ in pairs(builders) do
-			checkBuilder(unitID)
-		end
+		init()
 	end
 end
 
-local newUnitCommands = {}
+-- process newly given commands batched in Update() (because with huge build queue it eats memory and can crash lua)
 function widget:UnitCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOpts, cmdTag, playerID, fromSynced, fromLua)
-	if builderUnitDefs[unitDefID] then
-		newUnitCommands[unitID] = os.clock() + 0.05
+	if isBuilder[unitDefID] then
+		newBuilderCmd[unitID] = os.clock() + 0.05
 	end
 end
 
-
-local sec = 0
-local lastUpdate = 0
 function widget:Update(dt)
 	sec = sec + dt
 	if sec > lastUpdate + 0.12 then
 		lastUpdate = sec
-		disticon = Spring.GetConfigInt("UnitIconDist", 200)
+		disticon = math.min(350, Spring.GetConfigInt("UnitIconDist", 200))
 
 		-- process newly given commands (not done in widgetUnitCommand() because with huge build queue it eats memory and can crash lua)
-		local clock =  os.clock()
-		for unitID, cmdClock in pairs(newUnitCommands) do
+		local clock = os.clock()
+		for unitID, cmdClock in pairs(newBuilderCmd) do
 			if clock > cmdClock then
 				checkBuilder(unitID)
-				newUnitCommands[unitID] = nil
+				newBuilderCmd[unitID] = nil
 			end
 		end
 	end
 end
 
-function widget:UnitFinished(unitID, unitDefID, unitTeam)
-	if commandCreatedUnitsIDs[unitID] then
-		commandCreatedUnits[commandCreatedUnitsIDs[unitID]] = nil
-		commandCreatedUnitsIDs[unitID] = nil
+function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+	local x,_,z = spGetUnitPosition(unitID)
+	local id = unitTeam..'_'..unitDefID..'_'..floor(x)..'_'..floor(z)
+	if command[id] then
+		command[id] = nil
+		createdUnit[id] = true
+		createdUnitID[unitID] = id
 	end
 end
 
-function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-	local x,y,z = Spring.GetUnitPosition(unitID)
-	if command[unitTeam..'_'..unitDefID..'_'..x..'_'..z] then
-		command[unitTeam..'_'..unitDefID..'_'..x..'_'..z] = nil
-		commandCreatedUnitsIDs[unitID] = unitTeam..'_'..unitDefID..'_'..x..'_'..z
-		commandCreatedUnits[unitTeam..'_'..unitDefID..'_'..x..'_'..z] = true
+local function clearCommandUnit(unitID)
+	if createdUnitID[unitID] then
+		command[createdUnitID[unitID]] = nil
+		createdUnit[createdUnitID[unitID]] = nil
+		createdUnitID[unitID] = nil
 	end
-	if builderUnitDefs[unitDefID] then
-		builders[unitID] = true
-	end
+end
+
+function widget:UnitFinished(unitID, unitDefID, unitTeam)
+	clearCommandUnit(unitID)
 end
 
 function widget:UnitDestroyed(unitID, unitDefID, unitTeam, builderID)
-	if builders[unitID] then
-		builders[unitID] = nil
-		newUnitCommands[unitID] = nil
-		clearBuilderCommands(unitID)
+	if isBuilder[unitDefID] then
+		newBuilderCmd[unitID] = nil
+		clearbuilderCommands(unitID)
 	end
-	if commandCreatedUnitsIDs[unitID] then
-		commandCreatedUnits[commandCreatedUnitsIDs[unitID]] = nil
-	end
+	clearCommandUnit(unitID)
 end
 
 function widget:RecvLuaMsg(msg, playerID)
@@ -212,10 +198,7 @@ function widget:RecvLuaMsg(msg, playerID)
 end
 
 function widget:DrawWorld()
-	if chobbyInterface then return end
-
-	if Spring.IsGUIHidden() then return end
-	--gl.DepthTest(true)
+	if chobbyInterface or Spring.IsGUIHidden() then return end
 
 	local camX, camY, camZ = spGetCameraPosition()
 	local dist
@@ -233,7 +216,7 @@ function widget:DrawWorld()
 				glPushMatrix()
 				glLoadIdentity()
 				glTranslate( x, y, z )
-				glRotate( degrees, 0, 1.0, 0 )
+				glRotate( degrees, 0, 1, 0 )
 				glUnitShape(myCmd.id, myCmd.teamid, false, false, false)
 				glPopMatrix()
 				commandVisible = commandVisible + 1
@@ -243,19 +226,4 @@ function widget:DrawWorld()
 			end
 		end
 	end
-	--Spring.Echo(commandVisible)
-	--gl.DepthTest(false)
 end
-
-
---function widget:GetConfigData(data)
---	savedTable = {}
---	savedTable.maxDisplayedUnits = maxDisplayedUnits
---	return savedTable
---end
---
---function widget:SetConfigData(data)
---	if data.maxDisplayedUnits ~= nil then
---		maxDisplayedUnits = data.maxDisplayedUnits
---	end
---end
