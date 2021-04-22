10d88faab405c3ba34af21d8f5d53c7b4399470e
Floris <floris@ruwhof.net>
2021-04-22 21:36:16 +0200

    replay buttons: restructuring

--- luaui/Widgets/gui_replaybuttons.lua
+++ luaui/Widgets/gui_replaybuttons.lua
@@ -36,14 +36,106 @@ local GL_ONE_MINUS_SRC_ALPHA = GL.ONE_MINUS_SRC_ALPHA
 local GL_ONE = GL.ONE
 
 local RectRound = Spring.FlowUI.Draw.RectRound
+local UiButton = Spring.FlowUI.Draw.Button
 local elementCorner = Spring.FlowUI.elementCorner
 
 local chobbyInterface, font, backgroundGuishader, buttonsList, speedButtonsList, buttonlist, active_button, bgpadding
 
+local function speedButtonColor(i)
+	return { 0, 0, 0, 0.6 }
+end
+
+local function add_button(buttonlist, x, y, w, h, text, name, color)
+	local new_button = {}
+	new_button.x = x
+	new_button.y = y
+	new_button.w = w
+	new_button.h = h
+	new_button.text = text
+	new_button.name = name
+	if color then
+		new_button.color = color
+	end
+	table.insert(buttonlist, new_button)
+end
+
+local function point_in_rect(x1, y1, x2, y2, px, py)
+	if px > x1 and px < x2 and py > y1 and py < y2 then
+		return true
+	end
+	return false
+end
+
+local function clicked_button (b)
+	local mx, my, click = Spring.GetMouseState()
+	local mousex = mx / vsx
+	local mousey = my / vsy
+	for i = 1, #b, 1 do
+		if click and point_in_rect(b[i].x, b[i].y, b[i].x + b[i].w, b[i].y + b[i].h, mousex, mousey) then
+			return b[i].name, i
+		end
+	end
+	--keyboard:
+	--if (enter_was_down and active_button > 0 and active_button < #buttons+1) then enter_was_down = false return b[active_button].name, active_button end
+	return "NOBUTTONCLICKED"
+end
+
+local function setReplaySpeed (speed, i)
+	local s = Spring.GetGameSpeed()
+	--Spring.Echo ("setting speed to: " , speed , " current is " , s)
+	if speed > s then
+		--speedup
+		Spring.SendCommands("setminspeed " .. speed)
+		Spring.SendCommands("setminspeed " .. 0.1)
+	else
+		--slowdown
+		wantedSpeed = speed
+	end
+end
+
+local function uiText(text, x, y, s, options)
+	if text == " " or text == "  " then
+		return
+	end
+	font:Begin()
+	font:Print(text, math.floor((x * vsx) + 0.5), math.floor((y * vsy) + 0.5), math.floor((s * vsx) + 0.5), options)
+	font:End()
+end
+
+local function uiRect(x, y, x2, y2, cs, tl, tr, br, bl, c1, c2)
+	RectRound(math.floor((x * vsx) + 0.5), math.floor((y * vsy) + 0.5), math.floor((x2 * vsx) + 0.5), math.floor((y2 * vsy) + 0.5), cs, tl, tr, br, bl, c1, c2)
+end
+
+local function draw_buttons(b)
+	for i = 1, #b do
+		--UiButton(b[i].x, b[i].y, b[i].x + b[i].w, b[i].y + b[i].h, 0,1,1,0, 1,1,1,1, nil, { 0, 0, 0, 0.8 }, { 0.2, 0.2, 0.2, 0.8 }, bgpadding * 0.5)
+		gl.Color(1, 0, 0, 0.66)
+		uiRect(b[i].x, b[i].y, b[i].x + b[i].w, b[i].y + b[i].h, elementCorner, 0, 1, 1, 0, { 0.05, 0.05, 0.05, ui_opacity }, { 0, 0, 0, ui_opacity })
+		uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner * 0.66, 0, 1, 1, 0, { 0.3, 0.3, 0.3, ui_opacity * 0.1 }, { 1, 1, 1, ui_opacity * 0.1 })
+		-- gloss
+		glBlending(GL_SRC_ALPHA, GL_ONE)
+		uiRect(b[i].x, b[i].y + (b[i].h * 0.55), b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner * 0.66, 1, 1, 0, 0, { 1, 1, 1, 0.01 * glossMult }, { 1, 1, 1, 0.055 * glossMult })
+		uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + (b[i].h * 0.4), elementCorner * 0.66, 0, 0, 1, 1, { 1, 1, 1, 0.04 * glossMult }, { 1, 1, 1, 0 })
+		glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+		uiText(b[i].text, b[i].x, b[i].y + b[i].h / 2, (0.0115), 'vo')
+	end
+end
+
+function widget:ViewResize()
+	vsx, vsy = Spring.GetViewGeometry()
+	widgetScale = (0.5 + (vsx * vsy / 5700000))
+	sceduleUpdate = true
+
+	bgpadding = Spring.FlowUI.elementPadding
+	elementCorner = Spring.FlowUI.elementCorner
+
+	font = WG['fonts'].getFont(fontfile2)
+end
+
 function widget:Initialize()
 	widget:ViewResize()
 	if not Spring.IsReplay() then
-		Spring.Echo("[Replay Control] Replay not detected, Shutting Down.")
 		widgetHandler:RemoveWidget(self)
 		return
 	end
@@ -56,12 +148,8 @@ function widget:Initialize()
 	end
 	speedbuttons[2].color = { 0.75, 0, 0, 0.66 }
 	dy = dy + h
-	local text = "  skip"
-	if Spring.GetGameFrame() > 0 then
-		text = "  ||"
-	end
+	local text = (Spring.GetGameFrame() > 0 and "  ||" or "  skip")
 	add_button(buttons, wPos.x, wPos.y, 0.037, 0.033, text, "playpauseskip", { 0, 0, 0, 0.6 })
-
 end
 
 function widget:Shutdown()
@@ -70,10 +158,6 @@ function widget:Shutdown()
 	end
 	gl.DeleteList(speedButtonsList)
 	gl.DeleteList(buttonsList)
-end
-
-function speedButtonColor (i)
-	return { 0, 0, 0, 0.6 }
 end
 
 function widget:RecvLuaMsg(msg, playerID)
@@ -95,7 +179,7 @@ function widget:DrawScreen()
 				gl.DeleteList(backgroundGuishader)
 			end
 			backgroundGuishader = gl.CreateList(function()
-				RectRound(sX(wPos.x), sY(wPos.y), sX(wPos.x + 0.037), sY(wPos.y + dy), elementCorner, 0, 1, 1, 0)
+				RectRound(math.floor((wPos.x * vsx) + 0.5), math.floor((wPos.y * vsy) + 0.5), math.floor(((wPos.x + 0.037) * vsx) + 0.5),  math.floor(((wPos.y + dy) * vsy) + 0.5), elementCorner, 0, 1, 1, 0)
 			end)
 			WG['guishader'].InsertDlist(backgroundGuishader, 'replaybuttons')
 		else
@@ -122,14 +206,14 @@ function widget:DrawScreen()
 	local mousex, mousey, buttonstate = Spring.GetMouseState()
 	local b = speedbuttons
 	local topbutton = #speedbuttons
-	if point_in_rect(buttons[1].x, buttons[1].y, b[topbutton].x + b[topbutton].w, b[topbutton].y + b[topbutton].h, uiX(mousex), uiY(mousey)) then
+	if point_in_rect(buttons[1].x, buttons[1].y, b[topbutton].x + b[topbutton].w, b[topbutton].y + b[topbutton].h, mousex / vsx, mousey / vsy) then
 		for i = 1, #b, 1 do
-			if point_in_rect(b[i].x, b[i].y, b[i].x + b[i].w, b[i].y + b[i].h, uiX(mousex), uiY(mousey)) or i == active_button then
-				uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner*0.66, 0, 1, 1, 0, { 0.3, 0.3, 0.3, buttonstate and 0.25 or 0.15 }, { 1, 1, 1, buttonstate and 0.25 or 0.15 })
+			if point_in_rect(b[i].x, b[i].y, b[i].x + b[i].w, b[i].y + b[i].h, mousex / vsx, mousey / vsy) or i == active_button then
+				uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner * 0.66, 0, 1, 1, 0, { 0.3, 0.3, 0.3, buttonstate and 0.25 or 0.15 }, { 1, 1, 1, buttonstate and 0.25 or 0.15 })
 				-- gloss
 				glBlending(GL_SRC_ALPHA, GL_ONE)
-				uiRect(b[i].x, b[i].y + (b[i].h * 0.55), b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner*0.66, 1, 1, 0, 0, { 1, 1, 1, 0.06 }, { 1, 1, 1, buttonstate and 0.4 or 0.25 })
-				uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + (b[i].h * 0.4), elementCorner*0.66, 0, 0, 1, 1, { 1, 1, 1, buttonstate and 0.25 or 0.15 }, { 1, 1, 1, 0 })
+				uiRect(b[i].x, b[i].y + (b[i].h * 0.55), b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner * 0.66, 1, 1, 0, 0, { 1, 1, 1, 0.06 }, { 1, 1, 1, buttonstate and 0.4 or 0.25 })
+				uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + (b[i].h * 0.4), elementCorner * 0.66, 0, 0, 1, 1, { 1, 1, 1, buttonstate and 0.25 or 0.15 }, { 1, 1, 1, 0 })
 				glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 				uiText(b[i].text, b[i].x, b[i].y + b[i].h / 2, (0.0115), 'vo')
 				break
@@ -137,12 +221,12 @@ function widget:DrawScreen()
 		end
 		b = buttons
 		for i = 1, #b, 1 do
-			if point_in_rect(b[i].x, b[i].y, b[i].x + b[i].w, b[i].y + b[i].h, uiX(mousex), uiY(mousey)) or i == active_button then
-				uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner*0.66, 0, 1, 1, 0, { 0.3, 0.3, 0.3, buttonstate and 0.25 or 0.15 }, { 1, 1, 1, buttonstate and 0.25 or 0.15 })
+			if point_in_rect(b[i].x, b[i].y, b[i].x + b[i].w, b[i].y + b[i].h, mousex / vsx, mousey / vsy) or i == active_button then
+				uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner * 0.66, 0, 1, 1, 0, { 0.3, 0.3, 0.3, buttonstate and 0.25 or 0.15 }, { 1, 1, 1, buttonstate and 0.25 or 0.15 })
 				-- gloss
 				glBlending(GL_SRC_ALPHA, GL_ONE)
-				uiRect(b[i].x, b[i].y + (b[i].h * 0.55), b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner*0.66, 1, 1, 0, 0, { 1, 1, 1, 0.06 }, { 1, 1, 1, buttonstate and 0.4 or 0.25 })
-				uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + (b[i].h * 0.4), elementCorner*0.66, 0, 0, 1, 1, { 1, 1, 1, buttonstate and 0.25 or 0.15 }, { 1, 1, 1, 0 })
+				uiRect(b[i].x, b[i].y + (b[i].h * 0.55), b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner * 0.66, 1, 1, 0, 0, { 1, 1, 1, 0.06 }, { 1, 1, 1, buttonstate and 0.4 or 0.25 })
+				uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + (b[i].h * 0.4), elementCorner * 0.66, 0, 0, 1, 1, { 1, 1, 1, buttonstate and 0.25 or 0.15 }, { 1, 1, 1, 0 })
 				glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 				uiText(b[i].text, b[i].x, b[i].y + b[i].h / 2, (0.0115), 'vo')
 				break
@@ -158,8 +242,8 @@ function widget:MousePress(x, y, button)
 	local cb, i = clicked_button(speedbuttons)
 	if cb ~= "NOBUTTONCLICKED" then
 		setReplaySpeed(speeds[i], i)
+		--reset all buttons colors
 		for i = 1, #speeds do
-			--reset all buttons colors
 			speedbuttons[i].color = speedButtonColor(i)
 		end
 		speedbuttons[i].color = { 0.75, 0, 0, 0.66 }
@@ -188,19 +272,6 @@ function widget:MousePress(x, y, button)
 	end
 end
 
-function setReplaySpeed (speed, i)
-	local s = Spring.GetGameSpeed()
-	--Spring.Echo ("setting speed to: " , speed , " current is " , s)
-	if (speed > s) then
-		--speedup
-		Spring.SendCommands("setminspeed " .. speed)
-		Spring.SendCommands("setminspeed " .. 0.1)
-	else
-		--slowdown
-		wantedSpeed = speed
-	end
-end
-
 local uiOpacitySec = 0
 function widget:Update(dt)
 	uiOpacitySec = uiOpacitySec + dt
@@ -217,126 +288,18 @@ function widget:Update(dt)
 		end
 	end
 
-	if (wantedSpeed) then
-		if (Spring.GetGameSpeed() > wantedSpeed) then
+	if wantedSpeed then
+		if Spring.GetGameSpeed() > wantedSpeed then
 			Spring.SendCommands("slowdown")
 		else
 			wantedSpeed = nil
 		end
 	end
-	if #Spring.GetSelectedUnits() ~= 0 then
-		isActive = false
-	else
-		isActive = true
-	end
+	isActive = #Spring.GetSelectedUnits() == 0
 end
 
-function widget:GameFrame (f)
-	if (f == 1) then
+function widget:GameFrame(f)
+	if f == 1 then
 		buttons[1].text = "  ||"
 	end
-end
-
-------------------------------------------------------------------------------------------
---a simple UI framework with buttons
---Feb 2011 by knorke
-local glPopMatrix = gl.PopMatrix
-local glPushMatrix = gl.PushMatrix
-
---UI coordinaten zu scalierten screen koordinaten
-function sX (uix)
-	return math.floor((uix * vsx) + 0.5)
-end
-function sY (uiy)
-	return math.floor((uiy * vsy) + 0.5)
-end
----...und andersrum!
-function uiX (sX)
-	return sX / vsx
-end
-function uiY (sY)
-	return sY / vsy
-end
-
-function widget:ViewResize()
-	vsx, vsy = Spring.GetViewGeometry()
-	widgetScale = (0.5 + (vsx * vsy / 5700000))
-	sceduleUpdate = true
-
-	bgpadding = Spring.FlowUI.elementPadding
-	elementCorner = Spring.FlowUI.elementCorner
-
-	font = WG['fonts'].getFont(fontfile2)
-end
-
-function uiText (text, x, y, s, options)
-	if (text == " " or text == "  ") then
-		return
-	end --archivement: unlock +20 fps
-	font:Begin()
-	font:Print(text, sX(x), sY(y), sX(s), options)
-	font:End()
-end
-
-function uiRect(x, y, x2, y2, cs, tl, tr, br, bl, c1, c2)
-	RectRound(sX(x), sY(y), sX(x2), sY(y2), cs, tl, tr, br, bl, c1, c2)
-	--gl.Rect (sX(x), sY(y), sX(x2), sY(y2))
-end
-
-function draw_buttons (b)
-	local mousex, mousey = Spring.GetMouseState()
-	for i = 1, #b, 1 do
-		if b[i].color then
-			gl.Color(unpack(b[i].color))
-		else
-			gl.Color(1, 0, 0, 0.66)
-		end
-
-		local padding = bgpadding
-		uiRect(b[i].x, b[i].y, b[i].x + b[i].w, b[i].y + b[i].h, elementCorner, 0, 1, 1, 0, { 0.05, 0.05, 0.05, ui_opacity }, { 0, 0, 0, ui_opacity })
-		uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner*0.66, 0, 1, 1, 0, { 0.3, 0.3, 0.3, ui_opacity * 0.1 }, { 1, 1, 1, ui_opacity * 0.1 })
-		-- gloss
-		glBlending(GL_SRC_ALPHA, GL_ONE)
-		uiRect(b[i].x, b[i].y + (b[i].h * 0.55), b[i].x + b[i].w - (bgpadding / vsx), b[i].y + b[i].h - (bgpadding / vsy), elementCorner*0.66, 1, 1, 0, 0, { 1, 1, 1, 0.01 * glossMult }, { 1, 1, 1, 0.055 * glossMult })
-		uiRect(b[i].x, b[i].y, b[i].x + b[i].w - (bgpadding / vsx), b[i].y + (b[i].h * 0.4), elementCorner*0.66, 0, 0, 1, 1, { 1, 1, 1, 0.04 * glossMult }, { 1, 1, 1, 0 })
-		glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-
-		uiText(b[i].text, b[i].x, b[i].y + b[i].h / 2, (0.0115), 'vo')
-	end
-end
-
-function add_button (buttonlist, x, y, w, h, text, name, color)
-	local new_button = {}
-	new_button.x = x
-	new_button.y = y
-	new_button.w = w
-	new_button.h = h
-	new_button.text = text
-	new_button.name = name
-	if color then
-		new_button.color = color
-	end
-	table.insert(buttonlist, new_button)
-end
-
-function point_in_rect (x1, y1, x2, y2, px, py)
-	if px > x1 and px < x2 and py > y1 and py < y2 then
-		return true
-	end
-	return false
-end
-
-function clicked_button (b)
-	local mx, my, click = Spring.GetMouseState()
-	local mousex = uiX(mx)
-	local mousey = uiY(my)
-	for i = 1, #b, 1 do
-		if (click == true and point_in_rect(b[i].x, b[i].y, b[i].x + b[i].w, b[i].y + b[i].h, mousex, mousey)) then
-			return b[i].name, i
-		end
-		--if (mouse_was_down == false and click == true and point_in_rect (b[i].x, b[i].y, b[i].x+b[i].w, b[i].y+b[i].h,  mousex, mousey)) then mouse_was_down = true end
-	end
-	--keyboard:
-	--if (enter_was_down and active_button > 0 and active_button < #buttons+1) then enter_was_down = false return b[active_button].name, active_button end
-	return "NOBUTTONCLICKED"
 end
