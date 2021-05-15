59081b4240fdd6a17d75248c44744ef423fe6d69
Floris <floris@ruwhof.net>
2021-05-15 15:37:44 +0200

    optimized gadget: Prevent outside-of-map hax + skip for gaia units

--- luarules/gadgets/unit_prevent_aircraft_hax.lua
+++ luarules/gadgets/unit_prevent_aircraft_hax.lua
@@ -4,7 +4,7 @@
 function gadget:GetInfo()
 	return {
 		name = "Prevent outside-of-map hax",
-		desc = "Prevent outside-of-map hax",
+		desc = "Prevent mobile unit outside-of-map hax (unless its gaia)",
 		author = "Beherith",
 		date = "3 27 2011",
 		license = "CC BY SA",
@@ -17,19 +17,46 @@ if not gadgetHandler:IsSyncedCode() then
 	return false
 end
 
-local spGetUnitPosition = Spring.GetUnitPosition
-local spGetAllUnits = Spring.GetAllUnits
+local rangeLimit = 3000
 
+local gaiaTeamID = Spring.GetGaiaTeamID()
 local mapX = Game.mapSizeX
 local mapZ = Game.mapSizeZ
+local allMobileUnits = {}
+local spGetUnitPosition = Spring.GetUnitPosition
+
+local isMobileUnit = {}
+for unitDefID, udef in pairs(UnitDefs) do
+	if not udef.isBuilding or udef.speed == 0 then
+		isMobileUnit[unitDefID] = true
+	end
+end
+
+function gadget:Initialize()
+	for ct, unitID in pairs(Spring.GetAllUnits()) do
+		gadget:UnitFinished(unitID, Spring.GetUnitDefID(unitID), Spring.GetUnitTeam(unitID))
+	end
+end
+
+function gadget:UnitFinished(unitID, unitDefID, unitTeam)
+	if isMobileUnit[unitDefID] and unitTeam ~= gaiaTeamID then
+		allMobileUnits[unitID] = true
+	end
+end
+
+function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
+	allMobileUnits[unitID] = nil
+end
 
 function gadget:GameFrame(f)
 	if f % 61 == 0 then
-		local all_units = spGetAllUnits()
-		for _, unitID in pairs(all_units) do
+		local minMap = -rangeLimit
+		local maxMapX = mapX + rangeLimit
+		local maxMapZ = mapZ + rangeLimit
+		for unitID, _ in pairs(allMobileUnits) do
 			local x, y, z = spGetUnitPosition(unitID)
 			if not (z == nil or x == nil) then
-				if z < -10000 or x < -10000 or z > mapZ + 10000 or x > mapX + 10000 then
+				if z < minMap or x < minMap or z > maxMapZ or x > maxMapX then
 					Spring.DestroyUnit(unitID, false, true)
 				end
 			end
