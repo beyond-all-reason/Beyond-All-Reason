af227a97e00a012fd05f944b4adf4d7f3cc9f486
Floris <floris@ruwhof.net>
2021-06-04 18:22:05 +0200

    loadscreen: made ready for guide backgrounds

--- luaintro/Addons/loadprogress.lua
+++ luaintro/Addons/loadprogress.lua
@@ -11,22 +11,18 @@ if addon.InGetInfo then
 	}
 end
 
-------------------------------------------
-
 local tips = require "savetable.lua"
 
 local startTime = -1
 local cachedLoadTimes = VFS.FileExists("loadprogress_cached.lua") and VFS.Include("loadprogress_cached.lua") or { }
 local cachedTotalTime = (cachedLoadTimes[Game.mapName] or -1) * 0.97 --*0.97 cause else last rendered frame would show 99%
 
-
 local function mix(x,y,a)
 	return x * (1-a) + y * a
 end
 
-
 function SG.GetLoadProgress()
-	if (startTime < 0) then startTime = os.clock() end
+	if startTime < 0 then startTime = os.clock() end
 
 	if cachedTotalTime > 0 then
 		return math.max(0.0, math.min(1.0, (os.clock() - startTime) / cachedTotalTime))
@@ -34,7 +30,6 @@ function SG.GetLoadProgress()
 		return -1
 	end
 end
-
 
 function addon.Shutdown()
 	cachedLoadTimes[Game.mapName] = os.clock() - startTime
