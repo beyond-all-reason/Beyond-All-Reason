78cfb74f60adf8d673034f96199148b1b5164633
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2021-06-06 18:09:23 -0400

    i18n: Remove allyTeam death messages

--- luaui/Widgets/death_messages.lua
+++ luaui/Widgets/death_messages.lua
@@ -10,8 +10,21 @@ function widget:GetInfo()
 	}
 end
 
-local deathMessages = VFS.Include("luarules/configs/death_messages.lua")
-local allyTeamDeathInfo = {}
+local deathMessageKeys = {
+	'bowOut',
+	'gone',
+	'conquer',
+	'toast',
+	'takenOut',
+	'defeat',
+	'bitterEnd',
+	'rodeOff',
+	'dismantle',
+	'terminate',
+	'annihilate',
+	'crater',
+}
+local teamNames = {}
 
 local function getTeamNames(teamID)
 	local playerNames = {}
@@ -38,80 +51,28 @@ local function getTeamNames(teamID)
 	return playerNames
 end
 
-local function getAllyTeamNames(allyTeamID)
-	local playerNames = {}
-	local teams = Spring.GetTeamList(allyTeamID)
-
-	for _, teamID in pairs(teams) do
-		local teamPlayerNames = getTeamNames(teamID)
-
-		for _, name in pairs(teamPlayerNames) do
-			table.concat(playerNames, name)
-		end
-	end
-
-	return playerNames
-end
-
-local function isAllyTeamDead(allyTeamID)
-	local teams = Spring.GetTeamList(allyTeamID)
-
-	for _, teamID in pairs(teams) do
-		local _, _, isDead = Spring.GetTeamInfo(teamID)
-
-		if not isDead then
-			return false
-		end
-	end
-
-	return true
-end
-
-local function notifyAllyTeamDeath(allyTeamID)
-	local playerNameList = getAllyTeamNames(allyTeamID)
-
-	if playerNameList == nil or next(playerNameList) == nil then
-		Spring.Log(widget:GetInfo().name, LOG.ERROR, "AllyTeam " .. allyTeamID .. ": all teams have empty player names lists")
-	else
-		local playerNames = table.concat(playerNameList, ', ')
-		local n = math.random(#deathMessages.allyTeam)
-		local message = Spring.I18N('tips.deathMessages.allyTeam.' .. deathMessages.allyTeam[n], { team = allyTeamID, playerList = playerNames })
-		Spring.SendMessage(message)
-	end
-
-	allyTeamDeathInfo[allyTeamID].notified = true
-end
-
 local function notifyTeamDeath(teamID)
-	local playerNameList = getTeamNames(teamID)
+	local playerNameList = teamNames[teamID]
 
 	if playerNameList == nil or next(playerNameList) == nil then
 		Spring.Log(widget:GetInfo().name, LOG.ERROR, "Team " .. teamID .. ": no names in players list")
 	else
 		local playerNames = table.concat(playerNameList, ', ')
-		local n = math.random(#deathMessages.team)
-		local message = Spring.I18N('tips.deathMessages.team.' .. deathMessages.team[n], { team = teamID, playerList = playerNames })
+		local n = math.random(#deathMessageKeys)
+		local message = Spring.I18N('tips.deathMessages.team.' .. deathMessageKeys[n], { playerList = playerNames })
 
 		Spring.SendMessage(message)
-	end
-
-	local _, _, _, _, _, allyTeamID = Spring.GetTeamInfo(teamID)
-
-	if not allyTeamDeathInfo[allyTeamID].notified and isAllyTeamDead(allyTeamID) then
-		notifyAllyTeamDeath(allyTeamID)
 	end
 end
 
 function widget:TeamDied(teamID)
-	Spring.Echo("Team Died: " .. teamID)
 	notifyTeamDeath(teamID)
 end
 
 function widget:Initialize()
-	local allyTeams = Spring.GetAllyTeamList()
+	local teams = Spring.GetTeamList()
 
-	for _, allyTeamID in pairs(allyTeams) do
-		allyTeamDeathInfo[allyTeamID] = {}
-		allyTeamDeathInfo[allyTeamID].notified = false
+	for _, teamID in pairs(teams) do
+		teamNames[teamID] = getTeamNames(teamID)
 	end
 end
