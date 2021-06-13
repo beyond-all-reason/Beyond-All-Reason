d2ddbb6844925c6f1f378a57f0542da84dd68817
Floris <floris@ruwhof.net>
2021-06-13 12:39:16 +0200

    logo adjuster: added pause icon

--- luaui/Widgets/logo_adjuster.lua
+++ luaui/Widgets/logo_adjuster.lua
@@ -9,10 +9,47 @@ function widget:GetInfo()
   }
 end
 
+local imagePlain = "bitmaps/logo.png"
+local imageBattle = "bitmaps/logo_battle.png"
+local imageBattlePaused = "bitmaps/logo_battlepaused.png"
+
+local previousGameFrame = Spring.GetGameFrame()
+local paused = false
+local sec = 0
+
 function widget:Initialize()
-    Spring.SetWMIcon("bitmaps/logo_battle.png")
+    Spring.SetWMIcon(imageBattle)
 end
 
 function widget:Shutdown()
-    Spring.SetWMIcon("bitmaps/logo.png")
+    Spring.SetWMIcon(imagePlain)
+end
+
+-- possibly display gameover/trophy icon when being the winner?
+function widget:GameOver()
+
+end
+
+function widget:Update(dt)
+	sec = sec + dt
+	if sec > 0.5 then
+		sec = 0
+		local now = os.clock()
+		local gameFrame = Spring.GetGameFrame()
+		if gameFrame > 0 then
+			if not paused then
+				if gameFrame == previousGameFrame then
+					Spring.SetWMIcon(imageBattlePaused)
+					paused = true
+
+				end
+			else
+				if gameFrame ~= previousGameFrame then
+					Spring.SetWMIcon(imageBattle)
+					paused = false
+				end
+			end
+		end
+		previousGameFrame = gameFrame
+	end
 end
