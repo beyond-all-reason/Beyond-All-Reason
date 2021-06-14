bc1eb9f7165749ca4dc965f84c4958f364d299bf
Beherith <mysterme@gmail.com>
2021-06-14 12:19:32 +0200

    Update dbg_combattest.lua

--- luaui/Widgets/dbg_combattest.lua
+++ luaui/Widgets/dbg_combattest.lua
@@ -1,7 +1,7 @@
 function widget:GetInfo()
    return {
       name      = "DBG fightertest",
-      desc      = "use /luaui givecore, /luaui givearm and /luaui givefeatures",
+      desc      = "use /luaui fightertest [unitname1] [unitname2] [unitcount]",
       author    = "Beherith",
       version   = "v1.1",
       date      = "2016",
@@ -11,25 +11,28 @@ function widget:GetInfo()
    }
 end
 
-local maxfighters = 200
+local maxunits = 200
 local feedstep = 20
 local mapcx = Game.mapSizeX/2
 local mapcz = Game.mapSizeZ/2
 local mapcy = Spring.GetGroundHeight(mapcx,mapcz)
 local enabled = false
-local testunit = "armbull"
+local team1unit = "corvamp"
+local team2unit = "corvamp"
 
-local function feedteam(teamid)
+
+
+local function feedteam(teamid, teamunit)
 	local unitcount = Spring.GetTeamUnits(teamid)
-	if (#unitcount < maxfighters) then
+	if (#unitcount < maxunits) then
 		local cmd = string.format(
 			"give %d %s %d @%d,%d,%d",
 			feedstep,
-			testunit,
+			teamunit,
 			teamid,
-			mapcx + 2000*math.random(),
+			mapcx + 1500*(math.random() -0.5),
 			mapcy,
-			mapcz + 2000*math.random()
+			mapcz + 1500* (math.random() -0.5)
 		)
 		Spring.SendCommands({cmd})
 	end
@@ -38,15 +41,47 @@ end
    
 function widget:GameFrame(n)
 	if (n % 3 == 0) and enabled then
-		feedteam(0, 'armfig')
-		feedteam(1, 'corveng')
+		feedteam(0, team1unit)
+		feedteam(1, team2unit)
 	end
 end
 
+-- a possible command structure could be:
+-- /luaui fightertest [noparams = stop]
+
+-- /luaui fightertest unitdefnamea unitdefnameb targetperteam
+
+local function Split(s, delimiter)
+    local result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
+split_string = Split("Hello World!", " ")
+
 function widget:TextCommand(command)
-	--Spring.Echo(command)
 	if (string.find(command, "fightertest") == 1) then 
-		enabled = not enabled 
+    Spring.Echo(command)
+    enabled = not enabled 
+    if enabled then
+        local tokens = Split(command," ")
+        if tokens[2] then 
+          team1unit = tokens[2]
+          if tokens[3] then
+            team2unit = tokens[3]
+            if tokens[4] then
+              maxunits = tonumber(tokens[4])
+              if maxunits == nil then maxunits = 200 end
+            end
+          else
+            team2unit = team1unit
+          end
+          
+        end
+    
+    end
 	end
 end
 	
