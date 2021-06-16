66ea252506d06d1b67cd5f2a582780dbfb41f299
Damgam <c_2_damiano@op.pl>
2021-06-16 11:44:34 +0200

    Colors Gadget is complete :D

--- luarules/gadgets/game_autocolors.lua
+++ luarules/gadgets/game_autocolors.lua
@@ -61,7 +61,7 @@ FFAColors = {
 
 
 AllyColors = {
-    [2] = { -- TwoTeams
+    [2] = { -- Two Teams
         [1] = {0,       80,     255 },      -- Armada Blue
         [2] = {10,      232,    18  },      -- Green
         [3] = {94,      9,      178 },      -- Purple
@@ -71,19 +71,38 @@ AllyColors = {
         [7] = {178,     255,    227 },      -- Aqua
         [8] = {8,       37,     190 },      -- Dark Blue
     },
+    -- [3] = { -- Three Teams
+    --     [1] = {10,      232,    18  },      -- Green
+    --     [2] = {10,      232,    18  },      -- Green
+    --     etc..
+    -- },
 }
 
 EnemyColors = {
-    [2] = { -- TwoTeams
-        [1] = {255,     16,     5   },      -- Cortex Red
-        [2] = {255,     232,    22  },      -- Yellow
-        [3] = {255,     125,    32  },      -- Orange
-        [4] = {229,     18,     120 },      -- Pink
-        [5] = {255,     243,    135 },      -- Bleached Yellow
-        [6] = {166,     14,     5   },      -- Blood
-        [7] = {251,     167,    120 },      -- Skin? lol
-        [8] = {118,     39,     6   },      -- Brown
+    [2] = { -- Two Teams
+        [1] = {
+            [1] = {255,     16,     5   },      -- Cortex Red
+            [2] = {255,     232,    22  },      -- Yellow
+            [3] = {255,     125,    32  },      -- Orange
+            [4] = {229,     18,     120 },      -- Pink
+            [5] = {255,     243,    135 },      -- Bleached Yellow
+            [6] = {166,     14,     5   },      -- Blood
+            [7] = {251,     167,    120 },      -- Skin? lol
+            [8] = {118,     39,     6   },      -- Brown
+        },
     },
+    -- [3] = { -- Three Teams
+    --     [1] = { -- First Enemy Team
+    --         [1] = {255,     16,     5   },      -- Cortex Red
+    --         [2] = {255,     16,     5   },      -- Cortex Red
+    --         etc..
+    --     }
+    --     [2] = { -- Second Enemy Team
+    --         [1] = {255,     16,     5   },      -- Cortex Red
+    --         [2] = {255,     16,     5   },      -- Cortex Red
+    --         etc..
+    --     }
+    -- }
 }
 
 ScavColor = {97, 36, 97}
@@ -101,11 +120,42 @@ local function MissingColorHandler(teamID, allyTeam, myTeam, myAllyTeam)
     Spring.Echo("Missing Team Color for TeamID: ".. teamID)
 end
 
-local function EnemyColorHandler(teamID, allyTeam)
+local function EnemyColorHandler(teamID, allyTeam, allyTeamCount)
     local myTeam = spGetMyTeamID()
     local myAllyTeam = spGetMyAllyTeamID()
-    local spectator = spGetSpectatingState()
-    
+    --local spectator = spGetSpectatingState()
+    if not EATeams[allyTeam] then
+        EATeams[allyTeam] = true
+        if EACount then
+            EACount[allyTeam] = #EACount + 1
+        else
+            EACount[allyTeam] = 1
+        end
+        EATeamsCount[allyTeam] = 0
+    end
+    EATeamsCount[allyTeam] = EATeamsCount[allyTeam] + 1
+    --Spring.Echo("allyTeamCount "..allyTeamCount)
+    --Spring.Echo("EACount[allyTeam] "..EACount[allyTeam])
+    --Spring.Echo("EATeams[allyTeam] "..EATeams[allyTeam])
+    --Spring.Echo("EATeamsCount[allyTeam] "..EATeamsCount[allyTeam])
+    if EnemyColors[allyTeamCount] then
+        if EnemyColors[allyTeamCount][EACount[allyTeam]] then
+            if EnemyColors[allyTeamCount][EACount[allyTeam]][EATeamsCount[allyTeam]] then
+                spSetTeamColor(
+                    teamID, 
+                    EnemyColors[allyTeamCount][EACount[allyTeam]][EATeamsCount[allyTeam]][1] /255, 
+                    EnemyColors[allyTeamCount][EACount[allyTeam]][EATeamsCount[allyTeam]][2] /255,
+                    EnemyColors[allyTeamCount][EACount[allyTeam]][EATeamsCount[allyTeam]][3] /255
+                )
+            else
+                MissingColorHandler(teamID, allyTeam, myTeam, myAllyTeam)
+            end
+        else
+            MissingColorHandler(teamID, allyTeam, myTeam, myAllyTeam)
+        end
+    else
+        MissingColorHandler(teamID, allyTeam, myTeam, myAllyTeam)
+    end
     -- planned
 end
 
@@ -115,6 +165,9 @@ local function UpdatePlayerColors()
     local myTeam = spGetMyTeamID()
     local myAllyTeam = spGetMyAllyTeamID()
     local spectator = spGetSpectatingState()
+    EATeams = {}
+    EACount = {}
+    EATeamsCount = {}
     for i = 1,#teams do
         local teamID = teams[i]
         local _, leader, isDead, isAiTeam, side, allyTeam, incomeMultiplier, customTeamKeys = spGetTeamInfo(teamID)
@@ -150,15 +203,16 @@ local function UpdatePlayerColors()
             else
                 if allyTeam == myAllyTeam then
                     allyCounter = allyCounter+1
-                    if AllyColors[2][allyCounter] then
-                        spSetTeamColor(teamID, AllyColors[2][allyCounter][1] /255, AllyColors[2][allyCounter][2] /255, AllyColors[2][allyCounter][3] /255)
+                    if AllyColors[#allyteams-1][allyCounter] then
+                        spSetTeamColor(teamID, AllyColors[#allyteams-1][allyCounter][1] /255, AllyColors[#allyteams-1][allyCounter][2] /255, AllyColors[#allyteams-1][allyCounter][3] /255)
                     else
                         MissingColorHandler(teamID, allyTeam, myTeam, myAllyTeam)
                     end
                 else
                     enemyCounter = enemyCounter+1
-                    if EnemyColors[2][enemyCounter] then
-                        spSetTeamColor(teamID, EnemyColors[2][enemyCounter][1] /255, EnemyColors[2][enemyCounter][2] /255, EnemyColors[2][enemyCounter][3] /255)
+                    if EnemyColors[#allyteams-1][1] then
+                        EnemyColorHandler(teamID, allyTeam, #allyteams-1)
+                        --spSetTeamColor(teamID, EnemyColors[2][enemyCounter][1] /255, EnemyColors[2][enemyCounter][2] /255, EnemyColors[2][enemyCounter][3] /255)
                     else
                         MissingColorHandler(teamID, allyTeam, myTeam, myAllyTeam)
                     end
@@ -169,6 +223,9 @@ local function UpdatePlayerColors()
     ffaCounter = 0
     allyCounter = 0
     enemyCounter = 0
+    EATeams = nil
+    EACount = nil
+    EATeamsCount = nil
 end
 
 function gadget:Initialize()
