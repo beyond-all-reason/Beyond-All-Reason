e0635f6447870b1c635011dc60ab65ebc9c4f9b5
Beherith <mysterme@gmail.com>
2021-06-20 11:59:23 +0200

    minesweep ceg closer to actual range

--- effects/minesweep.lua
+++ effects/minesweep.lua
@@ -4,7 +4,7 @@ return {
 		["GROUNDFLASH"] = {
 			flashsize = 300,
 			flashalpha = 0.4,
-			circlegrowth = 20,
+			circlegrowth = 14,
 			circlealpha = 0.1,
 			ttl = 20,
 			color = {0.2, 0.9, 0.2},
