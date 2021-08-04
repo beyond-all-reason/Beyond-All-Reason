0bfe313889640fccdb2dd0efdd507106462de011
Floris <floris@ruwhof.net>
2021-08-04 21:21:23 +0200

    added widget: geothermal snap

---
+++ luaui/Widgets/cmd_geothermal_snap.lua
@@ -0,0 +1,162 @@
+function widget:GetInfo()
+	return {
+		name      = "Geothermal Snap",
+		desc      = "Snaps selected geothermal unit to the nearest geo spot",
+		author    = "Niobium, Floris",
+		version   = "",
+		date      = "August 2021",
+		license   = "GNU GPL, v2 or later",
+		layer     = -1,
+		enabled   = true,
+	}
+end
+
+local spGetActiveCommand = Spring.GetActiveCommand
+local spGetMouseState = Spring.GetMouseState
+local spTraceScreenRay = Spring.TraceScreenRay
+
+local chobbyInterface
+local geoSelected = false
+local mousePressed = false
+
+local isGeo = {}
+for uDefID, uDef in pairs(UnitDefs) do
+	if uDef.needGeo then
+		isGeo[uDefID] = true
+	end
+end
+
+local spots = {}
+local showGeothermalUnits = false
+local function checkGeothermalFeatures()
+	showGeothermalUnits = false
+	local geoFeatureDefs = {}
+	for defID, def in pairs(FeatureDefs) do
+		if def.geoThermal then
+			geoFeatureDefs[defID] = true
+		end
+	end
+	spots = {}
+	local features = Spring.GetAllFeatures()
+	local spotCount = 0
+	for i = 1, #features do
+		if geoFeatureDefs[Spring.GetFeatureDefID(features[i])] then
+			showGeothermalUnits = true
+			local x, y, z = Spring.GetFeaturePosition(features[i])
+			spotCount = spotCount + 1
+			spots[spotCount] = {x, y, z}
+		end
+	end
+end
+
+local function GetClosestSpot(x, z)
+	local bestSpot
+	local bestDist = math.huge
+	for i = 1, #spots do
+		local spot = spots[i]
+		local dx, dz = x - spot[1], z - spot[3]
+		local dist = dx*dx + dz*dz
+		if dist < bestDist then
+			bestSpot = spot
+			bestDist = dist
+		end
+	end
+	return bestSpot
+end
+
+
+local function DoLine(x1, y1, z1, x2, y2, z2)
+    gl.Vertex(x1, y1, z1)
+    gl.Vertex(x2, y2, z2)
+end
+
+function widget:Initialize()
+	if checkGeothermalFeatures then
+		checkGeothermalFeatures()
+	end
+end
+
+function widget:GameFrame(gf)
+	if checkGeothermalFeatures then
+		checkGeothermalFeatures()
+		checkGeothermalFeatures = nil
+
+		if not showGeothermalUnits then
+			widgetHandler:RemoveWidget()
+		end
+	end
+end
+
+function widget:RecvLuaMsg(msg, playerID)
+	if msg:sub(1,18) == 'LobbyOverlayActive' then
+		chobbyInterface = (msg:sub(1,19) == 'LobbyOverlayActive1')
+	end
+end
+
+function widget:MousePress(x, y, button)
+	local cmdID, unitDefID, c, d = Spring.GetActiveCommand()
+	--Spring.Echo(cmdID, unitDefID, c, d)
+	if unitDefID and isGeo[-unitDefID] then
+		geoSelected = true
+	end
+end
+
+function widget:MouseRelease(x, y, button)
+
+end
+
+function widget:DrawWorld()
+	if chobbyInterface then return end
+
+	-- command = geo?
+	local _, cmdID = spGetActiveCommand()
+	if not (cmdID and isGeo[-cmdID]) then
+		geoSelected = false
+		return
+	end
+
+	local mx, my, mb, mmb, mrb = spGetMouseState()
+	local _, pos = spTraceScreenRay(mx, my, true)
+	if not pos then return end
+
+	-- Find build position
+	local bx, by, bz = Spring.Pos2BuildPos(-cmdID, pos[1], pos[2], pos[3])
+	local bestPos = GetClosestSpot(bx, bz)
+	if not bestPos then
+		return
+	end
+	local bface = Spring.GetBuildFacing()
+
+	if geoSelected then
+		if mb then
+			mousePressed = true
+		elseif mousePressed then
+			mousePressed = false
+			--Spring.Echo(cmdID, bestPos[1], bestPos[2], bestPos[3], bface)
+			Spring.GiveOrder(cmdID, {bestPos[1], bestPos[2], bestPos[3], bface}, {})
+			Spring.SetActiveCommand(0)
+			geoSelected = false
+		end
+	end
+
+	-- Draw
+	gl.DepthTest(false)
+
+	gl.LineWidth(1.49)
+    gl.Color(1, 1, 0, 0.5)
+    gl.BeginEnd(GL.LINE_STRIP, DoLine, bx, by, bz, bestPos[1], bestPos[2], bestPos[3])
+	gl.LineWidth(1.0)
+
+	gl.DepthTest(true)
+	gl.DepthMask(true)
+
+	gl.Color(1, 1, 1, 0.5)
+	gl.PushMatrix()
+		gl.Translate(bestPos[1], bestPos[2], bestPos[3])
+		gl.Rotate(90 * bface, 0, 1, 0)
+		gl.UnitShape(-cmdID, Spring.GetMyTeamID(), false, true, false)
+	gl.PopMatrix()
+
+	gl.DepthTest(false)
+	gl.DepthMask(false)
+end
