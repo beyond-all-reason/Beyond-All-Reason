97df584ddda7281266bd03ecac093890d4f86c60
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2021-08-07 17:52:24 -0400

    Cleanup: Separate common code into pure Lua and Spring

--- init.lua
+++ init.lua
@@ -1,8 +1,11 @@
-VFS.Include('common/numberfunctions.lua')
+-- This file holds common functions that should be available globally
+
+VFS.Include('initBase.lua')
+
 VFS.Include('common/utilities.lua')
 VFS.Include('common/debugFunctions.lua')
 
-if not Script.GetSynced() then
+if not ( Script.GetSynced() ) then
 	-- I18N is purely client side and should never be called in a synced context
 	VFS.Include("modules/i18n/i18n.lua")
 end
