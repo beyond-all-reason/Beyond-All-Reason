e98d7790aad58f4f9c7871b1dcdb69ba7a1a6fdb
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2021-08-08 10:52:24 -0400

    Cleanup: Consolidate string.split calls

--- luarules/gadgets/unit_morph.lua
+++ luarules/gadgets/unit_morph.lua
@@ -77,21 +77,6 @@ function GetTechLevel(udid)
 end
 ]]--
 
-function string:split( inSplitPattern, outResults )
-  if not outResults then
-    outResults = { }
-  end
-  local theStart = 1
-  local theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
-  while theSplitStart do
-    table.insert( outResults, string.sub( self, theStart, theSplitStart-1 ) )
-    theStart = theSplitEnd + 1
-    theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
-  end
-  table.insert( outResults, string.sub( self, theStart ) )
-  return outResults
-end
-
 -- // for use with mods like CA <_<
 local function GetTechLevel(UnitDefID)
   --return UnitDefs[UnitDefID].techLevel or 0
