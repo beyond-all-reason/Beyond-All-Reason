99b6ae71d4a26e0c7485480526d9dc4440a6e4d1
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2021-08-09 20:07:15 -0400

    Cleanup: Merge initBase.lua into init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
--- This file holds common functions that should be available globally
+-- This file includes common functionality that should be available globally
 
-VFS.Include('initBase.lua')
-
+VFS.Include('common/numberfunctions.lua')
+VFS.Include('common/stringFunctions.lua')
 VFS.Include('common/springFunctions.lua')
 
-if not Script.GetSynced() then
+if not (Script.GetSynced and Script.GetSynced()) then
 	-- I18N is purely client side and should never be called in a synced context
 	VFS.Include("modules/i18n/i18n.lua")
 end
