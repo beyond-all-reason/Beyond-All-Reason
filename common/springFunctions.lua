c10fd5488d7e5a0c52ae5130a4386c933862daec
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2021-08-25 20:13:03 -0400

    Bugfix: Fix modoptions with value false being changed

--- common/springFunctions.lua
+++ common/springFunctions.lua
@@ -33,11 +33,11 @@ if Spring.GetModOptions then
 		local key = modOption.key
 
 		if modOption.type ~= "section" then
-			if not modOptions[key] then
+			if modOptions[key] == nil then
 				modOptions[key] = modOption.def
 			end
 
-			if modOption.type == 'bool' then
+			if (modOption.type == 'bool') and (type(modOptions[key]) ~= 'boolean') then
 				local value = tonumber(modOptions[key])
 				modOptions[key] = value == 1 and true or false
 			end
