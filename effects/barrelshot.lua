56c3d3aed5ac07bf1e6f4a71b4065f8fbf6c717c
Floris <floris@ruwhof.net>
2021-09-06 15:05:07 +0200

    armmg purple barrelshot effect for scavengers

--- effects/barrelshot.lua
+++ effects/barrelshot.lua
@@ -198,23 +198,6 @@ local definitions = {
     --},
   },
 
-  -- ["barrelshot-mg"] = {
-  --   mgfire = {
-  --     air                = true,
-  --     class              = [[CExpGenSpawner]],
-  --     count              = 2,
-  --     ground             = true,
-  --     water              = true,
-  --     underwater         = true,
-  --     properties = {
-  --       dir                = [[dir]],
-  --       delay              = [[0 r5]],
-  --       explosiongenerator = [[custom:barrelshot-fire]],
-  --       --pos                = [[-5 r10, 0, -5 r10]],
-  --     },
-  --   },
-  -- },
-
   ["barrelshot-flak"] = {
     fire = {
       air                = true,
@@ -425,25 +408,6 @@ local definitions = {
         pos                = [[0.0, 1, 0.0]],
       },
     },
-    -- fire2 = {
-    --   air                = true,
-    --   class              = [[CBitmapMuzzleFlame]],
-    --   count              = 1,
-    --   ground             = true,
-    --   underwater         = 1,
-    --   water              = true,
-    --   properties = {
-    --     colormap           = [[1 0.89 0.6 0.013   0.6 0.25 0.06 0.01   0.4 0.06 0.015 0.006   0 0 0 0.01]],
-    --     dir                = [[dir]],
-    --     frontoffset        = 0,
-    --     fronttexture       = [[none]],
-    --     length             = -9,
-    --     sidetexture        = [[shotgunside]],
-    --     size               = 13,
-    --     sizegrowth         = -0.5,
-    --     ttl                = 15,
-    --   },
-    -- },
       fireglow = {
         air                = true,
         class              = [[CSimpleParticleSystem]],
@@ -472,62 +436,6 @@ local definitions = {
           useairlos          = true,
         },
       },
-    --   smoke2 = {
-    --       air                = true,
-    --       class              = [[CSimpleParticleSystem]],
-    --       count              = 1,
-    --       ground             = true,
-    --       water              = true,
-    --       properties = {
-    --           airdrag            = 0.92,
-    --           colormap           = [[0 0 0 0    0.95 0.63 0.17 0.70    0.13 0.12 0.12 0.38   0.012 0.011 0.10 0.22   0.011 0.010 0.09 0.16   0 0 0 0.01]],
-    --           directional        = true,
-    --           emitrot            = 0,
-    --           emitrotspread      = 8,
-    --           emitvector         = [[dir]],
-    --           gravity            = [[-0.02 r0.04, -0.005, -0.02 r0.04]],
-    --           numparticles       = 4,
-    --           particlelife       = 15,
-    --           particlelifespread = 20,
-    --           particlesize       = 1.3,
-    --           particlesizespread = 3.7,
-    --           particlespeed      = 1.1,
-    --           particlespeedspread = 2.1,
-    --           pos                = [[0.0, 0, 0.0]],
-    --           sizegrowth         = 0.30,
-    --           sizemod            = 1,
-    --           texture            = [[smoke_puff2]],
-    --           useairlos          = true,
-    --       },
-    --   },
-    -- smoke = {
-    --   air                = true,
-    --   class              = [[CSimpleParticleSystem]],
-    --   count              = 1,
-    --   ground             = true,
-    --   water              = true,
-    --   properties = {
-    --       airdrag            = 0.91,
-    --       colormap           = [[0.95 0.69 0.17 0.95    0.35 0.20 0.13 0.65   0.15 0.14 0.12 0.60    0.12 0.12 0.12 0.25   0 0 0 0.01]],
-    --       directional        = true,
-    --       emitrot            = 0,
-    --       emitrotspread      = 9,
-    --       emitvector         = [[dir]],
-    --       gravity            = [[-0.006 r0.012, 0, -0.006 r0.012]],
-    --       numparticles       = 2,
-    --       particlelife       = 13,
-    --       particlelifespread = 9,
-    --       particlesize       = 4,
-    --       particlesizespread = 7,
-    --       particlespeed      = 1.40,
-    --       particlespeedspread = 0.5,
-    --       pos                = [[0.0, 0, 0.0]],
-    --       sizegrowth         = 0.92,
-    --       sizemod            = 1,
-    --       texture            = [[cloudpuff]],
-    --       useairlos          = true,
-    --   },
-    -- },
   },
 
 }
@@ -614,7 +522,7 @@ definitions["barrelshot-huge"].fire2.properties.size									  = definitions["ba
 definitions["barrelshot-huge"].fire2.properties.sizegrowth              = definitions["barrelshot-huge"].fire2.properties.sizegrowth * 1.4
 definitions["barrelshot-huge"].fire2.properties.ttl										  = definitions["barrelshot-huge"].fire2.properties.ttl * 0.8
 definitions["barrelshot-huge"].fireglow.properties.particlesize					= definitions["barrelshot-huge"].fireglow.properties.particlesize * size * 0.8
---definitions["barrelshot-huge"].fireglow.properties.particlelife					= definitions["barrelshot-huge"].fireglow.properties.particlelife * size * 0.8 
+--definitions["barrelshot-huge"].fireglow.properties.particlelife					= definitions["barrelshot-huge"].fireglow.properties.particlelife * size * 0.8
 --definitions["barrelshot-huge"].fireglow.properties.colormap        			= [[0.122 0.066 0.013 0.03   0 0 0 0.01]]
 definitions["barrelshot-huge"].smoke2.properties.particlelifespread			= definitions["barrelshot-huge"].smoke2.properties.particlelifespread * size
 definitions["barrelshot-huge"].smoke2.properties.particlesize				    = definitions["barrelshot-huge"].smoke2.properties.particlesize * size * 1.3
@@ -844,6 +752,11 @@ local purpleEffects = {
             colormap = [[0.075 0.03 0.12 0.04   0 0 0 0.01]]
         }
     },
+	mgfire = {
+		properties = {
+			explosiongenerator = [[custom:barrelshot-fire-purple]],
+		}
+	}
 }
 for defName, def in pairs(scavengerDefs) do
     for effect, effectParams in pairs(purpleEffects) do
@@ -851,11 +764,11 @@ for defName, def in pairs(scavengerDefs) do
             for param, paramValue in pairs(effectParams) do
                 if scavengerDefs[defName][effect][param] then
                     if param == 'properties' then
-                        for property,propertyValue in pairs(paramValue) do
-                            if scavengerDefs[defName][effect][param][property] then
-                                scavengerDefs[defName][effect][param][property] = propertyValue
-                            end
-                        end
+						for property,propertyValue in pairs(paramValue) do
+							if scavengerDefs[defName][effect][param][property] then
+								scavengerDefs[defName][effect][param][property] = propertyValue
+							end
+						end
                     else
                         scavengerDefs[defName][effect][param] = paramValue
                     end
