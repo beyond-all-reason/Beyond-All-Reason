586e56e562255bc01fe0f61c6e07f4d899841a31
icexuick <info@ixstudios.nl>
2021-09-16 22:33:23 +0200

    Ship Wake FX upgrade & Optimization #5
    Rest of the T2 ships for Cortex + Last 2 tweaks to Armships

--- units/CorShips/T2/corsjam.lua
+++ units/CorShips/T2/corsjam.lua
@@ -89,6 +89,9 @@ return {
 			},
 		},
 		sfxtypes = {
+			explosiongenerators = {
+				[1] = "custom:waterwake-tiny",
+			},
 			pieceexplosiongenerators = {
 				[1] = "deathceg2",
 				[2] = "deathceg3",
