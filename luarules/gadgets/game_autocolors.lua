849f2574f260beb63f2f3e7de49356220196a513
Damgam <c_2_damiano@op.pl>
2021-09-18 15:43:50 +0200

    re-enabled randomized colors, but with new palette

--- luarules/gadgets/game_autocolors.lua
+++ luarules/gadgets/game_autocolors.lua
@@ -42,6 +42,186 @@ if gadgetHandler:IsSyncedCode() then
         
         [2] = { -- Two Teams
             [1] = { -- First Team Blue
+                -- Blue
+                [1] = {0,       80,     255 },  -- Armada Blue #0050ff
+                [2] = {102,     150,    255 },  -- Lighter Blue #6696ff
+                [3] = {179,     203,    255 },  -- Very Light Blue #b3cbff
+                [4] = {0,       57,     179 },  -- Dark Blue #0039b3
+                -- Green    
+                [5] = {0,       255,    0   },  -- Normal Green #00ff00
+                [6] = {102,     255,    102 },  -- Lighter Green #66ff66
+                [7] = {179,     255,    179 },  -- Very Light Green #b3ffb3
+                [8] = {0,       153,    0   },  -- Dark Green #009900
+                -- Aqua 
+                [9] = {0,       255,    255 },  -- Normal Aqua #00ffff
+                [10]= {128,     255,    255 },  -- Lighter Aqua #80ffff
+                [11]= {179,     255,    255 },  -- Very Light Aqua #b3ffff
+                [12]= {0,       153,    153 },  -- Dark Aqua #009999
+                -- Purple (can't go full spectrum because of Scavengers)
+                [13]= {153,     102,    255 },  -- Lighter Purple #9966ff
+                [14]= {204,     179,    255 },  -- Very Light Purple #ccb3ff
+            },  
+            [2] = { -- Second Team Red  
+                -- Red  
+                [1] = {255,     16,     5   },  -- Cortex Red #ff0f06
+                [2] = {255,     107,    102 },  -- Lighter Red #ff6b66
+                [3] = {255,     181,    179 },  -- Very Light Red #ffb5b3
+                [4] = {153,     5,      0   },  -- Dark Red #990500
+                -- Yellow   
+                [5] = {255,     255,    0   },  -- Normal Yellow #ffff00
+                [6] = {255,     255,    102 },  -- Lighter Yellow #ffff66
+                [7] = {255,     255,    179 },  -- Very Light Yellow #ffffb3
+                [8] = {204,     204,    0   },  -- Dark Yellow #cccc00
+                -- Orange   
+                [9] = {255,     153,    0   },  -- Normal Orange #ff9900
+                [10]= {255,     184,    77  },  -- Lighter Orange #ffc266
+                [11]= {255,     224,    179 },  -- Very Light Orange #ffe0b3
+                [12]= {179,     107,    0   },  -- Dark Orange/Brown #b36b00
+                -- Pink
+                [13]= {255,     0,      128 },  -- Pink #ff0080
+                [14]= {255,     102,    179 },  -- Lighter Pink #ff66b3
+                [15]= {179,     0,      89  },  -- Dark Pink #b30059
+            },
+        },
+        
+        [3] = { -- Three Teams
+            [1] = { -- First Team Blue
+                [1] = {82,      151,    255  },
+                [2] = {47,      66,     238  },
+                [3] = {147,     226,    251  },
+                [4] = {8,       37,     190  },
+                [5] = {35,      11,     129  },
+            },
+            [2] = { -- Second Team Red
+                [1] = {231,     0,      0   },
+                [2] = {255,     125,    32  },
+                [3] = {255,     232,    22  },
+                [4] = {166,     14,     5   },
+                [5] = {118,     39,     6   },
+            },
+            [3] = { -- Third Team Green
+                [1] = {10,      232,    32  },
+                [2] = {10,      142,    7   },
+                [3] = {117,     253,    147 },
+                [4] = {5,       84,     13  },
+                [5] = {45,      57,     9   },
+            },
+        },
+
+        [4] = { -- Four Teams
+            [1] = { -- First Team Blue
+                [1] = {82,      151,    255  },
+                [2] = {47,      66,     238  },
+                [3] = {147,     226,    251  },
+                [4] = {8,       37,     190  },
+            },
+            [2] = { -- Second Team Red
+                [1] = {231,     0,      0   },
+                [2] = {255,     125,    32  },
+                [3] = {255,     232,    22  },
+                [4] = {166,     14,     5   },
+            },
+            [3] = { -- Third Team Green
+                [1] = {10,      232,    32  },
+                [2] = {10,      142,    7   },
+                [3] = {117,     253,    147 },
+                [4] = {5,       84,     13  },
+            },
+            [4] = { -- Fourth Team Purple
+                [1] = {200,     102,    246 },
+                [2] = {134,     10,     232 },
+                [3] = {191,     169,    255 },
+                [4] = {94,      9,      178 },
+            },
+        },
+
+        [5] = { -- Five Teams
+            [1] = { -- First Team Blue
+                [1] = {82,      151,    255  },
+                [2] = {47,      66,     238  },
+                [3] = {147,     226,    251  },
+            },
+            [2] = { -- Second Team Red
+                [1] = {231,     0,      0   },
+                [2] = {255,     125,    32  },
+                [3] = {166,     14,     5   },
+            },
+            [3] = { -- Third Team Green
+                [1] = {10,      232,    32  },
+                [2] = {10,      142,    7   },
+                [3] = {117,     253,    147 },
+            },
+            [4] = { -- Fourth Team Purple
+                [1] = {200,     102,    246 },
+                [2] = {134,     10,     232 },
+                [3] = {191,     169,    255 },
+            },
+            [5] = { -- Fifth Team Yellow
+                [1] = {255,     232,    22  },
+                [2] = {191,     151,    8   },
+                [3] = {255,     243,    135 },
+            },
+        },
+
+        [6] = { -- Six Teams
+            [1] = { -- First Team Blue
+                [1] = {82,      151,    255  },
+                [2] = {47,      66,     238  },
+            },
+            [2] = { -- Second Team Red
+                [1] = {231,     0,      0   },
+                [2] = {166,     14,     5   },
+            },
+            [3] = { -- Third Team Green
+                [1] = {10,      232,    32  },
+                [2] = {10,      142,    7   },
+            },
+            [4] = { -- Fourth Team Purple
+                [1] = {200,     102,    246 },
+                [2] = {134,     10,     232 },
+            },
+            [5] = { -- Fifth Team Yellow 
+                [1] = {255,     232,    22  },
+                [2] = {191,     151,    8   },
+            },
+            [6] = { -- Sixth team Orange
+                [1] = {255,     161,    73  },
+                [2] = {222,     93,     0   },
+            },
+        },
+    }
+
+    local OldFFAColors = {
+        [1] = {0,       80,     255 },    
+        [2] = {255,     16,     5   },    
+        [3] = {10,      232,    18  },  
+        [4] = {255,     232,    22  }, 
+        [5] = {147,     226,    251 },    
+        [6] = {255,     125,    32  },  
+        [7] = {41,      166,    176 },
+        [8] = {229,     18,     120 }, 
+        [9] = {191,     169,    255 },
+        [10] ={255,     243,    135 }, 
+        [11] ={0,       170,    99  },
+        [12] ={72,      9,      24  },
+        [13] ={117,     253,    147 }, 
+        [14] ={251,     167,    120 },
+        [15] ={39,      63,     84  },
+        [16] ={118,     39,     6   },
+        [17] ={127,     170,    255 },
+        [18] ={165,     0,      0   },
+        [19] ={0,       106,    127 },
+        [20] ={130,     130,    0   },
+        [21] ={40,      165,    0   },
+        [22] ={171,     90,     0   },
+        [23] ={0,       38,     117 },
+        [24] ={165,     0,      138 },
+    }
+
+    local OldTeamColors = {
+        
+        [2] = { -- Two Teams
+            [1] = { -- First Team Blue
                 [1] = {0,       80,     255 },    
                 [2] = {10,      232,    18  },    
                 [3] = {147,     226,    251 },     
@@ -194,10 +374,11 @@ if gadgetHandler:IsSyncedCode() then
     end
 
     local ffaCounts = 1
+    local ffaLoop = 1
     local allyTeamCounts = 0
     local TeamCounts = {}
 
-    Sequential = true
+    Sequential = false
     -- GenerateRandomizedTeamColorTables
     if Sequential == true then
         RandomizedFFAColors = FFAColors
@@ -251,16 +432,17 @@ if gadgetHandler:IsSyncedCode() then
             Spring.SetTeamRulesParam(teamID, "AutoTeamColorBlue", GaiaColor[3])
         elseif isFFA then
             if RandomizedFFAColors[ffaCounts] then
-                Spring.SetTeamRulesParam(teamID, "AutoTeamColorRed", RandomizedFFAColors[ffaCounts][1])
-                Spring.SetTeamRulesParam(teamID, "AutoTeamColorGreen", RandomizedFFAColors[ffaCounts][2])
-                Spring.SetTeamRulesParam(teamID, "AutoTeamColorBlue", RandomizedFFAColors[ffaCounts][3])
+                Spring.SetTeamRulesParam(teamID, "AutoTeamColorRed", RandomizedFFAColors[ffaCounts][1]+math.random(-ffaLoop,ffaLoop))
+                Spring.SetTeamRulesParam(teamID, "AutoTeamColorGreen", RandomizedFFAColors[ffaCounts][2]+math.random(-ffaLoop,ffaLoop))
+                Spring.SetTeamRulesParam(teamID, "AutoTeamColorBlue", RandomizedFFAColors[ffaCounts][3]+math.random(-ffaLoop,ffaLoop))
                 ffaCounts = ffaCounts + 1
             else
                 ffaCounts = 1
+                ffaLoop = ffaLoop + 15
                 if RandomizedFFAColors[ffaCounts] then
-                    Spring.SetTeamRulesParam(teamID, "AutoTeamColorRed", RandomizedFFAColors[ffaCounts][1])
-                    Spring.SetTeamRulesParam(teamID, "AutoTeamColorGreen", RandomizedFFAColors[ffaCounts][2])
-                    Spring.SetTeamRulesParam(teamID, "AutoTeamColorBlue", RandomizedFFAColors[ffaCounts][3])
+                    Spring.SetTeamRulesParam(teamID, "AutoTeamColorRed", RandomizedFFAColors[ffaCounts][1]+math.random(-ffaLoop,ffaLoop))
+                    Spring.SetTeamRulesParam(teamID, "AutoTeamColorGreen", RandomizedFFAColors[ffaCounts][2]+math.random(-ffaLoop,ffaLoop))
+                    Spring.SetTeamRulesParam(teamID, "AutoTeamColorBlue", RandomizedFFAColors[ffaCounts][3]+math.random(-ffaLoop,ffaLoop))
                     ffaCounts = ffaCounts + 1
                 else
                     Spring.Echo("[AUTOCOLORS] Error: Missing FFA Colors")
@@ -274,21 +456,22 @@ if gadgetHandler:IsSyncedCode() then
         else
             if not TeamCounts[allyTeamID] then
                 allyTeamCounts = allyTeamCounts + 1
-                TeamCounts[allyTeamID] = {allyTeamCounts,1}
+                TeamCounts[allyTeamID] = {allyTeamCounts,1, 1}
             end
             if RandomizedTeamColors[allyTeamNumber] then
                 if RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]] then
                     if RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]] then
-                        Spring.SetTeamRulesParam(teamID, "AutoTeamColorRed", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][1])
-                        Spring.SetTeamRulesParam(teamID, "AutoTeamColorGreen", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][2])
-                        Spring.SetTeamRulesParam(teamID, "AutoTeamColorBlue", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][3])
+                        Spring.SetTeamRulesParam(teamID, "AutoTeamColorRed", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][1]+math.random(-TeamCounts[allyTeamID][3],TeamCounts[allyTeamID][3]))
+                        Spring.SetTeamRulesParam(teamID, "AutoTeamColorGreen", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][2]+math.random(-TeamCounts[allyTeamID][3],TeamCounts[allyTeamID][3]))
+                        Spring.SetTeamRulesParam(teamID, "AutoTeamColorBlue", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][3]+math.random(-TeamCounts[allyTeamID][3],TeamCounts[allyTeamID][3]))
                         TeamCounts[allyTeamID][2] = TeamCounts[allyTeamID][2] + 1
                     else
                         TeamCounts[allyTeamID][2] = 1
+                        TeamCounts[allyTeamID][3] = TeamCounts[allyTeamID][3] + 50
                         if RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]] then
-                            Spring.SetTeamRulesParam(teamID, "AutoTeamColorRed", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][1])
-                            Spring.SetTeamRulesParam(teamID, "AutoTeamColorGreen", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][2])
-                            Spring.SetTeamRulesParam(teamID, "AutoTeamColorBlue", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][3])
+                            Spring.SetTeamRulesParam(teamID, "AutoTeamColorRed", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][1]+math.random(-TeamCounts[allyTeamID][3],TeamCounts[allyTeamID][3]))
+                            Spring.SetTeamRulesParam(teamID, "AutoTeamColorGreen", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][2]+math.random(-TeamCounts[allyTeamID][3],TeamCounts[allyTeamID][3]))
+                            Spring.SetTeamRulesParam(teamID, "AutoTeamColorBlue", RandomizedTeamColors[allyTeamNumber][TeamCounts[allyTeamID][1]][TeamCounts[allyTeamID][2]][3]+math.random(-TeamCounts[allyTeamID][3],TeamCounts[allyTeamID][3]))
                             TeamCounts[allyTeamID][2] = TeamCounts[allyTeamID][2] + 1
                         else
                             Spring.Echo("[AUTOCOLORS] Error: Missing Team Colors")
