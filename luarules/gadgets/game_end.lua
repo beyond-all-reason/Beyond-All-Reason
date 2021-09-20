fab18df78d6230bacf2dcec61bf664dd7ed18c18
Floris <floris@ruwhof.net>
2021-09-20 21:25:09 +0200

    Revert "game_end: optimization retry"
    This reverts commit 9a2f10fa7ec72ac6a159ba2137d988639b7d87a1.

--- luarules/gadgets/game_end.lua
+++ luarules/gadgets/game_end.lua
@@ -15,7 +15,11 @@ if gadgetHandler:IsSyncedCode() then
 	-- Allyteam explosion when no coms are left (killing all remaining units of that allyteam) is implemented in teamcomends.lua
 
 	local sharedDynamicAllianceVictory = Spring.GetModOptions().shareddynamicalliancevictory
-	local fixedallies = Spring.GetModOptions().fixedallies
+
+	local ignoreGaia = true
+
+	--------------------------------------------------------------------------------
+	--------------------------------------------------------------------------------
 
 	local KillTeam = Spring.KillTeam
 	local GetAllyTeamList = Spring.GetAllyTeamList
@@ -30,33 +34,35 @@ if gadgetHandler:IsSyncedCode() then
 	local AreTeamsAllied = Spring.AreTeamsAllied
 	local GetGameFrame = Spring.GetGameFrame
 
-	local playerQuitIsDead = true	-- gets turned off for 1v1's
-	local oneTeamWasActive = false
-	local teamToAllyTeam = {}
-	local playerIDtoAIs = {}
-	local gaiaTeamID = Spring.GetGaiaTeamID()
-	local gaiaAllyTeamID = select(6, Spring.GetTeamInfo(gaiaTeamID))
-	local playerList = GetPlayerList()
-
-	local allyTeamInfos = {}
-	--allyTeamInfos structure: (excluding gaia)
+	--------------------------------------------------------------------------------
+	--------------------------------------------------------------------------------
+	--allyTeamInfos structure:
 	-- allyTeamInfos = {
 	--	[allyTeamID] = {
 	--		teams = {
 	--			[teamID]= {
-	--				players = {
-	--					[playerID] = isControlling
-	--				},
 	--				unitCount,
+	--				isGaia,
 	--				dead,
 	--				isAI,
 	--				isControlled,
+	--				players = {
+	--					[playerID] = isControlling
+	--				},
 	--			},
 	--		},
 	--		unitCount,
+	--		isGaia,
 	--		dead,
 	--	},
 	--}
+	local playerQuitIsDead = true
+	local oneTeamWasActive = false
+	local allyTeamInfos = {}
+	local teamToAllyTeam = {}
+	local playerIDtoAIs = {}
+	local gaiaTeamID = Spring.GetGaiaTeamID()
+	local playerList = GetPlayerList()
 
 
 	local function UpdateAllyTeamIsDead(allyTeamID)
@@ -67,9 +73,6 @@ if gadgetHandler:IsSyncedCode() then
 				dead = dead and (teamInfo.dead or not teamInfo.hasLeader)
 			else
 				dead = dead and (teamInfo.dead or not teamInfo.isControlled)
-			end
-			if not dead then
-				break
 			end
 		end
 		allyTeamInfos[allyTeamID].dead = dead
@@ -93,7 +96,7 @@ if gadgetHandler:IsSyncedCode() then
 		-- if team isn't AI controlled, then we need to check if we have attached players
 		if not teamInfo.isAI then
 			teamInfo.isControlled = false
-			for _, isControlling in pairs(teamInfo.players) do
+			for _,isControlling in pairs(teamInfo.players) do
 				if isControlling and isControlling > (gf - 60) then -- this entire crap is needed because GetPlayerInfo returns active = false for the next 30 gameframes after savegame load, and results in immediate end of loaded games if > 1v1 game
 					teamInfo.isControlled = true
 					break
@@ -107,16 +110,7 @@ if gadgetHandler:IsSyncedCode() then
 			allyTeamInfos[AIAllyTeam].teams[AITeam].isControlled = active
 		end
 		allyTeamInfos[allyTeamID].teams[teamID] = teamInfo
-
 		UpdateAllyTeamIsDead(allyTeamID)
-	end
-
-
-	local function CheckAllPlayers()
-		playerList = GetPlayerList()
-		for _, playerID in ipairs(playerList) do
-			CheckPlayer(playerID)
-		end
 	end
 
 
@@ -126,6 +120,8 @@ if gadgetHandler:IsSyncedCode() then
 
 
 	function gadget:Initialize()
+		playerList = GetPlayerList()
+
 		if Spring.GetModOptions().deathmode == 'neverend' then
 			gadgetHandler:RemoveGadget(self)
 			return
@@ -133,11 +129,12 @@ if gadgetHandler:IsSyncedCode() then
 
 		local teamCount = 0
 		for _, teamID in ipairs(GetTeamList()) do
-			if teamID ~= gaiaTeamID then
+			if not ignoreGaia or teamID ~= gaiaTeamID then
 				teamCount = teamCount + 1
 			end
 		end
-		if teamCount < 2 then  -- sandbox mode
+
+		if teamCount < 2 then  -- sandbox mode (gaia + possibly one player)
 			gadgetHandler:RemoveGadget(self)
 			return
 		elseif teamCount == 2 then  -- let player quit & rejoin in 1v1
@@ -145,21 +142,23 @@ if gadgetHandler:IsSyncedCode() then
 		end
 
 		-- at start, fill in the table of all alive allyteams
-		local gaiaAllyTeamID = select(6, Spring.GetTeamInfo(gaiaTeamID))
 		for _, allyTeamID in ipairs(GetAllyTeamList()) do
-			if allyTeamID ~= gaiaAllyTeamID then
-				local allyTeamInfo = {
-					unitCount = 0,
-					teams = {},
-				}
-				for _, teamID in ipairs(GetTeamList(allyTeamID)) do
-					teamToAllyTeam[teamID] = allyTeamID
-					local teamInfo = {
-						players = {},
-						hasLeader = select(2, GetTeamInfo(teamID, false)) >= 0,
-					}
+			local allyTeamInfo = {}
+			allyTeamInfo.unitCount = 0
+			allyTeamInfo.teams = {}
+			for _, teamID in ipairs(GetTeamList(allyTeamID)) do
+				teamToAllyTeam[teamID] = allyTeamID
+				local teamInfo = {}
+				teamInfo.players = {}
+				-- gaia
+				if teamID == gaiaTeamID then
+					allyTeamInfo.isGaia = true
+					teamInfo.isGaia = true
+					teamInfo.isControlled = true
+				else
 					-- engine AI
 					teamInfo.isAI = select(4, GetTeamInfo(teamID, false))
+					teamInfo.hasLeader = select(2, GetTeamInfo(teamID, false)) >= 0
 					if teamInfo.isAI then
 						-- store who hosts that engine AI
 						local AIHostPlayerID = select(3, GetAIInfo(teamID))
@@ -172,15 +171,38 @@ if gadgetHandler:IsSyncedCode() then
 						teamInfo.isAI = true
 						teamInfo.isControlled = true
 					end
-
-					teamInfo.unitCount = GetTeamUnitCount(teamID)
-					allyTeamInfo.unitCount = allyTeamInfo.unitCount + teamInfo.unitCount
-					allyTeamInfo.teams[teamID] = teamInfo
 				end
-				allyTeamInfos[allyTeamID] = allyTeamInfo
+				teamInfo.unitCount = GetTeamUnitCount(teamID)
+				allyTeamInfo.unitCount = allyTeamInfo.unitCount + teamInfo.unitCount
+				allyTeamInfo.teams[teamID] = teamInfo
+			end
+			allyTeamInfos[allyTeamID] = allyTeamInfo
+		end
+		for _, playerID in ipairs(playerList) do
+			CheckPlayer(playerID)
+		end
+	end
+
+
+	local function IsCandidateWinner(allyTeamID)
+		return not allyTeamInfos[allyTeamID].dead and (not ignoreGaia or not allyTeamInfos[allyTeamID].isGaia)
+	end
+
+
+	-- find the last remaining allyteam
+	local function CheckSingleAllyVictoryEnd()
+		local winnerCount = 0
+		local candidateWinners = {}
+		for allyTeamID in pairs(allyTeamInfos) do
+			if IsCandidateWinner(allyTeamID) then
+				winnerCount = winnerCount + 1
+				candidateWinners[winnerCount] = allyTeamID
+			end
+			if winnerCount > 1 then
+				return false
 			end
 		end
-		CheckAllPlayers()
+		return candidateWinners
 	end
 
 
@@ -197,35 +219,16 @@ if gadgetHandler:IsSyncedCode() then
 	end
 
 
-	-- find the last remaining allyteam
-	local function CheckSingleAllyVictoryEnd()
-		local winnerCount = 0
-		local candidateWinners = {}
-		local count = 0
-		for allyTeamID in pairs(allyTeamInfos) do
-			count = count + 1
-			if not allyTeamInfos[allyTeamID].dead then
-				winnerCount = winnerCount + 1
-				candidateWinners[winnerCount] = allyTeamID
-			end
-			if winnerCount > 1 then
-				return false
-			end
-		end
-		return candidateWinners
-	end
-
-
 	-- we have to cross check all the alliances
 	local function CheckSharedAllyVictoryEnd()
 		local candidateWinners = {}
 		local winnerCountSquared = 0
 		local aliveCount = 0
 		for allyTeamA in pairs(allyTeamInfos) do
-			if not allyTeamInfos[allyTeamA].dead then
+			if IsCandidateWinner(allyTeamA) then
 				aliveCount = aliveCount + 1
 				for allyTeamB in pairs(allyTeamInfos) do
-					if not allyTeamInfos[allyTeamB].dead and AreAllyTeamsDoubleAllied(allyTeamA, allyTeamB) then
+					if IsCandidateWinner(allyTeamB) and AreAllyTeamsDoubleAllied(allyTeamA, allyTeamB) then
 						-- store both check directions
 						-- since we're gonna check if we're allied against ourself, only secondAllyTeamID needs to be stored
 						candidateWinners[allyTeamB] = true
@@ -240,23 +243,25 @@ if gadgetHandler:IsSyncedCode() then
 		end
 
 		-- all the allyteams alive are bidirectionally allied against eachother, they are all winners
-		--local winnersCorrectFormat = {}
+		local winnersCorrectFormat = {}
 		local winnersCorrectFormatCount = 0
 		for winner in pairs(candidateWinners) do
 			winnersCorrectFormatCount = winnersCorrectFormatCount + 1
-			--winnersCorrectFormat[winnersCorrectFormatCount] = winner
+			winnersCorrectFormat[winnersCorrectFormatCount] = winner
 		end
 		return winnersCorrectFormatCount
 	end
 
 
 	function gadget:GameFrame(gf)
+		for _, playerID in ipairs(playerList) do
+			CheckPlayer(playerID) -- because not all events that we want to test call gadget:PlayerChanged (e.g. allying)
+		end
 		local winners
-		if fixedallies then
+		if not sharedDynamicAllianceVictory then
 			winners = CheckSingleAllyVictoryEnd()
 		else
-			CheckAllPlayers()	-- not sure how much this is actually needed
-			winners = sharedDynamicAllianceVictory and CheckSharedAllyVictoryEnd() or CheckSingleAllyVictoryEnd()
+			winners = CheckSharedAllyVictoryEnd()
 		end
 
 		if winners then
@@ -270,8 +275,10 @@ if gadgetHandler:IsSyncedCode() then
 
 
 	function gadget:PlayerChanged(playerID) -- not all events that we want to test call gadget:PlayerChanged (e.g. allying)
-		CheckAllPlayers()
+		playerList = GetPlayerList()
+		CheckPlayer(playerID)
 	end
+
 
 	function gadget:TeamDied(teamID)
 		local allyTeamID = teamToAllyTeam[teamID]
@@ -279,14 +286,10 @@ if gadgetHandler:IsSyncedCode() then
 		allyTeamInfo.teams[teamID].dead = true
 		allyTeamInfos[allyTeamID] = allyTeamInfo
 		UpdateAllyTeamIsDead(allyTeamID)
-		CheckAllPlayers()
 	end
 
 
 	function gadget:UnitCreated(unitID, unitDefID, unitTeamID)
-		if unitTeamID == gaiaTeamID then
-			return
-		end
 		local allyTeamID = teamToAllyTeam[unitTeamID]
 		local allyTeamInfo = allyTeamInfos[allyTeamID]
 		allyTeamInfo.teams[unitTeamID].unitCount = allyTeamInfo.teams[unitTeamID].unitCount + 1
@@ -298,9 +301,6 @@ if gadgetHandler:IsSyncedCode() then
 
 
 	function gadget:UnitDestroyed(unitID, unitDefID, unitTeamID)
-		if unitTeamID == gaiaTeamID then
-			return
-		end
 		local allyTeamID = teamToAllyTeam[unitTeamID]
 		local allyTeamInfo = allyTeamInfos[allyTeamID]
 		local teamUnitCount = allyTeamInfo.teams[unitTeamID].unitCount - 1
@@ -308,6 +308,9 @@ if gadgetHandler:IsSyncedCode() then
 		allyTeamInfo.teams[unitTeamID].unitCount = teamUnitCount
 		allyTeamInfo.unitCount = allyTeamUnitCount
 		allyTeamInfos[allyTeamID] = allyTeamInfo
+		if allyTeamInfo.isGaia and ignoreGaia then
+			return
+		end
 
 		if allyTeamUnitCount == 0 then
 			for teamID in pairs(allyTeamInfo.teams) do
@@ -345,7 +348,8 @@ if gadgetHandler:IsSyncedCode() then
 	end
 
 
-else	-- Unsynced
+else
+	-- Unsynced
 
 
 	local IsCheatingEnabled = Spring.IsCheatingEnabled
@@ -363,12 +367,12 @@ else	-- Unsynced
 	end
 
 	function gadget:GameFrame(gf)
-		if not cheated then
+		if cheated == false then
 			cheated = IsCheatingEnabled()
 		end
 	end
 
-	local function ScenarioGameEnd(_, winners)
+	function ScenarioGameEnd(_, winners)
 		if Spring.IsReplay() then
 			return
 		end
