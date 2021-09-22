7906cd5dff6f4276c0c893354d8098091256fded
Damgam <c_2_damiano@op.pl>
2021-09-22 23:00:48 +0200

    gadget to prevent overlapping structures (nanos for now)

---
+++ luarules/gadgets/unit_stack_hack_fix.lua
@@ -0,0 +1,99 @@
+function gadget:GetInfo()
+    return {
+      name      = "Anti Stacking Hax",
+      desc      = "123",
+      author    = "Damgam",
+      date      = "2021",
+      layer     = -100,
+      enabled   = true,
+    }
+end
+
+if not gadgetHandler:IsSyncedCode() then
+	return false
+end
+
+affectedUnits = {}
+local affectedUnitTypes = {
+    "armnanotc",
+    "cornanotc",
+}
+
+local function CheckIfUnitIsAffected(unitName)
+    for i = 1,#affectedUnitTypes do
+        if unitName == affectedUnitTypes[i] then
+            return true
+        end
+        if (string.find(unitName, "_scav")) then
+            local scavUnitName = string.sub(unitName, 1, string.len(unitName)-5)
+            if scavUnitName == affectedUnitTypes[i] then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+
+function gadget:UnitCreated(unitID, unitDefID)
+    local unitName = UnitDefs[unitDefID].name
+    if CheckIfUnitIsAffected(unitName) then
+        Spring.Echo("Added "..unitName)
+        table.insert(affectedUnits, unitID)
+    end
+end
+
+function gadget:UnitDestroyed(unitID, unitDefID)
+    local unitName = UnitDefs[unitDefID].name
+    if #affectedUnits > 0 then
+        for i = 1,#affectedUnits do
+            if affectedUnits[i] == unitID then
+                Spring.Echo("Removed "..unitName)
+                table.remove(affectedUnits, i)
+            end
+        end
+    end
+end
+
+function gadget:GameFrame(n)
+    if #affectedUnits > 0 then
+        for i = 1,#affectedUnits do
+            local unitID = affectedUnits[i]
+            local unitDefID = Spring.GetUnitDefID(unitID)
+            local testRange = math.floor(((UnitDefs[unitDefID].xsize + UnitDefs[unitDefID].zsize)*0.5)*7.8)
+            local nearestAlly = Spring.GetUnitNearestAlly(unitID, testRange)
+            if nearestAlly then
+                local nearestAllyName = UnitDefs[Spring.GetUnitDefID(nearestAlly)].name
+                local nearestAllyCanMove = UnitDefs[Spring.GetUnitDefID(nearestAlly)].canMove
+                if not nearestAllyCanMove then
+                    local x,y,z = Spring.GetUnitPosition(unitID)
+                    local ax,ay,az = Spring.GetUnitPosition(nearestAlly)
+                    local r = math.random(1,4)
+                    if r == 1 then
+                        if x == ax then
+                            Spring.SetUnitPosition(unitID, x+math.random(-testRange,testRange), z+math.random(-testRange,testRange))
+                        end
+                    elseif r == 2 then
+                        if z == az then
+                            Spring.SetUnitPosition(unitID, x+math.random(-testRange,testRange), z+math.random(-testRange,testRange))
+                        end
+                    elseif r == 3 then
+                        if x > ax then
+                            Spring.SetUnitPosition(unitID, x+1, z)
+                        end
+                        if x < ax then
+                            Spring.SetUnitPosition(unitID, x-1, z)
+                        end
+                    elseif r == 4 then
+                        if z > az then
+                            Spring.SetUnitPosition(unitID, x, z+1)
+                        end
+                        if z < az then
+                            Spring.SetUnitPosition(unitID, x, z-1)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
