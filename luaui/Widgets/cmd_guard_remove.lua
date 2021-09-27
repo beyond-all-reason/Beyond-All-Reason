5a61f3b5d9be60cc0f91cffa834bb120fdbc3833
Born2Crawl <born2crawl@gmail.com>
2021-09-27 18:42:24 +0200

    Remove Zero-K related code, unecessary for BAR

--- luaui/Widgets/cmd_guard_remove.lua
+++ luaui/Widgets/cmd_guard_remove.lua
@@ -3,7 +3,7 @@ function widget:GetInfo()
 	return {
 		name      = "Guard Remove",
 		desc      = "Removes non-terminating orders when they seem to have been used accidentally.",
-		author    = "Google Frog",
+		author    = "Google Frog, Born2Crawl (adapted for BAR)",
 		date      = "13 July 2017",
 		license   = "GNU GPL, v2 or later",
 		layer     = 0,
@@ -14,31 +14,11 @@ end
 include("keysym.h.lua")
 VFS.Include("LuaRules/Configs/customcmds.h.lua")
 
---------------------------------------------------------------------------------
--- Epic Menu Options
---------------------------------------------------------------------------------
-
-options_path = 'Settings/Unit Behaviour'
-options = {
-	keepTarget = {
-		name = "Shift removes constructor guard",
-		type = "bool",
-		value = true,
-		desc = "Removes non-terminating commands (guard and patrol) from constructor command queues when they have a command added to their queue.",
-		noHotkey = true,
-	},
-}
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
 local doCommandRemove = false
 
 local removableCommand = {
 	[CMD.GUARD] = true,
 	[CMD.PATROL] = true,
-	--[CMD_ORBIT] = true,
-	--[CMD_AREA_GUARD] = true,
 }
 
 local function IsValidUnit(unitID)
