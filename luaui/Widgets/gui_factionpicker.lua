ad4b7aa8f7e51ff66e2fba6453a7d4a893f12edf
Floris <floris@ruwhof.net>
2021-10-05 14:33:58 +0200

    factionpicker: fix + cleanup

--- luaui/Widgets/gui_factionpicker.lua
+++ luaui/Widgets/gui_factionpicker.lua
@@ -10,8 +10,6 @@ function widget:GetInfo()
 	}
 end
 
-local chobbyLoaded = (Spring.GetMenuName and string.find(string.lower(Spring.GetMenuName()), 'chobby') ~= nil)
-
 local restorePreviousFaction = false
 
 local factions = {
@@ -25,19 +23,11 @@ local width = 0
 local height = 0
 local bgBorderOrg = 0.003
 local bgBorder = bgBorderOrg
-local bgMargin = 0.008
 
 local myTeamID = Spring.GetMyTeamID()
 local stickToBottom = true
 
 local startDefID = Spring.GetTeamRulesParam(myTeamID, 'startUnit')
-
-local ui_tileopacity = tonumber(Spring.GetConfigFloat("ui_tileopacity", 0.012) or 0.012)
-local bgtexScale = tonumber(Spring.GetConfigFloat("ui_tilescale", 7) or 7)	-- lower = smaller tiles
-local bgtexSize
-
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
 
 local factionRect = {}
 for i, faction in pairs(factions) do
@@ -53,111 +43,22 @@ local ui_opacity = tonumber(Spring.GetConfigFloat("ui_opacity", 0.6) or 0.66)
 local ui_scale = tonumber(Spring.GetConfigFloat("ui_scale", 1) or 1)
 local glossMult = 1 + (2 - (ui_opacity * 2))    -- increase gloss/highlight so when ui is transparant, you can still make out its boundaries and make it less flat
 
+local isSpec = Spring.GetSpectatingState()
 local backgroundRect = {}
 local lastUpdate = os.clock() - 1
 
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
-
 local os_clock = os.clock
 
-local GL_QUADS = GL.QUADS
-local GL_TRIANGLE_FAN = GL.TRIANGLE_FAN
-local glBeginEnd = gl.BeginEnd
-local glTexture = gl.Texture
-local glTexRect = gl.TexRect
 local glColor = gl.Color
-local glRect = gl.Rect
-local glVertex = gl.Vertex
-local glDepthTest = gl.DepthTest
-
 local glBlending = gl.Blending
 local GL_SRC_ALPHA = GL.SRC_ALPHA
 local GL_ONE_MINUS_SRC_ALPHA = GL.ONE_MINUS_SRC_ALPHA
 local GL_ONE = GL.ONE
 
-local mCos = math.cos
-local mSin = math.sin
-local math_min = math.min
-
-local isSpec = Spring.GetSpectatingState()
 
 local font, font2, bgpadding, chobbyInterface, dlistGuishader, dlistFactionpicker, bpWidth, bpHeight, rectMargin, fontSize
 
 local RectRound, UiElement, UiUnit
-
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
-
-local function DrawTexRectRound(px, py, sx, sy, cs, tl, tr, br, bl, offset)
-	local csyMult = 1 / ((sy - py) / cs)
-
-	local function drawTexCoordVertex(x, y)
-		local yc = 1 - ((y - py) / (sy - py))
-		local xc = (offset * 0.5) + ((x - px) / (sx - px)) + (-offset * ((x - px) / (sx - px)))
-		yc = 1 - (offset * 0.5) - ((y - py) / (sy - py)) + (offset * ((y - py) / (sy - py)))
-		gl.TexCoord(xc, yc)
-		gl.Vertex(x, y, 0)
-	end
-
-	-- mid section
-	drawTexCoordVertex(px + cs, py)
-	drawTexCoordVertex(sx - cs, py)
-	drawTexCoordVertex(sx - cs, sy)
-	drawTexCoordVertex(px + cs, sy)
-
-	-- left side
-	drawTexCoordVertex(px, py + cs)
-	drawTexCoordVertex(px + cs, py + cs)
-	drawTexCoordVertex(px + cs, sy - cs)
-	drawTexCoordVertex(px, sy - cs)
-
-	-- right side
-	drawTexCoordVertex(sx, py + cs)
-	drawTexCoordVertex(sx - cs, py + cs)
-	drawTexCoordVertex(sx - cs, sy - cs)
-	drawTexCoordVertex(sx, sy - cs)
-
-	-- bottom left
-	if ((py <= 0 or px <= 0) or (bl ~= nil and bl == 0)) and bl ~= 2 then
-		drawTexCoordVertex(px, py)
-	else
-		drawTexCoordVertex(px + cs, py)
-	end
-	drawTexCoordVertex(px + cs, py)
-	drawTexCoordVertex(px + cs, py + cs)
-	drawTexCoordVertex(px, py + cs)
-	-- bottom right
-	if ((py <= 0 or sx >= vsx) or (br ~= nil and br == 0)) and br ~= 2 then
-		drawTexCoordVertex(sx, py)
-	else
-		drawTexCoordVertex(sx - cs, py)
-	end
-	drawTexCoordVertex(sx - cs, py)
-	drawTexCoordVertex(sx - cs, py + cs)
-	drawTexCoordVertex(sx, py + cs)
-	-- top left
-	if ((sy >= vsy or px <= 0) or (tl ~= nil and tl == 0)) and tl ~= 2 then
-		drawTexCoordVertex(px, sy)
-	else
-		drawTexCoordVertex(px + cs, sy)
-	end
-	drawTexCoordVertex(px + cs, sy)
-	drawTexCoordVertex(px + cs, sy - cs)
-	drawTexCoordVertex(px, sy - cs)
-	-- top right
-	if ((sy >= vsy or sx >= vsx) or (tr ~= nil and tr == 0)) and tr ~= 2 then
-		drawTexCoordVertex(sx, sy)
-	else
-		drawTexCoordVertex(sx - cs, sy)
-	end
-	drawTexCoordVertex(sx - cs, sy)
-	drawTexCoordVertex(sx - cs, sy - cs)
-	drawTexCoordVertex(sx, sy - cs)
-end
-function TexRectRound(px, py, sx, sy, cs, tl, tr, br, bl, zoom)
-	gl.BeginEnd(GL.QUADS, DrawTexRectRound, px, py, sx, sy, cs, tl, tr, br, bl, zoom)
-end
 
 local function checkGuishader(force)
 	if WG['guishader'] then
@@ -228,8 +129,6 @@ function widget:ViewResize()
 			posX = 0
 		end
 	end
-
-	bgtexSize = bgpadding * bgtexScale
 
 	backgroundRect = { posX * vsx, (posY - height) * vsy, (posX + width) * vsx, posY * vsy }
 
@@ -330,6 +229,7 @@ function drawFactionpicker()
 	local maxCellWidth = math.floor((contentWidth / #factions) + 0.5)
 	local cellSize = math.min(maxCellHeight, maxCellWidth)
 	local padding = bgpadding
+
 	for i, faction in pairs(factions) do
 		factionRect[i] = {
 			math.floor(backgroundRect[3] - padding - (cellSize * i)),
@@ -337,19 +237,24 @@ function drawFactionpicker()
 			math.floor(backgroundRect[3] - padding - (cellSize * (i - 1))),
 			math.floor(backgroundRect[2] + cellSize)
 		}
-		glColor(1,1,1,1)
+		local disabled = Spring.GetTeamRulesParam(myTeamID, 'startUnit') ~= factions[i][1]
+		if disabled then
+			glColor(0.55,0.55,0.55,1)
+		else
+			glColor(1,1,1,1)
+		end
 		UiUnit(factionRect[i][1]+bgpadding, factionRect[i][2] + bgpadding, factionRect[i][3], factionRect[i][4],
 			nil,
 			1,1,1,1,
 			0,
-			nil, nil,
-			(Spring.GetTeamRulesParam(myTeamID, 'startUnit') == factions[i][1] and ":lr256,256:" or ":lgr256,256:") .. factions[i][3]
+			nil, disabled and 0 or nil,
+			factions[i][3]
 		)
 		-- faction name
-		if startDefID == factions[i][1] then
+		if not disabled then
 			font2:Print(factions[i][2], factionRect[i][1] + ((factionRect[i][3] - factionRect[i][1]) * 0.5), factionRect[i][2] + ((factionRect[i][4] - factionRect[i][2]) * 0.22) - (fontSize * 0.5), fontSize * 0.96, "co")
 		else
-			font2:Print("\255\200\200\200"..factions[i][2], factionRect[i][1] + ((factionRect[i][3] - factionRect[i][1]) * 0.5), factionRect[i][2] + ((factionRect[i][4] - factionRect[i][2]) * 0.22) - (fontSize * 0.5), fontSize * 0.96, "co")
+			font2:Print("\255\170\170\170"..factions[i][2], factionRect[i][1] + ((factionRect[i][3] - factionRect[i][1]) * 0.5), factionRect[i][2] + ((factionRect[i][4] - factionRect[i][2]) * 0.22) - (fontSize * 0.5), fontSize * 0.96, "co")
 		end
 	end
 	font2:End()
@@ -442,8 +347,7 @@ function widget:SetConfigData(data)
 			for i,v in pairs(factions) do
 				if factions[i][1] == startDefID then
 					startDefID = factions[i][1]
-					-- tell initial spawn
-					Spring.SendLuaRulesMsg('\138' .. tostring(factions[i][1]))
+					Spring.SendLuaRulesMsg('\138' .. tostring(factions[i][1]))	-- tell initial spawn
 					break
 				end
 			end
