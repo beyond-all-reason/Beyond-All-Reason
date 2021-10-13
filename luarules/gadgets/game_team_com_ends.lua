54ab782d71a51737ca5ef6cbedd6b086ca03a513
Floris <floris@ruwhof.net>
2021-10-13 19:02:34 +0200

    fixed armcs/cormls

--- luarules/gadgets/game_team_com_ends.lua
+++ luarules/gadgets/game_team_com_ends.lua
@@ -58,7 +58,7 @@ local function commanderDeath(teamID, unitID)
 			local units = Spring.GetTeamUnits(teamID)
 			totalUnits = totalUnits + #units
 		end
-		local periodMult = math.max(0.33, math.min(1, totalUnits / 250))	-- make low unitcount blow up faster
+		local periodMult = math.max(0.4, math.min(1, totalUnits / 300))	-- make low unitcount blow up faster
 		for _, teamID in ipairs(GetTeamList(allyTeamID)) do
 			GG.wipeoutTeam(teamID, x, z, unitID, periodMult)
 			if not select(3, Spring.GetTeamInfo(teamID)) then
