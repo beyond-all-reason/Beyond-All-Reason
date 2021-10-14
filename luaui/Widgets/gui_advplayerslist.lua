46e25ff64cda126df32ff31d36ea21473037dc05
Floris <floris@ruwhof.net>
2021-10-14 23:25:51 +0200

    playerlist: changed flag set

--- luaui/Widgets/gui_advplayerslist.lua
+++ luaui/Widgets/gui_advplayerslist.lua
@@ -102,8 +102,10 @@ local bgpadding = 3
 -- IMAGES
 --------------------------------------------------------------------------------
 
-local imageDirectory = ":l:" .. LUAUI_DIRNAME .. "Images/advplayerslist/"
+local imageDirectory = ":lb:" .. LUAUI_DIRNAME .. "Images/advplayerslist/"
 local flagsDirectory = imageDirectory .. "flags/"
+local flagsExt = '.png'
+local flagHeight = 10
 
 local pics = {
     currentPic = imageDirectory .. "indicator.dds",
@@ -2353,9 +2355,9 @@ end
 
 function DrawCountry(country, posY)
     if country ~= nil and country ~= "??" then
-        gl_Texture(flagsDirectory .. string.upper(country) .. ".dds")
+        gl_Texture(flagsDirectory .. string.upper(country) .. flagsExt)
         gl_Color(1, 1, 1)
-        DrawRect(m_country.posX + widgetPosX + 3, posY + 1, m_country.posX + widgetPosX + 17, posY + 15)
+        DrawRect(m_country.posX + widgetPosX + 3, posY + 8 - (flagHeight/2), m_country.posX + widgetPosX + 17, posY + 8 + (flagHeight/2))
     end
 end
 
