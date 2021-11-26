35a3ab3cca453f144e65c11943d7cd9ae4f5b3a7
fruehling76 <39665636+fruehling76@users.noreply.github.com>
2021-11-26 11:14:27 +0100

    update corfav
    new  model

--- units/CorVehicles/corfav.lua
+++ units/CorVehicles/corfav.lua
@@ -50,7 +50,7 @@ return {
 			firingceg = "",
 			flare1name = "flare",
 			kickback = "0",
-			model_author = "Mr Bob, PtaQ",
+			model_author = "Flaka",
 			normaltex = "unittextures/cor_normal.dds",
 			restoretime = "3000",
 			rockstrength = "0",
