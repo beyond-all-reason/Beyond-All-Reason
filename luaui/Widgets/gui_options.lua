6351c9820c098ebc1a7210b5fd2215d674e46956
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2021-12-04 10:57:25 -0500

    Options: Remove more obsolete code

--- luaui/Widgets/gui_options.lua
+++ luaui/Widgets/gui_options.lua
@@ -1900,26 +1900,7 @@ function init()
 		},
 
 		{ id = "ssao", group = "gfx", category = types.basic, widget = "SSAO", name = texts.option.ssao, type = "bool", value = GetWidgetToggleValue("SSAO"), description = texts.option.ssao_descr },
-		--{ id = "ssao", group = "gfx", category = types.basic, name = texts.option.ssao, type = "select", options = { 'disabled', 'enabled', 'high' }, value = 0, description = texts.option.ssao_descr,
-		--  onchange = function(i, value)
-		--	  if value == 1 then
-		--		  widgetHandler:DisableWidget("SSAO")
-		--	  else
-		--		  if not GetWidgetToggleValue("SSAO") then
-		--			  widgetHandler:EnableWidget("SSAO")
-		--		  end
-		--		  saveOptionValue('SSAO', 'ssao', 'setPreset', { 'preset' }, value - 1)
-		--	  end
-		--  end,
-		--  onload = function(i)
-		--	  if not GetWidgetToggleValue("SSAO") then
-		--		  options[getOptionByID('ssao')].value = 1
-		--	  else
-		--		  loadWidgetData("SSAO", "ssao", { 'preset' })
-		--		  options[getOptionByID('ssao')].value = options[getOptionByID('ssao')].value + 1
-		--	  end
-		--  end,
-		--},
+
 		{ id = "ssao_strength", group = "gfx", category = types.dev, name = widgetOptionColor .. "   " .. texts.option.ssao_strength, type = "slider", min = 5, max = 11, step = 1, value = 8, description = '',
 		  onchange = function(i, value)
 			  saveOptionValue('SSAO', 'ssao', 'setStrength', { 'strength' }, value)
@@ -1938,17 +1919,6 @@ function init()
 			  loadWidgetData("Bloom Shader Deferred", "bloomdeferredbrightness", { 'glowAmplifier' })
 		  end,
 		},
-
-		--{ id = "bloom", group = "gfx", category = types.basic, widget = "Bloom Shader", name = texts.option.bloom, type = "bool", value = GetWidgetToggleValue("Bloom Shader"), description = texts.option.bloom_descr },
-		--{ id = "bloombrightness", group = "gfx", category = types.advanced, name = widgetOptionColor .. "   " .. texts.option.bloombrightness, type = "slider", min = 0.1, max = 0.25, step = 0.05, value = 0.15, description = '',
-		--  onchange = function(i, value)
-		--	  saveOptionValue('Bloom Shader', 'bloom', 'setBrightness', { 'basicAlpha' }, value)
-		--  end,
-		--  onload = function(i)
-		--	  loadWidgetData("Bloom Shader", "bloombrightness", { 'basicAlpha' })
-		--  end,
-		--},
-
 
 		{ id = "lighteffects", group = "gfx", category = types.basic, name = texts.option.lighteffects, type = "bool", value = GetWidgetToggleValue("Light Effects"), description = texts.option.lighteffects_descr,
 		  onload = function(i)
@@ -1989,15 +1959,6 @@ function init()
 
 		{ id = "label_gfx_environment", group = "gfx", name = texts.option.label_environment, category = types.basic },
 		{ id = "label_gfx_environment_spacer", group = "gfx", category = types.basic },
-
-		--{ id = "losopacity", group = "gfx", category = types.advanced, name = texts.option.lineofsight..widgetOptionColor .. "  " .. texts.option.losopacity, type = "slider", min = 0.5, max = 3, step = 0.1, value = (WG['los'] ~= nil and WG['los'].getOpacity ~= nil and WG['los'].getOpacity()) or 1, description = '',
-		--  onload = function(i)
-		--	  loadWidgetData("LOS colors", "losopacity", { 'opacity' })
-		--  end,
-		--  onchange = function(i, value)
-		--	  saveOptionValue('LOS colors', 'los', 'setOpacity', { 'opacity' }, value)
-		--  end,
-		--},
 
 		{ id = "water", group = "gfx", category = types.basic, name = texts.option.water, type = "select", options = { 'basic', 'reflective', 'dynamic', 'reflective&refractive', 'bump-mapped' }, value = desiredWaterValue + 1,
 		  onload = function(i)
@@ -2047,7 +2008,6 @@ function init()
 			  Spring.SetConfigInt("TreeWind", (value and 1 or 0))
 		  end,
 		},
-
 
 		{ id = "snow", group = "gfx", category = types.basic, widget = "Snow", name = texts.option.snow, type = "bool", value = GetWidgetToggleValue("Snow"), description = texts.option.snow_descr },
 		{ id = "snowmap", group = "gfx", category = types.dev, name = widgetOptionColor .. "   " .. texts.option.snowmap, type = "bool", value = true, description = texts.option.snowmap_descr,
@@ -2116,15 +2076,6 @@ function init()
 		  end
 		},
 
-		--{ id = "airjets", group = "gfx", category = types.dev, widget = "Airjets GL4", name = texts.option.airjets, type = "bool", value = GetWidgetToggleValue("Airjets GL4"), description = texts.option.airjets_descr },
-		--{ id = "jetenginefx_lights", group = "gfx", category = types.dev, name = widgetOptionColor .. "   " .. texts.option.jetenginefx_lights, type = "bool", value = true, description = texts.option.jetenginefx_lights_descr,
-		--  onload = function(i)
-		--	  loadWidgetData("Light Effects", "lups_jetenginefx_lights", { 'enableThrusters' })
-		--  end,
-		--  onchange = function(i, value)
-		--	  saveOptionValue('Light Effects', 'lighteffects', 'setThrusters', { 'enableThrusters' }, value)
-		--  end,
-		--},
 		{ id = "dof", group = "gfx", category = types.advanced, widget = "Depth of Field", name = texts.option.dof, type = "bool", value = GetWidgetToggleValue("Depth of Field"), description = texts.option.dof_descr },
 		{ id = "dof_autofocus", group = "gfx", category = types.dev, name = widgetOptionColor .. "   " .. texts.option.dof_autofocus, type = "bool", value = true, description = texts.option.dof_autofocus_descr,
 		  onload = function(i)
