25314f528b8665e4918a14d577aba93bf9eb6754
Damgam <c_2_damiano@op.pl>
2021-12-14 22:29:21 +0100

    scav boss nerf

--- luarules/gadgets/scavengers/boot.lua
+++ luarules/gadgets/scavengers/boot.lua
@@ -777,7 +777,7 @@ function gadget:UnitCreated(unitID, unitDefID, unitTeam)
 		for i = 1,#bossUnitList.Bosses do
 			if unitName == bossUnitList.Bosses[i] then
 				FinalBossUnitID = unitID
-				Spring.SetUnitArmored(unitID, true , 1/(spawnmultiplier*3))
+				Spring.SetUnitArmored(unitID, true , 1/(spawnmultiplier))
 				initialbosshealth = Spring.GetUnitHealth(unitID)
 
 				local stopScavUnits = Spring.GetTeamUnits(GaiaTeamID)
