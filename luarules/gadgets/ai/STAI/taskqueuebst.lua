48c42a7427ef3dab1f01af752eb323491ba2e8ce
vvbudh <vvbudh@fastmail.com>
2021-12-21 23:14:41 -0800

    I have made several modifications to the maximum number of units on the battlefield the AI plans to build. As well as changing the queue order to make the units they provide more useful in quantity. Example:5 rezzers instead of only 2. Speed really does matter!! Larger attack force and two scouts spawn instead of 1 for the first wave.

--- luarules/gadgets/ai/STAI/taskqueuebst.lua
+++ luarules/gadgets/ai/STAI/taskqueuebst.lua
@@ -28,7 +28,7 @@ local function MaxBuildDist(unitName, speed)
 end
 
 function TaskQueueBST:Init()
-	self.DebugEnabled = false
+	self.DebugEnabled = true
 	self.role = nil
 	self.active = false
 	self.currentProject = nil
