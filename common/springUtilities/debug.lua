77cd2654ac8be6ee32a2b7de28f4d6b12b368f42
Beherith <mysterme@gmail.com>
2022-01-05 16:43:24 +0100

    Add new Spring.Debug.TraceEcho( ... ) function, which will print all ... params, and also the stack trace of the callers.

--- common/springUtilities/debug.lua
+++ common/springUtilities/debug.lua
@@ -30,7 +30,38 @@ local function tableEcho(data, name, indent, tableChecked)
 	Spring.Echo(indent .. "},")
 end
 
+local function traceEcho(...)
+	local myargs = {...}
+	infostr = ""
+	for i,v in ipairs(myargs) do
+		infostr = infostr .. tostring(v) .. "\t"
+	end
+	if infostr ~= "" then infostr = infostr .. " " end 
+	local functionstr = "Trace:["
+	for i = 2, 10 do
+		if debug.getinfo(i) then
+			local funcName = (debug and debug.getinfo(i) and debug.getinfo(i).name)
+			if funcName then
+				functionstr = functionstr .. tostring(funcName) .. " <- "
+			else break end
+		else break end
+	end
+	functionstr = functionstr .. "]"
+	local arguments = ""
+	local funcName1 = (debug and debug.getinfo(2) and debug.getinfo(2).name) or "??"
+	if funcName1 ~= "??" then 
+		for i = 1, 10 do
+			local name, value = debug.getlocal(2, i)
+			if not name then break end
+			local sep = ((arguments == "") and "") or  "; "
+			arguments = arguments .. sep .. ((name and tostring(name)) or "name?") .. "=" .. tostring(value)
+		end
+	end
+	Spring.Echo(infostr .. functionstr .. " Args:(" .. arguments .. ")")
+end
+
 return {
 	ParamsEcho = paramsEcho,
 	TableEcho = tableEcho,
+	TraceEcho = traceEcho,
 }
