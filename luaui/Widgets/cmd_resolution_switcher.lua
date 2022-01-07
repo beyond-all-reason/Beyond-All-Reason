3ac43f37890da9d4008edaf35eeb02c890df4ca6
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2022-01-07 14:42:19 -0500

    Resolution: Refactor resolution testing widget to screen mode switching interface

--- luaui/Widgets/cmd_resolution_switcher.lua
+++ luaui/Widgets/cmd_resolution_switcher.lua
@@ -1,12 +1,13 @@
 function widget:GetInfo()
 	return {
-		name = "Resolution Testing",
-		desc = "Debug changing fulscreen/borderless/resolution",
+		name = "Screen Mode/Resolution Switcher",
+		desc = "Interface for setting screen mode and resolution",
 		layer = 0,
 		enabled = true,
 	}
 end
 
+local screenModes = {}
 local firstPassDrawFrame
 local screenModeIndex = 0
 
@@ -16,44 +17,45 @@ local windowType = {
 	windowed   = 3,
 }
 
-local screenModes = {}
-local display = -1
-for _, videoMode in ipairs(Platform.availableVideoModes) do
-	-- only capture the first occurance of the display index. Will contain maximum supported resolution
-	if display ~= videoMode.display then
-		display = videoMode.display
-		local fullscreen = {
-			display = display,
-			displayName = videoMode.displayName,
-			name = "Fullscreen",
-			type = windowType.fullscreen,
-			width = videoMode.w,
-			height = videoMode.h,
-		}
+local function refreshScreenModes()
+	local display = -1
+	for _, videoMode in ipairs(Platform.availableVideoModes) do
+		-- only capture the first occurance of the display index. Will contain maximum supported resolution
+		if display ~= videoMode.display then
+			display = videoMode.display
+			local fullscreen = {
+				display = display,
+				displayName = videoMode.displayName,
+				name = "Fullscreen",
+				type = windowType.fullscreen,
+				width = videoMode.w,
+				height = videoMode.h,
+			}
 
-		local borderless = {
-			display = display,
-			name = "Borderless",
-			displayName = videoMode.displayName,
-			type = windowType.borderless,
-			width = videoMode.w,
-			height = videoMode.h,
-		}
-		table.insert(screenModes, fullscreen)
-		table.insert(screenModes, borderless)
-	end
+			local borderless = {
+				display = display,
+				name = "Borderless",
+				displayName = videoMode.displayName,
+				type = windowType.borderless,
+				width = videoMode.w,
+				height = videoMode.h,
+			}
+			table.insert(screenModes, fullscreen)
+			table.insert(screenModes, borderless)
+		end
 
-	if videoMode.w >= 800 and videoMode.h > 600 then
-		local windowed = {
-			display = display,
-			displayName = videoMode.displayName,
-			name = videoMode.w .. " × " .. videoMode.h,
-			type = windowType.windowed,
-			width = videoMode.w,
-			height = videoMode.h,
-		}
+		if videoMode.w >= 800 and videoMode.h > 600 then
+			local windowed = {
+				display = display,
+				displayName = videoMode.displayName,
+				name = videoMode.w .. " × " .. videoMode.h,
+				type = windowType.windowed,
+				width = videoMode.w,
+				height = videoMode.h,
+			}
 
-		table.insert(screenModes, windowed)
+			table.insert(screenModes, windowed)
+		end
 	end
 end
 
@@ -63,16 +65,15 @@ local function changeScreenMode(index)
 	local screenMode = screenModes[index]
 
 	if screenMode.type == windowType.fullscreen then
-		Spring.Echo("windowType.fullscreen", screenMode.width, screenMode.height)
 		Spring.SetWindowGeometry(screenMode.display, 0, 0, screenMode.width, screenMode.height, true, true)
 	elseif screenMode.type == windowType.borderless then
-		Spring.Echo("windowType.borderless", 0, 0, screenMode.width, screenMode.height)
 		Spring.SetWindowGeometry(screenMode.display, 0, 0, screenMode.width, screenMode.height, false, true)
+	-- Windowed mode has a chicken-and-egg problem, where you can't know the window borders until you've already switched to windowed mode
+	-- This cannot be done in two consecutive SetWindowGeometry() calls, as there must be a one draw frame delay before the values of GetWindowGeometry() are updated
 	elseif screenMode.type == windowType.windowed then
 		local _, _, _, _ , borderTop, borderLeft, borderBottom, borderRight = Spring.GetWindowGeometry()
 		local width = screenMode.width - borderLeft - borderRight
 		local height = screenMode.height - borderTop - borderBottom
-		Spring.Echo("windowType.windowed", borderLeft, borderTop, width, height)
 		Spring.SetWindowGeometry(screenMode.display, borderLeft, borderTop, width, height, false, false)
 
 		if firstPassDrawFrame then
@@ -91,20 +92,15 @@ function widget:Update()
 end
 
 function widget:Initialize()
-	Spring.Echo("[Resolution Test] Use command '\\res #' to change screen specs")
-	local displayName
-	for i, mode in ipairs(screenModes) do
-		if mode.displayName ~= displayName then
-			Spring.Echo(mode.displayName)
-			displayName = mode.displayName
-		end
-		Spring.Echo(i .. ": " .. mode.name)
-	end
-end
+	refreshScreenModes()
 
-function widget:TextCommand(command)
-	if string.sub(command, 1, 4) == "res " then
-		screenModeIndex = tonumber(string.sub(command, 5))
+	WG['screenMode'] = { }
+
+	WG['screenMode'].GetScreenModes = function()
+		return screenModes
+	end
+
+	WG['screenMode'].SetScreenMode = function(screenModeIndex)
 		changeScreenMode(screenModeIndex)
 	end
 end
