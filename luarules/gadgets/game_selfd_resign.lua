d903379c3af83881f6b109cd00584ad89a1735a3
Floris <floris@ruwhof.net>
2022-01-31 21:42:24 +0100

    selfd resign fix

--- luarules/gadgets/game_selfd_resign.lua
+++ luarules/gadgets/game_selfd_resign.lua
@@ -83,7 +83,7 @@ if gadgetHandler:IsSyncedCode() then
 							break
 						elseif selfdUnitCount >= triggerResignAmount then
 							local LuaAI = Spring.GetTeamLuaAI(teamID)
-							if not string.find(LuaAI, "Scavengers") then
+							if not LuaAI or not ( string.find(LuaAI, "Scavengers") or string.find(LuaAI, "Raptors") ) then
 								forceResignTeam(teamID)
 							end
 							break
