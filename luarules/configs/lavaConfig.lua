63022686e84ddd8d3dc06a807e39a1496c1fe355
icexuick <info@ixstudios.nl>
2022-03-04 16:06:38 +0100

    First Lava Config for new Hotstepper 5

--- luarules/configs/lavaConfig.lua
+++ luarules/configs/lavaConfig.lua
@@ -1,7 +1,7 @@
 local mapName = Game.mapName:lower()
 Spring.Echo("Lava Mapname", mapName)
 lavaMap = false
---[[ 
+--[[ EXAMPLE
     
 addTideRhym(HeightLevel, Speed, Delay for next TideRhym in seconds)
 
@@ -24,6 +24,22 @@ end
 
 ]]
 
+if string.find(mapName, "hotstepper") then
+    lavaMap = true
+    if (gadgetHandler:IsSyncedCode()) then
+        lavaLevel = 100 -- pre-game lava level
+        lavaGrow = 0.25
+        lavaDamage = 100 -- damage per second
 
+        addTideRhym (99, 0.25, 5*60) -- needs to be -1 than pre-game lava level
+        addTideRhym (236, 0.25, 5)
+        addTideRhym (100, 0.25, 5*60)
+        addTideRhym (236, 0.25, 5)
+        addTideRhym (100, 0.25, 5*60)
+        addTideRhym (300, 0.50, 1)
+        addTideRhym (355, 0.15, 60)
+        addTideRhym (395, 0.20, 10)
+    end
+end
 
 
