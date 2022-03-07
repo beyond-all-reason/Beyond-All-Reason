6bb7aaf024036b8af01e5ea7fc5ec7d99083f091
Beherith <mysterme@gmail.com>
2022-03-07 13:17:26 +0100

    refactor and document lava

--- luaui/Widgets/map_lava_GL4.lua
+++ luaui/Widgets/map_lava_GL4.lua
@@ -17,7 +17,7 @@ local lavaDistortion = texturesamplingmode .. "LuaUI/images/lavadistortion.png"
 
 local lavaShader 
 local lavaPlaneVAO
-local lavalevel = 50
+local lavalevel = 0
 
 local foglightShader
 local foglightVAO
@@ -26,29 +26,38 @@ local numfoglightVerts
 local foglightenabled = true
 local fogheightabovelava = 50
 
+local tideamplitude = 2
+local tideperiod = 200
+local lavatidelevel = 0
+
 local heatdistortx = 0
 local heatdistortz = 0
 
+local elmosPerSquare = 256 -- The resolution of the lava 
 
 local luaShaderDir = "LuaUI/Widgets/Include/"
 local LuaShader = VFS.Include(luaShaderDir.."LuaShader.lua")
 VFS.Include(luaShaderDir.."instancevbotable.lua") -- we are only gonna use the plane maker func of this
 
 
-local elmosPerSquare = 256 -- larger numbers are lower resolution 
 local unifiedShaderConfig = {
-	vertex_displacement = 0, 
-	HEIGHTOFFSET = 2.0, 
-	COASTWIDTH = 20.0,
-	WORLDUVSCALE = 4.0,
-	COASTCOLOR = "vec3(2.0, 0.5, 0.0)",
-	SPECULAREXPONENT = 64.0, 
-	SPECULARSTRENGTH = 1.0, 
-	LOSDARKNESS = 0.5,
-	FOGHEIGHTABOVELAVA = fogheightabovelava,
-	FOGCOLOR = "vec3(2.0, 0.5, 0.0)",
-	FOGFACTOR = 0.1,
-	EXTRALIGHTCOAST = 0.4,
+	-- for lavaplane
+	HEIGHTOFFSET = 2.0,  -- how many elmos above the 'actual' lava height we should render, to avoid ROAM clipping artifacts
+	COASTWIDTH = 20.0, -- how wide the coast of the lava should be
+	WORLDUVSCALE = 4.0, -- How many times to tile the lava texture across the entire map
+	COASTCOLOR = "vec3(2.0, 0.5, 0.0)", -- the color of the lava coast
+	SPECULAREXPONENT = 64.0,  -- the specular exponent of the lava plane
+	SPECULARSTRENGTH = 1.0, -- The peak brightness of specular highlights
+	LOSDARKNESS = 0.5, -- how much to darken the out-of-los areas of the lava plane
+	SHADOWSTRENGTH = 0.4, -- how much light a shadowed fragment can recieve
+	OUTOFMAPHEIGHT = -100, -- what value to use when we are sampling the heightmap outside of the true bounds
+	
+	-- for foglight:
+	FOGHEIGHTABOVELAVA = fogheightabovelava, -- how much higher above the lava the fog light plane is
+	FOGCOLOR = "vec3(2.0, 0.5, 0.0)", -- the color of the fog light
+	FOGFACTOR = 0.1, -- how dense the fog is
+	EXTRALIGHTCOAST = 0.4, -- how much extra brightness should coastal areas get
+	FOGLIGHTDISTORTION = 4.0, -- lower numbers are higher distortion amounts
 }
 
 
@@ -139,7 +148,10 @@ float heightAtWorldPos(vec2 w){
 	// Some texel magic to make the heightmap tex perfectly align:
 	const vec2 heightmaptexel = vec2(8.0, 8.0);
 	w +=  vec2(-8.0, -8.0) * (w * inverseMapSize) + vec2(4.0, 4.0) ;
-	vec2 uvhm = clamp(w, heightmaptexel, mapSize.xy - heightmaptexel) * inverseMapSize;
+	
+	vec2 uvhm = clamp(w, heightmaptexel, mapSize.xy - heightmaptexel);
+	uvhm = uvhm	* inverseMapSize;
+	
 	return texture(heightmapTex, uvhm, 0.0).x; 
 }
 
@@ -151,17 +163,18 @@ out vec4 fragColor;
 void main() {
 	
 	vec4 camPos = cameraViewInv[3];
-	 
-	float gametime = (timeInfo.x + timeInfo.w) * 0.006666 + randpervertex.x ;
 	
 	// Sample emissive as heat indicator here for later displacement
 	vec4 nodiffuseEmit =  texture(lavaDiffuseEmit, worldUV.xy * WORLDUVSCALE );
 	
 	vec2 rotatearoundvertices = worldUV.zw * 0.003;
 	
-	float localheight = heightAtWorldPos(worldPos.xz);
+	float localheight = OUTOFMAPHEIGHT ;
+	if (inboundsness > 0)
+		localheight = heightAtWorldPos(worldPos.xz);
 	
 	if (localheight > lavaHeight - HEIGHTOFFSET ) discard; 
+	
 	// Calculate how far the fragment is from the coast
 	float coastfactor = clamp((localheight-lavaHeight + COASTWIDTH + HEIGHTOFFSET) * 0.05,  0.0, 1.0);
 	
@@ -186,12 +199,12 @@ void main() {
 
 	// We shift the distortion texture camera-upwards according to the uniforms that got passed in
 	vec2 camshift =  vec2(heatdistortx, heatdistortz) * 0.001;
-	vec4 distortionTexture = texture(lavaDistortion, (worldUV.xy + camshift) *45.2 ) ;
+	vec4 distortionTexture = texture(lavaDistortion, (worldUV.xy + camshift) * 45.2) ;
 	
-	vec2 distortion = distortionTexture.xy * 0.2 *0.02;
-	distortion.xy *=  clamp(nodiffuseEmit.a * 0.5 + coastfactor, 0.2, 2.0);
+	vec2 distortion = distortionTexture.xy * 0.2 * 0.02;
+	distortion.xy *= clamp(nodiffuseEmit.a * 0.5 + coastfactor, 0.2, 2.0);
 
-	vec2 diffuseNormalUVs =  worldUV.xy * WORLDUVSCALE + distortion .xy + rotatearoundvertices;
+	vec2 diffuseNormalUVs =  worldUV.xy * WORLDUVSCALE + distortion.xy + rotatearoundvertices;
 	vec4 diffuseEmit =   texture(lavaDiffuseEmit , diffuseNormalUVs);
 	vec4 normalHeight =  texture(lavaNormalHeight, diffuseNormalUVs);
 	
@@ -311,89 +324,79 @@ in DataVS {
 
 const vec2 inverseMapSize = 1.0 / mapSize.xy;
 
-
-
 out vec4 fragColor;
 
 #line 22000
-
-
 void main() {
 	
 	vec4 camPos = cameraViewInv[3];
-	 
 
-	
-	vec2 rotatearoundvertices = worldUV.zw * 0.003;
-
-	// Calculate how far the fragment is from the coast
 	// We shift the distortion texture camera-upwards according to the uniforms that got passed in
 	vec2 camshift =  vec2(heatdistortx, heatdistortz) * 0.01;
 	
 	//Get the fragment depth 
 	// note that WE CANT GO LOWER THAN THE ACTUAL LAVA LEVEL!
 	
+	// convert the pixel-coordinates of the fragment to UV coords for sampling the depth buffers
 	vec2 screenUV = gl_FragCoord.xy/ viewGeometry.xy;
 	
+	// Sample the depth buffers, and choose whichever is closer to the screen
 	float mapdepth = texture(mapDepths, screenUV).x;
 	float modeldepth = texture(modelDepths, screenUV).x;
+	mapdepth = min(mapdepth, modeldepth); 
 	
-	// we need to get the world position of each depth fragment... 
 	// the W weight factor here is incorrect, as it comes from the depth buffers, and not the fragments own depth.
 	
-	mapdepth = min(mapdepth, modeldepth); // choose map or model
-	
+	// Convert to normalized device coordinates, and calculate inverse view projection
 	vec4 mapWorldPos =  vec4(  vec3(screenUV.xy * 2.0 - 1.0, mapdepth),  1.0);
 	mapWorldPos = cameraViewProjInv * mapWorldPos;
 	mapWorldPos.xyz = mapWorldPos.xyz/ mapWorldPos.w; // YAAAY this works!
-	float origmapy = mapWorldPos.y;
+	float trueFragmentHeight = mapWorldPos.y;
 	
 	// clip mapWorldPos according to true lava height
 	if (mapWorldPos.y< lavaHeight - FOGHEIGHTABOVELAVA) {
 		// we need to make a vector from cam to fogplane position
 		vec3 camtofogplane = mapWorldPos.xyz - camPos.xyz;
-		camtofogplane = 50* camtofogplane /abs(camtofogplane.y);
+		
+		// and scale it to make it 
+		camtofogplane = FOGHEIGHTABOVELAVA * camtofogplane /abs(camtofogplane.y);
 		mapWorldPos.xyz = worldPos.xyz + camtofogplane;
 	}
 	
-	vec4 distortionTexture = texture(lavaDistortion, (worldUV.xy * 22.0  + camshift)) ;
-	const float normalizingfactor = 4.0;
-	float fogdistort = (normalizingfactor + distortionTexture.x + distortionTexture.y)/ normalizingfactor ;
-	
+	// Calculate how long the vector from top of foglightplane to lava or world pos actually is
 	float actualfogdepth = length(mapWorldPos.xyz - worldPos.xyz) ;
-	
 	float fogAmount = 1.0 - exp2(- FOGFACTOR * FOGFACTOR * actualfogdepth  * 0.5);
 	
+	// sample the distortiontexture according to camera shift and scale it down
+	vec4 distortionTexture = texture(lavaDistortion, (worldUV.xy * 22.0  + camshift)) ;
+	float fogdistort = (FOGLIGHTDISTORTION + distortionTexture.x + distortionTexture.y)/ FOGLIGHTDISTORTION ;
+	
+	
+	// apply some distortion to the fog
 	fogAmount *= fogdistort;
 	
-	// ok good, lets add some extra brigtness near the coasts!
-	float disttocoast = abs(origmapy- (lavaHeight - FOGHEIGHTABOVELAVA - HEIGHTOFFSET));
+	// lets add some extra brigtness near the coasts, by finding the distance of the lavaplane to the coast
+	float disttocoast = abs(trueFragmentHeight- (lavaHeight - FOGHEIGHTABOVELAVA - HEIGHTOFFSET));
 	
 	float extralightcoast =  clamp(1.0 - disttocoast * 0.05, 0.0, 1.0);
 	extralightcoast = pow(extralightcoast, 3.0) * EXTRALIGHTCOAST;
 	
 	fogAmount += extralightcoast;
 	
-	
-	//fragColor.rgb = mapnormal.xyz; // ok good normals works
-	//fragColor.rgb = fract(vec3(mapdepth) * 0.01);
-	
 	//fragColor.rgb = fract(vec3(gl_FragCoord.z * 11.1 )); // good this works too
 	//fragColor.rgb = fract(mapWorldPos.xyz * 0.02);
 	fragColor.rgb = FOGCOLOR;
 	fragColor.a = fogAmount;
 	
+	// fade out the foglightplane if it is far out of bounds
 	fragColor.a *= clamp(  inboundsness * 2.0 +2.0, 0.0, 1.0);
-	//fragColor.a += sin(heatdistortx+ heatdistortz) * 0.01;
-	//if (camPos.y < lavaHeight - FOGHEIGHTABOVELAVA) fragColor.a = 1.0;
 }
 ]]
 
 
 
 function widget:GameFrame()
-	local gf = Spring.GetGameFrame()
-	lavalevel = math.sin(gf/1000) * 50 + 50
+	lavatidelevel = math.sin(Spring.GetGameFrame() / tideperiod) * tideamplitude + lavalevel
 end
 
 function widget:Initialize()
@@ -431,7 +434,7 @@ function widget:Initialize()
 			heatdistortx = 1,
 			heatdistortz = 1,
 		  },
-	}, "Lava Shader API")
+	}, "Lava Shader")
 	
 	
 	fogLightVSSrc = fogLightVSSrc:gsub("//__ENGINEUNIFORMBUFFERDEFS__", engineUniformBufferDefs)
@@ -440,12 +443,9 @@ function widget:Initialize()
 		vertex = fogLightVSSrc:gsub("//__DEFINES__", LuaShader.CreateShaderDefinesString(unifiedShaderConfig)),
 		fragment = foglightFSSrc:gsub("//__DEFINES__", LuaShader.CreateShaderDefinesString(unifiedShaderConfig)),
 		uniformInt = {
-			--heightmapTex = 0,
 			mapDepths = 0,
 			modelDepths = 1,
 			lavaDistortion = 2,
-			--mapNormals = 2, 
-			--modelNormals = 5,
 		},
 		uniformFloat = {
 			lavaHeight = 1,
@@ -467,7 +467,7 @@ function widget:Initialize()
 end
 
 function widget:DrawWorldPreUnit()
-	if lavalevel then 
+	if lavatidelevel then 
 		local _, gameSpeed, isPaused = Spring.GetGameSpeed()
 		if not isPaused then 
 			local camX, camY, camZ = Spring.GetCameraDirection()
@@ -479,7 +479,7 @@ function widget:DrawWorldPreUnit()
 		--Spring.Echo(camX, camZ, heatdistortx, heatdistortz,gameSpeed, isPaused)
 
 		lavaShader:Activate()
-		lavaShader:SetUniform("lavaHeight",lavalevel)
+		lavaShader:SetUniform("lavaHeight",lavatidelevel)
 		lavaShader:SetUniform("heatdistortx",heatdistortx)
 		lavaShader:SetUniform("heatdistortz",heatdistortz)
 
@@ -490,8 +490,8 @@ function widget:DrawWorldPreUnit()
 		gl.Texture(4, "$shadow")-- Texture file
 		gl.Texture(5, "$info")-- Texture file
 
-		gl.DepthTest(GL.LEQUAL)
-		gl.DepthMask(true)
+		gl.DepthTest(GL.LEQUAL) -- dont draw fragments below terrain
+		gl.DepthMask(true) -- dont write to the depth buffer, it doesnt help later on when we are doing the glowy bits
 		
 		lavaPlaneVAO:DrawElements(GL.TRIANGLES)
 		lavaShader:Deactivate()
@@ -505,30 +505,24 @@ function widget:DrawWorldPreUnit()
 		gl.Texture(3, false)-- Texture file
 		gl.Texture(4, false)-- Texture file
 		gl.Texture(5, false)-- Texture file
-		
-		
-
 	end
 end
+
 function widget:DrawWorld()
-	if lavalevel and foglightenabled then 
+	if lavatidelevel and foglightenabled then 
 			--Now to draw the fog light a good 32 elmos above it :)
 		foglightShader:Activate()
-		foglightShader:SetUniform("lavaHeight",lavalevel + fogheightabovelava)
+		foglightShader:SetUniform("lavaHeight",lavatidelevel + fogheightabovelava)
 		foglightShader:SetUniform("heatdistortx",heatdistortx)
 		foglightShader:SetUniform("heatdistortz",heatdistortz)
 
-		--gl.Texture(0, "$heightmap")-- Texture file
 		gl.Texture(0, "$map_gbuffer_zvaltex")-- Texture file
 		gl.Texture(1, "$model_gbuffer_zvaltex")-- Texture file
 		gl.Texture(2, lavaDistortion)-- Texture file
-		--gl.Texture(2, "$map_gbuffer_normtex")-- Texture file
-		--gl.Texture(5, "$model_gbuffer_normtex")-- Texture file
-
 		
-		gl.Blending(GL.SRC_ALPHA, GL.ONE)
-		gl.DepthTest(GL.LEQUAL)
-		gl.DepthMask(false)
+		gl.Blending(GL.SRC_ALPHA, GL.ONE) -- this will additively blend the foglight above everything
+		gl.DepthTest(GL.LEQUAL) -- dont draw fragments below the foglightlevel 
+		gl.DepthMask(false) -- dont write to the depth buffer
 		
 		lavaPlaneVAO:DrawElements(GL.TRIANGLES)
 		foglightShader:Deactivate()
@@ -542,4 +536,8 @@ function widget:DrawWorld()
 		
 		gl.Blending(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA)
 	end
+end
+
+function widget:Shutdown()
+	Spring.SetDrawWater(true)
 end
