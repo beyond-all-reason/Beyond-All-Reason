e48b9398fb2b5ecd43a39fab7dcd2f9e0bed7950
Amadeus Folego <amadeusfolego@gmail.com>
2022-03-25 16:09:04 -0300

    ClearGroupSelection: Fix return true statement

--- luaui/Widgets/cmd_unitgroup_clear_selection_on_empty.lua
+++ luaui/Widgets/cmd_unitgroup_clear_selection_on_empty.lua
@@ -25,8 +25,6 @@ local function OnGroupSelected(cmd)
 	if spGetGroupUnitsCount(string.sub(cmd, 6, 6)) == 0 then
 		spSelectUnitMap({}, false)
 	end
-
-	return true
 end
 
 local function ManageAction(doAdd)
