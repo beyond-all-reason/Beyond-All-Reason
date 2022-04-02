b5b1ace5b3dab62fd74ea7efebc95a4a6e62a7df
Floris <floris@ruwhof.net>
2022-04-02 20:32:56 +0200

    settings: make soundtrack start at loadscreen toggle actually work

--- luaintro/Addons/music.lua
+++ luaintro/Addons/music.lua
@@ -26,59 +26,61 @@ function addon.Shutdown()
 end
 
 function addon.Initialize()
-	local originalSoundtrackEnabled = Spring.GetConfigInt('UseSoundtrackNew', 1)
-	local legacySoundtrackEnabled 	= Spring.GetConfigInt('UseSoundtrackOld', 0)
-	local customSoundtrackEnabled	= Spring.GetConfigInt('UseSoundtrackCustom', 1)
-	
-	
-	local musicPlaylist = {}
-	if originalSoundtrackEnabled == 1 then
-		local musicDirOriginal 		= 'music/original'
-		table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirOriginal..'/loading', '*.ogg'))
-	end
+	if Spring.GetConfigInt('music_loadscreen', 1) == 1 then
+		local originalSoundtrackEnabled = Spring.GetConfigInt('UseSoundtrackNew', 1)
+		local legacySoundtrackEnabled 	= Spring.GetConfigInt('UseSoundtrackOld', 0)
+		local customSoundtrackEnabled	= Spring.GetConfigInt('UseSoundtrackCustom', 1)
 
-	-- Legacy Soundtrack List
-	if legacySoundtrackEnabled == 1 then
-		local musicDirLegacy 		= 'music/legacy'
-		table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirLegacy..'/loading', '*.ogg'))
-	end
 
-	-- Custom Soundtrack List
-	if customSoundtrackEnabled == 1 then
-		local musicDirCustom 		= 'music/custom'
-		table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom..'/loading', '*.ogg'))
-	end
-
-	if #musicPlaylist == 0 then
+		local musicPlaylist = {}
 		if originalSoundtrackEnabled == 1 then
 			local musicDirOriginal 		= 'music/original'
-			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirOriginal..'/warhigh', '*.ogg'))
-			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirOriginal..'/warlow', '*.ogg'))
+			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirOriginal..'/loading', '*.ogg'))
 		end
-	
+
 		-- Legacy Soundtrack List
 		if legacySoundtrackEnabled == 1 then
 			local musicDirLegacy 		= 'music/legacy'
-			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirLegacy..'/war', '*.ogg'))
+			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirLegacy..'/loading', '*.ogg'))
 		end
-	
+
 		-- Custom Soundtrack List
 		if customSoundtrackEnabled == 1 then
 			local musicDirCustom 		= 'music/custom'
-			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom, '*.ogg'))
-			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom..'/warhigh', '*.ogg'))
-			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom..'/warlow', '*.ogg'))
-			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom..'/war', '*.ogg'))
+			table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom..'/loading', '*.ogg'))
 		end
-	end
 
-	local musicvolume = Spring.GetConfigInt("snd_volmusic", 50) * 0.02
-	if #musicPlaylist > 1 then
-		local pickedTrack = math.ceil(#musicPlaylist*math.random())
-		Spring.PlaySoundStream(musicPlaylist[pickedTrack], 1)
-		Spring.SetSoundStreamVolume(musicvolume)
-	elseif #musicPlaylist == 1 then
-		Spring.PlaySoundStream(musicPlaylist[1], 1)
-		Spring.SetSoundStreamVolume(musicvolume)
+		if #musicPlaylist == 0 then
+			if originalSoundtrackEnabled == 1 then
+				local musicDirOriginal 		= 'music/original'
+				table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirOriginal..'/warhigh', '*.ogg'))
+				table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirOriginal..'/warlow', '*.ogg'))
+			end
+
+			-- Legacy Soundtrack List
+			if legacySoundtrackEnabled == 1 then
+				local musicDirLegacy 		= 'music/legacy'
+				table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirLegacy..'/war', '*.ogg'))
+			end
+
+			-- Custom Soundtrack List
+			if customSoundtrackEnabled == 1 then
+				local musicDirCustom 		= 'music/custom'
+				table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom, '*.ogg'))
+				table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom..'/warhigh', '*.ogg'))
+				table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom..'/warlow', '*.ogg'))
+				table.mergeInPlace(musicPlaylist, VFS.DirList(musicDirCustom..'/war', '*.ogg'))
+			end
+		end
+
+		local musicvolume = Spring.GetConfigInt("snd_volmusic", 50) * 0.02
+		if #musicPlaylist > 1 then
+			local pickedTrack = math.ceil(#musicPlaylist*math.random())
+			Spring.PlaySoundStream(musicPlaylist[pickedTrack], 1)
+			Spring.SetSoundStreamVolume(musicvolume)
+		elseif #musicPlaylist == 1 then
+			Spring.PlaySoundStream(musicPlaylist[1], 1)
+			Spring.SetSoundStreamVolume(musicvolume)
+		end
 	end
 end
