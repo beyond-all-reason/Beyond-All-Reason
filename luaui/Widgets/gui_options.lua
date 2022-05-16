0b7799ea615a058a3edebec21851d075c8064638
Floris <floris@ruwhof.net>
2022-05-16 22:08:19 +0200

    Revert "options: applying iconshidewithui"

--- luaui/Widgets/gui_options.lua
+++ luaui/Widgets/gui_options.lua
@@ -4960,8 +4960,6 @@ function widget:Initialize()
 		end
 	end
 
-	Spring.SendCommands("iconshidewithui " .. Spring.GetConfigInt("UnitIconsHideWithUI", 0))
-
 	-- make sure fog-start is smaller than fog-end in case maps have configured it this way
 	if gl.GetAtmosphere("fogEnd") <= gl.GetAtmosphere("fogStart") then
 		Spring.SetAtmosphere({ fogEnd = gl.GetAtmosphere("fogStart") + 0.01 })
