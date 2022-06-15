69693940278306271abe6426520e4fb195ce5af8
Beherith <mysterme@gmail.com>
2022-06-15 07:03:31 +0200

    Revert "BARHotkey: Refactor to use scancodes (#1021)"
    This reverts commit e8633efc072782b812d1051bfd88c79f1e8295af.

--- luaui/Widgets/cmd_bar_hotkeys.lua
+++ luaui/Widgets/cmd_bar_hotkeys.lua
@@ -92,16 +92,16 @@ local engineBinds = {
 	{ "Ctrl+9", "specteam", "18" },
 	{ "Ctrl+0", "specteam", "19" },
 
-	{ "Any+0", "group0" },
-	{ "Any+1", "group1" },
-	{ "Any+2", "group2" },
-	{ "Any+3", "group3" },
-	{ "Any+4", "group4" },
-	{ "Any+5", "group5" },
-	{ "Any+6", "group6" },
-	{ "Any+7", "group7" },
-	{ "Any+8", "group8" },
-	{ "Any+9", "group9" },
+        { "Any+0", "group0" },
+		{ "Any+1", "group1" },
+		{ "Any+2", "group2" },
+		{ "Any+3", "group3" },
+		{ "Any+4", "group4" },
+		{ "Any+5", "group5" },
+		{ "Any+6", "group6" },
+		{ "Any+7", "group7" },
+		{ "Any+8", "group8" },
+		{ "Any+9", "group9" },
 
 	{       "[", "buildfacing",  "inc" },
 	{ "Shift+[", "buildfacing",  "inc" },
@@ -177,27 +177,40 @@ local engineBinds = {
 	{ "Any+f12", "screenshot"     },
 	{ "Alt+enter", "fullscreen"  },
 
-	{ "Any+`" , "drawlabel" },
+	{  "Any+`", "drawlabel" },
 	{ "Any+\\", "drawlabel" },
-	{ "Any+~" , "drawlabel" },
-	{ "Any+§" , "drawlabel" },
-	{ "Any+^" , "drawlabel" },
+	{  "Any+~", "drawlabel" },
+	{  "Any+§", "drawlabel" },
+	{  "Any+^", "drawlabel" },
 
-	{ "Any+`",    "drawinmap"  },
-	{ "Any+\\",   "drawinmap"  },
-	{ "Any+~",    "drawinmap"  },
-	{ "Any+§",    "drawinmap"  },
-	{ "Any+^",    "drawinmap"  },
+	{    "Any+`",    "drawinmap"  },
+	{ "Up+Any+`",    "drawinmap"  },
+	{    "Any+\\",   "drawinmap"  },
+	{ "Up+Any+\\",   "drawinmap"  },
+	{    "Any+~",    "drawinmap"  },
+	{ "Up+Any+~",    "drawinmap"  },
+	{    "Any+§",    "drawinmap"  },
+	{ "Up+Any+§",    "drawinmap"  },
+	{    "Any+^",    "drawinmap"  },
+	{ "Up+Any+^",    "drawinmap"  },
 
-	{ "Any+up",       "moveforward" },
-	{ "Any+down",     "moveback"    },
-	{ "Any+right",    "moveright"   },
-	{ "Any+left",     "moveleft"    },
-	{ "Any+pageup",   "moveup"      },
-	{ "Any+pagedown", "movedown"    },
+	{    "Any+up",       "moveforward" },
+	{ "Up+Any+up",       "moveforward" },
+	{    "Any+down",     "moveback"    },
+	{ "Up+Any+down",     "moveback"    },
+	{    "Any+right",    "moveright"   },
+	{ "Up+Any+right",    "moveright"   },
+	{    "Any+left",     "moveleft"    },
+	{ "Up+Any+left",     "moveleft"    },
+	{    "Any+pageup",   "moveup"      },
+	{ "Up+Any+pageup",   "moveup"      },
+	{    "Any+pagedown", "movedown"    },
+	{ "Up+Any+pagedown", "movedown"    },
 
 	{     "Any+ctrl", "moveslow" },
+	{  "Up+Any+ctrl", "moveslow" },
 	{    "Any+shift", "movefast" },
+	{ "Up+Any+shift", "movefast" },
 
 	{ "Ctrl+a", "select", "AllMap++_ClearSelection_SelectAll+"                                        },
 	{ "Ctrl+b", "select", "AllMap+_Builder_Idle+_ClearSelection_SelectOne+"                           },
@@ -289,15 +302,31 @@ local function makeBindsTable(keyLayout)
 		{ "Alt+numpad+", "increasespeed" },
 		{ "Alt+numpad-", "decreasespeed" },
 
-		{       "sc_[", "buildfacing" , "inc" },
-		{ "Shift+sc_[", "buildfacing" , "inc" },
-		{       "sc_]", "buildfacing" , "dec" },
-		{ "Shift+sc_]", "buildfacing" , "dec" },
+		{      "1", "specteam", "0"  },
+		{      "2", "specteam", "1"  },
+		{      "3", "specteam", "2"  },
+		{      "4", "specteam", "3"  },
+		{      "5", "specteam", "4"  },
+		{      "6", "specteam", "5"  },
+		{      "7", "specteam", "6"  },
+		{      "8", "specteam", "7"  },
+		{      "9", "specteam", "8"  },
 
-		{       "Alt+sc_z", "buildspacing", "inc" },
-		{ "Shift+Alt+sc_z", "buildspacing", "inc" },
-		{       "Alt+sc_x", "buildspacing", "dec" },
-		{ "Shift+Alt+sc_x", "buildspacing", "dec" },
+		{             "[", "buildfacing" , "inc" },
+		{       "Shift+[", "buildfacing" , "inc" },
+		{             "]", "buildfacing" , "dec" },
+		{       "Shift+]", "buildfacing" , "dec" },
+		{             ".", "buildfacing" , "dec" },
+		{       "Shift+.", "buildfacing" , "dec" },
+		{             ",", "buildfacing" , "inc" }, --because some keyboards don't have [ and ] keys
+		{       "Shift+,", "buildfacing" , "inc" },
+		{               O, "buildfacing" , "inc" }, --apparently some keyboards don't have , and . either...
+		{     "Shift+"..O, "buildfacing" , "inc" },
+
+		{       "Alt+"..Z, "buildspacing", "inc" },
+		{ "Shift+Alt+"..Z, "buildspacing", "inc" },
+		{       "Alt+"..X, "buildspacing", "dec" },
+		{ "Shift+Alt+"..X, "buildspacing", "dec" },
 
 		{                A, "attack"          },
 		{      "Shift+"..A, "attack"          },
@@ -356,32 +385,58 @@ local function makeBindsTable(keyLayout)
 		{ "Ctrl+f4", "viewrot"  },
 		{ "Ctrl+f5", "viewfree" },
 
-		{ "Any+f1" , "ShowElevation"          },
-		{ "Any+f2" , "ShowPathTraversability" },
-		{ "Any+f3" , "LastMsgPos"             },
-		{ "Any+f4" , "ShowMetalMap"           },
-		{ "Any+f5" , "HideInterface"          },
-		{ "Any+f6" , "MuteSound"              },
-		{ "Any+f7" , "DynamicSky"             },
-		{    "f11" , "luaui selector"         },
-		{ "Any+f12", "screenshot"     , "png" },
+		{ "Any+f1", "ShowElevation"          },
+		{ "Any+f2", "ShowPathTraversability" },
+		{ "Any+f3", "LastMsgPos"             },
+		{ "Any+f4", "ShowMetalMap"           },
+		{ "Any+f5", "HideInterface"          },
+		{ "Any+f6", "MuteSound"              },
+		{ "Any+f7", "DynamicSky"             },
+		{    "f11", "luaui selector"         },
 
-		{ "Ctrl+Shift+f8", "savegame"       },
-		{ "Alt+enter",     "fullscreen"     },
+		{ "Ctrl+Shift+f8", "savegame"  },
+		{       "Any+f12", "screenshot png" },
+		{     "Alt+enter", "fullscreen" },
 
-		{ "Any+sc_`" , "drawinmap" },
-		{ "Any+sc_`" , "drawlabel" },
-		{ "sc_`,sc_`", "drawlabel" },
+		{     "Any+`", "drawinmap"  },
+		{  "Up+Any+`", "drawinmap"  },
+		{    "Any+\\", "drawinmap"  },
+		{ "Up+Any+\\", "drawinmap"  },
+		{     "Any+~", "drawinmap"  },
+		{  "Up+Any+~", "drawinmap"  },
+		{     "Any+§", "drawinmap"  },
+		{  "Up+Any+§", "drawinmap"  },
+		{     "Any+^", "drawinmap"  },
+		{  "Up+Any+^", "drawinmap"  },
 
-		{ "Any+up",       "moveforward"  },
-		{ "Any+down",     "moveback"     },
-		{ "Any+right",    "moveright"    },
-		{ "Any+left",     "moveleft"     },
-		{ "Any+pageup",   "moveup"       },
-		{ "Any+pagedown", "movedown"     },
+		{  "Any+`", "drawlabel" },
+		{    "`,`", "drawlabel" },
+		{ "Any+\\", "drawlabel" },
+		{  "\\,\\", "drawlabel" },
+		{  "Any+~", "drawlabel" },
+		{    "~,~", "drawlabel" },
+		{  "Any+§", "drawlabel" },
+		{    "§,§", "drawlabel" },
+		{  "Any+^", "drawlabel" },
+		{    "^,^", "drawlabel" },
 
-		{ "Any+ctrl",     "moveslow"     },
-		{ "Any+shift",    "movefast"     },
+		{    "Any+up",       "moveforward"  },
+		{ "Up+Any+up",       "moveforward"  },
+		{    "Any+down",     "moveback"     },
+		{ "Up+Any+down",     "moveback"     },
+		{    "Any+right",    "moveright"    },
+		{ "Up+Any+right",    "moveright"    },
+		{    "Any+left",     "moveleft"     },
+		{ "Up+Any+left",     "moveleft"     },
+		{    "Any+pageup",   "moveup"       },
+		{ "Up+Any+pageup",   "moveup"       },
+		{    "Any+pagedown", "movedown"     },
+		{ "Up+Any+pagedown", "movedown"     },
+
+		{    "Any+ctrl",     "moveslow"     },
+		{ "Up+Any+ctrl",     "moveslow"     },
+		{    "Any+shift",    "movefast"     },
+		{ "Up+Any+shift",    "movefast"     },
 
 		{ "Ctrl+"..A,    "select", "AllMap++_ClearSelection_SelectAll+"                                                                                       },
 		{ "Ctrl+"..B,    "select", "AllMap+_Builder_Idle+_ClearSelection_SelectOne+"                                                                          },
@@ -532,7 +587,8 @@ local function makeBindsTable(keyLayout)
 		{ "numpad-", "snd_volume_decrease" },
 
 		-- los_colors
-		{ "Any+sc_;", "losradar" },
+		{    'Any+;', 'losradar' },
+		{ 'Any+0xf6', 'losradar' }, -- o-umlaut for German keyboards, located where the semicolon normally is
 
 		--unit_stats
 		{ "Any+space", 'unit_stats' },
@@ -546,23 +602,21 @@ local function makeBindsTable(keyLayout)
 	table.insert(_binds,  {         Y, "settargetnoground" })
 
 	-- if WG['Auto Group'] then
-	table.insert(_binds,  { "Alt+sc_`",  "remove_from_autogroup" })
-	table.insert(_binds,  { "Ctrl+sc_`", "remove_one_unit_from_group" })
+	table.insert(_binds,  { "Alt+`",  "remove_from_autogroup" })
+	table.insert(_binds,  { "Ctrl+`", "remove_one_unit_from_group" })
+	table.insert(_binds,  { "Alt+^",  "remove_from_autogroup" })        -- different keyboard layouts
+	table.insert(_binds,  { "Ctrl+^", "remove_one_unit_from_group" })
 
 	for i = 0, 9 do
-		if i ~= 0 then
-			table.insert(_binds, { i , "specteam", i-1 })
-		end
+        table.insert(_binds, { 'Alt+'..i , "add_to_autogroup", i })
 
-		table.insert(_binds, { 'Alt+'..i , "add_to_autogroup", i })
+        table.insert(_binds, { i                , "group", i                })
+        table.insert(_binds, { 'Ctrl+'..i       , "group", "set "..i        })
+        table.insert(_binds, { 'Shift+'..i      , "group", "selectadd "..i })
+        table.insert(_binds, { 'Ctrl+Shift+'..i , "group", "add "..i        })
+        table.insert(_binds, { 'Ctrl+Alt+'..i        , "group", "selecttoggle "..i     })
 
-		table.insert(_binds, { i               , "group", i                  })
-		table.insert(_binds, { 'Ctrl+'..i      , "group", "set "..i          })
-		table.insert(_binds, { 'Shift+'..i     , "group", "selectadd "..i    })
-		table.insert(_binds, { 'Ctrl+Shift+'..i, "group", "add "..i          })
-		table.insert(_binds, { 'Ctrl+Alt+'..i  , "group", "selecttoggle "..i })
-
-	end
+        end
 
 
 	return _binds
