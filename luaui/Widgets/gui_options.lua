31c77215b26a8c9754d7c8f03ee1ae1dd15496d4
Floris <floris@ruwhof.net>
2022-07-15 14:29:40 +0200

    options: mute when offscreen fix

--- luaui/Widgets/gui_options.lua
+++ luaui/Widgets/gui_options.lua
@@ -665,8 +665,9 @@ function widget:Update(dt)
 		local prevIsOffscreen = isOffscreen
 		isOffscreen = select(6, Spring.GetMouseState())
 		if isOffscreen ~= prevIsOffscreen then
+			local prevIsOffscreenTime = isOffscreenTime
 			isOffscreenTime = os.clock()
-			if isOffscreen then
+			if isOffscreen and not prevIsOffscreenTime then
 				prevOffscreenVolume = tonumber(Spring.GetConfigInt("snd_volmaster", 40) or 40)
 			end
 		end
