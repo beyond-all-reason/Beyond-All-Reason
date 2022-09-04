d3095c16b15ffe7c546af2da02f811f19680c32d
Amadeus Folego <amadeusfolego@gmail.com>
2022-09-04 18:35:54 -0300

    Hotkeys: Unbindall on load, keyreload on shutdown

--- luaui/Widgets/cmd_bar_hotkeys.lua
+++ luaui/Widgets/cmd_bar_hotkeys.lua
@@ -14,201 +14,6 @@ end
 local currentLayout
 local keyConfig = VFS.Include("luaui/configs/keyboard_layouts.lua")
 
--- table of stuff that we unbind on load
--- copypaste the contents from rts/Game/UI/KeyBindings.cpp on engine to update
-local engineBinds = {
-	{            "esc", "quitmessage" },
-	{      "Shift+esc", "quitmenu"    },
-	{ "Ctrl+Shift+esc", "quitforce"   },
-	{  "Alt+Shift+esc", "reloadforce" },
-	{      "Any+pause", "pause"       },
-
-	{     "c", "controlunit" },
-	{ "Any+h", "sharedialog" },
-	{ "Any+i", "gameinfo"    },
-
-	{           "Any+j", "mouse2"         },
-	{       "backspace", "mousestate"     },
-	{ "Shift+backspace", "togglecammode"  },
-	{  "Ctrl+backspace", "togglecammode"  },
-	{         "Any+tab", "toggleoverview" },
-
-	{  "Any+enter", "chat"           },
-	{ "Alt+ctrl+a", "chatswitchally" },
-	{ "Alt+ctrl+s", "chatswitchspec" },
-
-	{       "Any+tab", "edit_complete"  },
-	{ "Any+backspace", "edit_backspace" },
-	{    "Any+delete", "edit_delete"    },
-	{      "Any+home", "edit_home"      },
-	{      "Alt+left", "edit_home"      },
-	{       "Any+end", "edit_end"       },
-	{     "Alt+right", "edit_end"       },
-	{        "Any+up", "edit_prev_line" },
-	{      "Any+down", "edit_next_line" },
-	{      "Any+left", "edit_prev_char" },
-	{     "Any+right", "edit_next_char" },
-	{     "Ctrl+left", "edit_prev_word" },
-	{    "Ctrl+right", "edit_next_word" },
-	{     "Any+enter", "edit_return"    },
-	{    "Any+escape", "edit_escape"    },
-
-	{ "Ctrl+v", "pastetext" },
-
-	{ "Any+home", "increaseViewRadius" },
-	{ "Any+end",  "decreaseViewRadius" },
-
-	{ "Alt+insert",  "speedup"  },
-	{ "Alt+delete",  "slowdown" },
-	{ "Alt+=",       "speedup"  },
-	{ "Alt++",       "speedup"  },
-	{ "Alt+-",       "slowdown" },
-	{ "Alt+numpad+", "speedup"  },
-	{ "Alt+numpad-", "slowdown" },
-
-	{       ",", "prevmenu" },
-	{       ".", "nextmenu" },
-	{ "Shift+,", "decguiopacity" },
-	{ "Shift+.", "incguiopacity" },
-
-	{      "1", "specteam", "0"  },
-	{      "2", "specteam", "1"  },
-	{      "3", "specteam", "2"  },
-	{      "4", "specteam", "3"  },
-	{      "5", "specteam", "4"  },
-	{      "6", "specteam", "5"  },
-	{      "7", "specteam", "6"  },
-	{      "8", "specteam", "7"  },
-	{      "9", "specteam", "8"  },
-	{      "0", "specteam", "9"  },
-	{ "Ctrl+1", "specteam", "10" },
-	{ "Ctrl+2", "specteam", "11" },
-	{ "Ctrl+3", "specteam", "12" },
-	{ "Ctrl+4", "specteam", "13" },
-	{ "Ctrl+5", "specteam", "14" },
-	{ "Ctrl+6", "specteam", "15" },
-	{ "Ctrl+7", "specteam", "16" },
-	{ "Ctrl+8", "specteam", "17" },
-	{ "Ctrl+9", "specteam", "18" },
-	{ "Ctrl+0", "specteam", "19" },
-
-	{ "Any+0", "group0" },
-	{ "Any+1", "group1" },
-	{ "Any+2", "group2" },
-	{ "Any+3", "group3" },
-	{ "Any+4", "group4" },
-	{ "Any+5", "group5" },
-	{ "Any+6", "group6" },
-	{ "Any+7", "group7" },
-	{ "Any+8", "group8" },
-	{ "Any+9", "group9" },
-
-	{       "[", "buildfacing",  "inc" },
-	{ "Shift+[", "buildfacing",  "inc" },
-	{       "]", "buildfacing",  "dec" },
-	{ "Shift+]", "buildfacing",  "dec" },
-	{   "Any+z", "buildspacing", "inc" },
-	{   "Any+x", "buildspacing", "dec" },
-
-	{            "a", "attack"          },
-	{      "Shift+a", "attack"          },
-	{        "Alt+a", "areaattack"      },
-	{  "Alt+Shift+a", "areaattack"      },
-	{        "Alt+b", "debug"           },
-	{        "Alt+v", "debugcolvol"     },
-	{        "Alt+p", "debugpath"       },
-	{            "d", "manualfire"      },
-	{      "Shift+d", "manualfire"      },
-	{       "Ctrl+d", "selfd"           },
-	{ "Ctrl+Shift+d", "selfd", "queued" },
-	{            "e", "reclaim"         },
-	{      "Shift+e", "reclaim"         },
-	{            "f", "fight"           },
-	{      "Shift+f", "fight"           },
-	{        "Alt+f", "forcestart"      },
-	{            "g", "guard"           },
-	{      "Shift+g", "guard"           },
-	{            "k", "cloak"           },
-	{      "Shift+k", "cloak"           },
-	{            "l", "loadunits"       },
-	{      "Shift+l", "loadunits"       },
-	{            "m", "move"            },
-	{      "Shift+m", "move"            },
-	{        "Alt+o", "singlestep"      },
-	{            "p", "patrol"          },
-	{      "Shift+p", "patrol"          },
-	{            "q", "groupselect"     },
-	{            "q", "groupadd"        },
-	{       "Ctrl+q", "aiselect"        },
-	{      "Shift+q", "groupclear"      },
-	{            "r", "repair"          },
-	{      "Shift+r", "repair"          },
-	{            "s", "stop"            },
-	{      "Shift+s", "stop"            },
-	{            "u", "unloadunits"     },
-	{      "Shift+u", "unloadunits"     },
-	{            "w", "wait"            },
-	{      "Shift+w", "wait", "queued"  },
-	{            "x", "onoff"           },
-	{      "Shift+x", "onoff"           },
-
-
-	{  "Ctrl+t", "trackmode" },
-	{   "Any+t", "track" },
-
-	{ "Ctrl+f1", "viewfps"  },
-	{ "Ctrl+f2", "viewta"   },
-	{ "Ctrl+f3", "viewspring" },
-	{ "Ctrl+f4", "viewrot"  },
-	{ "Ctrl+f5", "viewfree" },
-
-	{ "Any+f1", "ShowElevation"          },
-	{ "Any+f2", "ShowPathTraversability" },
-	{ "Any+f3", "LastMsgPos"             },
-	{ "Any+f4", "ShowMetalMap"           },
-	{ "Any+f5", "HideInterface"          },
-	{ "Any+f6", "MuteSound"              },
-	{ "Any+f7", "DynamicSky"             },
-	{  "Any+l",  "togglelos"             },
-
-	{ "Ctrl+Shift+f8",  "savegame" },
-	{ "Ctrl+Shift+f10", "createvideo" },
-	{ "Any+f11", "screenshot"     },
-	{ "Any+f12", "screenshot"     },
-	{ "Alt+enter", "fullscreen"  },
-
-	{ "Any+`" , "drawlabel" },
-	{ "Any+\\", "drawlabel" },
-	{ "Any+~" , "drawlabel" },
-	{ "Any+§" , "drawlabel" },
-	{ "Any+^" , "drawlabel" },
-
-	{ "Any+`",    "drawinmap"  },
-	{ "Any+\\",   "drawinmap"  },
-	{ "Any+~",    "drawinmap"  },
-	{ "Any+§",    "drawinmap"  },
-	{ "Any+^",    "drawinmap"  },
-
-	{ "Any+up",       "moveforward" },
-	{ "Any+down",     "moveback"    },
-	{ "Any+right",    "moveright"   },
-	{ "Any+left",     "moveleft"    },
-	{ "Any+pageup",   "moveup"      },
-	{ "Any+pagedown", "movedown"    },
-
-	{     "Any+ctrl", "moveslow" },
-	{    "Any+shift", "movefast" },
-
-	{ "Ctrl+a", "select", "AllMap++_ClearSelection_SelectAll+"                                        },
-	{ "Ctrl+b", "select", "AllMap+_Builder_Idle+_ClearSelection_SelectOne+"                           },
-	{ "Ctrl+c", "select", "AllMap+_ManualFireUnit+_ClearSelection_SelectOne+"                         },
-	{ "Ctrl+r", "select", "AllMap+_Radar+_ClearSelection_SelectAll+"                                  },
-	{ "Ctrl+v", "select", "AllMap+_Not_Builder_Not_Commander_InPrevSel_Not_InHotkeyGroup+_SelectAll+" },
-	{ "Ctrl+w", "select", "AllMap+_Not_Aircraft_Weapons+_ClearSelection_SelectAll+"                   },
-	{ "Ctrl+x", "select", "AllMap+_InPrevSel_Not_InHotkeyGroup+_SelectAll+"                           },
-	{ "Ctrl+z", "select", "AllMap+_InPrevSel+_ClearSelection_SelectAll+"                              }
-}
-
 local function makeBindsTable(keyLayout)
 	local Z = keyLayout[1][1]
 	local X = keyLayout[1][2]
@@ -569,29 +374,30 @@ local function makeBindsTable(keyLayout)
 	return _binds
 end
 
-local function loadEngineBindings(prefix)
-	prefix = prefix or ''
-
+local function loadEngineBindings()
 	for _, v in ipairs(engineBinds) do
-		local command = prefix..'bind '..v[1]..' '..v[2]
-		if prefix == '' and v[3] then
+		local command = 'bind '..v[1]..' '..v[2]
+		if v[3] then
 			command = command..' '..v[3]
 		end
 		Spring.SendCommands(command)
 	end
 end
 
-local function loadBindings(prefix)
-	prefix = prefix or ''
-	local keyLayout = keyConfig.keyLayouts[currentLayout]
-
-	if prefix == '' and WG['buildmenu'] and WG['buildmenu'].reloadBindings then
+local function reloadBuildMenuBindings()
+	if WG['buildmenu'] and WG['buildmenu'].reloadBindings then
 		WG['buildmenu'].reloadBindings()
 	end
+end
+
+local function loadBindings()
+	local keyLayout = keyConfig.keyLayouts[currentLayout]
+
+	reloadBuildMenuBindings()
 
 	for _, v in ipairs(makeBindsTable(keyLayout)) do
-		local command = prefix..'bind '..v[1]..' '..v[2]
-		if prefix == '' and v[3] then
+		local command = 'bind '..v[1]..' '..v[2]
+		if v[3] then
 			command = command..' '..v[3]
 		end
 		Spring.SendCommands(command)
@@ -599,24 +405,20 @@ local function loadBindings(prefix)
 end
 
 local function reloadBindings()
-	local newLayout = Spring.GetConfigString("KeyboardLayout", 'qwerty')
+	Spring.SendCommands("unbindall")
 
-	if currentLayout then
-		loadBindings('un')
-	end
+	currentLayout = Spring.GetConfigString("KeyboardLayout", 'qwerty')
 
-	currentLayout = newLayout
 	loadBindings()
 end
 
 function widget:Initialize()
-	loadEngineBindings('un')
 	reloadBindings()
 
 	WG.reloadBindings = reloadBindings
 end
 
 function widget:Shutdown()
-	loadBindings('un')
-	loadEngineBindings()
+	Spring.SendCommands("keyreload")
+	reloadBuildMenuBindings()
 end
