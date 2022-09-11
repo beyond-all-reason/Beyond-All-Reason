9b69b748c9a4dd075e00e1564ecf5b82d67f2253
Amadeus Folego <amadeusfolego@gmail.com>
2022-09-11 11:18:35 -0300

    LuaMocks: Add more items

---
+++ luamocks/Widgets.lua
@@ -0,0 +1,8 @@
+local numberMock =42
+local stringMock ="TestString"
+local tableMock ={exampletable= true}
+local booleanMock =true
+local functionMock = function (bar) return bar; end
+
+widget = {}
+WG = {}
