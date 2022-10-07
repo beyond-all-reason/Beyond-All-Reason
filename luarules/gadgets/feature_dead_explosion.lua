5b01206f8ae673c0c347f00ba62831906dec70e7
Floris <floris@ruwhof.net>
2022-10-08 00:10:06 +0200

    Feature Death Explosion: tweak

--- luarules/gadgets/feature_dead_explosion.lua
+++ luarules/gadgets/feature_dead_explosion.lua
@@ -22,7 +22,7 @@ if gadgetHandler:IsSyncedCode() then
 	local isCorpse = {}
 	for featureDefID, featureDef in pairs(FeatureDefs) do
 		if featureDef.customParams.category and featureDef.customParams.category == 'corpses' then
-			isCorpse[featureDefID] = true
+			isCorpse[featureDefID] = math.min(featureDef.footprintX or 2, 5)
 		end
 	end
 
@@ -31,16 +31,8 @@ if gadgetHandler:IsSyncedCode() then
         if isCorpse[featureDefID] then
             local _, _, resurrectProgress = Spring.GetFeatureHealth(featureID)
             if resurrectProgress < 0.98 then
-                local fsize
-                local x,y,z = Spring.GetFeaturePosition (featureID)
-                if FeatureDefs[featureDefID].footprintX then
-                    fsize = FeatureDefs[featureDefID].footprintX
-                else
-                    fsize = 2
-                end
-                if fsize > 5 then fsize = 5 end
-                fsize = math.ceil(fsize)
-                --SPAWN CEG HERE
+                local x,y,z = Spring.GetFeaturePosition(featureID)
+				local fsize = isCorpse[featureDefID]
                 Spring.SpawnCEG(effectsTable[fsize].ceg, x, y, z)
                 Spring.PlaySoundFile(effectsTable[fsize].sound, 1, x, y, z, 'sfx')
             end
