f9070e5de47dc133c92f25f31d7e5f8e95551e8e
fruehling76 <39665636+fruehling76@users.noreply.github.com>
2022-10-13 15:01:06 +0200

    Update armthor.lua
    reduced max vel and damage of lightning weapon

--- units/ArmGantry/armthor.lua
+++ units/ArmGantry/armthor.lua
@@ -22,7 +22,7 @@ return {
 		mass = 100000,
 		maxdamage = 50000,
 		maxslope = 14,
-		maxvelocity = 2.0,
+		maxvelocity = 1.9,
 		maxwaterdepth = 20,
 		movementclass = "HTANK5",
 		nochasecategory = "VTOL",
@@ -165,7 +165,7 @@ return {
 					light_radius_mult = 1.5,
 				},
 				damage = {
-					default = 320,
+					default = 300,
 					subs = 30,
 				},
 			},
