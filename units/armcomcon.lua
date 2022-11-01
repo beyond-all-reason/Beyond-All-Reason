a8bb0a375a175477f236ccdc25eb4b3bc648b4c9
Floris <floris@ruwhof.net>
2022-11-01 15:27:20 +0100

    Revert "Initiate the SPOOKTOBER festivities!"
    This reverts commit bdd0fba1977f6630d26e705bc633fb56fa384dc3.

--- units/armcomcon.lua
+++ units/armcomcon.lua
@@ -43,7 +43,7 @@ return {
 		mincloakdistance = 50,
 		movementclass = "COMMANDERBOT",
 		nochasecategory = "ALL",
-		objectname = "Units/armcom_hw.s3o",
+		objectname = "Units/ARMCOM.s3o",
 		pushresistant = true,
 		radardistance = 700,
 		radaremitheight = 40,
