ccd0d420155e01fcaa6508f70e926065ed4483b7
Floris <floris@ruwhof.net>
2022-12-01 21:11:05 +0100

    deathceg light tweak

--- luaui/configs/DeferredLightsGL4WeaponsConfig.lua
+++ luaui/configs/DeferredLightsGL4WeaponsConfig.lua
@@ -249,9 +249,9 @@ local gibLight = {
 	pieceName = nil, -- optional
 	lightConfig = {
 		posx = 0, posy = 0, posz = 0, radius = 36,
-		r = 1, g = 0.9, b = 0.5, a = 0.33,
+		r = 1, g = 0.9, b = 0.5, a = 0.22,
 		color2r = 0.9, color2g = 0.75, color2b = 0.25, colortime = 0.3, -- point lights only, colortime in seconds for unit-attache
-		modelfactor = 0.4, specular = 0.5, scattering = 0.8, lensflare = 0,
+		modelfactor = 0.4, specular = 0.5, scattering = 0.7, lensflare = 0,
 		lifetime = 300, sustain = 3, aninmtype = 0 -- unused
 	},
 }
