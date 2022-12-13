6645583b34749792b763fea04f5c51d35b208ef5
Beherith <mysterme@gmail.com>
2022-12-13 10:14:38 +0100

    Major improvements to gui_infolos api!

---
+++ luaui/Widgets/Shaders/infolos.frag.glsl
@@ -0,0 +1,49 @@
+#version 130
+//__DEFINES__
+
+uniform float time;
+uniform float outputAlpha;
+uniform vec2 losTexSize;
+uniform vec2 airlosTexSize;
+uniform vec2 radarTexSize;
+
+uniform sampler2D tex0;
+uniform sampler2D tex1;
+uniform sampler2D tex2;
+varying vec2 texCoord;
+
+
+//! source: http://www.ozone3d.net/blogs/lab/20110427/glsl-random-generator/
+float rand(const in vec2 n)
+{
+	return fract(sin(dot(n, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+vec4 getTexel(in sampler2D tex, in vec2 p, in vec2 sizes)
+{
+	vec4 c = vec4(0.0);
+	for (int i = 0; i<SAMPLES; i++) {
+		vec2 off = vec2(time + float(i)*0.02);
+		off = (vec2(rand(p.st + off.st), rand(p.ts - off.ts)) * 2.0 - 1.0) / sizes;
+		c += texture2D(tex, p + off * RESOLUTION);
+	}
+	c *= 1.0/SAMPLES;
+	return c;
+	//return smoothstep(0.5, 1.0, c);
+}
+
+
+void main() {
+	gl_FragColor  = vec4(0.0);
+
+	float los = getTexel(tex0, texCoord, vec2(LOSXSIZE,LOSYSIZE)).r;
+	float airlos = getTexel(tex1, texCoord, vec2(AIRLOSXSIZE,AIRLOSYSIZE)).r;
+	vec2 radarJammer = getTexel(tex2, texCoord, vec2(RADARXSIZE,RADARYSIZE)).rg;
+	
+	gl_FragColor.r = los;
+	gl_FragColor.g += airlos * 0.33;
+	gl_FragColor.b = 0.5;
+	gl_FragColor.b += radarJammer.r;
+	gl_FragColor.b -= 2*radarJammer.g;
+	gl_FragColor.a = outputAlpha;
+}
--- luaui/Widgets/gui_infolos.lua
+++ luaui/Widgets/gui_infolos.lua
@@ -16,28 +16,32 @@ end
 local GL_RGBA32F_ARB = 0x8814
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
+-- About:
+	-- This API presents an easy -to-use smoothed LOS texture for other widgets to do their shading based on
+	-- The RED channel contains LOS level, where 0.33 = airlos
+	-- the GREEN channel contains AIRLOS level
+	-- the BLUE channel contains RADAR coverage
+		-- 0.5 = no radar
+		-- >0.5 = radar coverage
+		-- <0.5 = jammer
+	-- It runs every gameFrame
 -- TODO: 2022.12.12
 	-- make it work?
 	-- make api share?
+	-- a clever thing might be to have 1 texture per allyteam?
+	-- some bugginess with jammer range?
 
+local autoreload = false
 ---- CONFIGURABLE PARAMETERS: -----------------------------------------
 
 local shaderConfig = {
-	-- These are static parameters, cannot be changed during runtime
-	RAYMARCHSTEPS = 32, -- must be at least one, quite expensive
-	RESOLUTION = 2, -- THIS IS EXTREMELY IMPORTANT and specifies the fog plane resolution as a whole! 1 = max, 2 = half, 4 = quarter etc.
-	FOGTOP = 300, -- deprecated
-	NOISESAMPLES = 8, -- how many samples of 3D noise to take
-	NOISESCALE = 1.2,
-	NOISETHRESHOLD = -0.0,
+	SAMPLES = 4, -- quality setting
+	RESOLUTION = 2, -- Number of times to downsample (fraction of heightmap rez!)
 }
-
 ---------------------------------------------------------------------------
 
-local alwaysColor, losColor, radarColor, jamColor, radarColor2 = Spring.GetLosViewColors()
-
-local autoreload = true
-
+local alwaysColor, losColor, radarColor, jamColor, radarColor2 = Spring.GetLosViewColors() --unused
+local outputAlpha = 0.07
 local infoShader
 local infoTexture
 local texX, texY
@@ -45,166 +49,17 @@ local luaShaderDir = "LuaUI/Widgets/Include/"
 local LuaShader = VFS.Include(luaShaderDir.."LuaShader.lua")
 VFS.Include(luaShaderDir.."instancevbotable.lua")
 
+local vsSrcPath = "LuaUI/Widgets/Shaders/infolos.vert.glsl"
+local fsSrcPath = "LuaUI/Widgets/Shaders/infolos.frag.glsl"
 
+local miplevels = {2^3, 2^4, 2^3, 1} -- los, airlos and radar mip levels
 
-function widget:Initialize()
-	local alwaysColor, losColor, radarColor, jamColor, radarColor2 = Spring.GetLosViewColors()
-	texX = Game.mapSizeX/8
-	texY = Game.mapSizeZ/8
-	
-	infoTexture = gl.CreateTexture(texX, texY, {
-		min_filter = GL.LINEAR,
-		mag_filter = GL.LINEAR,
-		wrap_s = GL.CLAMP_TO_EDGE,
-		wrap_t = GL.CLAMP_TO_EDGE,
-		fbo = true,
-		format = GL.RGBA8,
-		})
-		
-	infoShader = LuaShader({
-			--while this vertex shader seems to do nothing, it actually does the very important world space to screen space mapping for gl.TexRect!
-		definitions = {
-			Spring.GetConfigInt("HighResInfoTexture") and "#define HIGH_QUALITY" or "",
-		},
-		vertex = [[#version 130
-			varying vec2 texCoord;
-			void main() {
-				texCoord = gl_MultiTexCoord0.st;
-				gl_Position = vec4(gl_Vertex.xyz, 1.0);
-			}
-		]],
-		fragment = [[#version 130
-		#ifdef HIGH_QUALITY
-			#extension GL_ARB_texture_query_lod : enable
-		#endif
-			uniform float time;
-			uniform vec4 alwaysColor;
-			uniform vec4 losColor;
-			uniform vec4 radarColor1;
-			uniform vec4 radarColor2;
-			uniform vec4 jamColor;
-			uniform sampler2D tex0;
-			uniform sampler2D tex1;
-			uniform sampler2D tex2;
-			uniform sampler2D tex3;
-			varying vec2 texCoord;
-		#ifdef HIGH_QUALITY
-			//! source: http://www.ozone3d.net/blogs/lab/20110427/glsl-random-generator/
-			float rand(const in vec2 n)
-			{
-				return fract(sin(dot(n, vec2(12.9898, 78.233))) * 43758.5453);
-			}
-			vec4 getTexel(in sampler2D tex, in vec2 p)
-			{
-				int lod = int(textureQueryLOD(tex, p).x);
-				vec2 texSize = vec2(textureSize(tex, lod));
-				vec2 off = vec2(time);
-				vec4 c = vec4(0.0);
-				for (int i = 0; i<4; i++) {
-					off = (vec2(rand(p.st + off.st), rand(p.ts - off.ts)) * 2.0 - 1.0) / texSize;
-					c += texture2D(tex, p + off);
-				}
-				c *= 0.25;
-				return smoothstep(0.5, 1.0, c);
-			}
-		#else
-			#define getTexel texture2D
-		#endif
-
-			const mat2 m = mat2(0.707107, -0.707107, 0.707107, 0.707107);
-
-			float diagLines1(vec2 uv) {
-				float col = 1.0;
-
-				uv = m * uv;
-				uv *= 2048.0;
-				uv += 2.0 * vec2(-time, -time);
-
-				float c = sin(uv.x) * 0.15;
-				col -= c;
-				col = pow(col, 2.75);
-
-				return col;
-			}
-			
-			float diagLines2(vec2 uv) {
-				float col = 1.0;
-
-				uv = m * uv;
-				uv *= 2048.0;
-				uv += 2.0 * vec2(-time, -time);
-
-				float c = sin(uv.x) * 0.20;
-				col -= c;
-				col = pow(col, 4.0);
-
-				return col;
-			}
-
-			float radarAnim(vec2 uv) {
-				float d = distance(vec2(0.5), uv);
-				float d0 = 0.1 * time;
-				float dt1 = 1.0 - smoothstep(0.0, 0.1, fract(4.0 * (d0 - d)));
-				dt1 = pow(dt1, 8.0);
-
-				return mix(1.0, 8.0, dt1);
-			}
-
-			float hex2(vec2 p, float width, float coreSize) {
-				p.x *= 0.57735 * 2.0;
-				p.y += mod(floor(p.x), 2.0) * 0.5;
-				p = abs((mod(p, 1.0) - 0.5));
-				float val = abs(max(p.x * 1.5 + p.y, p.y * 2.0) - 1.0);
-				return smoothstep(coreSize, width, val);
-			}
-
-			void main() {
-				gl_FragColor  = vec4(0.0);
-				
-				float height = texture2D(tex3, texCoord).x;
-				float heightStatus = smoothstep(-3.0, 0.0, height);
-
-				vec2 radarJammer = getTexel(tex2, texCoord).rg;
-
-				float radarFull = step(0.8, radarJammer.r);
-				float radarEdge = float(radarJammer.r > 0.0 && radarJammer.r <= 0.9);
-
-				vec4 radarColor = mix(radarColor2 * radarEdge, radarColor1 * radarFull, radarJammer.r);
-
-				gl_FragColor += jamColor * radarJammer.g;
-				gl_FragColor += radarColor;// * hex2(64.0 * texCoord, 0.025, 0.2) * radarAnim(texCoord);
-
-				float los = getTexel(tex0, texCoord).r;
-				float airlos = getTexel(tex1, texCoord).r;
-				float losStatus = mix(los, airlos, 0.2);
-
-				gl_FragColor += losColor * losStatus;
-
-				//gl_FragColor.rgb += alwaysColor.rgb * mix(hex2(64.0 * texCoord, 0.025, 0.2), 1.0, max(radarFull, losStatus));
-
-				float terraIncognitaStatus = 1.0 - max(radarFull, losStatus);
-				float terraIncognitaEffect = mix(diagLines2(texCoord), diagLines1(texCoord), heightStatus);
-				
-				gl_FragColor.rgb += alwaysColor.rgb * mix(1.0, terraIncognitaEffect, terraIncognitaStatus);
-
-				gl_FragColor.a = 0.03;
-				gl_FragColor.rgba = vec4(0.0);
-				
-				gl_FragColor.r = los;
-				gl_FragColor.g += airlos * 0.33;
-				gl_FragColor.b = 0.5;
-				gl_FragColor.b += radarJammer.r;
-				gl_FragColor.b -= 2*radarJammer.g;
-				
-				gl_FragColor.a = 0.07;
-			}
-		]],
+local shaderSourceCache = {
+		vssrcpath = vsSrcPath,
+		fssrcpath = fsSrcPath,
 		uniformFloat = {
-			alwaysColor = alwaysColor,
-			losColor    = losColor,
-			radarColor1 = radarColor,
-			jamColor    = jamColor,
-			radarColor2 = radarColor2,
+			outputAlpha = 0.1,
+			time = 1.0,
 		},
 		uniformInt = {
 			tex0 = 0,
@@ -216,29 +71,58 @@ function widget:Initialize()
 			[0] = "$info:los",
 			[1] = "$info:airlos",
 			[2] = "$info:radar",
-			[3] = "$heightmap",
 		},
+		shaderName = "InfoLOS GL4",
+		shaderConfig = shaderConfig
 	}
-	)
+
+local function GetInfoLOSTexture()
+	return infoTexture
+end
+
+function widget:Initialize()
+	local alwaysColor, losColor, radarColor, jamColor, radarColor2 = Spring.GetLosViewColors()
+	texX = (Game.mapSizeX/8)/shaderConfig.RESOLUTION
+	texY = (Game.mapSizeZ/8)/shaderConfig.RESOLUTION
 	
-	shaderCompiled = infoShader:Initialize()
-	if (shaderCompiled == nil) then
-		return
+	for name, tex in pairs({LOS = "$info:los", AIRLOS = "$info:airlos", RADAR = "$info:radar" }) do
+		local texInfo = gl.TextureInfo(tex)
+		shaderConfig[name .. 'XSIZE'] = texInfo.xsize
+		shaderConfig[name .. 'YSIZE'] = texInfo.ysize
+		Spring.Debug.TableEcho(texInfo)
 	end
+	
+	
+	infoTexture = gl.CreateTexture(texX, texY, {
+		min_filter = GL.LINEAR,
+		mag_filter = GL.LINEAR,
+		wrap_s = GL.CLAMP_TO_EDGE,
+		wrap_t = GL.CLAMP_TO_EDGE,
+		fbo = true,
+		format = GL.RGBA8, -- more than enough
+		})
+		
+	infoShader =  LuaShader.CheckShaderUpdates(shaderSourceCache)
+	shaderCompiled = infoShader:Initialize()
+	if not shaderCompiled then Spring.Echo("Failed to compile InfoLOS GL4") end 
+	
+
+	
+	WG['infolosapi'] = {}
+	WG['infolosapi'].GetInfoLOSTexture = GetInfoLOSTexture
+	widgetHandler:RegisterGlobal('GetInfoLOSTexture', WG['infolosapi'].GetInfoLOSTexture)
 end
 
 function widget:Shutdown()
 	if infoTexture then gl.DeleteTexture(infoTexture) end
-
+	WG['infolosapi'] = nil
+	widgetHandler:DeregisterGlobal('GetInfoLOSTexture')
 end
 
-local windX = 0
-local windZ = 0
-
-local gameFramePassed
+local updateInfoLOSTexture
 function widget:GameFrame(n)
-	if (n%2) == 0 then 
-		gameFramePassed = true
+	if (n%1) == 0 then 
+		updateInfoLOSTexture = true
 	end
 end
 
@@ -250,7 +134,6 @@ local function renderToTextureFunc() -- this draws the fogspheres onto the textu
 	gl.Texture(0, "$info:los")
 	gl.Texture(1, "$info:airlos")
 	gl.Texture(2, "$info:radar") --$info:los
-	gl.Texture(3, "$heightmap")
 	gl.Blending(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA)
 	gl.TexRect(-1, -1, 1, 1, 0, 0, 1, 1)
 	gl.Texture(0, false)
@@ -259,28 +142,36 @@ local function renderToTextureFunc() -- this draws the fogspheres onto the textu
 	gl.Texture(3, false)
 end
 
+local lastUpdate = Spring.GetTimer()
+
 function widget:DrawWorldPreUnit() 
-	
-	gl.DepthMask(false) -- dont write to depth buffer
-	gl.Culling(false) -- cause our tris are reversed in plane vbo
-	if gameFramePassed then 
+	local nowtime = Spring.GetTimer()
+	local deltat = Spring.DiffTimers(nowtime, lastUpdate)
+	-- keeping outputAlpha identical is a very important trick for never-before-seen areas!
+	--outputAlpha = math.min(1.0, math.max(0.07,deltat))
+	--Spring.Echo(deltat,outputAlpha)
+	if outputAlpha > 0.07 or deltat > outputAlpha then updateInfoLOSTexture = true end 
+	if updateInfoLOSTexture then
+		lastUpdate = nowtime
+
+		gl.DepthMask(false) -- dont write to depth buffer
+		gl.Culling(false) -- cause our tris are reversed in plane vbo
 		infoShader:Activate()
-		infoShader:SetUniformFloat("alwaysColor", alwaysColor[1],alwaysColor[2],alwaysColor[3],alwaysColor[4])
-		infoShader:SetUniformFloat("losColor", losColor[1],losColor[2],losColor[3],losColor[4])
-		--infoShader:SetUniformFloat("radarColor1", radarColor1)
-		--infoShader:SetUniformFloat("jamColor", jamColor)
-		--infoShader:SetUniformFloat("radarColor2", radarColor2)
+		infoShader:SetUniformFloat("outputAlpha", outputAlpha)
+		infoShader:SetUniformFloat("time", Spring.GetDrawFrame() / 1000)
 		gl.RenderToTexture(infoTexture, renderToTextureFunc)
 		infoShader:Deactivate()
-		gameFramePassed = false
+		updateInfoLOSTexture = false
+		gl.DepthMask(false) --"BK OpenGL state resets", reset to default state
 	end
-
-
-	gl.DepthMask(false) --"BK OpenGL state resets", reset to default state
 end
 
-function widget:DrawScreen()
-	gl.Texture(0, infoTexture)
-	gl.Blending(GL.ONE, GL.ZERO)
-	gl.TexRect(0, 0, 1000, 1000, 0, 0, 1, 1)
+function widget:DrawScreen() -- the debug display output
+	if autoreload then 
+		infoShader = LuaShader.CheckShaderUpdates(shaderSourceCache) or infoShader
+		gl.Color(1,1,1,1) -- use this to show individual channels of the texture!
+		gl.Texture(0, infoTexture)
+		gl.Blending(GL.ONE, GL.ZERO)
+		gl.TexRect(0, 0, texX, texY, 0, 0, 1, 1)
+	end
 end
