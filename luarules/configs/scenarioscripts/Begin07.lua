420423f47ddc1d143ee443527c5ab02b4e52fdca
Ezahiel <114177265+Ezahiel@users.noreply.github.com>
2022-12-21 09:49:27 +0000

    rly?

--- luarules/configs/scenarioscripts/Begin07.lua
+++ luarules/configs/scenarioscripts/Begin07.lua
@@ -7,23 +7,27 @@ end
 
 	local objectiveUnits = {
 		{name = 'armck', x = 164, y = 432, z = 2649, rot = 0, teamID = 0, queue = {
-		[1] = {cmdID = CMD.MOVE, params = {123, 456, 789}},
-		[2] = {cmdID = CMD.PATROL, params = {222, 333, 444}},
-		[3] = {cmdID = CMD.PATROL, params = {555, 666, 777}},
+		{cmdID = CMD.MOVE, position = {{px = 2183, py = 432, pz = 484}}},
+		{cmdID = CMD.PATROL, position = {{px = 108, py = 426,pz = 2091}}},
+		{cmdID = CMD.PATROL, position = {{px = 659, py = 435, pz = 2680}}}
 		}},
 		{name = 'armpw', x = 160, y = 432, z = 2649, rot = 0, teamID = 0, queue = {
-		[1]	= {cmdID = CMD.PATROL, params = {px = 659, py = 435, pz = 2680}}
+		{cmdID = CMD.PATROL, position = {659, 435, 2680}}
 		}},
 		{name = 'armrock', x = 156, y = 432, z = 2649, rot = 0, teamID = 0},
-		{name = 'coradvsol', x = 82, y = 200, z = 3671, rot = 1, teamID = 1 },
-		{name = 'armham', x = 152, y = 432, z = 2649, rot = 0, teamID = 0, px = 108, py = 426, pz = 2091},
+		--{name = 'coradvsol', x = 82, y = 200, z = 3671, rot = 1, teamID = 1 },
+		--{name = 'armham', x = 152, y = 432, z = 2649, rot = 0, teamID = 0, px = 108, py = 426, pz = 2091},
 	}
 
 		for k , unit in pairs(objectiveUnits) do
+			if UnitDefNames[unit.name] then
 			local unitID = Spring.CreateUnit(unit.name, unit.x, unit.y, unit.z, unit.rot, unit.teamID)
-			for i = 1, #unit.queue do
-				local order = unit.queue[i]
-				Spring.GiveOrderToUnit(unitID, order.cmdID, order.params, CMD.OPT_SHIFT)
+
+				for i = 1, #unit.queue do
+					local order = unit.queue[i]
+					order.position = {} --or {order.position{"px"}, order.position["py"], order.position["pz"]}
+					Spring.GiveOrderToUnit(unitID, order.cmdID, order.position, CMD.OPT_SHIFT)
+				end
 			end
 		end
 
