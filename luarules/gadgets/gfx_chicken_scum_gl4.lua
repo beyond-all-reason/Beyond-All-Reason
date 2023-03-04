bf924a1c49a9288f9b95a63f79785b19b0cb1001
Beherith <mysterme@gmail.com>
2023-03-04 18:12:55 +0100

    clean up alien guts for scum

--- luarules/gadgets/gfx_chicken_scum_gl4.lua
+++ luarules/gadgets/gfx_chicken_scum_gl4.lua
@@ -221,8 +221,8 @@ if gadgetHandler:IsSyncedCode() then
 else
 
 
-	local texcolorheight = "LuaUI/images/alien_guts_colorheight.dds"
-	local texnormalspec =  "LuaUI/images/alien_guts_normalspec.dds"
+	local texcolorheight = "LuaUI/images/raptor_scum/alien_guts_colorheight.dds"
+	local texnormalspec =  "LuaUI/images/raptor_scum/alien_guts_normalspec.dds"
 	local texdistortion =  "LuaUI/images/lavadistortion.png"
 	local resolution = 32
 
