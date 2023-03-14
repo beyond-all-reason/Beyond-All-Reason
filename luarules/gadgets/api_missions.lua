76f365df04d9b2a89f92003b5ed7ded0c4d38b0a
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2023-03-14 17:21:52 -0400

    Mission API: Initial architectural skeleton and gadget

--- luarules/gadgets/api_missions.lua
+++ luarules/gadgets/api_missions.lua
@@ -1,48 +1,43 @@
 function gadget:GetInfo()
 	return {
-		name = "Mission API loader",
-		desc = "Load and populate global mission table",
+		name = "Mission API triggers",
+		desc = "Load and poll mission triggers, and dispatch actions",
 		date = "2023.03.14",
+		license = "GNU GPL, v2 or later",
 		layer = 0,
 		enabled = true,
 	}
 end
 
 if not gadgetHandler:IsSyncedCode() then
-	return false
+	return
 end
-
-local scriptPath
-local rawTriggers, rawActions
 
 local function loadMission()
-	local mission = VFS.Include("singleplayer/" .. scriptPath)
-	rawTriggers = mission.Triggers
-	rawActions = mission.Actions
-
-	GG['MissionAPI'].TriggersController.PreprocessRawTriggers(rawTriggers)
-	GG['MissionAPI'].ActionsController.PreprocessRawActions(rawActions)
-	GG['MissionAPI'].TriggersController.PostprocessTriggers()
-end
-
-function gadget:Initialize()
-	-- TODO: Actually pass script path in modoptions
-	scriptPath = 'test_mission.lua'-- Spring.GetModOptions().mission_path
+	-- TODO: Actually pass script path in scenariooptions modoption
+	local missionSettings = Spring.GetModOptions().scenariooptions
+	local scriptPath = missionSettings.path
 
 	if not scriptPath then
 		gadgetHandler:RemoveGadget()
 	end
 
+	VFS.Include("singleplayer/" .. scriptPath)	
+end
+
+-- monitor and dispatch triggers
+-- when trigger invoked, set activated, count, etc.
+-- disable trigger if appropriate
+-- invoke actions
+
+function gadget:Initialize()
 	GG['MissionAPI'] = {}
-	GG['MissionAPI'].TriggersController = VFS.Include('luarules/mission_api/triggers.lua')
-	GG['MissionAPI'].ActionsController = VFS.Include('luarules/mission_api/actions.lua')
-	GG['MissionAPI'].ActionsDispatcher = VFS.Include('luarules/mission_api/actions_dispatcher.lua')
-	GG['MissionAPI'].Difficulty = Spring.GetModOptions().mission_difficulty --TODO: add mission difficulty modoption
 
 	loadMission();
+	-- loading the mission sciprt needs to populate the global triggers and actions tables
+	-- may be confusing if GG['MissionAPI'] is declared here but modified elsewhere
 
-	GG['MissionAPI'].Triggers = GG['MissionAPI'].TriggersController.GetTriggers()
-	GG['MissionAPI'].Actions = GG['MissionAPI'].ActionsController.GetActions()
+	-- load triggers into table(s)
 end
 
 function gadget:Shutdown()
