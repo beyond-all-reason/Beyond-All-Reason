6b3bbd63e5c863e3b17dc97f60139769b67635a7
Damgam <c_2_damiano@op.pl>
2023-03-18 07:41:01 +0100

    don't cut off end of the track if fades are disabled

--- luaui/Widgets/gui_advplayerslist_music_new.lua
+++ luaui/Widgets/gui_advplayerslist_music_new.lua
@@ -1040,7 +1040,7 @@ function widget:GameFrame(n)
 						or (currentTrackListString == "warLow" and warMeter <= warLowLevel * 0.5 )) then -- WarLow music is playing, but it has been quite peaceful recently. Let's switch to peace music at 50% of WarLow threshold
 							fadeDirection = -2
 							fadeOutSkipTrack = true
-					elseif playedTime >= totalTime - 12 then
+					elseif (playedTime >= totalTime - 12 and Spring.GetConfigInt("UseSoundtrackFades", 1) == 1) then
 						fadeDirection = -1
 					end
 				end
