8bbb3f36d3ca977020269606dc007d1ba2a8b68f
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2023-03-20 15:50:07 -0400

    Mission API: Add action dipatcher, trigger monitor

--- luarules/configs/scenarioscripts/API/actions_dispatcher.lua
+++ luarules/configs/scenarioscripts/API/actions_dispatcher.lua
@@ -1,7 +1,43 @@
+local types = GG['MissionAPI'].ActionsController.Types
+local actions = GG['MissionAPI'].ActionsController.GetActions
+
 local function sendMessage(message)
 	Spring.Echo(message)
 end
 
+local typeMapping = {
+	-- types.EnableTrigger = ,
+	-- types.DisableTrigger = ,
+	-- types.IssueOrders = ,
+	-- types.AllowCommands = ,
+	-- types.RestrictCommands = ,
+	-- types.AlterBuildlist = ,
+	-- types.EnableBuildOption = ,
+	-- types.DisableBuildOption = ,
+	-- types.SpawnUnits = ,
+	-- types.SpawnConstruction = ,
+	-- types.DespawnUnits = ,
+	-- types.SpawnWeapons = ,
+	-- types.SpawnEffects = ,
+	-- types.RevealLOS = ,
+	-- types.UnrevealLOS = ,
+	-- types.AlterMapZones = ,
+	-- types.TransferUnits = ,
+	-- types.ControlCamera = ,
+	-- types.Pause = ,
+	-- types.Unpause = ,
+	-- types.PlayMedia = ,
+	[types.SendMessage] = sendMessage,
+	-- types.Victory = ,
+	-- types.Defeat = ,
+}
+
+local function invoke(actionId)
+	local actionFunction = typeMapping[actions[actionId].type]
+
+	actionFunction(actions[actionId].parameters)
+end
+
 return {
-	SendMessage = sendMessage,
+	Invoke = invoke,
 }
