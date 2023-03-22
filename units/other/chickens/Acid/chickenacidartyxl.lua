6ccbcbd4353cf94da9d5c3aa35bbb75643165da7
Damgam <c_2_damiano@op.pl>
2023-03-22 22:50:04 +0100

    added/updated missing/outdated chicken unitpics

--- units/other/chickens/Acid/chickenacidartyxl.lua
+++ units/other/chickens/Acid/chickenacidartyxl.lua
@@ -6,7 +6,7 @@ return {
 		buildcostenergy = 12320,
 		buildcostmetal = 396,
 		builder = false,
-		buildpic = "chickens/chickenacidarty.DDS",
+		buildpic = "chickens/chickenacidartyxl.DDS",
 		buildtime = 6750,
 		canattack = true,
 		canguard = true,
