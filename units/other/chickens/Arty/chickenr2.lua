32a0200dd7c7eceb1c14087d4d66ea7dac9cf7a9
Damgam <c_2_damiano@op.pl>
2023-03-27 03:00:28 +0200

    Raptor Rapid Fire Artillery reload nerf

--- units/other/chickens/Arty/chickenr2.lua
+++ units/other/chickens/Arty/chickenr2.lua
@@ -100,7 +100,7 @@ return {
 				noselfdamage = true,
 				proximitypriority = -4,
 				range = 2000,
-				reloadtime = 0.8,
+				reloadtime = 2.4,
 				rgbcolor = "1 0.5 0.1",
 				size = 5.5,
 				sizedecay = 0.09,
