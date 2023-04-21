e54908aa7fc6b609edf9fae9ba892ef740785b74
Floris <floris@ruwhof.net>
2023-04-21 15:27:02 +0200

    settings: improved filter (ignoring some markup)

--- luaui/Widgets/gui_options.lua
+++ luaui/Widgets/gui_options.lua
@@ -5737,7 +5737,10 @@ function init()
 		local filteredOptions = {}
 		for i, option in pairs(options) do
 			if option.name and option.name ~= '' and option.type and option.type ~= 'label' then
-				if string.find(string.lower(option.name), string.lower(inputText), nil, true) then
+				--local name = string.gsub(option.name, "(\\[0-9]+)+", "")
+				local name = string.gsub(option.name, widgetOptionColor, "")
+				name = string.gsub(name, "  ", " ")
+				if string.find(string.lower(name), string.lower(inputText), nil, true) then
 					filteredOptions[#filteredOptions+1] = option
 				elseif option.description and option.description ~= '' and string.find(string.lower(option.description), string.lower(inputText), nil, true) then
 					filteredOptions[#filteredOptions+1] = option
