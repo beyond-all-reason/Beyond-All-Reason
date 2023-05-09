e467b6622b16ce3d657aec7c0094d4c973bd881b
Timothy Verbruggen <73989722+Avaristimo@users.noreply.github.com>
2023-05-10 00:50:30 +0200

    Update luarules/gadgets/unit_dgun_behaviour.lua
    Co-authored-by: WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>

--- luarules/gadgets/unit_dgun_behaviour.lua
+++ luarules/gadgets/unit_dgun_behaviour.lua
@@ -6,7 +6,7 @@ function gadget:GetInfo()
 		desc = "D-Gun projectiles hug ground, deterministic damage against Commanders",
 		author = "Anarchid, Sprung",
 		layer = 0,
-		enabled = true -- Disabled for now because d-gun has been replaced with EMP weapon
+		enabled = enabled
 	}
 end
 
