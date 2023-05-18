43784abb9d246542e5e9207fe3de565fa07585cf
Floris <floris@ruwhof.net>
2023-05-18 15:05:39 +0200

    mex snap: fix pregame snapping

--- luaui/Widgets/cmd_mex_snap.lua
+++ luaui/Widgets/cmd_mex_snap.lua
@@ -7,7 +7,7 @@ function widget:GetInfo()
 		version   = "v1.2",
 		date      = "November 2010",
 		license   = "GNU GPL, v2 or later",
-		layer     = 1001,
+		layer     = -1,
 		enabled   = true,
 		handler   = true
 	}
