f1c4fe14279b06cd277b4c8cbab7f14fb2673677
Kyle Shepherd <kyleanthonyshepherd@gmail.com>
2023-05-23 22:59:03 -0500

    Add cruise missile custom weapon behavior

--- luarules/gadgets/unit_custom_weapons_behaviours.lua
+++ luarules/gadgets/unit_custom_weapons_behaviours.lua
@@ -13,6 +13,7 @@ end
 if gadgetHandler:IsSyncedCode() then
 
 	local projectiles = {}
+	local active_projectiles = {}
 	local checkingFunctions = {}
 	local applyingFunctions = {}
 	local math_sqrt = math.sqrt
@@ -25,6 +26,63 @@ if gadgetHandler:IsSyncedCode() then
 			specialWeaponCustomDefs[id] = def.customParams
 		end
 	end
+
+	checkingFunctions.cruise = {}
+	checkingFunctions.cruise["distance>0"] = function (proID)
+		--Spring.Echo()
+
+		if Spring.GetProjectileTimeToLive(proID) <= 0 then
+			return true
+		end
+		local targetTypeInt,target = Spring.GetProjectileTarget(proID)
+		local xx,yy,zz
+		local xxv,yyv,zzv
+		if targetTypeInt == string.byte('g') then
+			xx = target[1]
+			yy = target[2]
+			zz = target[3]
+		end
+		if targetTypeInt == string.byte('u') then
+			_,_,_,_,_,_,xx,yy,zz = Spring.GetUnitPosition(target,true,true)
+		end
+		local xp,yp,zp = Spring.GetProjectilePosition(proID)
+		local vxp,vyp,vzp = Spring.GetProjectileVelocity(proID)
+		local mag = math_sqrt(vxp*vxp+vyp*vyp+vzp*vzp)
+		local infos = projectiles[proID]
+		if math_sqrt((xp-xx)^2 + (yp-yy)^2 + (zp-zz)^2) > tonumber(infos.lockon_dist) then
+			yg = Spring.GetGroundHeight(xp,zp)
+			nx,ny,nz,slope= Spring.GetGroundNormal(xp,zp)
+			--Spring.Echo(Spring.GetGroundNormal(xp,zp))
+			--Spring.Echo(tonumber(infos.cruise_height)*slope)
+			if yp < yg + tonumber(infos.cruise_min_height) then
+				active_projectiles[proID] = true
+				Spring.SetProjectilePosition(proID,xp,yg + tonumber(infos.cruise_min_height),zp)
+				local norm = (vxp*nx+vyp*ny+vzp*nz)
+				xxv = vxp - norm*nx*0
+				yyv = vyp - norm*ny
+				zzv = vzp - norm*nz*0
+				Spring.SetProjectileVelocity(proID,xxv,yyv,zzv)
+			end
+			if yp > yg + tonumber(infos.cruise_max_height) and active_projectiles[proID] and vyp > -mag*.25 then
+				-- do not clamp to max height if
+				-- vertical velocity downward is more than 1/4 of current speed
+				-- probably just went off lip of steep cliff
+				Spring.SetProjectilePosition(proID,xp,yg + tonumber(infos.cruise_max_height),zp)
+				local norm = (vxp*nx+vyp*ny+vzp*nz)
+				xxv = vxp - norm*nx*0
+				yyv = vyp - norm*ny
+				zzv = vzp - norm*nz*0
+				Spring.SetProjectileVelocity(proID,xxv,yyv,zzv)
+			end
+			return false
+		else
+			return true
+		end
+	end
+
+	applyingFunctions.cruise = function (proID)
+		return false
+    end
 
 	checkingFunctions.cannonwaterpen = {}
 	checkingFunctions.cannonwaterpen["ypos<0"] = function (proID)
@@ -107,11 +165,13 @@ if gadgetHandler:IsSyncedCode() then
 		local wDefID = Spring.GetProjectileDefID(proID)
 		if specialWeaponCustomDefs[wDefID] then
 			projectiles[proID] = specialWeaponCustomDefs[wDefID]
+			active_projectiles[proID] = nil
 		end
 	end
 
 	function gadget:ProjectileDestroyed(proID)
 		projectiles[proID] = nil
+		active_projectiles[proID] = nil
 	end
 
 	function gadget:GameFrame(f)
@@ -119,6 +179,7 @@ if gadgetHandler:IsSyncedCode() then
 			if checkingFunctions[infos.speceffect][infos.when](proID) == true then
 				applyingFunctions[infos.speceffect](proID)
 				projectiles[proID] = nil
+				active_projectiles[proID] = nil
 			end
 		end
 	end
