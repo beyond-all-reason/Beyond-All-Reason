fe3070533a4c443afc9236de90afa0c3fa6a5442
Kyle Shepherd <kyleanthonyshepherd@gmail.com>
2023-05-26 18:13:14 -0500

    stop blinking with 3+ players and readied.

--- luaui/Widgets/gui_pregameui.lua
+++ luaui/Widgets/gui_pregameui.lua
@@ -376,7 +376,10 @@ function widget:DrawScreen()
 			else
 				colorString = os.clock() % 0.75 <= 0.375 and "\255\255\255\255" or "\255\222\222\222"
 			end
-			if blinkButton and os.clock() % 0.75 <= 0.375 then
+			if readied then
+				colorString = "\255\222\222\222"
+			end
+			if blinkButton and not readied and os.clock() % 0.75 <= 0.375 then
 				local mult = 1.33
 				UiButton(buttonRect[1], buttonRect[2], buttonRect[3], buttonRect[4], 1, 1, 1, 1, 1, 1, 1, 1, nil, { readyButtonColor[1]*0.55*mult, readyButtonColor[2]*0.55*mult, readyButtonColor[3]*0.55*mult, 1 }, { readyButtonColor[1]*mult, readyButtonColor[2]*mult, readyButtonColor[3]*mult, 1 })
 			end
