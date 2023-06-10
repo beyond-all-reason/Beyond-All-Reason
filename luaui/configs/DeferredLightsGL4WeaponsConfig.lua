99f254a8220015cf51662fff1677b073dab21d32
Floris <floris@ruwhof.net>
2023-06-10 16:19:23 +0200

    unit lights: fix for some missing scav unit lights + toned down lighting slightly for a bunch of units

--- luaui/configs/DeferredLightsGL4WeaponsConfig.lua
+++ luaui/configs/DeferredLightsGL4WeaponsConfig.lua
@@ -254,10 +254,10 @@ local gibLight = {
 	lightType = 'point', -- or cone or beam
 	pieceName = nil, -- optional
 	lightConfig = {
-		posx = 0, posy = 0, posz = 0, radius = 38,
-		r = 1, g = 0.9, b = 0.5, a = 0.12,
+		posx = 0, posy = 0, posz = 0, radius = 36,
+		r = 1, g = 0.9, b = 0.5, a = 0.08,
 		color2r = 0.9, color2g = 0.75, color2b = 0.25, colortime = 0.3, -- point lights only, colortime in seconds for unit-attache
-		modelfactor = 0.4, specular = 0.5, scattering = 0.7, lensflare = 0,
+		modelfactor = 0.4, specular = 0.5, scattering = 0.5, lensflare = 0,
 		lifetime = 300, sustain = 3, aninmtype = 0 -- unused
 	},
 }
