bcc4b52aad90b936974af9e86b071cdd14596f07
sprunk <spr.ng@o2.pl>
2023-07-03 19:02:59 +0200

    Add an explicit smoothmesh modrule
    So that people who want to tweak it can just flip it and not have to worry about finding its name etc

--- gamedata/modrules.lua
+++ gamedata/modrules.lua
@@ -90,6 +90,7 @@ local modrules = {
 	},
 
 	system = {
+		enableSmoothMesh = true,
 		pathFinderSystem = 0,			-- Which pathfinder does the game use? Can be 0 - The legacy default pathfinder, 1 - Quad-Tree Pathfinder System (QTPFS) or -1 - disabled.
 		pathFinderUpdateRate = 0.0001,	-- default: 0.007.  Controls how often the pathfinder updates; larger values means more rapid updates.
 		pathFinderRawDistMult = 100000,	-- default: 1.25.  Engine does raw move with a limited distance, this multiplier adjusts that
