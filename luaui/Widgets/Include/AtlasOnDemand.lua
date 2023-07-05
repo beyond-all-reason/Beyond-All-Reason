e7322745914c6fc0a1b0e644b52f61c19b9c4897
Beherith <mysterme@gmail.com>
2023-07-05 22:05:10 +0200

    AtlasOnDemand workaround AMD allocating garbage new textures

--- luaui/Widgets/Include/AtlasOnDemand.lua
+++ luaui/Widgets/Include/AtlasOnDemand.lua
@@ -186,6 +186,9 @@ local function MakeAtlasOnDemand(config)
 		debug = config.debug,
 		hastasks = false,
 	}
+	
+	-- add an initial blanking command to the whole goddamned thing because AMD allocates garbage as texture memory
+	AtlasOnDemand.renderImageTaskList[1] = {id = AtlasOnDemand.blankimg, w = config.xresolution , h = config.yresolution, x = 0, y = 0 }
 	for x = 1, AtlasOnDemand.xslots do
 		AtlasOnDemand.fill[x] = {}
 		for y = 1, AtlasOnDemand.yslots do 
