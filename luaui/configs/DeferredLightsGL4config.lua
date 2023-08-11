9cef6cbc90a3e74fe7e2fff60256327fcbd32812
Floris <floris@ruwhof.net>
2023-08-11 10:51:34 +0200

    api unit tracker: dont ignore units having customparam nohealthbars

--- luaui/configs/DeferredLightsGL4config.lua
+++ luaui/configs/DeferredLightsGL4config.lua
@@ -3843,9 +3843,9 @@ local unitDefLights = {
 		eyes = {
 			lightType = 'point',
 			pieceName = 'base',
-			lightConfig = { posx = 0, posy = 2, posz = 8, radius = 10,
+			lightConfig = { posx = 0, posy = 3.5, posz = 8, radius = 10,
 							color2r = 1, color2g = 0.2, color2b = 0.2, colortime = 0,
-							r = 1, g = 0.2, b = 0.2, a = 0.07,
+							r = 1, g = 0.2, b = 0.2, a = 0.09,
 							modelfactor = 0.4, specular = 0, scattering = 0.7, lensflare = 0,
 							lifetime = 0, sustain = 0, animtype = 0 },
 		},
