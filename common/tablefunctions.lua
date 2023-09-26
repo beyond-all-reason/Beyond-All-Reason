d78e8e098bd359cca675131f61dd0df71c5c4493
Nicolas Busseneau <nicolas@busseneau.fr>
2023-09-26 15:28:23 +0200

    common: additions / modifications to table functions (#2040)
    * common: prepare table functions tests
    
    This commit introduces a new `common\tableFunctionsTests.lua` file that
    can be loaded by uncommenting its `VFS.Include` call in `init.lua`.
    
    It is commented by default as this intended to be a facility for
    developers working on `common\tablefunctions.lua`, but we don't want
    them to run for end users.
    
    * common: rework `table.mergeInPlace` (non-deep version)
    
    This commit reworks the non-deep version of `table.mergeInPlace` to
    actually do it in place without making a copy, and adds a test to verify
    its functionality.
    
    We also take this opportunity to document both `table.merge*` functions,
    though no functional change have been made (just variables renamed).
    
    * common: add `table.(count|getKeyOf|contains)` functions
    
    These are simple enough and should be self-explanatory.
    
    * common: add `table.remove(If|All|First)` functions
    
    These functions are intended to be more useful alternatives to
    the default `table.remove`. It should be noted that `table.removeFirst`
    will always try to keep a sequence intact.
    
    Tests are provided to verify functionality.
    
    * common: add `table.shuffle` function
    
    This commit adds a function able to shuffle a table using Knuth
    (Fisher–Yates) algorithm. We only implemented the classical version,
    which can only handle sequences and not generic tables.
    
    A test is provided to verify appropriate distribution of the values
    following a shuffle.
    
    * common: add `pairsByKeys` function
    
    This commit adds a `pairs`-like iterator function allowing to traverse
    a table in the order of its keys. Its implementation has been copied
    straight from the docs at https://www.lua.org/pil/19.3.html.
    
    Since it's not registered under the `table` namespace by in the global
    namespace, so that it can be used in a way as similar as possible to
    `pairs` from the point of view of developers, we additionally expose it
    via the `luarules\system.lua` and `luaui\system.lua` global registers.

--- common/tablefunctions.lua
+++ common/tablefunctions.lua
@@ -1,3 +1,9 @@
+--[[
+IMPORTANT NOTICE: Tests for these functions are provided via
+`common/tableFunctionsTests.lua`, but the tests do not run unless you uncomment
+them in `init.lua` (because they're not free to run, so we don't want them to
+run for end users.)
+]]
 
 if not table.copy then
 	function table:copy()
@@ -14,19 +20,23 @@ if not table.copy then
 end
 
 if not table.merge then
-	-- Recursively merge two tables and return the result in a new table.
-	-- When there is a conflict, values in 'secondary' override values in 'primary'.
-	function table.merge(primary, secondary)
-		local new = table.copy(primary)
-		for key, v in pairs(secondary) do
+	---Return a new table of values from mergeData recursively merged into
+	---mergeTarget, using deep copies. When there is a conflict, values in
+	---mergeData take precedence.
+	---@param mergeTarget table
+	---@param mergeData table
+	---@return table
+	function table.merge(mergeTarget, mergeData)
+		local new = table.copy(mergeTarget)
+		for key, value in pairs(mergeData) do
 			-- key not used in default, assign it the value at same key in override
-			if not new[key] and type(v) == "table" then
-				new[key] = table.copy(v)
+			if not new[key] and type(value) == "table" then
+				new[key] = table.copy(value)
 				-- values at key in both default and override are tables, merge those
-			elseif type(new[key]) == "table" and type(v) == "table"  then
-				new[key] = table.merge(new[key], v)
+			elseif type(new[key]) == "table" and type(value) == "table" then
+				new[key] = table.merge(new[key], value)
 			else
-				new[key] = v
+				new[key] = value
 			end
 		end
 		return new
@@ -34,30 +44,24 @@ if not table.merge then
 end
 
 if not table.mergeInPlace then
-	-- Recursively merge values, mutating the table 'mergeTarget'.
-	-- When there is a conflict, values in 'mergeData' take precedence.
+	---Recursively in-place merge values from mergeData into mergeTarget. When
+	---there is a conflict, values in mergeData take precedence.
+	---@param mergeTarget table
+	---@param mergeData table
+	---@param deep? boolean if true, deep copy tables coming from mergeData (default: false)
+	---@return table mergeTarget
 	function table.mergeInPlace(mergeTarget, mergeData, deep)
-		if not deep then
-			local mergedData = table.merge(mergeTarget, mergeData)
-
-			for key, value in pairs(mergedData) do
+		deep = deep or false
+		for key, value in pairs(mergeData) do
+			if type(value) == 'table' and type(mergeTarget[key] or false) == 'table' then
+				table.mergeInPlace(mergeTarget[key], value, deep)
+			elseif type(value) == "table" and deep then
+				mergeTarget[key] = table.copy(value)
+			else
 				mergeTarget[key] = value
 			end
-
-			return
 		end
-
-		for i, v in pairs(mergeData) do
-			if mergeTarget[i] and type(mergeTarget[i]) == "table" and type(v) == "table"  then
-				table.mergeInPlace(mergeTarget[i], v, deep)
-			else
-				if type(v) == "table" then
-					mergeTarget[i] = table.copy(v)
-				else
-					mergeTarget[i] = v
-				end
-			end
-		end
+		return mergeTarget
 	end
 end
 
@@ -79,7 +83,7 @@ if not table.toString then
 		elseif dataType == "table" then
 			local str
 			if key then
-				str = key ..  "={"
+				str = key .. "={"
 			else
 				str = "{"
 			end
@@ -104,11 +108,151 @@ if not table.invert then
 	end
 end
 
-
 if not table.append then
 	function table.append(appendTarget, appendData)
 		for _, value in pairs(appendData) do
 			table.insert(appendTarget, value)
 		end
+	end
+end
+
+if not table.count then
+	---Count the number of values in table.
+	---Note that this always works, whereas the default length operator (#table)
+	---only works if the table is a Lua sequence (i.e. indexes form a contiguous
+	---sequence starting from 1).
+	---@param tbl table
+	---@return integer
+	function table.count(tbl)
+		local count = 0
+		for _ in pairs(tbl) do
+			count = count + 1
+		end
+		return count
+	end
+end
+
+if not table.getKeyOf then
+	---Find key of value in table.
+	---Will always return the first key found, no matter if the table contains
+	---multiple instances of the value.
+	---@generic K, V
+	---@param tbl table<K, V>
+	---@param value V
+	---@return K? # key if found, nil otherwise
+	function table.getKeyOf(tbl, value)
+		for key, v in pairs(tbl) do
+			if v == value then
+				return key
+			end
+		end
+		return nil
+	end
+end
+
+if not table.contains then
+	---Check if value is in table.
+	---@generic V
+	---@param tbl table<any, V>
+	---@param value V
+	---@return boolean
+	function table.contains(tbl, value)
+		return table.getKeyOf(tbl, value) ~= nil
+	end
+end
+
+if not table.removeIf then
+	---Remove values in table if they match the given predicate.
+	---@generic V
+	---@param tbl table<any, V>
+	---@param predicate fun(value: V): boolean
+	function table.removeIf(tbl, predicate)
+		for key, value in pairs(tbl) do
+			if predicate(value) then
+				tbl[key] = nil
+			end
+		end
+	end
+end
+
+if not table.removeAll then
+	---Remove all instances of value in table.
+	---@generic V
+	---@param tbl table<any, V>
+	---@param value V
+	function table.removeAll(tbl, value)
+		table.removeIf(tbl, function(v) return v == value end)
+	end
+end
+
+if not table.removeFirst then
+	---Remove first instance of value in table.
+	---If table is a Lua sequence (i.e. indexes form a contiguous sequence
+	---starting from 1), it will use `table.remove` to keep the sequence
+	---contiguous, otherwise it will `nil` the instance.
+	---@generic V
+	---@param tbl V[]|table<any, V>
+	---@param value V
+	---@return boolean # true if a value was removed, false otherwise
+	function table.removeFirst(tbl, value)
+		-- first, try to handle the table as a proper Lua sequence
+		-- this will fail as soon as there's a gap (missing integer index), but if
+		-- the table is a sequence then we want to keep that property by using
+		-- `table.remove` to keep the sequence intact without any gaps
+		for index, v in ipairs(tbl) do
+			if v == value then
+				table.remove(tbl, index)
+				return true
+			end
+		end
+
+		-- otherwise, try to handle the table normally and simply `nil` the value
+		local found = table.getKeyOf(tbl, value)
+		if found ~= nil then
+			tbl[found] = nil
+			return true
+		end
+
+		return false
+	end
+end
+
+if not table.shuffle then
+	---Shuffle sequence using Knuth (Fisher–Yates) algorithm.
+	---@param sequence any[] must be a Lua sequence (i.e. indexes form a contiguous sequence starting from 1), with the exception that we optionally allow starting from 0
+	---@param firstIndex? 0|1 first index in the sequence (optional, default: 1)
+	function table.shuffle(sequence, firstIndex)
+		firstIndex = firstIndex or 1
+		for i = firstIndex, #sequence - 2 + firstIndex do
+			local j = math.random(i, #sequence)
+			sequence[i], sequence[j] = sequence[j], sequence[i]
+		end
+	end
+end
+
+if not pairsByKeys then
+	---pairs-like iterator function traversing the table in the order of its keys.
+	---Natural sort order will be used by default, optionally pass a comparator
+	---function for custom sorting.
+	---@generic K, V
+	---@param tbl table<K, V>
+	---@param keySortFunction? fun(a: K, b: K): boolean comparator function passed to table.sort for sorting keys
+	---@return fun(table: table<K, V>, index?: K): K, V
+	---@return table<K, V>
+	---(Implementation copied straight from the docs at https://www.lua.org/pil/19.3.html.)
+	function pairsByKeys(tbl, keySortFunction)
+		local keys = {}
+		for key in pairs(tbl) do table.insert(keys, key) end
+		table.sort(keys, keySortFunction)
+		local i = 0           -- iterator variable
+		local iter = function() -- iterator function
+			i = i + 1
+			if keys[i] == nil then
+				return nil
+			else
+				return keys[i], tbl[keys[i]]
+			end
+		end
+		return iter
 	end
 end
