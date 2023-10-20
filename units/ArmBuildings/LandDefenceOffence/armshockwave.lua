dfe487605ea8c04ba0661614859926bb1500e1cd
Hornet <37483246+TheSilverHornet@users.noreply.github.com>
2023-10-21 00:43:03 +0100

    HP update

--- units/ArmBuildings/LandDefenceOffence/armshockwave.lua
+++ units/ArmBuildings/LandDefenceOffence/armshockwave.lua
@@ -25,7 +25,7 @@ return {
 		icontype = "building",
 		idleautoheal = 5,
 		idletime = 1800,
-		maxdamage = 2800,
+		maxdamage = 4200,
 		maxslope = 30,
 		maxwaterdepth = 20,
 		metalstorage = 600,
