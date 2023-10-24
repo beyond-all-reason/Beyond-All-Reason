9ad9048957efa8492b1ba183cfacd00cfdea900a
Hornet <37483246+TheSilverHornet@users.noreply.github.com>
2023-10-24 18:47:56 +0100

    Issue fixed, and scopes seperated

--- luarules/gadgets/unit_paralyze_damage_limit.lua
+++ luarules/gadgets/unit_paralyze_damage_limit.lua
@@ -28,30 +28,33 @@ local excluded = {
 	[UnitDefNames.armantiship.id] = true,
 	[UnitDefNames.corantiship.id] = true,
 }
+
+
 local isBuilding = {}
-local unitOhms = {}
+local unitOhms = {} -- rework related
+
 for udid, ud in pairs(UnitDefs) do
+	for id, v in pairs(excluded) do
+		if string.find(ud.name, UnitDefs[id].name) then
+			excluded[udid] = v
+		end
+		if ud.isBuilding then
+			isBuilding[udid] = true
+		end
+	end
 
-    for id, v in pairs(excluded) do
-        if string.find(ud.name, UnitDefs[id].name) then
-            excluded[udid] = v
-        end
-        if ud.isBuilding then
-            isBuilding[udid] = true
-        end
-    end
-
-
+	
 	if Spring.GetModOptions().emprework==true then
 		unitOhms[udid] = ud.customParams.paralyzemultiplier or 1
 		--Spring.Echo('ohm', ud.customParams.paralyzemultiplier)
 		if tonumber(ud.customParams.paralyzemultiplier) or 0 > 0 then
 		--Spring.Echo('ohm', ud.name, ud.customParams.paralyzemultiplier)
 		end
-		
-	end
-
+	end			
+	
 end
+
+	
 
 local weaponParalyzeDamageTime = {}
 for weaponDefID, def in pairs(WeaponDefs) do
@@ -61,31 +64,45 @@ end
 local spGetUnitHealth = Spring.GetUnitHealth
 
 
-		Spring.Echo('hornet debug emp loaded')
+--Spring.Echo('hornet debug emp loaded')
 --Spring.Debug.TableEcho(unitOhms)
 function gadget:UnitPreDamaged(uID, uDefID, uTeam, damage, paralyzer, weaponID, projID, aID, aDefID, aTeam)
 
 
     if paralyzer then
+--Spring.Debug.TableEcho(isBuilding)
 --Spring.Echo('hornet debug here2')
         -- restrict the max paralysis time of mobile units
         if aDefID and uDefID and weaponID and not isBuilding[uDefID] and not excluded[uDefID] then
-		Spring.Echo('hornet debug emp')
+		--Spring.Echo('hornet debug emp')
+			
 			
 			local hp, maxHP, currentEmp = spGetUnitHealth(uID)
 			local effectiveHP = Game.paralyzeOnMaxHealth and maxHP or hp
 			local paralyzeDeclineRate = Game.paralyzeDeclineRate
-			
+			local thismaxtime = 0
+			local ohm = 0
+			if Spring.GetModOptions().emprework==true then
+				ohm = unitOhms[uDefID]--	 or 0)) <= 0 and 0.6 or unitOhms[uDefID]
+				-- if default resistance, maxstun cap slightly lowered
+				-- if nondefault, max stun affected by resistance
+				-- as drain is static this is only way to limit that variably, which is needed for T3 impact of EMP.
+				--T3s now have slight weakness to EMP & can be stunned a little longer, which offsets the buff they get from high HP pool x the increased drain rate (without this, continuous sources are at huge disadvantage vs T3 units.)
 
-			local ohm = unitOhms[uDefID]--	 or 0)) <= 0 and 0.6 or unitOhms[uDefID]
-			-- if default resistance, maxstun cap slightly lowered
-			-- if nondefault, max stun affected by resistance, as drain is static this is only way to limit that variably, which is needed for t3 impact of EMP
-			local thismaxtime = weaponParalyzeDamageTime[weaponID] * ((ohm == 1 and 0.75) or ohm)
+				--Spring.Echo('ohm',ohm)
+				--nts, useless without raised caps on cont sources, mothball pending current playtests
+				--if ((0.01+ohm)>1) then --type coerce
+					--ohm = ohm * 1.4 -- with 1.4, an EMP resistance of say 1.2 gains EMP only slightly faster but has nearly double max charge capacity relative to lesser units
+				--end
+				thismaxtime = weaponParalyzeDamageTime[weaponID] * ((ohm == 1 and 0.85) or ohm)
+			else	
+				thismaxtime = weaponParalyzeDamageTime[weaponID]
+			end
 			
-			Spring.Echo('raw stuntime, ohm, using mult value, thismaxtime (pre-minumum)', weaponParalyzeDamageTime[weaponID], ohm, ((ohm == 1 and 0.5) or ohm), thismaxtime)
+			--Spring.Echo('raw stuntime, ohm, using mult value, thismaxtime (pre-minumum)', weaponParalyzeDamageTime[weaponID], ohm, ((ohm == 1 and 0.5) or ohm), thismaxtime)
 			--thismaxtime = math.max(1, thismaxtime)--prevent microstuns (compounds oddly with shuri unfortunately)
 			
-			
+			--still obey the hard global cap though
 			thismaxtime = math.min(maxTime, thismaxtime)
 			--Spring.Echo('times', weaponParalyzeDamageTime[weaponID], thismaxtime, unitOhms[uDefID] or 1)
 			
@@ -93,14 +110,19 @@ function gadget:UnitPreDamaged(uID, uDefID, uTeam, damage, paralyzer, weaponID, 
 			local maxEmpDamage = (1 + (thismaxtime / paralyzeDeclineRate)) * effectiveHP
 
 			newdamage = math.max(0, math.min(damage, maxEmpDamage - currentEmp))
-            Spring.Echo('h mh ph wpt old new',hp,maxHP, currentEmp, thismaxtime, damage, newdamage)
+			--Spring.Echo('h mh ph wpt old new',hp,maxHP, currentEmp, thismaxtime, damage, newdamage)
 
-            Spring.Echo(Game.paralyzeDeclineRate)
-            Spring.Echo(Game.paralyzeOnMaxHealth)
+			--Spring.Echo(Game.paralyzeDeclineRate)
+			--Spring.Echo(Game.paralyzeOnMaxHealth)
 			damage = newdamage
 			--Spring.Debug.TableEcho(Game)
-            --damage = mh +6 
-            --Spring.Echo('new',h,mh, ph, max_para_damage, max_para_time, damage)
+			--damage = mh +6 
+			--Spring.Echo('new',h,mh, ph, max_para_damage, max_para_time, damage)
+				
+			
+			
+			
+			
         end
         return damage, 1
     end
