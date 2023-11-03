ae1eb28fc2fef4377926d9a8ed71b322a8447d9d
Amadeus Folego <amadeusfolego@gmail.com>
2023-11-02 23:04:23 -0300

    Lint and autoformat

--- luarules/gadgets/cmd_factory_stop_production.lua
+++ luarules/gadgets/cmd_factory_stop_production.lua
@@ -3,29 +3,29 @@
 
 function gadget:GetInfo()
 	return {
-		name      = "Factory Stop Production",
-		desc      = "Adds a command to clear the factory queue",
-		author    = "GoogleFrog",
-		date      = "13 November 2016",
-		license   = "GNU GPL, v2 or later",
-		layer     = 0,
-		enabled   = true
+		name = "Factory Stop Production",
+		desc = "Adds a command to clear the factory queue",
+		author = "GoogleFrog",
+		date = "13 November 2016",
+		license = "GNU GPL, v2 or later",
+		layer = 0,
+		enabled = true,
 	}
 end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-if (not gadgetHandler:IsSyncedCode()) then
-  return false  --  no unsynced code
+if not gadgetHandler:IsSyncedCode() then
+	return false --  no unsynced code
 end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 local spGetFactoryCommands = Spring.GetFactoryCommands
-local spGiveOrderToUnit    = Spring.GiveOrderToUnit
-local spInsertUnitCmdDesc  = Spring.InsertUnitCmdDesc
+local spGiveOrderToUnit = Spring.GiveOrderToUnit
+local spInsertUnitCmdDesc = Spring.InsertUnitCmdDesc
 
 local CMD_OPT_CTRL = CMD.OPT_CTRL
 local CMD_REMOVE = CMD.REMOVE
@@ -46,12 +46,12 @@ end
 --------------------------------------------------------------------------------
 
 local stopProductionCmdDesc = {
-	id      = CMD_STOP_PRODUCTION,
-	type    = CMDTYPE.ICON,
-	name    = 'Stop Production',
-	action  = 'stopproduction',
-	cursor  = 'Stop', -- Probably does nothing
-	tooltip = 'Stop Production: Clear factory production queue.',
+	id = CMD_STOP_PRODUCTION,
+	type = CMDTYPE.ICON,
+	name = "Stop Production",
+	action = "stopproduction",
+	cursor = "Stop", -- Probably does nothing
+	tooltip = "Stop Production: Clear factory production queue.",
 }
 
 --------------------------------------------------------------------------------
@@ -59,7 +59,7 @@ local stopProductionCmdDesc = {
 -- Handle the command
 
 function gadget:AllowCommand_GetWantedCommand()
-	return {[CMD_STOP_PRODUCTION] = true}
+	return { [CMD_STOP_PRODUCTION] = true }
 end
 
 function gadget:AllowCommand_GetWantedUnitDefID()
@@ -67,7 +67,7 @@ function gadget:AllowCommand_GetWantedUnitDefID()
 end
 
 function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
-	if (cmdID ~= CMD_STOP_PRODUCTION) or (not isFactory[unitDefID]) then
+	if (cmdID ~= CMD_STOP_PRODUCTION) or not isFactory[unitDefID] then
 		return true
 	end
 
@@ -75,6 +75,7 @@ function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOpt
 	if not commands then
 		return
 	end
+
 	for i = 1, #commands do
 		spGiveOrderToUnit(unitID, CMD_REMOVE, commands[i].tag, CMD_OPT_CTRL)
 	end
