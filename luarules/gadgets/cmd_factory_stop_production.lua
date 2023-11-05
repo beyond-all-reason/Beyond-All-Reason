887f082a7fcd3986a298e441e020b42188f7705e
Amadeus Folego <amadeusfolego@gmail.com>
2023-11-05 06:28:44 -0300

    Fix calling stopproduction on empty queue and adjust for repeat

--- luarules/gadgets/cmd_factory_stop_production.lua
+++ luarules/gadgets/cmd_factory_stop_production.lua
@@ -26,6 +26,7 @@ end
 local spGetFactoryCommands = Spring.GetFactoryCommands
 local spGiveOrderToUnit = Spring.GiveOrderToUnit
 local spInsertUnitCmdDesc = Spring.InsertUnitCmdDesc
+local spGetUnitStates = Spring.GetUnitStates
 
 local CMD_OPT_CTRL = CMD.OPT_CTRL
 local CMD_REMOVE = CMD.REMOVE
@@ -52,6 +53,8 @@ local stopProductionCmdDesc = {
 	action = "stopproduction",
 	cursor = "Stop", -- Probably does nothing
 	tooltip = "Stop Production: Clear factory production queue.",
+	hidden = false,
+	queueing = false,
 }
 
 --------------------------------------------------------------------------------
@@ -66,24 +69,40 @@ function gadget:AllowCommand_GetWantedUnitDefID()
 	return isFactory
 end
 
-function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
+function gadget:AllowCommand(unitID, unitDefID, _, cmdID)
 	if (cmdID ~= CMD_STOP_PRODUCTION) or not isFactory[unitDefID] then
 		return true
 	end
 
 	local commands = spGetFactoryCommands(unitID, -1)
-	if not commands then
+
+	if not commands or #commands == 0 then
 		return
 	end
 
-	local startIndex = math.min(2, #commands) -- We want to remove all except first command, unless only one command
+	-- If:
+	-- - Repeat is on: clear the full queue (we can't remove the command and make it an altenqueue without losing build progress)
+	-- - Repeat is off:
+	--   - If 1 command only, clear it
+	--   - If more than 1 command, clear all except first (allow buildprogress to finish)
+	--
+	-- Remark: We can make repeat consistent with no repeat, at the cost of
+	-- adding state to track when the unit was finished after issuing the clear
+	-- queue except first cmd and clearing, however I think this is an issue
+	-- better handled in engine (we should be able to change a cmd from non-alt
+	-- to alt)
+	local startIndex = select(4, spGetUnitStates(unitID, false, true)) and 1 or math.min(2, #commands)
 
 	for i = startIndex, #commands do
 		spGiveOrderToUnit(unitID, CMD_REMOVE, commands[i].tag, CMD_OPT_CTRL)
 	end
-	spGiveOrderToUnit(unitID, CMD_WAIT, EMPTY, 0) -- Removes wait if there is a wait but doesn't readd it.
-	spGiveOrderToUnit(unitID, CMD_WAIT, EMPTY, 0) -- If a factory is waiting, it will not clear the current build command, even if the cmd is removed.
-	-- See: http://zero-k.info/Forum/Post/237176#237176 for details.
+
+	-- Removed all, remove wait
+	if startIndex == 1 then
+		spGiveOrderToUnit(unitID, CMD_WAIT, EMPTY, 0) -- Removes wait if there is a wait but doesn't readd it.
+		spGiveOrderToUnit(unitID, CMD_WAIT, EMPTY, 0) -- If a factory is waiting, it will not clear the current build command, even if the cmd is removed.
+		-- See: http://zero-k.info/Forum/Post/237176#237176 for details.
+	end
 end
 
 --------------------------------------------------------------------------------
