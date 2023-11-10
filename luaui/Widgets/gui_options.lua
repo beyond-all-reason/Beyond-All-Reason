b2ddd53116cf233b48951e0b85abd0a3b7595871
Amadeus Folego <amadeusfolego@gmail.com>
2023-11-10 11:59:34 -0300

    feat(gridmenu): expose option to return to categories on assign (#2217)
    On b825b881b5bd940914aa6986860f57c7fd8fbe9e the default was changed to
    always return to categories when queueing commands after assign.
    
    This is not the behavior in other RTSs with grid commands.
    
    This commit exposes the setting

--- luaui/Widgets/gui_options.lua
+++ luaui/Widgets/gui_options.lua
@@ -2926,6 +2926,13 @@ function init()
 			  end
 		  end,
 		},
+		{ id = "gridmenu_alwaysreturn", group = "control", category = types.advanced, name = Spring.I18N('ui.settings.option.gridmenu_alwaysreturn'), type = "bool", value = (WG['buildmenu'] ~= nil and WG['buildmenu'].getShowPrice ~= nil and WG['buildmenu'].getShowPrice()), description = Spring.I18N('ui.settings.option.gridmenu_alwaysreturn_descr'),
+		  onload = function()
+		  end,
+		  onchange = function(_, value)
+			  saveOptionValue('Grid menu', 'gridmenu', 'setAlwaysReturn', { 'alwaysReturn' }, value)
+		  end,
+		},
 
 
 		{ id = "label_ui_cursor", group = "control", name = Spring.I18N('ui.settings.option.label_cursor'), category = types.basic },
