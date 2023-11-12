eb28b9ff7df0999bcdbc966c1d74429f0c01f6a1
Amadeus Folego <amadeusfolego@gmail.com>
2023-11-12 20:19:13 -0300

    fix(gridmenu): alwaysReturn setting not being saved properly
    Made a few mistakes while naming the setting and porting the option

--- luaui/Widgets/gui_gridmenu.lua
+++ luaui/Widgets/gui_gridmenu.lua
@@ -22,7 +22,7 @@ VFS.Include("luarules/configs/customcmds.h.lua")
 
 SYMKEYS = table.invert(KEYSYMS)
 
-local alwaysReturn = true
+local alwaysReturn = false
 
 local keyConfig = VFS.Include("luaui/configs/keyboard_layouts.lua")
 local currentLayout = Spring.GetConfigString("KeyboardLayout", "qwerty")
@@ -2154,7 +2154,7 @@ end
 
 function widget:GetConfigData()
 	return {
-		returnToCategoriesOnPick = returnToCategoriesOnPick,
+		alwaysReturn = alwaysReturn,
 		showPrice = showPrice,
 		showRadarIcon = showRadarIcon,
 		showGroupIcon = showGroupIcon,
@@ -2165,8 +2165,8 @@ function widget:GetConfigData()
 end
 
 function widget:SetConfigData(data)
-	if data.returnToCategoriesOnPick ~= nil then
-		returnToCategoriesOnPick = data.returnToCategoriesOnPick
+	if data.alwaysReturn ~= nil then
+		alwaysReturn = data.alwaysReturn
 	end
 	if data.showPrice ~= nil then
 		showPrice = data.showPrice
