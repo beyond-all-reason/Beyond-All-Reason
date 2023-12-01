4bff7832464fc1aae52c34bc29f48816ec6944bc
Jazcash <Jazcash@users.noreply.github.com>
2023-12-01 18:32:03 +0000

    preserve commands after transport when commands are within 400 dist

--- luaui/Widgets/cmd_transport_preserve_commands.lua
+++ luaui/Widgets/cmd_transport_preserve_commands.lua
@@ -1,16 +1,17 @@
 function widget:GetInfo()
     return {
-        name	= "Preserve Commands After Transport",
-        desc	= "Preserves a unit's build queue after it has been transported, ignores non-build commands",
+        name	= "Preserve Commands",
+        desc	= "Preserves a unit's command queue after it has been transported",
         author  = "Jazcash",
         date 	= "October 2023",
         license	= "idklmao",
         layer 	= 0,
-        enabled	= true
+        enabled	= false
     }
 end
 
 local orders = {}
+local distToIgnore = 400 -- any initial commands outside of this distance from the unload point will be ignored, to stop units walking back to their origin
 
 function widget:Initialize()
     if Spring.IsReplay() then
@@ -25,13 +26,19 @@ end
 function widget:UnitUnloaded(unitID, unitDefID, unitTeam, transportID, transportTeam)
     if (orders[unitID] and #orders[unitID]) then
         local newOrders = {}
-        local firstBuildCmd = false
+        local cmdInsideDist = false
 
         for i, command in ipairs(orders[unitID]) do
-            if (command.id < 0 and firstBuildCmd == false) then
-                firstBuildCmd = true
+            if (#command.params >= 3) then
+                local x, y, z = Spring.GetUnitPosition(unitID)
+                local unloadedPos = { x = x, y = y, z = z }
+                local cmdPos = { x = command.params[1], y = command.params[2], z = command.params[3] }
+                local distFromCmd = distance(unloadedPos, cmdPos)
+                if (distFromCmd <= distToIgnore) then
+                    cmdInsideDist = true
+                end
             end
-            if (firstBuildCmd == true) then
+            if (cmdInsideDist == true) then
                 table.insert(newOrders, {command.id, command.params, command.options})
             end
         end
@@ -40,4 +47,11 @@ function widget:UnitUnloaded(unitID, unitDefID, unitTeam, transportID, transport
 
         orders[unitID] = nil
     end
+end
+
+function distance(pos1, pos2)
+	local xd = pos1.x - pos2.x
+	local yd = pos1.z - pos2.z
+	local dist = math.sqrt(xd*xd + yd*yd)
+	return dist
 end
