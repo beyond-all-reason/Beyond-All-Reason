fc3ee0a30d0ffbc6dda4129f290f9cedf74050af
Citrine <salinecitrine@gmail.com>
2024-01-11 18:20:41 -0800

    Add summary of serpent.lua
    Co-authored-by: sprunk <spr.ng@o2.pl>

--- common/luaUtilities/serpent.lua
+++ common/luaUtilities/serpent.lua
@@ -1,3 +1,4 @@
+-- provides a consistent interface for de/serialisation
 local n, v = "serpent", "0.303" -- (C) 2012-18 Paul Kulchenko; MIT License
 local c, d = "Paul Kulchenko", "Lua serializer and pretty printer"
 local snum = {[tostring(1/0)]='1/0 --[[math.huge]]',[tostring(-1/0)]='-1/0 --[[-math.huge]]',[tostring(0/0)]='0/0'}
