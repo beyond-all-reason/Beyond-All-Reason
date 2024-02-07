5c2a201ddfe74a33c5a31df4f5e12aef264093aa
autolumn <89279737+autolumn@users.noreply.github.com>
2024-02-07 10:59:54 +0100

    feat(gadgets): raptor aggro leaderboard - fix to work as standalone widget

--- luaui/Widgets/gui_raptorStatsPanel.lua
+++ luaui/Widgets/gui_raptorStatsPanel.lua
@@ -10,29 +10,117 @@ function widget:GetInfo()
 	}
 end
 
-local useWaveMsg			= VFS.Include('LuaRules/Configs/raptor_spawn_defs.lua').useWaveMsg
-local RaptorCommon 			= VFS.Include('LuaRules/gadgets/raptors/common.lua')
+local useWaveMsg        = VFS.Include('LuaRules/Configs/raptor_spawn_defs.lua').useWaveMsg
+local Set               = VFS.Include('common/SetList.lua').NewSetListMin
 
-local DiffTimers            = Spring.DiffTimers
-local GetAIInfo             = Spring.GetAIInfo
-local GetAllUnits           = Spring.GetAllUnits
-local GetGaiaTeamID         = Spring.GetGaiaTeamID
-local GetGameRulesParam     = Spring.GetGameRulesParam
-local GetGameSeconds        = Spring.GetGameSeconds
-local GetModOptions         = Spring.GetModOptions
-local GetMyTeamID           = Spring.GetMyTeamID
-local GetPlayerInfo         = Spring.GetPlayerInfo
-local GetPlayerList         = Spring.GetPlayerList
-local GetTeamList           = Spring.GetTeamList
-local GetTeamLuaAI          = Spring.GetTeamLuaAI
-local GetTimer              = Spring.GetTimer
-local GetUnitDefID          = Spring.GetUnitDefID
-local GetUnitTeam           = Spring.GetUnitTeam
-local GetViewGeometry       = Spring.GetViewGeometry
-local I18N                  = Spring.I18N
-local SendCommands          = Spring.SendCommands
-local UnitDefs              = UnitDefs
-local Utilities             = Spring.Utilities
+local DiffTimers        = Spring.DiffTimers
+local GetAIInfo         = Spring.GetAIInfo
+local GetAllUnits       = Spring.GetAllUnits
+local GetGaiaTeamID     = Spring.GetGaiaTeamID
+local GetGameRulesParam = Spring.GetGameRulesParam
+local GetGameSeconds    = Spring.GetGameSeconds
+local GetModOptions     = Spring.GetModOptions
+local GetMyTeamID       = Spring.GetMyTeamID
+local GetPlayerInfo     = Spring.GetPlayerInfo
+local GetPlayerList     = Spring.GetPlayerList
+local GetTeamList       = Spring.GetTeamList
+local GetTeamLuaAI      = Spring.GetTeamLuaAI
+local GetTimer          = Spring.GetTimer
+local GetUnitDefID      = Spring.GetUnitDefID
+local GetUnitTeam       = Spring.GetUnitTeam
+local GetViewGeometry   = Spring.GetViewGeometry
+local I18N              = Spring.I18N
+local SendCommands      = Spring.SendCommands
+local UnitDefs          = UnitDefs
+local Utilities         = Spring.Utilities
+
+-- to be deleted pending PR #2572
+local WALLS             = Set()
+WALLS:Add("armdrag")
+WALLS:Add("armfort")
+WALLS:Add("cordrag")
+WALLS:Add("corfort")
+WALLS:Add("scavdrag")
+WALLS:Add("scavfort")
+local raptorTeamID
+local teams = GetTeamList()
+for _, teamID in ipairs(teams) do
+	local teamLuaAI = GetTeamLuaAI(teamID)
+	if (teamLuaAI and string.find(teamLuaAI, "Raptors")) then
+		raptorTeamID = teamID
+	end
+end
+if not raptorTeamID then
+	raptorTeamID = GetGaiaTeamID()
+end
+
+-- Calculate an eco value based on energy and metal production
+-- Echo("Built units eco value: " .. ecoValue)
+
+-- Ends up building an object like:
+-- {
+--  0: [non-eco]
+--	25: [t1 windmill, t1 solar, t1 mex],
+--	75: [adv solar]
+--	1000: [fusion]
+--	3000: [adv fusion]
+-- }
+local function EcoValueDef(unitDef)
+	local ecoValue = 1
+	if unitDef.energyMake then
+		ecoValue = ecoValue + unitDef.energyMake
+	end
+	if unitDef.energyUpkeep and unitDef.energyUpkeep < 0 then
+		ecoValue = ecoValue - unitDef.energyUpkeep
+	end
+	if unitDef.windGenerator then
+		ecoValue = ecoValue + unitDef.windGenerator * 0.75
+	end
+	if unitDef.tidalGenerator then
+		ecoValue = ecoValue + unitDef.tidalGenerator * 15
+	end
+	if unitDef.extractsMetal and unitDef.extractsMetal > 0 then
+		ecoValue = ecoValue + 200
+	end
+	if unitDef.customParams and unitDef.customParams.energyconv_capacity then
+		ecoValue = ecoValue + tonumber(unitDef.customParams.energyconv_capacity) / 2
+	end
+
+	-- Decoy fusion support
+	if unitDef.customParams and unitDef.customParams.decoyfor == "armfus" then
+		ecoValue = ecoValue + 1000
+	end
+
+	-- Make it extra risky to build T2 eco
+	if unitDef.customParams and unitDef.customParams.techlevel and tonumber(unitDef.customParams.techlevel) > 1 then
+		ecoValue = ecoValue * tonumber(unitDef.customParams.techlevel) * 2
+	end
+
+	-- Anti-nuke - add value to force players to go T2 economy, rather than staying T1
+	if unitDef.customParams and (unitDef.customParams.unitgroup == "antinuke" or unitDef.customParams.unitgroup == "nuke") then
+		ecoValue = 1000
+	end
+
+	return ecoValue
+end
+
+local function IsValidEcoUnitDef(unitDef, teamID)
+	-- skip Raptor AI, moving units and player built walls
+	if teamID == raptorTeamID or unitDef.canMove or WALLS.hash[unitDef.name] ~= nil then
+		return false
+	end
+	return true
+end
+
+local RaptorCommon
+if io.open('LuaRules/gadgets/raptors/common.lua', "r") == nil then
+	RaptorCommon = {
+		EcoValueDef       = EcoValueDef,
+		IsValidEcoUnitDef = IsValidEcoUnitDef
+	}
+else
+	RaptorCommon = VFS.Include('LuaRules/gadgets/raptors/common.lua')
+end
 
 local customScale           = 1
 local widgetScale           = customScale
@@ -97,25 +185,6 @@ local rules                     = {
 	"lagging",
 	"raptorDifficulty",
 }
--- to be deleted pending PR #2572
-local WALLS                     = Set()
-WALLS:Add("armdrag")
-WALLS:Add("armfort")
-WALLS:Add("cordrag")
-WALLS:Add("corfort")
-WALLS:Add("scavdrag")
-WALLS:Add("scavfort")
-local raptorTeamID
-local teams = GetTeamList()
-for _, teamID in ipairs(teams) do
-	local teamLuaAI = GetTeamLuaAI(teamID)
-	if (teamLuaAI and string.find(teamLuaAI, "Raptors")) then
-		raptorTeamID = teamID
-	end
-end
-if not raptorTeamID then
-	raptorTeamID = GetGaiaTeamID()
-end
 
 local function updatePos(x, y)
 	local x0 = (viewSizeX * 0.94) - (w * widgetScale) / 2
@@ -141,8 +210,8 @@ local function PlayerAggroValues()
 			_, playerName = GetAIInfo(teamID)
 		end
 
-		local aggroEcoValue = playersAggroEcos[teamID]
-		if playerName and not playerName:find("Raptors") and aggroEcoValue and aggroEcoValue > 0 then
+		local aggroEcoValue = playersAggroEcos[teamID] or 0
+		if playerName and not playerName:find("Raptors") then
 			sum = sum + aggroEcoValue
 			table.insert(playerAggros, {
 				value = aggroEcoValue,
@@ -537,7 +606,6 @@ function widget:LanguageChanged()
 	refreshMarqueeMessage = true
 	updatePanel = true
 end
-
 
 local function RegisterUnit(unitID, unitDefID, unitTeam)
 	local unitDef = UnitDefs[unitDefID]
