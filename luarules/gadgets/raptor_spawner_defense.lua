3cb75e327d644e77eaec9b89e46f5c042bc21aef
autolumn <89279737+autolumn@users.noreply.github.com>
2024-02-22 19:57:46 +0100

    feat(gadgets): raptor aggro leaderboard - gadget use table also

--- luarules/gadgets/raptor_spawner_defense.lua
+++ luarules/gadgets/raptor_spawner_defense.lua
@@ -1268,7 +1268,7 @@ if gadgetHandler:IsSyncedCode() then
 
 		if not unitDef.canMove then
 			-- Calculate an eco value based on energy and metal production
-			local ecoValue = RaptorCommon.EcoValueDef(unitDef)
+			local ecoValue = RaptorCommon.defIDsEcoValues[unitDefID]
 			-- Spring.Echo("Built units eco value: " .. ecoValue)
 
 			-- Ends up building an object like:
