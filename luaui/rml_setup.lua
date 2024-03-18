d5392d0f453616ee6e5b179e0b40f5a8934e8cf0
ChrisClark13 <krizasan@gmail.com>
2024-03-17 20:32:11 -0600

    fix up example widgets

--- luaui/rml_setup.lua
+++ luaui/rml_setup.lua
@@ -1,14 +1,24 @@
 --  file:    rml.lua
 --  brief:   RmlUi Setup
---  author:  lov
+--  author:  lov + ChrisFloofyKitsune
 --
---  Copyright (C) 2023.
+--  Copyright (C) 2024.
 --  Licensed under the terms of the GNU GPL, v2 or later.
 
 if (RmlGuard or not RmlUi) then
 	return
 end
+-- don't allow this initialization code to be run multiple times
 RmlGuard = true
+
+--[[
+	Recoil uses a custom set of Lua bindings (check out rts/Rml/SolLua/bind folder in the C++ engine code)
+	Aside from the Lua API, the rest of the RmlUi documentation is still relevant
+		https://mikke89.github.io/RmlUiDoc/index.html
+]]
+
+-- create a common context to be used, data models must have unique name within the same context though
+RmlUi.CreateContext("common")
 
 -- Load fonts
 RmlUi.LoadFontFace("fonts/Poppins-Regular.otf", true)
@@ -26,7 +36,7 @@ RmlUi.LoadFontFace("fonts/monospaced/SourceCodePro-Medium.otf")
 
 -- when "cursor: normal" is set via RCSS, "cursornormal" will be sent to the engine... and so on for the rest
 RmlUi.SetMouseCursorAlias("default", 'cursornormal')
-RmlUi.SetMouseCursorAlias("pointer", 'Move') -- command cursors use the command name
+RmlUi.SetMouseCursorAlias("pointer", 'Move') -- command cursors use the command name. TODO: replace with actual pointer cursor
 RmlUi.SetMouseCursorAlias("move", 'uimove')
 RmlUi.SetMouseCursorAlias("nesw-resize", 'uiresized2')
 RmlUi.SetMouseCursorAlias("nwse-resize", 'uiresized1')
