2c26ee61713c803513ccd600fcaa0d24e7ea2346
TheSilverHornet <37483246+TheSilverHornet@users.noreply.github.com>
2024-03-19 00:22:13 +0000

    Fix: Infestor and Liche gadget potential crashes (#2743)

--- luarules/gadgets/unit_infestor_spawn.lua
+++ luarules/gadgets/unit_infestor_spawn.lua
@@ -18,22 +18,29 @@ if not gadgetHandler:IsSyncedCode() then
 end
 
 
+
+local infestor = {}
+
+-- setup
 if UnitDefNames.leginfestor then
-    local infestor = {}
     infestor[UnitDefNames.leginfestor.id] = true
 
     if (UnitDefNames.leginfestor_scav) then
         infestor[UnitDefNames.leginfestor_scav.id] = true
     end
+end
 
+function gadget:Initialize()
+    if table.count(infestor) <= 0 then
+        gadgetHandler:RemoveGadget(self)
+    end
+end
 
-    function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-        if (builderID) then
-            if infestor[unitDefID] and infestor[Spring.GetUnitDefID(builderID)] then
-                Spring.GiveOrderToUnit(unitID, CMD.GUARD, { builderID }, { "shift" })
-            end
+function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+    if (builderID) then
+        if infestor[unitDefID] and infestor[Spring.GetUnitDefID(builderID)] then
+            Spring.GiveOrderToUnit(unitID, CMD.GUARD, { builderID }, { "shift" })
         end
     end
-else
-    gadgetHandler:RemoveGadget(self)
 end
+
