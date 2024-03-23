b36205e69e6b64d24706b57cf1a3715ec6532839
Damgam <c_2_damiano@op.pl>
2024-03-23 14:23:31 +0100

    some code tweaks

--- luarules/gadgets/game_boombox_spawner.lua
+++ luarules/gadgets/game_boombox_spawner.lua
@@ -20,27 +20,56 @@ local AliveBoomboxes = {}
 local SelfDQueue = {}
 local positionCheckLibrary = VFS.Include("luarules/utilities/damgam_lib/position_checks.lua")
 
-for i = 1,100 do
-    if math.random(1,10) == 1 then -- April Fools odds
-    -- if math.random(1,2137) == 1 then -- Normal Day odds
-        for j = 1,1000 do
-            local posx = math.random(math.floor(Game.mapSizeX*0.1), math.ceil(Game.mapSizeX*0.9))
-            local posz = math.random(math.floor(Game.mapSizeX*0.1), math.ceil(Game.mapSizeX*0.9))
-            local posy = Spring.GetGroundHeight(posx, posz)
-            if posy > 0 and positionCheckLibrary.FlatAreaCheck(posx, posy, posz, 64, 25, true) then
-                local boomboxID = Spring.CreateUnit("boombox", posx, posy, posz, "west", Spring.GetGaiaTeamID())
-                if boomboxID then
-                    AliveBoomboxes[boomboxID] = true
-                    break
-                end
+function getNearestBlocker(x, z)
+	local lowestDist = math.huge
+	local metalSpots = GG["resource_spot_finder"] and GG["resource_spot_finder"].metalSpotsList or nil
+	if metalSpots then
+		for i = 1, #metalSpots do
+			local spot = metalSpots[i]
+            if spot then
+			    local dx, dz = x - spot.x, z - spot.z
+			    local dist = dx * dx + dz * dz
+			    if dist < lowestDist then
+			    	lowestDist = dist
+			    end
             end
-        end
-
+		end
     end
+    local features = Spring.GetFeaturesInCylinder(x, z, 192)
+    for index, featureID in pairs(features) do
+        if FeatureDefs[Spring.GetFeatureDefID(featureID)].name == "geovent" or FeatureDefs[Spring.GetFeatureDefID(featureID)].geoThermal then
+            lowestDist = 0
+        end
+    end
+    local units = Spring.GetUnitsInCylinder(x, z, 192)
+    if #Spring.GetUnitsInCylinder(x, z, 192) > 0 then
+        lowestDist = 0
+    end
+    return math.sqrt(lowestDist)
 end
 
 function gadget:GameFrame(frame)
     if frame == 1 then
+        for i = 1,100 do
+            if math.random(1,10) == 1 then -- April Fools odds
+            -- if math.random(1,5000) == 1 then -- Normal Day odds
+                for j = 1,1000 do
+                    local posx = math.random(math.floor(Game.mapSizeX*0.02), math.ceil(Game.mapSizeX*0.98))
+                    local posz = math.random(math.floor(Game.mapSizeX*0.02), math.ceil(Game.mapSizeX*0.98))
+                    local posy = Spring.GetGroundHeight(posx, posz)
+                    local blockerDistance = getNearestBlocker(posx, posz)
+                    Spring.Echo(blockerDistance)
+                    if posy > 0 and positionCheckLibrary.FlatAreaCheck(posx, posy, posz, 64, 25, true) and blockerDistance > 196 then
+                        local boomboxID = Spring.CreateUnit("boombox", posx, posy, posz, "west", Spring.GetGaiaTeamID())
+                        if boomboxID then
+                            AliveBoomboxes[boomboxID] = true
+                            break
+                        end
+                    end
+                end
+            end
+        end
+
         for unitID, _ in pairs(AliveBoomboxes) do
             Spring.SetUnitNeutral(unitID, true)
             Spring.SetUnitStealth(unitID, true)
@@ -51,9 +80,10 @@ function gadget:GameFrame(frame)
             Spring.SetUnitSensorRadius(unitID, 'airLos', 0)
             Spring.SetUnitSensorRadius(unitID, 'radar', 0)
             Spring.SetUnitSensorRadius(unitID, 'sonar', 0)
+            Spring.SetUnitAlwaysVisible(unitID, true)
         end
     end
-    if frame%60 == 0 then
+    if frame%5 == 0 then
         for unitID, _ in pairs(AliveBoomboxes) do
             Spring.SetUnitHealth(unitID, 10000000)
         end
@@ -71,5 +101,13 @@ end
 function gadget:UnitTaken(unitID, unitDefID, oldTeam, newTeam)
     if UnitDefs[unitDefID].name == "boombox" then
         SelfDQueue[unitID] = Spring.GetGameFrame()+90
+    end
+end
+
+function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture)
+    if UnitDefs[unitDefID].name == "boombox" and oldTeam ~= Spring.GetGaiaTeamID() then
+        return false
+    else
+        return true
     end
 end
