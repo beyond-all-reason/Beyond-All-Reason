117f0ee9d856837a77b0da1ce3cff6ccc299f34e
Damgam <c_2_damiano@op.pl>
2024-03-30 18:20:16 +0100

    tweaks

--- luarules/gadgets/game_boombox_spawner.lua
+++ luarules/gadgets/game_boombox_spawner.lua
@@ -18,6 +18,16 @@ if not gadgetHandler:IsSyncedCode() then
     return
 end
 
+AprilFoolsPlayerIDs = {}
+
+AprilFoolsCounter = 0
+function gadget:RecvLuaMsg(msg, playerID)
+    if string.find(msg, "aprilfools 1") and not AprilFoolsPlayerIDs[playerID] then
+        AprilFoolsCounter = AprilFoolsCounter + 1
+        AprilFoolsPlayerIDs[playerID] = true
+    end
+end
+
 local AliveBoomboxes = {}
 local SelfDQueue = {}
 local positionCheckLibrary = VFS.Include("luarules/utilities/damgam_lib/position_checks.lua")
@@ -52,15 +62,13 @@ end
 
 function gadget:GameFrame(frame)
     if frame == 1 then
-        for i = 1,100 do
-            if math.random(1,10) == 1 then -- April Fools odds
-            -- if math.random(1,5000) == 1 then -- Normal Day odds
+        for i = 1,10*#Spring.GetTeamList() do
+            if (AprilFoolsCounter >= #Spring.GetPlayerList()*0.4 and math.random(1,10) == 1) or math.random(1,10000) == 1 then
                 for j = 1,1000 do
                     local posx = math.random(math.floor(Game.mapSizeX*0.02), math.ceil(Game.mapSizeX*0.98))
                     local posz = math.random(math.floor(Game.mapSizeX*0.02), math.ceil(Game.mapSizeX*0.98))
                     local posy = Spring.GetGroundHeight(posx, posz)
                     local blockerDistance = getNearestBlocker(posx, posz)
-                    Spring.Echo(blockerDistance)
                     if posy > 0 and positionCheckLibrary.FlatAreaCheck(posx, posy, posz, 64, 25, true) and blockerDistance > 196 then
                         local boomboxID = Spring.CreateUnit("boombox", posx, posy, posz, "west", Spring.GetGaiaTeamID())
                         if boomboxID then
@@ -113,3 +121,4 @@ function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture)
         return true
     end
 end
+
