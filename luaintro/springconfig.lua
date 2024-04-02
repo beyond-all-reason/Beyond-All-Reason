169e746bbbbae83fbbb5c910b0d1ae83f9c9ca3f
Beherith <mysterme@gmail.com>
2024-04-02 16:46:28 +0200

    Disable the 3 sim MT parts
    Spring.SetConfigInt("AnimationMT", 0)
    Spring.SetConfigInt("UpdateBoundingVolumeMT", 0)
    Spring.SetConfigInt("UpdateWeaponVectorsMT", 0)

--- luaintro/springconfig.lua
+++ luaintro/springconfig.lua
@@ -187,5 +187,7 @@ Spring.SetConfigInt("MouseDragCircleCommandThreshold", baseDragThreshold + 16)
 Spring.SetConfigInt("MouseDragBoxCommandThreshold", baseDragThreshold + 16)
 Spring.SetConfigInt("MouseDragFrontCommandThreshold", baseDragThreshold + 16)
 
--- This config int control some multithreading functionality, and are now set to their disabled state while we debug if they could be the cause of some sync errors:
+-- These config ints control some multithreading functionality, and are now set to their disabled state while we debug if they could be the cause of some sync errors:
 Spring.SetConfigInt("AnimationMT", 0)
+Spring.SetConfigInt("UpdateBoundingVolumeMT", 0)
+Spring.SetConfigInt("UpdateWeaponVectorsMT", 0)
