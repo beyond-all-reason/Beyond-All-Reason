56dba99f60f8b240260d57c26a3ba7d99541fb6e
Beherith <mysterme@gmail.com>
2024-04-02 14:54:48 +0200

    Disable AnimationMT for two weeks while we check their sync status.

--- luaintro/springconfig.lua
+++ luaintro/springconfig.lua
@@ -186,3 +186,6 @@ Spring.SetConfigInt("MouseDragSelectionThreshold", baseDragThreshold)
 Spring.SetConfigInt("MouseDragCircleCommandThreshold", baseDragThreshold + 16)
 Spring.SetConfigInt("MouseDragBoxCommandThreshold", baseDragThreshold + 16)
 Spring.SetConfigInt("MouseDragFrontCommandThreshold", baseDragThreshold + 16)
+
+-- This config int control some multithreading functionality, and are now set to their disabled state while we debug if they could be the cause of some sync errors:
+Spring.SetConfigInt("AnimationMT", 0)
