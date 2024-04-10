fdddaedf3e05045929411d252307cb0d7333dd75
Damgam <c_2_damiano@op.pl>
2024-04-10 16:30:55 +0200

    Boombox tweaks and cleanup

--- luarules/gadgets/game_boombox_spawner.lua
+++ luarules/gadgets/game_boombox_spawner.lua
@@ -10,15 +10,10 @@ function gadget:GetInfo()
     }
 end
 
---if true then
---    return -- kill it for now
---end
-
 if not gadgetHandler:IsSyncedCode() then
     return
 end
 
-local AliveBoomboxes = {}
 local SelfDQueue = {}
 local positionCheckLibrary = VFS.Include("luarules/utilities/damgam_lib/position_checks.lua")
 
@@ -52,8 +47,8 @@ end
 
 function gadget:GameFrame(frame)
     if frame == 1 then
-        for i = 1,10*#Spring.GetTeamList() do
-            if math.random(1,10000) == 1 then 
+        for i = 1,#Spring.GetTeamList() do
+            if math.random(1,1000) == 1 then
                 for j = 1,1000 do
                     local posx = math.random(math.floor(Game.mapSizeX*0.02), math.ceil(Game.mapSizeX*0.98))
                     local posz = math.random(math.floor(Game.mapSizeZ*0.02), math.ceil(Game.mapSizeZ*0.98))
@@ -62,25 +57,21 @@ function gadget:GameFrame(frame)
                     if posy > 0 and positionCheckLibrary.FlatAreaCheck(posx, posy, posz, 64, 25, true) and blockerDistance > 196 then
                         local boomboxID = Spring.CreateUnit("boombox", posx, posy, posz, "west", Spring.GetGaiaTeamID())
                         if boomboxID then
-                            AliveBoomboxes[boomboxID] = true
+                            Spring.SetUnitNeutral(boomboxID, true)
+                            Spring.SetUnitStealth(boomboxID, true)
+                            Spring.SetUnitNoMinimap(boomboxID, true)
+                            Spring.SetUnitMaxHealth(boomboxID, 10000)
+                            Spring.SetUnitHealth(boomboxID, 10000)
+                            Spring.SetUnitSensorRadius(boomboxID, 'los', 0)
+                            Spring.SetUnitSensorRadius(boomboxID, 'airLos', 0)
+                            Spring.SetUnitSensorRadius(boomboxID, 'radar', 0)
+                            Spring.SetUnitSensorRadius(boomboxID, 'sonar', 0)
+                            Spring.SetUnitAlwaysVisible(boomboxID, true)
                             break
                         end
                     end
                 end
             end
-        end
-
-        for unitID, _ in pairs(AliveBoomboxes) do
-            Spring.SetUnitNeutral(unitID, true)
-            Spring.SetUnitStealth(unitID, true)
-            Spring.SetUnitNoMinimap(unitID, true)
-            Spring.SetUnitMaxHealth(unitID, 10000)
-            Spring.SetUnitHealth(unitID, 10000)
-            Spring.SetUnitSensorRadius(unitID, 'los', 0)
-            Spring.SetUnitSensorRadius(unitID, 'airLos', 0)
-            Spring.SetUnitSensorRadius(unitID, 'radar', 0)
-            Spring.SetUnitSensorRadius(unitID, 'sonar', 0)
-            Spring.SetUnitAlwaysVisible(unitID, true)
         end
     end
     if frame%5 == 0 then
@@ -88,7 +79,6 @@ function gadget:GameFrame(frame)
             if Spring.GetGameFrame() > time then
                 Spring.DestroyUnit(unitID)
                 SelfDQueue[unitID] = nil
-                AliveBoomboxes[unitID] = nil
                 break
             end
         end
@@ -97,7 +87,7 @@ end
 
 function gadget:UnitTaken(unitID, unitDefID, oldTeam, newTeam)
     if UnitDefs[unitDefID].name == "boombox" then
-        SelfDQueue[unitID] = Spring.GetGameFrame()+90
+        SelfDQueue[unitID] = Spring.GetGameFrame()+30
     end
 end
 
@@ -111,6 +101,5 @@ end
 
 function gadget:UnitDestroyed(unitID, unitDefID)
     SelfDQueue[unitID] = nil
-    AliveBoomboxes[unitID] = nil
 end
 
