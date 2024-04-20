8d2724f0ef5ec0fdc8999755a587da9019400115
TheSilverHornet <37483246+TheSilverHornet@users.noreply.github.com>
2024-04-20 03:28:12 +0100

    [EMP Rework] Fix immune units taking EMP damage (#2858)
    * emp rework tuning
    
    * rework calibration testing
    
    * BW tuning
    
    * Rework BW testing
    
    * emp rework: faster drain testing
    
    * EMP intensity update
    
    * Preliminary EMP-rework tweaks
    
    * Re-rebalancing everything EMP
    
    A bit more partial restoration of vanilla's emp silo+lolcannon interaction and spybot+antinuke interaction.
    Heavy nerfs and re-nerfs to emp elsewhere.
    
    * Fixed nuke silo emp resist oversight
    
    * temp
    
    * tuning abductor for mono target mode
    
    * nudge
    
    * bugfix
    
    * nudge
    
    ---------
    
    Co-authored-by: SilverSolZ <96927629+SilverSolZ@users.noreply.github.com>

--- luarules/gadgets/unit_paralyze_damage_limit.lua
+++ luarules/gadgets/unit_paralyze_damage_limit.lua
@@ -47,8 +47,8 @@ for udid, ud in pairs(UnitDefs) do
 
 	
 	if modOptions.emprework==true then
-		unitOhms[udid] = ud.customParams.paralyzemultiplier or 1
-		--Spring.Echo('ohm', ud.customParams.paralyzemultiplier)
+		unitOhms[udid] = ud.customParams.paralyzemultiplier == nil and 1 or tonumber(ud.customParams.paralyzemultiplier)--it arrives as a string because WHY NOT
+		
 		if tonumber(ud.customParams.paralyzemultiplier) or 0 > 0 then
 		--Spring.Echo('ohm', ud.name, ud.customParams.paralyzemultiplier)
 		end
@@ -97,16 +97,20 @@ function gadget:UnitPreDamaged(uID, uDefID, uTeam, damage, paralyzer, weaponID, 
 					--ohm = ohm * 1.4 -- with 1.4, an EMP resistance of say 1.2 gains EMP only slightly faster but has nearly double max charge capacity relative to lesser units
 				--end
 				thismaxtime = weaponParalyzeDamageTime[weaponID] * ((ohm == 1 and 0.85) or ohm)
+
+
+				if ohm>0 then
+					bufferdamage = hp / 200
+					--rootdamage = (damage / 50) * hp^0.5
+					--Spring.Echo('h damage rootdamage',hp,damage, rootdamage)
+					damage = damage + bufferdamage --overcome relative effects drain (eg stunned unit with 90000 hp loses 900 emp damage a tick, whereas unit with 900 hp loses 9 a tick. impossible to balance low damage emp weapons to overcome this without making them OP vs low HP units)
+				end
+				
 			else	
 				thismaxtime = weaponParalyzeDamageTime[weaponID]
 			end
 			
-			
-			bufferdamage = hp / 200
-			--rootdamage = (damage / 50) * hp^0.5
-			--Spring.Echo('h damage rootdamage',hp,damage, rootdamage)
-			damage = damage + bufferdamage --overcome relative effects drain (eg stunned unit with 90000 hp loses 900 emp damage a tick, whereas unit with 900 hp loses 9 a tick. impossible to balance low damage emp weapons to overcome this without making them OP vs low HP units)
-			
+
 			
 			--Spring.Echo('raw stuntime, ohm, using mult value, thismaxtime (pre-minumum)', weaponParalyzeDamageTime[weaponID], ohm, ((ohm == 1 and 0.5) or ohm), thismaxtime)
 			--thismaxtime = math.max(1, thismaxtime)--prevent microstuns (compounds oddly with shuri unfortunately)
