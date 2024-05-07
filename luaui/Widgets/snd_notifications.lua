a26b422b8d9214a50d748c1c09d4c38054834a25
Floris <floris@ruwhof.net>
2024-05-07 16:24:21 +0200

    sound notifications: get notification duration from sound file

--- luaui/Widgets/snd_notifications.lua
+++ luaui/Widgets/snd_notifications.lua
@@ -29,6 +29,9 @@ local tutorialPlayLimit = 2		-- display the same tutorial message only this many
 
 --------------------------------------------------------------------------------
 
+local wavFileLengths = VFS.Include('sounds/sound_file_lengths.lua')
+VFS.Include('common/wav.lua')
+
 local LastPlay = {}
 local Sound = {}
 local customNotifications = {}
@@ -39,13 +42,12 @@ local gameframe = spGetGameFrame()
 
 local lockPlayerID
 local gaiaTeamID = Spring.GetGaiaTeamID()
-function addSound(name, file, minDelay, duration, messageKey, unlisted)
+local function addSound(name, file, minDelay, messageKey, unlisted)
 	Sound[name] = {
 		file = file,
 		delay = minDelay,
-		duration = duration,
 		messageKey = messageKey,
-	 }
+	}
 
 	soundList[name] = true
 	if not unlisted then
@@ -90,13 +92,13 @@ for notifID, notifDef in pairs(soundsTable) do -- Temporary to keep it working f
 		end
 	end
 	if notifSounds[1] then
-		addSound(notifID, notifSounds, notifDef.delay, notifDef.length, notifDef.sound[1].text, notifDef.unlisted) -- bandaid, picking text from first variation always.
+		addSound(notifID, notifSounds, notifDef.delay, notifDef.sound[1].text, notifDef.unlisted) -- bandaid, picking text from first variation always.
 	end
 end
 
 
-local function AddNotification(name, files, minDelay, duration, messageKey, unlisted)
-	customNotifications[name] = { files, minDelay, duration, messageKey, unlisted }
+local function AddNotification(name, files, minDelay, messageKey, unlisted)
+	customNotifications[name] = { files, minDelay, messageKey, unlisted }
 	local newFiles = {}
 	for i, file in pairs(files) do
 		if VFS.FileExists(soundFolder .. file) then
@@ -109,7 +111,7 @@ local function AddNotification(name, files, minDelay, duration, messageKey, unli
 		end
 	end
 	if newFiles[1] then
-		addSound(name, newFiles, minDelay, duration, messageKey, unlisted)
+		addSound(name, newFiles, minDelay, messageKey, unlisted)
 	end
 end
 
@@ -390,8 +392,8 @@ function widget:Initialize()
 	WG['notifications'].setPlayTrackedPlayerNotifs = function(value)
 		playTrackedPlayerNotifs = value
 	end
-	--WG['notifications'].addSound = function(name, file, minDelay, duration, messageKey, unlisted)
-	--	addSound(name, file, minDelay, duration, messageKey, unlisted)
+	--WG['notifications'].addSound = function(name, file, minDelay, messageKey, unlisted)
+	--	addSound(name, file, minDelay, messageKey, unlisted)
 	--end
 	WG['notifications'].addEvent = function(value, force)
 		if Sound[value] then
@@ -406,7 +408,7 @@ function widget:Initialize()
 	end
 
 	for name, params in pairs(customNotifications) do
-		AddNotification(name, params[1], params[2], params[3], params[4], params[5])
+		AddNotification(name, params[1], params[2], params[3], params[4])
 	end
 
 	if Spring.Utilities.Gametype.IsRaptors() and Spring.Utilities.Gametype.IsScavengers() then
@@ -716,12 +718,17 @@ local function playNextSound()
 	if #soundQueue > 0 then
 		local event = soundQueue[1]
 		local isTutorialNotification = (string.sub(event, 1, 2) == 't_')
-		nextSoundQueued = sec + Sound[event].duration + silentTime
 		if not muteWhenIdle or not isIdle or isTutorialNotification then
 			local m = 1
 			if spoken and Sound[event].file and Sound[event].file[1] ~= '' then
 				m = math.random(1,#Sound[event].file)
 				Spring.PlaySoundFile(Sound[event].file[m], globalVolume, 'ui')
+				local duration = wavFileLengths[string.sub(Sound[event].file[m], 8)]
+				if not duration then
+					duration = ReadWAV(Sound[event].file[m])
+					duration = duration.Length
+				end
+				nextSoundQueued = sec + (duration or 3) + silentTime
 			end
 			if displayMessages and WG['messages'] and Sound[event].messageKey then
 				WG['messages'].addMessage(Spring.I18N(Sound[event].messageKey))
