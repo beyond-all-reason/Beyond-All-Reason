99c576032e869a9d77b4c8fe0e2dad689e0ee754
Zephyr Skies <distantzephyrskies@gmail.com>
2024-05-13 13:13:39 -0400

    update defencerange.lua to include new legion units

--- luaui/Widgets/gui_defenserange.lua
+++ luaui/Widgets/gui_defenserange.lua
@@ -94,9 +94,13 @@ modConfig["BYAR"]["unitList"] = {
 	corbuzz = { weapons = { 1 } },
 	
 	--LEGION
-	legstarfall = { weapons = { 1 } },
-	legbombard = { weapons = { 1 } },
-	legperdition = { weapons = { 1 } },
+	legstarfall = { weapons = { 1 } }, -- LOLCannon
+	legbombard = { weapons = { 1 } }, -- t2 pop-up
+	legperdition = { weapons = { 1 } }, -- tacnuke
+	leglht = { weapons = { 1 } }, -- t1 heatray tower
+	legdtr = { weapons = { 1 } }, -- t1 pop-up riot cannon
+	legcluster = { weapons = { 1 } },  -- t1 cluster arty
+	leglupara = { weapons = { 2 } }, -- t1.5 burst flak
 	
 	-- SCAVENGERS
 	scavengerdroppodbeacon_scav = { weapons = { 1 } },
