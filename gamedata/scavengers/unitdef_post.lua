b204982741ea05c6e987d883be4d8f94f18d4b32
Damgam <c_2_damiano@op.pl>
2024-05-17 15:58:40 +0200

    Scav Armada Commanders health nerf

--- gamedata/scavengers/unitdef_post.lua
+++ gamedata/scavengers/unitdef_post.lua
@@ -146,6 +146,13 @@ local function scavUnitDef_Post(name, uDef)
 		uDef.customparams.iscommander = nil
 	end
 
+	-- Evocom adjustments
+	if string.find(name, "armcomlvl") then -- nerf health of armada evocom, since it's invisible and hard to deal with because of that
+		if uDef.health then
+			uDef.health = uDef.health * 0.5
+		end
+	end
+
 
 
 	-- Extra Units for Scavs
