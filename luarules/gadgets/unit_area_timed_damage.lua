992ba41e59768414cc13629f26ea673979b7e275
efrec <quantumscrewdriver@gmail.com>
2024-06-16 05:19:55 -0400

    No longer feels like pseudocode. Integrated configurable loop time and frame res, added delayed-start timed areas.

--- luarules/gadgets/unit_area_timed_damage.lua
+++ luarules/gadgets/unit_area_timed_damage.lua
@@ -109,87 +109,127 @@ for unitDefID, unitDef in pairs(UnitDefs) do
 	end
 end
 
--- Areas are binned into intervals and updated once per second.
-local frame = 1
-local areas = {}
-local freed = {}
-local empty = {}
-for ii = 1, 30 do
+local explosionCache = {
+	weaponDef         = 0, -- uses the ID from area_weaponDef.id
+	edgeEffectiveness = 1,
+	explosionSpeed    = 10000,
+	damageGround      = false,
+}
+
+-- Setup for the update loop.
+
+frameResolution = math.clamp(math.round(frameResolution), 1, Game.gameSpeed) -- ? complains about args
+loopDuration    = math.clamp(loopDuration, 0.25, 1)
+
+-- The discrete loop length can differ from the target update time, which we adjust against.
+-- e.g. for a 1s period at 30fps with res=29, we end up looping a single group over 0.9667s.
+local loopFrameCount = frameResolution * math.round(Game.gameSpeed * loopDuration / frameResolution)
+loopDuration = loopFrameCount * (1 / Game.gameSpeed) -- note: may be outside the clamp range.
+local loopRate = 1 / loopDuration
+
+local groupCount = loopFrameCount / frameResolution
+local groupDuration = groupCount * (1 / Game.gameSpeed)
+
+local areas = {} -- Maintain a contiguous array of ongoing areas.
+local freed = {} -- Supported by an array of pseudo-holes.
+local nfree = {} -- Which are tracked by a running count.
+local empty = {} -- And patched with an `empty` element.
+                 -- ? idk if this works in 5.1; the goal is to keep #group[index] an O(1).
+
+for ii = 1, groupCount do
 	areas[ii] = {}
 	freed[ii] = {}
+	nfree[ii] = 0
 end
+
+-- Timekeeping uses cycling counts.
+local ticks = 0
+local frame = 0
+local time  = 0
+
+-- We also keep a queue of delayed areas.
+local queue = {}
 
 ---------------------------------------------------------------------------------------------------------------
 
 local function StartTimedArea(x, y, z, weaponParams)
 	local elevation = math.max(0, Spring.GetGroundHeight(x, z))
-	if y <= elevation + weaponParams.atd_radius * 0.5 then
-		-- This interval won't recur for another 30 frames; if you want immediate damage
-		-- on contact, you have to put it in the base weapon.
+	local radius = weaponParams.area_weaponDef.damageAreaOfEffect
+
+	-- Create an area on ground -- immediately.
+	if y <= elevation + radius * 0.5 then
 		local group = areas[frame]
 		local holes = freed[frame]
+		local sizeh = nfree[frame]
 
 		local index
-		if #holes then
-			index = holes[#holes]
-			holes[#holes] = nil
+		if sizeh > 0 then
+			index = holes[sizeh]
+			sizeh = sizeh - 1
 		else
 			index = #group + 1
 		end
 
+		-- Groups are looped periodically in GameFrame to spawn explosions.
 		group[index] = {
 			weaponParams = weaponParams,
-			endTime = weaponParams.area_duration + Spring.GetGameTime(),
-			x = x,
-			y = elevation,
-			z = z,
+			endTime      = weaponParams.area_duration + time,
+			x            = x,
+			y            = elevation,
+			z            = z,
 		}
 
-		Spring.SpawnCEG(
-			weaponParams.area_ongoingCEG,
-			x, elevation, z,
-			0, 0, 0,
-			weaponParams.area_weaponDef.damageAreaOfEffect, -- Just used for scaling?
-			weaponParams.area_weaponDef.damages[0]          -- Just used for scaling?
-		)
-	end
-end
-
-local function StopTimedArea(group, index)
-	if index == #group then
-		group[#group] = nil
-	else
-		group[index] = empty
-		freed[#freed + 1] = index
+		-- Ideally, area timed weapons are represented by a continuous vfx.
+		-- But another option is to use an explosiongenerator on the area weapon.
+		if weaponParams.area_ongoingCEG then
+			Spring.SpawnCEG(
+				weaponParams.area_ongoingCEG,
+				x, elevation, z,
+				0, 0, 0,
+				radius,                                -- Just used for scaling?
+				weaponParams.area_weaponDef.damages[0] -- Just used for scaling?
+			)
+		end
+	-- Create an area on ground -- eventually.
+	elseif y < elevation + radius * 3 then
+		local timeToStart = 0.9 * math.sqrt((y - elevation - radius * 0.5) * 2 / Game.gravity)
+		local delayFrame = Spring.GetGameFrame()
+		delayFrame = math.max(delayFrame + 1, delayFrame + math.round(timeToStart / Game.gameSpeed))
+		if not queue[delayFrame] then queue[delayFrame] = {} end
+		local delayedAreas = queue[delayFrame]
+		delayedAreas[#delayedAreas+1] = { x, elevation, z, weaponParams }
 	end
 end
 
 local function UpdateTimedAreas()
-	local frameAreas = areas[frame]
-	local frameRate = Spring.GetGameSpeed()
-	local frameTime = Spring.GetGameSeconds() - 0.5 / frameRate
-	-- local damageRate = 30 / frameRate -- todo
+	local params = explosionCache
+	local group = areas[frame]
+	local holes = freed[frame]
+	local sizeg = #group
+	local sizeh = nfree[frame]
 
-	local explosion = {
-		weaponDef          = 0, -- todo: Great. Now I gotta make a weapon for all of these.
-		edgeEffectiveness  = 1,
-		explosionSpeed     = 10000,
-		damageGround       = false,
-	}
-
-	-- Deal recurring damage by spawning explosions.
-	for index = #frameAreas, 1, -1 do
-		local timedArea = frameAreas[index]
+	for index = sizeg, 1, -1 do
+		local timedArea = group[index]
 		if timedArea ~= empty then
-			if timedArea.endTime > frameTime then
-				-- todo: Will napalm/acid destroy shields now? Troubling thought.
-				explosion.weaponDef = timedArea.params.area_weaponDef
-				Spring.SpawnExplosion(timedArea.x, timedArea.y, timedArea.z, 0, 0, 0, explosion)
+			if time <= timedArea.endTime then
+				params.weaponDef = timedArea.params.area_weaponDef.id
+				Spring.SpawnExplosion(timedArea.x, timedArea.y, timedArea.z, 0, 0, 0, params)
+			elseif index == sizeg then
+				group[index] = nil
+				sizeg = sizeg - 1
 			else
-				StopTimedArea(frameAreas, index)
+				group[index] = empty
+				holes[sizeh] = index
+				sizeh = sizeh + 1
 			end
+		elseif index == #group then
+			group[index] = nil
+			holes[sizeh] = nil
+			sizeg = sizeg -1
+			sizeh = sizeh -1
 		end
 	end
+	nfree[frame] = sizeh
 end
 
 ---------------------------------------------------------------------------------------------------------------
@@ -214,9 +254,38 @@ function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerD
 end
 
 function gadget:GameFrame(gameFrame)
-	-- Bin frames into intervals over the range [1, 30]:
-	frame = (gameFrame % 30) + 1
-	UpdateTimedAreas()
+	-- Manage ongoing timed areas.
+	ticks = ticks + 1
+	if ticks == frameResolution then
+		ticks = 0
+		if frame == groupCount
+		then frame = 1
+		else frame = frame + 1 end
+		time = Spring.GetGameSeconds() + groupDuration * 0.5
+
+		-- Create explosions.
+		UpdateTimedAreas()
+	end
+
+	-- Manage delayed-start timed areas.
+	-- These should be uncommon enough that the shield check isn't so bad.
+	for _, delayedAreas in pairs(queue) do
+		for ii = #delayedAreas,-1,1 do
+			-- todo: Exclude areas covered by active shields.
+			-- todo: How much overlap is too much? To center? More, less? I feel like "less"?
+			-- todo: Like wouldn't you be mad if a fire spread through your shield? You'd be mad.
+			local isInActiveShield = false
+			if isInActiveShield then table.remove(delayedAreas, ii) end
+		end
+	end
+
+	-- Activate delayed-start timed areas.
+	if queue[gameFrame] then
+		for _, args in ipairs(queue[gameFrame]) do
+			StartTimedArea(args[1], args[2], args[3], args[4])
+		end
+		queue[gameFrame] = nil
+	end
 end
 
 function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projID, attackID, attackDefID, attackTeam)
@@ -231,7 +300,7 @@ function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, w
 			if y > -8 then
 				damage = damage * loopRate
 				if weaponParams.area_damagedCEG then
-				Spring.SpawnCEG(weaponParams.area_damagedCEG, x, y + 8, z, 0, 0, 0, 0, damage)
+					Spring.SpawnCEG(weaponParams.area_damagedCEG, x, y + 8, z, 0, 0, 0, 0, damage)
 				end
 				return damage, areaImpulseRate
 			else
@@ -247,10 +316,52 @@ function gadget:FeaturePreDamaged(featureID, featureTeam, damage, weaponDefID, p
 	if weaponParams then
 		damage = damage * loopRate
 		if weaponParams.area_damagedCEG then
-		local _,_,_, x,y,z = Spring.GetFeaturePosition(featureID, true)
-		Spring.SpawnCEG(weaponParams.area_damagedCEG, x, y + 8, z, 0, 0, 0, 0, damage)
+			local _,_,_, x,y,z = Spring.GetFeaturePosition(featureID, true)
+			Spring.SpawnCEG(weaponParams.area_damagedCEG, x, y + 8, z, 0, 0, 0, 0, damage)
 		end
 		return damage, areaImpulseRate
 	end
 	return damage, 1
+end
+
+---------------------------------------------------------------------------------------------------------------
+
+-- Tests
+
+if true then
+	-- Define a single test routine, for both weapons and units.
+	-- This needs to be yelly and shouty and angry at least until this branch gets approvals.
+	local warn = '[area_timed_damage] [warn] '
+	local info = '[area_timed_damage] [info] '
+	local function TestStuff(id, params, defTable)
+		local name = defTable[id].name
+
+		-- Check for durations that we do not support properly.
+		local duration = params.area_duration
+		if duration <= loopDuration then
+			Spring.Echo(warn..'Duration shorter than loop duration: '..name)
+		end
+		if 0.1 < 1 - (math.floor(duration / loopDuration) * loopDuration) / duration then
+			Spring.Echo(warn..'Duration cut off due to loop period: '..name)
+		end
+
+		-- Check for missing explosion generators.
+		-- Prob too hard: Check for EGs that are too short/long for their effects' durations.
+		local highDamage = 80
+		if not params.area_damagedCEG then
+			if not params.area_ongoingCEG then
+				Spring.Echo(info..'Area weapon has no CEGs in customparams: '..name)
+			else
+				if params.area_weaponDef.damages[0] > highDamage then
+					Spring.Echo(info..'High-damage area weapon without damagedCEG: '..name)
+				end
+			end
+		elseif not params.area_ongoingCEG and params.area_weaponDef.damages[0] > highDamage then
+			Spring.Echo(info..'High-damage area weapon without ongoingCEG: '..name)
+		end
+	end
+
+	-- Run the tests.
+	for id, params in pairs(weaponAreaParams) do TestStuff(id, params, WeaponDefs) end
+	for id, params in pairs(onDeathAreaParams) do TestStuff(id, params, UnitDefs) end
 end
