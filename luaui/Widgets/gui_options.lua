14498e363c9c501a006a70a3400fc30009aa9ff9
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2024-06-18 20:58:12 -0400

    Cleanup: Remove obsolete Scavengers code and associated files

--- luaui/Widgets/gui_options.lua
+++ luaui/Widgets/gui_options.lua
@@ -2807,11 +2807,6 @@ function init()
 		  end,
 		},
 
-		{ id = "scav_messages", group = "notif", category = types.basic, name = Spring.I18N('ui.settings.option.scav_messages'), type = "bool", value = tonumber(Spring.GetConfigInt("scavmessages", 1) or 1) == 1, description = "",
-		  onchange = function(i, value)
-			  Spring.SetConfigInt("scavmessages", (value and 1 or 0))
-		  end,
-		},
 		{ id = "scav_voicenotifs", group = "notif", category = types.basic, widget = "Scavenger Audio Reciever", name = Spring.I18N('ui.settings.option.scav_voicenotifs'), type = "bool", value = GetWidgetToggleValue("Scavenger Audio Reciever"), description = Spring.I18N('ui.settings.option.scav_voicenotifs_descr') },
 
 		{ id = "notifications_tutorial", group = "notif", name = Spring.I18N('ui.settings.option.notifications_tutorial'), category = types.basic, type = "bool", value = (WG['notifications'] ~= nil and WG['notifications'].getTutorial()), description = Spring.I18N('ui.settings.option.notifications_tutorial_descr'),
@@ -5596,7 +5591,6 @@ function init()
 
 	if not scavengersAIEnabled then
 		options[getOptionByID('scav_voicenotifs')] = nil
-		options[getOptionByID('scav_messages')] = nil
 	end
 
 	if not GetWidgetToggleValue('Grid menu') then
