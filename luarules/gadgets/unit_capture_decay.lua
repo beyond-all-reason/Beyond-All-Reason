f77ef2ea49de338bb5f5feeb790ba096249c7048
Damgam <c_2_damiano@op.pl>
2024-07-04 18:42:39 +0200

    Capture Decay Optimisation (#3314)
    Made this gadget practically free.

--- luarules/gadgets/unit_capture_decay.lua
+++ luarules/gadgets/unit_capture_decay.lua
@@ -14,33 +14,38 @@ if not gadgetHandler:IsSyncedCode() then
     return
 end
 
-local aliveUnits = {}
-function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-    aliveUnits[unitID] = {previousCaptureProgress = 0, ticksFromLastCapture = 999}
-end
+local unitsWithCaptureProgress = {}
 
 function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
-    aliveUnits[unitID] = nil
+    unitsWithCaptureProgress[unitID] = nil
 end
 
 function gadget:GameFrame(frame)
-    for unitID, data in pairs(aliveUnits) do
+    for unitID, data in pairs(unitsWithCaptureProgress) do
         if unitID%30 == frame%30 then
             local captureLevel = select(4, Spring.GetUnitHealth(unitID))
             if captureLevel and captureLevel > 0 then
                 if captureLevel <= data.previousCaptureProgress then
-                    aliveUnits[unitID].ticksFromLastCapture = aliveUnits[unitID].ticksFromLastCapture+1
-                    SendToUnsynced("unitCaptureFrame", unitID, math.max(captureLevel, 0))
+                    unitsWithCaptureProgress[unitID].ticksFromLastCapture = unitsWithCaptureProgress[unitID].ticksFromLastCapture+1
                 else
-                    aliveUnits[unitID].ticksFromLastCapture = 0
+                    unitsWithCaptureProgress[unitID].ticksFromLastCapture = 0
                     SendToUnsynced("unitCaptureFrame", unitID, math.max(captureLevel, 0))
                 end
-                aliveUnits[unitID].previousCaptureProgress = captureLevel
-                if aliveUnits[unitID].ticksFromLastCapture >= 10 then -- with how things are set up, that will be about 10 seconds
-                    Spring.SetUnitHealth(unitID, {capture = math.max(captureLevel-((aliveUnits[unitID].ticksFromLastCapture-10)*0.001), 0)})
-                    SendToUnsynced("unitCaptureFrame", unitID, math.max(captureLevel-((aliveUnits[unitID].ticksFromLastCapture-10)*0.001), 0))
+                unitsWithCaptureProgress[unitID].previousCaptureProgress = captureLevel
+                if unitsWithCaptureProgress[unitID].ticksFromLastCapture >= 10 then -- with how things are set up, that will be about 10 seconds
+                    Spring.SetUnitHealth(unitID, {capture = math.max(captureLevel-((unitsWithCaptureProgress[unitID].ticksFromLastCapture-10)*0.001), 0)})
                 end
+            else
+                unitsWithCaptureProgress[unitID] = nil
             end
         end
     end
+end
+
+
+function gadget:AllowUnitCaptureStep(builderID, builderTeam, unitID, unitDefID, part)
+    if not unitsWithCaptureProgress[unitID] then
+        unitsWithCaptureProgress[unitID] = {previousCaptureProgress = 0, ticksFromLastCapture = 999}
+    end
+    return true
 end
