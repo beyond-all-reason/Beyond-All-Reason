8e5d1e274840397410f9e039a07ffafd1ff99cf2
Damgam <c_2_damiano@op.pl>
2024-07-06 19:25:08 +0200

    Tweaked Scav Cloud formula to be more consistent between small and big maps.
    Tweaked Scav Cloud formula to be more consistent between small and big maps.

--- luarules/gadgets/scav_cloud_spawner.lua
+++ luarules/gadgets/scav_cloud_spawner.lua
@@ -26,10 +26,11 @@ function gadget:GetInfo()
     
     local mapx = Game.mapSizeX
     local mapz = Game.mapSizeZ
-    local cloudMult = math.ceil(((mapx+mapz)*0.5)/4000)
-    local maxMists = (#teams-2)*5*cloudMult
+    local cloudMult = math.ceil((math.ceil(((mapx+mapz)*0.5)/512)^2)/72)
+    local maxMists = (#teams-2)*cloudMult
     
     function gadget:GameFrame(frame)
+        Spring.Echo("CloudMultiplier", cloudMult)
         for _ = 1, cloudMult do
             local randomx = math.random(0, mapx)
             local randomz = math.random(0, mapz)
@@ -43,7 +44,7 @@ function gadget:GetInfo()
             local randomy = Spring.GetGroundHeight(randomx, randomz)
             if GG.IsPosInRaptorScum(randomx, randomy, randomz) then
                 Spring.SpawnCEG("scavradiation-lightning",randomx,randomy+100,randomz,0,0,0)
-                if math.random(0, 1000) == 0 then
+                if math.random(0, 100) == 0 then
                     if Spring.GetGameRulesParam("scavTechAnger") > 10 and Spring.GetGameRulesParam("scavTechAnger") < 50 and Spring.GetTeamUnitDefCount(scavTeamID, UnitDefNames["scavmist_scav"].id) < maxMists then
                         local mist = Spring.CreateUnit("scavmist_scav", randomx, randomy, randomz, math.random(0,3), scavTeamID)
                         if mist then
@@ -55,7 +56,7 @@ function gadget:GetInfo()
                             Spring.GiveOrderToUnit(mist, CMD.PATROL, {randomx+math.random(-256,256), randomy, randomz+math.random(-256,256)}, {"shift"})
                         end
                     end
-                elseif math.random(0, 1000) == 0 then
+                elseif math.random(0, 100) == 0 then
                     if Spring.GetGameRulesParam("scavTechAnger") > 40 and Spring.GetGameRulesParam("scavTechAnger") < 90 and Spring.GetTeamUnitDefCount(scavTeamID, UnitDefNames["scavmistxl_scav"].id) < maxMists then
                         local mist = Spring.CreateUnit("scavmistxl_scav", randomx, randomy, randomz, math.random(0,3), scavTeamID)
                         if mist then
@@ -67,7 +68,7 @@ function gadget:GetInfo()
                             Spring.GiveOrderToUnit(mist, CMD.PATROL, {randomx+math.random(-256,256), randomy, randomz+math.random(-256,256)}, {"shift"})
                         end
                     end
-                elseif math.random(0, 1000) == 0 then
+                elseif math.random(0, 100) == 0 then
                     if Spring.GetGameRulesParam("scavTechAnger") > 80 and Spring.GetTeamUnitDefCount(scavTeamID, UnitDefNames["scavmistxxl_scav"].id) < maxMists then
                         local mist = Spring.CreateUnit("scavmistxxl_scav", randomx, randomy, randomz, math.random(0,3), scavTeamID)
                         if mist then
