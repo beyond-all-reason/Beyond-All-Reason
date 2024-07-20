8031b4876133feed74ba126e0ec91d9f198dbe4b
Hornet <37483246+TheSilverHornet@users.noreply.github.com>
2024-07-21 00:33:38 +0100

    .

--- effects/minesweep.lua
+++ effects/minesweep.lua
@@ -1,12 +1,12 @@
 return {
 	["MINESWEEP"] = {
-		usedefaultexplosions = false;
+		usedefaultexplosions = true;
 		["GROUNDFLASH"] = {
-			flashsize = 300,
+			flashsize = 100,
 			flashalpha = 0.4,
-			circlegrowth = 14,
+			circlegrowth = 4,
 			circlealpha = 0.1,
-			ttl = 20,
+			ttl = 10,
 			color = {0.2, 0.9, 0.2},
 		},
 	},
