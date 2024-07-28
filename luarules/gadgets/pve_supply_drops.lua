55d3497fb7a6eb6251fb22af56ced855884fbe11
Damgam <c_2_damiano@op.pl>
2024-07-28 07:20:54 +0200

    Added Always Visible version of commander spawn effect. (#3489)
    Added Always Visible version of commander spawn effect. Used in Scavengers and Lootboxes.

--- luarules/gadgets/pve_supply_drops.lua
+++ luarules/gadgets/pve_supply_drops.lua
@@ -143,7 +143,7 @@ local function SpawnLootbox(posx, posy, posz)
 	if spawnedUnit then
 		Spring.SetUnitNeutral(spawnedUnit, true)
 		Spring.SetUnitAlwaysVisible(spawnedUnit, true)
-		Spring.SpawnCEG("commander-spawn", posx, posy, posz, 0, 0, 0)
+		Spring.SpawnCEG("commander-spawn-alwaysvisible", posx, posy, posz, 0, 0, 0)
 		Spring.PlaySoundFile("commanderspawn-mono", 1.0, posx, posy, posz, 0, 0, 0, "sfx")
 		GG.ComSpawnDefoliate(posx, posy, posz)
 	end
