9ac18fa4833c4c382985bf4117629de3f1decf90
SethDGamre <sethdgamre@gmail.com>
2024-07-31 20:38:55 -0500

    Update unit_dgun_behaviour.lua

--- luarules/gadgets/unit_dgun_behaviour.lua
+++ luarules/gadgets/unit_dgun_behaviour.lua
@@ -11,11 +11,22 @@ end
 if not gadgetHandler:IsSyncedCode() then
 	return
 end
+
+local spGetProjectileDefID = Spring.GetProjectileDefID
+local spGetUnitShieldState = Spring.GetUnitShieldState
+local spGetUnitDefID = Spring.GetUnitDefID
+local spSetUnitShieldState = Spring.SetUnitShieldState
+local diag = math.diag
+
+
+local dgunWeaponsTTL = {}
 local dgunWeapons = {}
 for weaponDefID, weaponDef in ipairs(WeaponDefs) do
 	if weaponDef.type == 'DGun' then
+		Spring.Echo(weaponDef)
 		Script.SetWatchProjectile(weaponDefID, true)
 		dgunWeapons[weaponDefID] = weaponDef
+		dgunWeaponsTTL[weaponDefID] = math.ceil((weaponDef.range/weaponDef.projectilespeed)*30)
 	end
 end
 
@@ -62,7 +73,8 @@ end
 
 function gadget:ProjectileCreated(proID, proOwnerID, weaponDefID)
 	if dgunWeapons[weaponDefID] then
-		flyingDGuns[proID] = true
+		flyingDGuns[proID] = Spring.GetGameFrame()+dgunWeaponsTTL[weaponDefID]
+		Spring.Echo("projectile created", dgunWeaponsTTL[weaponDefID], flyingDGuns[proID])
 	end
 end
 
@@ -71,16 +83,18 @@ function gadget:ProjectileDestroyed(proID)
 	groundedDGuns[proID] = nil
 end
 
-function gadget:GameFrame()
+function gadget:GameFrame(frame)
 	for proID in pairs(flyingDGuns) do
-		-- Fireball is hitscan while in flight, engine only applies AoE damage after hitting the ground,
-		-- so we need to add the AoE damage manually for flying projectiles
-		addVolumetricDamage(proID)
-
 		local x, y, z = Spring.GetProjectilePosition(proID)
 		local h = Spring.GetGroundHeight(x, z)
-
-		if y < h + 1 or y < 0 then -- assume ground or water collision
+		Spring.Echo("Frame", frame, "proIDTimeoutF", proID)
+		if frame > proID then
+			Spring.DeleteProjectile(proID)
+			flyingDGuns[proID] = nil
+		elseif y < h + 1 or y < 0 then -- assume ground or water collision
+			-- Fireball is hitscan while in flight, engine only applies AoE damage after hitting the ground,
+			-- so we need to add the AoE damage manually for flying projectiles
+			addVolumetricDamage(proID)
 			-- normalize horizontal velocity
 			local dx, _, dz, speed = Spring.GetProjectileVelocity(proID)
 			local norm = speed / math.sqrt(dx^2 + dz^2)
@@ -94,26 +108,40 @@ function gadget:GameFrame()
 	end
 
 	for proID in pairs(groundedDGuns) do
-		local x, y, z = Spring.GetProjectilePosition(proID)
-		-- place projectile slightly under ground to ensure fiery trail
-		local verticalOffset = 1
-		Spring.SetProjectilePosition(proID, x, math.max(Spring.GetGroundHeight(x, z), 0) - verticalOffset, z)
+		if frame > proID then
+			Spring.DeleteProjectile(proID)
+		else
+			local x, y, z = Spring.GetProjectilePosition(proID)
+			-- place projectile slightly under ground to ensure fiery trail
+			local verticalOffset = 1
+			Spring.SetProjectilePosition(proID, x, math.max(Spring.GetGroundHeight(x, z), 0) - verticalOffset, z)
 
-		-- NB: no removal; do this every frame so that it doesn't fly off a cliff or something
+			-- NB: no removal; do this every frame so that it doesn't fly off a cliff or something
+		end
 	end
 end
 
-function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
-	if dgunWeapons[weaponDefID] and isCommander[attackerDefID] and (isCommander[unitDefID] or isDecoyCommander[unitDefID]) then
-		if isDecoyCommander[unitDefID] then
-			return dgunWeapons[weaponDefID].damages[0]
-		else
-			Spring.DeleteProjectile(projectileID)
-			local x, y, z = Spring.GetUnitPosition(unitID)
-			Spring.SpawnCEG("dgun-deflect", x, y, z, 0, 0, 0, 0, 0)
-			local armorClass = UnitDefs[unitDefID].armorType
-			return dgunWeapons[weaponDefID].damages[armorClass]
-		end
-	end
-	return damage
+function gadget:ShieldPreDamaged(proID, proOwnerID, shieldEmitterWeaponNum, shieldCarrierUnitID, bounceProjectile, beamEmitterWeaponNum, beamEmitterUnitID, startX, startY, startZ, hitX, hitY, hitZ)
+    if proID then
+        Spring.Echo("Start", startX, startY, startZ, "Hit", hitX, hitY, hitZ)
+
+        -- Calculate the direction vector
+        local dirX = hitX - startX
+        local dirY = hitY - startY
+        local dirZ = hitZ - startZ
+
+        -- Normalize the direction vector
+        local length = math.sqrt(dirX * dirX + dirY * dirY + dirZ * dirZ)
+        dirX = dirX / length
+        dirY = dirY / length
+        dirZ = dirZ / length
+
+        -- Move the projectile back by 2 units
+        local newX = hitX - 30 * dirX
+        local newY = hitY - 30 * dirY
+        local newZ = hitZ - 30 * dirZ
+
+        Spring.SetProjectilePosition(proID, newX, newY, newZ)
+    end
+	return false
 end
