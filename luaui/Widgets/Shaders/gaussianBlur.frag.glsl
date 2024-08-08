aba478dcb6b417685632871b991ceada1dee89c3
Beherith <mysterme@gmail.com>
2024-08-08 20:37:58 +0200

    SSAO Rework. See notes.
    Architectural changes
    
    Unit stencil texture
    All features and units model bounding boxes are rendered onto a downsized texture, that may be requested and reused by any subsequent pass.
    
    Gbuffer Fusion pass
    Removed merging of misc tex buffers, saving a fullscreen buffers worth of vram and some gpu load
    Removed merging of map and model normals, saving another fullscreen buffer, since these were also queried only once per fragment in ssao shader
    In the gbuffer fusion shader, map model occlusion is indicated by a positive z in viewpos tex
    The entire gbuffer fusion shader can be turned off, directly sampling the map and model depth buffers, this is most useful when rendering ssao to a downsampled target
    SSAO
    The ssao shader samples multiple neighbouring texels when downsampling, to reduce shimmer in the downsampled target.
    Random hemispherical samples are now generated across a fibonacci sphere for even surface distribution
    Outputs are now
    RG channel is viewpos xy normals, which is output as zero vector for ground fragments
    B channel is relative depth of fragment
    A is occlusion
    Blur
    Blur shader is now designed to work in a single pair of horizontal and vertical passes, and the dir uniform controls wether it should perform its final mixdown
    Blur shader has correct offsets and weights
    Compares the neighbouring normals and depths when blurring
    Identifies samples that are outliers with no similar neighbours
    Blur shader reuses the SSAO texture in its second pass, saving more vram
    During final mixdown, occlusion is packed into alpha, and brightening (inverse occlusion) is passed into rgb
    
    Compositing
    A different gl blending operation is used, where the ssao alpha darkens the original image, and the ssao color brightens the original.

--- luaui/Widgets/Shaders/gaussianBlur.frag.glsl
+++ luaui/Widgets/Shaders/gaussianBlur.frag.glsl
@@ -1,37 +1,126 @@
 #version 150 compatibility
 
-uniform sampler2D tex;
+//__DEFINES__
 
-uniform float offsets[###BLUR_HALF_KERNEL_SIZE###];
-uniform float weights[###BLUR_HALF_KERNEL_SIZE###];
+uniform sampler2D tex;
+uniform sampler2D unitStencilTex;
+
+uniform float offsets[BLUR_HALF_KERNEL_SIZE];
+uniform float weights[BLUR_HALF_KERNEL_SIZE];
+
+
+#define NORM2SNORM(value) (value * 2.0 - 1.0)
+#define SNORM2NORM(value) (value * 0.5 + 0.5)
 
 uniform vec2 dir;
+uniform float strengthMult;
 
 uniform vec2 viewPortSize;
-
+#line 1018
 void main(void)
 {
-	vec2 uv = gl_FragCoord.xy / viewPortSize;
-	vec4 acc = texture( tex, uv ) * weights[0];
-	float okCoords;
+	//vec2 uv = gl_FragCoord.xy / viewPortSize;
 
-	for (int i = 1; i < ###BLUR_HALF_KERNEL_SIZE###; ++i) {
-		vec2 uvOff = offsets[i] * dir / viewPortSize;
-		vec2 uvP = uv + uvOff;
-		vec2 uvN = uv - uvOff;
+	vec2 uv = gl_FragCoord.xy / vec2(HSX,HSY);
 
-		float weightP = weights[i];
-		float weightN = weights[i];
 
-		okCoords = float( all(bvec4( greaterThanEqual(uvP, vec2(0.0)), lessThanEqual(uvP, vec2(1.0)) )) );
-		weightP *= okCoords;
+	#if USE_STENCIL == 1 
+		if (texture(unitStencilTex, uv).r < 0.1) {
+			gl_FragColor = vec4(0.0,0.0, 0.0, 1.0);	return;
+		}
+	#endif
 
-		okCoords = float( all(bvec4( greaterThanEqual(uvN, vec2(0.0)), lessThanEqual(uvN, vec2(1.0)) )) );
-		weightN *= okCoords;
+		// Allrighty, this is a completely novel implementation now, that will attempt to use a bilateral weighting!
+		// tex contains normals, alpha contains occlusion
+		// We will weight individual samples by dot-ing them.
+		//new bilateral filter implementation
 
-		acc += texture( tex, uvP ) * weightP;
-		acc += texture( tex, uvN ) * weightN;
-	}
+		// TODO: ground has null vector normals, use that
+		// implement outlier detection too 
+		// MORE GROUND EFFECT! USE UNIFORM KERNEL FOR GROUND!
 
-	gl_FragColor = acc;
+		vec4 texSample = texture( tex, uv );
+		vec3 myNormal = NORM2SNORM(texSample.rgb);
+		myNormal.z = texSample.z;
+
+		float imground =step(dot(myNormal.xy, myNormal.xy), 0.1);
+		
+		float weightSum = weights[0];
+		if (imground > 0.5) {
+			//weightSum = 1.0/(2 * BLUR_HALF_KERNEL_SIZE-1);
+		}
+		float howLit = texSample.a * weightSum;
+		float unWeighted = howLit;
+		float myDistance = myNormal.z; // 1 at full distance, 0 at camera
+		vec2 texelOffset = dir / vec2(HSX,HSY);
+
+		// possibly better L1 cache locality via non expanding stepping?
+		for (int i = 1; i < (BLUR_HALF_KERNEL_SIZE  ); ++i) { // i goes from 1 to BLUR_HALF_KERNEL_SIZE-1
+			vec2 uvOff = offsets[i] * texelOffset ;
+			vec2 uvP = uv + uvOff;
+			vec2 uvN = uv - uvOff;
+
+			float weightP = weights[i];
+			float weightN = weights[i];
+			if (imground > 0.5) {
+				//weightP = 1.0/(2 * BLUR_HALF_KERNEL_SIZE-1);
+				//weightN = 1.0/(2* BLUR_HALF_KERNEL_SIZE-1);
+				//uvP = uv + 2*float(i) *  dir / vec2(HSX,HSY);
+				//uvN = uv - 2*float(i) *  dir / vec2(HSX,HSY);
+			}
+
+
+			vec4 leftSample = texture( tex, uvP );
+			float zclosel = step(abs(myDistance - leftSample.z), ZTHRESHOLD);
+			float dotclosel = smoothstep(MINCOSANGLE, 1.0, dot(myNormal.xy, NORM2SNORM(leftSample.xy)));
+			dotclosel = max(dotclosel, imground);
+			float leftWeight = weightP * dotclosel * zclosel;
+			weightSum += leftWeight;
+			howLit += leftWeight * leftSample.a;
+			unWeighted += weightP * leftSample.a;
+
+			vec4 rightSample = texture( tex, uvN );
+			float zcloser = step(abs(myDistance - rightSample.z), ZTHRESHOLD);
+			float dotcloser = smoothstep(MINCOSANGLE, 1.0, dot(myNormal.xy, NORM2SNORM(rightSample.xy)));
+			dotcloser = max(dotclosel, imground);
+			float rightWeight = weightN * dotcloser * zcloser;
+			weightSum += rightWeight;
+			howLit += rightWeight * rightSample.a;
+			unWeighted += weightP * rightSample.a;
+		}
+
+		howLit = howLit / weightSum;		
+		if (weightSum < (weights[0] + MINSELFWEIGHT) ){
+			howLit = mix(howLit,unWeighted, OUTLIERCORRECTIONFACTOR);
+
+		};
+		//if (imground > 0.5) {howLit = unWeighted;}
+		gl_FragColor = vec4(texSample.rgb, howLit);
+
+		// FINAL MIXDOWN, vert pass last
+		if (dir.y > 0.5) { 
+			if (imground > 0.5) howLit = howLit * howLit;
+			howLit = pow(howLit, BLUR_POWER);
+			howLit = sqrt(howLit * howLit + BLUR_CLAMP);
+			#if DEBUG_BLUR == 1 
+				gl_FragColor.rgba = vec4(howLit);
+			#else
+				gl_FragColor.rgba = vec4(howLit);
+				#if (BRIGHTEN != 0) 
+					if (imground < 0.5) {  // is model fragment
+						float distRatio = SSAO_FADE_DIST_1/(1.0 * SSAO_FADE_DIST_0);
+						float distFactor = 1.0 - smoothstep(0, distRatio, myDistance - distRatio ); // 0 at far distance, 1 at near distance
+
+						float brightenFactor = (BRIGHTEN/255.0) * howLit * distFactor;
+						gl_FragColor.rgb = vec3(brightenFactor);
+					}else{
+						gl_FragColor.rgb = vec3(0);
+
+					}
+				#endif
+				gl_FragColor.rgb *= strengthMult;
+				gl_FragColor.a = 1.0 - ((1.0 - gl_FragColor.a) * strengthMult );
+			#endif
+
+		};
 }
