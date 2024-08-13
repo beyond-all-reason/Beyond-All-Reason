3b022e6c6bda5c25b29afaeb61cc4877f00b5415
icexuick <info@ixstudios.nl>
2024-08-14 00:34:59 +0200

    Updated tweaks to mixing teamcolor

--- modelmaterials_gl4/templates/cus_gl4.frag.glsl
+++ modelmaterials_gl4/templates/cus_gl4.frag.glsl
@@ -1293,7 +1293,7 @@ void main(void){
 			}
 			float sintime =	fract(simFrame * 0.02); // pulses every 3 seconds
 			myPerlin.g = myPerlin.g * 0.5 + 0.5;
-			texColor2.a = 1.0 - clamp(cloakedness*0.54, 0.0, 0.54);
+			texColor2.a = 1.0 - clamp(cloakedness*0.49, 0.0, 0.49);
 			float perlinline1 = clamp(1.0 - 20* abs(myPerlin.g - fract(simFrame * 0.005)), 0.0, 1.0);
 			float perlinline2 = clamp(1.0 - 20* abs(myPerlin.g - fract(simFrame * 0.005 +0.5)), 0.0, 1.0);
 			float cloaknoise = cloakedness*perlinline1 + cloakedness*perlinline2;
@@ -1304,7 +1304,7 @@ void main(void){
 
 			float highLightOpacity = clamp(1.0 - dotcamera, 0, 1);
 			highLightOpacity = highLightOpacity * highLightOpacity;
-			outColor.rgb += highLightOpacity * teamCol.rgb * 1.0 * cloakedness;
+			outColor.rgb = mix(outColor.rgb, teamCol.rgb * 3.0, highLightOpacity * cloakedness);
 			
 			//Add bloom to the perlin noise:
 			outSpecularColor.rgb+= vec3(clamp(cloaknoise * 0.75,0.0,1.0));
