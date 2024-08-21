92a521025cdc3755ebabdf9eaa2c57b8a7152076
Tharsis <tharsisvein@gmail.com>
2024-08-21 16:26:31 +1200

    Update DeferredLightsGL4config.lua

--- luaui/configs/DeferredLightsGL4config.lua
+++ luaui/configs/DeferredLightsGL4config.lua
@@ -1258,7 +1258,18 @@ local unitLights = {
 							lifetime = 0, sustain = 0, animtype = 0},
 		},
 	},
-	['legnanotct2'] = {
+	['legnanotc'] = {
+		nanolight = {
+			lightType = 'point',
+			pieceName = 'emitnano',
+			lightConfig = { posx = 0, posy = 0, posz = 1, radius = 33,
+							color2r = 0, color2g = 0, color2b = 0, colortime = 0,
+							r = -1, g = 1, b = 1, a = 0.4,
+							modelfactor = 0.5, specular = 0.5, scattering = 1, lensflare = 10,
+							lifetime = 0, sustain = 0, animtype = 0},
+		},
+	},
+	['legnanotcplat'] = {
 		nanolight = {
 			lightType = 'point',
 			pieceName = 'emitnano',
