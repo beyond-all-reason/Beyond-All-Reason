53800cce65dbd06264c941dc9a6a48599868bd96
SethDGamre <sethdgamre@gmail.com>
2024-08-25 21:10:16 -0500

    Update unit_transport_fall_damage.lua
    this PR fixes the gadget so it doesn't sometimes randomly kill units being unloaded normally.
    This fixes the evocom's effigies not protecting commanders being transported

--- luarules/gadgets/unit_transport_fall_damage.lua
+++ luarules/gadgets/unit_transport_fall_damage.lua
@@ -2,25 +2,26 @@ function gadget:GetInfo()
 	return {
 		name = "Fall Damage from Transports",
 		desc = "All units that fall from transports except Commandos receive height and mass proportional damage",
-		author = "Beherith",
+		author = "Beherith, SethDGamre",
 		date = "2023.06.22",
 		license = "GNU GPL, v2 or later",
 		layer = 0,
-		enabled = false
+		enabled = true
 	}
 end
 
 if not gadgetHandler:IsSyncedCode() then
 	return
 end
+--this gadget is enabled as a bandaid fix for commander effigies. Units cannot be moved when released from transports using Spring.SetUnitPosition until it lands, meaning effigy transposition cannot occur until commander lands. This gadget is pending approval from the GDT for mainline use.
 
 --use customparams.fall_damage_multiplier = <number> to overwrite default damage multiplier as defined below.
-
 local fallDamageMultipliers = {}
 local masses = {}
 local droppedunits = {}
 local heightThreshold = 32 -- if unit is at least 32 elmos up consider it falling
-local defaultDamageMult = 0.02 -- damage is 2% of mass * height if not defined in customParam.fall_damage_multiplier
+local landedThreshold = 0 -- once the unit's height is equal or below ground height, take damage.
+local defaultDamageMult = 0.03 -- damage is 3% of mass * dropHeight if not defined in customParam.fall_damage_multiplier
 
 for unitDefID, unitDef in ipairs(UnitDefs) do
 	masses[unitDefID] = unitDef.mass
@@ -32,30 +33,33 @@ end
 local function GetUnitHeightAboveGroundAndWater(unitID) -- returns nil for invalid units
 	if (Spring.GetUnitIsDead(unitID) ~= false) or (Spring.ValidUnitID(unitID) ~= true) then return nil end
 
-	local px, py, pz = Spring.GetUnitBasePosition(unitID)
+	local px, py, pz = Spring.GetUnitPosition(unitID)
 	if px and py and pz  then
-		local gh = math.max(0, Spring.GetGroundHeight( px, pz ))
-		return py - gh
+		local groundHeight = math.max(0, Spring.GetGroundHeight( px, pz ))
+		return py - groundHeight
 	else
 		return nil
 	end
 end
 
 function gadget:UnitUnloaded(unitID, unitDefID, unitTeam, transportID, transportTeam)
-	local unitHeight = GetUnitHeightAboveGroundAndWater(unitID)
-	if unitHeight and unitHeight > heightThreshold then
-		local damageMult = fallDamageMultipliers[unitDefID] or defaultDamageMult
-		droppedunits[unitID] = unitHeight * masses[unitDefID] * damageMult
+	local deadTransport = Spring.GetUnitIsDead(transportID)
+	if deadTransport then
+		local unitDropDistance = GetUnitHeightAboveGroundAndWater(unitID)
+		if unitDropDistance and unitDropDistance > heightThreshold then
+			local damageMult = fallDamageMultipliers[unitDefID] or defaultDamageMult
+			droppedunits[unitID] = unitDropDistance * masses[unitDefID] * damageMult
+		end
 	end
 end
 
 function gadget:GameFrame()
 	if next(droppedunits) then
-		for unitID, falldamage in pairs(droppedunits) do
+		for unitID, dropDamage in pairs(droppedunits) do
 			local unitHeight = GetUnitHeightAboveGroundAndWater(unitID)
 			if unitHeight then
-				if unitHeight < heightThreshold then
-					Spring.AddUnitDamage(unitID, falldamage, 0, Spring.GetGaiaTeamID(), 1)
+				if unitHeight <= landedThreshold then
+					Spring.AddUnitDamage(unitID, dropDamage, 0, Spring.GetGaiaTeamID(), 1)
 					droppedunits[unitID] = nil
 				end
 			else -- dead
