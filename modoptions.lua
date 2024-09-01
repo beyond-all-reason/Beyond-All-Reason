ab1570bc287b5d6594899cb392695313f974099f
SethDGamre <165520713+SethDGamre@users.noreply.github.com>
2024-08-31 19:00:22 -0500

    Expired modoption cleanup incl. Energy Sharing Rework, XP Multiplier, and Experimental Air Collisions (#3658)
    The game design team decided these lobby options needed to go.
    
        - Deleted everything related to energy sharing rework. Tested to make sure the status bar at the top of the screen still worked fine due to partial integration.
    
        - Removed general XP gain multiplier from lobby options.
    
        - Removed experimental no air collision modoption, but retained default behavior.

--- modoptions.lua
+++ modoptions.lua
@@ -1157,29 +1157,6 @@ local options = {
     -- Hidden Tests
 
     {
-        key    	= "experimentalnoaircollisions",
-        name   	= "Aircraft Collisions Override",
-        desc   	= "Aircraft Collisions Override",
-        hidden 	= true,
-        type   	= "bool",
-        section = "options_experimental",
-        def  	= false,
-    },
-
-    {
-        key    	= "experimentalxpgain",
-        name   	= "XP Gain Multiplier",
-        desc   	= "XP Gain Multiplier",
-        hidden 	= true,
-        type   	= "number",
-        section = "options_experimental",
-        def    	= 1,
-        min    	= 0.1,
-        max    	= 10,
-        step   	= 0.1,
-    },
-
-    {
         key    	= "experimentalstandardgravity",
         name   	= "Gravity Override",
         desc   	= "Override map gravity for weapons",
@@ -1270,16 +1247,6 @@ local options = {
         key 	= "junorework",
         name 	= "Juno Rework",
         desc 	= "Juno stuns certain units (such as radars and jammers) rather than magically deleting them",
-        type 	= "bool",
-        hidden 	= true,
-        section = "options_experimental",
-        def 	= false,
-    },
-
-    {
-        key 	= "energy_share_rework",
-        name 	= "Energy Share Rework",
-        desc 	= "Additional energy overflow/underflow mechanics. 10% of the energy income is re-distributed to prevent E-stalling.",
         type 	= "bool",
         hidden 	= true,
         section = "options_experimental",
