fa1fb80100775ccd3c945b325308013469f3da16
SethDGamre <sethdgamre@gmail.com>
2024-09-01 20:34:37 -0500

    adjust cegs for capture clouds to be easier to see through

--- luarules/gadgets/scav_spawner_defense.lua
+++ luarules/gadgets/scav_spawner_defense.lua
@@ -1948,7 +1948,7 @@ if gadgetHandler:IsSyncedCode() then
 								Spring.SetUnitHealth(unitID, {health = maxHealth})
 								SendToUnsynced("unitCaptureFrame", unitID, 0.95)
 								Spring.SpawnCEG("scav-spawnexplo", ux, uy, uz, 0,0,0)
-								--Spring.SpawnCEG("scavmistxl", ux, uy+100, uz, 0,0,0)
+								Spring.SpawnCEG("scavmist", ux, uy+100, uz, 0,0,0)
 								Spring.SpawnCEG("scavradiation", ux, uy+100, uz, 0,0,0)
 								Spring.SpawnCEG("scavradiation-lightning", ux, uy+100, uz, 0,0,0)
 
@@ -1956,15 +1956,15 @@ if gadgetHandler:IsSyncedCode() then
 							else
 								Spring.SetUnitHealth(unitID, {capture = math.min(captureLevel+captureProgress, 1)})
 								SendToUnsynced("unitCaptureFrame", unitID, math.min(captureLevel+captureProgress, 1))
-								Spring.SpawnCEG("scav-spawnexplo", ux, uy, uz, 0,0,0)
-								if math.random() <= 0.25 then
-									Spring.SpawnCEG("scavmistxl", ux, uy+100, uz, 0,0,0)
+								Spring.SpawnCEG("scaspawn-trail", ux, uy, uz, 0,0,0)
+								if math.random() <= 0.1 then
+									Spring.SpawnCEG("scavmist", ux, uy+100, uz, 0,0,0)
 								end
 								if math.random() <= 0.1 then
-									Spring.SpawnCEG("scavradiation", ux, uy+100, uz, 0,0,0)
+									--Spring.SpawnCEG("scavradiation", ux, uy+100, uz, 0,0,0)
 								end
 								if math.random() <= 0.1 then
-									Spring.SpawnCEG("scavradiation-lightning", ux, uy+100, uz, 0,0,0)
+									--Spring.SpawnCEG("scavradiation-lightning", ux, uy+100, uz, 0,0,0)
 								end
 								GG.addUnitToCaptureDecay(unitID)
 							end
