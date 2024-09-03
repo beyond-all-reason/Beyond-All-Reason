3143dbb7122abbfef2d92acbf284589cff80dbe5
efrec <quantumscrewdriver@gmail.com>
2024-09-03 19:38:42 -0400

    middle path: split params, no UnitDestroyed
    and since I'm here: replaced text matching for resistances with a lookup, moved setup to Initialize

--- luarules/gadgets/unit_area_timed_damage.lua
+++ luarules/gadgets/unit_area_timed_damage.lua
@@ -17,8 +17,9 @@ end
 
 --------------------------------------------------------------------------------
 
-local TimedDamageWeapons = {}
-local TimedDamageDyingUnits = {}
+local timedDamageWeapons = {}
+local unitDamageImmunity = {}
+local aliveExplosions = {}
 
 -- Params:
 -- ceg - ceg to spawn when explosion happens
@@ -28,52 +29,78 @@ local TimedDamageDyingUnits = {}
 -- range - from center to edge, in elmos
 -- resistance - defines which units are resistant to this type of damage when it matches with 'areadamageresistance' customparameter in a unit.
 
-for weaponDefID, weaponDef in ipairs(WeaponDefs) do
-    if weaponDef.customParams and weaponDef.customParams.timed_area_ceg then
-        local params = weaponDef.customParams
-        TimedDamageWeapons[weaponDefID] = {
-            ceg        = params.area_onhit_ceg,
-            damageCeg  = params.area_onhit_damageceg, -- lowercase
-            resistance = params.area_onhit_resistance   ,
-            damage     = tonumber(params.area_onhit_damage),
-            range      = tonumber(params.area_onhit_range),
-            time       = tonumber(params.area_onhit_time),
-        }
-    end
-end
-
-for unitDefID, unitDef in ipairs(UnitDefs) do
-    if unitDef.customParams.timed_area_ceg then
-        local params = unitDef.customParams
-        TimedDamageDyingUnits[unitDefID] = {
-            ceg        = params.area_ondeath_ceg,
-            damageCeg  = params.area_ondeath_damageceg, -- lowercase
-            resistance = params.area_ondeath_resistance,
-            damage     = tonumber(params.area_ondeath_damage),
-            range      = tonumber(params.area_ondeath_range),
-            time       = tonumber(params.area_ondeath_time),
-            weapon     = WeaponDefNames[UnitDefs[unitDefID].deathExplosion].id or -1,
-        }
-    end
-end
-
 --------------------------------------------------------------------------------
 
-local aliveExplosions = {}
-
 function gadget:Initialize()
-    for weaponID in pairs(TimedDamageWeapons) do
-        Script.SetWatchExplosion(weaponID, true)
+    local areaDamageTypes = {}
+    for weaponDefID, weaponDef in ipairs(WeaponDefs) do
+        if weaponDef.customParams and weaponDef.customParams.area_onhit_ceg then
+            local custom = weaponDef.customParams
+            local params = {
+                ceg        = custom.area_onhit_ceg,
+                damageCeg  = custom.area_onhit_damageceg,
+                resistance = string.lower(custom.area_onhit_resistance),
+                damage     = tonumber(custom.area_onhit_damage),
+                range      = tonumber(custom.area_onhit_range),
+                time       = tonumber(custom.area_onhit_time),
+            }
+            if params.resistance then
+                areaDamageTypes[params.resistance] = true
+            else
+                params.resistance = "none"
+            end
+            timedDamageWeapons[weaponDefID] = params
+        end
+    end
+    for unitDefID, unitDef in ipairs(UnitDefs) do
+        if unitDef.customParams.area_ondeath_ceg then
+            local custom = unitDef.customParams
+            local params = {
+                ceg        = custom.area_ondeath_ceg,
+                damageCeg  = custom.area_ondeath_damageceg,
+                resistance = string.lower(custom.area_onhit_resistance),
+                damage     = tonumber(custom.area_ondeath_damage),
+                range      = tonumber(custom.area_ondeath_range),
+                time       = tonumber(custom.area_ondeath_time),
+            }
+            if params.resistance ~= nil then
+                areaDamageTypes[params.resistance] = true
+            else
+                params.resistance = "none"
+            end
+            timedDamageWeapons[WeaponDefNames[unitDef.deathExplosion].id] = params
+            timedDamageWeapons[WeaponDefNames[unitDef.selfDExplosion].id] = params
+        end
     end
 
-    if not next(TimedDamageWeapons) and not next(TimedDamageDyingUnits) then
+    for unitDefID, unitDef in ipairs(UnitDefs) do
+        local immunities = {}
+        if unitDef.canFly then
+            immunities["all"] = true
+        elseif unitDef.customParams.areadamageresistance then
+            local resistance = string.lower(unitDef.customParams.areadamageresistance)
+            for damageType in pairs(areaDamageTypes) do
+                if string.find(resistance, damageType, nil, true) then
+                    immunities[damageType] = true
+                end
+            end
+        end
+        unitDamageImmunity[unitDefID] = immunities
+    end
+
+    if next(timedDamageWeapons) then
+        for weaponDefID in pairs(timedDamageWeapons) do
+            Script.SetWatchExplosion(weaponDefID, true)
+        end
+    else
+        Spring.Log(gadget:GetInfo().name, LOG.INFO, "No timed areas found. Removing gadget.")
         gadgetHandler:RemoveGadget(self)
     end
 end
 
-function gadget:Explosion(weaponDefID, px, py, pz, AttackerID, ProjectileID)
-    if TimedDamageWeapons[weaponDefID] ~= nil then
-        local explosion = TimedDamageWeapons[weaponDefID]
+function gadget:Explosion(weaponDefID, px, py, pz, attackerID, projectileID)
+    if timedDamageWeapons[weaponDefID] ~= nil then
+        local explosion = timedDamageWeapons[weaponDefID]
         if py <= math.max(Spring.GetGroundHeight(px, pz), 0) + explosion.range*0.5 then
             local currentTime = Spring.GetGameSeconds()
             aliveExplosions[#aliveExplosions+1] = {
@@ -86,31 +113,8 @@ function gadget:Explosion(weaponDefID, px, py, pz, AttackerID, ProjectileID)
                 ceg = explosion.ceg,
                 damageCeg = explosion.damageCeg,
                 resistance = explosion.resistance,
-                owner = AttackerID,
+                owner = attackerID,
                 weapon = weaponDefID,
-            }
-        end
-    end
-end
-
-function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
-    if TimedDamageDyingUnits[unitDefID] ~= nil then
-        local explosion = TimedDamageDyingUnits[unitDefID]
-        local px, py, pz = Spring.GetUnitPosition(unitID)
-        if py <= math.max(Spring.GetGroundHeight(px, pz), 0) + explosion.range*0.5 then
-            local currentTime = Spring.GetGameSeconds()
-            aliveExplosions[#aliveExplosions+1] = {
-                x = px,
-                y = math.max(Spring.GetGroundHeight(px, pz), 0),
-                z = pz,
-                endTime = currentTime + explosion.time,
-                damage = explosion.damage,
-                range = explosion.range,
-                ceg = explosion.ceg,
-                damageCeg = explosion.damageCeg,
-                resistance = explosion.resistance,
-                owner = unitID,
-                weapon = explosion.weapon,
             }
         end
     end
@@ -125,19 +129,15 @@ function gadget:GameFrame(frame)
                 local y = explosionStats.y
                 local z = explosionStats.z
                 local damage = explosionStats.damage*0.733
-                local resistance = explosionStats.resistance
+                local damageType = explosionStats.resistance
 
                 Spring.SpawnCEG(explosionStats.ceg, x, y + 8, z, 0, 0, 0)
 
                 local unitsInRange = Spring.GetUnitsInSphere(x, y, z, explosionStats.range)
                 for j = 1,#unitsInRange do
                     local unitID = unitsInRange[j]
-                    local unitDef = UnitDefs[Spring.GetUnitDefID(unitID)]
-                    if not (
-                        unitDef.canFly or
-                        (unitDef.customParams.areadamageresistance ~= nil and
-                            string.find(unitDef.customParams.areadamageresistance, resistance, nil, true))
-                    ) then
+                    local unitImmunities = unitDamageImmunity[Spring.GetUnitDefID(unitID)]
+                    if not unitImmunities.all and not unitImmunities[damageType] then
                         Spring.AddUnitDamage(unitID, damage, 0, explosionStats.owner, explosionStats.weapon)
                         local ux, uy, uz = Spring.GetUnitPosition(unitID)
                         Spring.SpawnCEG(explosionStats.damageCeg, ux, uy + 8, uz, 0, 0, 0)
