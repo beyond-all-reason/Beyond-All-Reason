6ad4be04b8516a3815a39fd5b00e70f4b55ca68a
SethDGamre <sethdgamre@gmail.com>
2024-09-08 00:33:40 -0500

    add max impulse limitations to gadget
    now this gadget also stops space launches by setting a max impulse that can be taken by units.
    
    It works consistently up  until units pinball off each other uncommonly causing them to bounce unpredictably. It's still within almost acceptable parems and the fall damage takes care of any cheesy launch strats.
    
    performance measured with 200 catapults firing at once against 200ish mammoths. Performance spike of .3%. During a crazy metal map 6v6 barb game that filled all of adamantine whatever that map's called, the largest performance hit I saw was .18%. Most of the time it takes 0% performance and very little loading %

--- luarules/gadgets/unit_collision_damage_behavior.lua
+++ luarules/gadgets/unit_collision_damage_behavior.lua
@@ -1,7 +1,7 @@
 function gadget:GetInfo()
 	return {
 		name = "Collision Damage Behavior",
-		desc = "Magnifies the default engine ground and object collision damage and changes the behavior thereof",
+		desc = "Magnifies the default engine ground and object collision damage and handles max impulse limits",
 		author = "SethDGamre",
 		date = "2024.8.29",
 		license = "GNU GPL, v2 or later",
@@ -10,39 +10,87 @@ function gadget:GetInfo()
 	}
 end
 
-if not gadgetHandler:IsSyncedCode() then
-	return
-end
---this gadget 
+if not gadgetHandler:IsSyncedCode() then return end
 
 --use customparams.fall_damage_multiplier = <number> a multiplier that's applied to defaultDamageMultiplier.
 local fallDamageMagnificationFactor = 100 --the multiplier by which default engine ground collision damage is multiplied. 
 
-local spGetUnitVelocity = Spring.GetUnitVelocity
+
 
 local groundCollisionDefID = Game.envDamageTypes.GroundCollision
 local objectCollisionDefID = Game.envDamageTypes.ObjectCollision
-local objectCollisionVelocityThreshold = 3.3
-local fallDamageMultipliers = {}
+local objectCollisionVelocityThreshold = 3.3 --this defines how fast a unit has to be moving in order to take object collision damage
+local maxImpulseThreshold = 0.08 --incease this to make units move less from impulse. This defines the max impulse damage a unit can take expressed in a percentage multiplier
+local cooldownsFrameThreshold = Game.gameSpeed --this is how long in frames before a unit's maximum impulse limit resets.
 
+local spGetUnitVelocity = Spring.GetUnitVelocity
+local mathmin = math.min
+
+local fallDamageMultipliers = {}
+local unitsMaxImpulse = {}
+local weaponDefIDImpulses = {}
+local overImpulseCooldowns = {}
+
+local currentModulusFrame = 0
 
 for unitDefID, unitDef in ipairs(UnitDefs) do
 	local fallDamageMultiplier = unitDef.customParams.fall_damage_multiplier or 1.0
 	fallDamageMultipliers[unitDefID] = fallDamageMultiplier*fallDamageMagnificationFactor
+	unitsMaxImpulse[unitDefID] = unitDef.mass/maxImpulseThreshold
+end
+
+for weaponDefID, wDef in ipairs(WeaponDefs) do
+	if wDef.damages and wDef.damages.impulseBoost and wDef.damages.impulseFactor then
+		weaponDefIDImpulses[weaponDefID] = {impulseboost = wDef.damages.impulseBoost, impulsefactor = wDef.damages.impulseFactor}
+	end
+end
+
+local function calculateImpulseData(unitDefID, damage, weaponDefID)
+	local impulse = (damage + weaponDefIDImpulses[weaponDefID].impulseboost) * weaponDefIDImpulses[weaponDefID].impulsefactor
+	local maxImpulse = unitsMaxImpulse[unitDefID]
+	local impulseMultiplier = mathmin(maxImpulse/impulse, 1)
+	return impulse, impulseMultiplier
 end
 
 function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
-	if weaponDefID == objectCollisionDefID then
-		local _, velY, _, velLength = spGetUnitVelocity(unitID)
-		if velLength > objectCollisionVelocityThreshold and -velY > (velLength/3) then --prevents mostly horizontal object collisions from taking damage, allows damage if dropped from above
-			damage = damage*fallDamageMultipliers[unitDefID]
-			return damage
+	if weaponDefID > 0 then --this section handles limiting maximum impulse
+		local impulseMultiplier = 1
+		local impulse = 0
+		if not overImpulseCooldowns[unitID] then
+			impulse, impulseMultiplier = calculateImpulseData(unitDefID, damage, weaponDefID)
+			overImpulseCooldowns[unitID] = {expireframe = currentModulusFrame + cooldownsFrameThreshold, highestimpulse = impulse, impulsemultiplier = impulseMultiplier}
+			return damage, impulseMultiplier
+		elseif overImpulseCooldowns[unitID] and overImpulseCooldowns[unitID].highestimpulse < impulse then
+			impulse, impulseMultiplier = calculateImpulseData(unitDefID, damage, weaponDefID)
+			impulseMultiplier = impulseMultiplier - overImpulseCooldowns[unitID].impulsemultiplier
+			overImpulseCooldowns[unitID] = {expireframe = currentModulusFrame + cooldownsFrameThreshold, highestimpulse = impulse, impulsemultiplier = impulseMultiplier}
+			return damage, impulseMultiplier
 		else
-			return 0
+			return damage, 0
 		end
-		return 0
-	elseif weaponDefID == groundCollisionDefID then
-		damage = damage*fallDamageMultipliers[unitDefID]
-		return damage
+	else
+		if weaponDefID == groundCollisionDefID then --handles ground collision events
+			damage = damage*fallDamageMultipliers[unitDefID]
+			return damage, 0
+		elseif weaponDefID == objectCollisionDefID then --handles object collision events
+			local _, velY, _, velLength = spGetUnitVelocity(unitID)
+			if velLength > objectCollisionVelocityThreshold and -velY > (velLength/3) then --prevents mostly horizontal object collisions from taking damage, allows damage if dropped from above
+				damage = damage*fallDamageMultipliers[unitDefID]
+				return damage, 0
+			else
+				return 0, 0
+			end
+		end
+	end
+end
+
+function gadget:GameFrame(frame)
+	if frame % 5 == 3 then
+		for unitID, data in pairs (overImpulseCooldowns) do
+			if data.expireframe < frame  then --this is done to prevent a unit from being impulsed multiple times in a compounding fashion in a performant way.
+				overImpulseCooldowns[unitID] = nil
+			end
+		end
+		currentModulusFrame = frame
 	end
 end
