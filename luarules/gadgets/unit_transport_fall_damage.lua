aba256e09c70b1fffec013971ef58d2780c0bfe0
SethDGamre <sethdgamre@gmail.com>
2024-09-09 19:13:53 -0500

    cleaning up unit_transport_behavior some

--- luarules/gadgets/unit_transport_fall_damage.lua
+++ luarules/gadgets/unit_transport_fall_damage.lua
@@ -15,7 +15,7 @@ if not gadgetHandler:IsSyncedCode() then
 end
 --this gadget is enabled as a bandaid fix for commander effigies. Units cannot be moved when released from transports using Spring.SetUnitPosition until it lands, meaning effigy transposition cannot occur until commander lands. This gadget is pending approval from the GDT for mainline use.
 
---use customparams.fall_damage_multiplier = <number> to overwrite defaultDamageMult
+--use customparams.fall_damage_multiplier = <number> to overwrite defaultDamageMultiplier
 local defaultDamageMultiplier = 1.0 -- A multiplier representing the percentage of health lost from the velocity of a freefall. Also applies proportionally to velocities from explosion impulse.
 local velocityStopThresholdMultiplier = 0.4 -- once the unit's velocity is equal to or below peakvelocity*velocityStopDivisor, take damage.
 local velocityStopCountThreshold = 3 --number of frames below threshold before damage is applied
@@ -27,7 +27,9 @@ local fallingUnits = {}
 local transportedUnits = {}
 
 for unitDefID, unitDef in ipairs(UnitDefs) do
-	dropDamages[unitDefID] = unitDef.customParams.fall_damage_multiplier or math.ceil(unitDef.health*defaultDamageMult/5)
+	local damageReductionMultiplier = 0.2
+	local baseDamageMultiplier = unitDef.customParams.fall_damage_multiplier or defaultDamageMultiplier
+	dropDamages[unitDefID] = math.ceil(unitDef.health*baseDamageMultiplier * damageReductionMultiplier)
 end
 
 local function CheckValidUnitVelocity(unitID) -- returns nil for invalid units
@@ -53,44 +55,42 @@ end
 
 function gadget:UnitDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
 	local velX, velY, velZ, velLength = Spring.GetUnitVelocity(unitID)
-	if velLength > velocityThreshold and not fallingUnits[unitID] and not transportedUnits[unitID] then
+	if not fallingUnits[unitID] and not transportedUnits[unitID] and velLength > velocityThreshold then
 		fallingUnits[unitID] = {unitdefid = unitDefID, damagemultiplier = dropDamages[unitDefID], peakvelocity = 0, stopcount = 0}
 	end
 end
 
 function gadget:GameFrame()
-	if next(fallingUnits) then
-		for unitID, data in pairs(fallingUnits) do
-			if data.transportid then
-				local deadTransport = Spring.GetUnitIsDead(data.transportid)
-				if deadTransport then
-					data.transportid = nil -- if transport is dead, remove its ID from the falling unit
-				end
+	for unitID, data in pairs(fallingUnits) do
+		if data.transportid then
+			local deadTransport = Spring.GetUnitIsDead(data.transportid)
+			if deadTransport then
+				data.transportid = nil -- if transport is dead, remove its ID from the falling unit
 			end
-			local velX, velY, velZ, currentVelocity = CheckValidUnitVelocity(unitID)
-			if currentVelocity then
-				if data.peakvelocity > currentVelocity then -- velocity slowing
-					if currentVelocity <= (data.peakvelocity * velocityStopThresholdMultiplier) then -- landed
-						if not data.vely then 
-							data.vely = velY
-							data.velocity = currentVelocity
-						end
-						data.stopcount = data.stopcount+1
-						if data.stopcount > velocityStopCountThreshold then
-							if not data.transportid and data.vely ~= 0 and data.velocity ~= 0 then -- if no transport, damage
-								local yProportion = data.vely / data.velocity
-								yProportion = -yProportion
-								Spring.AddUnitDamage(unitID, damage, 0, Spring.GetGaiaTeamID(), 1)
-							end
-							fallingUnits[unitID] = nil -- remove after landing
-						end
+		end
+		local velX, velY, velZ, currentVelocity = CheckValidUnitVelocity(unitID)
+		if currentVelocity then
+			if data.peakvelocity > currentVelocity then -- velocity slowing
+				if currentVelocity <= (data.peakvelocity * velocityStopThresholdMultiplier) then -- landed
+					if not data.vely then 
+						data.vely = velY
+						data.velocity = currentVelocity
 					end
-				elseif not data.transportid then -- increase peak velocity if not protected by transport
-					data.peakvelocity = currentVelocity
+					data.stopcount = data.stopcount+1
+					if data.stopcount > velocityStopCountThreshold then
+						if not data.transportid and data.vely ~= 0 and data.velocity ~= 0 then -- if no transport, damage
+							local yProportion = data.vely / data.velocity
+							yProportion = -yProportion
+							Spring.AddUnitDamage(unitID, damage, 0, Spring.GetGaiaTeamID(), 1)
+						end
+						fallingUnits[unitID] = nil -- remove after landing
+					end
 				end
-			else -- dead
-				fallingUnits[unitID] = nil
+			elseif not data.transportid then -- increase peak velocity if not protected by transport
+				data.peakvelocity = currentVelocity
 			end
+		else -- dead
+			fallingUnits[unitID] = nil
 		end
 	end
 end
