3374eea2c7449e3ef076cb6af9207cc368198583
Citrine <salinecitrine@gmail.com>
2024-09-12 13:40:24 -0700

    refactor(blueprint): set layer to 1, to allow other keybinds priority (#3687)
    * refactor(blueprint): set layer to 1, to allow other keybinds priority
    
    This allows things like adding `bind  Alt+sc_c gridmenu_key 1 3` before
    blueprint binds, so that, when using grid menu, alt+build on a factory
    builds a unit instead of creating a blueprint.

--- luaui/Widgets/cmd_blueprint.lua
+++ luaui/Widgets/cmd_blueprint.lua
@@ -3,7 +3,7 @@ function widget:GetInfo()
 		name = "Blueprint",
 		desc = "Saves and queues groups of unit blueprints",
 		license = "GNU GPL, v2 or later",
-		layer = 0,
+		layer = 1, -- after gridmenu(0), to let factories use alt+xyz hotkeys
 		enabled = true,
 		handler = true,
 	}
