dd6ea1f43756dd568e5f0ea22ea7ec9f074225f4
robertthepie <stanlytheplatypus@gmail.com>
2024-09-20 15:08:42 +0100

    How to play raptors/scavs in adv options

--- modoptions.lua
+++ modoptions.lua
@@ -507,6 +507,14 @@ local options = {
         def     =  true,
     },
 
+	{
+		key		= "sub_header",
+		name	= "To Play Add a Raptors AI to the enemy Team: [Add AI]; [RaptorsAI]",
+		desc	= "",
+		section	= "raptor_defense_options",
+		type	= "subheader",
+	},
+
     {
         key     = "sub_header",
         section = "raptor_defense_options",
@@ -656,6 +664,15 @@ local options = {
         type    = "subheader",
         def     =  true,
     },
+
+	{
+		key		= "sub_header",
+		name	= "To Play Add a Scavangers AI to the enemy Team: [Add AI]; [ScavangersAI]",
+		desc	= "",
+		section	= "scav_defense_options",
+		type	= "subheader",
+	},
+
 
     {
         key     = "sub_header",
