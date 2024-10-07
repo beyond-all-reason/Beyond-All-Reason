af88b29f25a7b4eb1713d75ae2f406e612b7ab69
SethDGamre <165520713+SethDGamre@users.noreply.github.com>
2024-10-07 17:19:21 -0500

    Fix Scorpion wonky range issues when firing from elevated positions (#3794)
    * fix wonky scorpion height range issues
    
    * fix non-functional collapsed state dmg reduction
    
    * Revert "fix non-functional collapsed state dmg reduction"
    
    This reverts commit deec920a4f58dc96e50bf38e9551fcfa3f11c8c3.

--- units/CorBuildings/LandDefenceOffence/corvipe.lua
+++ units/CorBuildings/LandDefenceOffence/corvipe.lua
@@ -104,7 +104,6 @@ return {
 			vipersabot = {
 				areaofeffect = 24,
 				avoidfeature = false,
-				burnblow = true,
 				cegtag = "missiletrailviper",
 				craterareaofeffect = 0,
 				craterboost = 0,
