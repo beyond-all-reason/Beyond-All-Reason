d2bd7ae5b7bdd6eb1d23eaef54a2189c405c0489
efrec <quantumscrewdriver@gmail.com>
2024-10-08 12:15:16 -0400

    Trigger timed areas per-frame. Nicer customparam loading.

--- luarules/gadgets/unit_area_timed_damage.lua
+++ luarules/gadgets/unit_area_timed_damage.lua
@@ -26,6 +26,10 @@ local areaSizePresets = {
      200, 225, 250, 275, 300,
 }
 
+-- Defaults and customparams
+local prefixes = { unit = 'area_ondeath_', weapon = 'area_onhit_' }
+local damage, time, range, resistance = 30, 10, 75, "none"
+
 --------------------------------------------------------------------------------
 -- Local variables -------------------------------------------------------------
 
@@ -47,84 +51,92 @@ local frameIndex
 damageInterval = math.round(Game.gameSpeed * damageInterval)
 
 --------------------------------------------------------------------------------
+-- Local functions -------------------------------------------------------------
+
+local function getExplosionParams(def, prefix)
+    local params = {
+        ceg        = def.customParams[ prefix.."ceg"        ],
+        damageCeg  = def.customParams[ prefix.."damageceg"  ],
+        resistance = def.customParams[ prefix.."resistance" ] or resistance,
+        damage     = def.customParams[ prefix.."damage"     ] or damage,
+        frames     = def.customParams[ prefix.."time"       ] or time,
+        range      = def.customParams[ prefix.."range"      ] or range,
+    }
+    params.damage = tonumber(params.damage) * (damageInterval/Game.gameSpeed)
+    params.frames = tonumber(params.frames) * Game.gameSpeed
+    params.frames = math.round(params.frames / damageInterval) * damageInterval
+    params.range = tonumber(params.range)
+    params.resistance = string.lower(params.resistance)
+    return params
+end
+
+-- Change (eg) fire-area-150-repeating to fire-area-<range>-repeating for tweakdefs:
+local sub1, sub2 = '-area-', '-repeating'
+local pattern = sub1..'\d+'..sub2
+local midX, midZ = Game.mapSizeX / 2, Game.mapSizeZ / 2
+local lowY = Spring.GetGroundHeight(midX, midZ) - 10000
+local function getNearestCEG(weaponDefID, params)
+    local ceg, range = params.ceg, params.range
+    local sizeBest, diffBest = math.huge, math.huge
+    for ii = 1, #areaSizePresets do
+        local size = areaSizePresets[ii]
+        local diff = math.abs(range / size - size / range)
+        if diff < diffBest then
+            local cegTest = string.gsub(ceg, pattern, sub1..math.floor(sizeBest)..sub2)
+            local success, cegID = Spring.SpawnCEG(cegTest, midX, lowY, midZ) -- hidden-ish
+            if success and cegID then
+                diffBest = diff
+                sizeBest = size
+            end
+        end
+    end
+    if sizeBest < math.huge then
+        ceg = string.gsub(ceg, '\d+', sizeBest, 1)
+        return ceg, sizeBest
+    end
+end
+
+--------------------------------------------------------------------------------
+-- Gadget callins --------------------------------------------------------------
 
 function gadget:Initialize()
     timedDamageWeapons = {}
     unitDamageImmunity = {}
 
-    for weaponDefID, weaponDef in ipairs(WeaponDefs) do
-        if weaponDef.customParams and weaponDef.customParams.area_onhit_ceg then
-            local custom = weaponDef.customParams
-            local params = {
-                ceg        = custom.area_onhit_ceg,
-                damageCeg  = custom.area_onhit_damageceg,
-                resistance = string.lower(custom.area_onhit_resistance),
-                damage     = tonumber(custom.area_onhit_damage or 0) * (22/30),
-                range      = tonumber(custom.area_onhit_range),
-                time       = tonumber(custom.area_onhit_time),
-            }
-            timedDamageWeapons[weaponDefID] = params
+    local weaponDefBaseIndex = 0
+    for weaponDefID = weaponDefBaseIndex, #WeaponDefs do
+        local weaponDef = WeaponDefs[weaponDefID]
+        if weaponDef.customParams and weaponDef.customParams[prefixes.weapon.."ceg"] then
+            timedDamageWeapons[weaponDefID] = getExplosionParams(weaponDef, prefixes.weapon)
         end
     end
-
     for unitDefID, unitDef in ipairs(UnitDefs) do
-        if unitDef.customParams.area_ondeath_ceg then
-            local custom = unitDef.customParams
-            local params = {
-                ceg        = custom.area_ondeath_ceg,
-                damageCeg  = custom.area_ondeath_damageceg,
-                resistance = string.lower(custom.area_onhit_resistance),
-                damage     = tonumber(custom.area_ondeath_damage or 0) * (22/30),
-                range      = tonumber(custom.area_ondeath_range),
-                time       = tonumber(custom.area_ondeath_time),
-            }
+        if unitDef.customParams[prefixes.unit.."ceg"] then
+            local params = getExplosionParams(unitDef, prefixes.unit)
             timedDamageWeapons[WeaponDefNames[unitDef.deathExplosion].id] = params
             timedDamageWeapons[WeaponDefNames[unitDef.selfDExplosion].id] = params
         end
     end
 
-    local areaCegSizes = { 37.5, 46, 54, 62.5, 75, 87.5, 100, 125, 150, 175, 200, 225, 250, 275, 300 }
-    local areaDamageTypes = {}
     for weaponDefID, params in pairs(timedDamageWeapons) do
-        -- Example how to tweak areas:
-        -- for udid, udef in pairs(UnitDefs) do
-        --     for name, wdef in pairs(udef.weapondefs or {}) do
-        --         if wdef.customparams and wdef.customparams.area_onhit_ceg then
-        --             wdef.customparams.area_onhit_time = 4
-        --             wdef.customparams.area_onhit_range = 200
-        --             wdef.projectiles = 1
-        --             Spring.Echo('[unit_area_timed_damage] tweaked weapon '..wdef.name..' => '..table.toString(wdef.customparams))
-        --         end
-        --     end
-        -- end
-        -- While areas of effect are tweak-able, CEGs are not.
-        -- Try to keep timed areas and their visuals in sync with one another:
-        if not string.find(params.ceg, '-'..math.floor(params.range)..'-', nil, true) then
-            local cegName = params.ceg
-            local rangeNew = math.huge
-            local diffBest = math.huge
-            for ii = 1, #areaCegSizes do
-                local diff = math.abs(params.range / areaCegSizes[ii] - areaCegSizes[ii] / params.range)
-                if diff < diffBest then
-                    local success, cegID = Spring.SpawnCEG(string.gsub(cegName, '\d+', rangeNew, 1), 0, -9e9, 0)
-                    if cegID ~= nil then
-                        diffBest = diff
-                        rangeNew = areaCegSizes[ii]
-                    else
-                        Spring.Log(gadget:GetInfo().name, LOG.WARNING, 'Did not find CEG: ' .. string.gsub(cegName, params.range, rangeNew, 1))
-                    end
-                end
-            end
-            if rangeNew < math.huge then
-                params.ceg = string.gsub(cegName, params.range, rangeNew, 1)
-                params.range = rangeNew
-                Spring.Log(gadget:GetInfo().name, LOG.INFO, 'Set '..WeaponDefs[weaponDefID].name..' to range, ceg = '..params.range..', '..params.ceg)
+        if  string.find(params.ceg, pattern, nil, false) and not
+            string.find(params.ceg, sub1..math.floor(params.range)..sub2)
+        then
+            local ceg, range = getNearestCEG(weaponDefID, params)
+            local name = WeaponDefs[weaponDefID].name
+            if ceg and range then
+                params.ceg = ceg
+                params.range = range
+                Spring.Log(gadget:GetInfo().name, LOG.INFO, 'Set '..name..' to range, ceg = '..params.range..', '..params.ceg)
             else
                 timedDamageWeapons[weaponDefID] = nil
-                Spring.Log(gadget:GetInfo().name, LOG.INFO, 'Removed '..WeaponDefs[weaponDefID].name)
+                Spring.Log(gadget:GetInfo().name, LOG.WARN, 'Removed '..name..' from area timed damage weapons.')
             end
         end
+    end
 
+    local areaDamageTypes = {}
+    for weaponDefID, params in pairs(timedDamageWeapons) do
         if params.resistance ~= nil and params.resistance ~= "none" then
             areaDamageTypes[params.resistance] = true
         else
@@ -151,11 +163,13 @@ function gadget:Initialize()
         for weaponDefID in pairs(timedDamageWeapons) do
             Script.SetWatchExplosion(weaponDefID, true)
         end
-        frameIndex = 1 + (Spring.GetGameFrame() % 22)
         aliveExplosions = {}
-        for ii = 1, 22 do
+        for ii = 1, damageInterval do
             aliveExplosions[ii] = {}
         end
+        gameFrame = Spring.GetGameFrame()
+        frameIndex = 1 + (gameFrame % damageInterval)
+        frameExplosions = aliveExplosions[frameIndex]
     else
         Spring.Log(gadget:GetInfo().name, LOG.INFO, "No timed areas found. Removing gadget.")
         gadgetHandler:RemoveGadget(self)
@@ -165,14 +179,13 @@ end
 function gadget:Explosion(weaponDefID, px, py, pz, attackerID, projectileID)
     if timedDamageWeapons[weaponDefID] ~= nil then
         local explosion = timedDamageWeapons[weaponDefID]
-        if py <= math.max(Spring.GetGroundHeight(px, pz), 0) + explosion.range*0.5 then
-            local currentTime = Spring.GetGameSeconds()
-            local frameExplosions = aliveExplosions[frameIndex]
+        local elevation = math.max(Spring.GetGroundHeight(px, pz), 0)
+        if py <= elevation + explosion.range*0.5 then
             frameExplosions[#frameExplosions+1] = {
                 x = px,
-                y = math.max(Spring.GetGroundHeight(px, pz), 0),
+                y = elevation,
                 z = pz,
-                endTime = currentTime + explosion.time,
+                endFrame = gameFrame + explosion.frames,
                 damage = explosion.damage,
                 range = explosion.range,
                 ceg = explosion.ceg,
@@ -186,10 +199,11 @@ function gadget:Explosion(weaponDefID, px, py, pz, attackerID, projectileID)
 end
 
 function gadget:GameFrame(frame)
-    frame = 1 + (frame % 22)
+    offset = 1 + (frame % damageInterval)
     local currentTime = Spring.GetGameSeconds()
-    for explosionID, explosionStats in pairs(aliveExplosions[frame]) do
-        if explosionStats.endTime >= currentTime then
+    local explosions = aliveExplosions[offset]
+    for explosionID, explosionStats in pairs(explosions) do
+        if explosionStats.endFrame >= frame then
             local x = explosionStats.x
             local y = explosionStats.y
             local z = explosionStats.z
@@ -221,9 +235,12 @@ function gadget:GameFrame(frame)
                 local ux, uy, uz = Spring.GetFeaturePosition(featureID)
                 Spring.SpawnCEG(explosionStats.damageCeg, ux, uy + 8, uz, 0, 0, 0)
             end
-        else -- This explosion is outdated, we can remove it from the list
-            aliveExplosions[frame][explosionID] = nil
+        else
+            explosions[explosionID] = nil
         end
     end
-    frameIndex = frame
+
+    gameFrame = frame
+    frameIndex = offset
+    frameExplosions = explosions
 end
