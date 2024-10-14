873d0d44123a97a04d0c0f248e2389031c9fcca3
Damgam <c_2_damiano@op.pl>
2024-10-14 21:58:26 +0200

    Added Epic Arquebus (#3846)
    Added Epic Arquebus

--- gamedata/scavengers/unitdef_post.lua
+++ gamedata/scavengers/unitdef_post.lua
@@ -277,13 +277,16 @@ local function scavUnitDef_Post(name, uDef)
 	elseif name == "coraap_scav" then
 		local numBuildoptions = #uDef.buildoptions
 		uDef.buildoptions[numBuildoptions + 1] = "corcrw_scav"
-	elseif name == "corgant_scav" or name == "leggant_scav" then
+	elseif name == "corgant_scav" then
 		local numBuildoptions = #uDef.buildoptions
 		uDef.buildoptions[numBuildoptions + 1] = "corkarganetht4_scav"
 		uDef.buildoptions[numBuildoptions + 2] = "corgolt4_scav"
 		uDef.buildoptions[numBuildoptions + 3] = "corakt4_scav"
 		uDef.buildoptions[numBuildoptions + 4] = "corthermite_scav"
 		uDef.buildoptions[numBuildoptions + 5] = "cormandot4_scav"
+	elseif name == "leggant_scav" then
+		local numBuildoptions = #uDef.buildoptions
+		uDef.buildoptions[numBuildoptions + 1] = "legsrailt4_scav"
 	elseif name == "armca_scav" or name == "armck_scav" or name == "armcv_scav" then
 		--local numBuildoptions = #uDef.buildoptions
 	elseif name == "corca_scav" or name == "corck_scav" or name == "corcv_scav" then
