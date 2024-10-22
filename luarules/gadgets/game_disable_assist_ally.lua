ca68810f6a472d19b62d625632a6717ea18b5640
Floris <floris@ruwhof.net>
2024-10-22 15:16:55 +0200

    reapply non legion changes

--- luarules/gadgets/game_disable_assist_ally.lua
+++ luarules/gadgets/game_disable_assist_ally.lua
@@ -16,7 +16,9 @@ end
 if not gadgetHandler:IsSyncedCode() then
 	return false
 end
-if not Spring.GetModOptions().disable_assist_ally_construction then
+if	not (Spring.GetModOptions().disable_assist_ally_construction
+	-- tax force enables this
+	or (Spring.GetModOptions().tax_resource_sharing_amount or 0) ~= 0) then
 	return false
 end
 
--- luarules/gadgets/game_disable_unit_sharing.lua
+++ luarules/gadgets/game_disable_unit_sharing.lua
@@ -16,7 +16,12 @@ end
 if not gadgetHandler:IsSyncedCode() then
 	return false
 end
-if not Spring.GetModOptions().disable_unit_sharing then
+
+if not (Spring.GetModOptions().disable_unit_sharing
+	-- tax force enables this
+	or (Spring.GetModOptions().tax_resource_sharing_amount or 0) ~= 0)
+	-- unit market handles the restriction instead if enabled so that selling still works
+	or Spring.GetModOptions().unit_market then
 	return false
 end
 
