e33d520ee9776845da833ee77fa53fb4117ccc76
robertthepie <stanlytheplatypus@gmail.com>
2024-10-23 17:51:23 +0100

    Minesweeper: Address Feedback and fix

--- luarules/gadgets/unit_minesweeper_detection.lua
+++ luarules/gadgets/unit_minesweeper_detection.lua
@@ -5,7 +5,7 @@ function gadget:GetInfo()
 	return {
 		name = "Minesweeper Detection",
 		desc = "Shows mines around minesweepers",
-		author = "Hornet, Cleaning: robert the pie",
+		author = "Hornet, robert the pie",
 		date = "May 25th, 2024",
 		license = "GNU GPL, v2 or later",
 		layer = 0,
@@ -17,20 +17,27 @@ if not gadgetHandler:IsSyncedCode() then
 	return false
 end
 
+local spSetUnitLosState		= Spring.SetUnitLosState
+local spSetUnitLosMask		= Spring.SetUnitLosMask
+local spGetUnitPosition		= Spring.GetUnitPosition
+local spGetUnitsInCylinder	= Spring.GetUnitsInCylinder
+local spGetUnitDefID		= Spring.GetUnitDefID
+local spGetUnitTeam			= Spring.GetUnitTeam
+
 local minesweeperRanges = {}
 local minesweepers = {}
 
-local mineIDs = {}
+local mineDefIDs = {}
 local revealedMines = {}
 
-for udid, ud in pairs(UnitDefs) do
-	if ud.customParams.minesweeper then
-		minesweeperRanges[udid] = ud.customParams.minesweeper
-	elseif ud.customParams.mine then
-		mineIDs[udid] = true
+for unitDefID, unitDef in pairs(UnitDefs) do
+	if unitDef.customParams.minesweeper then
+		minesweeperRanges[unitDefID] = unitDef.customParams.minesweeper
+	elseif unitDef.customParams.mine then
+		mineDefIDs[unitDefID] = true
 	end
 end
-if table.count(minesweeperRanges) <= 0 or table.count(mineIDs) <= 0 then
+if table.count(minesweeperRanges) == 0 or table.count(mineDefIDs) == 0 then
 	return false
 end
 
@@ -42,82 +49,58 @@ for _, teamID in pairs(Spring.GetTeamList()) do
 	minesweepers[teamID] = {}
 end
 
-
-function gadget:UnitCreated(unitID, unitDefID, unitTeam)
+function gadget:MetaUnitAdded(unitID, unitDefID, unitTeam)
 	if minesweeperRanges[unitDefID] then
 		minesweepers[unitTeam][unitID] = unitDefID
 	end
 end
 
-function gadget:UnitTaken(unitID, UnitDefID, oldTeam, newTeam)
-	if minesweeperRanges[unitDefID] then
-		minesweepers[oldTeam][unitID] = nil
-		minesweepers[newTeam][unitID] = unitDefID
-	end
-end
-
-function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
+function gadget:MetaUnitRemoved(unitID, unitDefID, unitTeam)
 	if minesweeperRanges[unitDefID] and minesweepers[unitTeam][unitID] then
 		minesweepers[unitTeam][unitID] = nil
 	end
 end
 
-local function processesTeamsSweepers(currentTeam)
+local function processTeamsSweepers(teamID)
 	local minesToReveal = {}
 
-	for sweeper, sweeperDef in pairs(minesweepers[currentTeam])  do
-		local x, y, z = Spring.GetUnitPosition(sweeper)
+	for sweeper, sweeperDef in pairs(minesweepers[teamID])  do
+		local x, y, z = spGetUnitPosition(sweeper)
 		if x and z then
-			local nearUnits = Spring.GetUnitsInCylinder(x, z, minesweeperRanges[sweeperDef])
+			local nearUnits = spGetUnitsInCylinder(x, z, minesweeperRanges[sweeperDef])
 			for _, nearUnit in ipairs(nearUnits) do
-				local nearUnitDefID = Spring.GetUnitDefID(nearUnit)
-				if currentTeam ~= Spring.GetUnitTeam(nearUnit) then
-					if mineIDs[nearUnitDefID] then
-						minesToReveal[nearUnit] = true
-					end
+				local nearUnitDefID = spGetUnitDefID(nearUnit)
+				if mineDefIDs[nearUnitDefID] and teamID ~= spGetUnitTeam(nearUnit) then
+					minesToReveal[nearUnit] = true
 				end
 			end
 		end
 	end
 
 	for mineToReveal, _ in pairs(minesToReveal) do
-		if revealedMines[currentTeam][mineToReveal] == nil then
-			revealedMines[currentTeam][mineToReveal] = true
+		if revealedMines[teamID][mineToReveal] == nil then
+			revealedMines[teamID][mineToReveal] = true
 			--show in vision and radar, and prevent engine from immediately resseting that state
-			Spring.Echo("revleaed", mineToReveal, "to", currentTeam)
-			Spring.SetUnitLosState(mineToReveal, teamIDs[currentTeam], 3)
-			Spring.SetUnitLosMask(mineToReveal, teamIDs[currentTeam], 3)
+			spSetUnitLosState(mineToReveal, teamIDs[teamID], 3)
+			spSetUnitLosMask(mineToReveal, teamIDs[teamID], 3)
 		end
 	end
 end
 
 local function processRevealedMines(teamID)
 	for revealedMine, _ in pairs(revealedMines[teamID]) do
-		Spring.SetUnitLosMask(revealedMine, teamID, 0)
+		spSetUnitLosMask(revealedMine, teamIDs[teamID], 0)
 		revealedMines[teamID][revealedMine] = nil
 	end
 end
 
--- if we have more teams than frames per second we use a slightly slower method
-local GAMESPEED = Game.gameSpeed
-if #teamIDs + 1 >= GAMESPEED then
-	function gadget:GameFrame(f)
-		local loopedFrame = f % GAMESPEED
-		for teamID in pairs(teamIDs) do
-			if teamID % GAMESPEED == loopedFrame then
-				processRevealedMines(teamID)
-				processesTeamsSweepers(teamID)
-			end
+local processingInterval = Game.gameSpeed
+function gadget:GameFrame(n)
+	local loopedFrame = n % processingInterval
+	for teamID in pairs(teamIDs) do
+		if teamID % processingInterval == loopedFrame then
+			processRevealedMines(teamID)
+			processTeamsSweepers(teamID)
 		end
-	end
-else
-	function gadget:GameFrame(f)
-		local currentTeam = f%GAMESPEED
-		if currentTeam > #teamIDs then
-			return
-		end
-
-		processRevealedMines(currentTeam)
-		processesTeamsSweepers(currentTeam)
 	end
 end
