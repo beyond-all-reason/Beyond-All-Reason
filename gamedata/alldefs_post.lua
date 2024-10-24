c81481fc0996b078fda6fa76e210a7ab43265846
nevniv <174193948+nevniv@users.noreply.github.com>
2024-10-24 09:56:21 -0400

    Revert "Disable drone carriers with a table instead"
    This reverts commit 9b043b57ed1d5969df7ddae7d796d0a50ef9df18.

--- gamedata/alldefs_post.lua
+++ gamedata/alldefs_post.lua
@@ -207,18 +207,7 @@ function UnitDef_Post(name, uDef)
 				uDef.maxthisunit = 0
 			elseif uDef.canfly then
 				uDef.maxthisunit = 0
-			end
-			local DroneCarriers = {
-				armdronecarry = true,
-				--armtrident = true,  --This has a depth charge launcher as well
-				cordronecarry = true,
-				--corsentinel = true, --This has a depth charge launcher as well
-				leghive = true,
-				legfhive = true,
-				--legtriariusdrone = true, --This has a heat ray as well, although it is still in development
-				legvcarry = true
-			}
-			if DroneCarriers[name] then
+			elseif uDef.customparams.childreninheritxp == "DRONE" then
 				uDef.maxthisunit = 0
 			end
 			local AircraftFactories = {
