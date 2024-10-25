d34e9b1f7dcc5dd955a47358102f82b3426cc760
Saurtron <saurtron@riseup.net>
2024-10-25 13:41:56 +0200

    Add comment.

--- common/luaUtilities/color.lua
+++ common/luaUtilities/color.lua
@@ -2,6 +2,7 @@ local floor = math.floor
 local schar = string.char
 
 local function ColorStringEx(R, G, B, A, oR, oG, oB, oA)
+	-- Formats alpha and also outline color.
 	return "\254" .. schar(floor(R * 255)) .. schar(floor(G * 255)) ..
 		schar(floor(B * 255)) .. schar(floor(A * 255)) ..
 		schar(floor(oR * 255)) .. schar(floor(oG * 255)) ..
@@ -16,6 +17,7 @@ local function ColorArray(R, G, B)
 end
 
 local function ColorString(R, G, B)
+	-- Standard R, G, B color code.
 	local R255, G255, B255 = ColorArray(R, G, B)
 
 	return "\255" .. schar(R255) .. schar(G255) .. schar(B255)
