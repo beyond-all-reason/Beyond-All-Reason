b736c47b1a7d45cfcd205be92c835de734dd4704
Tharsy <75606229+Tharsy@users.noreply.github.com>
2024-11-04 00:37:23 +1300

    Add Legion Venator and T2 Walls (#3906)
    Add Venator and t2 Walls
    
    - Remodel legvenator
    - Add legrwall
    - Remodel legforti
    - Updated legcom anims to hid trails when cloaked

--- luaui/Widgets/gfx_airjets_gl4.lua
+++ luaui/Widgets/gfx_airjets_gl4.lua
@@ -107,7 +107,8 @@ local effectDefs = {
 		{ color = { 0.2, 0.4, 0.5 }, width = 3.5, length = 30, piece = "thrusta", limit = true },
 	},
 	["legvenator"] = {
-		{ color = { 0.3, 0.4, 0.6 }, width = 5.5, length = 55, piece = "thrusta", limit = true },
+		{ color = { 0.1, 0.6, 0.4 }, width = 3, length = 24, piece = "lthrust", limit = true },
+		{ color = { 0.1, 0.6, 0.4 }, width = 3, length = 24, piece = "rthrust", limit = true },
 	},
 	-- radar
 	["armawac"] = {
