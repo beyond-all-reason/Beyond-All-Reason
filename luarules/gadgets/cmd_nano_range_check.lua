797b41cc9665192e421ad29074fd672edef1ec44
Nehroz <x7088@pm.me>
2024-11-05 17:55:36 +0100

    Initial Gadget style replacement.

---
+++ luarules/gadgets/cmd_nano_range_check.lua
@@ -0,0 +1,37 @@
+function gadget:GetInfo()
+    return {
+        name = "Construction Turrets Range Check",
+        desc = "Stops construction turrets from getting assigned to guards, repair, reclaim and attacks out of reach.",
+        author = "Nehroz",
+        date = "2024.11.05", -- update date.
+        license = "GNU GPL, v2 or later",
+        layer = 0,
+        version = "1.0",
+        enabled = true,
+    }
+end
+
+local function isNano(unitDef)
+    return unitDef.isImmobile and unitDef.isBuilder
+end
+
+local function isValidCommandID(commandID)
+    return commandID == CMD.REPAIR or commandID == CMD.GUARD or commandID == CMD.RECLAIM or commandID == CMD.ATTACK
+end
+
+function gadget:AllowCommand(unitID, unitDefID, _teamID, cmdID, cmdParams, _cmdOptions)
+    local unitDef = UnitDefs[unitDefID]
+    if not isNano(unitDef) then return true end
+    if not isValidCommandID(cmdID) then return true end
+    if #cmdParams ~= 1 then return true end -- only handle ID targets, fallthrough for area select; let the intended scripts handle those first.
+
+    local range = unitDef.buildDistance
+    local x, y, z = Spring.GetUnitPosition(unitID)
+    local cmdX, cmdY, cmdZ = Spring.GetUnitPosition(cmdParams[1])
+    if cmdX == nil then return true end -- in case of feature
+    local distance = math.sqrt((cmdX - x)^2 + (cmdY - y)^2 + (cmdZ - z)^2)
+    if distance > (range + unitDef.radius) then
+        return false
+    end
+    return true
+end
