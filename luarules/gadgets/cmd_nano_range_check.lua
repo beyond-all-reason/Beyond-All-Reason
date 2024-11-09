2b4e1c9c59d2d4db279dacf625e9359c4956bfa9
Nehroz <x7088@pm.me>
2024-11-09 13:51:41 +0100

    changes to checks.

--- luarules/gadgets/cmd_nano_range_check.lua
+++ luarules/gadgets/cmd_nano_range_check.lua
@@ -3,7 +3,7 @@ function gadget:GetInfo()
 		name = "Construction Turrets Range Check",
 		desc = "Stops construction turrets from getting assigned to guards, repair, reclaim and attacks out of reach.",
 		author = "Nehroz",
-		date = "2024.11.05", -- update date.
+		date = "2024.11.09", -- update date.
 		license = "GNU GPL, v2 or later",
 		layer = 0,
 		version = "1.0",
@@ -12,7 +12,7 @@ function gadget:GetInfo()
 end
 
 local function isNano(unitDef)
-	return unitDef.isImmobile and unitDef.isBuilder
+	return unitDef.isFactory == false and unitDef.isStaticBuilder
 end
 
 local function isValidCommandID(commandID)
@@ -28,13 +28,13 @@ function gadget:AllowCommand(unitID, unitDefID, _teamID, cmdID, cmdParams, _cmdO
 	local unitDef = UnitDefs[unitDefID]
 	if not isNano(unitDef) then return true end
 	if not isValidCommandID(cmdID) then return true end
-	if #cmdParams ~= 1 then return true end -- only handle ID targets, fallthrough for area select; Let the intended scripts handle those first, cache resulting commands on ID.
-	if not UnitDefs[Spring.GetUnitDefID(cmdParams[1])].isImmobile then return true end -- REVIEW If possible, remove double neg.
+	if #cmdParams ~= 1 then return true end -- only handle ID targets, fallthrough for area selects; Let the intended scripts handle, catch resulting commands on ID.
+	local cmdX, cmdY, cmdZ = Spring.GetUnitPosition(cmdParams[1])
+	if cmdX == nil then return true end -- in case of feature; Already distanced checked by respective scripts.
+	if UnitDefs[Spring.GetUnitDefID(cmdParams[1])].canMove then return true end -- ignore movable targets
 
 	local range = unitDef.buildDistance
 	local x, y, z = Spring.GetUnitPosition(unitID)
-	local cmdX, cmdY, cmdZ = Spring.GetUnitPosition(cmdParams[1])
-	if cmdX == nil then return true end -- in case of feature; Already distanced checked by respective scripts. TODO pre-get exklusion; feature check.
 	local distance = math.sqrt((cmdX - x)^2 + (cmdY - y)^2 + (cmdZ - z)^2)
 	if distance > (range + unitDef.radius) then
 		return false
