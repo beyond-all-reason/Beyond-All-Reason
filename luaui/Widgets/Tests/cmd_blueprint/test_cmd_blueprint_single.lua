a7aa8a87fbfb42b8214d2a5e7e5cd87199e1d48f
saurtron <saurtron@riseup.net>
2024-11-21 21:34:27 +0100

    Add Test.prepareWidget and Test.restoreWidget to simplify common test initializations (#3951)
    Introduce Test.prepareWidget and Test.restoreWidget to simplify common test initializations

--- luaui/Widgets/Tests/cmd_blueprint/test_cmd_blueprint_single.lua
+++ luaui/Widgets/Tests/cmd_blueprint/test_cmd_blueprint_single.lua
@@ -9,14 +9,8 @@ function setup()
 
 	Test.clearMap()
 
-	initialWidgetActive = widgetHandler.knownWidgets[widgetName].active
-	if initialWidgetActive then
-		widgetHandler:DisableWidgetRaw(widgetName)
-	end
-	widgetHandler:EnableWidgetRaw(widgetName, true)
+	widget = Test.prepareWidget(widgetName)
 
-	widget = widgetHandler:FindWidget(widgetName)
-	assert(widget)
 	mock_saveBlueprintsToFile = Test.mock(widget, "saveBlueprintsToFile")
 
 	initialCameraState = Spring.GetCameraState()
@@ -28,11 +22,6 @@ end
 
 function cleanup()
 	Test.clearMap()
-
-	widgetHandler:DisableWidgetRaw(widgetName)
-	if initialWidgetActive then
-		widgetHandler:EnableWidgetRaw(widgetName, false)
-	end
 
 	Spring.SetCameraState(initialCameraState)
 end
