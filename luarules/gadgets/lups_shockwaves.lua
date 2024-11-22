ddbc4131cf9350c160b7b4a64867b1f917bfcf15
Beherith <mysterme@gmail.com>
2024-11-22 10:30:29 +0100

    Disable LUPS shockwaves for goot

--- luarules/gadgets/lups_shockwaves.lua
+++ luarules/gadgets/lups_shockwaves.lua
@@ -6,7 +6,7 @@ function gadget:GetInfo()
 		date      = "Jan. 2008",
 		license   = "GNU GPL, v2 or later",
 		layer     = 0,
-		enabled   = true
+		enabled   = false, -- F*** YEAH FINALLY RID OF THIS 16 years later
 	}
 end
 
