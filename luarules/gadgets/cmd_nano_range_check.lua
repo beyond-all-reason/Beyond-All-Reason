910e306eae8387bafcb73324da75fbb713c12844
Nehroz <x7088@pm.me>
2024-12-01 17:31:48 +0100

    final, changes.

--- luarules/gadgets/cmd_nano_range_check.lua
+++ luarules/gadgets/cmd_nano_range_check.lua
@@ -3,95 +3,63 @@ function gadget:GetInfo()
 		name = "Construction Turrets Range Check",
 		desc = "Stops construction turrets from getting assigned to guards, repair, reclaim and attacks out of reach.",
 		author = "Nehroz",
-		date = "2024.11.09", -- update date.
-		license = "GNU GPL, v2 or later",
+		date = "2024.12.01",
 		layer = 0,
-		version = "1.0",
 		enabled = true,
 	}
 end
 
-local frequency = 10 -- only applies to movable units under tracked nanos.
+
+if not gadgetHandler:IsSyncedCode() then return end
+
 --- key is the nano's ID, followed by a Set-like with the unitID of target currently inside the nano's range.
 --- Used for CommandArray manipulation
 ---@type {[number] : {number : boolean}}
 local trackingTable = {}
 
-local function isNano(unitDef)
-	return unitDef.isFactory == false and unitDef.isStaticBuilder
-end
-
-local function isValidCommandID(commandID)
-	return (
-		   commandID == CMD.REPAIR
-		or commandID == CMD.GUARD
-		or commandID == CMD.RECLAIM
-		or commandID == CMD.ATTACK
-	)
-end
-
-local function reindexArray(tbl)
-	local newTbl = {}
-	for _, value in pairs(tbl) do
-		table.insert(newTbl, value)
+local constructionTurretsDefs = {}
+for unitDefID, unitDef in ipairs(UnitDefs) do
+	if unitDef.isFactory == false and unitDef.isStaticBuilder then
+		constructionTurretsDefs[unitDefID] = {
+			maxBuildDistance = unitDef.buildDistance + unitDef.radius,
+			buildRange3D = unitDef.buildRange3D,
+		}
 	end
-	return newTbl
 end
 
-local function cmdToCmdSpec(tbl)
-	local newTbl = {}
-	for _, cmd in pairs(tbl) do
-		table.insert(newTbl, {cmd.id, cmd.params, cmd.options})
-	end
-	return newTbl
+
+function gadget:Initialize()
+	gadgetHandler:RegisterAllowCommand(CMD.REPAIR)
+	gadgetHandler:RegisterAllowCommand(CMD.GUARD)
+	gadgetHandler:RegisterAllowCommand(CMD.RECLAIM)
+	gadgetHandler:RegisterAllowCommand(CMD.ATTACK)
 end
 
 function gadget:GameFrame(frame)
-	if frame % frequency ~= 0 then return end
 	for nanoID in pairs(trackingTable) do
-		if not Spring.ValidUnitID(nanoID) then
-			trackingTable[nanoID] = nil
-		else
-			local nanoDef = UnitDefs[Spring.GetUnitDefID(nanoID)]
-			local maxDistance = nanoDef.buildDistance + nanoDef.radius
-			local cmds = Spring.GetUnitCommands(nanoID, -1)
-			local isChanged = false
+		local nanoDefID = Spring.GetUnitDefID(nanoID)
+		local maxDistance = constructionTurretsDefs[nanoDefID].maxBuildDistance
+		local commands = Spring.GetUnitCommands(nanoID, -1)
 
-			for i, cmd in pairs(cmds) do
-				local uID = cmd.params[1]
-				if Spring.ValidUnitID(uID) then
-					local distance = Spring.GetUnitSeparation(nanoID, uID, nanoDef.buildRange3D, false)
-					if distance < maxDistance then -- Inside range
-						trackingTable[nanoID][uID] = true
-					elseif trackingTable[nanoID][uID] then -- Outside range
-						trackingTable[nanoID][uID] = nil
-						cmds[i] = nil
-						isChanged = true
-					end
-				end
-			end
-
-			if isChanged then
-				if #cmds > 0 then
-					cmds = reindexArray(cmds)
-					cmds[1].options.shift = false -- Ensure proper command behavior
-					Spring.GiveOrderArrayToUnit(nanoID, cmdToCmdSpec(cmds))
-				else
-					trackingTable[nanoID] = nil
-					Spring.GiveOrderToUnit(nanoID, CMD.STOP, {}, {})
+		for i=#commands, 1, -1 do
+			local unitID = commands[i].params[1]
+			if Spring.ValidUnitID(unitID) then
+				local distance = Spring.GetUnitSeparation(nanoID, unitID, constructionTurretsDefs[nanoDefID].buildRange3D, false)
+				if distance < maxDistance then -- Inside range
+					trackingTable[nanoID][unitID] = true
+				elseif trackingTable[nanoID][unitID] then -- Outside range
+					trackingTable[nanoID][unitID] = nil
+					Spring.GiveOrderToUnit(nanoID, CMD.REMOVE, commands[i].tag, 0)
 				end
 			end
 		end
 	end
 end
 
-function gadget:AllowCommand(unitID, unitDefID, _teamID, cmdID,
-	cmdParams, _cmdOptions, _cmdTag, _synced, _fromLua)
-
-	local unitDef = UnitDefs[unitDefID]
-	if not isNano(unitDef) then return true end
-	if not isValidCommandID(cmdID) then return true end
-	if #cmdParams ~= 1 then return true end -- only handle ID targets, fallthrough for area selects; Let the intended scripts handle, catch resulting commands on ID.
+function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID,	cmdParams, cmdOptions, cmdTag, synced, fromLua)
+	if not constructionTurretsDefs[unitDefID] then return true end
+	-- only handle ID targets, fallthrough for area selects; Let the intended scripts handle, catch resulting commands on ID.
+	if #cmdParams ~= 1 then return true end
 	local distance = math.huge
 	local targetId = cmdParams[1]
 
@@ -104,28 +72,23 @@ function gadget:AllowCommand(unitID, unitDefID, _teamID, cmdID,
 			end
 			return true
 		end
-		distance = Spring.GetUnitSeparation(unitID, targetId, unitDef.buildRange3D, false)
+		distance = Spring.GetUnitSeparation(unitID, targetId, constructionTurretsDefs[unitDefID].buildRange3D, false)
 	else
 		targetId = targetId - Game.maxUnits
 		if not Spring.ValidFeatureID(targetId) then return end
 		distance = Spring.GetUnitFeatureSeparation(unitID, targetId, false)
 	end
 
-	if distance > (unitDef.buildDistance + unitDef.radius) then
+	if distance > constructionTurretsDefs[unitDefID].maxBuildDistance then
 		return false
 	end
 	return true
 end
 
 function gadget:UnitDestroyed(unitID)
-	for nanoID in pairs(trackingTable) do
-		if nanoID == unitID then
-			trackingTable[nanoID] = nil
-		end
-	end
-	for nanoID, v in pairs(trackingTable) do
-		if v == unitID then
-			trackingTable[nanoID][unitID] = nil
-		end
+	trackingTable[unitID] = nil
+
+	for nanoID, _ in pairs(trackingTable) do
+		trackingTable[nanoID][unitID] = nil
 	end
 end
