4b59429a7af2927e6b6ba6ad734980abbd82575d
SethDGamre <sethdgamre@gmail.com>
2024-12-07 08:34:18 -0600

    logic improved, cleaner

--- luarules/gadgets/unit_weapon_smart_select_helper.lua
+++ luarules/gadgets/unit_weapon_smart_select_helper.lua
@@ -16,18 +16,16 @@ if not gadgetHandler:IsSyncedCode() then return end
 
 --static
 local frameCheckModulo = Game.gameSpeed
-local cmdAttack = CMD.ATTACK
 local deferredRetentionFrames = Game.gameSpeed * 7.5
+local failedToFireMultiplier = Game.gameSpeed * 1.25
 
 --variables
 local gameFrame = 0
 
 --functions
-local ggGetUnitTarget = GG.GetUnitTarget
-local spGetUnitCurrentCommand = Spring.GetUnitCurrentCommand
 local spCallCOBScript = Spring.CallCOBScript
 local spGetUnitWeaponHaveFreeLineOfFire = Spring.GetUnitWeaponHaveFreeLineOfFire
-local spGetUnitCommands = Spring.GetUnitCommands
+local spGetUnitWeaponTarget = Spring.GetUnitWeaponTarget
 
 --tables
 local smartUnits = {}
@@ -41,128 +39,117 @@ function gadget:Initialize()
     end
 end
 
-for unitDefID, def in ipairs(UnitDefs) do
-	if def.weapons then
-		local weapons = def.weapons
-		for weaponNumber, weaponData in pairs(weapons) do
-			local weaponDefID = weapons[weaponNumber].weaponDef
-			Spring.Echo("weaponNumber", weaponNumber, weaponDefID)
-			if WeaponDefs[weaponDefID].customParams.smart_preferred_weapon then
-				smartWeaponDefs[unitDefID] = {}
-				smartWeaponDefs[unitDefID].preferredWeapon = weaponNumber
-				Spring.Echo("smart_preferred_weapon", weaponNumber)
-			end
-			if WeaponDefs[weaponDefID].customParams.smart_deferred_weapon then
-				smartWeaponDefs[unitDefID].deferredWeapon = weaponNumber
-				Spring.Echo("smart_deferred_weapon", weaponNumber)
-			end
-			if WeaponDefs[weaponDefID].customParams.smart_trajectory_checker then --this is necessary until Spring.GetUnitWeaponHaveFreeLineOfFire doesn't check cannon trajectory from QueryWeaponX() piece in animation script callin
-				smartWeaponDefs[unitDefID].trajectoryCheckerWeapon = weaponNumber
-				Spring.Echo("smart_trajectory_checker", weaponNumber)
-			end
-		end
-	end
-end
-
-local function weaponTargettingCheck(attackerID, targetData)
-	if not targetData then return end
-	local defData = smartWeaponDefs[smartUnits[attackerID].unitDefID]
-	if #targetData == 1 then --unit target
-		local canShoot = spGetUnitWeaponHaveFreeLineOfFire(attackerID, defData.trajectoryCheckerWeapon, targetData[1])
-		if not canShoot then
-			spCallCOBScript(attackerID, smartUnits[attackerID].overrideScriptID, 0, defData.deferredWeapon)
-			return false
-		elseif canShoot and gameFrame > smartUnits[attackerID].overrideExpirationFrame then -- to prevent the enemy from moving their units in and out of high/low LOS to stop firing due to constant readjustment
-			spCallCOBScript(attackerID, smartUnits[attackerID].overrideScriptID, 0, defData.preferredWeapon)
-			smartUnits[attackerID].overrideExpirationFrame = gameFrame + deferredRetentionFrames
-			return true
-		end
-	elseif #targetData > 1 then --ground target
-		local canShoot = spGetUnitWeaponHaveFreeLineOfFire(attackerID, defData.trajectoryCheckerWeapon, _, _, _, targetData[1], targetData[2], targetData[3])
-		if not canShoot then
-			spCallCOBScript(attackerID, smartUnits[attackerID].overrideScriptID, 0, defData.deferredWeapon)
-			return true
-		elseif canShoot then
-			spCallCOBScript(attackerID, smartUnits[attackerID].overrideScriptID, 0, defData.preferredWeapon)
-			return false
-		end
-	else
-		
-	end
-end
-
-
-local aggroDecayRate = 0.9
-local pAggro = 1
-local dAggro = 1.5
-local function targetCheck(attackerID)
-	local attackerData = smartUnits[attackerID]
-	local defData = smartWeaponDefs[attackerData.unitDefID]
-    local pTargetType, pIsUserTarget, pTarget = Spring.GetUnitWeaponTarget(attackerID, defData.preferredWeapon)
-	local dIsUserTarget = select(2, Spring.GetUnitWeaponTarget(attackerID, defData.deferredWeapon))
-
-	attackerData.aggroBias = attackerData.aggroBias * aggroDecayRate
-		
-	if pTargetType == 1 then
-		local preferredCanShoot = spGetUnitWeaponHaveFreeLineOfFire(attackerID, defData.trajectoryCheckerWeapon, pTarget)
-		Spring.Echo("targetID! isuserTarget:", pIsUserTarget)
-
-		if pIsUserTarget and preferredCanShoot then
-			attackerData.aggroBias = attackerData.aggroBias + pAggro
-
-		elseif dIsUserTarget then
-			attackerData.aggroBias = attackerData.aggroBias - dAggro
-
-		elseif preferredCanShoot then
-			attackerData.aggroBias = attackerData.aggroBias + pAggro
-
-		else
-			attackerData.aggroBias = attackerData.aggroBias - dAggro
-		end
-
-	elseif pTargetType == 2 then
-		Spring.Echo("coordinates! isuserTarget:", pIsUserTarget)
-	end
-
-	if attackerData.aggroBias >= 0 then
-		spCallCOBScript(attackerID, attackerData.overrideScriptID, 0, defData.preferredWeapon)
-	else
-		spCallCOBScript(attackerID, attackerData.overrideScriptID, 0, defData.deferredWeapon)
-	end
-	Spring.Echo("aggroBias", attackerData.aggroBias)
-end
-
--- local function manualCommandIssued(attackerID)
---     local returnTargetTable = {}
---     if spGetUnitCurrentCommand(attackerID) == cmdAttack then
---         local attackTarget = spGetUnitCommands(attackerID, 1)
---         if attackTarget and attackTarget[1] and attackTarget[1].params then
---             returnTargetTable = attackTarget[1].params
---         end
---     end
---     local setTargetData = ggGetUnitTarget(attackerID) or {}
---     if setTargetData then
---         if type(setTargetData) == "number" then
---             returnTargetTable[1] = setTargetData
---             return returnTargetTable
--- 		elseif next(setTargetData) then
---             return setTargetData
---         end
---     end
---     return returnTargetTable
--- end
-
-
 function gadget:UnitCreated(unitID, unitDefID)
 	if smartWeaponDefs[unitDefID] then
 		smartUnits[unitID] = {
 			unitDefID = unitDefID,
 			overrideScriptID = Spring.GetCOBScriptID(unitID, "OverrideAimingState"),
 			overrideExpirationFrame = gameFrame,
-			aggroBias = 0
+			aggroBias = 0,
+			preferredReloadFrame = 0,
+			errorTallyMultiplier = 0,
 		}
 	end
 end
+
+for unitDefID, def in ipairs(UnitDefs) do
+	if def.weapons then
+		local weapons = def.weapons
+		for weaponNumber, weaponData in pairs(weapons) do
+			local weaponDefID = weapons[weaponNumber].weaponDef
+			if WeaponDefs[weaponDefID].customParams.smart_preferred_weapon then
+				smartWeaponDefs[unitDefID] = {}
+				smartWeaponDefs[unitDefID].preferredWeapon = weaponNumber
+				smartWeaponDefs[unitDefID].failedToFireFrameThreshold = WeaponDefs[weaponDefID].reload * failedToFireMultiplier --zzz this is gonna be problematic for moving slow turn units like vanguard
+				if def.speed and def.speed ~= 0 then
+					smartWeaponDefs[unitDefID].canMove = true
+					Spring.Echo(def.name, smartWeaponDefs[unitDefID].canMove)
+				end
+			end
+			if WeaponDefs[weaponDefID].customParams.smart_deferred_weapon then
+				smartWeaponDefs[unitDefID].deferredWeapon = weaponNumber
+			end
+		end
+	end
+end
+
+local aggroDecayRate = 0.85
+local tallyDecayRate = 0.98
+local pManualAggro = 11
+local pAutoAggro = 5
+local dManualAggro = 9
+local dAutoAggro = 3
+local dErrorAggro = -300
+local errorRecencyThreshold = Game.gameSpeed * 15
+local errorTallyMultiplierCap = 4
+
+local function failureToFireCheck(attackerID, attackerData, defData)
+    if attackerData.preferredReloadFrame < gameFrame - defData.failedToFireFrameThreshold then
+        attackerData.preferredReloadFrame = math.max(
+            Spring.GetUnitWeaponState(attackerID, defData.preferredWeapon, 'reloadFrame'),
+            Spring.GetUnitWeaponState(attackerID, defData.deferredWeapon, 'reloadFrame')
+        )
+    end
+
+    if attackerData.preferredReloadFrame < gameFrame - defData.failedToFireFrameThreshold and
+	attackerData.preferredReloadFrame > gameFrame - errorRecencyThreshold then --to ensure it isn't just an old reload
+		return true
+    else
+		return false
+    end
+end
+
+
+local function updateAimingState(attackerID)
+    local attackerData = smartUnits[attackerID]
+    local defData = smartWeaponDefs[attackerData.unitDefID]
+
+    local pTargetType, pIsUserTarget, pTarget = spGetUnitWeaponTarget(attackerID, defData.preferredWeapon)
+    local dIsUserTarget = select(2, spGetUnitWeaponTarget(attackerID, defData.deferredWeapon))
+
+    attackerData.aggroBias = attackerData.aggroBias * aggroDecayRate
+	attackerData.errorTallyMultiplier = attackerData.errorTallyMultiplier * tallyDecayRate
+    
+    local preferredCanShoot = false
+    if pTargetType == 1 then
+        preferredCanShoot = spGetUnitWeaponHaveFreeLineOfFire(attackerID, defData.preferredWeapon, pTarget)
+    elseif pTargetType == 2 then
+        preferredCanShoot = spGetUnitWeaponHaveFreeLineOfFire(attackerID, defData.preferredWeapon, _, _, _, pTarget[1], pTarget[2], pTarget[3])
+    end
+
+    local failureToFire = failureToFireCheck(attackerID, attackerData, defData)
+
+    if pIsUserTarget and preferredCanShoot then
+        if failureToFire then
+            attackerData.errorTallyMultiplier = math.min(attackerData.errorTallyMultiplier + 1, errorTallyMultiplierCap)
+            attackerData.aggroBias = dErrorAggro * attackerData.errorTallyMultiplier ^ attackerData.errorTallyMultiplier
+        else
+            attackerData.aggroBias = attackerData.aggroBias + pManualAggro
+        end
+    elseif dIsUserTarget then
+        attackerData.aggroBias = attackerData.aggroBias - dManualAggro
+    else
+		if failureToFire then
+			attackerData.errorTallyMultiplier = math.min(attackerData.errorTallyMultiplier + 1, errorTallyMultiplierCap)
+			attackerData.aggroBias = dErrorAggro * attackerData.errorTallyMultiplier ^ attackerData.errorTallyMultiplier
+        elseif preferredCanShoot then
+            attackerData.aggroBias = attackerData.aggroBias + pAutoAggro
+        elseif dIsUserTarget ~= nil then
+            attackerData.aggroBias = attackerData.aggroBias - dAutoAggro
+        end
+    end
+
+    if attackerData.aggroBias >= 0 then
+        spCallCOBScript(attackerID, attackerData.overrideScriptID, 0, defData.preferredWeapon)
+    else
+        spCallCOBScript(attackerID, attackerData.overrideScriptID, 0, defData.deferredWeapon)
+    end
+
+    Spring.Echo(attackerData.errorTallyMultiplier, attackerData.aggroBias)
+end
+
+
+
 
 function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
 	smartUnits[unitID] = nil
@@ -172,8 +159,7 @@ function gadget:GameFrame(frame)
 	if frame % frameCheckModulo == 3 then
 		gameFrame = frame
 		for attackerID in pairs(smartUnits) do
-			local targetData = targetCheck(attackerID)
-			--weaponTargettingCheck(attackerID, targetData)
+			updateAimingState(attackerID)
 		end
 	end
 end
