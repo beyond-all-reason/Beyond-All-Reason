7c7ddc542db52d71996aaa947c2b31af2718e441
NicklasHansen95 <70760445+NicklasHansen95@users.noreply.github.com>
2024-12-09 14:20:29 +0100

    Add custom param to simplify smart_select

--- units/Legion/Utilities/legnanotct2.lua
+++ units/Legion/Utilities/legnanotct2.lua
@@ -48,6 +48,7 @@ return {
 			model_author = "Tharsis",
 			normaltex = "unittextures/leg_normal.dds",
 			subfolder = "CorBuildings/LandUtil",
+			isnanoturret = true,
 		},
 		sfxtypes = {
 			pieceexplosiongenerators = {
