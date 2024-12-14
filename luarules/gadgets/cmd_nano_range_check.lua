ce912d45f5c0f52bb27c7981871679d4fa4f854e
Nehroz <x7088@pm.me>
2024-12-14 04:37:41 +0100

    Optimisation Step 2. - Extension of trackingTable. - Dynamic chunking.
    Don't touch yet, you're not helping.

--- luarules/gadgets/cmd_nano_range_check.lua
+++ luarules/gadgets/cmd_nano_range_check.lua
@@ -13,11 +13,12 @@ if not gadgetHandler:IsSyncedCode() then return end
 Spring.SetLogSectionFilterLevel(gadget:GetInfo().name, LOG.INFO) -- WARN Remove in final version.
 Spring.Log(gadget:GetInfo().name, LOG.INFO,"Unsync Load.")
 
-
 --- key is the nano's ID, followed by a Set-like with the unitID of target currently inside the nano's range.
 --- Used for CommandArray manipulation
----@type {[number] : {number : boolean}}
+---@type {[number] : {number : {cmdTag : integer, status : integer}}}
 local trackingTable = {}
+local trackingTableSize = 0
+local chunkingFrameSize = 1
 
 local constructionTurretsDefs = {}
 for unitDefID, unitDef in ipairs(UnitDefs) do
@@ -49,6 +50,7 @@ function gadget:Initialize()
 	gadgetHandler:RegisterAllowCommand(CMD.REPAIR)
 	gadgetHandler:RegisterAllowCommand(CMD.GUARD)
 	gadgetHandler:RegisterAllowCommand(CMD.RECLAIM)
+	gadgetHandler:RegisterAllowCommand(CMD.STOP)
 end
 
 local gate = false
@@ -58,38 +60,65 @@ function gadget:GameFrame(frame)
 	local nanosRun = 0
 	local unitsCalced = 0
 
-	if frame % 60 > 10 then return end -- run 10 frames, skip 50; allow LuaUser.cpp to catch up. running full every 1s
-	local pointer = frame % 10
-	for nanoID in pairs(trackingTable) do
-		if pointer % 10 == 0 then
+	if frame % 15 then -- update dynamic chunking size
+		if trackingTableSize <= 50 then
+			chunkingFrameSize = 1
+		elseif trackingTableSize <= 100 then
+			-- linear from 50 to 100 nanos, from chunk size 1 to 10.
+			-- ratio: 1 = 9/50 * (50+b) -> b = -8
+			chunkingFrameSize = math.floor((9 / 50) * trackingTableSize - 8)
+		else
+			chunkingFrameSize = 10
+		end
+	end
+
+	local cmdCache = {}
+	local pointer = frame % chunkingFrameSize -- chunking offset
+	for nanoID, targets in pairs(trackingTable) do
+		if pointer % chunkingFrameSize == 0 then
 			local nanoDefID = Spring.GetUnitDefID(nanoID)
 			local maxDistance = constructionTurretsDefs[nanoDefID].maxBuildDistance
-			local commands = Spring.GetUnitCommands(nanoID, -1)
 
-			for i=#commands, 1, -1 do
-				local unitID = commands[i].params[1]
-				if Spring.ValidUnitID(unitID) then
-					local distance = Spring.GetUnitSeparation(nanoID, unitID, constructionTurretsDefs[nanoDefID].buildRange3D, false)
-					if distance < maxDistance then -- Inside range
-						trackingTable[nanoID][unitID] = true
-						inside = inside +1
-					elseif trackingTable[nanoID][unitID] then -- Outside range
-						trackingTable[nanoID][unitID] = nil
-						Spring.GiveOrderToUnit(nanoID, CMD.REMOVE, commands[i].tag, 0)
-						removes = removes +1
+			for targetID, commandData in pairs(targets) do
+				if Spring.ValidUnitID(targetID) then
+					-- cmdTag gathering
+					if commandData.cmdTag == -1 then
+						if not cmdCache[nanoID] then
+							-- REVISE Only way to obtain legit tag data; will create light lag.
+							cmdCache[nanoID] = Spring.GetUnitCommands(nanoID, -1)
+						end
+						for _, cmd in ipairs(cmdCache[nanoID]) do
+							if cmd.params[1] == targetID then
+								trackingTable[nanoID][targetID].cmdTag = cmd.tag
+							end
+						end
+					end
+					-- distance processing
+					local distance = Spring.GetUnitSeparation(nanoID, targetID, constructionTurretsDefs[nanoDefID].buildRange3D, false)
+					if distance < maxDistance then
+						if commandData.status == 0 then -- Entering range.
+							commandData.status = 1
+							inside = inside +1
+						end
+					else
+						if commandData.status == 1 then -- Exiting range.
+							Spring.GiveOrderToUnit(nanoID, CMD.REMOVE, commandData.cmdTag, 0) ---@diagnostic disable-line: param-type-mismatch
+							targets[targetID] = nil
+							removes = removes +1
+						end
 					end
 					unitsCalced = unitsCalced +1
+				else
+					targets[targetID] = nil -- Clean up invalid targets.
 				end
 			end
-			-- clear nano when chaining commands
-			if #commands == 1  then
-				if commands[1].id == CMD.FIGHT then --expected idle
-					trackingTable[nanoID] = nil
-				end
-			end
-			nanosRun = nanosRun +1
 		end
 		pointer = pointer + 1
+
+		if next(targets) == nil then -- Clean empty nanos.
+			trackingTable[nanoID] = nil
+			trackingTableSize = trackingTableSize - 1
+		end
 	end
 
 	if not isEmptyTable(trackingTable) or gate then
@@ -112,22 +141,30 @@ end
 
 function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID,	cmdParams, cmdOptions, cmdTag, synced, fromLua)
 	if not constructionTurretsDefs[unitDefID] then return true end
+	-- Handle stops on nanos
+	if cmdID == CMD.STOP then
+		trackingTable[unitID] = nil
+		trackingTableSize = trackingTableSize - 1
+	end
 	-- only handle ID targets, fallthrough for area selects; Let the intended scripts handle, catch resulting commands on ID.
 	if #cmdParams ~= 1 then return true end
 	local distance = math.huge
 	local targetId = cmdParams[1]
 
-	if targetId < Game.maxUnits then
+	if targetId < Game.maxUnits then -- Feature handling
 		if not Spring.ValidUnitID(targetId) then return end
 		local targetUnitDef = UnitDefs[Spring.GetUnitDefID(targetId)]
 		if targetUnitDef.canMove then
 			if not trackingTable[unitID] then
 				trackingTable[unitID] = {}
+				trackingTableSize = trackingTableSize + 1
 			end
+			trackingTable[unitID][cmdParams[1]] = {cmdTag = -1, status = 0} -- default to outside
 			return true
 		end
 		distance = Spring.GetUnitSeparation(unitID, targetId, constructionTurretsDefs[unitDefID].buildRange3D, false)
 	else
+		-- Handle Features
 		targetId = targetId - Game.maxUnits
 		if not Spring.ValidFeatureID(targetId) then return end
 		distance = Spring.GetUnitFeatureSeparation(unitID, targetId, false)
@@ -140,7 +177,10 @@ function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID,	cmdParams, cmdOpt
 end
 
 function gadget:UnitDestroyed(unitID)
-	trackingTable[unitID] = nil
+	if trackingTable[unitID] then
+		trackingTable[unitID] = nil
+		trackingTableSize = trackingTableSize - 1
+	end
 
 	for nanoID, _ in pairs(trackingTable) do
 		trackingTable[nanoID][unitID] = nil
