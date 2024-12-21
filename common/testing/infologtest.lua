7202292fa2225a5f5e3782496a859e9e24e4788e
Saurtron <saurtron@riseup.net>
2024-12-21 18:33:55 +0100

    Run an extra test on headless checking for errors at infolog.txt.

---
+++ common/testing/infologtest.lua
@@ -0,0 +1,34 @@
+-- 'hidden' test checking infolog.txt for errors, used by headless runs.
+
+local maxErrors = 10
+
+local function skipErrors(line)
+	if string.find(line, 'Could not finalize projectile-texture atlas', nil, true) then
+		return true
+	end
+end
+
+local function infologTest()
+	local errors = {}
+	local infolog = VFS.LoadFile("infolog.txt")
+	if infolog then
+		local fileLines = string.lines(infolog)
+		for i, line in ipairs(fileLines) do
+			if string.find(line, 'Error:', nil, true) and not skipErrors(line) then
+				errors[#errors+1] = line
+				if #errors > maxErrors then
+					return errors
+				end
+			end
+		end
+	end
+	return errors
+end
+
+
+function test()
+	local errors = infologTest()
+	if #errors > 0 then
+		error(table.concat(errors, "\n"), 0)
+	end
+end
