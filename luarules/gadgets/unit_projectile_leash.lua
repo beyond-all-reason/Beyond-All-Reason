575c13abf8ab6aa0858f9ceb9e38c18a416275f9
SethDGamre <sethdgamre@gmail.com>
2024-12-23 21:43:48 -0600

    Update unit_projectile_leash.lua

--- luarules/gadgets/unit_projectile_leash.lua
+++ luarules/gadgets/unit_projectile_leash.lua
@@ -11,8 +11,8 @@ end
 if not gadgetHandler:IsSyncedCode() then return end
 
 ---- unit customParams ----
--- use weaponDef customparams.projectile_overrange_distance to destroy projectiles exceeding its limit.
--- use weaponDef customparams.projectile_leash_range to destroy projectiles when they exceed range/projectile_overrange_distance and projectile_leash_range relative to unit position.
+-- use weaponDef customparams.overrange_distance to destroy projectiles exceeding its limit.
+-- use weaponDef customparams.leash_distance to destroy projectiles when they exceed range/overrange_distance and leash_distance relative to unit position.
 
 ---- optional customParams ----
 -- use weaponDef customparams.weaponDef.customParams.projectile_destruction_method = "string" to change how projectiles are destroyed.
@@ -22,18 +22,22 @@ if not gadgetHandler:IsSyncedCode() then return end
 --static values
 local forcedDescentUpdateFrames = math.ceil(Game.gameSpeed / 5)
 local compoundingMultiplier = 1.1 --compounding multiplier that influences the arc at which projectiles are forced to descend
+local gravityMultiplier = 1.8
 local descentSpeedStartingMultiplier = 0.15
-local flightTimeSlop = 0.9
-local projectileWatchModulus = math.ceil(Game.gameSpeed / 3)
+local myGravityFallback = 0.1445 --positive values give upwards gravity
 
 --functions
 local spGetUnitPosition = Spring.GetUnitPosition
 local mathRandom = math.random
 local mathCeil = math.ceil
+local mathFloor = math.floor
+local mathSqrt = math.sqrt
 local spGetProjectilePosition = Spring.GetProjectilePosition
 local spSetProjectileCollision = Spring.SetProjectileCollision
 local spGetProjectileVelocity = Spring.GetProjectileVelocity
 local spSetProjectileVelocity = Spring.SetProjectileVelocity
+local spGetProjectileGravity = Spring.GetProjectileGravity
+local spSetProjectileGravity = Spring.SetProjectileGravity
 
 --tables
 local defWatchTable = {}
@@ -41,6 +45,7 @@ local projectileMetaData = {}
 local flightTimeWatch = {}
 local projectileWatch = {}
 local forcedDescentTable = {}
+local gravityIncreaseTable = {}
 local killQueue = {}
 
 --variables
@@ -48,16 +53,15 @@ local gameFrame = 0
 
 
 for weaponDefID, weaponDef in pairs(WeaponDefs) do
-	if weaponDef.customParams.projectile_leash_range or weaponDef.customParams.projectile_overrange_distance then
+
+	if weaponDef.customParams.leash_distance or weaponDef.customParams.overrange_distance then
 		local watchParams = {}
-		if weaponDef.customParams.projectile_leash_range then
-			watchParams.leashRangeSq = (tonumber(weaponDef.customParams.projectile_leash_range)) ^ 2
-		end
-		if (weaponDef.salvoSize and weaponDef.salvoSize > 1) or (weaponDef.projectiles and weaponDef.projectiles > 1) then
-			watchParams.multiShot = true
+		if weaponDef.customParams.leash_distance then
+			watchParams.leashRangeSq = (tonumber(weaponDef.customParams.leash_distance)) ^ 2
 		end
 
-		local overRange = tonumber(weaponDef.customParams.projectile_overrange_distance) or weaponDef.range
+		local overRange = tonumber(weaponDef.customParams.overrange_distance) or weaponDef.range
+		watchParams.overRange = overRange
 		watchParams.overRangeSq = overRange ^ 2
 
 		local ascentFrames = 0
@@ -65,19 +69,27 @@ for weaponDefID, weaponDef in pairs(WeaponDefs) do
 			ascentFrames = weaponDef.uptime * Game.gameSpeed
 		end
 		watchParams.flightTimeFrames =
-		math.max(math.floor(((overRange / weaponDef.projectilespeed) * flightTimeSlop + ascentFrames) - projectileWatchModulus), 1)
+		math.max(math.floor(((overRange / weaponDef.projectilespeed) + ascentFrames)), 1)
 		watchParams.weaponDefID = weaponDefID
-		Spring.Echo("flightTimeFrames", watchParams.flightTimeFrames)
 
 		local destructionMethod = weaponDef.customParams.projectile_destruction_method or "explode"
 		if destructionMethod == "descend" then
 			watchParams.descentMethod = true
+		elseif destructionMethod == "gravity" then
+			watchParams.gravityMethod = true
+			watchParams.myGravity = -weaponDef.myGravity or -myGravityFallback --positive values give upwards gravity
+			if watchParams.myGravity == 0 then
+				watchParams.myGravity = -myGravityFallback
+			end
 		end
+
+
 
 		defWatchTable[weaponDefID] = watchParams
 		Script.SetWatchWeapon(weaponDefID, true)
 	end
 end
+
 
 local function distanceTooFar(maxRangeSq, x1, z1, x2, z2)
 	local dx = x2 - x1
@@ -88,11 +100,72 @@ local function distanceTooFar(maxRangeSq, x1, z1, x2, z2)
 end
 
 
-local function setDestructionFrame(proID)
-	local triggerFrame = mathCeil(gameFrame + mathRandom(projectileWatchModulus))
+local function recalculateFlightTime(proID, maxRange, x1, z1, x2, z2)
+    local dx = x2 - x1
+    local dz = z2 - z1
+    local distance = mathSqrt(dx * dx + dz * dz)
+
+    -- Check if the projectile is within the max range
+    if distance < maxRange then
+        local vx, _, vz = spGetProjectileVelocity(proID)
+        if not vx then return false end
+        
+        local proSpeed = mathSqrt(vx * vx + vz * vz)
+
+        local remainingDistance = maxRange - distance
+
+        if remainingDistance <= 0 then
+            return false
+        end
+
+        local frames = mathCeil(remainingDistance / proSpeed)
+
+        return frames
+    else
+        return false
+    end
+end
+
+
+
+local function projectileOverRangeCheck(weaponRange, originX, originZ, projectileX, projectileZ)
+	local dx1 = originX - projectileX
+	local dz1 = originZ - projectileZ
+	local weaponRangeSq = weaponRange * weaponRange
+	local distanceToOriginSq = dx1 * dx1 + dz1 * dz1
+	if distanceToOriginSq > weaponRangeSq then
+		if leashRange then
+			local distanceToOwner
+			local ownerX, ownerY, ownerZ = spGetUnitPosition(proOwnerID)
+			if ownerX then
+				local dx2 = ownerX - projectileX
+				local dz2 = ownerZ - projectileZ
+				distanceToOwner = mathSqrt(dx2 * dx2 + dz2 * dz2)
+				if distanceToOwner > leashRange then
+					return true
+				end
+			end
+			return false
+		end
+		return true
+	end
+	return false
+end
+
+
+local function setDestructionFrame(proID, newFlightTime)
+	local triggerFrame = gameFrame + newFlightTime --mathCeil(gameFrame + mathRandom(projectileWatchModulus))
 
 	killQueue[triggerFrame] = killQueue[triggerFrame] or {}
 	killQueue[triggerFrame][#killQueue[triggerFrame] + 1] = proID
+end
+
+
+local function setCheckFrame(proID, newFlightTime)
+	local triggerFrame = gameFrame + newFlightTime
+
+	flightTimeWatch[triggerFrame] = flightTimeWatch[triggerFrame] or {}
+	flightTimeWatch[triggerFrame][#flightTimeWatch[triggerFrame] + 1] = proID
 end
 
 
@@ -100,13 +173,7 @@ function gadget:ProjectileCreated(proID, proOwnerID, weaponDefID)
 	local defData = defWatchTable[weaponDefID]
 	if not defData then return end
 
-	local triggerFrame = gameFrame + defData.flightTimeFrames
-	local triggerFrameTable = flightTimeWatch[triggerFrame]
-	if not triggerFrameTable then
-		triggerFrameTable = {}
-		flightTimeWatch[triggerFrame] = triggerFrameTable
-	end
-	triggerFrameTable[#triggerFrameTable + 1] = proID
+	setCheckFrame(proID, defData.flightTimeFrames)
 
 	local metaData = { weaponDefID = weaponDefID, proOwnerID = proOwnerID }
 	local originX, _, originZ = spGetUnitPosition(proOwnerID)
@@ -116,50 +183,46 @@ function gadget:ProjectileCreated(proID, proOwnerID, weaponDefID)
 	projectileMetaData[proID] = metaData
 end
 
+function gadget:ProjectileDestroyed(proID)
+	projectileMetaData[proID] = nil
+	gravityIncreaseTable[proID] = nil
+end
+
 function gadget:GameFrame(frame)
 	gameFrame = frame
 
 	if flightTimeWatch[frame] then
 		for i, proID in ipairs(flightTimeWatch[frame]) do
-			projectileWatch[proID] = true
-		end
-		flightTimeWatch[frame] = nil
-	end
-
-	if frame % projectileWatchModulus == 2 then
-		for proID, bool in pairs(projectileWatch) do
 			local projectileX, _, projectileZ = spGetProjectilePosition(proID)
 			if projectileX then
 				local proData = projectileMetaData[proID]
 				local defData = defWatchTable[proData.weaponDefID]
-				if distanceTooFar(defData.overRangeSq, proData.originX, proData.originZ, projectileX, projectileZ) then
-					if defData.leashRangeSq then
-						local ownerX, _, ownerZ = spGetUnitPosition(proData.proOwnerID)
-						if not ownerX then
-							setDestructionFrame(proID)
-							projectileWatch[proID] = nil
-						elseif distanceTooFar(defData.leashRangeSq, ownerX, ownerZ, projectileX, projectileZ) then
-							setDestructionFrame(proID)
-							projectileWatch[proID] = nil
-						end
-					else
-						setDestructionFrame(proID)
-						projectileWatch[proID] = nil
-					end
+				local newFlightTime = recalculateFlightTime(proID, defData.overRange, proData.originX, proData.originZ, projectileX, projectileZ)
+				if newFlightTime then
+					setDestructionFrame(proID, newFlightTime)
+				else
+					setDestructionFrame(proID, 1) --destroy this frame
 				end
 			else
-				projectileWatch[proID] = nil -- remove destroyed projectiles
+				projectileMetaData[proID] = nil
 			end
 		end
+		flightTimeWatch[frame] = nil
 	end
 
 	if killQueue[frame] then
 		local descentMultiplier = descentSpeedStartingMultiplier
 		for i, proID in ipairs(killQueue[frame]) do
-			if defWatchTable[projectileMetaData[proID].weaponDefID].descentMethod then
-				forcedDescentTable[proID] = descentMultiplier
-			else
-				spSetProjectileCollision(proID)
+			local proData = projectileMetaData[proID]
+			if proData then
+				local defData = defWatchTable[proData.weaponDefID]
+				if defData.descentMethod then
+					forcedDescentTable[proID] = descentMultiplier
+				elseif defData.gravityMethod then
+					gravityIncreaseTable[proID] = defData.myGravity
+				else
+					spSetProjectileCollision(proID)
+				end
 			end
 			projectileMetaData[proID] = nil
 		end
@@ -176,6 +239,15 @@ function gadget:GameFrame(frame)
 			else
 				forcedDescentTable[proID] = nil
 			end
+		end
+	end
+
+	if frame % 10 == 4 then
+		for proID, myGravity in pairs(gravityIncreaseTable) do
+			myGravity = myGravity * gravityMultiplier
+			gravityIncreaseTable[proID] = myGravity
+			spSetProjectileGravity(proID, myGravity)
+			Spring.Echo(myGravity, frame, gravityMultiplier)
 		end
 	end
 end
