2e9f89cabe350d7ad215b914fa228ca6edcb832d
Damgam <damiano1398@gmail.com>
2025-01-04 16:45:21 +0100

    Revert "Shields Re-Rework (#4077)"
    This reverts commit c4b018b3a94f8607a525065a71284559ee74a3f6.

--- modoptions.lua
+++ modoptions.lua
@@ -1250,10 +1250,10 @@ local options = {
     -- Hidden Tests
     {
         key    	= "shieldsrework",
-        name   	= "Shields Rework v2.0",
-        desc   	= "Shields block plasma. Overkill damage is absorbed. Shield is down for the duration required to recharge the overkill damage at normal energy cost.",
+        name   	= "Shields Rework",
+        desc   	= "Shields block all projectiles. Overkill damage is blocked once before reaching 0% charge. Shields are disabled for a few seconds upon reaching 0%.",
         type   	= "bool",
-        hidden 	= false,
+        hidden 	= true,
         section = "options_experimental",
         def  	= false,
     },
