d7fb4bf3c5858a304d7e5a2221744d8264c85439
SethDGamre <sethdgamre@gmail.com>
2025-01-04 12:08:25 -0600

    Update alldefs_post.lua

--- gamedata/alldefs_post.lua
+++ gamedata/alldefs_post.lua
@@ -1535,7 +1535,8 @@ function WeaponDef_Post(name, wDef)
 				wDef.shield.exterior = true
 			end
 
-			if wDef.customparams and ((not wDef.interceptedbyshieldtype or wDef.interceptedbyshieldtype ~= 1) and wDef.weapontype ~= "Cannon") then
+			if ((not wDef.interceptedbyshieldtype or wDef.interceptedbyshieldtype ~= 1) and wDef.weapontype ~= "Cannon") then
+				wDef.customparams = wDef.customparams or {}
 				wDef.customparams.shield_aoe_penetration = true
 			end
 
