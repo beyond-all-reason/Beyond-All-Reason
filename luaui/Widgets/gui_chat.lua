fef22d0ca9e34e7051f4d1bb8832f14d765a2109
Saurtron <saurtron@riseup.net>
2025-01-08 11:13:27 +0100

    Don't ignore deprecation messages when devmode and singleplayer detected.

--- luaui/Widgets/gui_chat.lua
+++ luaui/Widgets/gui_chat.lua
@@ -22,6 +22,7 @@ local LineTypes = {
 local utf8 = VFS.Include('common/luaUtilities/utf8.lua')
 
 local L_DEPRECATED = LOG.DEPRECATED
+local isDevSingle = (Spring.Utilities.IsDevMode() and Spring.Utilities.Gametype.IsSinglePlayer())
 
 local showHistoryWhenChatInput = true
 
@@ -2090,7 +2091,7 @@ function widget:MapDrawCmd(playerID, cmdType, x, y, z, a, b, c)
 end
 
 function widget:AddConsoleLine(lines, priority)
-	if priority and priority == L_DEPRECATED then return end
+	if priority and priority == L_DEPRECATED and not isDevSingle then return end
 	lines = lines:match('^%[f=[0-9]+%] (.*)$') or lines
 	for line in lines:gmatch("[^\n]+") do
 		processAddConsoleLine(spGetGameFrame(), line, true)
