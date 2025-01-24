2675f1b852947a1e5a518c7a86c0f289f581427b
Floris <floris@ruwhof.net>
2025-01-24 17:04:31 +0100

    support even larger player sizes + auto hide enemies list when aliveteams > 40 + take ui scale better into account

--- luaui/Widgets/gui_advplayerslist.lua
+++ luaui/Widgets/gui_advplayerslist.lua
@@ -1425,11 +1425,19 @@ function SortList()
             end
         end
     end
-    local deadTeamSize = 0.66
-    playerScale = math.max(0.4, math.min(1, 31 / (aliveTeams+(deadTeams*deadTeamSize))))
-    if #Spring_GetAllyTeamList() > 24 then
-        playerScale = playerScale - (playerScale * ((#Spring_GetAllyTeamList()-2)/400))  -- reduce size some more when mega ffa
+    -- hide enemies when there are more than 40 teams on startup
+    if not initiated and Spring.GetGameFrame() == 0 then
+        initiated = true
+        if aliveTeams > 40 then
+            enemyListShow = false
+        end
     end
+    local deadTeamSize = 0.66
+    playerScale = math.min(1, 38 / (aliveTeams+(deadTeams*deadTeamSize)))
+    if #Spring_GetAllyTeamList() > 24 then
+        playerScale = playerScale - 0.05 - (playerScale * ((#Spring_GetAllyTeamList()-2)/200))  -- reduce size some more when mega ffa
+    end
+    playerScale = playerScale - (playerScale * (Spring.GetConfigFloat("ui_scale", 1)-1))
 
     -- calls the (cascade) sorting for players
     vOffset = SortAllyTeams(vOffset)
