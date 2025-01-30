816405f5debc3826c8f2019e4f20357bae4fd85c
Varunda <66178574+Varunda@users.noreply.github.com>
2025-01-30 11:55:17 -0800

    update widget:UnitDestroyed to include attackerID, attackerDefID, and attackerTeam
    visibility checks already occur when populating these fields, so this does not leak data that players wouldn't have access to otherwise

--- luaui/barwidgets.lua
+++ luaui/barwidgets.lua
@@ -2262,11 +2262,11 @@ function widgetHandler:UnitFromFactory(unitID, unitDefID, unitTeam, factID, fact
 	return
 end
 
-function widgetHandler:UnitDestroyed(unitID, unitDefID, unitTeam)
+function widgetHandler:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
 	widgetHandler:MetaUnitRemoved(unitID, unitDefID, unitTeam)
 	tracy.ZoneBeginN("W:UnitDestroyed")
 	for _, w in ipairs(self.UnitDestroyedList) do
-		w:UnitDestroyed(unitID, unitDefID, unitTeam)
+		w:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
 	end
 	tracy.ZoneEnd()
 	return
