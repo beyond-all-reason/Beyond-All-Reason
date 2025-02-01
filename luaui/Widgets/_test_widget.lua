e59faa197b8ff2c7546c140a99dc13af6871a66c
patroldo <patroldo97@gmail.com>
2025-02-02 01:58:05 +0200

    Added showing basic end-game results

--- luaui/Widgets/_test_widget.lua
+++ luaui/Widgets/_test_widget.lua
@@ -7,14 +7,13 @@ end
 
 function widget:GetInfo()
 	return {
-		name      = "Demo RML Gui",
-		desc      = "A sandbox for the Rml powered GUI.",
-		author    = "ChrisFloofyKitsune",
-		date      = "2024-03-17",
-		license   = "https://unlicense.org/",
-		layer     = -828888,
-		handler   = true,
-		enabled   = true
+		name = "Demo RML Gui",
+		desc = "A sandbox for the Rml powered GUI.",
+		author = "ChrisFloofyKitsune",
+		date = "2024-03-17",
+		license = "https://unlicense.org/",
+		layer = -828888,
+		enabled = true,
 	}
 end
 
@@ -24,7 +23,9 @@ widget.rmlContext = nil
 local playerListByTeam = {} -- does not contain specs
 
 -- this can be overwritten later to change what code exampleEventHook calls
-local eventCallback = function(ev, ...) Spring.Echo('orig function says', ...) end;
+local eventCallback = function(ev, ...)
+	Spring.Echo("orig function says", ...)
+end
 
 local isVisible = false
 local dm_handle
@@ -34,16 +35,38 @@ local threshold = 150000
 local modelForPresenter = {
 	resourcesDestroyed = {
 		playerName = "Test",
-		score = 12321
+		score = 12321,
+		visible = true,
 	},
 	enemiesDestroyed = {},
 	resourcesEfficiency = {},
 	traitor = {},
 	goldenCow = {},
 	ecoAward = {},
-	damageReceived = {},
-	commanderSleep = {}
+	damageRecieved = {},
+	commanderSleep = {},
 }
+
+local function initializeContext()
+	widget.rmlContext = RmlUi.CreateContext(widget.whInfo.name)
+	-- use the DataModel handle to set values
+	-- only keys declared at the DataModel's creation can be used
+	dm_handle = widget.rmlContext:OpenDataModel("data_model_test", {
+		model = modelForPresenter,
+		exampleValue = "Changes when clicked",
+		-- Functions inside a DataModel cannot be changed later
+		-- so instead a function variable external to the DataModel is called and _that_ can be changed
+		exampleEventHook = function(...)
+			eventCallback(...)
+		end,
+	})
+
+	eventCallback = function(ev, ...)
+		Spring.Echo(ev.parameters.mouse_x, ev.parameters.mouse_y, ev.parameters.button, ...)
+		local options = { "ow", "oof!", "stop that!", "clicking go brrrr" }
+		dm_handle.exampleValue = options[math.random(1, 4)]
+	end
+end
 
 local function findPlayerName(teamID)
 	local plList = playerListByTeam[teamID]
@@ -52,27 +75,38 @@ local function findPlayerName(teamID)
 	if plList[1] then
 		name = plList[1]
 		if #plList > 1 then
-			name = Spring.I18N('ui.awards.coop', { name = name })
+			name = Spring.I18N("ui.awards.coop", { name = name })
 		end
 	else
-		name = Spring.I18N('ui.awards.unknown')
+		name = Spring.I18N("ui.awards.unknown")
 	end
 
 	return name
 end
 
-local function createAward(award, note, winnersTable)
-	local winnerTeamID, secondTeamID, thirdTeamID = winnersTable[1].teamID, winnersTable[2].teamID, winnersTable[3].teamID
+local function createAward(award, winnersTable)
+	local winnerTeamID, secondTeamID, thirdTeamID =
+		winnersTable[1].teamID, winnersTable[2].teamID, winnersTable[3].teamID
 	local winnerScore, secondScore, thirdScore = winnersTable[1].score, winnersTable[2].score, winnersTable[3].score
 	local winnerName, secondName, thirdName
 
+	local notAwardedText = Spring.I18N("ui.awards.notAwarded")
+
 	winnerName = winnerTeamID >= 0 and findPlayerName(winnerTeamID) or notAwardedText
 	secondName = secondTeamID >= 0 and findPlayerName(secondTeamID) or notAwardedText
-	thirdName  = thirdTeamID  >= 0 and findPlayerName(thirdTeamID)  or notAwardedText
+	thirdName = thirdTeamID >= 0 and findPlayerName(thirdTeamID) or notAwardedText
+
+	modelForPresenter[award].score = winnerScore
+	modelForPresenter[award].playerName = winnerName
+	modelForPresenter[award].visible = true
 end
 
 local function ProcessAwards(awards)
-	if not awards then return end
+	if not awards then
+		Spring.Echo("i am here new")
+		return
+	end
+	Spring.Echo("proceeding new")
 	WG.awards = awards
 	local traitorWinner = awards.traitor[1]
 	local cowAwardWinner = awards.goldenCow[1].teamID
@@ -81,41 +115,20 @@ local function ProcessAwards(awards)
 	table.insert(compoundAwards, awards.damageReceived[1])
 	table.insert(compoundAwards, awards.sleep[1])
 	if awards.ecoKill[1].teamID >= 0 then
-		FirstAward = createAward('fuscup', Spring.I18N('ui.awards.resourcesDestroyed'), awards.ecoKill)
+		createAward("resourcesDestroyed", awards.ecoKill)
 	end
-	if awards.fightKill[1].teamID >= 0 then
-		SecondAward = createAward('bullcup', Spring.I18N('ui.awards.enemiesDestroyed'), awards.fightKill)
+	if awards.eco[1].teamID >= 0 then
+		createAward("ecoAward", awards.ecoKill)
 	end
-	if awards.efficiency[1].teamID >= 0 then
-		ThirdAward = createAward('comwreath', Spring.I18N('ui.awards.resourcesEfficiency'), awards.efficiency)
+	if awards.damageReceived[1].teamID >= 0 then
+		createAward("damageRecieved", awards.ecoKill)
 	end
-
-	if traitorWinner.score > threshold then
-		FourthAward = createAward('traitor', Spring.I18N('ui.awards.traitor'), awards.traitor)
+	if awards.sleep[1].teamID >= 0 then
+		createAward("sleep", awards.ecoKill)
 	end
-	if cowAwardWinner ~= -1 then
-		CowAward = createAward('cow', Spring.I18N('ui.awards.didEverything'), awards.goldenCow)
-	end
-	OtherAwards = createAward('other', 'Other awards', compoundAwards)
 end
 
 function showDocument()
-	-- use the DataModel handle to set values
-	-- only keys declared at the DataModel's creation can be used
-	dm_handle = widget.rmlContext:OpenDataModel("data_model_test", {
-		model = modelForPresenter,
-		exampleValue = 'Changes when clicked',
-		-- Functions inside a DataModel cannot be changed later
-		-- so instead a function variable external to the DataModel is called and _that_ can be changed
-		exampleEventHook = function(...) eventCallback(...) end
-	});
-
-	eventCallback = function (ev, ...)
-		Spring.Echo(ev.parameters.mouse_x, ev.parameters.mouse_y, ev.parameters.button, ...)
-		local options = {"ow", "oof!", "stop that!", "clicking go brrrr"}
-		dm_handle.exampleValue = options[math.random(1, 4)]
-	end
-
 	document = widget.rmlContext:LoadDocument("LuaUi/Widgets/rml_assets/simple_demo.rml", widget)
 	document:ReloadStyleSheet()
 	document:Show()
@@ -128,9 +141,9 @@ function hideDocument()
 end
 
 local function initializeWidget()
-	widget.rmlContext = RmlUi.CreateContext(widget.whInfo.name)
-	WG['rml_ui_widget'] = {}
-	WG['rml_ui_widget'].toggle = function()
+	initializeContext()
+	WG["rml_ui_widget"] = {}
+	WG["rml_ui_widget"].toggle = function()
 		if isVisible == false then
 			showDocument()
 			isVisible = true
@@ -139,13 +152,13 @@ local function initializeWidget()
 			isVisible = false
 		end
 	end
-	WG['rml_ui_widget'].isvisible = function()
+	WG["rml_ui_widget"].isvisible = function()
 		return isVisible
 	end
 end
 
 local function disableShowEndGraph()
-	Spring.SendCommands('endgraph 2')
+	Spring.SendCommands("endgraph 2")
 end
 
 local function initializeTeamList()
@@ -164,7 +177,8 @@ local function initializeTeamList()
 end
 
 local function registerGlobalFunctionForReceivingAwardsFromHandler()
-	widgetHandler:RegisterGlobal('GadgetReceiveAwardsNew', ProcessAwards)
+	widgetHandler:RegisterGlobal("GadgetReceiveAwardsNew", ProcessAwards)
+	Spring.Echo("Registered gnew ")
 end
 
 function widget:Initialize()
@@ -172,6 +186,7 @@ function widget:Initialize()
 	registerGlobalFunctionForReceivingAwardsFromHandler()
 	initializeWidget()
 	initializeTeamList()
+	ProcessAwards(WG.awards)
 end
 
 function widget:Shutdown()
@@ -181,4 +196,5 @@ function widget:Shutdown()
 	if widget.rmlContext then
 		RmlUi.RemoveContext(widget.whInfo.name)
 	end
+	widgetHandler:DeregisterGlobal("GadgetReceiveAwardsNew")
 end
