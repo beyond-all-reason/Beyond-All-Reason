3eb283cf4034256913f8bf9cd286036ff435838a
Floris <floris@ruwhof.net>
2025-02-13 18:12:08 +0100

    windgenerator -> windGenerator

--- luarules/gadgets/unit_windmill_control.lua
+++ luarules/gadgets/unit_windmill_control.lua
@@ -22,7 +22,7 @@ end
 
 local unitEnergyMultiplier = {}
 for udid, ud in pairs(UnitDefs) do
-	if ud.windgenerator then
+	if ud.windGenerator then
 		if energyMultActive then
 			windDefs[udid] = true
 		end
