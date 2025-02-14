5976bec95241b0b88ad522a72918a087225b2968
tetrisface <89279737+tetrisface@users.noreply.github.com>
2025-02-14 01:25:22 +0100

    feat(unit evolution): chunk evolutions to handle mass tweaks - format

--- luarules/gadgets/unit_evolution.lua
+++ luarules/gadgets/unit_evolution.lua
@@ -233,29 +233,29 @@ if gadgetHandler:IsSyncedCode() then
 		ReAssignAssists(newUnitID,unitID)
 
 
-			if commandQueue[1] then
-				local teamID = Spring.GetUnitTeam(unitID)
-				for _,command in pairs(commandQueue) do
-					local coded = command.options.coded + (command.options.shift and 0 or CMD.OPT_SHIFT) -- orders without SHIFT can appear at positions other than the 1st due to CMD.INSERT; they'd cancel any previous commands if added raw
-					if command.id < 0 then -- repair case for construction
-						local units = CallAsTeam(teamID, Spring.GetUnitsInRectangle, command.params[1] - 16, command.params[3] - 16, command.params[1] + 16, command.params[3] + 16, -3)
-						local notFound = true
-						for j = 1, #units do
-							local areaUnitID = units[j]
-							if Spring.GetUnitDefID(areaUnitID) == -command.id then
-								Spring.GiveOrderToUnit(newUnitID, CMD.REPAIR, areaUnitID, coded)
-								notFound = false
-								break
-							end
+		if commandQueue[1] then
+			local teamID = Spring.GetUnitTeam(unitID)
+			for _,command in pairs(commandQueue) do
+				local coded = command.options.coded + (command.options.shift and 0 or CMD.OPT_SHIFT) -- orders without SHIFT can appear at positions other than the 1st due to CMD.INSERT; they'd cancel any previous commands if added raw
+				if command.id < 0 then -- repair case for construction
+					local units = CallAsTeam(teamID, Spring.GetUnitsInRectangle, command.params[1] - 16, command.params[3] - 16, command.params[1] + 16, command.params[3] + 16, -3)
+					local notFound = true
+					for j = 1, #units do
+						local areaUnitID = units[j]
+						if Spring.GetUnitDefID(areaUnitID) == -command.id then
+							Spring.GiveOrderToUnit(newUnitID, CMD.REPAIR, areaUnitID, coded)
+							notFound = false
+							break
 						end
-						if notFound then
-							Spring.GiveOrderToUnit(newUnitID, command.id, command.params, coded)
-						end
-					else
+					end
+					if notFound then
 						Spring.GiveOrderToUnit(newUnitID, command.id, command.params, coded)
 					end
+				else
+					Spring.GiveOrderToUnit(newUnitID, command.id, command.params, coded)
 				end
 			end
+		end
 
 		if transporter then
 			spGiveOrderToUnit(transporter, CMD.LOAD_UNITS, { newUnitID }, 0)
