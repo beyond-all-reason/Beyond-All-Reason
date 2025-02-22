0152a7fba8a43ac9cdabbef96715c16dc2ddd0aa
TheFutureKnight <23344114+TheFutureKnight@users.noreply.github.com>
2025-02-22 20:42:20 +0200

    changed check for factory, allow only build command

--- luaui/Widgets/unit_idle_guard.lua
+++ luaui/Widgets/unit_idle_guard.lua
@@ -1,8 +1,9 @@
-local WIDGET_VER = 1
+local WIDGET_VER = 1.1
 
 --[[
 ### CHANGELOGS ###
 1.0 INITIAL
+1.1 Change checking for factory
 ]]
 
 
@@ -26,9 +27,9 @@ function widget:GetInfo()
   -- Automatically generated local definitions
 
   local CMD_GUARD            = CMD.GUARD
+  local OPT_SHIFT            = CMD.OPT_SHIFT
   local spGetMyTeamID        = Spring.GetMyTeamID
   local spGiveOrderToUnit    = Spring.GiveOrderToUnit
-  local spGetUnitDefID       = Spring.GetUnitDefID
   local spGetUnitsInSphere   = Spring.GetUnitsInSphere
   local spGetUnitCommands    = Spring.GetUnitCommands
 
@@ -37,11 +38,12 @@ function widget:GetInfo()
   --------------------------------------------------------------------------------
 
 function widget:UnitCmdDone(unitID, unitDefID, unitTeam,
-                          _, cmdParams, _, _)
+                          cmdID, cmdParams, _, _)
 
   if (unitTeam ~= spGetMyTeamID()) then
     return
   end
+
   local ud = UnitDefs[unitDefID]
   if ud and ud.isBuilder and ud.canAssist and ud.canMove and not ud.isFactory then
     -- Check if the unit has a queue
@@ -49,15 +51,15 @@ function widget:UnitCmdDone(unitID, unitDefID, unitTeam,
       if cmds > 0 then
         return
       end
-    -- Check for factory (hacky way for this but works)
-      if cmdParams[1] and cmdParams[2] and cmdParams[3] then
+
+    -- Check for factory
+      if cmdID < 0 then
         local fdTable = spGetUnitsInSphere(cmdParams[1], cmdParams[2], cmdParams[3], 50)
         for _, fact in ipairs(fdTable) do -- Get first result that matches a factory
           if fact then
-            local fdDef = spGetUnitDefID(fact)
-            local factDef = UnitDefs[fdDef]
+            local factDef = UnitDefs[-cmdID]
             if factDef.isFactory then
-              spGiveOrderToUnit(unitID, CMD_GUARD, fact, CMD.OPT_SHIFT)
+              spGiveOrderToUnit(unitID, CMD_GUARD, fact, OPT_SHIFT)
               break
             end
           end
