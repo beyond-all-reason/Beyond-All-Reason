3ecdc0a05403a65614ac7039294ca3bbb72a5549
Floris <floris@ruwhof.net>
2025-03-08 19:47:33 +0100

    disable UseFontConfigLib

--- luaintro/springconfig.lua
+++ luaintro/springconfig.lua
@@ -215,5 +215,5 @@ Spring.SetConfigInt("UpdateWeaponVectorsMT", 1)
 
 -- Breaking/limiting the curse of RA 𓀀 𓀁 𓀂 𓀃 𓀄 𓀅 𓀆 𓀇 𓀈 𓀉 𓀊 𓀋 𓀌 𓀍 𓀎 𓀏 𓀐 𓀑 𓀒 𓀓 𓀔 𓀕 𓀖
 Spring.SetConfigInt("MaxFontTries", 2)
---Spring.SetConfigInt("UseFontConfigLib", 1)
+Spring.SetConfigInt("UseFontConfigLib", 0)
 
