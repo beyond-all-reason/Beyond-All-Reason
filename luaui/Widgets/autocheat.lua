a88e4642a9727e91c9f44e28b40a99eb2ce4d2b1
tetrisface <89279737+tetrisface@users.noreply.github.com>
2025-03-11 18:29:53 +0100

    switch to Update callin

--- luaui/Widgets/autocheat.lua
+++ luaui/Widgets/autocheat.lua
@@ -14,31 +14,17 @@ function widget:GetInfo()
 	}
 end
 
-local gf = Spring.GetGameFrame()
+function widget:Update(f)
+	if not Spring.IsCheatingEnabled() then
+		Spring.SendCommands("say !cheats")
+		Spring.SendCommands("say !hostsay /globallos")
+		Spring.SendCommands("say !hostsay /godmode")
+		--Spring.SendCommands("say !hostsay /nocost")
 
-function widget:Initialize()
-	local enable = (not Spring.IsCheatingEnabled())
-	if enable then
-		widgetHandler:UpdateCallIn('GameFrame');
-	else
-		widgetHandler:RemoveCallIn('GameFrame');
+		Spring.SendCommands("cheat")
+		Spring.SendCommands("globallos")
+		Spring.SendCommands("godmode")
+		--Spring.SendCommands("nocost")
 	end
-end
-
-function widget:GameFrame(f)
-	if f > gf then
-		if not Spring.IsCheatingEnabled() and not Spring.IsReplay() then 
-			Spring.SendCommands("say !cheats")
-			Spring.SendCommands("say !hostsay /globallos")
-			Spring.SendCommands("say !hostsay /godmode")
-			--Spring.SendCommands("say !hostsay /nocost")
-
-			Spring.SendCommands("cheat")
-			Spring.SendCommands("globallos")
-			Spring.SendCommands("godmode")
-			--Spring.SendCommands("nocost")
-		end
-
-		widgetHandler:RemoveCallIn('GameFrame');
-	end
+	widgetHandler:RemoveCallIn('Update');
 end
