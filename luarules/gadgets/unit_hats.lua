1fee8701c00948cb617c8d30b0626be8a00b7902
Floris <ruwetuin@hotmail.com>
2025-03-23 19:07:28 +0100

    Update unit_hats.lua (#4580)

--- luarules/gadgets/unit_hats.lua
+++ luarules/gadgets/unit_hats.lua
@@ -170,16 +170,16 @@ function gadget:GameFrame(gf)
 						if string.sub(UnitDefs[unitDefID].name, 1, 3) == 'arm' then
 							local scriptEnv = Spring.UnitScript.GetScriptEnv(unitID)
 							if scriptEnv then
-								if kings[playerName] and scriptEnv['ShowCrown'] then
+								if MatchPlayer(kings, playerName, accountID) and scriptEnv['ShowCrown'] then
 									Spring.UnitScript.CallAsUnit(unitID, scriptEnv['ShowCrown'], true)
 								end
-								if goldMedals[playerName] and scriptEnv['ShowMedalGold'] then
+								if MatchPlayer(goldMedals, playerName, accountID) and scriptEnv['ShowMedalGold'] then
 									Spring.UnitScript.CallAsUnit(unitID, scriptEnv['ShowMedalGold'], true)
 								end
-								if silverMedals[playerName] and scriptEnv['ShowMedalSilver'] then
+								if MatchPlayer(silverMedals, playerName, accountID) and scriptEnv['ShowMedalSilver'] then
 									Spring.UnitScript.CallAsUnit(unitID, scriptEnv['ShowMedalSilver'], true)
 								end
-								if bronzeMedals[playerName] and scriptEnv['ShowMedalBronze'] then
+								if MatchPlayer(bronzeMedals, playerName, accountID) and scriptEnv['ShowMedalBronze'] then
 									Spring.UnitScript.CallAsUnit(unitID, scriptEnv['ShowMedalBronze'], true)
 								end
 							end
