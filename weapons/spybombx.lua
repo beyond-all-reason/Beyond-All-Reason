8718f1bb4067083395e5604980e344fdbdc3e7a5
jcp219 <jcp219@gmail.com>
2025-03-23 16:44:54 -0400

    Conflict resolution / match master

--- weapons/spybombx.lua
+++ weapons/spybombx.lua
@@ -4,10 +4,6 @@ return {
 		commandfire = 1,
 		craterboost = 0,
 		cratermult = 0,
-		customparams = {
-			--fixed_stun_duration = "isBuilding=true:20,customparams.unitgroup=antinuke:20",	-- Fixed 20s stun duration for buildings and antinukes.
-			--paralyzetime_exception = "isBuilding=false:8",									-- UP TO 8s stun duration for mobile units (depending on hp vs damage)
-		},
 		edgeeffectiveness = 1,
 		explosiongenerator = "custom:genericshellexplosion-huge-lightning",
 		firestarter = 90,
