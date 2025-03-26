ddf487be034fdcdd8955e29012ea9c7d47fcd13d
Rhys van der Waerden <rhys.vdw@gmail.com>
2025-03-26 14:53:17 +1100

    Add gadget type

--- types/gadget.lua
+++ types/gadget.lua
@@ -1,6 +1,6 @@
 ---@meta
 
----@class Gadget : Callins, UnsyncedCallins, SyncedCallins
+---@class Gadget : Addon, Callins, UnsyncedCallins, SyncedCallins
 ---
 ---Gadgets can control game logic and receive synced and unsynced callins.
 ---
@@ -27,23 +27,9 @@
 ---@field ghInfo FullGadgetInfo
 local Gadget = {}
 
----@class GadgetInfo
----@field name string?
----@field desc string?
----@field author string?
----@field date string?
----@field license string?
----@field layer number?
----@field enabled boolean?
-
----@class FullGadgetInfo : GadgetInfo
+---@class FullGadgetInfo : AddonInfo
 ---@field filename string
 ---@field basename string
-
-
----Get info about a gadget.
----@return GadgetInfo
-function Gadget:GetInfo() end
 
 ---@type Gadget
 ---@diagnostic disable-next-line: lowercase-global
