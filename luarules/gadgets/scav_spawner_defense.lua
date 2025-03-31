25e991fdffcb7249087cd11574569bf68ad34000
A-childs-encyclopedia <abdhllah005@gmail.com>
2025-03-31 23:34:02 +0530

    Update scav_spawner_defense.lua

--- luarules/gadgets/scav_spawner_defense.lua
+++ luarules/gadgets/scav_spawner_defense.lua
@@ -187,7 +187,7 @@ if gadgetHandler:IsSyncedCode() then
 	local scavTeamID = Spring.Utilities.GetScavTeamID()
 	local scavAllyTeamID = Spring.Utilities.GetScavAllyTeamID()
 
-	local gaiaTeamID = GetGaiaTeamID() --??
+	local gaiaTeamID = GetGaiaTeamID()
 	if not scavTeamID then
 		scavTeamID = gaiaTeamID
 		scavAllyTeamID = select(6, Spring.GetTeamInfo(scavTeamID))
