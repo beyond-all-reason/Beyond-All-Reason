d49cbb228f1b1f449bf0525efb38a939e7c9c61e
A-childs-encyclopedia <abdhllah005@gmail.com>
2025-04-07 15:29:14 +0530

    Update raptor_spawn_defs.lua

--- luarules/configs/raptor_spawn_defs.lua
+++ luarules/configs/raptor_spawn_defs.lua
@@ -1348,7 +1348,15 @@ addNewSquad({
 	}
 })
 
-addNewSquad({ type = "special", minAnger = 80, units = 	{ "5 raptor_allterrain_swarmer_basic_t4_v1" }, weight = 2, maxAnger = 1000 })
+addNewSquad({
+	type = "special",
+	weight = 2,
+	minAnger = 80,
+	maxAnger = 1000,
+	units = {
+		{count = 5, unit = "raptor_allterrain_swarmer_basic_t4_v1" }
+	}
+})
 
 
 addNewSquad({ type = "special", minAnger = 50, units = 	{ "10 raptor_allterrain_swarmer_emp_t2_v1" } })
