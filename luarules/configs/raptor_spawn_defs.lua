09682cd3082ae7779078c19181841c93e5c555d4
A-childs-encyclopedia <abdhllah005@gmail.com>
2025-04-07 22:06:13 +0530

    Update raptor_spawn_defs.lua

--- luarules/configs/raptor_spawn_defs.lua
+++ luarules/configs/raptor_spawn_defs.lua
@@ -2275,13 +2275,37 @@ addNewSquad({ type = "special", minAnger = 80, units = 	{ "16 raptor_land_swarme
 addNewSquad({ type = "special", minAnger = 100, units = { "32 raptor_land_swarmer_brood_t2_v1" }, maxAnger = 1000 })
 
 
-addNewSquad({ type = "special", minAnger = 20, units = 	{ "1 raptor_land_swarmer_brood_t3_v1" } })
+addNewSquad({
+	type = "special",
+	minAnger = 20,
+	units = {
+		{count = 1, unit = "raptor_land_swarmer_brood_t3_v1"}
+	}
+})
 
-addNewSquad({ type = "special", minAnger = 40, units = 	{ "2 raptor_land_swarmer_brood_t3_v1" } })
+addNewSquad({
+	type = "special",
+	minAnger = 40,
+	units = {
+		{count = 2, unit = "raptor_land_swarmer_brood_t3_v1"}
+	}
+})
 
-addNewSquad({ type = "special", minAnger = 60, units = 	{ "4 raptor_land_swarmer_brood_t3_v1" } })
+addNewSquad({
+	type = "special",
+	minAnger = 60,
+	units = {
+		{count = 4, unit = "raptor_land_swarmer_brood_t3_v1"}
+	}
+})
 
-addNewSquad({ type = "special", minAnger = 80, units = 	{ "8 raptor_land_swarmer_brood_t3_v1" } })
+addNewSquad({
+type = "special",
+minAnger = 80,
+	units = {
+		{count = 8, unit = "raptor_land_swarmer_brood_t3_v1"}
+	}
+})
 
 addNewSquad({ type = "special", minAnger = 100, units = { "16 raptor_land_swarmer_brood_t3_v1" }, maxAnger = 1000 })
 
