23ec2185ae8cf04625c1ece1870e923fe586eee8
SamWain <slousebox@gmail.com>
2025-04-08 09:23:53 +1000

    Exclude walls from area set target
    Overwrites area set target to exclude walls if it detects non wall units in selection area

---
+++ luaui/Widgets/cmd_exclude_walls_area_set_target.lua
@@ -0,0 +1,81 @@
+function widget:GetInfo()
+	return {
+		name = "Exclude features from area set target",
+		desc = "Stops critters and walls from being included in area set target if units are present",
+		license = "GNU GPL, v2 or later",
+		layer = 0,
+		enabled = true
+	}
+end
+
+local spGetUnitDefID = Spring.GetUnitDefID
+local spGetUnitAllyTeam = Spring.GetUnitAllyTeam
+local excludedUnitsDefID = {74, 89, 102, 307, 323, 332}
+
+local CMD_SET_TARGET = 34923
+
+local gameStarted
+
+local function contains(table, value)
+	for i, DefID in ipairs(table) do
+		if (DefID == value) then
+		return true
+		end
+	end
+	return false
+end
+
+function maybeRemoveSelf()
+	if Spring.GetSpectatingState() and (Spring.GetGameFrame() > 0 or gameStarted) then
+		widgetHandler:RemoveWidget()
+	end
+end
+
+function widget:GameStart()
+	gameStarted = true
+	maybeRemoveSelf()
+end
+
+function widget:PlayerChanged(playerID)
+	maybeRemoveSelf()
+end
+
+function widget:Initialize()
+	if Spring.IsReplay() or Spring.GetGameFrame() > 0 then
+		maybeRemoveSelf()
+	end
+end
+
+function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
+	if cmdID ~= CMD_SET_TARGET or #cmdParams ~= 4 then
+		return
+	end
+
+	local cmdX, cmdY, cmdZ = cmdParams[1], cmdParams[2], cmdParams[3]
+
+	local mouseX, mouseY = Spring.WorldToScreenCoords(cmdX, cmdY, cmdZ)
+
+	local cmdRadius = cmdParams[4]
+
+	local areaUnits = Spring.GetUnitsInCylinder(cmdX, cmdZ, cmdRadius, -4)
+
+	local newCmds = {}
+	for i = 1, #areaUnits do
+		local unitID = areaUnits[i]
+			local newCmdOpts = {}
+			if #newCmds ~= 0 or cmdOpts.shift then
+				newCmdOpts = { "shift" }
+			end
+			local unitDefID = Spring.GetUnitDefID(unitID)
+			if not contains(excludedUnitsDefID, unitDefID) then
+				newCmds[#newCmds + 1] = { CMD_SET_TARGET, { unitID }, newCmdOpts }
+			end
+	end
+
+	if #newCmds > 0 then
+		Spring.GiveOrderArrayToUnitArray(Spring.GetSelectedUnits(), newCmds)
+		return true
+	end
+end
+
+
