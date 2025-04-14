73907cd44df55052eefac3d1fde762b187624a4f
SamWain <slousebox@gmail.com>
2025-04-14 14:40:46 +1000

    Remove unused variables and added bool somethingWasExcluded
    updated for loop to check if `somethingWasExcluded` and only replace the default command if true

--- luaui/Widgets/cmd_exclude_walls_area_set_target.lua
+++ luaui/Widgets/cmd_exclude_walls_area_set_target.lua
@@ -5,6 +5,7 @@ function widget:GetInfo()
 		name 		= "Exclude walls from area set target",
 		desc 		= "Stops walls from being included in area set target if units are present",
 		date		= "April 2025",
+		author		= "Slouse",
 		license 	= "GNU GPL, v2 or later",
 		layer 		= 0,
 		enabled 	= true
@@ -14,8 +15,6 @@ end
 local excludedUnitsDefID = {}
 local CMD_SET_TARGET = 34923
 local spGetUnitDefID = Spring.GetUnitDefID
-
-local gameStarted
 
 for id, unitDef in pairs(UnitDefs) do
 	if unitDef.customParams.objectify then
@@ -27,14 +26,14 @@ function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
 	if cmdID ~= CMD_SET_TARGET or #cmdParams ~= 4 then
 		return
 	end
+
 	local cmdX, cmdY, cmdZ, cmdRadius = unpack(cmdParams)
-
-	local mouseX, mouseY = Spring.WorldToScreenCoords(cmdX, cmdY, cmdZ)
-
 	local areaUnits = Spring.GetUnitsInCylinder(cmdX, cmdZ, cmdRadius)
 
 	local newCmds = {}
 	local newCmdOpts = 0
+	local somethingWasExcluded = false
+
 	for i = 1, #areaUnits do
 		local unitID = areaUnits[i]
 		local unitDefID = spGetUnitDefID(unitID)
@@ -43,10 +42,12 @@ function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
 				newCmdOpts = CMD.OPT_SHIFT
 			end
 			newCmds[#newCmds + 1] = {CMD_SET_TARGET, unitID, newCmdOpts}
+		else
+			somethingWasExcluded = true
 		end
 	end
 
-	if #newCmds > 0 then
+	if #newCmds > 0 and somethingWasExcluded then
 		Spring.GiveOrderArrayToUnitArray(Spring.GetSelectedUnits(), newCmds)
 		return true
 	end
