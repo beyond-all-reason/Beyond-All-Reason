4b3b2ea529b19b641b87bedf889ac8bf13c25e4d
SamWain <slousebox@gmail.com>
2025-04-15 13:25:11 +1000

    Allow set target of non neutral decoyed units
    Check if spGetUnitNeutral to allow targeting of units like armdrag when they undecoy to fire

--- luaui/Widgets/cmd_exclude_walls_area_set_target.lua
+++ luaui/Widgets/cmd_exclude_walls_area_set_target.lua
@@ -13,13 +13,26 @@ function widget:GetInfo()
 end
 
 local excludedUnitsDefID = {}
+local newCmds
+local somethingWasExcluded
+
 local CMD_SET_TARGET = 34923
+
 local spGetUnitDefID = Spring.GetUnitDefID
+local spGetUnitNeutral = Spring.GetUnitNeutral
 
 for id, unitDef in pairs(UnitDefs) do
 	if unitDef.customParams.objectify then
 		excludedUnitsDefID[id] = true
 	end
+end
+
+local function addNewCommand(unitID, cmdOpts)
+	local newCmdOpts = 0
+	if #newCmds ~= 0 or cmdOpts.shift then
+		newCmdOpts = CMD.OPT_SHIFT
+	end
+	newCmds[#newCmds + 1] = {CMD_SET_TARGET, unitID, newCmdOpts}
 end
 
 function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
@@ -30,20 +43,20 @@ function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
 	local cmdX, cmdY, cmdZ, cmdRadius = unpack(cmdParams)
 	local areaUnits = Spring.GetUnitsInCylinder(cmdX, cmdZ, cmdRadius)
 
-	local newCmds = {}
-	local newCmdOpts = 0
-	local somethingWasExcluded = false
-
+	newCmds = {}
+	somethingWasExcluded = false
 	for i = 1, #areaUnits do
 		local unitID = areaUnits[i]
 		local unitDefID = spGetUnitDefID(unitID)
+
 		if not excludedUnitsDefID[unitDefID] then
-			if #newCmds ~= 0 or cmdOpts.shift then
-				newCmdOpts = CMD.OPT_SHIFT
-			end
-			newCmds[#newCmds + 1] = {CMD_SET_TARGET, unitID, newCmdOpts}
+			addNewCommand(unitID, cmdOpts)
 		else
-			somethingWasExcluded = true
+			if not spGetUnitNeutral(unitID) then
+				addNewCommand(unitID, cmdOpts)
+			else
+				somethingWasExcluded = true
+			end
 		end
 	end
 
