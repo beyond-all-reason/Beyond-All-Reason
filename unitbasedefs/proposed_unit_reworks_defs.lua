19a390c4e1975cda97b46d46dd3ad61f35f90ba6
Mitvit <112669238+Mitvit@users.noreply.github.com>
2025-04-18 18:26:50 +0300

    Modoptions: Hide proposed unit reworks + Remove accurate lasers (#4752)
    - Hide proposed unit reworks modoption
    - Remove accurate lasers modoption
    - Empty proposed unit reworks file

--- unitbasedefs/proposed_unit_reworks_defs.lua
+++ unitbasedefs/proposed_unit_reworks_defs.lua
@@ -1,13 +1,5 @@
 local function proposed_unit_reworksTweaks(name, uDef)
 
-		if name == "armpw" then
-			uDef.metalcost = 60
-		end
-		if name == "corak" then
-			uDef.metalcost = 45
-			uDef.weapondefs.gator_laser.range = 220
-		end
-
 
 	return uDef
 end
