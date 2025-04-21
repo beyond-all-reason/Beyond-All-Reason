87f48f5216bf28dc0ddea8f246ca027378102ee7
Floris <floris@ruwhof.net>
2025-04-21 20:03:05 +0200

    rtt: font outline tweak

--- luaui/Widgets/camera_player_tv.lua
+++ luaui/Widgets/camera_player_tv.lua
@@ -787,7 +787,7 @@ function widget:ViewResize()
 	elementCorner = WG.FlowUI.elementCorner
 	RectRound = WG.FlowUI.Draw.RectRound
 
-	local outlineMult = math.max(1.2, 1/(vsy/1700))
+	local outlineMult = math.clamp(1/(vsy/1700), 1, 2)
 	font = WG['fonts'].getFont(nil, 1 * (useRenderToTexture and 1.6 or 1), 0.2 * (useRenderToTexture and outlineMult or 1), useRenderToTexture and 1.25+(outlineMult*0.25) or 1.25)
 	font2 = WG['fonts'].getFont(fontfile2, 2 * (useRenderToTexture and 1.6 or 1), 0.2 * (useRenderToTexture and 1.2*outlineMult or 1), 1.3+(outlineMult*0.25))
 
