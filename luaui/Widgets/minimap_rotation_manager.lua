01d2ca4d269b2c23ac4144b3d79efef3c8ab20ae
TheFutureKnight <23344114+TheFutureKnight@users.noreply.github.com>
2025-04-24 05:57:17 +0200

    Minimap Rotation Manager Widget

---
+++ luaui/Widgets/minimap_rotation_manager.lua
@@ -0,0 +1,97 @@
+function widget:GetInfo()
+	return {
+		name = "Minimap Rotation Manager",
+		desc = "Manages rotation states of the minimap.",
+		author = "TheFutureKnight",
+		date = "2025-24-4",
+		license = "GNU GPL, v2 or later",
+		layer = 0,
+		enabled = false,
+	}
+end
+
+local mode = 2 -- 1 = manual, 2 = auto flip, 3 = auto rotate
+local oldRotation
+local prevSnap
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local spSetMiniRot		= 	Spring.SetMiniMapRotation
+local spGetMiniRot		= 	Spring.GetMiniMapRotation
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local function manual_rotate(_,_,_, clock)
+	if mode ~= 1 then return end
+	local currentRadians = spGetMiniRot()
+	local currRotOption = math.floor((currentRadians / (math.pi / 2) + 0.5) % 4)
+	local newOption = (clock[1]) and ((currRotOption + 1) % 4) or ((currRotOption - 1) % 4)
+
+	spSetMiniRot(newOption * (math.pi / 2))
+end
+
+
+
+local function reloadBindings()
+	if mode ~= 1 then
+		widgetHandler:RemoveAction("rotate_clockwise")
+		widgetHandler:RemoveAction("rotate_counterclockwise")
+		return
+	else
+		widgetHandler:AddAction("rotate_clockwise", manual_rotate, {true}, "p")
+		widgetHandler:AddAction("rotate_counterclockwise", manual_rotate, {false}, "p")
+	end
+end
+
+
+function widget:Initialize()
+	mode = WG['options'].getOptionValue("minimaprotation") -- Sync up when the widget was unloaded
+	WG['rotationmanager'] = {}
+	WG['rotationmanager'].setMode = function(newMode)
+		mode = newMode
+		reloadBindings()
+	end
+	WG['rotationmanager'].getMode = function()
+		return mode
+	end
+	oldRotation = Spring.GetConfigInt("MiniMapCanFlip", 0) -- Store engine legacy behavior
+	Spring.SetConfigInt("MiniMapCanFlip", 0)
+end
+
+function widget:Shutdown()
+	WG['rotationmanager'] = nil
+	Spring.SetConfigInt("MiniMapCanFlip", oldRotation)
+end
+
+function widget:CameraRotationChanged(_, roty)
+	Spring.Echo(mode)
+	local newRot
+	if mode == 1 then return
+	elseif mode == 2 then
+		newRot = math.pi * math.floor((roty/math.pi) + 0.5)
+	elseif mode == 3 then
+		newRot = math.pi/2 * (math.floor((roty/(math.pi/2)) + 0.5) % 4)
+	end
+	Spring.Echo(newRot, prevSnap)
+	if newRot ~= prevSnap then
+		prevSnap = newRot
+		spSetMiniRot(newRot)
+	end
+end
+
+
+function widget:GetConfigData()
+	return {
+		mode = mode
+	}
+end
+
+
+function widget:SetConfigData(data)
+	if data.mode ~= nil then
+		mode = data.mode
+	end
+end
