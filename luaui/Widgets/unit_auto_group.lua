c8262cd4f42f5cf8c9371f8c546bc6b8cd79d39e
MasterBel2 <untakenprefix@gmail.com>
2025-05-09 22:50:25 +1000

    Publish Autogroup API (#4758)
    Allows programmatic access to operations previously only accessible through keybinds.

--- luaui/Widgets/unit_auto_group.lua
+++ luaui/Widgets/unit_auto_group.lua
@@ -118,6 +118,10 @@ local function ChangeUnitTypeAutogroupHandler(_, _, args, data)
 	local gr = args[1]
 	local removeAll = data and data['removeAll']
 
+	ChangeUnitTypeAutogroupHandler(gr, removeAll)
+end
+
+local function ChangeUnitTypeAutogroup(gr, removeAll)
 	if not removeAll and not gr then return end -- noop if add to autogroup and no argument
 
 	if removeAll then
@@ -200,8 +204,7 @@ local function RemoveOneUnitFromGroupHandler(_, _, args)
 	return true
 end
 
-local function loadAutogroupPresetHandler(cmd, optLine, optWords, data, isRepeat, release, actions)
-	local newPreset = tonumber(optWords[1])
+local function LoadAutogroupPreset(newPreset)
 	if not presets[newPreset] then
 		return
 	end
@@ -229,6 +232,10 @@ local function loadAutogroupPresetHandler(cmd, optLine, optWords, data, isRepeat
 	end
 end
 
+local function LoadAutogroupPresetHandler(cmd, optLine, optWords, data, isRepeat, release, actions)
+	LoadAutogroupPreset(tonumber(optWords[1]))
+end
+
 
 function widget:Initialize()
 
@@ -237,7 +244,7 @@ function widget:Initialize()
 	widgetHandler:AddAction("add_to_autogroup", ChangeUnitTypeAutogroupHandler, nil, "p") -- With a parameter, adds all units of this type to a specific autogroup
 	widgetHandler:AddAction("remove_from_autogroup", ChangeUnitTypeAutogroupHandler, { removeAll = true }, "p") -- Without a parameter, removes all units of this type from autogroups
 	widgetHandler:AddAction("remove_one_unit_from_group", RemoveOneUnitFromGroupHandler, nil, "p") -- Removes the closest of selected units from groups and selects only it
-	widgetHandler:AddAction("load_autogroup_preset", loadAutogroupPresetHandler, nil, "p") -- Changes the autogroup preset
+	widgetHandler:AddAction("load_autogroup_preset", LoadAutogroupPresetHandlerAction, nil, "p") -- Changes the autogroup preset
 
 	WG['autogroup'] = {}
 	WG['autogroup'].getImmediate = function()
@@ -255,6 +262,18 @@ function widget:Initialize()
 	end
 	WG['autogroup'].getGroups = function()
 		return unit2group
+	end
+	WG['autogroup'].addCurrentSelectionToAutogroup = function(groupNumber)
+		ChangeUnitTypeAutogroup(groupNumber)
+	end
+	WG['autogroup'].removeCurrentSelectionFromAutogroup = function()
+		ChangeUnitTypeAutogroup(nil, true)
+	end
+	WG['autogroup'].removeOneUnitFromGroup = function()
+		RemoveOneUnitFromGroupHandler()
+	end
+	WG['autogroup'].loadAutogroupPreset = function(newPreset)
+		LoadAutogroupPreset(newPreset)
 	end
 	if GetGameFrame() > 0 then
 		addAllUnits()
