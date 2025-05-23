229d1d50e124bb5a85d14579b04d254cdecb8dea
Saurtron <saurtron@riseup.net>
2025-05-23 10:20:06 +0200

    Backwards compatibility deprecated includes for LuaShader.
    - Also delete extra copy since it's an old one but should be compatible.

--- luaui/Include/LuaShader.lua
+++ luaui/Include/LuaShader.lua
@@ -1,6 +1,6 @@
 
 if not LuaShaderNewLocationMessageShown then
-	local msg = "A user widget is including LuaRules/Gadgets/Include/LuaShader.lua directly, please change it to use gl.LuaShader instead."
+	local msg = "A user widget is including LuaUI/Include/LuaShader.lua directly, please change it to use gl.LuaShader instead."
 	Spring.Log('UserWidget', LOG.DEPRECATED, msg)
 	LuaShaderNewLocationMessageShown = true
 end
