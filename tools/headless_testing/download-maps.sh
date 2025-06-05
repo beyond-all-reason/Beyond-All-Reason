aaef682c48102723c18c5e490ff4b8b77108b899
Saurtron <saurtron@riseup.net>
2025-06-05 21:00:11 +0200

    Update to Supreme Isthmus v2.1.

--- tools/headless_testing/download-maps.sh
+++ tools/headless_testing/download-maps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-engine/*/pr-downloader --filesystem-writepath "$BAR_ROOT" --download-map "Supreme Isthmus v1.8"
+engine/*/pr-downloader --filesystem-writepath "$BAR_ROOT" --download-map "Supreme Isthmus v2.1"
