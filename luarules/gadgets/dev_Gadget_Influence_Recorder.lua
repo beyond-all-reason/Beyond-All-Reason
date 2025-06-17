11b75ff5d1ef5aa600e7b56eff2d189e581a9ad2
DrChinny <guy1dawson@hotmail.com>
2025-06-17 21:11:27 +0100

    commit working version on 170625

--- luarules/gadgets/dev_Gadget_Influence_Recorder.lua
+++ luarules/gadgets/dev_Gadget_Influence_Recorder.lua
@@ -13,14 +13,14 @@ function gadget:GetInfo()
 end
 
 --todo
---check transfer units / capture working as it should
---add transport logic for moving static
---add antispam unit logic when mobile units > 1000?
---add allowing list at end game
---interesting deaths?
---remove all hardcoded units if possible
+--check transfer units / capture working as it should.
+--add transport logic for moving static.
+--add antispam unit logic when mobile units > 1000? done
+--add allowing list at end game. -> done
+--interesting deaths? -> no, can be done in widget.
+--remove all hardcoded units if possible. -> done
 -- I think I can use .techlevel (which seems only exists for non T1 units as far as I can see) AND .speed>0) for the first list, but don't see anything convenient for the second list (maybe sightdistance ==1, mine ==true)
-
+-- improve names eg masterNewUnitListMobile -> masterNewMobileUnitList.
 
 if gadgetHandler:IsSyncedCode() then
     return
@@ -48,6 +48,8 @@ else
     --     [UnitDefNames["cordrag"].id]    = true, ---xxx need a unitdef to autopopulate
     --     [UnitDefNames["armdrag"].id]    = true
     -- }
+    local spamDisabled = false
+    local debugTable = {}
     local spamUnits = {}
     local excludeUnits  = {}
 
@@ -62,14 +64,14 @@ else
             excludeUnits[unitDefID] = true
         end
 
-        if not unitDef.customParams.techlevel then --tech 1
+        if unitDef.customParams.techlevel == "1" then --tech 1
             if unitDef.speed > 0 then --mobile
                 spamUnits[unitDefID] = true
             end
         end     
     end
 
-    local spamDisabled = false
+    
     -- local spamUnits = { --Units that are T1 spam, late game can set to be ignored to save on performance. xxx need a unitdef to autopopulate
     --     [UnitDefNames["armflea"].id]    =true,
     --     [UnitDefNames["armpw"].id]      =true,
@@ -101,7 +103,7 @@ else
         local skippable = false
         if allyTeamID == nil then
             skippable = true
-            Spring.Echo("Error 001; no AllyTeamID", udID,allyTeamID)
+            Spring.Echo("Error 001; no AllyTeamID", udID,allyTeamID) --xxx remove 
         elseif allyTeamID == gaiaAllyTeamID then --gaia
             skippable = true
         elseif excludeUnits[udID] then --excluded
@@ -116,20 +118,29 @@ else
 
     local function UpdateNewMobileUnitListPosition() --call in just before changing to next replay frame
         for unitID,_ in pairs(masterNewUnitListMobile[replayFrame]) do
+            if spamDisabled then
+                if spamUnits[unitDefID] then --ignore spam units when option applied
+                    break
+                end
+            end
             local posX,_,posZ = Spring.GetUnitPosition(unitID)
-            masterNewUnitListMobile[replayFrame][unitID][3] = posX
-            masterNewUnitListMobile[replayFrame][unitID][4] = posZ
-            masterExistingUnitListMobile[replayFrame][unitID] = {posX, posZ} --only added if unit has moved.
-            mobileTrackingList[unitID] = {posX, posZ}
+            if posX then --if unit died within the last replayframe this will be nil, so don't need to add
+                masterNewUnitListMobile[replayFrame][unitID][3] = posX
+                masterNewUnitListMobile[replayFrame][unitID][4] = posZ
+                masterExistingUnitListMobile[replayFrame][unitID] = {posX, posZ}
+                mobileTrackingList[unitID] = {posX, posZ}
+                mobileTrackingCount = mobileTrackingCount + 1
+            end
         end
     end
 
     local function UpdateExistingMobileUnitLists() --call in just before changing to next replay frame. checks all living mobile units, adds to masterlist only if moved. xxx should I ceiling this?
         local posX,_,posZ
         for unitID, posData in pairs(mobileTrackingList) do
-            if not masterNewUnitListMobile[replayFrame][unitID] then --ignores new mobile units as these are already added in previous function
-                posX,_,posZ = Spring.GetUnitPosition(unitID)
-                if posX ~= posData[1] or posZ ~= posData[3] then
+            posX,_,posZ = Spring.GetUnitPosition(unitID)
+            if posX ~= posData[1] or posZ ~= posData[2] then
+                if not posX then--edge case when update() occurs on same gameframe that a unit dies 
+                else 
                     masterExistingUnitListMobile[replayFrame][unitID] = {posX, posZ} --only added if unit has moved.
                     mobileTrackingList[unitID] = {posX, posZ}
                 end
@@ -144,12 +155,19 @@ else
     end
 
     function gadget:UnitFinished(unitID, unitDefID, teamID)
-        if not CheckForSkippables(teamAllyTeamIDCache[teamID],unitDefID, true) then
+        if not CheckForSkippables(teamAllyTeamIDCache[teamID],unitDefID, false) then
             local unitDef = UnitDefs[unitDefID]
             if unitDef.speed >0 then
                 masterNewUnitListMobile[replayFrame][unitID] = {unitDefID, teamID,"X","Z"} --xxx can update to nils. note the unit position at time of creation may not match when the replay frame is recorded, so I must go through this list and update positions.
-                mobileTrackingList[unitID] = {"X","Z"}
-                mobileTrackingCount = mobileTrackingCount + 1
+                -- if not spamDisabled then
+                --     mobileTrackingList[unitID] = {"X","Z"}
+                --     mobileTrackingCount = mobileTrackingCount + 1
+                -- else
+                --     if not spamUnits[unitDefID] then
+                --         mobileTrackingList[unitID] = {"X","Z"}
+                --         mobileTrackingCount = mobileTrackingCount + 1
+                --     end
+                -- end
             else
                 local posX,_,posZ = Spring.GetUnitPosition(unitID)
                 masterNewUnitListStatic[replayFrame][unitID] = {unitDefID, teamID,posX,posZ}
@@ -158,38 +176,34 @@ else
     end
 
     function gadget:UnitDestroyed (unitID, unitDefID, teamID, attUnitID, attUnitDefID, attTeamID)
-        if not CheckForSkippables(teamAllyTeamIDCache[teamID],unitDefID, true) then
+        if not CheckForSkippables(teamAllyTeamIDCache[teamID],unitDefID, false) then
             masterDeadUnitListAll[replayFrame][unitID] = {unitDefID, teamID, attUnitDefID, attTeamID} --xxx this will add spam units to the dead list even if antispam enabled
             if mobileTrackingList[unitID] then
                 mobileTrackingList[unitID] = nil
                 mobileTrackingCount = mobileTrackingCount - 1
             end
-        elseif spamDisabled then -- when spam units are still on map we still need to remove them from lists once they die
-            if spamUnits[unitDefID] then
-                if mobileTrackingList[unitID] then
-                    masterDeadUnitListAll[replayFrame][unitID] = {unitDefID, teamID, attUnitDefID, attTeamID}
-                    mobileTrackingList[unitID] = nil
-                    mobileTrackingCount = mobileTrackingCount - 1
-                end
-            end
         end
     end
 
-    function gadget:UnitTaken(unitID, unitDefID, oldTeamID, newTeamID)
+    function gadget:UnitTaken(unitID, unitDefID, oldTeamID, newTeamID) --xxx what about incomplete units?
         if not CheckForSkippables(teamAllyTeamIDCache[oldTeamID],unitDefID, true) then
-            masterTransferedUnitListAll[replayFrame][unitID] = {oldTeamID,newTeamID}
+            if select(5,Spring.GetUnitHealth(unitID)) == 1 then
+                masterTransferedUnitListAll[replayFrame][unitID] = {oldTeamID,newTeamID}
+            end
         end
     end
     
     local function gbug() --xxx remove, just for debugging
         Spring.Echo("Gadget Bug Excluded:")
         for i, _ in pairs(excludeUnits) do
-            Spring.Echo(UnitDefs[i].name)
+            Spring.Echo(UnitDefs[i].translatedHumanName, UnitDefs[i].name)
         end
         Spring.Echo("Gadget Bug spamUnits:")
         for i, _ in pairs(spamUnits) do
-            Spring.Echo(UnitDefs[i].name)
+            Spring.Echo(UnitDefs[i].translatedHumanName, UnitDefs[i].name)
         end
+        Spring.Echo("Gadget Bug true checker:", Script.LuaUI("Influence"),not gameEnded)
+        Spring.Echo("debugTable",debugTable)
     end
 
     function gadget:Initialize()
@@ -199,27 +213,34 @@ else
         PrimeNextReplayFrame()
     end
 
+    
+
     function gadget:GameFrame(gf)
         if gf % replayFrameLength == 0 then --record every 10 seconds
-            if Script.LuaUI("Influence") and not gameEnded then --record only if companion widget is running
-                UpdateNewMobileUnitListPosition()
+            if Script.LuaUI("Influence") and not gameEnded then --record only if companion widget is running loaded
                 UpdateExistingMobileUnitLists()
+                UpdateNewMobileUnitListPosition()
+                debugTable[replayFrame] = gf
                 replayFrame = replayFrame + 1
                 PrimeNextReplayFrame()
+                Spring.Echo("mobileTrackingCount = ", gf ,replayFrame, mobileTrackingCount)
                 if mobileTrackingCount > 1000 then --ignore T1 spam if tracking too many mobile units, reenable if lower threshhold is met
                     spamDisabled = true
                 elseif mobileTrackingCount < 500 then
                     spamDisabled = false
                 end
-                if replayFrame % frameLengthModifierThreshold then --every [120] frames we will double the interval that a snapshot is taken, and half the next interval this is run. This will make stop the replay list getting too large in v long games.
+                if replayFrame % frameLengthModifierThreshold == 0 then --every [120] frames we will double the interval that a snapshot is taken, and half the next interval this is run. This will make stop the replay list getting too large in v long games.
                     replayFrameLength = replayFrameLength * 2
                     frameLengthModifierThreshold = math.max(frameLengthModifierThreshold / 2,15)
+                    Spring.Echo("frameLengthModifierThreshold = ",replayFrame, frameLengthModifierThreshold)
                 end
             end
         end
     end
 
     function gadget:GameOver()
+        Spring.Echo("Game Ended IR") --xxx remove
+        Spring.Echo(debugTable) --xxx remove
         MakeListsAvalibleToWidgets()
         gameEnded = true
     end
