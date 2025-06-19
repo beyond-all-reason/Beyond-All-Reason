f394311201e4dd934297220ac5b300de4200d48b
roberthartmann <roberthartmann@users.noreply.github.com>
2025-06-19 21:30:28 +0200

    Make combined builders show as a single idle builder through new customParam virtualunit (#5278)
    Sometimes units are composited of several 'subunits'. Right now those will show up as different idle builders at the idle builders widget.
    
    Noticed that at the "Printer" unit, so fixing it by using the new dontshowidle customParam introduced here

--- luaui/Widgets/gui_unit_idlebuilder_icons.lua
+++ luaui/Widgets/gui_unit_idlebuilder_icons.lua
@@ -34,10 +34,13 @@ local spGetUnitIsBeingBuilt = Spring.GetUnitIsBeingBuilt
 
 local unitConf = {}
 for unitDefID, unitDef in pairs(UnitDefs) do
-	if unitDef.buildSpeed > 0 and not string.find(unitDef.name, 'spy') and (unitDef.canAssist or unitDef.buildOptions[1]) and not unitDef.customParams.isairbase then
-		local xsize, zsize = unitDef.xsize, unitDef.zsize
-		local scale = 3.3 * ( (xsize+2)^2 + (zsize+2)^2 )^0.5
-		unitConf[unitDefID] = {7.5 +(scale/2.2), unitDef.height-0.1, unitDef.isFactory}
+	local cp = unitDef.customParams
+	if not (cp.virtualunit == "1") then
+		if unitDef.buildSpeed > 0 and not string.find(unitDef.name, 'spy') and (unitDef.canAssist or unitDef.buildOptions[1]) and not unitDef.customParams.isairbase then
+			local xsize, zsize = unitDef.xsize, unitDef.zsize
+			local scale = 3.3 * ( (xsize+2)^2 + (zsize+2)^2 )^0.5
+			unitConf[unitDefID] = {7.5 +(scale/2.2), unitDef.height-0.1, unitDef.isFactory}
+		end
 	end
 end
 
