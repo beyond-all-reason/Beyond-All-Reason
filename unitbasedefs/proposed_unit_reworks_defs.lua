081cfc2ee48f3732d8e13a9804b368b77540186c
Mitvit <112669238+Mitvit@users.noreply.github.com>
2025-06-20 17:16:15 +0300

    corspy e/bt cost down

--- unitbasedefs/proposed_unit_reworks_defs.lua
+++ unitbasedefs/proposed_unit_reworks_defs.lua
@@ -124,56 +124,52 @@ local function proposed_unit_reworksTweaks(name, uDef)
 	if name == "armlatnk" then
 		uDef.weapondefs.lightning.damage.default = 20
 	end
+	if name == "corspy" then
+		uDef.buildtime = 15000
+		uDef.energycost = 10000
+	end
 
-
-
-
-
-
-
-
-
-		if name == "armspy" then
-		uDef.script = "Units/ARMSPY2.cob"
-		uDef.selfdestructas = "smallExplosionGeneric"
-		uDef.weapondefs.crawl_dummy = {
-				areaofeffect = 32,
-				avoidfeature = false,
-				beamdecay = 0.5,
-				beamtime = 1,
-				beamttl = 1,
-				collidefriendly = false,
-				corethickness = 0.4,
-				craterareaofeffect = 0,
-				craterboost = 0,
-				cratermult = 0,
-				duration = 0.01,
-				edgeeffectiveness = 0.15,
-				energypershot = 500,
-				explosiongenerator = "custom:laserhit-emp",
-				impulsefactor = 0,
-				laserflaresize = 6.05,
-				name = "Heavy EMP beam",
-				noselfdamage = true,
-				paralyzer = true,
-				paralyzetime = 10,
-				range = 210,
-				reloadtime = 20,
-				rgbcolor = "0.7 0.7 1",
-				soundhitdry = "",
-				soundhitwet = "sizzle",
-				soundstart = "hackshotxl3",
-				soundtrigger = 1,
-				thickness = 3.5,
-				tolerance = 6000,
-				turret = false,
-				weapontype = "BeamLaser",
-				weaponvelocity = 1000,
-				damage = {
-					default = 18000,
-				},
-			}
-		end
+	if name == "armspy" then
+	uDef.script = "Units/ARMSPY2.cob"
+	uDef.selfdestructas = "smallExplosionGeneric"
+	uDef.weapondefs.crawl_dummy = {
+		areaofeffect = 32,
+		avoidfeature = false,
+		beamdecay = 0.5,
+		beamtime = 1,
+		beamttl = 1,
+		collidefriendly = false,
+		corethickness = 0.4,
+		craterareaofeffect = 0,
+		craterboost = 0,
+		cratermult = 0,
+		duration = 0.01,
+		edgeeffectiveness = 0.15,
+		energypershot = 500,
+		explosiongenerator = "custom:laserhit-emp",
+		impulsefactor = 0,
+		laserflaresize = 6.05,
+		name = "Heavy EMP beam",
+		noselfdamage = true,
+		paralyzer = true,
+		paralyzetime = 10,
+		range = 210,
+		reloadtime = 20,
+		rgbcolor = "0.7 0.7 1",
+		soundhitdry = "",
+		soundhitwet = "sizzle",
+		soundstart = "hackshotxl3",
+		soundtrigger = 1,
+		thickness = 3.5,
+		tolerance = 6000,
+		turret = false,
+		weapontype = "BeamLaser",
+		weaponvelocity = 1000,
+		damage = {
+			default = 18000,
+		},
+		}
+	end
 
 	return uDef
 end
