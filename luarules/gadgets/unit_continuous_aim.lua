fc8bf082a22ab850192b18f7ab92a2e9b500a317
Zecrus2 <72325649+Zecrus2@users.noreply.github.com>
2025-06-21 20:12:57 -0400

    Increased speed and turret rotation speed of legion T3 hover

--- luarules/gadgets/unit_continuous_aim.lua
+++ luarules/gadgets/unit_continuous_aim.lua
@@ -134,6 +134,7 @@ local convertedUnitsNames = {
 	['legrwall'] = 4,
 	['legjav'] = 1,
 	['legeshotgunmech'] = 3,
+	['legehovertank'] = 4,
 }
 --add entries for scavboss
 local scavengerBossV4Table = {'scavengerbossv4_veryeasy', 'scavengerbossv4_easy', 'scavengerbossv4_normal', 'scavengerbossv4_hard', 'scavengerbossv4_veryhard', 'scavengerbossv4_epic',
