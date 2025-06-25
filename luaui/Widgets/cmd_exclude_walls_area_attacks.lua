17251c13b568774bd608c2d68839de69a4edd56f
Saurtron <saurtron@riseup.net>
2025-06-25 15:48:57 +0200

    Allow targeting walls if they're the only targets of the attack.

--- luaui/Widgets/cmd_exclude_walls_area_attacks.lua
+++ luaui/Widgets/cmd_exclude_walls_area_attacks.lua
@@ -2,8 +2,8 @@ local widget = widget ---@type Widget
 
 function widget:GetInfo()
 	return {
-		name 		= "Exclude walls from area set target",
-		desc 		= "Stops walls from being included in area set target if units are present",
+		name 		= "Exclude walls from area attacks",
+		desc 		= "Stops walls from being included in area attacks if units are present",
 		date		= "April 2025",
 		author		= "Slouse",
 		license 	= "GNU GPL, v2 or later",
@@ -14,6 +14,8 @@ end
 
 local CMD_UNIT_CANCEL_TARGET = GameCMD.UNIT_CANCEL_TARGET
 local CMD_UNIT_SET_TARGET = GameCMD.UNIT_SET_TARGET
+local CMD_ATTACK = CMD.ATTACK
+local CMD_STOP = CMD.STOP
 
 local excludedUnitsDefID = {}
 
@@ -26,17 +28,18 @@ for id, unitDef in pairs(UnitDefs) do
 	end
 end
 
-local function addNewCommand(newCmds, unitID, cmdOpts)
+local function addNewCommand(newCmds, unitID, cmdOpts, cmdID)
 	if #newCmds == 0 and not cmdOpts.shift then
 		-- Need to clear orders if not in shift, since just sending the first one
 		-- as not-shift would sometimes fail if that unit is in the end not valid
-		newCmds[1] = {CMD_UNIT_CANCEL_TARGET, {}, {}}
+		local stopCmd = (cmdID == CMD_ATTACK) and CMD_STOP or CMD_UNIT_CANCEL_TARGET
+		newCmds[1] = {stopCmd, {}, {}}
 	end
-	newCmds[#newCmds + 1] = {CMD_UNIT_SET_TARGET, unitID, CMD.OPT_SHIFT}
+	newCmds[#newCmds + 1] = {cmdID, unitID, CMD.OPT_SHIFT}
 end
 
 function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
-	if cmdID ~= CMD_UNIT_SET_TARGET or #cmdParams ~= 4 then
+	if (cmdID ~= CMD_UNIT_SET_TARGET and cmdID ~= CMD_ATTACK) or #cmdParams ~= 4 then
 		return
 	end
 
@@ -50,14 +53,13 @@ function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
 		local unitDefID = spGetUnitDefID(unitID)
 
 		if not excludedUnitsDefID[unitDefID] then
-			addNewCommand(newCmds, unitID, cmdOpts)
+			addNewCommand(newCmds, unitID, cmdOpts, cmdID, (cmdID == CMD_ATTACK) and CMD_STOP or CMD_UNIT_CANCEL_TARGET)
 		elseif not spGetUnitNeutral(unitID) then	
-			addNewCommand(newCmds, unitID, cmdOpts)
+			addNewCommand(newCmds, unitID, cmdOpts, cmdID, (cmdID == CMD_ATTACK) and CMD_STOP or CMD_UNIT_CANCEL_TARGET)
 		else
 			somethingWasExcluded = true
 		end
 	end
-
 	if #newCmds > 0 and somethingWasExcluded then
 		Spring.GiveOrderArrayToUnitArray(Spring.GetSelectedUnits(), newCmds)
 		return true
