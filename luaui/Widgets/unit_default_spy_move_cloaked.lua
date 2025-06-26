beyond-all-reason/Beyond-All-Reason

commit d5bacbb9a038c9de3bdaa27085da7db840f8025f

Legion June 2025 Update 3 (#5357)
### Work Done
- A new all-terrain drone carrier (thanks to @DMikey86,
@JohnathanCrimson, and @Protar1458 for concept work)
- A new hovertank (model by @ShadowsAlT, concept art by
@JohnathanCrimson and @Protar1458)
- A new spybot (concept art by @Phill-Art)
- Remastered Legion Icons for Prometheus and Olympus. New icon for
Stronghold transport gunship.

### Work Needed
- [ ] Balance pass from @Zecrus2 

### Images
T3 All-Terrain Drone Carrier Mech

![image](https://github.com/user-attachments/assets/047ae961-9920-4e08-9e0f-abd7051a810f)
T3 Hovertank

![{6FDDB321-CBBE-4850-A9BF-111D28BB6AF9}](https://github.com/user-attachments/assets/0d063ad2-38c5-4aa2-961f-a5ac5ee44b04)
T2 Spybot

![{14B9099D-CCC6-4C11-B9A6-91723F72323C}](https://github.com/user-attachments/assets/e603bf74-e895-47f6-a35e-e4be36ae3fd7)

---------

Co-authored-by: DMikey86 <186913519+DMikey86@users.noreply.github.com>
Co-authored-by: Johnathan Crimson <179456402+JohnathanCrimson@users.noreply.github.com>
Co-authored-by: Zecrus2 <72325649+Zecrus2@users.noreply.github.com>
Co-authored-by: Phill-Art <202047222+Phill-Art@users.noreply.github.com>
Co-authored-by: ShadowsAlT <192256514+ShadowsAlT@users.noreply.github.com>

luaui/Widgets/unit_default_spy_move_cloaked.lua
@@ -15,6 +15,7 @@ end
 local spies  = {
 	[UnitDefNames.armspy.id] = true,
 	[UnitDefNames.corspy.id] = true,
+	[UnitDefNames.legaspy.id] = true,
 }
 
 local GetSelectedUnitsSorted = Spring.GetSelectedUnitsSorted

gamedata/armordefs.lua
@@ -305,6 +305,7 @@ local armorDefs = {
         "armspy",
 		"cormando",
 		"corspy",
+		"legaspy",
         "armdl",
 		"cordl",
 		"legctl",
@@ -543,6 +544,7 @@ local armorDefs = {
 		"cordroneold",
 		"legdrone",
 		"legheavydrone",
+		"legheavydronesmall",
 		"raptor_air_bomber_basic_t1_v1",
 		"raptor_air_bomber_basic_t2_v1",
 		"raptor_air_bomber_basic_t2_v2",

gamedata/icontypes.lua
@@ -2019,6 +2019,10 @@ local icontypes = {
 		bitmap = "icons/corsok_1.8.png",
 		size = 1.88999987
 	},
+	legehovertank = {
+		bitmap = "icons/corsok_1.8.png",
+		size = 1.88999987
+	},
 	corsolar = {
 		bitmap = "icons/energy1_1.0.png",
 		size = 1.04999995
@@ -2032,6 +2036,10 @@ local icontypes = {
 		size = 1.34399986
 	},
 	corspy = {
+		bitmap = "icons/spy.png",
+		size = 1.17999995
+	},
+	legaspy = {
 		bitmap = "icons/spy.png",
 		size = 1.17999995
 	},
@@ -2519,6 +2527,10 @@ local icontypes = {
 		bitmap = "icons/heavyheatraydrone.png",
 		size = 0.9
 	},
+	legheavydronesmall = {
+		bitmap = "icons/heavyheatraydrone.png",
+		size = 0.9
+	},
 	legfhp = {
 		bitmap = "icons/factory_hover.png",
 		size = 1.52250004
@@ -2832,7 +2844,7 @@ local icontypes = {
 		size = 1.29999995
 	},
 	legstronghold = {
-		bitmap = "icons/air_trans.png",
+		bitmap = "icons/air_t2_transport_shotgun.png",
 		size = 1.83749986
 	},
 	legjim = {
@@ -2870,6 +2882,10 @@ local icontypes = {
 	legvcarry = {
 		bitmap = "icons/vehicle_t2_dronecarryland.png",
 		size = 1.64999998
+	},
+	legeallterrainmech = {
+		bitmap = "icons/t3_allterrain_drone_cluster_bot.png",
+		size = 2.61999998
 	},
 	legvelite = {
 		bitmap = "icons/ship_raid.png",

commit 658d9b8348564dc5ed6fec4c7e4d6f96b979b8c5

Fix default-spy-move-cloaked when legion not enabled (#5412)
### Work done

- Be more careful about accessing unitdef.id without testing for their
existance before.

#### Addresses Issue(s)
- `[t=00:00:14.231212][f=-000001] Failed to load:
unit_default_spy_move_cloaked.lua ([string
"LuaUI/Widgets/unit_default_spy_move_cloaked.lua"]:18: attempt to index
field 'legaspy' (a nil value))
`

### Remarks

- broken with d5bacbb9a038c9de3bdaa27085da7db840f8025f

#### Test steps
- Start skirmish
- Check infolog.txt for the above error.

luaui/Widgets/unit_default_spy_move_cloaked.lua
@@ -12,11 +12,19 @@ function widget:GetInfo()
 	}
 end
 
-local spies  = {
-	[UnitDefNames.armspy.id] = true,
-	[UnitDefNames.corspy.id] = true,
-	[UnitDefNames.legaspy.id] = true,
+local spies  = {}
+
+local spyNames = {
+	'armspy',
+	'corspy',
+	'legaspy',
 }
+
+for _, spyName in ipairs(spyNames) do
+	if UnitDefNames[spyName] then
+		spies[UnitDefNames[spyName].id] = true
+	end
+end
 
 local GetSelectedUnitsSorted = Spring.GetSelectedUnitsSorted
 local GetUnitStates = Spring.GetUnitStates
@@ -42,6 +50,10 @@ function widget:PlayerChanged(playerID)
 end
 
 function widget:Initialize()
+    if #spies == 0 then
+	    widgetHandler:RemoveWidget()
+	    return
+    end
     if Spring.IsReplay() or Spring.GetGameFrame() > 0 then
         maybeRemoveSelf()
     end