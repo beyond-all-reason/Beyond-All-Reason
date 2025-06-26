6db9cb29d6072a5024d6e7c96ea886a57ac5e9a3
Zephyr Skies <135075008+ZephyrSkies7@users.noreply.github.com>
2025-06-26 14:09:12 -0400

    Add custom parameter to Legion Triton, allow it to be used on land (#5334)
    ### Work Done
    Adds a new custom parameter to the Legion Triton: `convertibleboat`, a
    true/false statement that aptly describes if something is a convertible
    boat or not.
    
    Adjusts unit_buildmenu_config to ensure that units with this custom
    parameter as true will not be counted as a sea unit, and therefore
    excluded from when sea units are disabled on land-only maps. This makes
    the Triton usable on land again whilst keeping its ability to be treated
    as a regular boat and targeted by torpedoes and depth-charges whilst in
    the water.
    
    ### Testing Required
    - [ ] Test on various land-only and sea maps to ensure all sea units are
    accessible/inaccessible whilst Triton is accessible all the time.
    
    ### Team and Lead
    - Technical Interface Team, @Ruwetuin 
    - Code Quality Team, @WatchTheFort 
    
    Thanks to @efrec for the suggestion!

--- units/Legion/Vehicles/T2 Vehicles/legfloat.lua
+++ units/Legion/Vehicles/T2 Vehicles/legfloat.lua
@@ -46,6 +46,7 @@ return {
 			normaltex = "unittextures/leg_normal.dds",
 			subfolder = "legvehicles/T2",
 			techlevel = 2,
+			enabled_on_no_sea_maps = true,
 		},
 		featuredefs = {
 			dead = {
