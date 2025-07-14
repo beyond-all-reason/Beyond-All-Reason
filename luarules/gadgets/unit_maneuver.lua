1a879b434be18c781c987fb3eef28c6611b22020
Malcolm Mitchell <mvlcolmm@gmail.com>
2025-07-14 20:06:48 +1000

    implemented efrec formatting suggestions

--- luarules/gadgets/unit_maneuver.lua
+++ luarules/gadgets/unit_maneuver.lua
@@ -14,7 +14,7 @@ function gadget:GetInfo()
 end
 
 if not gadgetHandler:IsSyncedCode() then 
-    return 
+	return 
 end
 
 local CMD_ATTACK = CMD.ATTACK
@@ -43,125 +43,135 @@ local recentlyAttacked = {}
 local nonAirUnits = {}
 
 function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions)
-        if (cmdID == CMD_MOVE or cmdID == CMD_FIGHT) and cmdParams[1] and cmdParams[2] and cmdParams[3] then
-            lastMovePosition[unitID] = {
-                x = cmdParams[1],
-                y = cmdParams[2],
-                z = cmdParams[3]
-            }
-        end
-        
-        if cmdID == CMD_STOP then
-            local x, y, z = spGetUnitPosition(unitID)
-            if x and y and z then
-                lastMovePosition[unitID] = { 
-                    x = x, 
-                    y = y, 
-                    z = z 
-                }
-            end
-        end
-    return true
+		if (cmdID == CMD_MOVE or cmdID == CMD_FIGHT) and cmdParams[1] and cmdParams[2] and cmdParams[3] then
+			lastMovePosition[unitID] = {
+				x = cmdParams[1],
+				y = cmdParams[2],
+				z = cmdParams[3]
+			}
+		end
+		
+		if cmdID == CMD_STOP then
+			local x, y, z = spGetUnitPosition(unitID)
+			
+			if x and y and z then
+				lastMovePosition[unitID] = { 
+					x = x, 
+					y = y, 
+					z = z 
+				}
+			end
+		end
+
+	return true
 end
 
 function gadget:UnitCreated(unitID, unitDefID, unitTeam)
-    local def = UnitDefs[unitDefID]
-    if def and def.sightDistance and not def.canFly then
-        unitSightDistances[unitID] = def.sightDistance
-        nonAirUnits[unitID] = true
-    end
+	local def = UnitDefs[unitDefID]
 
-    if not lastMovePosition[unitID] then
-        local x, y, z = spGetUnitPosition(unitID)
-        if x and y and z then
-            lastMovePosition[unitID] = { 
-                x = x, 
-                y = y, 
-                z = z 
-            }
-        end
-    end
+	if def and def.sightDistance and not def.canFly then
+		unitSightDistances[unitID] = def.sightDistance
+		nonAirUnits[unitID] = true
+	end
+
+	if not lastMovePosition[unitID] then
+		local x, y, z = spGetUnitPosition(unitID)
+		
+		if x and y and z then
+			lastMovePosition[unitID] = { 
+				x = x, 
+				y = y, 
+				z = z 
+			}
+		end
+	end
 end
 
 function gadget:UnitDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponID, attackerID, attackerDefID, attackerTeam)
-    if attackerID and attackerTeam ~= unitTeam then
-        recentlyAttacked[unitID] = spGetGameFrame
-    end
+	if attackerID and attackerTeam ~= unitTeam then
+		recentlyAttacked[unitID] = spGetGameFrame
+	end
 end
 
 function gadget:UnitDestroyed(unitID)
-    unitSightDistances[unitID] = nil
-    lastMovePosition[unitID] = nil
-    recentlyAttacked[unitID] = nil
-    nonAirUnits[unitID] = nil
+	unitSightDistances[unitID] = nil
+	lastMovePosition[unitID] = nil
+	recentlyAttacked[unitID] = nil
+	nonAirUnits[unitID] = nil
 end
 
 function gadget:GameFrame(frame)
-    for unitID, sightDist in pairs(unitSightDistances) do
-        if nonAirUnits[unitID] then
-            if unitID % UPDATE_INTERVAL == frame % UPDATE_INTERVAL then --first time trying something like this, let me know what you think
-                local unitStates = spGetUnitStates(unitID)
-                if unitStates.firestate ~= FIRESTATE_HOLDFIRE then
-                    if unitStates.movestate == MOVESTATE_MANEUVER then
-                        local cmdQueue = spGetUnitCommands(unitID, 1) or {}
-                        local currentPosition = { spGetUnitPosition(unitID) }
-                        local validTargetID = nil
-                        local targetID = spGetUnitNearestEnemy(unitID, sightDist, true)
-                        if targetID then
-                            local targetDefID = spGetUnitDefID(targetID)
-                            if targetDefID and not UnitDefs[targetDefID].canFly then
-                                validTargetID = targetID 
-                            end
-                        end
-                        if validTargetID then
-                            Spring.Echo(validTargetID, "is Valid target")
-                            -- Spring.Echo(cmdQueue)
-                            if unitStates.firestate == FIRESTATE_RETURNFIRE then
-                                local attackedRecently = recentlyAttacked[unitID] and (frame - recentlyAttacked[unitID] <= ATTACK_MEMORY_DURATION)
-                                if attackedRecently then
-                                    if not cmdQueue[1] then
-                                        -- Spring.Echo("[RETURN-FIRE CHASE]", unitID, "->" ,validTargetID) --couldn't get this echo to work
-                                        Spring.Echo("[RETURN-FIRE CHASE]")
-                                        spGiveOrderToUnit(unitID, CMD_ATTACK, {validTargetID}, {})
-                                    end
-                                end
-                            else
-                                if not cmdQueue[1] then
-                                    --This block never runs with current settings (UPDATE_INTERVAL = 15). Race condition with engine MOVESTATE_MANUEVER?
-                                    -- Spring.Echo("[CHASE]", unitID, "->", validTargetID)--couldn't get this echo to work
-                                    Spring.Echo("[CHASE]") 
-                                    spGiveOrderToUnit(unitID, CMD_ATTACK, {validTargetID}, {})
-                                end
-                            end
-                        else
-                            local origPosition = lastMovePosition[unitID]
-                            local radius = spGetUnitRadius(unitID) or RADIUS_FALLBACK
-                            if not cmdQueue[1] and (
-                                not origPosition or
-                                math.abs(currentPosition[1] - origPosition.x) > (radius * 2) or
-                                math.abs(currentPosition[3] - origPosition.z) > (radius * 2)
-                            ) then
-                                -- Spring.Echo("[RETURNING]", unitID, "->", origPosition.x, origPosition.y, origPosition.z) --couldn't get this echo to work
-                                Spring.Echo("[RETURNING]")
-                                spGiveOrderToUnit(unitID, CMD_MOVE, {
-                                    origPosition.x,
-                                    origPosition.y,
-                                    origPosition.z
-                                }, {})
-                            end
-                        end
-                    else
-                        local x, y, z = spGetUnitPosition(unitID)
-                        if x and y and z then
-                            lastMovePosition[unitID] = { 
-                                x = x, 
-                                y = y, 
-                                z = z 
-                            }
-                        end
-                    end
-                end
-            end
-        end
-    end
+	for unitID, sightDist in pairs(unitSightDistances) do
+		if nonAirUnits[unitID] then
+			if unitID % UPDATE_INTERVAL == frame % UPDATE_INTERVAL then --first time trying something like this, let me know what you think
+				local unitStates = spGetUnitStates(unitID)
+
+				if unitStates.firestate ~= FIRESTATE_HOLDFIRE then
+					if unitStates.movestate == MOVESTATE_MANEUVER then
+						local cmdQueue = spGetUnitCommands(unitID, 1) or {}
+						local currentPosition = { spGetUnitPosition(unitID) }
+						local validTargetID = nil
+						local targetID = spGetUnitNearestEnemy(unitID, sightDist, true)
+
+						if targetID then
+							local targetDefID = spGetUnitDefID(targetID)
+							if targetDefID and not UnitDefs[targetDefID].canFly then
+								validTargetID = targetID 
+							end
+						end
+
+						if validTargetID then
+							Spring.Echo(validTargetID, "is Valid target")
+							-- Spring.Echo(cmdQueue)
+							if unitStates.firestate == FIRESTATE_RETURNFIRE then
+								local attackedRecently = recentlyAttacked[unitID] and (frame - recentlyAttacked[unitID] <= ATTACK_MEMORY_DURATION)
+								
+								if attackedRecently then
+									if not cmdQueue[1] then
+										-- Spring.Echo("[RETURN-FIRE CHASE]", unitID, "->" ,validTargetID) --couldn't get this echo to work
+										Spring.Echo("[RETURN-FIRE CHASE]")
+										spGiveOrderToUnit(unitID, CMD_ATTACK, {validTargetID}, {})
+									end
+								end
+							else
+								if not cmdQueue[1] then
+									--This block never runs with current settings (UPDATE_INTERVAL = 15). Race condition with engine MOVESTATE_MANUEVER?
+									-- Spring.Echo("[CHASE]", unitID, "->", validTargetID)--couldn't get this echo to work
+									Spring.Echo("[CHASE]") 
+									spGiveOrderToUnit(unitID, CMD_ATTACK, {validTargetID}, {})
+								end
+							end
+						else
+							local origPosition = lastMovePosition[unitID]
+							local radius = spGetUnitRadius(unitID) or RADIUS_FALLBACK
+
+							if not cmdQueue[1] and (
+								not origPosition or
+								math.abs(currentPosition[1] - origPosition.x) > (radius * 2) or
+								math.abs(currentPosition[3] - origPosition.z) > (radius * 2)
+							) then
+								-- Spring.Echo("[RETURNING]", unitID, "->", origPosition.x, origPosition.y, origPosition.z) --couldn't get this echo to work
+								Spring.Echo("[RETURNING]")
+								spGiveOrderToUnit(unitID, CMD_MOVE, {
+									origPosition.x,
+									origPosition.y,
+									origPosition.z
+								}, {})
+							end
+						end
+					else
+						local x, y, z = spGetUnitPosition(unitID)
+						
+						if x and y and z then
+							lastMovePosition[unitID] = { 
+								x = x, 
+								y = y, 
+								z = z 
+							}
+						end
+					end
+				end
+			end
+		end
+	end
 end
