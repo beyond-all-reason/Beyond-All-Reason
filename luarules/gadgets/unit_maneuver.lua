241ab4edc0c4212d87377b89acfe4565bd9cfadb
Malcolm Mitchell <mvlcolmm@gmail.com>
2025-07-17 23:27:51 +1000

    refactored logic before testing return command retries to solve spam move when unit is pushed out of it's lastmoveposition

--- luarules/gadgets/unit_maneuver.lua
+++ luarules/gadgets/unit_maneuver.lua
@@ -22,7 +22,6 @@ local CMD_MOVE = CMD.MOVE
 local CMD_STOP = CMD.STOP
 local CMD_FIGHT = CMD.FIGHT
 local MOVESTATE_MANEUVER = 1
-local FIRESTATE_HOLDFIRE = 0
 local FIRESTATE_RETURNFIRE = 1
 local UPDATE_INTERVAL = 10
 local ATTACK_MEMORY_DURATION = 90
@@ -45,30 +44,32 @@ local notFlyingWithSight = {}
 
 for i = 1, #UnitDefs do
 	local def = UnitDefs[i]
-	if not def.canFly and def.sightDistance then
-		notFlyingWithSight[def.id] = {sightDist = def.sightDistance}
+	if not def.canFly and not def.isBuilding and def.sightDistance then
+		notFlyingWithSight[i] = def.sightDistance
 	end
 end
 
 function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions)
 		if (cmdID == CMD_MOVE or cmdID == CMD_FIGHT) and cmdParams[1] and cmdParams[2] and cmdParams[3] then
 			lastMovePosition[unitID] = { cmdParams[1], cmdParams[2], cmdParams[3] }
-		end
-		
-		if cmdID == CMD_STOP then
-			lastMovePosition[unitID] = { spGetUnitPosition(unitID) }
+	elseif cmdID == CMD_STOP then
+		local x, y, z = spGetUnitPosition(unitID)
+		lastMovePosition[unitID] = { x, y, z }
 		end
 
 	return true
 end
 
 function gadget:UnitCreated(unitID, unitDefID, unitTeam)
-	if notFlyingWithSight[unitDefID] then
-		customManeuverUnitIDs[unitID] = notFlyingWithSight[unitDefID].sightDist
-	end
+	local sightDist = notFlyingWithSight[unitDefID]
 
+	if sightDist then
+		customManeuverUnitIDs[unitID] = sightDist
+	end
+	-- returning to origin on factory maneuver setting
 	-- if not lastMovePosition[unitID] then
-	-- 	lastMovePosition[unitID] = { spGetUnitPosition(unitID) }-- returning to origin on factory maneuver setting
+	-- 	local x, y, z = spGetUnitPosition(unitID)
+	-- 	lastMovePosition[unitID] = { x, y, z }
 	-- end
 end
 
@@ -85,6 +86,40 @@ function gadget:UnitDestroyed(unitID)
 	customManeuverUnitIDs[unitID] = nil
 end
 
+local function IsAlreadyMovingTo()
+end
+
+-- units that are pushed out of lastmove position are calling this forever.
+-- maybe i need to reset last move for idle units somehow
+local function ReturnToOrigin(unitID, cmdQueue)
+	local currentPosition = { spGetUnitPosition(unitID) }
+	local originalPosition = lastMovePosition[unitID]
+	local radius = spGetUnitRadius(unitID) or RADIUS_FALLBACK
+	cmdQueue = cmdQueue or {}
+
+	if originalPosition and --not
+		(math.abs(currentPosition[1] - originalPosition[1]) > radius * RADIUS_BUFFER_MULTIPLIER or 
+		math.abs(currentPosition[3] - originalPosition[3]) > radius * RADIUS_BUFFER_MULTIPLIER) 
+	then
+		if cmdQueue[1] then
+			local params = cmdQueue[1].params
+			if params and #params == 3 then
+				if not (math.abs(params[1] - originalPosition[1]) <= radius and
+						math.abs(params[3] - originalPosition[3]) <= radius)
+				then	 
+					Spring.Echo(unitID, "[RETURN TO ORIGIN]")
+					spGiveOrderToUnit(unitID, CMD_MOVE, originalPosition, {})
+				end
+			end
+		else
+			Spring.Echo(unitID, "[RETURN TO ORIGIN]")
+			spGiveOrderToUnit(unitID, CMD_MOVE, originalPosition, {})
+		end
+	end
+
+	returnToOrigin[unitID] = true
+end
+
 function gadget:GameFrame(frame)
 	for unitID, sightDist in pairs(customManeuverUnitIDs) do
 		if unitID % UPDATE_INTERVAL == frame % UPDATE_INTERVAL then
@@ -94,55 +129,52 @@ function gadget:GameFrame(frame)
 			if unitStates.movestate == MOVESTATE_MANEUVER then
 				local cmdQueue = spGetUnitCommands(unitID, 1) or {}
 
-				if returnToOrigin[unitID] then
-					local currentPosition = { spGetUnitPosition(unitID) }
-					local originalPosition = lastMovePosition[unitID]
-					local radius = spGetUnitRadius(unitID) or RADIUS_FALLBACK
+					-- if returnToOrigin[unitID] then
+					-- 		local currentPosition = { spGetUnitPosition(unitID) }
+					-- 		local originalPosition = lastMovePosition[unitID]
+					-- 		local radius = spGetUnitRadius(unitID) or RADIUS_FALLBACK
 
-					if not cmdQueue[1] and (
-						not originalPosition or
-						math.abs(currentPosition[1] - originalPosition[1]) > (radius * 4) or -- spamming moves in large group becaue can't get to preferred spot
-						math.abs(currentPosition[3] - originalPosition[3]) > (radius * 4)
-					) then
-						Spring.Echo(unitID, "[RETURN TO ORIGIN]")
-						spGiveOrderToUnit(unitID, CMD_MOVE, { originalPosition[1], originalPosition[2], originalPosition[3] }, {})
-						returnToOrigin[unitID] = false
-					end
-
-					if not cmdQueue[1] then
-						returnToOrigin[unitID] = false
-					end
-				elseif unitStates.firestate == FIRESTATE_RETURNFIRE then
+					-- 		if not originalPosition or
+					-- 			(math.abs(currentPosition[1] - originalPosition[1]) > radius * RADIUS_BUFFER_MULTIPLIER or 
+					-- 			math.abs(currentPosition[3] - originalPosition[3]) > radius * RADIUS_BUFFER_MULTIPLIER) 
+					-- 		then
+					-- 			Spring.Echo(unitID, "[RETURN TO ORIGIN]")
+					-- 			spGiveOrderToUnit(unitID, CMD_MOVE, originalPosition, {})
+					-- 		end
+							
+					-- 		returnToOrigin[unitID] = false
+					-- 	end
+					-- elseif unitStates.firestate == FIRESTATE_RETURNFIRE then
+				if unitStates.firestate == FIRESTATE_RETURNFIRE then
 					local attackedRecently = recentlyAttacked[unitID] and (frame - recentlyAttacked[unitID] <= ATTACK_MEMORY_DURATION)
-					local targetID
-					-- Spring.Echo(targetID)
+					local targetID = spGetUnitNearestEnemy(unitID, sightDist, true)
 
 					if attackedRecently then
-						-- Spring.Echo("I've been attacked")
-						targetID = spGetUnitNearestEnemy(unitID, sightDist, true)
-
 						if targetID then	
 							local targetDefID = spGetUnitDefID(targetID)
-							if notFlyingWithSight[targetDefID] and not cmdQueue[1] then
-								Spring.Echo("[RETURNING CHASE]")
+							
+							-- if targetDefID and notFlyingWithSight[targetDefID] and not cmdQueue[1] then
+							if targetDefID and notFlyingWithSight[targetDefID] and (not cmdQueue[1] or returnToOrigin[unitID]) then
+								Spring.Echo(unitID, "[RETURNING CHASE]")
 								spGiveOrderToUnit(unitID, CMD_ATTACK, targetID)	
+								returnToOrigin[unitID] = false
+							end
+						end
+					else
+						if not targetID then
+							-- returnToOrigin[unitID] = true
+							ReturnToOrigin(unitID, cmdQueue)
 							end
 						end
 					end
 					
-					targetID = spGetUnitNearestEnemy(unitID, sightDist, true)
-					
-					if not targetID then
-						-- Spring.Echo("[I'm done chasing]")
-						returnToOrigin[unitID] = true
-					end
-				elseif cmdQueue[1] and cmdQueue[1].id == CMD_ATTACK then
-					Spring.Echo(unitID, "[CHASING]")
-					returnToOrigin[unitID] = true
+				if not cmdQueue[1] then
+					ReturnToOrigin(unitID)
+					-- elseif cmdQueue[1] and cmdQueue[1].id == CMD_ATTACK then
+					-- Spring.Echo(unitID, "[CHASING]")
+					-- returnToOrigin[unitID] = true
 				end
 			end
 		end
 	end
 end
-
-
