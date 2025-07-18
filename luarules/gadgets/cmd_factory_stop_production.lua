5041cde5595a1c2bdceeb97d630be6efccc457cf
efrec <quantumscrewdriver@gmail.com>
2025-07-18 14:56:46 -0400

    forward the command id only, not CMD_INSERT

--- luarules/gadgets/cmd_factory_stop_production.lua
+++ luarules/gadgets/cmd_factory_stop_production.lua
@@ -105,7 +105,7 @@ if gadgetHandler:IsSyncedCode() then
 		spGiveOrderToUnit(unitID, CMD_WAIT, EMPTY, 0) -- Removes wait if there is a wait but doesn't readd it.
 		spGiveOrderToUnit(unitID, CMD_WAIT, EMPTY, 0) -- If a factory is waiting, it will not clear the current build command, even if the cmd is removed.
 		-- See: http://zero-k.info/Forum/Post/237176#237176 for details.
-		SendToUnsynced(identifier, unitID, unitDefID, unitTeam, cmdID)
+		SendToUnsynced(identifier, unitID, unitDefID, unitTeam, CMD_STOP_PRODUCTION)
 	end
 
 	-- Add the command to factories
