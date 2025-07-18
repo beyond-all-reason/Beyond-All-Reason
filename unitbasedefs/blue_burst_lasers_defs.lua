ed99849e0553140d2e4f7b3008c562bf62f5fd45
Neb <maxpliskin@outlook.com>
2025-07-17 22:37:12 -0600

    Add HEMG to HLT

--- unitbasedefs/blue_burst_lasers_defs.lua
+++ unitbasedefs/blue_burst_lasers_defs.lua
@@ -50,21 +50,42 @@ local function blue_burst_lasersTweaks(name, uDef)
 			vtol = 7.5
 		}
 	elseif name == "armhlt" then
-		uDef.weapondefs.arm_laserh1.name = "Light g2g Burst Laser"
-		uDef.weapondefs.arm_laserh1.rgbcolor = "0.2 0.1 1.0"
-		uDef.weapondefs.arm_laserh1.beamburst = true
-		uDef.weapondefs.arm_laserh1.burst = 3
-		uDef.weapondefs.arm_laserh1.burstrate = 0.175
-		uDef.weapondefs.arm_laserh1.thickness = 6
-		uDef.weapondefs.arm_laserh1.reloadtime = 2.7
-		uDef.weapondefs.arm_laserh1.soundstart = "lasrlit3"
-		uDef.weapondefs.arm_laserh1.explosiongenerator = "custom:laserhit-medium-blue"
-		uDef.weapondefs.arm_laserh1.soundtrigger = false
-		uDef.weapondefs.arm_laserh1.energypershot = 25
-		uDef.weapondefs.arm_laserh1.damage = {
-			commanders = 290.25,
-			default = 193.5,
-			vtol = 17.5,
+		uDef.weapondefs = {
+			arm_laserh1 = {
+				areaofeffect = 45,
+				avoidfeature = false,
+				burst = 3,
+				burstrate = 0.1,
+				corethickness = 0.25,
+				craterareaofeffect = 0,
+				craterboost = 0,
+				cratermult = 0,
+				edgeeffectiveness = 0.15,
+				energypershot = 25,
+				explosiongenerator = "custom:plasmahit-medium",
+				firestarter = 90,
+				impulsefactor = 0,
+				name = "Long-Range g2g High Energy Machine Gun",
+				noselfdamage = true,
+				range = 620,
+				reloadtime = 1,
+				rgbcolor = "0.8 0.85 0.8",
+				size = 4,
+				stages = 8,
+				soundhit = "xplomed2",
+				soundhitwet = "splsbig",
+				soundstart = "flashemg",
+				soundtrigger = false,
+				tolerance = 10000,
+				turret = true,
+				weapontype = "Cannon",
+				weaponvelocity = 750,
+				damage = {
+					commanders = 107.5,
+					default = 71.67,
+					vtol = 6.48,
+				},
+			},
 		}
     elseif name == "armcom" then
         uDef.weapondefs.armcomlaser.name = "Light g2g/g2a Burst Laser"
@@ -99,21 +120,42 @@ local function blue_burst_lasersTweaks(name, uDef)
 		}
 
 	elseif name == "armfhlt" then
-		uDef.weapondefs.armfhlt_laser.name = "High-Energy g2g Burst Laser"
-		uDef.weapondefs.armfhlt_laser.rgbcolor = "0.2 0.1 1.0"
-		uDef.weapondefs.armfhlt_laser.beamburst = true
-		uDef.weapondefs.armfhlt_laser.burst = 3
-		uDef.weapondefs.armfhlt_laser.burstrate = 0.175
-		uDef.weapondefs.armfhlt_laser.thickness = 6
-		uDef.weapondefs.armfhlt_laser.reloadtime = 1.35
-		uDef.weapondefs.armfhlt_laser.soundstart = "lasrlit3"
-		uDef.weapondefs.armfhlt_laser.explosiongenerator = "custom:laserhit-medium-blue"
-		uDef.weapondefs.armfhlt_laser.soundtrigger = false
-		uDef.weapondefs.armfhlt_laser.energypershot = 13.33
-		uDef.weapondefs.armfhlt_laser.damage = {
-			commanders = 150,
-			default = 105,
-			vtol = 26,
+		uDef.weapondefs = {
+			armfhlt_laser = {
+				areaofeffect = 45,
+				avoidfeature = false,
+				burst = 3,
+				burstrate = 0.1,
+				corethickness = 0.25,
+				craterareaofeffect = 0,
+				craterboost = 0,
+				cratermult = 0,
+				edgeeffectiveness = 0.15,
+				energypershot = 25,
+				explosiongenerator = "custom:laserhit-medium-green",
+				firestarter = 90,
+				impulsefactor = 0,
+				name = "Long-Range g2g High Energy Machine Gun",
+				noselfdamage = true,
+				range = 620,
+				reloadtime = 0.9,
+				rgbcolor = "0.8 1 0.6",
+				size = 4,
+				stages = 8,
+				soundhit = "xplomed2",
+				soundhitwet = "splsbig",
+				soundstart = "flashemg",
+				soundtrigger = false,
+				tolerance = 10000,
+				turret = true,
+				weapontype = "Cannon",
+				weaponvelocity = 750,
+				damage = {
+					commanders = 100,
+					default = 70,
+					vtol = 17.33,
+				},
+			}
 		}
 
 	elseif name == "armraz" then
