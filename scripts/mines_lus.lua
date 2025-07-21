1ad3bb1a9f3fe7d11468f895df7255bed4ce79f2
Wybren Koelmans <wybrenkoelmans@gmail.com>
2025-07-21 21:00:32 +0200

    Optimized mines_lus.lua by caching methods and some small maintance like whitespace

--- scripts/mines_lus.lua
+++ scripts/mines_lus.lua
@@ -1,30 +1,39 @@
+local Spring_GetUnitDefID = Spring.GetUnitDefID
+local Spring_GetUnitPosition = Spring.GetUnitPosition
+local Spring_GetUnitNearestEnemy = Spring.GetUnitNearestEnemy
+local Spring_GetUnitIsBeingBuilt = Spring.GetUnitIsBeingBuilt
+local Spring_GetUnitStates = Spring.GetUnitStates
+local Spring_GetUnitIsStunned = Spring.GetUnitIsStunned
+local Spring_DestroyUnit = Spring.DestroyUnit
 
 local base = piece("base")
-local unitDefID = Spring.GetUnitDefID(unitID)
+local unitDefID = Spring_GetUnitDefID(unitID)
 local triggerRange = tonumber(UnitDefs[unitDefID].customParams.detonaterange) or 64
 
 local math_sqrt = math.sqrt
+
+local ux, uy, uz
 
 -- Author: Doo
 -- Requires customParams.detonaterange in unitDefs or 64 elmos range will be used
 -- Possible enhancements: Use GetUnitsInCylinder(or sphere) of detonaterange and check for any restriction on target units
 
 function GetClosestEnemyDistance()
-	targetID = Spring.GetUnitNearestEnemy(unitID, triggerRange)
+	local targetID = Spring_GetUnitNearestEnemy(unitID, triggerRange)
 	if targetID then
-		local tx,ty,tz = Spring.GetUnitPosition(targetID)
-		local dis = distance(ux,uy,uz,tx,ty,tz)
+		local tx, ty, tz = Spring_GetUnitPosition(targetID)
+		local dis = distance(ux, uy, uz, tx, ty, tz)
 		return dis
 	else
 		return math.huge
 	end
 end
 
-function distance(x1,y1,z1,x2,y2,z2)
-	local x = (x1-x2)
-	local y = (y1-y2)
-	local z = (z1-z2)
-	local dist = math_sqrt(x*x + y*y + z*z)
+function distance(x1, y1, z1, x2, y2, z2)
+	local x = (x1 - x2)
+	local y = (y1 - y2)
+	local z = (z1 - z2)
+	local dist = math_sqrt(x * x + y * y + z * z)
 	return dist
 end
 
@@ -44,27 +53,31 @@ function script.FireWeapon()
 end
 
 function script.Create()
-	ux,uy,uz = Spring.GetUnitPosition(unitID)
+	ux, uy, uz = Spring_GetUnitPosition(unitID)
 	StartThread(EnemyDetect)
 end
 
 function EnemyDetect()
 	while true do
-		local inProgress = Spring.GetUnitIsBeingBuilt(unitID)
-		local firestate = Spring.GetUnitStates(unitID, false)
-		local stunned = Spring.GetUnitIsStunned (unitID) 
-		if not inProgress and firestate and firestate > 0 and GetClosestEnemyDistance() <= triggerRange and not stunned then
-			StartThread(Detonate)
-			break
-		else
-			Sleep(1)
+		if not Spring_GetUnitIsBeingBuilt(unitID) then
+			local firestate = Spring_GetUnitStates(unitID)
+			if firestate and firestate > 0 then
+				if not Spring_GetUnitIsStunned(unitID) then
+					if GetClosestEnemyDistance() <= triggerRange then
+						StartThread(Detonate)
+						return
+					end
+				end
+			end
 		end
+
+		Sleep(1)
 	end
 end
 
 function Detonate()
 	Sleep(500)
-	Spring.DestroyUnit(unitID, false, false)
+	Spring_DestroyUnit(unitID, false, false)
 end
 
 function script.Killed()
