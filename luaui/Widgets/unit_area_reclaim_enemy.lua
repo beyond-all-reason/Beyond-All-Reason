2e3056ece7fee251740ac51b1178d951e005c7aa
Nemo Robles <nemesisrobles@hotmail.com>
2025-07-26 12:04:04 -0500

    using space for enemy reclaim to match engine and improving code

--- luaui/Widgets/unit_area_reclaim_enemy.lua
+++ luaui/Widgets/unit_area_reclaim_enemy.lua
@@ -3,16 +3,15 @@ local widget = widget ---@type Widget
 function widget:GetInfo()
 	return {
 		name			= "Area Reclaim Enemy",
-		desc			= "Hold down Alt an give area reclaim order on the ground or enemy.",
+		desc			= "Hold down Space an give area reclaim order on the ground or enemy to target enemies only during reclaim.",
 		author			= "NemoTheHero",
-		date			= "May 16, 2025",
+		date			= "July 26, 2025",
 		license			= "GNU GPL, v2 or later",
 		layer			= 0,
 		enabled			= true
 	}
 end
 
-local team = Spring.GetMyTeamID()
 local allyTeam = Spring.GetMyAllyTeamID()
 
 -- Speedups
@@ -31,17 +30,15 @@ local reclaimEnemy = Game.reclaimAllowEnemies
 
 local CMD_RECLAIM = CMD.RECLAIM
 
-local gameStarted
 
 
 function maybeRemoveSelf()
-    if Spring.GetSpectatingState() and (Spring.GetGameFrame() > 0 or gameStarted) then
+    if Spring.GetSpectatingState() and (Spring.GetGameFrame() > 0) then
         widgetHandler:RemoveWidget()
     end
 end
 
 function widget:GameStart()
-    gameStarted = true
     maybeRemoveSelf()
 end
 
@@ -50,89 +47,76 @@ function widget:PlayerChanged(playerID)
 end
 
 function widget:Initialize()
-    if Spring.IsReplay() or Spring.GetGameFrame() > 0 then
+    if Spring.IsReplay() then
         maybeRemoveSelf()
     end
 end
-
----Finds the average position of passed in units
----@param units table selected units
----@return table { x, z }
-local function getAvgPositionOfUnits(units)
-	local unitCount = 0
-	local tX, tZ = 0, 0
-	for i = 1, #units do
-		local id = units[i]
-		local x, _, z = spGetUnitPosition(id)
-		if z then
-			tX, tZ = tX+x, tZ+z
-			unitCount = unitCount + 1
-		end
-	end
-	if unitCount == 0 then return end
-	return { x = tX / unitCount, z = tZ / unitCount }
-end
-
-
 
 function widget:CommandNotify(id, params, options)
 	-- RECLAIM with area affect
 	if id ~= CMD_RECLAIM or #params ~= 4 then
 		return
 	end
-	-- holding alt key
-	if options.alt then
-		
-		local cx, cy, cz = params[1], params[2], params[3]
+	-- holding space key
+	if options.meta then
+
+		local cx, cy, cz, cr = unpack(params)
 
 		local mx,my,mz = spWorldToScreenCoords(cx, cy, cz)
 		local cType,id = spTraceScreenRay(mx,my)
 
-		if cType == "unit" or cType == "ground" then
+		if not ( cType == "unit" or cType == "ground" ) then
+			return
+		end
+		
 
-			local cr = params[4]
-			-- x,y,radius of command
-			local unitsWithReclaim = spGetSelectedUnits()
-			if #unitsWithReclaim == 0 then
-				return
+		-- x,y,radius of command
+		local unitsWithReclaim = spGetSelectedUnits()
+		   
+		local areaUnits = spGetUnitsInCylinder(cx, cz, cr)
+		local enemyUnits = {}
+		-- get all enemy units in the area
+		for i=1,#areaUnits do
+			local unitID    = areaUnits[i]
+			local enemyUnit = not Spring.AreTeamsAllied(Spring.GetUnitTeam(unitID), Spring.GetMyTeamID())
+			if enemyUnit then
+				table.insert(enemyUnits, unitID)
 			end
-			local averagePosUnits = getAvgPositionOfUnits(unitsWithReclaim)
+		end
+		-- if no enemies, we default to regular reclaim behavior
+		if #enemyUnits == 0 then
+			return
+		end
 
-			local areaUnits = spGetUnitsInCylinder(cx, cz, cr)
-			local enemyUnits = {}
-			for i=1,#areaUnits do
-				local unitID    = areaUnits[i]
-				local enemyUnit = spGetUnitAllyTeam(unitID) ~= allyTeam
-				if enemyUnit then
-					table.insert(enemyUnits, unitID)
-				end
+		-- sort enemyUnits by distance from averagePoint
+		local avgx, avgy, avgz = Spring.GetUnitArrayCentroid(unitsWithReclaim)
+
+		table.sort(enemyUnits,
+			function (unit1, unit2)
+				local x1, _, z1 = spGetUnitPosition(unit1)
+				local x2, _, z2 = spGetUnitPosition(unit2)
+				--distance formula
+				return math.hypot(avgx-x1, avgz-z1) <
+				       math.hypot(avgx-x2, avgz-z2)
 			end
+		)
 
-			if #enemyUnits == 0 then
-				return
-			end
 
-			-- sort enemyUnits by distance from averagePoint
-			table.sort(enemyUnits,
-				function (unit1, unit2)
-					local x1, _, z1 = spGetUnitPosition(unit1)
-					local x2, _, z2 = spGetUnitPosition(unit2)
-					--distance formula
-					return (((averagePosUnits.x-x1)^2)+((averagePosUnits.z-z1)^2))^.5 <
-					(((averagePosUnits.x-x2)^2)+((averagePosUnits.z-z2)^2))^.5
-				end
-			)
+		-- create array of commands to reclaim each enemy unit
+		local newCmds = {}
+		for i = 1, #enemyUnits do
+			local unitID = enemyUnits[i]
 			
-			local count = 0
-			for i=1,#enemyUnits do
-				local unitID = enemyUnits[i]
-				local cmdOpts = {"meta", "ctrl"}
-				if count ~= 0 or options.shift then
-					cmdOpts = {"shift", "meta", "ctrl"}
+				local cmdOpts = CMD.OPT_META + CMD.OPT_CTRL
+				if #newCmds ~= 0 or options.shift then
+					cmdOpts = cmdOpts + CMD.OPT_SHIFT
 				end
-				spGiveOrderToUnitArray( unitsWithReclaim, CMD_RECLAIM, {unitID}, cmdOpts)
-				count = count + 1
-			end
+				newCmds[#newCmds + 1] = { CMD_RECLAIM, unitID , cmdOpts }
+		end
+
+		-- add the command to all units with reclaim
+		if #newCmds > 0 then
+			Spring.GiveOrderArrayToUnitArray(unitsWithReclaim, newCmds)
 			return true
 		end
 	end
