b633b913ee8c00fbef8a52c601c718623d414767
hihoman23 <78002940+hihoman23@users.noreply.github.com>
2025-07-27 14:26:35 +0200

    Fix comment

--- luaui/Widgets/gui_idle_builders.lua
+++ luaui/Widgets/gui_idle_builders.lua
@@ -26,7 +26,7 @@ local maxIcons = 9
 local showRez = true
 local doUpdateForce = true
 local justSelected = false
-local listSorted = {} -- listUpdated[unitDefID]: set to true when idle list is updated, set to false when idleList[unitDefID] is sorted
+local listSorted = {} -- listSorted[unitDefID]: set to true when idle list is updated, set to false when idleList[unitDefID] is sorted
 
 local leftclick = 'LuaUI/Sounds/buildbar_add.wav'
 local rightclick = 'LuaUI/Sounds/buildbar_click.wav'
