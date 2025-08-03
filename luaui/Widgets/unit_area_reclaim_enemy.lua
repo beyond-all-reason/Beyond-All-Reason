40c673d657e16183df7b15f308dd982820c29e2d
Nemo Robles <nemesisrobles@hotmail.com>
2025-08-03 17:38:23 -0500

    early return if not meta and if avgx is nil

--- luaui/Widgets/unit_area_reclaim_enemy.lua
+++ luaui/Widgets/unit_area_reclaim_enemy.lua
@@ -48,77 +48,72 @@ end
 
 function widget:Initialize()
     if Spring.IsReplay() then
-        maybeRemoveSelf()
+		maybeRemoveSelf()
     end
 end
 
 function widget:CommandNotify(id, params, options)
-	-- RECLAIM with area affect
-	if id ~= CMD_RECLAIM or #params ~= 4 then
+	-- RECLAIM with area affect and holding space key if not default to regular reclaim behavior
+	if id ~= CMD_RECLAIM or #params ~= 4 or not options.meta then
 		return
 	end
-	-- holding space key
-	if options.meta then
+	
+	local cx, cy, cz, cr = unpack(params)
 
-		local cx, cy, cz, cr = unpack(params)
+	local mx,my,mz = spWorldToScreenCoords(cx, cy, cz)
+	local cType,id = spTraceScreenRay(mx,my)
 
-		local mx,my,mz = spWorldToScreenCoords(cx, cy, cz)
-		local cType,id = spTraceScreenRay(mx,my)
+	if not ( cType == "unit" or cType == "ground" ) then
+		return
+	end
 
-		if not ( cType == "unit" or cType == "ground" ) then
-			return
+	-- x,y,radius of command
+	local selectedUnits = spGetSelectedUnits()
+	   
+	local areaUnits = spGetUnitsInCylinder(cx, cz, cr)
+	local enemyUnits = {}
+	-- get all enemy units in the area
+	for i=1,#areaUnits do
+		local unitID = areaUnits[i]
+		local enemyUnit = not Spring.AreTeamsAllied(Spring.GetUnitTeam(unitID), Spring.GetMyTeamID())
+		if enemyUnit then
+			table.insert(enemyUnits, unitID)
 		end
-		
+	end
+	-- if no enemies, we default to regular reclaim behavior
+	if #enemyUnits == 0 then
+		return
+	end
 
-		-- x,y,radius of command
-		local unitsWithReclaim = spGetSelectedUnits()
-		   
-		local areaUnits = spGetUnitsInCylinder(cx, cz, cr)
-		local enemyUnits = {}
-		-- get all enemy units in the area
-		for i=1,#areaUnits do
-			local unitID    = areaUnits[i]
-			local enemyUnit = not Spring.AreTeamsAllied(Spring.GetUnitTeam(unitID), Spring.GetMyTeamID())
-			if enemyUnit then
-				table.insert(enemyUnits, unitID)
-			end
+	-- get avg point of selected units
+	local avgx, avgy, avgz = Spring.GetUnitArrayCentroid(selectedUnits)
+	-- sort enemyUnits by distance from averagePoint of selected units
+	table.sort(enemyUnits,
+		function (unit1, unit2)
+			local x1, _, z1 = spGetUnitPosition(unit1)
+			local x2, _, z2 = spGetUnitPosition(unit2)
+			--distance formula
+			return math.hypot(avgx-x1, avgz-z1) <
+			       math.hypot(avgx-x2, avgz-z2)
 		end
-		-- if no enemies, we default to regular reclaim behavior
-		if #enemyUnits == 0 then
-			return
+	)
+
+
+	-- create array of commands to reclaim each enemy unit
+	local newCmds = {}
+	for i = 1, #enemyUnits do
+		local unitID = enemyUnits[i]
+		local cmdOpts = CMD.OPT_META + CMD.OPT_CTRL
+		if #newCmds ~= 0 or options.shift then
+			cmdOpts = cmdOpts + CMD.OPT_SHIFT
 		end
+		newCmds[#newCmds + 1] = { CMD_RECLAIM, unitID , cmdOpts }
+	end
 
-		-- sort enemyUnits by distance from averagePoint
-		local avgx, avgy, avgz = Spring.GetUnitArrayCentroid(unitsWithReclaim)
-
-		table.sort(enemyUnits,
-			function (unit1, unit2)
-				local x1, _, z1 = spGetUnitPosition(unit1)
-				local x2, _, z2 = spGetUnitPosition(unit2)
-				--distance formula
-				return math.hypot(avgx-x1, avgz-z1) <
-				       math.hypot(avgx-x2, avgz-z2)
-			end
-		)
-
-
-		-- create array of commands to reclaim each enemy unit
-		local newCmds = {}
-		for i = 1, #enemyUnits do
-			local unitID = enemyUnits[i]
-			
-				local cmdOpts = CMD.OPT_META + CMD.OPT_CTRL
-				if #newCmds ~= 0 or options.shift then
-					cmdOpts = cmdOpts + CMD.OPT_SHIFT
-				end
-				newCmds[#newCmds + 1] = { CMD_RECLAIM, unitID , cmdOpts }
-		end
-
-		-- add the command to all units with reclaim
-		if #newCmds > 0 then
-			Spring.GiveOrderArrayToUnitArray(unitsWithReclaim, newCmds)
-			return true
-		end
+	-- add the command to all units with reclaim
+	if #newCmds > 0 then
+		Spring.GiveOrderArrayToUnitArray(selectedUnits, newCmds)
+		return true
 	end
 end
 
