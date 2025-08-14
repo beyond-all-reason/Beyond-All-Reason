e0f173cf2274cc6724ae3d2f5783249e927310f5
TheEngiMan <zxdaaz3@gmail.com>
2025-08-14 22:11:44 +0300

    Impulsefactor = true fix and alphabetic sort
    accidentally made impulsefcator = true, amazing.

--- gamedata/alldefs_post.lua
+++ gamedata/alldefs_post.lua
@@ -1020,19 +1020,21 @@ function UnitDef_Post(name, uDef)
 			uDef.weapondefs.corkorg_rocket.areaofeffect = 350
 			uDef.weapondefs.corkorg_rocket.avoidfriendly  = false
 			uDef.weapondefs.corkorg_rocket.cegtag = "cruisemissiletrail-tacnuke"
-			uDef.weapondefs.corkorg_rocket.edgeeffectiveness = 0.55
-			uDef.weapondefs.corkorg_rocket.impulsefactor = false
-			uDef.weapondefs.corkorg_rocket.energypershot = 17500
 			uDef.weapondefs.corkorg_rocket.collideenemy = false
-			uDef.weapondefs.corkorg_rocket.craterareaofeffect  = 512
 			uDef.weapondefs.corkorg_rocket.commandfire = true
+			uDef.weapondefs.corkorg_rocket.craterareaofeffect  = 512
 			uDef.weapondefs.corkorg_rocket.craterboost = 1.6
 			uDef.weapondefs.corkorg_rocket.cratermult = 0.7
+			uDef.weapondefs.corkorg_rocket.customparams.stockpilelimit = 2
+			uDef.weapondefs.corkorg_rocket.edgeeffectiveness = 0.55
+			uDef.weapondefs.corkorg_rocket.energypershot = 17500
 			uDef.weapondefs.corkorg_rocket.explosiongenerator = "custom:newnuketac"
-			uDef.weapondefs.corkorg_rocket.flighttime = 12
 			uDef.weapondefs.corkorg_rocket.firestarter = 0
+			uDef.weapondefs.corkorg_rocket.flighttime = 12
+			uDef.weapondefs.corkorg_rocket.impulsefactor = 1.1
 			uDef.weapondefs.corkorg_rocket.metalpershot = 250
 			uDef.weapondefs.corkorg_rocket.model = "cortronmissile.s3o"
+			uDef.weapondefs.corkorg_rocket.name = "Long range tactical nuclear warheads"
 			uDef.weapondefs.corkorg_rocket.range = 1000
 			uDef.weapondefs.corkorg_rocket.reloadtime = 3
 			uDef.weapondefs.corkorg_rocket.soundhit = "xplomed4"
@@ -1042,8 +1044,6 @@ function UnitDef_Post(name, uDef)
 			uDef.weapondefs.corkorg_rocket.tolerance = 4000
 			uDef.weapondefs.corkorg_rocket.weaponacceleration = 75
 			uDef.weapondefs.corkorg_rocket.weapontimer = 2.5
-			uDef.weapondefs.corkorg_rocket.name = "Long range tactical nuclear warheads"
-			uDef.weapondefs.corkorg_rocket.customparams.stockpilelimit = 2
 		end
 	end
 
