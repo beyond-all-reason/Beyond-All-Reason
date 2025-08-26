289d20794e5edf7b690627d1b96ac1c2f01c255c
Neb <maxpliskin@outlook.com>
2025-08-25 21:23:57 -0600

    update balance option

--- unitbasedefs/proposed_techsplit_balance_defs.lua
+++ unitbasedefs/proposed_techsplit_balance_defs.lua
@@ -38,14 +38,85 @@ local function proposed_techsplit_balanceTweaks(name, uDef)
 
 	if name == "armwar" then
 		uDef.speed = 60
-		uDef.weapondefs.armwar_laser.range = 280
-		uDef.weapondefs.armwar_laser.damage.default = 110
-		uDef.weapondefs.armwar_laser.damage.vtol = 18
+		uDef.weapondefs.armwar_laser.range = 300
 	end
 
 	if name == "corstorm" then
 		uDef.speed = 45
+		uDef.weapondefs.cor_bot_rocket.range = 600
+		uDef.health = 590
+	end
 
+	if name == "armrock" then
+		uDef.speed = 50
+		uDef.weapondefs.arm_bot_rocket.range = 575
+		uDef.health = 575
+	end
+
+	if name == "armhlt" then
+		uDef.weapondefs.armlaserh1.range = 750
+	end
+
+	if name == "corhlt" then
+		uDef.weapondefs.corlaserh1.range = 750
+	end
+
+	if name == "armpw" then
+		uDef.speed = 90
+		uDef.health = 300
+	end
+
+	if name == "armart" then
+		uDef.metalcost = 270
+		uDef.energycost = 4400
+		uDef.buildtime = 6000
+		uDef.weapondefs.tawf113_weapon.range = 830
+		uDef.weapondefs.tawf113_weapon.damage = {
+			default = 364,
+			subs = 122,
+			vtol = 40,
+		}
+	end
+
+	if name == "corwolv" then
+		uDef.metalcost = 340
+		uDef.energycost = 5000
+		uDef.buildtime = 7100
+		uDef.weapondefs.corwolv_gun.range = 805
+		uDef.weapondefs.corwolv_gun.damage = {
+			default = 775,
+			subs = 195,
+			vtol = 78,
+		}
+	end
+
+	if name == "armham" then
+		uDef.weapondefs.arm_ham = {
+				areaofeffect = 36,
+				avoidfeature = false,
+				craterareaofeffect = 0,
+				craterboost = 0,
+				cratermult = 0,
+				edgeeffectiveness = 0.15,
+				explosiongenerator = "custom:genericshellexplosion-small",
+				gravityaffected = "true",
+				impulsefactor = 3.6,
+				name = "Light g2g gauss cannon (low trajectory)",
+				noselfdamage = true,
+				predictboost = 0.4,
+				range = 380,
+				reloadtime = 1.73333,
+				soundhit = "xplomed3",
+				soundhitwet = "splshbig",
+				soundstart = "cannon1",
+				turret = true,
+				weapontype = "Cannon",
+				weaponvelocity = 500,
+				damage = {
+					default = 52,
+					vtol = 11,
+				},
+			}
 
 	if name == "corshiva" then
 		uDef.canmanualfire = true
