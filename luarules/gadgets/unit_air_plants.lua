8d88bd032f91e5185281cc3dfe60932235cdb57c
efrec <quantumscrewdriver@gmail.com>
2025-08-29 12:53:21 -0400

    detect factory and unit size to rationalize move distances
    This cuts through a lot of the junk and tension in the definitions used in the file. I'm still avoiding a geometric argument for the final move radius -- there is no derived or even assumed clash chance for two units both calling `moveFromPlant` etc -- but if tests (soon) show any awkward behavior then I will take that as a next step.

--- luarules/gadgets/unit_air_plants.lua
+++ luarules/gadgets/unit_air_plants.lua
@@ -17,53 +17,48 @@ if not gadgetHandler:IsSyncedCode() then
 end
 
 -- `unit_airunitsturnradius.lua` sets an arbitrary attack radius
--- which influences how air units handle movement and move goals
-local bomberAttackTurnRadius = 500
--- Given a large unit radius of 80, we need to clear double that
-local moveGoalMinimumRadius = 160
--- Move goals issued don't represent real tasks so should be loose
-local moveGoalLeashRadius = 4 * Game.squareSize
+-- which influences how air units pursue and satisfy move goals.
+local bomberAttackTurnRadius = 500 ---@type number
+-- Move goals issued don't represent real tasks so can be loose.
+local moveGoalLeashRadius = 4 * Game.squareSize ---@type number
 
 local EditUnitCmdDesc = Spring.EditUnitCmdDesc
 local FindUnitCmdDesc = Spring.FindUnitCmdDesc
 local InsertUnitCmdDesc = Spring.InsertUnitCmdDesc
 local GiveOrderToUnit = Spring.GiveOrderToUnit
 local SetUnitNeutral = Spring.SetUnitNeutral
+
 local CMD_AUTOREPAIRLEVEL = CMD.AUTOREPAIRLEVEL
 local CMD_IDLEMODE = CMD.IDLEMODE
 local CMD_LAND_AT = GameCMD.LAND_AT
 local CMD_AIR_REPAIR = GameCMD.AIR_REPAIR
 
-local isAirplantNames = {
-	corap = true,
-	coraap = true,
-	corplat = true,
-	corapt3 = true,
-
-	armap = true,
-	armaap = true,
-	armplat = true,
-	armapt3 = true,
-
-	legap = true,
-	legaap = true,
-	legapt3 = true,
-}
-local isAirplantNamesCopy = table.copy(isAirplantNames)
-for name,v in pairs(isAirplantNamesCopy) do
-	isAirplantNames[name..'_scav'] = true
-end
--- convert unitname -> unitDefID
 local isAirplant = {}
-for unitName, params in pairs(isAirplantNames) do
-	if UnitDefNames[unitName] then
-		isAirplant[UnitDefNames[unitName].id] = params
-	end
-end
+local unitMoveRadius = {} -- largest of the radii to avoid dipping or clashing
+local factoryMoveRadius = {} -- consistent minimum spread across factory units
 
-local unitMoveRadius = {} -- just enough to avoid collision
 for unitDefID, unitDef in ipairs(UnitDefs) do
-	unitMoveRadius[unitDefID] = math.max(unitDef.turnRadius, unitDef.radius)
+	if unitDef.isFactory and unitDef.buildOptions[1] then
+		local airOptionOnly = true
+		local unitRadiusMax = 0
+		for _, optionID in ipairs(unitDef.buildOptions) do
+			local option = UnitDefs[optionID]
+			if option.isAirUnit then
+				unitRadiusMax = math.max(option.radius, unitRadiusMax)
+			else
+				airOptionOnly = false
+				break
+			end
+		end
+		if airOptionOnly then
+			isAirplant[unitDefID] = true
+			factoryMoveRadius[unitDefID] = unitDef.radius + unitRadiusMax + moveGoalLeashRadius
+		end
+	end
+	if unitDef.isAirUnit and unitDef.canMove then
+		-- Dipping is tied to turn radius, clashing approximately to unit radius.
+		unitMoveRadius[unitDefID] = math.max(unitDef.turnRadius, unitDef.radius)
+	end
 end
 
 local plantList = {}
@@ -88,17 +83,25 @@ local airCmd = {
 }
 
 -- Fallback method to make sure units don't land atop their origin plant.
-local function moveFromPlant(unitID, factoryRadius)
+local function moveFromPlant(unitID, radius)
 	local ux, uy, uz = Spring.GetUnitPosition(unitID)
-	local theta = math.random() * 2 * math.pi
-	local radius = 2.5 * factoryRadius
-	local tx, tz = ux + radius * math.sin(theta), uz + radius * math.cos(theta)
+	local theta = math.random() * math.tau
+	local tx = ux + radius * math.sin(theta)
+	local tz = uz + radius * math.cos(theta)
 	local ty = Spring.GetGroundHeight(tx, tz)
 	GiveOrderToUnit(unitID, CMD.INSERT, { 0, CMD.MOVE, 0, tx, ty, tz }, 0)
 end
 
 -- Prevent air units from relaxing their target height when leaving the plant.
 local function moveToCommand(unitID, command, factoryRadius)
+	-- Within a short distance, air units do not dip when leaving the plant.
+	-- Generally consider the turnRadius as though it were a dynamic value:
+	local distanceMin = Spring.GetUnitMoveTypeData(unitID).turnRadius or 0
+	if distanceMin == 0 or distanceMin == bomberAttackTurnRadius then
+		distanceMin = unitMoveRadius[Spring.GetUnitDefID(unitID)]
+	end
+	distanceMin = math.max(distanceMin, factoryRadius)
+
 	local tx, ty, tz
 	if command then
 		if #command.params == 1 then
@@ -108,21 +111,14 @@ local function moveToCommand(unitID, command, factoryRadius)
 				tx, ty, tz = Spring.GetUnitPosition(command.params[1])
 			end
 		elseif #command.params >= 3 then
-			tx, ty, tz = command.params[1], command.params[2], command.params[3]
+			tx, tz = command.params[1], command.params[3]
 		end
 	end
 
 	if not tx then
-		moveFromPlant(unitID, factoryRadius)
+		moveFromPlant(unitID, distanceMin)
 		return
 	end
-
-	-- Within a short distance, air units do not dip when leaving the plant.
-	local distanceMin = Spring.GetUnitMoveTypeData(unitID).turnRadius or 0
-	if distanceMin == 0 or distanceMin == bomberAttackTurnRadius then
-		distanceMin = unitMoveRadius[Spring.GetUnitDefID(unitID)]
-	end
-	distanceMin = math.max(distanceMin, factoryRadius)
 
 	local ux, uy, uz = Spring.GetUnitPosition(unitID)
 	local dx = tx - ux
@@ -147,7 +143,7 @@ function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
 		GiveOrderToUnit(unitID, CMD_AUTOREPAIRLEVEL, { plantList[builderID].repairAt }, 0)
 		GiveOrderToUnit(unitID, CMD_IDLEMODE, {plantList[builderID].landAt}, 0)
 		SetUnitNeutral(unitID, true)
-		buildingUnits[unitID] = math.max(Spring.GetUnitRadius(builderID) or 0, moveGoalMinimumRadius)
+		buildingUnits[unitID] = factoryMoveRadius[builderID]
 	end
 end
 
