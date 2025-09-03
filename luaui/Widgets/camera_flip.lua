aa8d4354155d3dfd359f92d306e007f167ff7e51
TheDujin <kevindujin@gmail.com>
2025-09-03 00:13:13 -0700

    Resolve Spring camera flip issues by adding pi when positive and subtracting pi when negative

--- luaui/Widgets/camera_flip.lua
+++ luaui/Widgets/camera_flip.lua
@@ -27,19 +27,27 @@ local function cameraFlipHandler()
 		return true
 	end
 
-	-- camera is spring cam
-	-- CardinalLock messes up rotation
-	local cardinalLock = Spring.GetConfigInt("CamSpringLockCardinalDirections")
-	local lockCorrection = 0
-	if cardinalLock == 1 then
-		-- This value must be larger than the cardinal lock width of 0.2
-		lockCorrection = 1/3
-	end
+	--[[ Adding pi when positive and subtracting pi when negative allows the camera flip
+	to work regardless of the state of cardinal lock. This is because the cardinal lock
+	formula in the Recoil engine is not actually symmetrical around the cardinal directions.
+	
+	For example, when the user experiences cardinal locking to halfpi radians, their
+	actual camera ry is required to be between (1.1) * halfpi and (1.3) * halfpi.
+	
+	Conversely, in order to cardinal lock to -3 * halfpi radians (which is functionally
+	equivalent to halfpi radians), the user's actual camera ry is required to be between
+	(-3.1) * halfpi radians and (-3.3) * halfpi radians.
+	
+	In other words, the cardinal lock behavior is asymmetrical with regards to the cardinal
+	directions, and the asymmetry is mirrored around 0.0f.
 
+	Feel free to investigate and/or fix `static float GetRotationWithCardinalLock(float rot)` in
+	`SpringController.cpp`.
+	]]
 	if camState.ry > 0 then
-		camState.ry = camState.ry - math.pi - lockCorrection
+		camState.ry = camState.ry + math.pi
 	else
-		camState.ry = camState.ry + math.pi + lockCorrection
+		camState.ry = camState.ry - math.pi
 	end
 
 	Spring.SetCameraState(camState, 0)
