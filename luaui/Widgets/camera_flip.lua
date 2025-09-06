5de4cbee465b971e2b8688a36a8c188b72a744a8
TheDujin <kevindujin@gmail.com>
2025-09-05 22:10:37 -0700

    Simplify the comment to make it more digestible and to-the-point

--- luaui/Widgets/camera_flip.lua
+++ luaui/Widgets/camera_flip.lua
@@ -29,33 +29,31 @@ local function cameraFlipHandler()
 		return true
 	end
 
-	--[[ Adding pi when positive and subtracting pi when negative allows the camera flip
-	to work regardless of the state of cardinal lock. This is because the cardinal lock
-	formula in the Recoil engine is not actually symmetrical around the cardinal directions.
-	
-	For example, when the user experiences cardinal locking to halfpi radians, their
-	actual camera ry is required to be between (1.1) * halfpi and (1.3) * halfpi.
-	
-	Conversely, in order to cardinal lock to -3 * halfpi radians (which is functionally
-	equivalent to halfpi radians), the user's actual camera ry is required to be between
-	(-3.1) * halfpi radians and (-3.3) * halfpi radians.
-	
-	In other words, the cardinal lock behavior is asymmetrical with regards to the cardinal
-	directions, and the asymmetry is mirrored around 0.0f.
+	--[[
+	When cardinal locking is disabled, the camera's ry (y rotation in radians) is treated literally.
+	In this case, simply adding (or subtracting) pi from the ry correctly flips the camera by 180 degrees.
 
-	Notably, behavior around 0.0f is also unusual. All values between (-0.3) * halfpi and
-	(0.3) * halfpi will cardinally lock to 0. This requires special handling when ry is
-	between -0.1 and 0.1 * halfpi.
+	When cardinal locking is enabled, the camera's ry is interpolated to keep a small part of the
+	range near each of the four cardinal directions "sticky" to the cardinal direction.
 
-	Feel free to investigate and/or fix `static float GetRotationWithCardinalLock(float rot)` in
-	`SpringController.cpp`.
+	However, the interpolation formula has problematic behavior when ry is near zero.
+	This requires two adjustments:
+
+	1. Adding pi always works when the initial ry is positive, and subtracting pi
+	always works when the initial ry is negative. This avoids crossing 0, which
+	would cause issues.
+	2. When the initial ry is between (-0.1)halfpi and (0.1)halfpi, adding
+	or subtracting pi does not generate a full 180 degree rotation. A small
+	correction is necessary to increase the rotation to 180 degrees.
+
+	The interpolation formula is at `static float GetRotationWithCardinalLock(float rot)`
+	in `SpringController.cpp`
 	]]
-
 	local cardinalLock = Spring.GetConfigInt("CamSpringLockCardinalDirections")
 	local lockCorrection = 0
 	if cardinalLock == 1 and math.abs(camState.ry) < 0.1 * halfpi then
-		-- Edge case around 0.0f: values with absolute value less than 0.1 * halfpi
-		-- should be corrected so that they have absolute value between 0.1 and 0.3 * halfpi.
+		-- Edge case around 0.0f: cameare ry's with absolute value less than 0.1 * halfpi
+		-- require a small increase in rotation magnitude so that they work with the cardinal locking formula.
 		lockCorrection = 0.1 * halfpi
 	end
 	if camState.ry > 0 then
