f81de74adaf67815f2ce142324abf5748d1a93c3
SuperKitowiec <superkitowiec@gmail.com>
2025-09-27 17:11:04 +0200

    Allow changing default filtering behavior in options

--- luaui/Widgets/cmd_skip_allied_upgrade.lua
+++ luaui/Widgets/cmd_skip_allied_upgrade.lua
@@ -3,7 +3,7 @@ local widget = widget ---@type Widget
 function widget:GetInfo()
 	return {
 		name = "Skip Allied Upgrade Toggle",
-		desc = "Adds a flag indicating if allied buildings should be ignored when upgrading mexes and geos + a command to toggle this flag.",
+		desc = "Adds a toggle indicating if allied buildings should be ignored when upgrading mexes and geos.",
 		author = "SuperKitowiec",
 		date = "August 27, 2025",
 		license = "GNU GPL, v2 or later",
@@ -13,15 +13,20 @@ function widget:GetInfo()
 	}
 end
 
-local skipAlliedBuildings = false
-
 local SkipAlliedUpgradeWidget = {}
+local toggleIsActive = false
+
+SkipAlliedUpgradeWidget.shouldFilterByDefault = false
+function SkipAlliedUpgradeWidget.setShouldFilterByDefault(value)
+	SkipAlliedUpgradeWidget.shouldFilterByDefault = value
+end
 
 ---@param spots table Array of spots to filter
 ---@param unitDefIdsToCheck table
 ---@return table Filtered array of spots
 function SkipAlliedUpgradeWidget.filterOutAlliedSpots(spots, unitDefIdsToCheck)
-	if not skipAlliedBuildings then
+	local shouldFilter = SkipAlliedUpgradeWidget.shouldFilterByDefault ~= toggleIsActive
+	if not shouldFilter then
 		return spots
 	end
 	local filteredSpots = {}
@@ -54,15 +59,26 @@ function SkipAlliedUpgradeWidget.filterOutAlliedSpots(spots, unitDefIdsToCheck)
 end
 
 function widget:Initialize()
-	skipAlliedBuildings = false
+	toggleIsActive = false
 	WG['skip_allied_upgrade'] = SkipAlliedUpgradeWidget
 
-	widgetHandler:AddAction("skip_allied_upgrade", function() skipAlliedBuildings = true end, nil, "p")
-	widgetHandler:AddAction("skip_allied_upgrade", function() skipAlliedBuildings = false end, nil, "r")
+	widgetHandler:AddAction("toggle_allied_upgrade", function() toggleIsActive = true end, nil, "p")
+	widgetHandler:AddAction("toggle_allied_upgrade", function() toggleIsActive = false end, nil, "r")
 end
 
 function widget:Shutdown()
-	widgetHandler:RemoveAction("skip_allied_upgrade")
+	widgetHandler:RemoveAction("toggle_allied_upgrade")
+end
+
+function widget:GetConfigData()
+	return {
+		shouldFilterByDefault = SkipAlliedUpgradeWidget.shouldFilterByDefault
+	}
+end
+
+function widget:SetConfigData(data)
+	data = data or { shouldFilterByDefault = false }
+	SkipAlliedUpgradeWidget.shouldFilterByDefault = data.shouldFilterByDefault
 end
 
 
