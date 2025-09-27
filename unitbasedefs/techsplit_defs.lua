1d8389c2c53692511ac40d224529e22b2b7c1124
NebuchadnezzarII <maxpliskin@outlook.com>
2025-09-27 01:01:45 -0600

    Update techsplit_defs.lua (#5858)
    Fix small typo from reformatting

--- unitbasedefs/techsplit_defs.lua
+++ unitbasedefs/techsplit_defs.lua
@@ -1046,8 +1046,6 @@ local function techsplitTweaks(name, uDef)
     -- Enhanced ships given seaplanes instead of static AA
     -- Tech Split Balance
 	if name == "corthud" then 
-		uDef.metalcost = 280
-		uDef.health = 1800
 		uDef.speed = 54
 		uDef.weapondefs.arm_ham.range = 300
 		uDef.weapondefs.arm_ham.predictboost = 0.8
@@ -1153,6 +1151,7 @@ local function techsplitTweaks(name, uDef)
 		uDef.weapondefs.arm_artillery.hightrajectory = 1
 		uDef.weapondefs.arm_artillery.range = 1050
 		uDef.weapondefs.arm_artillery.reloadtime = 3.05
+        uDef.weapondefs.arm_artillery.weaponvelocity = 500
 		uDef.weapondefs.arm_artillery.damage = {
 			default = 488,
 			subs = 163,
