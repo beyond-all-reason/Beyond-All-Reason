922bc0e6e7861a0c56a77619261f7d7405789814
efrec <quantumscrewdriver@gmail.com>
2025-10-03 16:28:54 -0400

    add cmd_ignore_self

---
+++ luaui/Widgets/cmd_ignore_self.lua
@@ -0,0 +1,51 @@
+local widget = widget ---@type Widget
+
+function widget:GetInfo()
+	return {
+		name    = "Ignore Self",
+		desc    = "Remove selection volume to prevent mouse hover over the only selected unit",
+		author  = "efrec",
+		date    = "2025",
+		license = "GNU GPL, v2 or later",
+		layer   = 0,
+		enabled = true,
+	}
+end
+
+local restoreVolumeData = {}
+local selectedID
+
+local function removeSelectionVolume(unitID)
+	local sx, sy, sz, ox, oy, oz, shape, cont, axis, ignoreHits = Spring.GetUnitSelectionVolumeData(unitID)
+
+	local rvd = restoreVolumeData
+	rvd[1] = sx
+	rvd[2] = sy
+	rvd[3] = sz
+	rvd[4] = ox
+	rvd[5] = oy
+	rvd[6] = oz
+	rvd[7] = shape
+	rvd[8] = cont
+	rvd[9] = axis
+
+	---@diagnostic disable-next-line -- just not correct?
+	Spring.SetUnitSelectionVolumeData(unitID, 0, 0, 0, ox, oy, oz, 1, cont, axis)
+end
+
+local function restoreSelectionVolume(unitID)
+	Spring.SetUnitSelectionVolumeData(unitID, unpack(restoreVolumeData))
+end
+
+function widget:SelectionChanged(selected)
+	if selectedID ~= selected[1] then
+		if selectedID ~= nil then
+			restoreSelectionVolume(selectedID)
+			selectedID = nil
+		end
+		if selected[1] ~= nil and selected[2] == nil then
+			removeSelectionVolume(selected[1])
+			selectedID = selected[1]
+		end
+	end
+end
