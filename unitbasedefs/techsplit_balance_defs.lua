ae69cbdcd07432aa2753a70da28521618075f607
Neb <maxpliskin@outlook.com>
2025-10-04 00:45:45 -0600

    Update techsplit_balance_defs.lua

--- unitbasedefs/techsplit_balance_defs.lua
+++ unitbasedefs/techsplit_balance_defs.lua
@@ -17,11 +17,11 @@ local function techsplit_balanceTweaks(name, uDef)
 	end
 
 	if name == "armshltx" then
-		uDef.buildoptions[10] = "armfboy"
+		uDef.buildoptions[7] = "armfboy"
 	end
 
 	if name == "corgant" then
-		uDef.buildoptions[10] = "corgol"
+		uDef.buildoptions[8] = "corgol"
 	end
 
 	if name == "coravp" then 
@@ -30,6 +30,62 @@ local function techsplit_balanceTweaks(name, uDef)
 
 	if name == "armalab" then
 		uDef.buildoptions[10] = ""
+	end
+
+	if name == "armhlt" then
+		uDef.health = 4640
+		uDef.metalcost = 535
+		uDef.energycost = 5700
+		uDef.buildtime = 13700
+		uDef.weapondefs.arm_laserh1.range = 750
+		uDef.weapondefs.arm_laserh1.reloadtime = 2.9
+		uDef.weapondefs.arm_laserh1.damage = {
+			commanders = 801,
+			default = 534,
+			vtol = 48,
+		}
+	end
+
+    if name == "armfhlt" then
+		uDef.health = 7600
+		uDef.metalcost = 570
+		uDef.energycost = 7520
+		uDef.buildtime = 11700
+		uDef.weapondefs.armfhlt_laser.range = 750
+		uDef.weapondefs.armfhlt_laser.reloadtime = 1.45
+		uDef.weapondefs.armfhlt_laser.damage = {
+			commanders = 414,
+			default = 290,
+			vtol = 71,
+		}
+	end
+
+	if name == "corhlt" then
+		uDef.health = 4640
+		uDef.metalcost = 580
+		uDef.energycost = 5700
+		uDef.buildtime = 13800
+		uDef.weapondefs.cor_laserh1.range = 750
+		uDef.weapondefs.cor_laserh1.reloadtime = 1.8
+		uDef.weapondefs.cor_laserh1.damage = {
+			commanders = 540,
+			default = 360,
+			vtol = 41,
+		}
+	end
+
+    if name == "corfhlt" then
+		uDef.health = 7340
+		uDef.metalcost = 580
+		uDef.energycost = 7520
+		uDef.buildtime = 13800
+		uDef.weapondefs.corfhlt_laser.range = 750
+		uDef.weapondefs.corfhlt_laser.reloadtime = 1.5
+		uDef.weapondefs.corfhlt_laser.damage = {
+			commanders = 482,
+			default = 319,
+			vtol = 61,
+		}
 	end
 
 	return uDef
