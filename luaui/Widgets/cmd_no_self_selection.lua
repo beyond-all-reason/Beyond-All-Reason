c9a66733db300b11c7682ea962406a0b08d0f701
efrec <quantumscrewdriver@gmail.com>
2025-10-13 13:57:11 -0400

    version 1.0

--- luaui/Widgets/cmd_no_self_selection.lua
+++ luaui/Widgets/cmd_no_self_selection.lua
@@ -5,7 +5,8 @@ function widget:GetInfo()
 		name    = "Ignore Self",
 		desc    = "Avoid self-targeting with default commands (e.g. Guard on self)",
 		author  = "efrec",
-		date    = "2025",
+		date    = "2025-10-13",
+		version = "v1.0",
 		license = "GNU GPL, v2 or later",
 		layer   = -1e9, -- before other w:DefaultCommand
 		enabled = true,
