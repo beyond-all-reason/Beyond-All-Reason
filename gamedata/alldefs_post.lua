efd7e2d2cb829ef838300d3b0a9d3f783939fae1
Mitvit <112669238+Mitvit@users.noreply.github.com>
2025-10-19 23:01:27 +0300

    Update alldefs_post.lua
    Changed looking at customparams.targetborderoverride to just looking at targetborder.

--- gamedata/alldefs_post.lua
+++ gamedata/alldefs_post.lua
@@ -1802,11 +1802,8 @@ function WeaponDef_Post(name, wDef)
 		end
 		----------------------------------------
 
-		--Use targetborderoverride in weapondef customparams to override this global setting
 		--Controls whether the weapon aims for the center or the edge of its target's collision volume. Clamped between -1.0 - target the far border, and 1.0 - target the near border.
-		if wDef.customparams and wDef.customparams.targetborderoverride ~= nil then
-			wDef.targetborder = tonumber(wDef.customparams.targetborderoverride)
-		else
+		if wDef.targetborder == nil then
 			wDef.targetborder = 1 --Aim for just inside the hitsphere
 		end
 
