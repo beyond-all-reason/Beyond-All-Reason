d0424b13ae56e18e31126a6a4c01affb42a644ae
Daniel Harvey <keithdanielharvey@gmail.com>
2025-10-24 12:43:00 -0600

    unit transfer fixes
    I definitely lost this work moving between branches/computers/or during a rebase squash/reorder gone wrong, but yeah second time I've created this function

--- common/luaUtilities/team_transfer/unit_transfer_shared.lua
+++ common/luaUtilities/team_transfer/unit_transfer_shared.lua
@@ -122,6 +122,50 @@ function Shared.DeserializePolicy(serialized, senderId, receiverId)
   })
 end
 
+function Shared.GetModeUnitTypes(mode)
+  if mode == SharedEnums.UnitSharingMode.Disabled then
+    return {}
+  end
+
+  if mode == SharedEnums.UnitSharingMode.Enabled then
+    return {
+      SharedEnums.UnitType.Combat,
+      SharedEnums.UnitType.Economic,
+      SharedEnums.UnitType.Utility,
+      SharedEnums.UnitType.T2Constructor
+    }
+  end
+
+  if mode == SharedEnums.UnitSharingMode.CombatUnits then
+    return {SharedEnums.UnitType.Combat}
+  end
+
+  if mode == SharedEnums.UnitSharingMode.Economic then
+    return {SharedEnums.UnitType.Economic, SharedEnums.UnitType.T2Constructor}
+  end
+
+  if mode == SharedEnums.UnitSharingMode.EconomicPlusBuildings then
+    return {SharedEnums.UnitType.Economic, SharedEnums.UnitType.T2Constructor, SharedEnums.UnitType.Utility}
+  end
+
+  if mode == SharedEnums.UnitSharingMode.T2Cons then
+    return {SharedEnums.UnitType.T2Constructor}
+  end
+
+  if mode == SharedEnums.UnitSharingMode.CombatT2Cons then
+    return {SharedEnums.UnitType.Combat, SharedEnums.UnitType.T2Constructor}
+  end
+
+  error("Unknown mode: " .. mode)
+end
+
+local function UnitTypeMatchesMode(unitDef, mode)
+  local unitType = UnitSharingCategories.classifyUnitDef(unitDef)
+  local modeUnitTypes = Shared.GetModeUnitTypes(mode)
+  return table.contains(modeUnitTypes, unitType)
+end
+
+
 ---Get globals published by the module
 ---@param unitDef table
 ---@param mode string
@@ -138,31 +182,7 @@ local function EvaluateUnitForSharing(unitDef, mode)
     return true
   end
 
-  local unitType = UnitSharingCategories.classifyUnitDef(unitDef)
-
-  if mode == SharedEnums.UnitSharingMode.CombatUnits then
-    return unitType == SharedEnums.UnitType.Combat
-  end
-
-  if mode == SharedEnums.UnitSharingMode.Economic then
-    return unitType == SharedEnums.UnitType.Economic or unitType == SharedEnums.UnitType.T2Constructor
-  end
-
-  if mode == SharedEnums.UnitSharingMode.EconomicPlusBuildings then
-    return unitType == SharedEnums.UnitType.Economic or unitType == SharedEnums.UnitType.T2Constructor or
-    unitType == SharedEnums.UnitType.Utility
-  end
-
-  if mode == SharedEnums.UnitSharingMode.T2Cons then
-    return unitType == SharedEnums.UnitType.T2Constructor
-  end
-
-  if mode == SharedEnums.UnitSharingMode.CombatT2Cons then
-    return unitType == SharedEnums.UnitType.Combat or unitType == SharedEnums.UnitType.T2Constructor
-  end
-
-  Spring.Log("unit_transfer_shared", LOG.ERROR, "EvaluateUnitForSharing: unknown mode =", mode)
-  return false
+  return UnitTypeMatchesMode(unitDef, mode)
 end
 
 -- Allowed UnitDefID cache per mode for fast validation
