05b443ed6051cdfb62606bad872dc029f8dd43f6
David Norton <david.k.norton@gmail.com>
2025-10-27 21:08:00 -0500

    switching the 'stun' approach away from using EMP to using its own waitlist

--- luarules/gadgets/game_stun_units_after_sharing.lua
+++ luarules/gadgets/game_stun_units_after_sharing.lua
@@ -10,37 +10,30 @@ function gadget:GetInfo()
         enabled = true
     }
 end
-
--- Notes: Used paralyzeDamage because it was already there and easy to use. 
--- This could be expanded to instead use its own equivalent so as to allow for different UI representation of the unit-transfer mechanic
-
 -- Thanks to  Chronopolize, Hobo Joe, Chronographer,  Seth dGamre, and sprunk for providing suggestions
 
 if not gadgetHandler:IsSyncedCode() then
 	return false
 end
 
--- return early if not enabled
--- if (Spring.GetModOption().stun_unit_after_sharing == "off" or stun_units_after_transfer_duration_seconds <= 0) then 
---     return true
--- end
+--return early if not enabled
+if (Spring.GetModOption().stun_unit_after_sharing == "off" or stun_units_after_transfer_duration_seconds <= 0) then 
+    return true
+end
 
 local spGetUnitHealth = Spring.GetUnitHealth
 local spSetUnitHealth = Spring.SetUnitHealth
+local currentGameFrame = Spring.GetGameFrame()
 
-
+local transferedStunnedList = {}
 
 function gadget:AllowUnitTransfer(unitID, _, _, _, _)
     Spring.Echo("stun_unit_after_sharing: " .. tostring(Spring.GetModOptions().stun_unit_after_sharing))
     Spring.Echo("stun_units_after_transfer_duration_seconds: " .. tostring(Spring.GetModOptions().stun_units_after_transfer_duration_seconds))
     -- allow the unit to transfer, but stun the unit as the transfer happens
 	local inputStunSeconds = Spring.GetModOptions().stun_units_after_transfer_duration_seconds
-    local health, maxHealth, paralyzeDamage, captureProgress = spGetUnitHealth(unitID)
-	local transferParalyzeDamage = maxHealth * (1 + (inputStunSeconds / Game.paralyzeDeclineRate))
+    -- that is, we're going to store the unit id with the future game frame that the unit will become available
+    local gameFrameThatUnitWillBeAvailable = currentGameFrame + (inputStunSeconds * Game.gameSpeed);
 
-    --if the unit is already paralyzed more than the transfer stun, take the greater of the two
-	local updatedParalyzeDamage = math.max(paralyzeDamage, transferParalyzeDamage)
-
-	spSetUnitHealth(unitID, {paralyze = updatedParalyzeDamage})
     return true
 end
