c5b4c8b768c138231233bd93bb30990fcd51fbda
TheChronographer <160436719+TheChronographer@users.noreply.github.com>
2025-11-04 02:37:45 +1100

    Automaticially unwait fully healed units (#6066)
    ### Work done
    Added a periodic check to unwait resurrected units when they are fully
    healed, or stopped being repaired. This is the same check as is used to
    add new units to autogroups.
    
    There is a slight chance of a unit being unwaited early if the checked
    frame lines up in a pause between the builder resurrecting and the
    builder repairing.
    
    Units that are builders are also not set to wait at all so they
    correctly appear as idle.
    
    Before:
    <img width="2058" height="1053" alt="image"
    src="https://github.com/user-attachments/assets/352dca97-9713-4542-a541-704880a9d262"
    />
    
    AFfter: 
    <img width="1487" height="982" alt="image"
    src="https://github.com/user-attachments/assets/8922c273-6e48-485d-8f00-521024a85802"
    />

--- luarules/gadgets/unit_resurrected.lua
+++ luarules/gadgets/unit_resurrected.lua
@@ -4,53 +4,98 @@ function gadget:GetInfo()
     return {
         name      = "resurrected param",
         desc      = "marks resurrected units as resurrected.",
-        author    = "Floris",
-        date      = "25 oct 2015",
+        author    = "Floris, Chronographer",
+        date      = "4 November 2025",
         license   = "GNU GPL, v2 or later",
         layer     = 5,
         enabled   = true
     }
 end
 
+if (not gadgetHandler:IsSyncedCode()) then
+  return false
+end
+
+local spGetUnitHealth = Spring.GetUnitHealth
+local spGetGameFrame = Spring.GetGameFrame
+local spGiveOrderToUnit = Spring.GiveOrderToUnit
+local spGetUnitCurrentCommand = Spring.GetUnitCurrentCommand
 local CMD_WAIT = CMD.WAIT
 
-if (gadgetHandler:IsSyncedCode()) then
+local canResurrect = {}
+local isBuilding = {}
+local isBuilder = {}
 
-    local canResurrect = {}
-	local isBuilding = {}
-    for unitDefID, unitDef in pairs(UnitDefs) do
-        if unitDef.canResurrect then
-            canResurrect[unitDefID] = true
-        end
-		if unitDef.isBuilding then
-			isBuilding[unitDefID] = true
-		end
+local createdFrame = {}
+local toBeUnWaited = {}
+local prevHealth = {}
+local currentCmd = {}
+
+for unitDefID, unitDef in pairs(UnitDefs) do
+    if unitDef.canResurrect then
+        canResurrect[unitDefID] = true
     end
+	if unitDef.isBuilding then
+		isBuilding[unitDefID] = true
+	end
+	if unitDef.isBuilder then
+		isBuilder[unitDefID] = true
+	end
+end
 
-    -- detect resurrected units here
-	function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-		if builderID and canResurrect[Spring.GetUnitDefID(builderID)] then
-			if not Spring.Utilities.Gametype.IsScavengers() then -- FIXME: Scavengers have constructors which can also resurrect, which f***s over the whole thing.
-				local rezRulesParam = Spring.GetUnitRulesParam(unitID, "resurrected")
-				if rezRulesParam == nil then
-					Spring.SetUnitRulesParam(unitID, "resurrected", 1, {inlos=true})
-				end
-				if not isBuilding[unitDefID] then
-					Spring.GiveOrderToUnit(unitID, CMD_WAIT, {}, 0)
+-- detect resurrected units here
+function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+	if builderID and canResurrect[Spring.GetUnitDefID(builderID)] then
+		if not Spring.Utilities.Gametype.IsScavengers() then -- FIXME: Scavengers have constructors which can also resurrect, which f***s over the whole thing.
+			local rezRulesParam = Spring.GetUnitRulesParam(unitID, "resurrected")
+			if rezRulesParam == nil then
+				Spring.SetUnitRulesParam(unitID, "resurrected", 1, {inlos=true})
+			end
+			if (not isBuilding[unitDefID]) and (not isBuilder[unitDefID]) then
+				createdFrame[unitID] = spGetGameFrame()
+				toBeUnWaited[unitID] = true
+				prevHealth[unitID] = 0
+				spGiveOrderToUnit(unitID, CMD_WAIT, {}, 0)
+			end
+		end
+		Spring.SetUnitHealth(unitID, spGetUnitHealth(unitID) * 0.05)
+	end
+	-- See: https://github.com/beyond-all-reason/spring/pull/471
+	-- if builderID and Spring.GetUnitCurrentCommand(builderID) == CMD.RESURRECT then
+	--	Spring.SetUnitHealth(unitID, Spring.GetUnitHealth(unitID) * 0.05)
+	-- end
+	-- this code is buggy.
+	-- Spring.GetUnitCurrentCommand(builderID) does not return CMD.RESURRECT in all cases
+	-- Switch to using same rule as the halo visual
+	-- which does have the limitation that *any* unit created by a builder that can rez
+	-- will be created at 5% HP
+	-- currently not an issue with BAR's current units, but is a limitation on any
+	-- future multi-purpose rez unit
+end
+
+function gadget:GameFrame(n)
+	if n % 10 == 0 then
+		if next(toBeUnWaited) ~= nil then
+			for unitID, check in pairs(toBeUnWaited) do
+				local health = spGetUnitHealth(unitID)
+				if health <= prevHealth[unitID] then -- stopped healing
+					createdFrame[unitID] = nil
+					toBeUnWaited[unitID] = nil
+					prevHealth[unitID] = nil
+					currentCmd = spGetUnitCurrentCommand(unitID, 1)
+					if currentCmd and currentCmd == CMD_WAIT then
+						spGiveOrderToUnit(unitID, CMD_WAIT, {}, 0)
+					end
+				else
+					prevHealth[unitID] = health
 				end
 			end
-			Spring.SetUnitHealth(unitID, Spring.GetUnitHealth(unitID) * 0.05)
 		end
-		-- See: https://github.com/beyond-all-reason/spring/pull/471
-		-- if builderID and Spring.GetUnitCurrentCommand(builderID) == CMD.RESURRECT then
-		--	Spring.SetUnitHealth(unitID, Spring.GetUnitHealth(unitID) * 0.05)
-		-- end
-		-- this code is buggy.
-		-- Spring.GetUnitCurrentCommand(builderID) does not return CMD.RESURRECT in all cases
-		-- Switch to using same rule as the halo visual
-		-- which does have the limitation that *any* unit created by a builder that can rez
-		-- will be created at 5% HP
-		-- currently not an issue with BAR's current units, but is a limitation on any
-		-- future multi-purpose rez unit
 	end
+end
+
+function gadget:UnitDestroyed(unitID, unitDefID, teamID)
+	createdFrame[unitID] = nil
+	toBeUnWaited[unitID] = nil
+	prevHealth[unitID] = nil
 end
