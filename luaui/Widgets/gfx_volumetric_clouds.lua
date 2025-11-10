beyond-all-reason/Beyond-All-Reason

commit a8664cd3cde14aa74524cb5a5decf6f0658bc43d

widgets: added lots of localization (#6110)

luaui/Widgets/gfx_volumetric_clouds.lua
@@ -14,6 +14,12 @@ function widget:GetInfo()
   }
 end
 
+
+-- Localized functions for performance
+
+-- Localized Spring API for performance
+local spEcho = Spring.Echo
+
 local enabled = true
 
 local opacityMult = 1
@@ -59,7 +65,7 @@ local function convertAltitude(input, default)
 		local percent = input:match("(%d+)%%")
 		result = gnd_max * (percent / 100)
 	end
-	--Spring.Echo(result)
+	--spEcho(result)
 	return result
 end
 
@@ -111,7 +117,7 @@ local spGetCameraPosition    = Spring.GetCameraPosition
 local spGetWind              = Spring.GetWind
 
 local function spEcho(words)
-	Spring.Echo('<Volumetric Clouds> '..words)
+	spEcho('<Volumetric Clouds> '..words)
 end
 
 --------------------------------------------------------------------------------

commit 2a32c5992f4c70471fcfe2be853edcf9346d4dfb

Fix recursive call to spEcho which crashes headless (#6139)
Introduced in a8664cd3cde14aa74524cb5a5decf6f0658bc43d

Calling spEcho would lead to infinite recusion, but this echo happens when removing due to headless.

luaui/Widgets/gfx_volumetric_clouds.lua
@@ -14,10 +14,6 @@ function widget:GetInfo()
   }
 end
 
-
--- Localized Spring API for performance
-local spEcho = Spring.Echo
-
 local enabled = true
 
 local opacityMult = 1
@@ -113,10 +109,7 @@ local glTexture              = gl.Texture
 local LuaShader 			 = gl.LuaShader
 local spGetCameraPosition    = Spring.GetCameraPosition
 local spGetWind              = Spring.GetWind
-
-local function spEcho(words)
-	spEcho('<Volumetric Clouds> '..words)
-end
+local spEcho				 = Spring.Echo
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
@@ -178,7 +171,7 @@ function widget:ViewResize()
 
 
 	if depthTexture == nil or fogTexture == nil then
-		spEcho("Removing fog widget, bad depth texture")
+		spEcho("<Volumetric Clouds> Removing fog widget, bad depth texture")
 		widgetHandler:RemoveWidget()
 	end
 end
@@ -568,7 +561,7 @@ local function init()
 		}, "Volumetric Clouds Depth Shader")
 
 		if not depthShader:Initialize() then
-			spEcho("Bad shader, reverting to non-GLSL widget.")
+			spEcho("<Volumetric Clouds> Bad shader, reverting to non-GLSL widget.")
 			enabled = false
 		end
 	end