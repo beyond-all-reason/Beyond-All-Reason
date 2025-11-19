1be5bede50ab924158a3cef3942d8d6214fe68ee
avezey-ci <86317133+avezey-ci@users.noreply.github.com>
2025-11-19 15:28:58 -0700

    Fix headless docker build dependencies

--- tools/headless_testing/download-engine.sh
+++ tools/headless_testing/download-engine.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -euo pipefail
+
 mkdir -p "$ENGINE_DESTINATION"
 
 TEMP_FILE=$(mktemp --suffix=.7z)
 
-curl -L "$ENGINE_URL" -o "$TEMP_FILE" && 7z x "$TEMP_FILE" -o"$ENGINE_DESTINATION" && rm -f temp.7z
+curl -L "$ENGINE_URL" -o "$TEMP_FILE"
+7z x "$TEMP_FILE" -o"$ENGINE_DESTINATION"
+rm -f "$TEMP_FILE"
