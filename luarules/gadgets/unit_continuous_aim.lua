282c7c53e4f2ae023708b2e4ad5b1a1b2ab50d76
efrec <quantumscrewdriver@gmail.com>
2025-11-24 19:03:56 -0500

    mostly replaced unit_continuous_aim.lua for dynamic maxunits
    This adds support for give/take/capture and dynamic team maxunits. Non-player teams get a very large reference value for their maxunits, making them less prone to reaimTime penalties and keeping a more smooth difficulty scaling as they produce more units. Player teams scale vs. a reference amount, 2400 (2000 maxunits + 400 default spam rating for padding), so unit control is never noticeably different due to game mode.
    
    It also allows spam score to decrease over time, treating this as a performance tradeoff rather than a penalty for making units, and spreads out the reaimTime penalty to all unitdefs on that team, not only the source "spam-prone" unit.
    
    Finally, all units are "spammable" to some degree, now, and all values have been adjusted to account for this.

--- luarules/gadgets/unit_continuous_aim.lua
+++ luarules/gadgets/unit_continuous_aim.lua
@@ -7,8 +7,8 @@ function gadget:GetInfo()
 		author = "Doo, Beherith",
 		date = "April 2018",
 		license = "GNU GPL, v2 or later",
-		layer = 0,
-		enabled = true, -- When we will move on 105 :)
+		layer = 0, -- after game_dynamic_maxunits for accurate unit caps
+		enabled = tonumber(Engine.versionMajor) >= 105,
 	}
 end
 
@@ -16,239 +16,101 @@ if not gadgetHandler:IsSyncedCode() then
 	return
 end
 
+local reaimTimeMax = 0.5 ---@type number Caps dynamic reaim times and filters unitDefs. In seconds.
+local spamRatingBase = 400 ---@type integer Each team's reaimtime increases by 1 each X units made.
+local spamRatingMax = 1000 ---@type integer Sets the minimum performance penalty for spammed units.
+local unitCapDefault = 2400 ---@type integer The per-team unit cap we assume in these spam ratings.
+
+local math_floor = math.floor
+local math_max = math.max
+local math_clamp = math.clamp
+
+local spGetTeamLuaAI = Spring.GetTeamLuaAI
+local spGetTeamMaxUnits = Spring.GetTeamMaxUnits
 local spSetUnitWeaponState = Spring.SetUnitWeaponState
-local tableCopy = table.copy
 
+local reaimFramesMax = math.round(reaimTimeMax * Game.gameSpeed)
+local unitCapReference = math_max(Spring.GetModOptions().maxunits, unitCapDefault)
+local unitCapNonPlayer = math_max(6000, unitCapReference) -- for one team vs. many
 
-local convertedUnitsNames = {
-	-- value is reaimtime in frames, engine default is 15
-	['armfav'] = 3,
-	['armbeamer'] = 3,
-	['armpw'] = 2,
-	['armpwt4'] = 2,
-	['armflea'] = 2,
-	['armrock'] = 2,
-	['armham'] = 2,
-	['armwar'] = 6,
-	['armjeth'] = 2,
-	['corfav'] = 3,
-	['corak'] = 2,
-	['corthud'] = 2,
-	['corstorm'] = 2,
-	['corcrash'] = 5,
-	['legkark'] = 2,
-	['corkark'] = 2,
-	['cordeadeye'] =2,
-	['armsnipe'] = 2,
-	['armfido'] = 3,
-	['armfboy'] = 2,
-	['armfast'] = 2,
-	['armamph'] = 3,
-	['armmav'] = 2,
-	['armspid'] = 3,
-	['armsptk'] = 5,
-	['armzeus'] = 3,
-	['coramph'] = 3,
-	['corcan'] = 2,
-	['corhrk'] = 5,
-	['cormando'] = 2,
-	['cormort'] = 2,
-	['corpyro'] = 2,
-	['cortermite'] = 2,
-	['armraz'] = 1,
-	['armmar'] = 3,
-	['armbanth'] = 1,
-	['corkorg'] = 1,
-	['armvang'] = 3,
-	['armcrus'] = 5,
-	['corsala'] = 6,
-	['corsiegebreaker'] = 5,
-	['legerailtank'] = 9,
+local unitReaimTimes = {}
+local unitSpamRating = {}
+local unitWeaponCount = {}
 
-	-- the following units get a faster reaimtime to counteract their turret acceleration
-	['armthor'] = 4,
-	['armflash'] = 6,
-	['corgator'] = 6,
-	['armdecade'] = 6,
-	['coresupp'] = 6,
-	['corhlt'] = 5,
-	['corfhlt'] = 5,
-	['cordoom'] = 5,
-	['corshiva'] = 5,
-	['corcat'] = 5,
-	['corkarg'] = 5,
-	['corbhmth'] = 5,
-	['armguard'] = 5,
-	['armamb'] = 5,
-	['corpun'] = 5,
-	['cortoast'] = 5,
-	['corbats'] = 5,
-	['corblackhy'] = 6,
-	['corscreamer'] = 5,
-	['corcom'] = 5,
-	['armcom'] = 5,
-	['cordecom'] = 5,
-	['armdecom'] = 5,
-	['legcom'] = 5,
-	['legdecom'] = 5,
-	['legcomlvl2'] = 5,
-	['legcomlvl3'] = 5,
-	['legcomlvl4'] = 5,
-	['legcomlvl5'] = 5,
-	['legcomlvl6'] = 5,
-	['legcomlvl7'] = 5,
-	['legcomlvl8'] = 5,
-	['legcomlvl9'] = 5,
-	['legcomlvl10'] = 5,
-	['legah'] = 5,
-	['legbal'] = 5,
-	['legbastion'] = 5,
-	['legcen'] = 3,
-	['legfloat'] = 5,
-	['leggat'] = 5,
-	['leggob'] = 5,
-	['leginc'] = 1,
-	['cordemon'] = 6,
-	['corcrwh'] = 7,
-	['leglob'] = 5,
-	['legmos'] = 5,
-	['leghades'] = 5,
-	['leghelios'] = 5,
-	['legheavydrone'] = 5,
-	['legkeres'] = 5,
-	['legrail'] = 5,
-	['legbar'] = 5,
-	['legcomoff'] = 5,
-	['legcomt2off'] = 5,
-	['legcomt2com'] = 5,
-	['legstr'] = 3,
-	['legamph'] = 4,
-	['legbart'] = 5,
-	['legmrv'] = 5,
-	['legsco'] = 5,
-	['leegmech'] = 5,
-	['legionnaire'] = 5,
-	['legafigdef'] = 5,
-	['legvenator'] = 5,
-    ['legmed'] = 5,
-	['legaskirmtank'] = 5,
-	['legaheattank'] = 3,
-	['legeheatraymech'] = 1,
-	['legtriariusdrone'] = 1,
-	['legnavydestro'] = 4,
-	['legeheatraymech_old'] = 1,
-	['legbunk'] = 3,
-	['legrwall'] = 4,
-	['legjav'] = 1,
-	['legeshotgunmech'] = 3,
-	['legehovertank'] = 4,
-	['armanavaldefturret'] = 4,
-}
---add entries for scavboss
-local scavengerBossV4Table = {'scavengerbossv4_veryeasy', 'scavengerbossv4_easy', 'scavengerbossv4_normal', 'scavengerbossv4_hard', 'scavengerbossv4_veryhard', 'scavengerbossv4_epic',
- 'scavengerbossv4_veryeasy_scav', 'scavengerbossv4_easy_scav', 'scavengerbossv4_normal_scav', 'scavengerbossv4_hard_scav', 'scavengerbossv4_veryhard_scav', 'scavengerbossv4_epic_scav'}
-for _, name in pairs(scavengerBossV4Table) do
-	convertedUnitsNames[name] = 4
-end
---if Spring.GetModOptions().emprework then
-	--convertedUnitsNames['armdfly'] = 50
---end
--- convert unitname -> unitDefID
-local convertedUnits = {}
-for name, params in pairs(convertedUnitsNames) do
-	if UnitDefNames[name] then
-		convertedUnits[UnitDefNames[name].id] = params
-	end
-end
-convertedUnitsNames = nil
-
-
-local spamUnitsTeamsNames = { --{unitDefID = {teamID = totalcreated,...}}
-	['armpw'] = {},
-	['armflea'] = {},
-	['armfav'] = {},
-	['corak'] = {},
-	['corfav'] = {},
-}
--- convert unitname -> unitDefID
-local spamUnitsTeams = {}
-for name, params in pairs(spamUnitsTeamsNames) do
-	if UnitDefNames[name] then
-		spamUnitsTeams[UnitDefNames[name].id] = params
-	end
-end
-spamUnitsTeamsNames = nil
-
-
-local spamUnitsTeamsReaimTimes = {} --{unitDefID = {teamID = currentReAimTime,...}}
-
-
--- for every spamThreshold'th spammable unit type built by this team, increase reaimtime by 1 for that team
-local spamThreshold = 100
-local maxReAimTime = 15
-
--- add for scavengers copies
-local convertedUnitsCopy = tableCopy(convertedUnits)
-for id, v in pairs(convertedUnitsCopy) do
-	if UnitDefNames[UnitDefs[id].name..'_scav'] then
-		convertedUnits[UnitDefNames[UnitDefs[id].name..'_scav'].id] = v
-	end
-end
-
-local spamUnitsTeamsCopy = tableCopy(spamUnitsTeams)
-for id,v in pairs(spamUnitsTeamsCopy) do
-	if UnitDefNames[UnitDefs[id].name..'_scav'] then
-		spamUnitsTeams[UnitDefNames[UnitDefs[id].name..'_scav'].id] = {}
-	end
-end
-
-for unitDefID, _ in pairs(spamUnitsTeams) do
-	spamUnitsTeamsReaimTimes[unitDefID] = {}
-end
-
-local unitWeapons = {}
-for unitDefID, _ in pairs(convertedUnits) do
-	local unitDef = UnitDefs[unitDefID]
-	if unitDef then
-		local weapons = unitDef.weapons
-		if #weapons > 0 then
-			unitWeapons[unitDefID] = {}
-			for id, _ in pairs(weapons) do
-				unitWeapons[unitDefID][id] = true	-- no need to store weapondefid
+-- Unit scripts have to manually check in script if it is at the desired heading.
+-- See: https://springrts.com/phpbb/viewtopic.php?t=36654
+for unitDefID, unitDef in pairs(UnitDefs) do
+	if #unitDef.weapons >= 1 then
+		if tonumber(unitDef.customParams.continuous_aim_time) then
+			local reaimTime = tonumber(unitDef.customParams.continuous_aim_time)
+			local reaimFrames = math_max(math.round(reaimTime * Game.gameSpeed), 1)
+			if reaimFrames < reaimFramesMax then
+				unitReaimTimes[unitDefID] = reaimFrames
+				unitWeaponCount[unitDefID] = #unitDef.weapons
 			end
-		else
-			-- units with no weapons shouldnt even be here
-			convertedUnits[unitDefID] = nil
+		end
+		local spamCount = tonumber(unitDef.customParams.continuous_aim_spam) or spamRatingBase
+		local spamScore = 1 / math.clamp(spamCount, 1, spamRatingMax) -- as reaimTime per unit
+		unitSpamRating[unitDefID] = spamScore * unitCapDefault -- as reaimTime/unit/max units
+	end
+end
+
+local teamMaxUnits = {}
+local teamReaimTimes = {}
+
+local function isNonPlayerEnemyTeam(teamID)
+	local luaAI = spGetTeamLuaAI(teamID)
+	return luaAI and (luaAI:find("Scavenger") or luaAI:find("Raptor")) -- seems not-clean tbh
+end
+
+local function getTeamMaxUnits(teamID)
+	local actual = spGetTeamMaxUnits(teamID)
+	local reference = isNonPlayerEnemyTeam(teamID) and unitCapNonPlayer or unitCapReference
+	return actual and (math_max(actual, reference) + reference) * 0.5 or reference
+end
+
+function gadget:MetaUnitAdded(unitID, unitDefID, unitTeam)
+	local unitReaimTime = unitReaimTimes[unitDefID]
+
+	if unitReaimTime then
+		local teamReaimTime = teamReaimTimes[unitTeam]
+		local addSpamRating = unitSpamRating[unitDefID] / teamMaxUnits[unitTeam]
+
+		teamReaimTime = teamReaimTime + addSpamRating
+		teamReaimTimes[unitTeam] = teamReaimTime
+		unitReaimTime = math_clamp(math_floor(unitReaimTime + teamReaimTime), 1, reaimFramesMax)
+
+		for weaponNum = 1, unitWeaponCount[unitDefID] do
+			-- NOTE: this will prevent unit from firing if it does not IMMEDIATELY return from AimWeapon (no sleeps, not wait for turns!)
+			-- So you have to manually check in script if it is at the desired heading
+			spSetUnitWeaponState(unitID, weaponNum, "reaimTime", unitReaimTime)
 		end
 	end
 end
 
-function gadget:UnitCreated(unitID, unitDefID, teamID)
-	if convertedUnits[unitDefID] then
-		local currentReaimTime = convertedUnits[unitDefID]
+function gadget:MetaUnitRemoved(unitID, unitDefID, teamID)
+	if unitSpamRating[unitDefID] then
+		teamReaimTimes[teamID] = teamReaimTimes[teamID] - unitSpamRating[unitDefID]
+	end
+end
 
-		if spamUnitsTeams[unitDefID] then
-			if not spamUnitsTeams[unitDefID][teamID] then
-				-- initialize for this team at base defaults
-				spamUnitsTeams[unitDefID][teamID] = 1
-				spamUnitsTeamsReaimTimes[unitDefID][teamID] = convertedUnits[unitDefID]
-			else
-				local spamCount = spamUnitsTeams[unitDefID][teamID] + 1
-				spamUnitsTeams[unitDefID][teamID] = spamCount
-				currentReaimTime = spamUnitsTeamsReaimTimes[unitDefID][teamID]
-				if spamCount % spamThreshold == 0 and currentReaimTime < maxReAimTime then
-					spamUnitsTeamsReaimTimes[unitDefID][teamID] = currentReaimTime + 1
-					--Spring.Echo("Unit type", unitDefID,'has been built', spamCount, 'times by team', teamID,'increasing reaimtime to ', currentReaimTime + 1)
-				end
-			end
-		end
-		if currentReaimTime < 15 then
-			for id, _ in pairs(unitWeapons[unitDefID]) do
-				-- NOTE: this will prevent unit from firing if it does not IMMEDIATELY return from AimWeapon (no sleeps, not wait for turns!)
-				-- So you have to manually check in script if it is at the desired heading
-				-- https://springrts.com/phpbb/viewtopic.php?t=36654
-				spSetUnitWeaponState(unitID, id, "reaimTime", currentReaimTime)
-			end
+function gadget:TeamDied(deadTeamID)
+	-- For now, this is our only source of TransferTeamMaxUnits after init.
+	for _, teamID in pairs(Spring.GetTeamList() or {}) do
+		teamMaxUnits[teamID] = getTeamMaxUnits(teamID)
+	end
+end
+
+function gadget:Initialize()
+	teamMaxUnits = {}
+	teamReaimTimes = {}
+
+	for _, teamID in pairs(Spring.GetTeamList() or {}) do
+		teamMaxUnits[teamID] = getTeamMaxUnits(teamID)
+		teamReaimTimes[teamID] = 0
+
+		for _, unitID in pairs(Spring.GetTeamUnits(teamID) or {}) do
+			gadget:MetaUnitAdded(unitID, Spring.GetUnitDefID(unitID), teamID)
 		end
 	end
 end
