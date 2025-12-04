ed8b15caad5acbb4d9ad468153c8197fb6ed382b
BradfordCB <47071970+BradfordCB@users.noreply.github.com>
2025-12-03 17:20:38 -0700

    nano,aa,mines,eyes

--- weapons/mine_heavy.lua
+++ weapons/mine_heavy.lua
@@ -1,19 +1,19 @@
 return {
 	mine_heavy = {
-		areaofeffect = 330,
+		areaofeffect = 530,--330
 		craterboost = 0,
 		cratermult = 0,
-		edgeeffectiveness = 0.5,
+		edgeeffectiveness = 0.75,
 		explosiongenerator = "custom:genericunitexplosion-large-dirty",
-		impulsefactor = 0.8,
+		impulsefactor = 4.8,--0.8
 		name = "HeavyMine",
-		range = 480,
+		range = 580,--480
 		reloadtime = 3.6,
 		soundhit = "xplolrg3",
 		soundstart = "largegun",
-		weaponvelocity = 250,
+		weaponvelocity = 450,--250
 		damage = {
-			default = 3000,
+			default = 22202,
 			mines = 1,
 		},
 	},
