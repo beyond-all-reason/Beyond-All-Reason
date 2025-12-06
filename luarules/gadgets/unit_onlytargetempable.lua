af77172b072264f2c3692958c821c7aee23340fa
emb3rn <embern@protonmail.com>
2025-12-06 22:23:12 +0000

    remove pair iteration and set every unitdef to false

--- luarules/gadgets/unit_onlytargetempable.lua
+++ luarules/gadgets/unit_onlytargetempable.lua
@@ -3,22 +3,24 @@ local gadget = gadget ---@type Gadget
 function gadget:GetInfo()
 	return {
 		name	= "Only Target Emp-able units",
-		desc	= "Prevents paralyzer units attacking anything other than empable units",
-		author	= "Floris",
-		date	= "February 2018",
-		license	= "GNU GPL, v2 or later",
-		layer	= 0,
-		enabled	= true,
+		desc    = "Prevents paralyzer units attacking anything other than empable units",
+		author  = "Floris",
+		date    = "February 2018",
+		license = "GNU GPL, v2 or later",
+		layer   = 0,
+		enabled = true,
 	}
 end
 
 local empUnits = {}
 local unEmpableUnits = {}
-for udid, unitDef in pairs(UnitDefs) do
+for udid = 1, #UnitDefs do
+	local unitDef = UnitDefs[udid]
 	local weapons = unitDef.weapons
-	for i=1, #weapons do
+	empUnits[udid] = false
+	for i = 1, #weapons do
 		if WeaponDefs[weapons[i].weaponDef].paralyzer then
-			empUnits[udid] = true 
+			empUnits[udid] = true
 		else
 			empUnits[udid] = false
 			break
@@ -30,19 +32,19 @@ for udid, unitDef in pairs(UnitDefs) do
 end
 
 function gadget:Initialize()
-    gadgetHandler:RegisterAllowCommand(CMD.ATTACK)
+	gadgetHandler:RegisterAllowCommand(CMD.ATTACK)
 end
 
 function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions, cmdTag, playerID, fromSynced, fromLua)
 	-- accepts: CMD.ATTACK
 	if empUnits[unitDefID]
-	and cmdParams[2] == nil
-	and type(cmdParams[1]) == 'number'
-	and UnitDefs[Spring.GetUnitDefID(cmdParams[1])] ~= nil then
-		if unEmpableUnits[Spring.GetUnitDefID(cmdParams[1])] then		--	and UnitDefs[Spring.GetUnitDefID(cmdParams[1])].customParams.paralyzemultiplier == '0' then
+		and cmdParams[2] == nil
+		and type(cmdParams[1]) == 'number'
+		and UnitDefs[Spring.GetUnitDefID(cmdParams[1])] ~= nil then
+		if unEmpableUnits[Spring.GetUnitDefID(cmdParams[1])] then --	and UnitDefs[Spring.GetUnitDefID(cmdParams[1])].customParams.paralyzemultiplier == '0' then
 			return false
 		else
-			local _,_,_,_,y = Spring.GetUnitPosition(cmdParams[1], true)
+			local _,_,_,_, y = Spring.GetUnitPosition(cmdParams[1], true)
 			if y and y >= 0 then
 				return true
 			else
