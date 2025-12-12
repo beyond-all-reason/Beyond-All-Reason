beyond-all-reason/Beyond-All-Reason

commit 700e18d12d6a4ca99150d6efdd7979cca9a2db4d

Remove invalid models when loading a map (#4641)
Taken from Zero-K: https://github.com/Mu-L/Zero-K/commit/be1ca36c7bad0e13756b8906ac73bde63f4b5d9d

gamedata/featuredefs_post.lua
@@ -66,3 +66,42 @@ for featureDefName, featureDef in pairs(FeatureDefs) do
 		featureDef.customparams.i18nfrom = proxy
 	end
 end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local function isModelOK(featureDef)
+	local specifiesModel = featureDef.object and (featureDef.object ~= "")
+
+	-- explicitly modelless (geo etc)
+	if featureDef.drawtype == -1 and not specifiesModel then
+		return true
+	end
+
+	-- implicitly modelless
+	if not featureDef.drawtype and not specifiesModel then
+		return true
+	end
+
+	-- explicitly specified to use a model, but doesn't provide one (gigachad.jpg)
+	if featureDef.drawtype == 0
+	and not specifiesModel then
+		return false
+	end
+
+	-- old tree renderer removed from engine
+	if tonumber(featureDef.drawtype or 0) > 0 then
+		return false
+	end
+
+	local modelPath = "objects3d/" .. featureDef.object
+	return VFS.FileExists(modelPath          , VFS.ZIP)
+	    or VFS.FileExists(modelPath .. ".3do", VFS.ZIP)
+end
+
+for name, def in pairs(FeatureDefs) do
+	if not isModelOK(def) then
+		Spring.Log("featuredefs_post.lua", LOG.WARNING, "Removing feature def", name, "for having invalid model that would crash the engine", def.object)
+		FeatureDefs[name] = nil
+	end
+end

features/invalid_models.lua.test
@@ -1,0 +1,41 @@
+-- Test cases for the "remove invalid models" check.
+-- Rename to .lua to "run" the test.
+
+return {
+	x_nonexistent_model = {
+		object = "caught_me_slippin.dae",
+	},
+	x_uses_model_but_nil = {
+		drawtype = 0,
+	},
+	x_uses_model_but_empty = {
+		drawtype = 0,
+		object = "",
+	},
+	x_uses_tree_1 = {
+		drawtype = 1,
+	},
+	x_uses_tree_777 = {
+		drawtype = 777,
+	},
+	x_uses_tree_1_provides_model = {
+		drawtype = 1,
+		object = "SimpleFlare.s3o"
+	},
+	x_uses_tree_777_provides_model = {
+		drawtype = 777,
+		object = "SimpleFlare.s3o"
+	},
+	x_nonexistent_model_drawtype_none = {
+		drawtype = -1,
+		object = "caught_me_slippin.dae",
+	},
+	x_nonexistent_model_drawtype_tree = {
+		drawtype = 1,
+		object = "caught_me_slippin.dae",
+	},
+	x_nonexistent_model_drawtype_tree_777 = {
+		drawtype = 777,
+		object = "caught_me_slippin.dae",
+	},
+}

commit 14809d1e126ab94dfae8e8a12d143072e59882d9

feature models can be s3o (#6387)
### Work done

- Allow feature models to use explicit .s3o extension
- By adding .s3o in featuredefs_post

Fixes an issue introduced in #4641.

Currently, only feature defs with an implicit .s3o model path extension
will load. This recently impacted the xmas commander wrecks, fixed in
#6385.

Looks like when an object name is passed w/ no extension, all the
extensions of each known parser type are checked (includes 3do, s3o,
gltf, glb):
https://github.com/beyond-all-reason/RecoilEngine/blob/a5e9db90f450a72ce9c5f341d617df9297ce3bfa/rts/Rendering/Models/IModelParser.cpp#L58-L61

Co-authored-by: efrec <quantumscrewdriver@gmail.com>

gamedata/featuredefs_post.lua
@@ -97,6 +97,7 @@ local function isModelOK(featureDef)
 	local modelPath = "objects3d/" .. featureDef.object
 	return VFS.FileExists(modelPath          , VFS.ZIP)
 	    or VFS.FileExists(modelPath .. ".3do", VFS.ZIP)
+	    or VFS.FileExists(modelPath .. ".s3o", VFS.ZIP)
 end
 
 for name, def in pairs(FeatureDefs) do