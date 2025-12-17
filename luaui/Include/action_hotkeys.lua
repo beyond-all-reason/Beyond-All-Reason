c84ed631a379a52a8379e2b8facbea414482b88e
TheChronographer <160436719+TheChronographer@users.noreply.github.com>
2025-12-17 21:05:11 +1100

    Unhardcode various hotkey tooltips.

--- luaui/Include/action_hotkeys.lua
+++ luaui/Include/action_hotkeys.lua
@@ -5,6 +5,12 @@ for _, keybinding in pairs(Spring.GetKeyBindings()) do
 	if (not actionHotkeys[cmd]) or keybinding.boundWith:len() < actionHotkeys[cmd]:len() then
 		actionHotkeys[cmd] = keybinding.boundWith
 	end
+	if keybinding.command == "select" then
+		local select_Cmd = keybinding.select
+		if (not actionHotkeys[select_Cmd]) or keybinding.boundWith:len() < actionHotkeys[select_Cmd]:len() then
+			actionHotkeys[select_Cmd] = keybinding.boundWith
+		end
+	end
 end
 
 return actionHotkeys
