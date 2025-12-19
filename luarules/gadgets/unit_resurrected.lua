de704db22c81388c2d08cc6791ec5802e150b4ce
SethDGamre <sethdgamre@gmail.com>
2025-12-18 19:42:08 -0600

    split as per Sprung's code

--- luarules/gadgets/unit_resurrected.lua
+++ luarules/gadgets/unit_resurrected.lua
@@ -7,8 +7,7 @@ function gadget:GetInfo()
         author    = "Floris, Chronographer, SethDGamre",
         date      = "4 November 2025",
         license   = "GNU GPL, v2 or later",
-        layer     = 5,
-        handler   = true,
+        layer     = 5, -- FIXME why?
         enabled   = true
     }
 end
@@ -17,199 +16,130 @@ if not gadgetHandler:IsSyncedCode() then
 	return false
 end
 
-local TIMEOUT = Game.gameSpeed * 3
 local CMD_RESURRECT = CMD.RESURRECT
 local CMD_WAIT = CMD.WAIT
 local CMD_FIRE_STATE = CMD.FIRE_STATE
 local CMD_MOVE_STATE = CMD.MOVE_STATE
+local VISIBILITY_INLOS = {inlos = true}
 local UPDATE_INTERVAL = Game.gameSpeed
+local TIMEOUT_FRAMES = Game.gameSpeed * 3 -- long enough to get grabbed by FeatureCreated callback
 
-local spGetGameFrame = Spring.GetGameFrame
 local spGetUnitHealth = Spring.GetUnitHealth
 local spGiveOrderToUnit = Spring.GiveOrderToUnit
 local spGetUnitCurrentCommand = Spring.GetUnitCurrentCommand
 local spGetFeatureRulesParam = Spring.GetFeatureRulesParam
-local spSetUnitRulesParam = Spring.SetUnitRulesParam
-local spGetUnitDefID = Spring.GetUnitDefID
 local spSetFeatureRulesParam = Spring.SetFeatureRulesParam
-local spGetUnitTeam = Spring.GetUnitTeam
 
-local gameFrame = spGetGameFrame()
-
-local corpseRegistryByDefID = {}
-local rezUnitDefs = {}
-local isBuilding = {}
-local isBuilder = {}
+local shouldWaitForHealing = {}
 local toBeUnWaited = {}
 local prevHealth = {}
-local currentCmd = {}
+local priorStates = {} -- unitID = { timeout, firestate, movestate, xp }
 
 for unitDefID, unitDef in pairs(UnitDefs) do
-	if unitDef.canResurrect then
-		rezUnitDefs[unitDefID] = true
-	end
-	if unitDef.isBuilding then
-		isBuilding[unitDefID] = true
-	end
-	if unitDef.isBuilder then
-		isBuilder[unitDefID] = true
-	end
+	shouldWaitForHealing[unitDefID] = (not unitDef.isBuilding) and (not unitDef.isBuilder)
 end
 
-
-local function getPositionHash(x, z) -- we use hashing as a bridge between UnitDestroyed and FeatureCreated which are separated by their death animation duration.
-	x = math.floor(x)
-	z = math.floor(z)
-	return string.format("%f:%f", x, z)
+local function RestoreStateMechanics(unitID, featureID)
+	Spring.SetUnitExperience(unitID, spGetFeatureRulesParam(featureID, "previous_xp") or 0)
 end
 
-function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam, weaponDefID)
-	local unitDefLink = corpseRegistryByDefID[unitDefID]
-	if not unitDefLink then
-		unitDefLink = {}
-		corpseRegistryByDefID[unitDefID] = unitDefLink
+local function RestoreStateGUI(unitID, featureID)
+	local firestate = spGetFeatureRulesParam(featureID, "previous_firestate")
+	if firestate then
+		spGiveOrderToUnit(unitID, CMD_FIRE_STATE, firestate, 0)
 	end
-	local x, y, z = Spring.GetUnitPosition(unitID)
-	if not x then
-		return
-	end
-	local xp = Spring.GetUnitExperience(unitID)
-	local unitStates = Spring.GetUnitStates(unitID)
-	local positionHash = getPositionHash(x, z)
-	unitDefLink[positionHash] = {
-		deadUnitID = unitID,
-		unitTeam = unitTeam,
-		xp = xp,
-		attackerID = attackerID,
-		attackerDefID = attackerDefID,
-		attackerTeam = attackerTeam,
-		attackerWeaponDefID = weaponDefID,
-		firestate = unitStates.firestate,
-		movestate = unitStates.movestate,
-		timeout = gameFrame + TIMEOUT
-	}
-end
 
-
-local function GetPriorUnitID(featureID)
-	local resurrectData = Spring.GetFeatureResurrect(featureID)
-	if resurrectData then
-		local resurrectUnitName = resurrectData
-		local resurrectUnitDefID
-		if resurrectUnitName then
-			local nameDef = UnitDefNames[resurrectUnitName]
-			if nameDef then
-				resurrectUnitDefID = nameDef.id
-			else
-				return -- Invalid unit name
-			end
-		end
-
-		local x, y, z = Spring.GetFeaturePosition(featureID)
-		local unitDefLink = corpseRegistryByDefID[resurrectUnitDefID]
-		if unitDefLink and x then
-			local positionHash = getPositionHash(x, z)
-			local corpseLink = unitDefLink[positionHash]
-			if corpseLink then
-				spSetFeatureRulesParam(featureID, "corpse_deadUnitID", corpseLink.deadUnitID)
-				spSetFeatureRulesParam(featureID, "corpse_xp", corpseLink.xp)
-				spSetFeatureRulesParam(featureID, "restored_firestate", corpseLink.firestate)
-				spSetFeatureRulesParam(featureID, "restored_movestate", corpseLink.movestate)
-				unitDefLink[positionHash] = nil
-				return corpseLink.deadUnitID
-			end
-		end
+	local movestate = spGetFeatureRulesParam(featureID, "previous_movestate")
+	if movestate then
+		spGiveOrderToUnit(unitID, CMD_MOVE_STATE, movestate, 0)
 	end
 end
 
 function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-	if builderID then
-		if rezUnitDefs[spGetUnitDefID(builderID)] then
-			if (not isBuilding[unitDefID]) and (not isBuilder[unitDefID]) then
-				toBeUnWaited[unitID] = true
-				prevHealth[unitID] = 0
-				spGiveOrderToUnit(unitID, CMD_WAIT, {}, 0)
-			end
-		end
+	if not builderID then
+		return
+	end
 
-		local cmdID, targetID = Spring.GetUnitWorkerTask(builderID)
-		if cmdID == CMD_RESURRECT and targetID then
-			local corpseLinkFeatureID = targetID - Game.maxUnits
-			if corpseLinkFeatureID then
-				local rezRulesParam = Spring.GetUnitRulesParam(unitID, "resurrected")
-				if rezRulesParam == nil then
-					spSetUnitRulesParam(unitID, "resurrected", 1, {inlos=true})
-					Spring.SetUnitHealth(unitID, spGetUnitHealth(unitID) * 0.05)
-				end
+	local cmdID, featureID = Spring.GetUnitWorkerTask(builderID)
+	if cmdID ~= CMD_RESURRECT then
+		return
+	end
+	if not Engine.FeatureSupport.noOffsetForFeatureID then
+		featureID = featureID - Game.maxUnits
+	end
 
-				spSetUnitRulesParam(unitID, "priorlife_deadUnitID", spGetFeatureRulesParam(corpseLinkFeatureID, "corpse_deadUnitID"), {inlos=true})
-				local xp = spGetFeatureRulesParam(corpseLinkFeatureID, "corpse_xp")
-				local firestate = spGetFeatureRulesParam(corpseLinkFeatureID, "restored_firestate")
-				local movestate = spGetFeatureRulesParam(corpseLinkFeatureID, "restored_movestate")
-				if firestate then
-					spGiveOrderToUnit(unitID, CMD_FIRE_STATE, {tonumber(firestate)}, 0)
-				end
-				if movestate then
-					spGiveOrderToUnit(unitID, CMD_MOVE_STATE, {tonumber(movestate)}, 0)
-				end
-				if xp then
-					Spring.SetUnitExperience(unitID, tonumber(xp))
-				end
-			end
-		end
+	-- Wait combat units so they don't wander off before they get repaired
+	if shouldWaitForHealing[unitDefID] then
+		toBeUnWaited[unitID] = true
+		prevHealth[unitID] = 0
+		spGiveOrderToUnit(unitID, CMD_WAIT, 0, 0)
+	end
+
+	-- FIXME: 1 -> true (0 is truthy in lua too), but would need to be fixed elsewhere as well
+	Spring.SetUnitRulesParam(unitID, "resurrected", 1, VISIBILITY_INLOS)
+
+	Spring.SetUnitHealth(unitID, spGetUnitHealth(unitID) * 0.05)
+
+	RestoreStateMechanics(unitID, featureID)
+
+	-- Don't retain GUI settings (movestate etc) from other players
+	if Spring.GetFeatureTeam(featureID) == Spring.GetUnitTeam(unitID) then
+		RestoreStateGUI(unitID, featureID)
 	end
 end
 
+function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
+	local states = Spring.GetUnitStates(unitID)
+	priorStates[unitID] = {
+		timeout = Spring.GetGameFrame() + TIMEOUT_FRAMES,
+		firestate = states.firestate,
+		movestate = states.movestate,
+		xp = Spring.GetUnitExperience(unitID)
+	}
+end
+
+function gadget:FeatureCreated(featureID, _, sourceID)
+	if not sourceID then
+		return
+	end
+
+	local states = priorStates[sourceID]
+	if not states then
+		return
+	end
+
+	spSetFeatureRulesParam(featureID, "previous_firestate", states.firestate)
+	spSetFeatureRulesParam(featureID, "previous_movestate", states.movestate)
+	spSetFeatureRulesParam(featureID, "previous_xp", states.xp)
+end
 
 function gadget:GameFrame(frame)
-	gameFrame = frame
+	if frame % UPDATE_INTERVAL ~= 0 then
+		return
+	end
 
-	if frame % UPDATE_INTERVAL == 0 then
-		for unitDefID, unitDefLink in pairs(corpseRegistryByDefID) do
-			for positionHash, corpseLink in pairs(unitDefLink) do
-				if corpseLink.timeout < frame then
-					unitDefLink[positionHash] = nil
-				end
-			end
-		end
-		if next(toBeUnWaited) ~= nil then
-			for unitID, check in pairs(toBeUnWaited) do
-				local health = spGetUnitHealth(unitID)
-				if not health then
-					toBeUnWaited[unitID] = nil
-					prevHealth[unitID] = nil
-				elseif health <= prevHealth[unitID] then -- stopped healing
-					toBeUnWaited[unitID] = nil
-					prevHealth[unitID] = nil
-					currentCmd = spGetUnitCurrentCommand(unitID, 1)
-					if currentCmd and currentCmd == CMD_WAIT then
-						spGiveOrderToUnit(unitID, CMD_WAIT, {}, 0)
-					end
-				else
-					prevHealth[unitID] = health
-				end
-			end
+	for unitID, state in pairs(priorStates) do
+		if state.timeout < frame then
+			priorStates[unitID] = nil
 		end
 	end
-end
 
-local originalFeatureCreated
-
-function gadget:Initialize()
-	local allUnits = Spring.GetAllUnits()
-	for i = 1, #allUnits do
-		local unitID = allUnits[i]
-		gadget:UnitCreated(unitID, spGetUnitDefID(unitID), spGetUnitTeam(unitID))
+	if next(toBeUnWaited) ~= nil then
+		for unitID, check in pairs(toBeUnWaited) do
+			local health = spGetUnitHealth(unitID)
+			if not health then
+				toBeUnWaited[unitID] = nil
+				prevHealth[unitID] = nil
+			elseif health <= prevHealth[unitID] then -- stopped healing
+				toBeUnWaited[unitID] = nil
+				prevHealth[unitID] = nil
+				local currentCmdID = spGetUnitCurrentCommand(unitID)
+				if currentCmdID == CMD_WAIT then
+					spGiveOrderToUnit(unitID, CMD_WAIT, 0, 0)
+				end
+			else
+				prevHealth[unitID] = health
+			end
+		end
 	end
-
-	originalFeatureCreated = gadgetHandler.FeatureCreated
-	gadgetHandler.FeatureCreated = function(self, featureID, allyTeam, sourceID)
-		local priorUnitID = sourceID or GetPriorUnitID(featureID)
-		originalFeatureCreated(self, featureID, allyTeam, priorUnitID)
-	end
 end
-
-function gadget:Shutdown()
-	gadgetHandler.FeatureCreated = originalFeatureCreated
-end
-
