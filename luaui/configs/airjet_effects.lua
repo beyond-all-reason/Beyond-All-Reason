959df6502d14e09c8f086b884c6e11cbdba37c0b
Tharsis <tharsisvein@gmail.com>
2025-12-19 16:38:31 +1300

    Add legion t2 subs

--- luaui/configs/airjet_effects.lua
+++ luaui/configs/airjet_effects.lua
@@ -379,6 +379,16 @@ return {
 		{ color = { 0.2, 0.8, 0.2 }, width = 5, length = 17, piece = "thrust2" },
 	},
 
+	-- Legion t2 subs
+	["leganavyheavysub"] = {
+		{ color = { 0.2, 0.8, 0.2 }, width = 3, length = 20, piece = "thrust1", emitVector = { 0, 0, -1 }, xzVelocity = 1.5, light = 1 },
+		{ color = { 0.2, 0.8, 0.2 }, width = 3, length = 20, piece = "thrust2", emitVector = { 0, 0, -1 }, xzVelocity = 1.5, light = 1 },
+		{ color = { 0.2, 0.8, 0.2 }, width = 3, length = 20, piece = "thrust3", emitVector = { 0, 0, -1 }, xzVelocity = 1.5, light = 1 },
+	},
+	["leganavybattlesub"] = {
+		{ color = { 0.2, 0.8, 0.2 }, width = 3, length = 20, piece = "thrusttrail", emitVector = { 0, 0, -1 }, xzVelocity = 1.5, light = 1 },
+	},
+
 
 	-- flying ships
 	["armfepocht4"] = {
