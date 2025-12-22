fc20c55e1fe7a2708d4622c7822ca835710ff376
Felnious <felnious12@gmail.com>
2025-12-22 06:09:32 -0700

    Refactor unit creation logic in test_critters.lua

--- luaui/Tests/critters/test_critters.lua
+++ luaui/Tests/critters/test_critters.lua
@@ -88,23 +88,29 @@ function runCritterTest()
 
 	-- 2. Create lots of units so critters will be cleaned up
 
-	SyncedRun(function(locals)
-		local GaiaTeamID  = Spring.GetGaiaTeamID()
-		local midX, midZ = locals.midX, locals.midZ
-		local y = Spring.GetGroundHeight(-2000, -1500) + 40
-		local spCreateUnit = Spring.CreateUnit
-		local unitName = locals.unitName
-		local function createUnit(def, x, z, teamID)
-			local x = midX + x
-			local z = midZ + z
-			spCreateUnit(def, x, y, z, "south", teamID)
+SyncedRun(function(locals)
+	local midX, midZ = locals.midX, locals.midZ
+	local unitName   = locals.unitName
+	local spCreateUnit = Spring.CreateUnit
+
+	local function createUnit(def, x, z, teamID)
+		x = midX + x
+		z = midZ + z
+		local y = Spring.GetGroundHeight(x, z) + 40
+		spCreateUnit(def, x, y, z, "south", teamID)
+	end
+
+	for i = 1, 60 do
+		for j = 1, 60 do
+			createUnit(unitName, -2000 + i * 50, -1500 + j * 50, 0)
 		end
-		for i=1, 60 do
-			for j=1, 60 do
-				createUnit(unitName, -2000+i*50, -1500+j*50, 0)
-			end
-		end
-	end, 500)
+	end
+end, 500, {
+	midX = midX,
+	midZ = midZ,
+	unitName = unitName,
+})
+
 
 	local GaiaTeamID = Spring.GetGaiaTeamID()
 
