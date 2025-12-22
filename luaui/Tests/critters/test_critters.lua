6cddd16e875bcff09466c2d5db050358cf5f7bd0
Felnious <felnious12@gmail.com>
2025-12-22 06:23:06 -0700

    Update test_critters.lua

--- luaui/Tests/critters/test_critters.lua
+++ luaui/Tests/critters/test_critters.lua
@@ -1,6 +1,5 @@
 function setup()
 	Test.clearMap()
-
 	Test.levelHeightMap()
 
 	Spring.SendCommands("globallos")
@@ -18,6 +17,8 @@ function runCritterTest()
 	local WAIT_FRAMES = 204 -- enough to trigger critter cleanup/restoring by gaia_critters
 	local unitName = 'armpw'
 	local critterName = 'critter_crab'
+
+	-- build critter lookup
 	local isCritter = {}
 	for udefID, def in ipairs(UnitDefs) do
 		if string.find(def.name, "critter_") then
@@ -26,11 +27,12 @@ function runCritterTest()
 	end
 
 	local midX, midZ = Game.mapSizeX / 2, Game.mapSizeZ / 2
+	local GaiaTeamID = Spring.GetGaiaTeamID()
 
+	-- helper: count living critters
 	local function countAliveCritters()
-		local allUnits = Spring.GetAllUnits()
 		local alive = 0
-		for _, unitID in pairs(allUnits) do
+		for _, unitID in ipairs(Spring.GetAllUnits()) do
 			local defID = Spring.GetUnitDefID(unitID)
 			if isCritter[defID] then
 				alive = alive + 1
@@ -39,37 +41,25 @@ function runCritterTest()
 		return alive
 	end
 
-	local function destroyNonCritters()
-		local unitDefID = UnitDefNames[unitName].id
-		SyncedRun(function(locals)
-			local unitDefID = locals.unitDefID
-			local allUnits = Spring.GetAllUnits()
-			for _, unitID in pairs(allUnits) do
-				local defID = Spring.GetUnitDefID(unitID)
-				if defID == unitDefID then
-					Spring.DestroyUnit(unitID, false, true, nil, true)
-				end
-			end
-		end, 500)
-	end
-
+	-------------------------------------------------------
 	-- 1. Create critters
+	-------------------------------------------------------
 
 	SyncedRun(function(locals)
-		local GaiaTeamID  = Spring.GetGaiaTeamID()
+		local GaiaTeamID = Spring.GetGaiaTeamID()
 		local critterName = locals.critterName
-		local midX, midZ  = locals.midX, locals.midZ
+		local midX, midZ = locals.midX, locals.midZ
 
-		local function createUnit(def, x, z, teamID)
+		local function createUnit(def, x, z)
 			x = midX + x
 			z = midZ + z
 			local y = Spring.GetGroundHeight(x, z) + 40
-			Spring.CreateUnit(def, x, y, z, "south", teamID)
+			Spring.CreateUnit(def, x, y, z, "south", GaiaTeamID)
 		end
-	
+
 		for i = 0, 5 do
 			for j = 0, 5 do
-				createUnit(critterName, 850 + i*50, 100 + j*50, GaiaTeamID)
+				createUnit(critterName, 850 + i * 50, 100 + j * 50)
 			end
 		end
 	end, 400, {
@@ -78,79 +68,84 @@ function runCritterTest()
 		midZ = midZ,
 	})
 
-
 	assertSuccessBefore(5, 5, function()
 		return #Spring.GetAllUnits() == 36
 	end)
 
 	assert(countAliveCritters() == 36)
 
+	-------------------------------------------------------
+	-- 2. Create pressure units (tracked explicitly)
+	-------------------------------------------------------
 
-	-- 2. Create lots of units so critters will be cleaned up
+	local pressureUnits = {}
 
-SyncedRun(function(locals)
-	local midX, midZ = locals.midX, locals.midZ
-	local unitName   = locals.unitName
-	local spCreateUnit = Spring.CreateUnit
+	SyncedRun(function(locals)
+		local midX, midZ = locals.midX, locals.midZ
+		local unitName = locals.unitName
+		local pressureUnits = locals.pressureUnits
+		local spCreateUnit = Spring.CreateUnit
 
-	local function createUnit(def, x, z, teamID)
-		x = midX + x
-		z = midZ + z
-		local y = Spring.GetGroundHeight(x, z) + 40
-		spCreateUnit(def, x, y, z, "south", teamID)
-	end
-
-	for i = 1, 60 do
-		for j = 1, 60 do
-			createUnit(unitName, -2000 + i * 50, -1500 + j * 50, 0)
+		local function createUnit(def, x, z)
+			x = midX + x
+			z = midZ + z
+			local y = Spring.GetGroundHeight(x, z) + 40
+			local unitID = spCreateUnit(def, x, y, z, "south", 0)
+			if unitID then
+				pressureUnits[#pressureUnits + 1] = unitID
+			end
 		end
-	end
-end, 500, {
-	midX = midX,
-	midZ = midZ,
-	unitName = unitName,
-})
 
-
-	local GaiaTeamID = Spring.GetGaiaTeamID()
+		for i = 1, 60 do
+			for j = 1, 60 do
+				createUnit(unitName, -2000 + i * 50, -1500 + j * 50)
+			end
+		end
+	end, 500, {
+		midX = midX,
+		midZ = midZ,
+		unitName = unitName,
+		pressureUnits = pressureUnits,
+	})
 
 	assertSuccessBefore(30, 10, function()
-  	  return #Spring.GetAllUnits() >= 3600
+		return #pressureUnits >= 3600
 	end)
-
 
 	Test.waitFrames(WAIT_FRAMES)
 
 	assert(countAliveCritters() < 36)
 
+	-------------------------------------------------------
+	-- 3. Destroy pressure units only
+	-------------------------------------------------------
 
-	-- 3. Destroy non critters so critters will be restored
-
-	local function destroyNonCritters()
-	SyncedRun(function(locals)
-		local isCritter = locals.isCritter
-		local allUnits = Spring.GetAllUnits()
-		for _, unitID in ipairs(allUnits) do
-			local defID = Spring.GetUnitDefID(unitID)
-			if not isCritter[defID] then
-				Spring.DestroyUnit(unitID, false, true, nil, true)
+	local function destroyPressureUnits()
+		SyncedRun(function(locals)
+			for _, unitID in ipairs(locals.pressureUnits) do
+				if Spring.ValidUnitID(unitID) then
+					Spring.DestroyUnit(unitID, false, true, nil, true)
+				end
 			end
-		end
-	end, 500, {
-		isCritter = isCritter,
-	})
-end
+		end, 500, {
+			pressureUnits = pressureUnits,
+		})
+	end
 
+	destroyPressureUnits()
 
 	assertSuccessBefore(15, 5, function()
-	for _, unitID in ipairs(Spring.GetAllUnits()) do
-		local defID = Spring.GetUnitDefID(unitID)
-		if not isCritter[defID] then
-			return false
+		for _, unitID in ipairs(pressureUnits) do
+			if Spring.ValidUnitID(unitID) then
+				return false
+			end
 		end
-	end
-	return true
-end)
+		return true
+	end)
+
+	-------------------------------------------------------
+	-- 4. Wait for critter restore tick
+	-------------------------------------------------------
 
 	Test.waitFrames(WAIT_FRAMES - (Spring.GetGameFrame() % WAIT_FRAMES))
 
