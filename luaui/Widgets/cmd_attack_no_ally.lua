ee8c99df0aea638d14b7d1a10bd4990642f7a17d
Zain Mustafa <108363829+ZainMGit@users.noreply.github.com>
2025-12-23 11:04:01 +0000

    Block commands on allied units for #6459
    - For CMD.ATTACK on allies: Block the command instead of redirect
    - For CMD.MANUALFIRE on allies: Block the command
    - This prevents any shooting at allied units, whether clicked on or near

--- luaui/Widgets/cmd_attack_no_ally.lua
+++ luaui/Widgets/cmd_attack_no_ally.lua
@@ -3,7 +3,7 @@ local widget = widget ---@type Widget
 function widget:GetInfo()
    return {
       name         = "Attack and Manual Fire no ally",
-      desc         = "Prevents attack aim to snap onto ally units (targets ground instead); prevents manual fire commands (cancels instead)",
+      desc         = "Prevents attack and manual fire aim to snap onto ally units (blocks command)",
       author       = "Ceddral, Floris",
       date         = "April 2018",
 	  license      = "GNU GPL, v2 or later",
@@ -25,10 +25,15 @@ end
 
 function widget:CommandNotify(cmdID, cmdParams, cmdOptions)
 	if cmdID == CMD.MANUALFIRE then
-		-- always cancel manual fire commands
-		Spring.Echo("Manual fire command intercepted and canceled")
-		Spring.GiveOrder(CMD.STOP, {}, cmdOptions)
-		return true
+		-- cancel manual fire commands on allies by blocking, on others by stop
+		if #cmdParams == 1 and Spring.IsUnitAllied(cmdParams[1]) then
+			Spring.Echo("Manual fire on allied unit blocked")
+			return true -- block the command
+		else
+			Spring.Echo("Manual fire command intercepted and canceled")
+			Spring.GiveOrder(CMD.STOP, {}, cmdOptions)
+			return true
+		end
 	elseif cmdID == CMD.ATTACK then
 		-- number of cmdParams should either be
 		-- 1 (unitID) or
@@ -40,16 +45,8 @@ function widget:CommandNotify(cmdID, cmdParams, cmdOptions)
 			return false
 		end -- still snap aim at enemy units
 
-		-- for attack on allies, redirect to ground instead of cancel
-		local mouseX, mouseY = Spring.GetMouseState()
-		local desc, cmdParams = Spring.TraceScreenRay(mouseX, mouseY, true)
-		if nil == desc then
-			return false
-		end -- off map, can not handle this properly here
-		--cmdParams[4] = nil	-- not a clue why ther is a 4th parameter
-
-		-- replace order
-		Spring.GiveOrder(cmdID, {cmdParams[1], cmdParams[2], cmdParams[3]}, cmdOptions)
+		-- for attack on allies, block the command
+		Spring.Echo("Attack on allied unit blocked")
 		return true
 	else
 		return false
