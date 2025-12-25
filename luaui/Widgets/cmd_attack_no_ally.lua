fb1d797524fe23bde37efc45145ec04827ef8b3d
Floris <floris@ruwhof.net>
2025-12-25 17:16:06 +0100

    Revert "Attack Command Change (#6475)"
    This reverts commit 4a789cb3f4fa9fad92a3d2f1a770509fe6dec88e.

--- luaui/Widgets/cmd_attack_no_ally.lua
+++ luaui/Widgets/cmd_attack_no_ally.lua
@@ -1,97 +1,51 @@
 local widget = widget ---@type Widget
 
 function widget:GetInfo()
-	return {
-		name         = "No fire on own units (full cancel)",
-		desc         = "Redirects attack on allies to ground and fully exits attack mode on RMB",
-		author       = "Ceddral, Floris (modified)",
-		date         = "April 2018 (modified Dec 2025)",
-		license      = "GNU GPL, v2 or later",
-		layer        = 0,
-		enabled      = true
-	}
-end
-
-local function ExitAttackMode()
-	-- STOP clears queued commands
-	Spring.GiveOrder(CMD.STOP, {}, {})
-	-- clears the active command cursor 
-	Spring.SetActiveCommand(0)
-end
-
-local function IssueGroundAttack(cmdOptions)
-	local mx, my = Spring.GetMouseState()
-	local _, pos = Spring.TraceScreenRay(mx, my, true)
-
-	if pos and pos[1] then
-		Spring.GiveOrder(CMD.ATTACK, { pos[1], pos[2], pos[3] }, cmdOptions or {})
-		return true
-	end
-	return false
+   return {
+      name         = "Attack no ally",
+      desc         = "Prevents attack-aim to snap onto ally units (targets ground instead)",
+      author       = "Ceddral, Floris",
+      date         = "April 2018",
+	  license      = "GNU GPL, v2 or later",
+      layer        = 0,
+      enabled      = true
+   }
 end
 
 
 function widget:Initialize()
 	WG['attacknoally'] = true
-	WG['manualfirennoally'] = true
-	WG['guardnoally'] = true
 end
 
 function widget:Shutdown()
 	WG['attacknoally'] = nil
-	WG['manualfirennoally'] = nil
-	WG['guardnoally'] = nil
 end
 
-function widget:MousePress(x, y, button)
-	-- Right mouse button
-	if button ~= 3 then
-		return false
-	end
-
-	local _, activeCmdID = Spring.GetActiveCommand()
-
-	if activeCmdID == CMD.ATTACK then
-		ExitAttackMode()
-		return true -- swallow RMB so engine doesn't re-trigger commands
-	end
-
-	return false
-end
-
--- Command interception
 function widget:CommandNotify(cmdID, cmdParams, cmdOptions)
-	if cmdID == CMD.ATTACK then
-		-- Ground attack = 3 params; only intercept unit-target attacks
-		if #cmdParams ~= 1 then
-			return false
-		end
-
-		local targetUnitID = cmdParams[1]
-		if not Spring.IsUnitAllied(targetUnitID) then
-			return false
-		end
-
-		-- Redirect attack on allied unit → ground attack
-		if not IssueGroundAttack(cmdOptions) then
-			ExitAttackMode()
-		end
-		return true
-
-	elseif cmdID == CMD.MANUALFIRE then
-		if #cmdParams == 1 and Spring.IsUnitAllied(cmdParams[1]) then
-			ExitAttackMode()
-			return true
-		end
-		return false
-
-	elseif cmdID == CMD.GUARD then
-		if #cmdParams == 1 and Spring.IsUnitAllied(cmdParams[1]) then
-			ExitAttackMode()
-			return true
-		end
+	if cmdID ~= CMD.ATTACK then
 		return false
 	end
 
-	return false
+	-- number of cmdParams should either be
+	-- 1 (unitID) or
+	-- 3 (map coordinates)
+	if #cmdParams ~= 1 then
+		return false
+	end
+	if not Spring.IsUnitAllied(cmdParams[1]) then
+		return false
+	end -- still snap aim at enemy units
+
+
+	-- get map position behind cursor
+	local mouseX, mouseY = Spring.GetMouseState()
+	local desc, cmdParams = Spring.TraceScreenRay(mouseX, mouseY, true)
+	if nil == desc then
+		return false
+	end -- off map, can not handle this properly here
+	--cmdParams[4] = nil	-- not a clue why ther is a 4th parameter
+
+	-- replace order
+	Spring.GiveOrder(cmdID, {cmdParams[1], cmdParams[2], cmdParams[3]}, cmdOptions)
+	return true
 end
