4809df5cb02678a64d3c1120140f35e6c453ea5d
efrec <quantumscrewdriver@gmail.com>
2025-12-25 15:53:58 -0500

    feat: custom weapon preaim range

--- luarules/gadgets/unit_preaim.lua
+++ luarules/gadgets/unit_preaim.lua
@@ -21,27 +21,35 @@ end
 --also when proximityPriority is heavily biased toward far targets
 
 local rangeBoost = {}
-local isPreaimUnit = {}
+local autoTargetRangeBoost = {}
 for unitDefID, unitDef in pairs(UnitDefs) do
 	if not unitDef.canFly then
-		local weapons = unitDef.weapons
-		if #weapons > 0 then
-			for i=1, #weapons do
-				if not WeaponDefs[weapons[i].weaponDef].customParams.exclude_preaim then
-					isPreaimUnit[unitDefID] = isPreaimUnit[unitDefID] or {}
+		local weaponBoost = {}
 
-					local weaponDefID = weapons[i].weaponDef
-					isPreaimUnit[unitDefID][i] = weaponDefID
-					rangeBoost[weaponDefID] = math.max(0.1 * WeaponDefs[weaponDefID].range, 20)
-				end
+		local weapons = unitDef.weapons
+		for i = 1, #weapons do
+			local weaponDefID = weapons[i].weaponDef
+			local weaponDef = WeaponDefs[weaponDefID]
+
+			if not weaponDef.customParams.exclude_preaim then
+				local range = weaponDef.range
+				local param = tonumber(weaponDef.customParams.preaim_range)
+				local boost = math.max(20, range * 0.10, (param or 0) - range)
+
+				weaponBoost[i] = weaponDefID
+				rangeBoost[weaponDefID] = boost
 			end
+		end
+
+		if next(weaponBoost) then
+			autoTargetRangeBoost[unitDefID] = weaponBoost
 		end
 	end
 end
 
 function gadget:UnitCreated(unitID, unitDefID)
-	if isPreaimUnit[unitDefID] then
-		for id, wdefID in pairs(isPreaimUnit[unitDefID]) do
+	if autoTargetRangeBoost[unitDefID] then
+		for id, wdefID in pairs(autoTargetRangeBoost[unitDefID]) do
 			Spring.SetUnitWeaponState(unitID, id, "autoTargetRangeBoost", rangeBoost[wdefID])
 		end
 	end
