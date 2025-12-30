c8a61e781ac7db3ac9a2dd4d390f14f7df7a2485
Zain Mustafa <mustafa98537@gmail.com>
2025-12-30 02:46:55 -0800

    attack 'set target' command given a sound effect on confirmation

---
+++ luaui/Widgets/snd_set_target_click.lua
@@ -0,0 +1,29 @@
+local widget = widget ---@type Widget
+
+function widget:GetInfo()
+	return {
+		name = "Set target click sound",
+		desc = "Plays a UI click sound whenever the set target command is used",
+		author = "Zain M",
+		date = "December 2025",
+		license = "GNU GPL, v2 or later",
+		layer = 0,
+		enabled = true
+	}
+end
+
+local CMD_SET_TARGET = GameCMD.UNIT_SET_TARGET
+local CMD_SET_TARGET_NO_GROUND = GameCMD.UNIT_SET_TARGET_NO_GROUND
+--File "click3" and "click4" used from https://mixkit.co/free-sound-effects/click/
+--free sound liscense, https://mixkit.co/terms/
+
+local SOUND_FILE = "LuaUI/Sounds/click3.wav"
+local SOUND_VOLUME = 0.15
+local SOUND_CHANNEL = "ui"
+
+function widget:CommandNotify(cmdID)
+	if cmdID == CMD_SET_TARGET or cmdID == CMD_SET_TARGET_NO_GROUND then
+		Spring.PlaySoundFile(SOUND_FILE, SOUND_VOLUME, SOUND_CHANNEL)
+	end
+end
+--aa
