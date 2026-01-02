3d81248711327cc7ac4915ad4e6d6f462e72a62d
Zain Mustafa <108363829+ZainMGit@users.noreply.github.com>
2026-01-02 15:15:39 -0800

    Metal for wreckage in GUI should match field display wreckage metal (#6559)
    Not really a critical fix but it was bothering me that the numbers are
    slightly different.
    
    ### Work done
    Changed the rounding done in ``gui_info`` for the metal calculation to
    use ``string.formatSI`` which is what is used t o calculate field metal
    values.
    
    
    #### BEFORE:
    <img width="1255" height="929" alt="image"
    src="https://github.com/user-attachments/assets/3afe8ccc-b970-4874-abb7-044779bf638b"
    />
    
    #### AFTER:
    <img width="938" height="992" alt="image"
    src="https://github.com/user-attachments/assets/dd3230a9-2908-4eb4-a9eb-2a0964dae2ba"
    />

--- luaui/Widgets/gui_info.lua
+++ luaui/Widgets/gui_info.lua
@@ -1847,12 +1847,12 @@ local function drawEngineTooltip()
 				local metal, _, energy, _ = Spring.GetFeatureResources(hoverData)
 				if energy > 0 then
 					height = height + heightStep
-					text = tooltipLabelTextColor..Spring.I18N('ui.info.energy').."  \255\255\255\000"..math.floor(energy)
+					text = tooltipLabelTextColor..Spring.I18N('ui.info.energy').."  \255\255\255\000"..string.formatSI(energy)
 					font:Print(text, backgroundRect[1] + contentPadding, backgroundRect[4] - contentPadding - (fontSize * 0.8) - height, fontSize, "o")
 				end
 				if metal > 0 then
 					height = height + heightStep
-					text = tooltipLabelTextColor..Spring.I18N('ui.info.metal').."  "..tooltipValueColor..math.floor(metal)
+					text = tooltipLabelTextColor..Spring.I18N('ui.info.metal').."  "..tooltipValueColor..string.formatSI(metal)
 					font:Print(text, backgroundRect[1] + contentPadding, backgroundRect[4] - contentPadding - (fontSize * 0.8) - height, fontSize, "o")
 				end
 				font:End()
@@ -2353,9 +2353,7 @@ function checkChanges()
 
 		if featureDef.reclaimable then
 			local metal, _, energy, _ = Spring.GetFeatureResources(featureID)
-			metal = math.floor(metal)
-			energy = math.floor(energy)
-			local reclaimText = Spring.I18N('ui.reclaimInfo.metal', { metal = metal }) .. "\255\255\255\128" .. " " .. Spring.I18N('ui.reclaimInfo.energy', { energy = energy })
+			local reclaimText = Spring.I18N('ui.reclaimInfo.metal', { metal = string.formatSI(metal) }) .. "\255\255\255\128" .. " " .. Spring.I18N('ui.reclaimInfo.energy', { energy = string.formatSI(energy) })
 			newTooltip = newTooltip .. "\n\n" .. reclaimText
 		end
 
