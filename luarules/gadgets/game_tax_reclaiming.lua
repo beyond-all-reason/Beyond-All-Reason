b5a770f62a57e1914e75e2eaee2683716028b999
RebelNode <kipale@kipale.com>
2026-01-11 12:50:46 +0200

    Use entirely different approach for taxing unit reclaim

--- luarules/gadgets/game_tax_reclaiming.lua
+++ luarules/gadgets/game_tax_reclaiming.lua
@@ -25,30 +25,14 @@ if Spring.GetModOptions().easytax then
 	sharingTax = 0.3 -- 30% tax for easytax modoption
 end
 
-local function isAlliedUnit(teamID, unitID)
-	local unitTeam = Spring.GetUnitTeam(unitID)
-	return teamID and unitTeam and teamID ~= unitTeam and Spring.AreTeamsAllied(teamID, unitTeam)
-end
-
-function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam, weaponDefID)
-	if attackerID == nil then
-		return
-	end
-	if weaponDefID == Game.envDamageTypes.Reclaimed then -- The unit was reclaimed
-		_,metalCost,_ =Spring.GetUnitCosts (unitID)
-		Spring.UseTeamResource(attackerTeam, "metal", metalCost * sharingTax)
-	end
-end
-
-function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions, cmdTag, synced)
-	-- Disallow reclaiming unfinished allied units/nanoframes since we cannot tax it (UnitDestroyed isn't triggered for unfinished units)
-	if (cmdID == CMD.RECLAIM and #cmdParams >= 1) then
-		local targetID = cmdParams[1]
-		if isAlliedUnit(unitTeam, targetID) then
-			_,_,_,_,buildProgress = Spring.GetUnitHealth(targetID)
-			if buildProgress < 1 then
-				return false
-			end
+-- We don't use UnitDestroyed because then we can't find out how much metal is in a partially built unit
+function gadget:AllowUnitBuildStep(builderID, builderTeam, unitID, unitDefID, step)
+	local hp,maxhp,_,_,currentBuild = Spring.GetUnitHealth(unitID)
+	local objectTeam = Spring.GetUnitTeam(unitID)
+    if step < 0 then -- reclaim step
+		if (hp + step * maxhp) <= 0 then -- we only care about the last bit
+			local metalCost = UnitDefs[unitDefID].metalCost * currentBuild
+			Spring.UseTeamResource(builderTeam, "metal", metalCost * sharingTax)
 		end
 	end
 	return true
