9fba55798689d8cf32927abfb41f28cd91168cbc
efrec <ecfrechette@gmail.com>
2026-01-24 13:09:10 -0500

    fix: Disable Assist Ally remove Roam states, validate unit orders (#6666)
    This is a bag of changes, I'd like this to be reviewed.
    
    ### Work done
    
    - Removes Roam as a move state setting, otherwise visible but
    unreachable on the button for builders and build-assisters. Later, we
    could disable Roam like this more selectively: only in a Guard, Area
    Repair, or Fight command. But that is trickier to do.
    - Added #params == 5 for CMD.REPAIR, which is the new "leashed" repair
    command.
    - Prevented Guard orders on incomplete allied units, since this will
    cause builders to assist.
    - Check for units that are transferred instantly, namely extractor and
    geothermal unit upgrades and sidegrades.
    - Encapsulated all the common logic, so caused a lot of shuffling
    around, but hopefully this is modular enough to be updated more easily
    next time. This is a bit awkward, as noted, on
    `isBuilderAllowedCommand`.
    - Delayed re-validating the command queue on shared builders until the
    end of the sim frame, so that no matter how units are transferred and no
    matter the call order to UnitGiven, the commands dropped are always the
    same.
    - Added an intermittent peek to the UnitBuildStep callin. This is closer
    to an anti-cheat pattern, so I'm not so sure about it. On random frames,
    the AllowUnitBuildStep callin is reenabled in the gadget, and units with
    conflicting build tasks are added to the list for their command queue to
    re-validate at the end of the frame.
    
    #### Setup
    
    - Enable the disable_assist_ally_construction or easytax modoptions.
    
    #### Test steps
    
    - [ ] Builders should have the correct move states available on the
    button: Only two, Hold Position and Maneuver.
    - [ ] Builders should work as before: they cannot assist or guard allied
    units or allied constructions that are in-progress. They can still
    repair allies.
    - [ ] Sharing constructors in large batches when they are guarding one
    another or assisting building one another does not cause any of those
    commands to be dropped.
    - [ ] Assuming you know other methods of bypassing this gadget: That
    method should be annoying and unreliable.
    
    
    <img width="490" height="148"
    alt="{577F223A-9B9E-454D-85F9-E6B12CD3F552}"
    src="https://github.com/user-attachments/assets/e852b38f-ea1b-4d08-a159-9e1e78afa90f"
    />
    
    ---------
    
    Co-authored-by: efrec <quantumscrewdriver@gmail.com>
    Co-authored-by: sprunk <spr.ng@o2.pl>

--- luarules/gadgets/game_disable_assist_ally.lua
+++ luarules/gadgets/game_disable_assist_ally.lua
@@ -7,7 +7,7 @@ function gadget:GetInfo()
 		author  = 'Rimilel',
 		date    = 'April 2024',
 		license = 'GNU GPL, v2 or later',
-		layer   = 0,
+		layer   = 1, -- after unit_mex_upgrade_reclaimer and unit_geo_upgrade_reclaimer
 		enabled = Spring.GetModOptions().disable_assist_ally_construction or Spring.GetModOptions().easytax,
 	}
 end
@@ -16,25 +16,116 @@ if not gadgetHandler:IsSyncedCode() then
 	return false
 end
 
+local spAreTeamsAllied = Spring.AreTeamsAllied
+local spGetUnitCurrentCommand = Spring.GetUnitCurrentCommand
 local spGetUnitDefID = Spring.GetUnitDefID
+local spGetUnitIsBeingBuilt = Spring.GetUnitIsBeingBuilt
 local spGetUnitTeam = Spring.GetUnitTeam
+local spGetUnitsInCylinder = Spring.GetUnitsInCylinder
 
 local CMD_GUARD = CMD.GUARD
 local CMD_REPAIR = CMD.REPAIR
-local CMD_INSERT = CMD.INSERT
 local CMD_MOVESTATE = CMD.MOVE_STATE
+local MOVESTATE_ROAM = CMD.MOVESTATE_ROAM
+local CMD_INSERT = CMD.INSERT
+
+local footprintSize = Game.squareSize * Game.footprintScale
+
+-- Local state
+
+local builderMoveStateCmdDesc = {
+	params = { 1, "Hold pos", "Maneuver", --[["Roam"]] },
+}
 
 local gaiaTeam = Spring.GetGaiaTeamID()
 
+local isFactory = {}
 local canBuildStep = {} -- i.e. anything that spends resources when assisted
 for unitDefID, unitDef in ipairs(UnitDefs) do
+	isFactory[unitDefID] = unitDef.isFactory
 	canBuildStep[unitDefID] = unitDef.isFactory or (unitDef.isBuilder and (unitDef.canBuild or unitDef.canAssist))
 end
 
+local checkUnitCommandList = {} -- Delay validating given units so the order of calls to UnitGiven does not matter.
+
+-- Local functions
+
+local function removeRoamMoveState(unitID)
+	local index = Spring.FindUnitCmdDesc(unitID, CMD_MOVESTATE)
+	if index then
+		local moveState = select(2, Spring.GetUnitStates(unitID, false))
+		local params = builderMoveStateCmdDesc.params
+		params[1] = math.min(moveState or 1, MOVESTATE_ROAM - 1)
+		Spring.EditUnitCmdDesc(unitID, index, builderMoveStateCmdDesc)
+	end
+end
+
 local function isComplete(unitID)
-	local beingBuilt, buildProgress = Spring.GetUnitIsBeingBuilt(unitID)
+	local beingBuilt, buildProgress = spGetUnitIsBeingBuilt(unitID)
 	return not beingBuilt or buildProgress >= 1
 end
+
+local function isAlliedUnit(teamID, unitID)
+	local unitTeam = spGetUnitTeam(unitID)
+	return unitTeam and teamID ~= unitTeam and spAreTeamsAllied(teamID, unitTeam)
+end
+
+-- Awkward, because we get the params in a table format ~half the time.
+local function isBuilderAllowedCommand(cmdID, p1, p2, p5, p6, unitTeam)
+	if cmdID == CMD_GUARD then
+		return not isAlliedUnit(unitTeam, p1) or (isComplete(p1) and not canBuildStep[spGetUnitDefID(p1)])
+	elseif cmdID == CMD_REPAIR then
+		if p6 or (not p5 and p2) or not p1 then -- check for 1 or 5 arguments
+			return true -- Area Repair is okay.
+		end
+		return not isAlliedUnit(unitTeam, p1) or (isComplete(p1))
+	elseif cmdID == CMD_MOVESTATE then
+		return p1 ~= MOVESTATE_ROAM
+	else
+		return true
+	end
+end
+
+local function validateCommands(unitID, unitTeam)
+	local GetUnitCurrentCommand = spGetUnitCurrentCommand
+	local tags, count = {}, 0
+
+	for index = 1, Spring.GetUnitCommandCount(unitID) do
+		local command, _, tag, p1, p2, _, _, p5, p6 = GetUnitCurrentCommand(unitID, index)
+		if not isBuilderAllowedCommand(command, p1, p2, p5, p6, unitTeam) then
+			count = count + 1
+			tags[count] = tag
+		end
+	end
+
+	if count > 0 then
+		Spring.GiveOrderToUnit(unitID, CMD.REMOVE, tags)
+	end
+end
+
+local PARAM = table.new(6, 0) -- need to check up to 5+1 arguments
+local EMPTY = {}
+local insertedCommands = {
+	[CMD_GUARD] = true,
+	[CMD_REPAIR] = true,
+	[CMD_MOVESTATE] = true,
+}
+
+local function resolveCommand(cmdParams)
+	local cmdID = cmdParams[2] or 0
+	if insertedCommands[cmdID] then
+		local p = PARAM
+		for i = 1, 6 do
+			p[i] = cmdParams[i + 3]
+		end
+		cmdParams = p
+	else
+		cmdID, cmdParams = 0, EMPTY
+	end
+	return cmdID, cmdParams
+end
+
+-- Engine call-ins
 
 function gadget:Initialize()
 	gadgetHandler:RegisterAllowCommand(CMD_GUARD)
@@ -43,18 +134,20 @@ function gadget:Initialize()
 	gadgetHandler:RegisterAllowCommand(CMD_MOVESTATE)
 end
 
-local params = { 0, 0, 0 }
-local EMPTY = {} -- stupid
-
-local function resolveCommand(cmdID, cmdParams)
-	local p = params
-	p[1], p[2], p[3] = cmdParams[4], cmdParams[5], cmdParams[6]
-	cmdID, cmdParams = cmdParams[2], p
-	if cmdID ~= CMD_GUARD and cmdID ~= CMD_REPAIR then
-		return 0, EMPTY
-	else
-		return cmdID, cmdParams
+function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+	if canBuildStep[unitDefID] then
+		removeRoamMoveState(unitID)
 	end
+
+	-- In unit_{xyz}_upgrade_reclaimer, units are transferred instantly,
+	-- so we can check immediately whether they are bypassing the rules:
+	if isAlliedUnit(unitTeam, builderID) then
+		checkUnitCommandList[unitID] = spGetUnitTeam(builderID)
+	end
+end
+
+function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
+	checkUnitCommandList[unitID] = nil
 end
 
 function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions, cmdTag, synced)
@@ -63,60 +156,65 @@ function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdO
 	end
 
 	if cmdID == CMD_INSERT then
-		cmdID, cmdParams = resolveCommand(cmdID, cmdParams)
+		cmdID, cmdParams = resolveCommand(cmdParams)
 	end
 
-	-- Disallow guard commands onto labs, units that have buildOptions or can assist
-	if cmdID == CMD_GUARD then
-		local targetID = cmdParams[1]
-		local targetTeam = Spring.GetUnitTeam(targetID)
+	return isBuilderAllowedCommand(cmdID, cmdParams[1], cmdParams[2], cmdParams[5], cmdParams[6], unitTeam)
+end
 
-		if targetTeam and unitTeam ~= Spring.GetUnitTeam(targetID) and Spring.AreTeamsAllied(unitTeam, targetTeam) then
-			if canBuildStep[Spring.GetUnitDefID(targetID)] then
-				return false
+function gadget:AllowUnitCreation(unitDefID, builderID, builderTeam, x, y, z, facing)
+	-- Identical blueprints placed on top of one another are converted to build assist.
+	if builderID and not isFactory[spGetUnitDefID(builderID)] then
+		local units = spGetUnitsInCylinder(x, z, footprintSize)
+		for _, unitID in pairs(units) do
+			if unitDefID == spGetUnitDefID(unitID) and not isComplete(unitID) and isAlliedUnit(builderTeam, unitID) then
+				return false, false
 			end
 		end
-		return true
 	end
-
-	-- Also disallow assisting building (caused by a repair command) units under construction
-	-- Area repair doesn't cause assisting, so it's fine that we can't properly filter it
-	if cmdID == CMD_REPAIR and #cmdParams == 1 then
-		local targetID = cmdParams[1]
-		local targetTeam = Spring.GetUnitTeam(targetID)
-
-		if targetTeam and unitTeam ~= Spring.GetUnitTeam(targetID) and Spring.AreTeamsAllied(unitTeam, targetTeam) then
-			if not isComplete(targetID) then
-				return false
-			end
-		end
-		return true
-	end
-
-	-- Disallow setting builders to roam because roam + fight lets them assist a
-	if cmdID == CMD_MOVESTATE and cmdParams[1] == 2 then
-		return false
-	end
-
-	return true
+	return true, true
 end
 
 function gadget:UnitGiven(unitID, unitDefID, newTeam, oldTeam)
 	if newTeam ~= gaiaTeam and canBuildStep[unitDefID] then
-		local tags, count = {}, 0
-		local command, _, tag, p1, p2
-		local GetUnitCurrentCommand = Spring.GetUnitCurrentCommand
-		for index = 1, Spring.GetUnitCommandCount(unitID) do
-			command, _, tag, p1, p2 = GetUnitCurrentCommand(unitID, index)
-			if (command == CMD_GUARD or command == CMD_REPAIR) and (p1 and not p2) and spGetUnitTeam(p1) ~= newTeam then
-				if not isComplete(p1) or (command == CMD_GUARD and canBuildStep[spGetUnitDefID(p1)]) then
-					count = count + 1
-					tags[count] = tag
-				end
-			end
-		end
-		if count > 0 then
-			Spring.GiveOrderToUnit(unitID, CMD.REMOVE, tags)
+		checkUnitCommandList[unitID] = newTeam
+	end
+end
+
+local function _GameFramePost(unitList)
+	local GetUnitIsDead = Spring.GetUnitIsDead
+	for unitID, newTeam in pairs(unitList) do
+		unitList[unitID] = nil
+		if GetUnitIsDead(unitID) == false then
+			validateCommands(unitID, newTeam)
 		end
 	end
+end
+function gadget:GameFramePost()
+	-- We rarely need to call this function:
+	if next(checkUnitCommandList) then
+		_GameFramePost(checkUnitCommandList)
+	end
+end
+
+-- Temp anti-cheat-esque guard. We check on random frames for units bypassing the rules.
+local function AllowUnitBuildStep(self, builderID, builderTeam, unitID, unitDefID, part)
+    if part > 0 and builderTeam ~= spGetUnitTeam(unitID) and not isComplete(unitID) then
+		checkUnitCommandList[builderID] = builderTeam
+		return false
+    end
+	return true
+end
+
+local seed = math.random(91, 119) -- skip spawn-in frames
+
+function gadget:GameFrame(frame)
+    if frame % seed == 0 then
+        gadget.AllowUnitBuildStep = AllowUnitBuildStep
+        gadgetHandler:UpdateCallIn("AllowUnitBuildStep")
+    elseif gadget.AllowUnitBuildStep then
+        gadget.AllowUnitBuildStep = nil
+        gadgetHandler:UpdateCallIn("AllowUnitBuildStep")
+        seed = math.random(1, 119)
+    end
 end
