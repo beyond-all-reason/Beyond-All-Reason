9a8ce1bb102ccd24d3d06b6c721b365aaa4ad32a
efrec <ecfrechette@gmail.com>
2026-01-26 04:07:03 -0500

    gate target border bugfix behind feature flag (#6755)
    ### Work done
    
    - Gates the bugfix adjustment to weapon defs' `targetborder` value
    behind a feature flag for the bugged versions.
    - By adding a new constants.lua to handle extending the engine-provided
    feature flags (and other constants). The new feature flag is written to
    allow the engine to provide its own, at any point.
    
    #### Addresses Issue(s)
    
    - The current engine version used in the default/player-facing Engine
    Test has fixed this issue, while the live game is using the bugfix. We
    can support both until no longer necessary.
    
    ---------
    
    Co-authored-by: efrec <quantumscrewdriver@gmail.com>

---
+++ common/constants.lua
@@ -0,0 +1,26 @@
+-- constants.lua ---------------------------------------------------------------
+-- General game and engine constants, for use in general Lua environments.
+
+if not Engine or not Spring then return end
+
+--------------------------------------------------------------------------------
+-- Version handling ------------------------------------------------------------
+
+-- An enhancement to add ellipsoidal and cylindrical targeting volumes was bugged briefly for BeamLaser and LightningCannon.
+if Engine.FeatureSupport.targetBorderBug == nil then
+	local inRange = Spring.IsEngineMinVersion(2025, 6, 4) and not Spring.IsEngineMinVersion(2025, 6, 14)
+	Engine.FeatureSupport.targetBorderBug = inRange
+end
+
+--------------------------------------------------------------------------------
+-- Extended LuaConst -----------------------------------------------------------
+
+if CMD then
+	CMD.NIL   = "n" -- Handling for unintended nil's.
+	CMD.ANY   = "a" -- Matches on all command values.
+	CMD.BUILD = "b" -- Filters for negative commands.
+
+	CMD.n     = "NIL"
+	CMD.a     = "ANY"
+	CMD.b     = "BUILD"
+end
