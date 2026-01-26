91f95c7816caf86bde9a9ebd7e3d198e28c377a0
efrec <quantumscrewdriver@gmail.com>
2026-01-25 20:53:18 -0500

    create constants.lua

---
+++ common/constants.lua
@@ -0,0 +1,6 @@
+-- constants.lua ---------------------------------------------------------------
+-- General game and engine constants, for use in general Lua environments.
+
+local Engine = Engine
+local Game = Game
+
