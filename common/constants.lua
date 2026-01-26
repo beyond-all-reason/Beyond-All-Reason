c2f334f2a7c6f2796d1f9471f99b27902b84c53c
efrec <quantumscrewdriver@gmail.com>
2026-01-25 21:16:35 -0500

    move system constants

--- common/constants.lua
+++ common/constants.lua
@@ -1,6 +1,17 @@
 -- constants.lua ---------------------------------------------------------------
 -- General game and engine constants, for use in general Lua environments.
 
-local Engine = Engine
-local Game = Game
+if not Engine or not Spring then return end
 
+--------------------------------------------------------------------------------
+-- Extended LuaConst -----------------------------------------------------------
+
+if CMD then
+	CMD.NIL   = "n" -- Handling for unintended nil's.
+	CMD.ANY   = "a" -- Matches on all command values.
+	CMD.BUILD = "b" -- Filters for negative commands.
+
+	CMD.n     = "NIL"
+	CMD.a     = "ANY"
+	CMD.b     = "BUILD"
+end
