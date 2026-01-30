59c248ff35155defe2ced98ca06161f463b63ea3
Mitvit <112669238+Mitvit@users.noreply.github.com>
2026-01-30 22:17:31 +0200

    Update alldefs_post.lua
    custom values wont be overwritten

--- gamedata/alldefs_post.lua
+++ gamedata/alldefs_post.lua
@@ -1078,10 +1078,10 @@ function UnitDef_Post(name, uDef)
 	end
 
 	-- Sets idleautoheal to 5hp/s after 1800 frames aka 1 minute. 
-	-- NOTE: currently does not check for exceptions! So random copypaste will be overwritten.
-	-- If you really want a custom idleautoheal/time, edit this to allow that.
-	if uDef.health then
+	if uDef.idleautoheal == nil then
 		uDef.idleautoheal = 5
+	end
+	if uDef.idletime == nil then
 		uDef.idletime = 1800
 	end
 
