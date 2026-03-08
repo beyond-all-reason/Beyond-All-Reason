a948b56bba013ae71353aae7b1694b0d6bb062c8
Zain Mustafa <mustafa98537@gmail.com>
2026-03-08 15:42:01 -0700

    Fixes weird case where units continue firing after holdfire

--- luaui/Widgets/cmd_holdfire_fix.lua
+++ luaui/Widgets/cmd_holdfire_fix.lua
@@ -16,13 +16,21 @@ end
 
 -- Localized Spring API for performance
 local spGetGameFrame = Spring.GetGameFrame
+local spGetMyTeamID = Spring.GetMyTeamID
 
 local CMD_FIRE_STATE = CMD.FIRE_STATE
 local CMD_INSERT = CMD.INSERT
 local CMD_STOP = CMD.STOP
 local CMD_UNIT_CANCEL_TARGET = GameCMD.UNIT_CANCEL_TARGET
-local spGiveOrder = Spring.GiveOrder
+local spGiveOrderToUnit = Spring.GiveOrderToUnit
 local gameStarted
+local myTeam
+
+local function DropCurrentTarget(unitID)
+	-- STOP clears attack orders, UNIT_CANCEL_TARGET clears an active weapon lock
+	spGiveOrderToUnit(unitID, CMD_INSERT, {0, CMD_STOP, 0}, {"alt"})
+	spGiveOrderToUnit(unitID, CMD_INSERT, {0, CMD_UNIT_CANCEL_TARGET, 0}, {"alt"})
+end
 
 function maybeRemoveSelf()
     if Spring.GetSpectatingState() and (spGetGameFrame() > 0 or gameStarted) then
@@ -36,19 +44,19 @@ function widget:GameStart()
 end
 
 function widget:PlayerChanged(playerID)
+	myTeam = spGetMyTeamID()
     maybeRemoveSelf()
 end
 
 function widget:Initialize()
+	myTeam = spGetMyTeamID()
     if Spring.IsReplay() or spGetGameFrame() > 0 then
         maybeRemoveSelf()
     end
 end
 
-function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
-	if cmdID == CMD_FIRE_STATE and cmdParams[1] == 0 then
-		-- STOP clears attack orders, UNIT_CANCEL_TARGET clears an active lock 
-		spGiveOrder(CMD_INSERT, {0, CMD_STOP, 0}, {"alt"})
-		spGiveOrder(CMD_INSERT, {0, CMD_UNIT_CANCEL_TARGET, 0}, {"alt"})
+function widget:UnitCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOpts, cmdTag, playerID, fromSynced, fromLua)
+	if teamID == myTeam and cmdID == CMD_FIRE_STATE and cmdParams and cmdParams[1] == 0 then
+		DropCurrentTarget(unitID)
 	end
 end
