f8ff07ace51939bfeac345d76bcce751ec3275be
Wybren Koelmans <wybrenkoelmans@gmail.com>
2026-03-11 17:13:35 +0100

    simplifly setup but depend on the engine making OS available for unsynced gadgets

--- luarules/gadgets/api_replay_metadata.lua
+++ luarules/gadgets/api_replay_metadata.lua
@@ -13,7 +13,14 @@ function gadget:GetInfo()
 end
 
 if gadgetHandler:IsSyncedCode() then
-    -- SYNCED --
+	-- SYNCED --
+	return
+end
+
+-- check if IO is available, if not disable the gadget since it won't be able to save anything
+if not io then
+	Spring.Echo("[ReplayMetadata] IO library not available, disabling gadget")
+	gadgetHandler:RemoveGadget()
 	return
 end
 
@@ -31,15 +38,21 @@ end
 local function SaveReplayMetadata()
 	local jsonData = Json.encode(replayMetadata)
 
-Spring.Echo("[ReplayMetadata] Saving replay metadata, size: " .. tostring(#jsonData) .. " bytes")
-Spring.Echo(jsonData)
+	Spring.Echo("[ReplayMetadata] Saving replay metadata, size: " .. tostring(#jsonData) .. " bytes")
 
-	-- Forward to the companion widget which has io access
-	if Script.LuaUI('ReplayMetadata_WriteFile') then
-		Script.LuaUI.ReplayMetadata_WriteFile(jsonData)
-	else
-		Spring.Echo("[ReplayMetadata] Warning: writer widget not loaded, cannot save to file")
+	local saveFilePath = Spring.GetReplayRecordingFilePath()
+	-- replace .sdfz with .json
+	saveFilePath = saveFilePath:gsub("%.sdfz$", ".json")
+
+	local file, err = io.open(saveFilePath, "w")
+	if not file then
+		Spring.Echo("[ReplayMetadata] Error opening file for writing: " .. tostring(err))
+		return
 	end
+
+	file:write(jsonData)
+	file:close()
+	Spring.Echo("[ReplayMetadata] Replay metadata saved successfully")
 end
 
 function gadget:Initialize()
@@ -53,7 +66,6 @@ function gadget:Initialize()
 end
 
 function gadget:GameOver()
-	Spring.Echo("[ReplayMetadata] Game over, saving replay metadata...")
 	SaveReplayMetadata()
 	if GG.ReplayMetadata then
 		GG.ReplayMetadata = nil
