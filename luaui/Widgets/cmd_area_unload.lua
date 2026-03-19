82a58d1a5fd7e82b4c17706ce289803d666d6e2f
Fx-Doo <hedibertille93@gmail.com>
2026-03-19 12:11:29 +0100

    Please don't ask me why it broke in the first place

--- luaui/Widgets/cmd_area_unload.lua
+++ luaui/Widgets/cmd_area_unload.lua
@@ -61,18 +61,18 @@ function widget:CommandNotify(id, params, options)
 			--if (2 * mathPi * ray*ray)/(#units) >= 128*128 then -- Surface check to prevent clumping (needs GUI before enabling check)
 			local alpha = 1
 			local b = math.floor(alpha * math_sqrt(#units))
-			local phi = (math_sqrt(5) + 1) / 2
+			local phi = mathPi * (3 - math_sqrt(5))
 			local theta, r, x, y, z
 			for k = 1, #units do
 				if not shift then
 					spGiveOrderToUnit(units[k], CMD.STOP, {}, 0)
 				end
-				r = radius(k, #units, b)
-				theta = 2 * mathPi * k / phi * phi
+				local r = radius(k, #units, b)
+				local theta = k * phi
 				x = params[1] + r * math.cos(theta) * ray
 				z = params[3] + r * math.sin(theta) * ray
 				y = Spring.GetGroundHeight(x, z)
-				spGiveOrderToUnit(units[k], CMD.UNLOAD_UNIT, { x, y, z }, { "shift" })
+				spGiveOrderToUnit(units[k], CMD.UNLOAD_UNIT, { x, y, z}, { "shift" })
 			end
 			--end
 			return true
