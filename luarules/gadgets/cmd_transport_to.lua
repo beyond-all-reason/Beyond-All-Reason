f195299bb3868bb140a0ff57933a556b84230306
Stormlight <Littletexan06@icloud.com>
2026-03-20 11:52:51 -0500

    Update cmd_transport_to.lua

--- luarules/gadgets/cmd_transport_to.lua
+++ luarules/gadgets/cmd_transport_to.lua
@@ -2,7 +2,7 @@ function gadget:GetInfo()
     return {
         name = "Transport To Command",
         desc = "Ferry system",
-        author = "You",
+        author = "Isajoefeat",
         layer = 0,
         enabled = true
     }
