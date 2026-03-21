6ad5de1c6a916f954cf3d5b7b18aad0d73afb43b
Søren Markert <46371470+sorenmarkert@users.noreply.github.com>
2026-03-21 18:48:32 +0100

    Mission API: Give orders to named units (#6965)
    The IssueOrders action now accepts orders containing a unitName as parameter. Such orders are then split into a series of orders for the names units.

--- luarules/gadgets/api_missions.lua
+++ luarules/gadgets/api_missions.lua
@@ -35,9 +35,10 @@ function gadget:Initialize()
 	-- TODO: Actually pass script path
 	--scriptPath = 'mission-api-tests/validation_test.lua'
 	--scriptPath = 'mission-api-tests/test_mission.lua'
-	--scriptPath = 'mission-api-tests/markers_test.lua'
-	scriptPath = 'mission-api-tests/unit_triggers_test.lua'
 	--scriptPath = 'mission-api-tests/sound_test.lua'
+	--scriptPath = 'mission-api-tests/markers_test.lua'
+	--scriptPath = 'mission-api-tests/unit_triggers_test.lua'
+	scriptPath = 'mission-api-tests/issue_orders_test.lua'
 
 	if not scriptPath then
 		gadgetHandler:RemoveGadget()
