1f71f94937c31f510842a56372c0c5aac9e9eca7
Stormlight <Littletexan06@icloud.com>
2026-03-26 15:36:42 -0500

    Update cmd_customformations2.lua

--- luaui/Widgets/cmd_customformations2.lua
+++ luaui/Widgets/cmd_customformations2.lua
@@ -243,12 +243,13 @@ end
 
 
 local function SetColor(cmdID, alpha)
-    if     cmdID == CMD_MOVE       		then glColor(0.5, 1.0, 0.5, alpha) -- Green
+    if     cmdID == CMD_MOVE              then glColor(0.5, 1.0, 0.5, alpha) -- Green
     elseif cmdID == CMD_ATTACK
-		or cmdID == CMD_MANUAL_LAUNCH 	then glColor(1.0, 0.2, 0.2, alpha) -- Red
-    elseif cmdID == CMD_UNLOADUNIT 		then glColor(1.0, 1.0, 0.0, alpha) -- Yellow
-    elseif cmdID == CMD_SETTARGET 		then glColor(1.0, 0.7, 0.0, alpha) -- Orange
-    else                                glColor(0.5, 0.5, 1.0, alpha) -- Blue
+        or cmdID == CMD_MANUAL_LAUNCH     then glColor(1.0, 0.2, 0.2, alpha) -- Red
+    elseif cmdID == CMD_UNLOADUNIT
+        or cmdID == CMD_TRANSPORT_TO      then glColor(1.0, 0.8, 0.0, alpha) -- Orange
+    elseif cmdID == CMD_SETTARGET         then glColor(1.0, 0.7, 0.0, alpha) -- Orange
+    else                                       glColor(0.5, 0.5, 1.0, alpha) -- Blue
     end
 end
 
