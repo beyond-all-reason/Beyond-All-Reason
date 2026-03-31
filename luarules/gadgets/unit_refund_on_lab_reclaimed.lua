7268c09a69e3d68662ff2cb013ede913be481fba
Pexo <pexo.dev@gmail.com>
2026-03-31 22:46:32 +0200

    spaces->tabs

--- luarules/gadgets/unit_refund_on_lab_reclaimed.lua
+++ luarules/gadgets/unit_refund_on_lab_reclaimed.lua
@@ -1,19 +1,19 @@
 local gadget = gadget ---@type Gadget
 
 function gadget:GetInfo()
-    return {
-        name      = "Refund on lab reclaimed",
-        desc      = "Refunds metal when factory is reclaimed by ally while producing",
-        author    = "Pexo",
-        date      = "29.03.2026",
-        license   = "GNU GPL, v2 or later",
-        layer     = 0,
-        enabled   = true
-    }
+	return {
+		name      = "Refund on lab reclaimed",
+		desc      = "Refunds metal when factory is reclaimed by ally while producing",
+		author    = "Pexo",
+		date      = "29.03.2026",
+		license   = "GNU GPL, v2 or later",
+		layer     = 0,
+		enabled   = true
+	}
 end
 
 if not gadgetHandler:IsSyncedCode() then
-  return
+	return
 end
 
 local spGetUnitDefID = Spring.GetUnitDefID
@@ -28,39 +28,39 @@ local factoryQueue = {}
 
 local isFactory = {}
 for udid = 1, #UnitDefs do
-  local ud = UnitDefs[udid]
-  if ud.isFactory then
-    isFactory[udid] = true
-  end
+	local ud = UnitDefs[udid]
+	if ud.isFactory then
+		isFactory[udid] = true
+	end
 end
 
 function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-  if builderID and isFactory[spGetUnitDefID(builderID)] then
-    factoryQueue[builderID] = unitID
-  end
+	if builderID and isFactory[spGetUnitDefID(builderID)] then
+		factoryQueue[builderID] = unitID
+	end
 end
 
 function gadget:UnitFromFactory(unitID, unitDefID, unitTeam, factoryID)
-  factoryQueue[factoryID] = nil
+	factoryQueue[factoryID] = nil
 end
 
 function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam, weaponDefID)
-  local unitBeingBuiltId = factoryQueue[unitID]
-  if not unitBeingBuiltId then
-    return
-  end
+	local unitBeingBuiltId = factoryQueue[unitID]
+	if not unitBeingBuiltId then 
+		return
+	end
 
-  factoryQueue[unitID] = nil
-  if weaponDefID ~= reclaimedWeaponDefID then
-    return
-  end
+	factoryQueue[unitID] = nil
+	if weaponDefID ~= reclaimedWeaponDefID then 
+		return
+	end
 
-  if not attackerTeam or not spAreTeamsAllied(unitTeam, attackerTeam) then
-    return
-  end
+	if not attackerTeam or not spAreTeamsAllied(unitTeam, attackerTeam) then 
+		return
+	end
 
-  local _, buildProgress = spGetUnitIsBeingBuilt(unitBeingBuiltId)
-  local _, metalCost, _ = spGetUnitCosts(unitBeingBuiltId)
-  local refund = metalCost * buildProgress
-  spAddUnitResource(unitBeingBuiltId, "m", refund)
+	local _, buildProgress = spGetUnitIsBeingBuilt(unitBeingBuiltId)
+	local _, metalCost, _ = spGetUnitCosts(unitBeingBuiltId)
+	local refund = metalCost * buildProgress
+	spAddUnitResource(unitBeingBuiltId, "m", refund)
 end
