7a393d3eb474d9daf263835b374a123e4d15f245
Daniel Harvey <keithdanielharvey@gmail.com>
2026-04-10 17:21:22 -0600

    gen(bar_codemod): i18n-kikito

--- luaui/i18nhelpers.lua
+++ luaui/i18nhelpers.lua
@@ -12,7 +12,7 @@ local function refreshUnitDefs()
 			local proxyUnitDef = UnitDefNames[proxyUnitDefName]
 			proxyUnitDefName = proxyUnitDef and proxyUnitDef.customParams.i18nfromunit or proxyUnitDefName
 
-			local fromUnitHumanName = I18N("units.names." .. proxyUnitDefName)
+			local fromUnitHumanName = I18N.unitName(proxyUnitDefName)
 			humanName = I18N("units.scavenger", { name = fromUnitHumanName })
 
 			if i18nDescriptionEntries[unitDefName] then
@@ -22,7 +22,7 @@ local function refreshUnitDefs()
 			end
 		else
 			local proxyUnitDefName = unitDef.customParams.i18nfromunit or unitDefName
-			humanName = I18N("units.names." .. proxyUnitDefName)
+			humanName = I18N.unitName(proxyUnitDefName)
 			tooltip = I18N("units.descriptions." .. proxyUnitDefName)
 		end
 
