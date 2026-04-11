b1c62f7010de8b92caee00974c73fed0a24e66f9
efrec <quantumscrewdriver@gmail.com>
2026-04-11 15:09:33 -0400

    move evocom modoption to data file

--- gamedata/alldefs_post.lua
+++ gamedata/alldefs_post.lua
@@ -49,6 +49,7 @@ local isHalloween = holidays.halloween
 local isXmas = holidays.xmas
 local holidayModels = VFS.Include("unitbasedefs/holiday_models.lua")
 
+local evocomTweaks = VFS.Include("unitbasedefs/evocom.lua").Tweaks
 local airReworkTweaks = VFS.Include("unitbasedefs/air_rework_defs.lua").airReworkTweaks
 local skyshiftUnitTweaks = VFS.Include("unitbasedefs/skyshiftunits_post.lua").skyshiftUnitTweaks
 local proposed_unit_reworksTweaks = VFS.Include("unitbasedefs/proposed_unit_reworks_defs.lua").proposed_unit_reworksTweaks
@@ -348,68 +349,8 @@ local function unitDef_Post(name, uDef)
 		end
 	end
 
-
 	if modOptions.evocom then
-		if customparams.evocomlvl or name == "armcom" or name == "corcom" or name == "legcom" then
-			local comLevel = customparams.evocomlvl
-			if modOptions.comrespawn == "all" or modOptions.comrespawn == "evocom" then--add effigy respawning, if enabled
-				customparams.respawn_condition = "health"
-
-				local numBuildoptions = #buildoptions
-				if comLevel == 2 then
-					buildoptions[numBuildoptions + 1] = "comeffigylvl1"
-				elseif comLevel == 3 or comLevel == 4 then
-					buildoptions[numBuildoptions + 1] = "comeffigylvl2"
-				elseif comLevel == 5 or comLevel == 6 then
-					buildoptions[numBuildoptions + 1] = "comeffigylvl3"
-				elseif comLevel == 7 or comLevel == 8 then
-					buildoptions[numBuildoptions + 1] = "comeffigylvl4"
-				elseif comLevel == 9 or comLevel == 10 then
-					buildoptions[numBuildoptions + 1] = "comeffigylvl5"
-				end
-			end
-			customparams.combatradius = 0
-			customparams.evolution_health_transfer = "percentage"
-
-			if uDef.power then
-				uDef.power = uDef.power/modOptions.evocomxpmultiplier
-			else
-				uDef.power = ((uDef.metalcost+(uDef.energycost/60))/modOptions.evocomxpmultiplier)
-			end
-
-			if  name == "armcom" then
-				customparams.evolution_target = "armcomlvl2"
-				customparams.inheritxpratemultiplier = 0.5
-				customparams.childreninheritxp = "TURRET MOBILEBUILT"
-				customparams.parentsinheritxp = "TURRET MOBILEBUILT"
-				customparams.evocomlvl = 1
-				elseif name == "corcom" then
-				customparams.evolution_target = "corcomlvl2"
-				customparams.evocomlvl = 1
-				elseif name == "legcom" then
-				customparams.evolution_target = "legcomlvl2"
-				customparams.evocomlvl = 1
-				end
-
-			if modOptions.evocomlevelupmethod == "dynamic" then
-				customparams.evolution_condition = "power"
-				customparams.evolution_power_multiplier = 1			-- Scales the power calculated based on your own combined power.
-				local evolutionPowerThreshold = customparams.evolution_power_threshold or 10000 --sets threshold for level 1 commanders
-				customparams.evolution_power_threshold = evolutionPowerThreshold*modOptions.evocomlevelupmultiplier
-			elseif modOptions.evocomlevelupmethod == "timed" then
-				customparams.evolution_timer = modOptions.evocomleveluptime*60*customparams.evocomlvl
-				customparams.evolution_condition = "timer_global"
-			end
-
-			if comLevel and modOptions.evocomlevelcap <= comLevel then
-				customparams.evolution_health_transfer = nil
-				customparams.evolution_target = nil
-				customparams.evolution_condition = nil
-				customparams.evolution_timer = nil
-				customparams.evolution_power_threshold = nil
-				customparams.evolution_power_multiplier = nil
-			end
-		end
+		evocomTweaks(uDef, modOptions) -- also adds effigies for higher-level commanders
 	end
 
 	if customparams.evolution_target then
