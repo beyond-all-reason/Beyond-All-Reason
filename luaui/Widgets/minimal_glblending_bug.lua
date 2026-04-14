e8adcfadb365142fe45228ec5c797cd303674595
TheDujin <kevindujin@gmail.com>
2026-04-13 22:22:22 -0700

    Add minimal replicating example of the bug (delete this before final merge!)

---
+++ luaui/Widgets/minimal_glblending_bug.lua
@@ -0,0 +1,29 @@
+local widget = widget ---@type Widget
+
+function widget:GetInfo()
+	return {
+		name = "DEBUG NOMERGE",
+		desc = "Minimal reproducing example for the gl.Blending bug",
+		author = "TheDujin",
+		date = "April 2026",
+		license = "Irrelevant",
+		layer = 1,
+		enabled = true
+	}
+end
+
+local lastEchoFrame = 0
+
+function widget:Initialize()
+
+end
+
+
+function widget:DrawScreen()
+	gl.Blending(false)
+	local frame = Spring.GetGameFrame()
+	if frame - lastEchoFrame >= 60 then
+		lastEchoFrame = frame
+		Spring.Echo("REMOVE DEBUG NOMERGE WIDGET BEFORE MERGE")
+	end
+end
