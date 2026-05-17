cb3e88104acf1cf6a276f2e6c8d242305f0d431e
efrec <quantumscrewdriver@gmail.com>
2026-05-17 10:08:38 -0400

    simplified constant expression

--- luarules/gadgets/unit_sunfacing.lua
+++ luarules/gadgets/unit_sunfacing.lua
@@ -18,16 +18,14 @@ end
 
 local spCallCOBScript = Spring.CallCOBScript
 local mathAtan2 = math.atan2
-local mathDeg = math.deg
-local mathTau = math.tau
-local mathPi = math.pi
+local COBSCALE_HEADING = (COBSCALE / math.deg(math.tau)) / math.pi
 
 local sundir, mapinfo
 local success = false
 
 local function solarpoint(unitID, unitDefID, team)
 	if success then
-		local sunheading = mathAtan2(sundir[1], sundir[3]) * ((COBSCALE / mathDeg(mathTau)) / mathPi) -- WIZARDRY INTENSIFIES (182.04)
+		local sunheading = mathAtan2(sundir[1], sundir[3]) * COBSCALE_HEADING -- WIZARDRY INTENSIFIES (182.04)
 		spCallCOBScript(unitID, "solarreturn", 3, 1, sunheading)
 	else
 		spCallCOBScript(unitID, "solarreturn", 3, 0, 0)
