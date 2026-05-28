405fc0e38e7baade88c3229e197dfabefb490de9
Fx-Doo <hedibertille93@gmail.com>
2026-05-28 22:04:25 +0200

    updated load patterns to handle size 2 units

--- scripts/Units/airTransports/armdfly/setup.lua
+++ scripts/Units/airTransports/armdfly/setup.lua
@@ -4,18 +4,32 @@ return {
         transporterSeats      = 4,
 
         slots = {
-            { name = "link4x4",   size = 4, requires = { "frlink2x2", "fllink2x2", "brlink2x2", "bllink2x2" } },
-            { name = "frlink2x2", size = 1, requires = { "link4x4" } },
-            { name = "fllink2x2", size = 1, requires = { "link4x4" } },
-            { name = "brlink2x2", size = 1, requires = { "link4x4" } },
-            { name = "bllink2x2", size = 1, requires = { "link4x4" } },
+		-- 4 sized
+            { name = "link4x4",   size = 4, requires = { "flink3x3", "blink3x3", "rlink3x3", "llink3x3", "frlink2x2", "fllink2x2", "brlink2x2", "bllink2x2" } },
+		-- 2 sized
+	    { name = "flink3x3", size = 2, requires = { "link4x4", "rlink3x3", "llink3x3", "frlink2x2", "fllink2x2" } },
+	    { name = "blink3x3", size = 2, requires = { "link4x4", "rlink3x3", "llink3x3", "brlink2x2", "bllink2x2" } },
+	    { name = "rlink3x3", size = 2, requires = { "link4x4", "flink3x3", "blink3x3", "frlink2x2", "brlink2x2" } },
+	    { name = "llink3x3", size = 2, requires = { "link4x4", "flink3x3", "blink3x3", "fllink2x2", "bllink2x2" } },
+		-- 1 sized
+            { name = "frlink2x2", size = 1, requires = { "link4x4", "flink3x3", "rlink3x3" } },
+            { name = "fllink2x2", size = 1, requires = { "link4x4", "flink3x3", "llink3x3" } },
+            { name = "brlink2x2", size = 1, requires = { "link4x4", "blink3x3", "rlink3x3" } },
+            { name = "bllink2x2", size = 1, requires = { "link4x4", "blink3x3", "llink3x3" } },
         },
     },
     loadMethod = {
         cegScaleFactor = 0.7,
         cegName        = "tractorbeam",
         beams = {
+		-- 4 sized
             link4x4   = { "brbeam", "blbeam", "frbeam", "flbeam" },
+		-- 2 sized
+	    flink3x3 = { "flbeam", "frbeam" },
+	    blink3x3 = { "blbeam", "brbeam" },
+	    rlink3x3 = { "frbeam", "brbeam" },
+	    llink3x3 = { "flbeam", "blbeam" },
+		-- 1 sized
             frlink2x2 = { "frbeam" },
             fllink2x2 = { "flbeam" },
             brlink2x2 = { "brbeam" },
