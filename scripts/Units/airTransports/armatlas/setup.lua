bfc7d28b4543a95994ebf5bf57087d0f66f3a6d3
Fx-Doo <hedibertille93@gmail.com>
2026-06-04 15:45:21 +0200

    removed stale entries in setup.lua

--- scripts/Units/airTransports/armatlas/setup.lua
+++ scripts/Units/airTransports/armatlas/setup.lua
@@ -1,23 +1,4 @@
 return {
-    cargo = {
-        primarySlot = "link3x3",
-        transporterSeats      = 2,
-        slots = {
-            { name = "link3x3",   size = 2, requires = {"rlink2x2", "llink2x2"} },
-            { name = "rlink2x2",   size = 1, requires = { "link3x3" } },
-            { name = "llink2x2",   size = 1, requires = { "link3x3" } },
-        },
-    },
-    loadMethod = {
-        cegScaleFactor = 0.7,
-        cegName        = "tractorbeam",
-        beams = {
-            link3x3   = { "rbeam" , "lbeam"},
-            rlink2x2   = { "rbeam"},
-            llink2x2   = { "lbeam"},
-
-        },
-    },
     anim = {
         idleHover = {
             piece = "base",
