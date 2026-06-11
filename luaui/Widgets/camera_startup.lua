3655d9e3c769a7d771493750b8accc23b716c75e
Wybren Koelmans <wybrenkoelmans@gmail.com>
2026-06-11 19:56:11 +0200

    feat(camera-startup): add Camera Startup widget to initialize camera at game start
    - no animation/transition (instant at first frame)
    - fixed zoom level
    - doesn't touch camera rotation
    - positions start box in center

---
+++ luaui/Widgets/camera_startup.lua
@@ -0,0 +1,34 @@
+local widget = widget ---@type Widget
+
+function widget:GetInfo()
+	return {
+		name = "Camera Startup",
+		desc = "Organize the camera to point at the startbox at the start of the game",
+		author = "uBdead",
+		date = "June 2026",
+		license = "GNU LGPL, v2.1 or later",
+		layer = 1,
+		enabled = true
+	}
+end
+
+function widget:Initialize()
+    -- Calculate the center of the startbox
+    local xMin,zMin,xMax,zMax = Spring.GetAllyTeamStartBox(Spring.GetMyAllyTeamID())
+    if not xMin or not zMin or not xMax or not zMax then
+        Spring.Echo("Camera Startup: Failed to get startbox coordinates")
+        return
+    end
+
+    local centerX = (xMin + xMax) / 2
+    local centerZ = (zMin + zMax) / 2
+
+    Spring.SetCameraTarget(centerX, 0, centerZ, 0.0000001)
+
+    -- Set a reasonable zoom level
+    local currentCameraState = Spring.GetCameraState(true)
+    currentCameraState.height = 5000
+    currentCameraState.dist = 5000
+
+    Spring.SetCameraState(currentCameraState)
+end
