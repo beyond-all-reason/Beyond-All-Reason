8d3aca59d1dd72241505d40598d65dc890ba9591
Chemdude8 <chemdude8@hotmail.com>
2026-06-13 15:46:32 -0600

    Move to proper gadget location

--- luarules/gadgets/game_dynamic_maxunits.lua
+++ luarules/gadgets/game_dynamic_maxunits.lua
@@ -9,8 +9,8 @@ function gadget:GetInfo()
     return {
         name      = "Dynamic Maxunits",
         desc      = "redistributes unit limit",
-        author    = "Floris",
-        date      = "May 2024",
+        author    = "Floris, Chemdude8",
+        date      = "June 2026",
         license   = "GNU GPL, v2 or later",
         layer     = 0,
         enabled   = true
@@ -73,9 +73,16 @@ end
 local scavengerRaptorLimit = 3500  -- Minimum maxunits for Scavenger/Raptor teams
 local equalizationFactor = 0.25  -- How much to equalize (0 = no equalization, 1 = full equalization). 0.25 means go 25% of the way toward equal allyteam totals
 
+local spTransferTeamMaxUnits = Spring.TransferTeamMaxUnits
+local math = math
+local string = string
 local mathFloor = math.floor
 local mathMin = math.min
+local modOptions = Spring.GetModOptions()
+local gaiaTeamID = Spring.GetGaiaTeamID()
 
+-- Local long running variables
+local killedTeamToCountTable = {}     -- either the teamID of the killed player in gaia-mode, or killedTeam..attackerTeam to count in transfer mode
 -- Store the calculated target maxunits for each allyteam during initialization
 local allyTeamTargetMaxUnits = {}
 
@@ -473,3 +480,75 @@ function gadget:TeamChanged(teamID)
 		-- No action needed as it already has the remainder
 	end
 end
+
+function gadget:RecvLuaMsg(msg, playerID)
+	-- Incoming message expected to look like Spring.SendLuaRulesMsg('pct|d|'..myTeamID..'|'..populationPlayer.team..'|'..shareAmount)
+	if string.find(msg, 'pct') == 1 then
+		Spring.Echo("PopCapTransfer message received " .. msg .. " from player: " .. playerID)
+
+		local splitIterator = string.split(msg, "|")
+		local arguments = {}
+		for i, v in pairs(splitIterator) do
+			arguments[i] = v
+		end
+		arguments[3] = tonumber(arguments[3])
+		arguments[4] = tonumber(arguments[4])
+		arguments[5] = tonumber(arguments[5])
+
+		-- Ensure the 'from' player matches the message producer
+		if #arguments == 5 and arguments[2] == 'd' and playerID == arguments[3] then
+			Spring.Echo("PopCapTransfer message parsed " .. msg .. " from player: " .. playerID)
+			-- Direct transfer from one player giving to another
+			spTransferTeamMaxUnits(arguments[3], arguments[4], arguments[5]);
+		end
+	end
+end
+
+function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam, weaponDefID)
+	if modOptions.populationtransfer == "disabled" or modOptions.populationtransfer == nil or modOptions.populationtransferratio == 0 then
+		return
+	end
+
+	-- In gaia mode you might not need an attackerTeam but lava cases could punish you unnecessarily then
+	if unitTeam == nil or attackerTeam == nil then
+		return
+	end
+
+	local transferIncrement = nil
+	if modOptions.populationtransfer == "reduce" then
+		killedTeamToCountTable[unitTeam] = (killedTeamToCountTable[unitTeam] == nil and modOptions.populationtransferratio) or
+			killedTeamToCountTable[unitTeam] + modOptions.populationtransferratio;
+		if math.abs(killedTeamToCountTable[unitTeam]) >= 1 then
+			transferIncrement = 1
+			killedTeamToCountTable[unitTeam] = 0
+		end
+	elseif modOptions.populationtransfer == "transfer" then
+		killedTeamToCountTable[unitTeam .. attackerTeam] = (killedTeamToCountTable[unitTeam .. attackerTeam] == nil and modOptions.populationtransferratio) or
+			killedTeamToCountTable[unitTeam .. attackerTeam] + modOptions.populationtransferratio;
+		if math.abs(killedTeamToCountTable[unitTeam .. attackerTeam]) >= 1 then
+			transferIncrement = 1
+			killedTeamToCountTable[unitTeam .. attackerTeam] = 0
+		end
+	end
+
+	if (transferIncrement == nil or math.abs(transferIncrement) < 1) then
+		return
+	end
+
+	if modOptions.populationtransferratio > 0 then
+		if modOptions.populationtransfer == "reduce" then
+			spTransferTeamMaxUnits(unitTeam, gaiaTeamID, 1);
+		elseif modOptions.populationtransfer == "transfer" then
+			spTransferTeamMaxUnits(unitTeam, attackerTeam, 1);
+		end
+	else
+		-- Intentionally not allowing taking population from gaia currently
+		--[[ if modOptions.populationtransfer == "reduce" then
+                   spTransferTeamMaxUnits(GaiaTeamID, unitTeam, 1);
+            else ]]
+		if modOptions.populationtransfer == "transfer" then
+			spTransferTeamMaxUnits(attackerTeam, unitTeam, 1);
+		end
+	end
+end
+
