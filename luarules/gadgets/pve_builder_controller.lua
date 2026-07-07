187ec2ccf5c0762662cbe1fc7aad6e9fd260aae7
Daniel Harvey <keithdanielharvey@gmail.com>
2026-07-06 21:26:15 -0600

    env: expose detached BAR modules to widget/gadget sandbox
    Also bootstraps the namespace in the spec harness (WithGlobalsDefined):
    the builders' BAR init used to ride engine-builders-env, which now waits
    for spring-split.

--- luarules/gadgets/pve_builder_controller.lua
+++ luarules/gadgets/pve_builder_controller.lua
@@ -94,16 +94,11 @@ function gadget:GameFrame(frame)
 						if #turretOptions > 1 then
 							local turret = turretOptions[math.random(1, #turretOptions)]
 							local x, y, z = Spring.GetUnitPosition(unitID)
-							Spring.GiveOrderToUnit(
-								unitID,
-								-turret,
-								{
-									x + math.random(-data.range, data.range),
-									y,
-									z + math.random(-data.range, data.range),
-								},
-								{}
-							)
+							Spring.GiveOrderToUnit(unitID, -turret, {
+								x + math.random(-data.range, data.range),
+								y,
+								z + math.random(-data.range, data.range),
+							}, {})
 							if data.isFactory then
 								for i = 1, math.random(1, 5) do
 									Spring.GiveOrderToUnit(unitID, -turret, {
