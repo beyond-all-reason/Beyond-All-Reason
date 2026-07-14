0add5ccf958f9b4e4df10ac6375eb0d4db12048c
Floris <ruwetuin@hotmail.com>
2026-07-14 18:21:31 +0200

    added i18n to unsynced gadget space (#8274)

--- modules/i18n/i18n.lua
+++ modules/i18n/i18n.lua
@@ -1,6 +1,7 @@
 local currentDirectory = "modules/i18n/"
 I18N_PATH = currentDirectory .. "i18nlib/i18n/" -- I18N_PATH is expected to be global inside the i18n module
 local i18n = VFS.Include(I18N_PATH .. "init.lua", nil, VFS.ZIP)
+local rmlUi = rawget(_G, 'RmlUi')
 
 local asianFont = 'fallbacks/SourceHanSans-Regular.ttc'
 local translationDirs = VFS.SubDirs('language')
@@ -8,22 +9,37 @@ local translationDirs = VFS.SubDirs('language')
 -- map of languageCode -> map of translation key -> translation string
 local languageTranslations = {}
 
-local function loadTranslationTable(languageCode, currentContext, data)
+local loadTranslationTable
+
+local function loadTranslationValue(languageCode, composedKey, value)
+	languageTranslations[languageCode][composedKey] = value
+end
+
+local function loadNestedTranslations(languageCode, composedKey, value)
+	loadTranslationTable(languageCode, composedKey, value)
+end
+
+local function ignoreTranslationValue()
+end
+
+local translationValueHandlers = {
+	string = loadTranslationValue,
+	table = loadNestedTranslations,
+}
+
+loadTranslationTable = function(languageCode, currentContext, data)
 	local composedKey
 	for k,v in pairs(data) do
 		composedKey = (currentContext and (currentContext .. '.') or "") .. tostring(k)
-		if type(v) == 'string' then
-			languageTranslations[languageCode][composedKey] = v
-		elseif type(v) == 'table' then
-			loadTranslationTable(languageCode, composedKey, v)
-		end
+		local valueHandler = translationValueHandlers[type(v)] or ignoreTranslationValue
+		valueHandler(languageCode, composedKey, v)
 	end
 end
 
 local function loadRmlTranslations(languageCode)
-	RmlUi.ClearTranslations()
+	rmlUi.ClearTranslations()
 	for k,v in pairs(languageTranslations[languageCode]) do
-		RmlUi.AddTranslationString('!!' .. k, v)
+		rmlUi.AddTranslationString('!!' .. k, v)
 	end
 end
 
@@ -38,12 +54,8 @@ end
 
 -- Loads all language translation files associated with languageCode.
 local function loadLanguageFiles(languageCode)
-	if languageFiles[languageCode] == nil then
-		return
-	end
-
 	languageTranslations[languageCode] = {}
-	for _, file in ipairs(languageFiles[languageCode]) do
+	for _, file in ipairs(languageFiles[languageCode] or {}) do
 		local i18nJson = VFS.LoadFile(file)
 		local i18nLua = { [languageCode] = Json.decode(i18nJson) }
 		loadTranslationTable(languageCode, nil, i18nLua[languageCode])
@@ -51,18 +63,8 @@ local function loadLanguageFiles(languageCode)
 	end
 end
 
--- Map of language code -> whether or not the language files are loaded.
-local languageLoaded = {}
-
--- Ensures that language translation files associated with languageCode are
--- loaded (if any).
 local function ensureLanguageLoaded(languageCode)
-	if languageLoaded[languageCode] then
-		return
-	end
-
 	loadLanguageFiles(languageCode)
-	languageLoaded[languageCode] = true
 end
 
 i18n.loadFile('language/test_unicode.lua')
@@ -84,25 +86,10 @@ ensureLanguageLoaded('en')
 function i18n.setLanguage(language)
 	ensureLanguageLoaded(language)
 	i18n.setLocale(language)
+end
+
+function i18n.loadWidgetLanguage(language)
 	loadRmlTranslations(language)
-
-	if gl.AddFallbackFont then return end
-
-	-- Font substitution is handled at the OS level, meaning we cannot control which fallback font is used
-	-- Manually switching fonts is requred until Spring handles font substitution at the engine level
-	-- LuaUI reload must be invoked for widgets to refresh all their font objects
-	local asianLanguage = language == 'zh'
-	local currentFont = Spring.GetConfigString('bar_font')
-
-	if asianLanguage and currentFont ~= asianFont then
-		Spring.SetConfigString("bar_font", asianFont)
-		Spring.SetConfigString("bar_font2", asianFont)
-		Spring.SendCommands("luarules reloadluaui")
-	elseif not asianLanguage and currentFont == asianFont then
-		Spring.SetConfigString("bar_font", "Poppins-Regular.otf")
-		Spring.SetConfigString("bar_font2", "Exo2-SemiBold.otf")
-		Spring.SendCommands("luarules reloadluaui")
-	end
 end
 
 return i18n
