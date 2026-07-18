e59a77814c51fcf3ad7375ec198fdef65f3082c7
efrec <quantumscrewdriver@gmail.com>
2026-07-18 18:35:18 -0400

    add set target variant, update description and init

--- luaui/Widgets/cmd_exclude_walls_area_attacks.lua
+++ luaui/Widgets/cmd_exclude_walls_area_attacks.lua
@@ -3,7 +3,7 @@ local widget = widget ---@type Widget
 function widget:GetInfo()
 	return {
 		name 		= "Exclude walls from area attacks",
-		desc 		= "Stops walls from being included in area attacks if units are present",
+		desc 		= "Removes walls and neutrals from area attacks if other units are present",
 		date		= "April 2025",
 		author		= "Slouse",
 		license 	= "GNU GPL, v2 or later",
@@ -12,30 +12,37 @@ function widget:GetInfo()
 	}
 end
 
-local CMD_UNIT_CANCEL_TARGET = GameCMD.UNIT_CANCEL_TARGET
-local CMD_UNIT_SET_TARGET = GameCMD.UNIT_SET_TARGET
-local CMD_ATTACK = CMD.ATTACK
-local CMD_STOP = CMD.STOP
+-- Keep in sync with unit_areaattack_limiter.lua
+local BATCH_LIMIT = 30
 
-local excludedUnitsDefID = {}
+local commandAreaToUnits = {
+	[CMD.ATTACK] = true,
+	[GameCMD.UNIT_SET_TARGET] = true,
+	[GameCMD.UNIT_SET_TARGET_NO_GROUND] = true,
+}
+
+local commandClearOrders = {
+	[GameCMD.UNIT_SET_TARGET]           = CMD.CMD_UNIT_CANCEL_TARGET,
+	[GameCMD.UNIT_SET_TARGET_NO_GROUND] = CMD.CMD_UNIT_CANCEL_TARGET,
+}
+for cmdID in pairs(commandAreaToUnits) do
+	commandClearOrders[cmdID] = commandClearOrders[cmdID] or CMD.STOP
+end
 
 local spGetUnitDefID = Spring.GetUnitDefID
 local spGetUnitNeutral = Spring.GetUnitNeutral
 local spGetSelectedUnits = Spring.GetSelectedUnits
 
--- Keep in sync with unit_areaattack_limiter.lua
-local BATCH_LIMIT = 30
+local CMD_ATTACK = CMD.ATTACK
 
-local isBombWeapon = {}
-for weaponDefID, weaponDef in pairs(WeaponDefs) do
-	if weaponDef.type == "AircraftBomb" then
-		isBombWeapon[weaponDefID] = true
-	end
-end
-
+local excludedUnitsDefID = {}
 local isBomberUnitDef = {}
 for unitDefID, unitDef in pairs(UnitDefs) do
-	if (unitDef.weapons and unitDef.weapons[1] and isBombWeapon[unitDef.weapons[1].weaponDef])
+	if unitDef.customParams.objectify then
+		excludedUnitsDefID[unitDefID] = true
+	end
+	-- Matching with unit_areaattack_limiter:
+	if (unitDef.weapons and unitDef.weapons[1] and WeaponDefs[unitDef.weapons[1].weaponDef].type == "AircraftBomb")
 		or string.find(unitDef.name, "armlance")
 		or string.find(unitDef.name, "cortitan")
 		or string.find(unitDef.name, "legatorpbomber") then
@@ -60,7 +67,7 @@ local function addNewCommand(newCmds, unitID, cmdOpts, cmdID)
 end
 
 function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
-	if (cmdID ~= CMD_UNIT_SET_TARGET and cmdID ~= CMD_ATTACK) or #cmdParams ~= 4 then
+	if not commandAreaToUnits[cmdID] or #cmdParams ~= 4 then
 		return
 	end
 
