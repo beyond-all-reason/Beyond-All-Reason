100605f23b2dbc95f89c9f8b03499b0be5a8137f
Damgam <c_2_damiano@op.pl>
2026-07-27 15:15:38 +0200

    Now supports infinitely deep, automatically picked up subfolders

--- luarules/mission_api/actions_loader.lua
+++ luarules/mission_api/actions_loader.lua
@@ -7,24 +7,13 @@ local function loadActionDefinitions()
 	local typesCount = 0
 	local parameters = {}
 	local actionFunctions = {}
-	local actionsSubdirs = VFS.SubDirs(ACTIONS_DIR)
+	local actionsSubdirs = {
+		ACTIONS_DIR,
+	}
 
-	local mainActionFiles = VFS.DirList(ACTIONS_DIR, ACTION_FILES_PATTERN) -- Keep this until all action files are in subfolders
-	for _, filePath in ipairs(mainActionFiles) do
-		local actionDefinitions = VFS.Include(filePath)
-		for _, actionDefinition in ipairs(actionDefinitions) do
-			typesCount = typesCount + 1
-			local actionType = actionDefinition.type
-
-			types[actionType] = typesCount
-			parameters[typesCount] = actionDefinition.parameters or {}
-			actionFunctions[typesCount] = actionDefinition.actionFunction
-		end
-	end
-
-	for _, subdirPath in ipairs(actionsSubdirs) do
-		Spring.Echo("subdirPath", subdirPath)
-		local actionFiles = VFS.DirList(subdirPath, ACTION_FILES_PATTERN)
+	repeat
+		--Spring.Echo("Mission API Actions SubDir", actionsSubdirs[1])
+		local actionFiles = VFS.DirList(actionsSubdirs[1], ACTION_FILES_PATTERN)
 		for _, filePath in ipairs(actionFiles) do
 			local actionDefinitions = VFS.Include(filePath)
 			for _, actionDefinition in ipairs(actionDefinitions) do
@@ -36,8 +25,13 @@ local function loadActionDefinitions()
 				actionFunctions[typesCount] = actionDefinition.actionFunction
 			end
 		end
-	end
 
+		for _, subDir in pairs(VFS.SubDirs(actionsSubdirs[1])) do
+			actionsSubdirs[#actionsSubdirs + 1] = subDir
+		end
+
+		table.remove(actionsSubdirs, 1)
+	until #actionsSubdirs == 0
 
 	return {
 		Types = types,
