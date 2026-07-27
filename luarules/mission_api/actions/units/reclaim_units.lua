3cffd91dbda309d3df0c943e697592ffdd1de81d
Damgam <c_2_damiano@op.pl>
2026-07-27 11:34:10 +0200

    Action files SubDirs
    - Prepared Actions Folder for subdirectories
    - SubDirs are autodetected
    - No subdirs inside subdirs
    - Moved Unit and Feature actions to subfolders
    - Backwards compatible with non-subdir files, easy to remove when no longer needed.

---
+++ luarules/mission_api/actions/units/reclaim_units.lua
@@ -0,0 +1,35 @@
+local ParameterTypes = GG['MissionAPI'].Modules.ParameterTypes.Types
+
+local function reclaimUnits(unitName, reclaimerTeam)
+    local tracking = GG['MissionAPI'].Modules.Tracking
+	if tracking.IsUnitNameUntracked(unitName) then return end
+
+    -- Copying table as UnitKilled trigger with SpawnUnits with the same name could cause infinite loop.
+    local trackedUnitIDs = table.copy(GG['MissionAPI'].trackedUnitIDs[unitName])
+	for unitID in pairs(trackedUnitIDs) do
+        if Spring.GetUnitIsDead(unitID) == false then
+            if not reclaimerTeam then
+                reclaimerTeam = Spring.GetUnitTeam(unitID)
+            end
+            local unitDef = UnitDefs[Spring.GetUnitDefID(unitID)]
+            if unitDef and unitDef.metalCost then
+                Spring.AddTeamResource(reclaimerTeam, "metal", unitDef.metalCost)
+            end
+            -- if unitDef and unitDef.energyCost then -- We don't give energy from reclaims, but putting it here just in case someone needs it later.
+            --     Spring.AddTeamResource(reclaimerTeam, "energy", unitDef.energyCost)
+            -- end
+            Spring.DestroyUnit(unitID, false, true)
+        end
+	end
+end
+
+return {
+    {
+	    type = 'ReclaimUnits',
+	    parameters = {
+	    	{ name = 'unitName', required = true, type = ParameterTypes.UnitName },
+	    	{ name = 'reclaimerTeam', required = false, type = ParameterTypes.TeamID },
+	    },
+	    actionFunction = reclaimUnits,
+    },
+}
--- luarules/mission_api/actions_loader.lua
+++ luarules/mission_api/actions_loader.lua
@@ -2,14 +2,16 @@ local ACTIONS_DIR = 'luarules/mission_api/actions/'
 local ACTION_FILES_PATTERN = '*.lua'
 
 local function loadActionDefinitions()
-	local actionFiles = VFS.DirList(ACTIONS_DIR, ACTION_FILES_PATTERN)
+	--local 
 
 	local types = {}
 	local typesCount = 0
 	local parameters = {}
 	local actionFunctions = {}
+	local actionsSubdirs = VFS.SubDirs(ACTIONS_DIR)
 
-	for _, filePath in ipairs(actionFiles) do
+	local mainActionFiles = VFS.DirList(ACTIONS_DIR, ACTION_FILES_PATTERN) -- Keep this until all action files are in subfolders
+	for _, filePath in ipairs(mainActionFiles) do
 		local actionDefinitions = VFS.Include(filePath)
 		for _, actionDefinition in ipairs(actionDefinitions) do
 			typesCount = typesCount + 1
@@ -20,6 +22,23 @@ local function loadActionDefinitions()
 			actionFunctions[typesCount] = actionDefinition.actionFunction
 		end
 	end
+
+	for _, subdirPath in ipairs(actionsSubdirs) do
+		Spring.Echo("subdirPath", subdirPath)
+		local actionFiles = VFS.DirList(subdirPath, ACTION_FILES_PATTERN)
+		for _, filePath in ipairs(actionFiles) do
+			local actionDefinitions = VFS.Include(filePath)
+			for _, actionDefinition in ipairs(actionDefinitions) do
+				typesCount = typesCount + 1
+				local actionType = actionDefinition.type
+
+				types[actionType] = typesCount
+				parameters[typesCount] = actionDefinition.parameters or {}
+				actionFunctions[typesCount] = actionDefinition.actionFunction
+			end
+		end
+	end
+
 
 	return {
 		Types = types,
