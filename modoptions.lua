183116a7059af01ec023fd9210e9fd3e9f224fd7
starlord <marlon.abastar@gmail.com>
2026-07-28 18:47:21 +0800

    Add extreme difficulty option for Scavenger Zombies
    The modconfig is already there, just wasn't included.

--- modoptions.lua
+++ modoptions.lua
@@ -1523,6 +1523,8 @@ local options = {
             { key = "normal", name = "Normal", desc = "Slow revival rate, normal strength."},
             { key = "hard", name = "Hard", desc = "Fast revival rate, stronger Scavenger Zombies." },
             { key = "nightmare", name = "Nightmare", desc = "Extreme revival rate, stronger Scavenger Zombies, 2-5 spawn per corpse." },
+			{ key = "extreme", name = "Extreme", desc = "Extreme revival rate, stronger Scavenger Zombies, 3-6 spawn per corpse." },
+
         }
     },
 
