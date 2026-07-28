3741a18c3ce7748b771e66300c506bcd01356d33
Rysica <rysica@noreply.github.com>
2026-07-28 10:25:47 -0700

    test: fix extractor upgrade test SyncedRun locals and enable Legion case

--- luaui/Tests/mex-building/test_extractor_upgrade_rules.lua
+++ luaui/Tests/mex-building/test_extractor_upgrade_rules.lua
@@ -10,63 +10,57 @@ function cleanup()
 	Test.clearMap()
 end
 
-local function createExtractor(unitName, teamID)
-	return SyncedRun(function(locals)
-		local x, z = Game.mapSizeX / 2, Game.mapSizeZ / 2
-		local y = Spring.GetGroundHeight(x, z)
-		local unitID = Spring.CreateUnit(locals.unitName, x, y, z, 0, locals.teamID)
-		assert(unitID, "failed to create " .. tostring(locals.unitName))
-		return unitID
-	end)
-end
-
-local function assertUpgrade(currentName, newName, expected, label)
-	Test.clearMap()
-
-	local myTeamID = Spring.GetMyTeamID()
-	local currentUnitID = createExtractor(currentName, myTeamID)
-	local newDefID = UnitDefNames[newName].id
-
-	local canUpgrade = WG["resource_spot_builder"].ExtractorCanBeUpgraded(currentUnitID, newDefID)
-	assertEqual(
-		canUpgrade,
-		expected,
-		string.format(
-			"%s: expected ExtractorCanBeUpgraded(%s -> %s) == %s, got %s",
-			label,
-			currentName,
-			newName,
-			tostring(expected),
-			tostring(canUpgrade)
-		)
-	)
-end
-
 function test()
 	assert(WG["resource_spot_builder"], "resource_spot_builder API widget not loaded")
 	assert(WG["resource_spot_builder"].ExtractorCanBeUpgraded, "ExtractorCanBeUpgraded missing")
 
-	-- 1. arm mex upgradable to arm stealthy mex
-	assertUpgrade("armmex", "armamex", true, "1")
+	local myTeamID = Spring.GetMyTeamID()
+	local x, z = Game.mapSizeX / 2, Game.mapSizeZ / 2
+	local y = Spring.GetGroundHeight(x, z)
+	local facing = 0
 
-	-- 2. arm stealthy mex not upgradable to arm t1 mex
-	assertUpgrade("armamex", "armmex", false, "2")
+	local cases = {
+		-- label, current unit, new unit, expected
+		{ "1", "armmex", "armamex", true },       -- arm mex -> stealthy mex
+		{ "2", "armamex", "armmex", false },      -- stealthy mex -> t1 mex
+		{ "3", "armgmm", "armageo", true },       -- prude -> ageo
+		{ "4", "armageo", "armgmm", true },       -- ageo -> prude
+		{ "5", "corbhmth", "legrampart", true },  -- cerberus -> legion rampart
+		{ "6", "armgeo", "corgeo", false },       -- t1 geo cross-faction
+		{ "7", "armageo", "corageo", false },     -- ageo cross-faction
+		{ "8", "armmoho", "cormoho", false },     -- t2 mex cross-faction
+	}
 
-	-- 3. prude upgradable to ageo
-	assertUpgrade("armgmm", "armageo", true, "3")
+	for i = 1, #cases do
+		local label, currentName, newName, expected = cases[i][1], cases[i][2], cases[i][3], cases[i][4]
 
-	-- 4. ageo upgradable to prude
-	assertUpgrade("armageo", "armgmm", true, "4")
+		assert(UnitDefNames[currentName], "missing UnitDefNames." .. currentName)
+		assert(UnitDefNames[newName], "missing UnitDefNames." .. newName)
 
-	-- 5. cerberus upgradable into legion antinuke jammer geo (Rampart)
-	assertUpgrade("corbhmth", "legrampart", true, "5")
+		Test.clearMap()
 
-	-- 6. t1 geo for one faction not upgradable to the same-tier t1 geo from another faction
-	assertUpgrade("armgeo", "corgeo", false, "6")
+		local unitName = currentName
+		local teamID = myTeamID
+		local currentUnitID = SyncedRun(function(locals)
+			return Spring.CreateUnit(locals.unitName, locals.x, locals.y, locals.z, locals.facing, locals.teamID)
+		end)
+		assert(currentUnitID, "failed to create " .. currentName)
 
-	-- 7. ageo for one faction not upgradable to ageo from another faction
-	assertUpgrade("armageo", "corageo", false, "7")
-
-	-- 8. t2 mex not upgradable into t2 mex for another faction
-	assertUpgrade("armmoho", "cormoho", false, "8")
+		local canUpgrade = WG["resource_spot_builder"].ExtractorCanBeUpgraded(
+			currentUnitID,
+			UnitDefNames[newName].id
+		)
+		assertEqual(
+			canUpgrade,
+			expected,
+			string.format(
+				"%s: expected ExtractorCanBeUpgraded(%s -> %s) == %s, got %s",
+				label,
+				currentName,
+				newName,
+				tostring(expected),
+				tostring(canUpgrade)
+			)
+		)
+	end
 end
