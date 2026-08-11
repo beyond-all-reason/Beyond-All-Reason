80a782b6265ced675a27405b49d4283583ddc046
efrec <quantumscrewdriver@gmail.com>
2026-08-11 11:01:54 -0400

    mission tests keep repeating this load order, DRY it

--- spec/spec_helper.lua
+++ spec/spec_helper.lua
@@ -69,6 +69,17 @@ _G.Spring.GetTeamAllyTeamID = _G.Spring.GetTeamAllyTeamID or function(teamID) re
 
 _G.GG = _G.GG or {}
 
+-- Build the modules and shims and shams for the Mission API. These have a specific load order.
+function _G.registerMissionApiModules()
+    _G.GG['MissionAPI'] = _G.GG['MissionAPI'] or {}
+    local modules = _G.GG['MissionAPI'].Modules or {}
+    _G.GG['MissionAPI'].Modules = modules
+    modules.ParameterTypes  = modules.ParameterTypes  or VFS.Include('luarules/mission_api/parameter_types.lua')
+    modules.SeismicContacts = modules.SeismicContacts or VFS.Include('luarules/mission_api/seismic_contacts.lua')
+    modules.DetectionLevels = modules.DetectionLevels or VFS.Include('luarules/mission_api/detection_levels.lua')
+    return modules
+end
+
 _G.CMD     = _G.CMD or {}
 _G.GameCMD = _G.GameCMD or {}
 
