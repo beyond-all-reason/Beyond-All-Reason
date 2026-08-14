4d57edd028d233e835179d03221f2046ccd0bfea
efrec <quantumscrewdriver@gmail.com>
2026-08-14 16:15:50 -0400

    (maybe) migrate nanoframe_hax

--- luarules/gadgets/unit_prevent_nanoframe_blocking_hax.lua
+++ luarules/gadgets/unit_prevent_nanoframe_blocking_hax.lua
@@ -18,28 +18,19 @@ end
 
 local blockingBuildProgress = 0.05
 
-local newNanoFrames = {} -- array table, i -> unitID
 local newNanoFrameNeutralState = {} -- hash table, unitID -> original neutral state
-local nanoFrameIdxToRemove = {}
 local CMD_ATTACK = CMD.ATTACK
 
 local function AddNanoFrame(unitID)
-	newNanoFrames[#newNanoFrames + 1] = unitID
-
 	local a, b, c, d, e, f, g = Spring.GetUnitBlocking(unitID)
 	Spring.SetUnitBlocking(unitID, a, b, false, d, e, f, g) -- non-blocking for projectiles
 
 	local neutral = Spring.GetUnitNeutral(unitID)
 	newNanoFrameNeutralState[unitID] = neutral
 	Spring.SetUnitNeutral(unitID, true)
-
-	--Spring.Echo("AddNanoFrame", #newNanoFrames, unitID)
 end
 
-local function RemoveNanoFrame(i)
-	local unitID = newNanoFrames[i]
-	--Spring.Echo("RemoveNanoFrame", i, unitID, newNanoFrameNeutralState[unitID])
-
+local function removeNanoFrame(unitID)
 	if Spring.ValidUnitID(unitID) then
 		local a, b, c, d, e, f, g = Spring.GetUnitBlocking(unitID)
 		Spring.SetUnitBlocking(unitID, a, b, true, d, e, f, g) -- blocking for projectiles
@@ -50,43 +41,10 @@ local function RemoveNanoFrame(i)
 			Spring.SetUnitNeutral(unitID, neutral)
 		end
 	end
-	table.remove(newNanoFrames, i)
 	newNanoFrameNeutralState[unitID] = nil
 end
 
-local function GetNanoFrameIdx(unitID)
-	local i = 1
-	while newNanoFrames[i] ~= unitID and i <= #newNanoFrames do
-		i = i + 1
-	end
-	if i > #newNanoFrames then
-		return
-	end
-	return i
-end
-
-local function CheckUnit(unitID)
-	-- check if we should remove this unit from our list
-	if not Spring.ValidUnitID(unitID) then
-		--Spring.Echo("to remove (invalid)", unitID)
-		return true
-	end
-
-	if Spring.GetUnitIsDead(unitID) then
-		--Spring.Echo("to remove (dead)", unitID)
-		return true
-	end
-
-	local _, buildProgress = Spring.GetUnitIsBeingBuilt(unitID)
-	if buildProgress >= blockingBuildProgress then
-		return true
-	end
-
-	return false
-end
-
 function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-	--local _,buildProgress = Spring.GetUnitIsBeingBuilt(unitID)
 	if builderID then
 		local _, _, projectileBlocking = Spring.GetUnitBlocking(unitID)
 		if projectileBlocking then
@@ -95,14 +53,14 @@ function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
 	end
 end
 
-function gadget:GameFrame(n)
-	local i = 1
-	while i <= #newNanoFrames do
-		local unitID = newNanoFrames[i]
-		if CheckUnit(unitID) then
-			RemoveNanoFrame(i)
-		else
-			i = i + 1
+function gadget:UnitBuildStepPost(unitIDs, count)
+	for i = 1, count do
+		local unitID = unitIDs[i]
+		if newNanoFrameNeutralState[unitID] ~= nil then
+			local _, buildProgress = Spring.GetUnitIsBeingBuilt(unitID)
+			if buildProgress and buildProgress >= blockingBuildProgress then
+				removeNanoFrame(unitID)
+			end
 		end
 	end
 end
@@ -118,10 +76,7 @@ function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOpt
 end
 
 function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, builderID)
-	if newNanoFrameNeutralState[unitID] ~= nil then
-		--Spring.Echo("to remove (destroyed)", unitID)
-		nanoFrameIdxToRemove[#nanoFrameIdxToRemove + 1] = GetNanoFrameIdx(unitID)
-	end
+	newNanoFrameNeutralState[unitID] = nil
 end
 
 function gadget:Initialize()
