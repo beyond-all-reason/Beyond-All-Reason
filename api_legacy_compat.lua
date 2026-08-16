ebf49f39292fd1fd0447ebab1c968ecd8c4b0c9b
crossgamervn-ux <crossgamervn@gmail.com>
2026-08-17 03:09:52 +0700

    Add legacy compatibility layer for BAR modules
    This file implements a compatibility layer for bridging detached BAR modules back to Spring, enabling older community widgets to function properly.

---
+++ api_legacy_compat.lua
@@ -0,0 +1,53 @@
+function widget:GetInfo()
+	return {
+		name        = "Legacy Compatibility Layer",
+		desc        = "Bridges detached BAR.* modules back to Spring.* for older community widgets",
+		author      = "BAR Community",
+		date        = "2026",
+		license     = "GNU GPL, v2 or later",
+		layer       = -math.huge,
+		enabled     = true,
+		alwaysStart = true,
+	}
+end
+
+local function installBridge()
+	if not Spring then return end
+
+	if BAR then
+		Spring.I18N      = Spring.I18N or BAR.I18N
+		Spring.Utilities = Spring.Utilities or BAR.Utilities
+		Spring.Debug     = Spring.Debug or BAR.Debug
+		Spring.Lava      = Spring.Lava or BAR.Lava
+	end
+
+	local mt = getmetatable(Spring) or {}
+	if mt.__compat_installed then return end
+	mt.__compat_installed = true
+
+	local orig_index = mt.__index
+	mt.__index = function(t, key)
+		if orig_index then
+			local val = (type(orig_index) == "function") and orig_index(t, key) or orig_index[key]
+			if val ~= nil then return val end
+		end
+
+		if BAR and BAR[key] ~= nil then
+			rawset(t, key, BAR[key])
+			return BAR[key]
+		end
+		return nil
+	end
+
+	setmetatable(Spring, mt)
+end
+
+installBridge()
+
+function widget:Initialize()
+	installBridge()
+end
+
+function widget:CommandsChanged()
+	installBridge()
+end
