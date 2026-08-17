232b5c04343f32eb1ed1aa3797c4e0a6f877c7b5
efrec <quantumscrewdriver@gmail.com>
2026-08-17 17:30:29 -0400

    update construction_finished, copying allyTeam changes over

--- luarules/mission_api/triggers/construction_finished.lua
+++ luarules/mission_api/triggers/construction_finished.lua
@@ -23,12 +23,10 @@ return {
 			if parameters.unitDefName and parameters.unitDefName ~= UnitDefs[unitDefID].name then
 				return
 			end
-			-- We likely must guard against unusual cases like in-progress capture by enemy teams.
-			-- When a factory is captured while building, instead, it calls StopBuild immediately.
-			if parameters.teamID and parameters.teamID ~= unitTeam then
+			if parameters.teamID and not Spring.AreTeamsAllied(parameters.teamID, unitTeam) then
 				return
 			end
-			if not context.IsNanoframeOwner(unitID, parameters.builderDefName, parameters.builderName) then
+			if not context.isBuildFrameOwner(unitID, parameters.builderDefName, parameters.builderName) then
 				return
 			end
 			context.ActivateTrigger(trigger)
