667060726fa5c483eefe809a2ac78d67f67abdf7
efrec <quantumscrewdriver@gmail.com>
2026-08-17 17:29:28 -0400

    missed a nanoframe => buildfrmae

--- luarules/gadgets/api_missions_triggers.lua
+++ luarules/gadgets/api_missions_triggers.lua
@@ -185,7 +185,7 @@ function gadget:Initialize()
 		ActivateTrigger          = activateTrigger,
 		DoesUnitHaveName         = doesUnitHaveName,
 		DoesFeatureHaveName      = doesFeatureHaveName,
-		IsNanoframeOwner         = isBuildFrameOwner,
+		isBuildFrameOwner         = isBuildFrameOwner,
 		InFactory                = inFactory,
 		WasUnderConstruction     = underConstruction,
 		GetUnitsInArea           = getUnitsInArea,
