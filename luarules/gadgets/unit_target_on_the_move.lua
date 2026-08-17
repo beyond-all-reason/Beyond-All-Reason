287f6cbcd94d7f017f03fe3cf69c6110cbdf4640
efrec <quantumscrewdriver@gmail.com>
2026-08-16 23:28:22 -0400

    fix: clear currentTargets correctly when updating lists

--- luarules/gadgets/unit_target_on_the_move.lua
+++ luarules/gadgets/unit_target_on_the_move.lua
@@ -853,6 +853,7 @@ if gadgetHandler:IsSyncedCode() then
 			return
 		end
 		local targets, teamID, weapons = unitData.targets, unitData.teamID, unitData.weapons
+		local currentTargets = unitData.currentTargets
 		local targetCount = #targets
 		local activeIndex = 0
 		local updateIndex = 0 -- table.remove is slow, as is iterating forward then backward, so we do an erase-remove
@@ -874,7 +875,8 @@ if gadgetHandler:IsSyncedCode() then
 					targets[updateIndex] = targetData
 				end
 			else
-				SendToUnsynced("targetDrop", unitID, index)
+				currentTargets[targetData.target] = nil
+				SendToUnsynced("targetDrop", unitID, updateIndex + 1)
 			end
 		end
 		if updateIndex == 0 then
