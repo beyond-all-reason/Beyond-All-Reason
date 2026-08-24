b28494cbe4ba7bc1f234863379bb1368584fa686
efrec <ecfrechette@gmail.com>
2026-08-24 10:43:13 -0400

    Mission API: Build queue status (#8692)
    ### Work done
    
    Patches the `ConstructionStarted` and `ConstructionFinished` triggers to ignore spawned and resurrected units that were never being built.
    
    Adds unit tracking parameters to filter by the builder unit.
    
    Converts placed build orders that become a build-assist so they are seen by `ConstructionStarted`. This only activates the trigger when the trigger filters on builderName or builderDefName; otherwise, the first to place activates, always.
    
    Adds the `ContructionCanceled` and `ProductionCanceled` triggers to handle build orders that fail.
    
    - The trigger `ConstructionCanceled` has use-case nuisances but I kept it for now: Any builder can complete another builder's task (some caveats), even an enemy, under the right circumstances.
    - The trigger `ProductionCanceled` papers over the engine's split choice to cancel units in progress when the factory is captured but not when captured inside a factory. We consider the build to be canceled in both cases, plus when the factory is destroyed (and ofc when the unit itself is canceled).
    
    Remaining `Production*` family triggers are produced elsewhere, PR tk.
    
    ### Testing
    
    Specs and trigger scenario test.
    
    Added specs to the existing triggers as well.

--- luarules/mission_api/loadout.lua
+++ luarules/mission_api/loadout.lua
@@ -30,8 +30,10 @@ end
 local function spawnUnit(unit, pos)
 	-- Make unitName available to MetaUnitAdded call-in which gets triggered by Spring.CreateUnit
 	GG['MissionAPI'].nameOfUnitBeingSpawned = unit.unitName
+	GG['MissionAPI'].spawnedUnitIsBeingBuilt = unit.construction
 	local unitID = Spring.CreateUnit(unit.unitDefName, pos.x, pos.y, pos.z, unit.facing or 's', unit.team, unit.construction)
 	GG['MissionAPI'].nameOfUnitBeingSpawned = nil
+	GG['MissionAPI'].spawnedUnitIsBeingBuilt = nil
 
 	if unitID and unit.neutral then
 		Spring.SetUnitNeutral(unitID, true)
---
+++ luarules/mission_api/triggers/construction_canceled.lua
@@ -0,0 +1,40 @@
+local ParameterTypes = GG['MissionAPI'].Modules.ParameterTypes.Types
+
+-- Units canceled while in production at a factory belong to `ProductionCanceled`, instead.
+
+-- This is very difficult to track if we mean anything outside "in-progress unit destroyed";
+-- nevertheless, this cancels on unit capture to match the behavior of `ProductionCanceled`.
+
+-- Builders can die yet construction does not "cancel" - other builders can finish the unit.
+-- Builders can cancel the command, yet may resume it, or keep the command but be in a Wait.
+
+return {
+	type = 'ConstructionCanceled',
+	parameters = {
+		{ name = 'unitName',    required = false, type = ParameterTypes.UnitName },
+		{ name = 'unitDefName', required = false, type = ParameterTypes.UnitDefName },
+		{ name = 'teamID',      required = false, type = ParameterTypes.TeamID },
+		requiresOneOf = { 'unitName', 'unitDefName' },
+	},
+	callins = {
+		-- Construction doesn't have the ambiguous decision-making between factory/buildee capture
+		-- but we copy the behavior of ProductionCanceled anyway so that this remains unsurprising.
+		MetaUnitRemoved = function(trigger, triggerID, context, unitID, unitDefID, unitTeam)
+			if not Spring.GetUnitIsBeingBuilt(unitID) or context.InFactory(unitID) then
+				return
+			end
+
+			local parameters = trigger.parameters
+			if parameters.unitName and not context.DoesUnitHaveName(unitID, parameters.unitName) then
+				return
+			end
+			if parameters.unitDefName and parameters.unitDefName ~= UnitDefs[unitDefID].name then
+				return
+			end
+			if parameters.teamID and parameters.teamID ~= unitTeam then
+				return
+			end
+			context.ActivateTrigger(trigger)
+		end,
+	},
+}
