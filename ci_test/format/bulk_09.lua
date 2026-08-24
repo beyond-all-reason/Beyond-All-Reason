95b5508ca1d3dab60b5d1a19eb6fdd2df8a663c7
efrec <quantumscrewdriver@gmail.com>
2026-08-24 13:28:09 -0400

    remove bad files

--- ci_test/format/bulk_09.lua
+++
@@ -1,6 +0,0 @@
--- CI fixture. Deliberately broken; delete ci_test/ before merging.
--- 1 misformatted block(s).
-
-local function  chunk_1 ( a,b )
-	return  {a,b, 'value' }
-end
--- ci_test/types/broken.lua
+++
@@ -1,4 +0,0 @@
--- CI fixture. Deliberately broken; delete ci_test/ before merging.
-local function oops(
-	return 1
-end
