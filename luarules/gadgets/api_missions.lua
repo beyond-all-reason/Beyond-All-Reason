b4e128ce47140c336b5824ba6ede55c62e9ad992
efrec <quantumscrewdriver@gmail.com>
2026-08-26 19:10:44 -0400

    reset tests

--- luarules/gadgets/api_missions.lua
+++ luarules/gadgets/api_missions.lua
@@ -66,7 +66,8 @@ function gadget:Initialize()
 	--local scriptPath = 'mission-api-tests/loadout_test.lua'
 	--local scriptPath = 'mission-api-tests/stages_and_objectives_test.lua'
 	--local scriptPath = 'mission-api-tests/unit_mover_test.lua'
-	local scriptPath = 'mission-api-tests/unit_detection_test.lua'
+	-- local scriptPath = 'mission-api-tests/unit_detection_test.lua'
+	-- local scriptPath = 'mission-api-tests/unit_triggers_test.lua'
 
 	if not scriptPath then
 		gadgetHandler:RemoveGadget()
