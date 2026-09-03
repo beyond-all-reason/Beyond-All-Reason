4e6ff2953cad73e83c489353d1dbacb020c189a9
TheDujin <kevindujin@gmail.com>
2026-09-03 00:17:38 -0700

    Send analytics only at the end of the game to prevent info leaks

--- luaui/Widgets/ana_dgun_griefing_prevention.lua
+++ luaui/Widgets/ana_dgun_griefing_prevention.lua
@@ -13,14 +13,31 @@ function widget:GetInfo()
 end
 
 local USE_WG_ANALYTICS = false -- set false to echo events locally while debugging
+local cachedEvents = {}
 
 local function DGunGriefingDetection(eventType, eventData)
-	if USE_WG_ANALYTICS and WG and WG.Analytics and WG.Analytics.SendEvent then
-		WG.Analytics.SendEvent(eventType, eventData)
+	if USE_WG_ANALYTICS then
+		cachedEvents[#cachedEvents + 1] = {
+			eventType = eventType,
+			eventData = eventData,
+		}
 		return
 	end
 
 	Spring.Echo(string.format("[DGunAnalytics] %s %s", eventType, table.toString(eventData)))
+end
+
+function widget:GameOver()
+	if not USE_WG_ANALYTICS or not WG or not WG.Analytics or not WG.Analytics.SendEvent then
+		return
+	end
+
+	for i = 1, #cachedEvents do
+		local event = cachedEvents[i]
+		WG.Analytics.SendEvent(event.eventType, event.eventData)
+	end
+
+	cachedEvents = {}
 end
 
 function widget:Initialize()
