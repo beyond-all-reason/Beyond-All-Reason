0cc40550a3e152c1409f3b8c791f1b8ca11e7a5d
WatchTheFort <36827423+WatchTheFort@users.noreply.github.com>
2026-09-07 12:59:46 +0300

    Add difficulties JSON, include as enum

--- luarules/mission_api/parameter_types.lua
+++ luarules/mission_api/parameter_types.lua
@@ -30,6 +30,7 @@ local types = {
 	WeaponDefName = "WeaponDefName",
 	Facing = "Facing",
 	SoundFile = "SoundFile",
+	Difficulty = "Difficulty",
 
 	-- Number Validators:
 	Number = "Number",
@@ -49,6 +50,10 @@ local types = {
 
 }
 
+-- Difficulties are read by the client, so must be available in JSON
+local difficultiesJSON = VFS.LoadFile("luarules/mission_api/difficulties.json")
+local difficulties = Json.decode(difficultiesJSON)
+
 local enums = {
 	[types.Facing] = {
 		[0] = true,
@@ -64,6 +69,8 @@ local enums = {
 		east = true,
 		west = true,
 	},
+
+	[types.Difficulty] = difficulties,
 }
 
 local enumSets = {
