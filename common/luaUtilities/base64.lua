a048ed05625f66644aa8d20e57dc3b734895babf
Rasoul <smarkert@gmail.com>
2026-09-08 20:55:36 +0200

    Review comments by @efrec: improve comment

--- common/luaUtilities/base64.lua
+++ common/luaUtilities/base64.lua
@@ -61,7 +61,7 @@ end
 
 -- decoding table
 -- The url alphabet ('-' and '_') is what base64Encode above emits and what the modoptions transport specifies.
--- '+' and '/' are accepted as aliases.
+-- '+' and '/' are accepted as aliases, making this decode both base64 and base64url.
 local base64bytes = {
 	['A'] = 0, ['B'] = 1, ['C'] = 2, ['D'] = 3, ['E'] = 4, ['F'] = 5, ['G'] = 6, ['H'] = 7, ['I'] = 8, ['J'] = 9,
 	['K'] = 10, ['L'] = 11, ['M'] = 12, ['N'] = 13, ['O'] = 14, ['P'] = 15, ['Q'] = 16, ['R'] = 17, ['S'] = 18,
