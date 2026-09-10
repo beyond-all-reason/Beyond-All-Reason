4d137614d185122ce7c9b81bb592b71b95a4ab87
efrec <quantumscrewdriver@gmail.com>
2026-09-10 12:48:36 -0400

    fix: Disable Air Units removes air-unit spawning drone weapons (only)

--- gamedata/alldefs_post.lua
+++ gamedata/alldefs_post.lua
@@ -205,6 +205,17 @@ end
 -- MODULE FUNCTIONS
 -------------------------
 
+local function spawnsAirUnit(carriedUnits)
+	for carriedName in string.gmatch(carriedUnits, "%S+") do
+		-- Scav units still have the base unit here but check against both sets anyway.
+		local carriedDef = UnitDefs[carriedName] or UnitDefs[(string.gsub(carriedName, "_scav$", ""))] ---@as table
+		if carriedDef and carriedDef.canfly then
+			return true
+		end
+	end
+	return false
+end
+
 local function unitDef_Post(name, uDef)
 	local isScav = string.sub(name, -5, -1) == "_scav"
 	local basename = isScav and string.sub(name, 1, -6) or name
@@ -311,8 +322,30 @@ local function unitDef_Post(name, uDef)
 			customparams.modoption_blocked = true
 		elseif uDef.canfly then
 			customparams.modoption_blocked = true
-		elseif customparams.restrictions_inclusion and string.find(customparams.restrictions_inclusion, "_noair_") then --used to remove factories and drone carriers with no other purpose (ex. leghive but not rampart)
+		elseif customparams.restrictions_inclusion and string.find(customparams.restrictions_inclusion, "_noair_") then --used to remove factories with no other purpose (ex. legap)
 			customparams.modoption_blocked = true
+		else
+			local strippedDrones = false
+			for weaponName, weaponDef in pairs(weapondefs) do
+				local carriedUnit = weaponDef.customparams and weaponDef.customparams.carried_unit
+				if carriedUnit and spawnsAirUnit(carriedUnit) then
+					weapondefs[weaponName] = nil
+					strippedDrones = true
+
+					-- Make a minimal effort toward cost adjustments:
+					local count = weaponDef.customparams.startingdronecount
+					if count then
+						uDef.metalcost = (uDef.metalcost or 0) - count * (carriedUnit.metalcost or 0)
+						uDef.energycost = (uDef.energycost or 0) - count * (carriedUnit.energycost or 0)
+					end
+					uDef.metalcost = (uDef.metalcost or 0) * 0.95
+					uDef.energycost = (uDef.energycost or 0) * 0.95
+				end
+			end
+			-- Keep drone spawners that have other weapons:
+			if strippedDrones and not next(weapondefs) then
+				customparams.modoption_blocked = true
+			end
 		end
 	end
 
