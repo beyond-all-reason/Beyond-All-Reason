719f7ac14d0b7167efba25125e9ff1fc7320e42a
Aron Wieck <aw@crown.de>
2026-09-11 05:10:20 +0200

    Fix command modifiers being lost when dragging waypoints

--- luaui/Widgets/unit_waypoint_dragger_2.lua
+++ luaui/Widgets/unit_waypoint_dragger_2.lua
@@ -176,6 +176,7 @@ local function MoveWayPoints(wpTbl, mx, my, finalize)
 			local cmdLink = wpData[5]
 			local cmdID = wpData[6].id
 			local cmdTag = wpData[6].tag
+			local cmdOptions = wpData[6].options.coded
 			local cmdUnitID = wpData[7]
 
 			if finalize then
@@ -183,11 +184,9 @@ local function MoveWayPoints(wpTbl, mx, my, finalize)
 					cmdLink = cmdTag
 				end
 				if cmdFacRad > 0 then
-					-- spGiveOrderToUnit(cmdUnitID, CMD.INSERT, {cmdNum, cmdID, 0, cx, cy, cz, cmdFacRad}, {"alt"})
-					spGiveOrderToUnit(cmdUnitID, CMD.INSERT, { cmdLink, cmdID, 0, cx, cy, cz, cmdFacRad }, 0)
+					spGiveOrderToUnit(cmdUnitID, CMD.INSERT, { cmdLink, cmdID, cmdOptions, cx, cy, cz, cmdFacRad }, 0)
 				else
-					-- spGiveOrderToUnit(cmdUnitID, CMD.INSERT, {cmdNum, cmdID, 0, cx, cy, cz}, {"alt"})
-					spGiveOrderToUnit(cmdUnitID, CMD.INSERT, { cmdLink, cmdID, 0, cx, cy, cz }, 0)
+					spGiveOrderToUnit(cmdUnitID, CMD.INSERT, { cmdLink, cmdID, cmdOptions, cx, cy, cz }, 0)
 				end
 				if not alt then
 					spGiveOrderToUnit(cmdUnitID, CMD.REMOVE, { cmdTag }, 0)
