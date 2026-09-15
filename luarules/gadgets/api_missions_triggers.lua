275d0c168ce70643c5aa23f62843cd9f8c4a0270
efrec <ecfrechette@gmail.com>
2026-09-15 02:54:14 -0400

    Mission API: Add trigger on mission started (#9208)
    Adds the `MissionStarted` trigger, dispatch, and spec.

--- luarules/gadgets/api_missions_triggers.lua
+++ luarules/gadgets/api_missions_triggers.lua
@@ -316,6 +316,10 @@ function gadget:Initialize()
 	end
 end
 
+function gadget:GameStart()
+	dispatchTriggerCallin("GameStart")
+end
+
 function gadget:GameFrame(frameNumber)
 	if frameNumber % Game.gameSpeed == 0 then
 		-- Reset reclaim income counters (read by ResourceIncome handlers):
