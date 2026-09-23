04c1e71efe26aae04eec8de8bc5d681f56eed01b
efrec <quantumscrewdriver@gmail.com>
2026-09-23 13:16:58 -0400

    feat: unit attribute definitions, controller, api
    Provides a very-basic effects system for applying multiple modifiers and overrides to unit attributes.
    
    Provides a universal get/set interface for unit attributes for capability narrowing e.g. for mission-api.
    
    A factor can attribute (the other kind) its effect to a "source", which can be scoped to the unit, unitdef, or unitdef-and-team. The controller composes these effects across their scopes and sources and applies them.

---
+++ luarules/gadgets/include/unit_attributes.lua
@@ -0,0 +1,45 @@
+-- unit_attributes.lua ---------------------------------------------------------
+
+---@class UnitAttributeDefinition
+---@field type "number"|"boolean"|"string"
+---@field canBeNegative? boolean
+---@field mobileOnly? boolean
+---@field builderOnly? boolean
+---@field multiplyOnly? boolean Its baseline value is always 1.0. Will drop any `set` operations.
+---@field isUnitState? boolean Has no baseline value. Drops any `multiply` and any unitdef scope.
+
+---@type table<string, UnitAttributeDefinition>
+local definitions = {
+	losRadius = { type = "number" },
+	airLosRadius = { type = "number" },
+	radarRadius = { type = "number" },
+	sonarRadius = { type = "number" },
+	seismicRadius = { type = "number" },
+	jammerRadius = { type = "number" },
+	sonarJamRadius = { type = "number" },
+	health = { type = "number", isUnitState = true },
+	maxHealth = { type = "number" },
+	speed = { type = "number", mobileOnly = true },
+	maxWantedSpeed = { type = "number", mobileOnly = true },
+	turnRate = { type = "number", mobileOnly = true },
+	maxAcc = { type = "number", mobileOnly = true },
+	maxDec = { type = "number", mobileOnly = true },
+	buildSpeed = { type = "number", builderOnly = true },
+	metalCost = { type = "number" },
+	energyCost = { type = "number" },
+	buildTime = { type = "number" },
+	mass = { type = "number" },
+	stealth = { type = "boolean" },
+	sonarStealth = { type = "boolean" },
+	seismicSignature = { type = "number" },
+	tooltip = { type = "string" },
+	maxWeaponRange = { type = "number" },
+	reloadTime = { type = "number" },
+	experience = { type = "number", isUnitState = true },
+	cloaked = { type = "boolean", isUnitState = true },
+	damage = { type = "number", multiplyOnly = true },
+}
+
+return {
+	Definitions = definitions,
+}
---
+++ types/CalloutParamsTables.lua
@@ -0,0 +1,10 @@
+---@meta
+
+-- Collector file for various params tables passed as arguments to engine callouts.
+
+---@class BuilderSpeeds
+---@field repair number
+---@field reclaim number
+---@field resurrect number
+---@field capture number
+---@field terraform number
