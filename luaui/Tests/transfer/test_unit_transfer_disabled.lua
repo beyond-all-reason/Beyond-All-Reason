keithharvey/bar

transfer: what may pass between allied teams
Units, resources and an empty team's assets all move by the same
question — may this pass, at what cost, and who is told. One pipeline
answers it, so there is a single place where a transfer is validated,
priced and announced rather than one per thing being moved.

Sharing was never a domain. It was a feature name over this pipeline
plus four construction gadgets and a tech feature, which is why every
boundary drawn around it failed: allied is a modifier on other domains'
policies, not a domain of its own. What is left once those leave is one
system, and transfer is what it does.

The tax rate comes with it. What a transfer costs is a property of the
transfer; it reads the tier tech owns, and that edge runs one way.

Shared spec builders gain the fixtures these specs need. They are only
used here, so they travel with the module that needs them.

luaui/Tests/sharing/test_unit_transfer_disabled.lua
@@ -1,0 +1,79 @@
+---@diagnostic disable: lowercase-global, undefined-field, duplicate-set-field
+
+local UnitTransferUnsynced = VFS.Include("modules/sharing/unit/unsynced.lua")
+
+local function GetAlliedTargetTeamID(myTeamID)
+	local teamList = Spring.GetTeamList()
+	for _, teamID in ipairs(teamList) do
+		if teamID ~= myTeamID and Spring.AreTeamsAllied(myTeamID, teamID) then
+			return teamID
+		end
+	end
+	return nil
+end
+
+local function skip()
+	return Spring.GetGameFrame() <= 0
+end
+
+local function setup()
+	Test.clearMap()
+
+	SyncedRun(function()
+		---@diagnostic disable-next-line: global-in-non-module
+		_G._origGetModOptions = _G._origGetModOptions or Spring.GetModOptions
+		Spring.GetModOptions = function()
+			local opts = _G._origGetModOptions()
+			opts.unit_sharing_mode = "none"
+			return opts
+		end
+	end, 60)
+
+	-- Wait for the unit transfer controller's policy cache to refresh (every 150 frames)
+	Test.waitFrames(160)
+end
+
+local function cleanup()
+	SyncedRun(function()
+		local orig = _G._origGetModOptions --[[@as function?]]
+		if orig then
+			Spring.GetModOptions = orig
+			---@diagnostic disable-next-line: global-in-non-module
+			_G._origGetModOptions = nil
+		end
+	end, 60)
+
+	-- Let the cache refresh back to the real mod options
+	Test.waitFrames(160)
+
+	Test.clearMap()
+end
+
+local function test()
+	local team0 = Spring.GetLocalTeamID()
+	local team1 = GetAlliedTargetTeamID(team0)
+	assert(team1 ~= nil, "expected at least one allied target team for transfer test")
+
+	local unitID = SyncedRun(function(locals)
+		local x, z = Game.mapSizeX / 2, Game.mapSizeZ / 2
+		local y = Spring.GetGroundHeight(x, z)
+		return Spring.CreateUnit("armpw", x, y, z, "south", locals.team0)
+	end, 60)
+
+	assert(unitID, "unit should have been created")
+	assert(Spring.GetUnitTeam(unitID) == team0, "unit should start on team0")
+
+	Spring.SelectUnitArray({ unitID })
+	UnitTransferUnsynced.ShareUnits(team1)
+
+	Test.waitFrames(10)
+
+	assert(Spring.GetUnitTeam(unitID) == team0, "unit should still belong to team0 when sharing is disabled, got team: " .. tostring(Spring.GetUnitTeam(unitID)))
+end
+
+return {
+	skip = skip,
+	setup = setup,
+	cleanup = cleanup,
+	test = test,
+}

beyond-all-reason/Beyond-All-Reason

luaui/Tests/transfer/test_unit_transfer_disabled.lua
@@ -1,0 +1,80 @@
+---@diagnostic disable: lowercase-global, undefined-field, duplicate-set-field
+
+local UnitTransferUnsynced = VFS.Include("modules/transfer/unit/unsynced.lua")
+
+local function GetAlliedTargetTeamID(myTeamID)
+	local teamList = Spring.GetTeamList()
+	for _, teamID in ipairs(teamList) do
+		if teamID ~= myTeamID and Spring.AreTeamsAllied(myTeamID, teamID) then
+			return teamID
+		end
+	end
+	return nil
+end
+
+local function skip()
+	return Spring.GetGameFrame() <= 0
+end
+
+local function setup()
+	Test.clearMap()
+
+	SyncedRun(function()
+		---@diagnostic disable-next-line: global-in-non-module
+		_G._origGetModOptions = _G._origGetModOptions or Spring.GetModOptions
+		Spring.GetModOptions = function()
+			local opts = BAR.GetModOptionsCopy()
+			opts.unit_sharing_mode = "none"
+			return opts
+		end
+	end, 60)
+
+	Test.waitFrames(160)
+end
+
+local function cleanup()
+	SyncedRun(function()
+		local orig = _G._origGetModOptions --[[@as function?]]
+		if orig then
+			Spring.GetModOptions = orig
+			---@diagnostic disable-next-line: global-in-non-module
+			_G._origGetModOptions = nil
+		end
+	end, 60)
+
+	Test.waitFrames(160)
+
+	Test.clearMap()
+end
+
+local function test()
+	local team0 = Spring.GetLocalTeamID()
+	local team1 = GetAlliedTargetTeamID(team0)
+	assert(team1 ~= nil, "expected at least one allied target team for transfer test")
+
+	local unitID = SyncedRun(function(locals)
+		local x, z = Game.mapSizeX / 2, Game.mapSizeZ / 2
+		local y = Spring.GetGroundHeight(x, z)
+		return Spring.CreateUnit("armpw", x, y, z, "south", locals.team0)
+	end, 60)
+
+	assert(unitID, "unit should have been created")
+	assert(Spring.GetUnitTeam(unitID) == team0, "unit should start on team0")
+
+	Spring.SelectUnitArray({ unitID })
+	UnitTransferUnsynced.ShareUnits(team1)
+
+	Test.waitFrames(10)
+
+	assert(
+		Spring.GetUnitTeam(unitID) == team0,
+		"unit should still belong to team0 when sharing is disabled, got team: " .. tostring(Spring.GetUnitTeam(unitID))
+	)
+end
+
+return {
+	skip = skip,
+	setup = setup,
+	cleanup = cleanup,
+	test = test,
+}