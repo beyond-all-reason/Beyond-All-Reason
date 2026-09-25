f046bbeba3c6fdca9080dc6ff6c1108cd4b4a092
efrec <quantumscrewdriver@gmail.com>
2026-09-24 22:04:36 -0400

    move the starburst trajectories into a module

--- luaui/Widgets/gui_attack_aoe.lua
+++ luaui/Widgets/gui_attack_aoe.lua
@@ -58,6 +58,11 @@ local spGetViewGeometry = Spring.GetViewGeometry
 local spIsAboveMiniMap = Spring.IsAboveMiniMap
 local spTraceRayGroundBetweenPositions = Spring.TraceRayGroundBetweenPositions
 
+local Starburst = VFS.Include("modules/starburst.lua")
+local getStarburstWeapon = Starburst.getStarburstWeapon
+local newStarburst = Starburst.newStarburst
+local stepStarburst = Starburst.stepStarburst
+
 local CMD_ATTACK = CMD.ATTACK
 local CMD_UNIT_SET_TARGET = GameCMD.UNIT_SET_TARGET
 local CMD_UNIT_SET_TARGET_NO_GROUND = GameCMD.UNIT_SET_TARGET_NO_GROUND
@@ -482,15 +487,7 @@ local function GetStarburstGroundCollisionPos(
 		dirX, dirY, dirZ = weaponDirX, weaponDirY, weaponDirZ
 	end
 
-	local speed = weaponInfo.startVelocity
 	local maxSpeed = weaponInfo.projectileSpeed
-	local acceleration = weaponInfo.weaponAcceleration
-	local turnRate = weaponInfo.turnRate
-	if turnRate == 0 then
-		turnRate = 0.06
-	end
-	local tracking = weaponInfo.tracking
-	local maxGoodDif = cos(tracking * 0.6)
 	targetVelocityX, targetVelocityY, targetVelocityZ = targetVelocityX or 0, targetVelocityY or 0, targetVelocityZ or 0
 	local targetMoves = weaponInfo.tracks and (targetVelocityX ~= 0 or targetVelocityY ~= 0 or targetVelocityZ ~= 0)
 	if not weaponInfo.tracks and (targetVelocityX ~= 0 or targetVelocityY ~= 0 or targetVelocityZ ~= 0) then
@@ -518,7 +515,8 @@ local function GetStarburstGroundCollisionPos(
 	end
 	-- The engine decrements uptime before the first trajectory update.
 	local ascentFrames = max(0, ceil(weaponInfo.uptime * Config.General.gameSpeed) - 1)
-	local turnToTarget = true
+	local starburstWeapon = weaponInfo.starburst
+	local starburst = newStarburst(dirX, dirY, dirZ, weaponInfo.startVelocity, ascentFrames, true)
 
 	for frame = 1, 512 do
 		if targetMoves then
@@ -535,56 +533,27 @@ local function GetStarburstGroundCollisionPos(
 			return tx, ty, tz, pathCount
 		end
 
-		if ascentFrames > 0 then
-			speed = min(speed + acceleration, maxSpeed)
-			ascentFrames = ascentFrames - 1
-		else
-			local targetDirX, targetDirY, targetDirZ =
-				targetDX / targetLength, targetDY / targetLength, targetDZ / targetLength
-			local directionDotTarget = dirX * targetDirX + dirY * targetDirY + dirZ * targetDirZ
-			local steerRate
-			if turnToTarget then
-				if directionDotTarget > 0.99 then
-					dirX, dirY, dirZ = targetDirX, targetDirY, targetDirZ
-					turnToTarget = false
-				else
-					steerRate = turnRate
-				end
-			else
-				speed = min(speed + acceleration, maxSpeed)
-				if directionDotTarget > maxGoodDif then
-					dirX, dirY, dirZ = targetDirX, targetDirY, targetDirZ
-				elseif tracking > 0 then
-					steerRate = tracking
-				end
-			end
-			if steerRate then
-				local turnX = targetDirX - dirX * directionDotTarget
-				local turnY = targetDirY - dirY * directionDotTarget
-				local turnZ = targetDirZ - dirZ * directionDotTarget
-				local turnLength = sqrt(turnX * turnX + turnY * turnY + turnZ * turnZ)
-				if turnLength > 0 then
-					turnX, turnY, turnZ = turnX / turnLength, turnY / turnLength, turnZ / turnLength
-					dirX = dirX + turnX * steerRate
-					dirY = dirY + turnY * steerRate
-					dirZ = dirZ + turnZ * steerRate
-					local directionLength = sqrt(dirX * dirX + dirY * dirY + dirZ * dirZ)
-					dirX, dirY, dirZ = dirX / directionLength, dirY / directionLength, dirZ / directionLength
-				end
-			end
-			if not turnToTarget and not targetMoves then
-				local hitDistance, hitX, hitY, hitZ = spTraceRayGroundBetweenPositions(px, py, pz, tx, ty, tz, false)
-				if hitDistance and hitDistance + 8 < targetLength then
-					pathCount = pathCount + 1
-					pathX[pathCount], pathY[pathCount], pathZ[pathCount] = hitX, hitY, hitZ
-					return hitX, hitY, hitZ, pathCount
-				end
+		stepStarburst(
+			starburst,
+			starburstWeapon,
+			targetDX / targetLength,
+			targetDY / targetLength,
+			targetDZ / targetLength
+		)
+		if not starburst.turnToTarget and not targetMoves then
+			local hitDistance, hitX, hitY, hitZ = spTraceRayGroundBetweenPositions(px, py, pz, tx, ty, tz, false)
+			if hitDistance and hitDistance + 8 < targetLength then
 				pathCount = pathCount + 1
-				pathX[pathCount], pathY[pathCount], pathZ[pathCount] = tx, ty, tz
-				return tx, ty, tz, pathCount
+				pathX[pathCount], pathY[pathCount], pathZ[pathCount] = hitX, hitY, hitZ
+				return hitX, hitY, hitZ, pathCount
 			end
+			pathCount = pathCount + 1
+			pathX[pathCount], pathY[pathCount], pathZ[pathCount] = tx, ty, tz
+			return tx, ty, tz, pathCount
 		end
 
+		local speed = starburst.speed
+		dirX, dirY, dirZ = starburst.dirX, starburst.dirY, starburst.dirZ
 		local nextX, nextY, nextZ = px + dirX * speed, py + dirY * speed, pz + dirZ * speed
 		local groundY = spGetGroundHeight(nextX, nextZ)
 		if groundY and nextY < groundY then
@@ -600,7 +569,7 @@ local function GetStarburstGroundCollisionPos(
 				return hitX, hitY, hitZ, pathCount
 			end
 		end
-		if not turnToTarget and targetLength <= speed + 8 then
+		if not starburst.turnToTarget and targetLength <= speed + 8 then
 			pathCount = pathCount + 1
 			pathX[pathCount], pathY[pathCount], pathZ[pathCount] = tx, ty, tz
 			return tx, ty, tz, pathCount
@@ -1127,14 +1096,12 @@ local function BuildWeaponInfo(unitDef, weaponDef, weaponNum)
 		info.isStarburst = true
 		info.projectileSpeed = weaponDef.projectilespeed
 		info.startVelocity = weaponDef.startvelocity
-		info.weaponAcceleration = weaponDef.weaponAcceleration
 		info.uptime = weaponDef.uptime
-		info.turnRate = weaponDef.turnRate
-		info.tracking = weaponDef.tracks and (weaponDef.turnRate or 0) or 0
 		info.tracks = weaponDef.tracks
 		info.leadLimit = weaponDef.leadLimit or -1
 		info.leadBonus = weaponDef.leadBonus or 0
 		info.fixedLauncher = weaponDef.fixedLauncher
+		info.starburst = getStarburstWeapon(weaponDef)
 		-- Check for nuclear weapons (customParams.nuclear)
 		if info.isNuke then
 			info.type = "nuke"
